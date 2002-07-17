3514b16844adc4f1fba9469b7aeb1ceb74d32327
Harald Meland <hmeland@ulrik.uio.no>
2002-07-17 16:23:23 +0000

    ou: Added dummy column `entity_type' to allow entity type check   against the `entity_id' table.
    ou_address, ou_phone and ou_contact_info: Removed.  Use
      entity_address, entity_phone and entity_contact_info instead, as
      none of these are restricted to any one type of entity.
    
    stedkode: Moved into a separate module.

--- design/ou_tables.sql
+++ design/ou_tables.sql
@@ -1,23 +1,9 @@
-/*** Note:
- *** The tables in code_value_tables.sql must have been created prior to
- *** the tables found here.
- ***/
-
-/* TBD: Dersom alle entitets-IDer har lik datatype (som
-	f.eks. "NUMERIC(12,0)"), kan man benytte en og samme sekvens
-	til å hente ny ID, uavhengig av hvilken type entitet IDen skal
-	representere.  På denne måten vil man ende opp med at en gitt
-	ID kun finnes for _en_ entitet i hele systemet.
-
-	Er dette lurt/nødvendig/ønskelig? */
-
-
 /*	ou
 
   This table defines what Organizational Units (OUs) the institution
   is made up of.  It does not say anything about how these OUs relate
   to each other (i.e. the organizational structure); see the table
-  ou_structure below for that.
+  `ou_structure' below for that.
 
   The names kept in this table should be in the default language for
   this installation.
@@ -25,27 +11,35 @@
  */
 CREATE TABLE ou
 (
+  /* Dummy column, needed for type check against `entity_id'. */
+  entity_type	CHAR VARYING(16)
+		NOT NULL
+		DEFAULT 'o'
+		CONSTRAINT ou_entity_type_chk CHECK (entity_type = 'o'),
+
   ou_id		NUMERIC(12,0)
 		CONSTRAINT ou_pk PRIMARY KEY,
   name		CHAR VARYING(512) NOT NULL,
   acronym	CHAR VARYING(15),
   short_name	CHAR VARYING(30),
   display_name	CHAR VARYING(80),
-  sort_name	CHAR VARYING(80)
+  sort_name	CHAR VARYING(80),
+  CONSTRAINT ou_entity_id FOREIGN KEY (entity_type, ou_id)
+    REFERENCES entity_id(entity_type, id)
 );
 
 
 /*	ou_structure
 
   What the organization structure (or structures, if there exists more
-  than one "perspective") looks like is defined by the data in this
+  than one `perspective') looks like is defined by the data in this
   table.
 
   Note that the structure(s) are built using nothing but the numeric,
-  strictly internal ID for OUs, and therefore are independent of
-  whatever "OU identifiers" the authoritative data sources use.
+  strictly internal OU IDs, and therefore are independent of whatever
+  OU identifiers the authoritative data sources use.
 
-  Root nodes are identified by NULL parent_id.
+  Root nodes are identified by NULL `parent_id'.
 
 */
 CREATE TABLE ou_structure
@@ -84,109 +78,4 @@ CREATE TABLE ou_name_language
   display_name	CHAR VARYING(80),
   sort_name	CHAR VARYING(80),
   CONSTRAINT ou_name_language_pk PRIMARY KEY (ou_id, language_code)
-);
-
-
-/*	ou_address
-
-
-
-*/
-CREATE TABLE ou_address
-(
-  ou_id		NUMERIC(12,0)
-		CONSTRAINT ou_address_ou_id REFERENCES ou(ou_id),
-  address_type	CHAR VARYING(16)
-		CONSTRAINT ou_address_address_type
-		  REFERENCES address_code(code),
-  aline1	CHAR VARYING(80),
-  aline2	CHAR VARYING(80),
-  aline3	CHAR VARYING(80),
-  aline4	CHAR VARYING(80),
-  p_o_box	CHAR VARYING(10),
-  postal_number	CHAR VARYING(8),
-  city		CHAR VARYING(128),
-  country	CHAR VARYING(128),
-  CONSTRAINT ou_address_pk PRIMARY KEY (ou_id, address_type)
-);
-
-
-/*	ou_phone
-
-  phone_number should contain no dashes/letters/whitespace, and should
-  be fully specified.  Examples: "+4712345678" and "*12345".
-
-*/
-CREATE TABLE ou_phone
-(
-  ou_id		NUMERIC(12,0)
-		CONSTRAINT ou_phone_ou_id REFERENCES ou(ou_id),
-  phone_type	CHAR VARYING(16)
-		CONSTRAINT ou_phone_phone_type
-		  REFERENCES phone_type_code(code),
-  phone_pref	NUMERIC(2,0),
-  phone_number	CHAR VARYING(20)
-		NOT NULL,
-  description	CHAR VARYING(512),
-  CONSTRAINT ou_phone_pk PRIMARY KEY (ou_id, phone_type, phone_pref)
-);
-
-
-/*	ou_contact_info
-
-
-
-*/
-CREATE TABLE ou_contact_info
-(
-  ou_id		NUMERIC(12,0)
-		CONSTRAINT ou_contact_info_ou_id REFERENCES ou(ou_id),
-  contact_type	CHAR VARYING(16)
-		CONSTRAINT ou_contact_info_contact_type
-		  REFERENCES contact_info_code(code),
-  contact_pref	NUMERIC(2,0),
-  contact_value	CHAR VARYING(255)
-		NOT NULL,
-  description	CHAR VARYING(512),
-  CONSTRAINT ou_contact_info_pk
-    PRIMARY KEY (ou_id, contact_type, contact_pref)
-);
-
-
-
-/***********************************************************************
- *** Modules related to OU. ********************************************
- ***********************************************************************/
-
-
-/***
-
- *** Module `stedkode' -- specific to the higher education sector in
- *** Norway.
-
- ***/
-
-/*	stedkode
-
-
-
-*/
-CREATE TABLE stedkode
-(
-  ou_id		NUMERIC(12,0)
-		CONSTRAINT stedkode_pk PRIMARY KEY
-		CONSTRAINT stedkode_ou_id REFERENCES ou(ou_id),
-  institusjon	NUMERIC(4,0)
-		NOT NULL,
-  fakultet	NUMERIC(2,0)
-		NOT NULL,
-  institutt	NUMERIC(2,0)
-		NOT NULL,
-  avdeling	NUMERIC(2,0)
-		NOT NULL,
-  katalog_merke	CHAR(1)
-		NOT NULL
-		CONSTRAINT stedkode_katalog_merke_bool
-		  CHECK (katalog_merke IN ('T', 'F')),
-  CONSTRAINT stedkode_kode UNIQUE (institusjon, fakultet, institutt, avdeling)
 );
