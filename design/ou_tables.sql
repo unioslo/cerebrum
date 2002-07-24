0403f63b015647b5989d66498dfe3202be596a76
Harald Meland <hmeland@ulrik.uio.no>
2002-07-24 19:50:57 +0000

    Renamed table   ou	-> ou_info

--- design/ou_tables.sql
+++ design/ou_tables.sql
@@ -1,4 +1,4 @@
-/*	ou
+/*	ou_info
 
   This table defines what Organizational Units (OUs) the institution
   is made up of.  It does not say anything about how these OUs relate
@@ -9,23 +9,25 @@
   this installation.
 
  */
-CREATE TABLE ou
+CREATE TABLE ou_info
 (
   /* Dummy column, needed for type check against `entity_id'. */
   entity_type	CHAR VARYING(16)
 		NOT NULL
 		DEFAULT 'o'
-		CONSTRAINT ou_entity_type_chk CHECK (entity_type = 'o'),
+		CONSTRAINT ou_info_entity_type_chk
+		  CHECK (entity_type = 'o'),
 
   ou_id		NUMERIC(12,0)
-		CONSTRAINT ou_pk PRIMARY KEY,
+		CONSTRAINT ou_info_pk PRIMARY KEY,
   name		CHAR VARYING(512) NOT NULL,
   acronym	CHAR VARYING(15),
   short_name	CHAR VARYING(30),
   display_name	CHAR VARYING(80),
   sort_name	CHAR VARYING(80),
-  CONSTRAINT ou_entity_id FOREIGN KEY (entity_type, ou_id)
-    REFERENCES entity_id(entity_type, id)
+  CONSTRAINT ou_info_entity_id
+    FOREIGN KEY (entity_type, ou_id)
+    REFERENCES entity_info(entity_type, entity_id)
 );
 
 
@@ -45,13 +47,16 @@ CREATE TABLE ou
 CREATE TABLE ou_structure
 (
   ou_id		NUMERIC(12,0)
-		CONSTRAINT ou_structure_ou_id REFERENCES ou(ou_id),
+		CONSTRAINT ou_structure_ou_id
+		  REFERENCES ou_info(ou_id),
   perspective	CHAR VARYING(16)
 		CONSTRAINT ou_structure_perspective
 		  REFERENCES ou_perspective_code(code),
   parent_id	NUMERIC(12,0)
-		CONSTRAINT ou_structure_parent_id REFERENCES ou(ou_id),
-  CONSTRAINT ou_structure_pk PRIMARY KEY (ou_id, perspective),
+		CONSTRAINT ou_structure_parent_id
+		  REFERENCES ou_info(ou_id),
+  CONSTRAINT ou_structure_pk
+    PRIMARY KEY (ou_id, perspective),
   CONSTRAINT ou_structure_parent_node
     FOREIGN KEY (parent_id, perspective)
     REFERENCES ou_structure(ou_id, perspective)
@@ -68,14 +73,16 @@ CREATE TABLE ou_name_language
 (
   ou_id		NUMERIC(12,0)
 		CONSTRAINT ou_name_language_ou_id
-		  REFERENCES ou(ou_id),
+		  REFERENCES ou_info(ou_id),
   language_code	CHAR VARYING(16)
 		CONSTRAINT ou_name_language_language_code
 		  REFERENCES language_code(code),
-  name		CHAR VARYING(512) NOT NULL,
+  name		CHAR VARYING(512)
+		NOT NULL,
   acronym	CHAR VARYING(15),
   short_name	CHAR VARYING(30),
   display_name	CHAR VARYING(80),
   sort_name	CHAR VARYING(80),
-  CONSTRAINT ou_name_language_pk PRIMARY KEY (ou_id, language_code)
+  CONSTRAINT ou_name_language_pk
+    PRIMARY KEY (ou_id, language_code)
 );
