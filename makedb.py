8e966c27a9ae026f2845a1b1229f852f328e5534
Rune Frøysa <runefro@usit.uio.no>
2002-09-06 13:06:29 +0000

    Utility that creates the database tables and does some initial population

---
+++ makedb.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import sys
+import re
+
+from Cerebrum import Database
+
+def main():
+    Cerebrum = Database.connect(user="cerebrum")
+    makedbs(Cerebrum)
+
+
+def makedbs(Cerebrum):
+    for f in ('drop_core_tables.sql', 'core_tables.sql', 'core_data.sql', 'pop.sql', 'mod_stedkode.sql'):
+        runfile("design/%s" % f, Cerebrum)
+
+def runfile(fname, Cerebrum):
+    print "Reading file: <%s>" % fname
+    f = file(fname)
+    text = "".join(f.readlines())
+    long_comment = re.compile(r"/\*.*?\*/", re.DOTALL)
+    text = re.sub(long_comment, "", text)
+    line_comment = re.compile(r"--.*")
+    text = re.sub(line_comment, "", text)
+    text = re.sub(r"\s+", " ", text)
+    for ddl in text.split(";"):
+        ddl = ddl.strip()
+        if not ddl:
+            continue
+        try:
+            res = Cerebrum.execute(ddl)
+        except:
+            print "CMD: [%s] -> " % ddl
+            print "  database error:", sys.exc_info()[1]
+        else:
+            print "ret: "+str(res)
+
+if __name__ == '__main__':
+    main()
