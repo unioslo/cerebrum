471c116d30de105f654a601f94facecbbdc35114
Harald Meland <hmeland@ulrik.uio.no>
2002-09-09 12:17:46 +0000

    New functions: separate_entries(), keep_entries() and reject_entries().

--- Cerebrum/Utils.py
+++ Cerebrum/Utils.py
@@ -48,3 +48,42 @@ def this_module():
             correct_mod = mod
     assert correct_mod is not None
     return correct_mod
+
+def separate_entries(rows, *predicates):
+    """Separate `rows' into (keep, reject) tuple based on `predicates'.
+
+    The `rows' argument should be a sequence of db_row.py-generated
+    objects.  Each predicate in `predicate' should be a (key, value)
+    tuple.  The key must be a valid attribute name of each row object.
+
+    The rows are separated according to these rules:
+    1. By default rows go to the keep list.
+    2. If a predicate's value is None, that predicate is ignored.
+    3. Compare each predicate's value with the attribute named key in
+       each row.  Rows matching all predicates go to the keep list,
+       while the rest end up in the reject list.
+
+    """
+    keep = []
+    reject = []
+    for row in rows:
+        ok = 1
+        for key, value in predicates:
+            if value is None:
+                continue
+            ok = (row[key] == value)
+            if not ok:
+                break
+        if ok:
+            keep.append(row)
+        else:
+            reject.append(row)
+    return (keep, reject)
+
+def keep_entries(rows, *predicates):
+    "Return the `keep' part of separate_entries() return value."
+    return separate_entries(rows, *predicates)[0]
+
+def reject_entries(rows, *predicates):
+    "Return the `reject' part of separate_entries() return value."
+    return separate_entries(rows, *predicates)[1]
