32203f809e29948c005cd6a08af3f39a85937824
Rune Frøysa <runefro@usit.uio.no>
2002-09-30 08:00:46 +0000

    This version uses the new populate methods for updating the cerebrum database.  Work in progress.

--- contrib/no/uio/import_OU.py
+++ contrib/no/uio/import_OU.py
@@ -4,7 +4,7 @@ import re
 import pickle
 import sys
 
-from Cerebrum import Database
+from Cerebrum import Database,Constants,Errors
 from Cerebrum.modules.no.uio import OU
 
 class StedData(object):
@@ -30,6 +30,8 @@ class StedData(object):
         stedinfo = {}
         for c in self.colnames:
             stedinfo[c] = info.pop(0)
+            if(stedinfo[c] == ''):
+                stedinfo[c] = None
         stedkode = "%s-%s-%s" % (stedinfo['fakultetnr'], stedinfo['instituttnr'], stedinfo['gruppenr'])
         return (stedkode, stedinfo)
 
@@ -38,49 +40,58 @@ stedfile = "/u2/dumps/LT/sted.dta";
 def main():
     Cerebrum = Database.connect(user="cerebrum")
     steder = les_sted_info()
+    co = Constants.Constants(Cerebrum)
     ou = OU.OU(Cerebrum)
+    new_ou = OU.OU(Cerebrum)
     i = 1
     stedkode2ou = {}
     for k in steder.values():
         i = i + 1
+        # if(k['fakultetnr'] != "33"): continue
+        new_ou.clear()
+
+        new_ou.populate(k['stednavn'], k['fakultetnr'],
+                        k['instituttnr'], k['gruppenr'], acronym=k['akronym'],
+                        short_name=k['forkstednavn'], display_name=k['stednavn'],
+                        sort_name=k['stednavn'])
+        new_ou.affect_addresses(co.system_lt, co.address_street,
+                                co.address_post)
+        new_ou.populate_address(co.address_post, addr="%s\n%s" %
+                                (k['adresselinje1_intern_adr'],
+                                 k['adresselinje2_intern_adr']),
+                                zip=k['poststednr_intern_adr'],
+                                city=k['poststednavn_intern_adr'])
+        new_ou.populate_address(co.address_street, addr="%s\n%s" %
+                                (k['adresselinje1_besok_adr'],
+                                 k['adresselinje2_besok_adr']),
+                                zip=k['poststednr_besok_adr'],
+                                city=k['poststednavn_besok_adr'])
         try:
-            stedkode = "%s-%s-%s" % (k['fakultetnr'], k['instituttnr'], k['gruppenr'])
             ou.get_stedkode(k['fakultetnr'], k['instituttnr'], k['gruppenr'])
-            stedkode2ou[stedkode] = ou.ou_id
+            ou.find(ou.ou_id)
 
+            if not (ou == new_ou):
+                print "Changed %s %s %s" % (k['fakultetnr'], k['instituttnr'], k['gruppenr'])
+                new_ou.write_db(ou)
+            new_ou.ou_id = ou.ou_id
+        except Errors.NotFoundError: # Cerebrum.Errors.NotFoundError:
+            print "New entry"
+            new_ou.write_db()
             
+        stedkode = "%s-%s-%s" % (k['fakultetnr'], k['instituttnr'], k['gruppenr'])
+        stedkode2ou[stedkode] = new_ou.ou_id
+        Cerebrum.commit()
 
-            # Todo: compare old and new
-        except : # Cerebrum.Errors.NotFoundError:
-            id = ou.new(k['stednavn'], k['akronym'], k['forkstednavn'],
-                        k['stednavn'], k['stednavn'])
-            stedkode = "%s-%s-%s" % (k['fakultetnr'], k['instituttnr'], k['gruppenr'])
-            stedkode2ou[stedkode] = id
-            ou.find(id)             # new setter ikke denne i self (bug?)
-            ou.add_stedkode(k['fakultetnr'], k['instituttnr'], k['gruppenr'])
-            ou.add_entity_address('LT', 'p', addr="%s\n%s" %
-                                  (k['adresselinje1_intern_adr'],
-                                   k['adresselinje2_intern_adr']),
-                                  zip=k['poststednr_intern_adr'],
-                                  city=k['poststednavn_intern_adr'])
-            #                      country=k['landnavn_intern_adr'])
-            ou.add_entity_address('LT', 's', addr="%s\n%s" %
-                                  (k['adresselinje1_besok_adr'],
-                                   k['adresselinje2_besok_adr']),
-                                  zip=k['poststednr_besok_adr'],
-                                  city=k['poststednavn_besok_adr'])
-#                                  country=k['landnavn_besok_adr'])
-            if k['telefonnr'].strip() != '':
-                ou.add_entity_phone('LT', 'f', k['telefonnr'])
+    # sys.exit()
     existing_ou_mappings = {}
-    for t in ou.get_structure_mappings('LT'):
+    for t in ou.get_structure_mappings(co.perspective_lt):
         existing_ou_mappings[t[0]] = t[1]
         
     # Now populate ou_structure
     for stedkode in steder.keys():
-        rec_make_stedkode(stedkode, ou, existing_ou_mappings, steder, stedkode2ou)
+        rec_make_stedkode(stedkode, ou, existing_ou_mappings, steder, stedkode2ou, co)
 
-def rec_make_stedkode(stedkode, ou, existing_ou_mappings, steder, stedkode2ou):
+def rec_make_stedkode(stedkode, ou, existing_ou_mappings, steder, stedkode2ou, co):
     """Recursively create the ou_id -> parent_id mapping"""
     sted = steder[stedkode]
     org_stedkode = "%s-%s-%s" % (sted['fakultetnr_for_org_sted'],
@@ -105,9 +116,9 @@ def rec_make_stedkode(stedkode, ou, existing_ou_mappings, steder, stedkode2ou):
 
     ou.find(stedkode2ou[stedkode])
     if stedkode2ou.has_key(org_stedkode):
-        ou.set_parent('LT', stedkode2ou[org_stedkode])
+        ou.set_parent(co.perspective_lt, stedkode2ou[org_stedkode])
     else:
-        ou.set_parent('LT', None)
+        ou.set_parent(co.perspective_lt, None)
     existing_ou_mappings[stedkode2ou[stedkode]] = org_stedkode_ou
 
 def les_sted_info():
