907e583643c2cb2f4d3dc35d071d1aa16826dbb9
Rune Frøysa <runefro@usit.uio.no>
2002-10-07 11:45:00 +0000

    Added database-independent sequence usage.  Tested with PostgreSQL

--- design/core_data.sql
+++ design/core_data.sql
@@ -8,51 +8,52 @@ INSERT INTO entity_type_code (code, code_str, description) VALUES
   (2004, 'g', 'Group - see table "cerebrum.group_info" and friends.');
 
 INSERT INTO contact_info_code (code, code_str, description) VALUES
-  (code_seq.nextval, 'PHONE', 'Phone');
+  ([:sequence schema=cerebrum name=code_seq op=next], 'PHONE', 'Phone');
 INSERT INTO contact_info_code (code, code_str, description) VALUES
-  (code_seq.nextval, 'FAX', 'Fax');
+  ([:sequence schema=cerebrum name=code_seq op=next], 'FAX', 'Fax');
 
 INSERT INTO address_code (code, code_str, description) VALUES
-  (code_seq.nextval, 'POST', 'Post address');
+  ([:sequence schema=cerebrum name=code_seq op=next], 'POST', 'Post address');
 INSERT INTO address_code (code, code_str, description) VALUES
-  (code_seq.nextval, 'STREET', 'Street address');
+  ([:sequence schema=cerebrum name=code_seq op=next], 'STREET', 'Street address');
 
 INSERT INTO gender_code (code, code_str, description) VALUES
-  (code_seq.nextval, 'F', 'Female');
+  ([:sequence schema=cerebrum name=code_seq op=next], 'F', 'Female');
 INSERT INTO gender_code (code, code_str, description) VALUES
-  (code_seq.nextval, 'M', 'Male');
+  ([:sequence schema=cerebrum name=code_seq op=next], 'M', 'Male');
 
 INSERT INTO person_external_id_code (code, code_str, description) VALUES
-  (code_seq.nextval, 'NO_BIRTHNO', 'Norwegian birth number');
+  ([:sequence schema=cerebrum name=code_seq op=next], 'NO_BIRTHNO', 'Norwegian birth number');
 
 INSERT INTO person_name_code (code, code_str, description) VALUES
-  (code_seq.nextval, 'FIRST', 'First name');
+  ([:sequence schema=cerebrum name=code_seq op=next], 'FIRST', 'First name');
 INSERT INTO person_name_code (code, code_str, description) VALUES
-  (code_seq.nextval, 'LAST', 'Last name');
+  ([:sequence schema=cerebrum name=code_seq op=next], 'LAST', 'Last name');
 INSERT INTO person_name_code (code, code_str, description) VALUES
-  (code_seq.nextval, 'FULL', 'Full name');
-
-INSERT INTO person_affiliation_code (code, code_str, description) VALUES
-  (code_seq.nextval, 'EMPLOYEE', 'Employed');
+  ([:sequence schema=cerebrum name=code_seq op=next], 'FULL', 'Full name');
 
 INSERT INTO person_affiliation_code (code, code_str, description) VALUES
-  (code_seq.nextval, 'STUDENT', 'Student');
+  ([:sequence schema=cerebrum name=code_seq op=next], 'EMPLOYEE', 'Employed');
 
 INSERT INTO person_aff_status_code (affiliation, status, status_str, description) VALUES
-  (code_seq.currval, code_seq.currval, 'VALID', 'Valid');
+  ([:sequence schema=cerebrum name=code_seq op=current], [:sequence schema=cerebrum name=code_seq op=current], 'VALID', 'Valid');
+
+INSERT INTO person_affiliation_code (code, code_str, description) VALUES
+  ([:sequence schema=cerebrum name=code_seq op=next], 'STUDENT', 'Student');
+
+INSERT INTO person_aff_status_code (affiliation, status, status_str, description) VALUES
+  ([:sequence schema=cerebrum name=code_seq op=current], [:sequence schema=cerebrum name=code_seq op=current], 'VALID', 'Valid');
 
 /*
   UIO specific systems, will be moved to a separate file later
 */
 
 INSERT INTO authoritative_system_code (code, code_str, description) VALUES
-  (code_seq.nextval, 'LT', 'LT');
+  ([:sequence schema=cerebrum name=code_seq op=next], 'LT', 'LT');
 INSERT INTO authoritative_system_code (code, code_str, description) VALUES
-  (code_seq.nextval, 'FS', 'FS');
+  ([:sequence schema=cerebrum name=code_seq op=next], 'FS', 'FS');
 
 INSERT INTO ou_perspective_code (code, code_str, description) VALUES
-  (code_seq.nextval, 'LT', 'LT');
+  ([:sequence schema=cerebrum name=code_seq op=next], 'LT', 'LT');
 INSERT INTO ou_perspective_code (code, code_str, description) VALUES
-  (code_seq.nextval, 'FS', 'FS');
-
-commit;
+  ([:sequence schema=cerebrum name=code_seq op=next], 'FS', 'FS');
