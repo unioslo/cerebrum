4668a14a1cff199eaaab5ca1d5600139d1ce7c31
Harald Meland <hmeland@ulrik.uio.no>
2002-10-10 14:22:40 +0000

    Reworked.  Should be approaching usability.

--- design/mod_posix_user.sql
+++ design/mod_posix_user.sql
@@ -1,28 +1,52 @@
-CREATE TABLE posix_shell
+CREATE TABLE posix_shell_code
 (
-  shell		CHAR VARYING(16)
-		CONSTRAINT posix_shell_pk PRIMARY KEY,
-  shell_path	CHAR VARYING(64)
-		NOT NULL,
-  CONSTRAINT posix_shell_path_unique UNIQUE (shell_path)
+  code		NUMERIC(6,0)
+		CONSTRAINT posix_shell_code_pk PRIMARY KEY,
+  code_str	CHAR VARYING(16)
+		NOT NULL
+		CONSTRAINT posix_shell_codestr_u UNIQUE,
+  /* Longer shell strings are possible, but not very likely... */
+  shell		CHAR VARYING(512)
+		NOT NULL
+		CONSTRAINT posix_shell_code_shell_u UNIQUE
+);
+
+
+/*	posix_group
+
+  Extra information for groups that exist as POSIX (file) groups.
+
+  Names of POSIX groups are registered in entity_name (with any
+  value_domain_code the installation thinks appropriate for group
+  names).  This implies that any business rules for length etc. of
+  user names are handled outside the database.
+
+  Even though POSIX groups can have passwords associated with them,
+  this is very rare and hence not supported by this module.
+
+  gid
+	Unix numeric filegroup ID.
+
+ */
+CREATE TABLE posix_group
+(
+  group_id	NUMERIC(12,0)
+		CONSTRAINT posix_group_pk PRIMARY KEY
+		CONSTRAINT posix_group_group_id
+		  REFERENCES group_info(group_id),
+  gid		NUMERIC(12,0)
+                CONSTRAINT posix_group_gid_chk
+                  CHECK (gid >= 0 AND gid < 2147483648)
+		CONSTRAINT posix_group_gid UNIQUE
 );
 
 
 /*	posix_user
 
-  There are several reasons for having separate '*_domain'-columns for
-  name, uid and gid:
-
-   * This is necessary if e.g. 'name' should be separately unique in
-     NIS domain X and Y, while 'uid' should be unique across both of
-     these NIS domains.
-
-   * It's useful to allow separate reservation of user names, uids and
-     gids (as these reservations are coupled to the same "value
-     domain" names.
-
-  TBD: Holder argumentasjonen over, eller er det bedre å bruke kun en
-       kolonne for å indikere verdi-domene for alle tre verdiene?
+  Names of POSIX users are registered in entity_name (with any
+  value_domain_code the installation thinks appropriate for user
+  names).  This implies that any business rules for length etc. of
+  user names are handled outside the database.
 
   'gecos'	For personal users the POSIX gecos field will default
 		to the owning persons full name.  The default can be
@@ -30,67 +54,29 @@ CREATE TABLE posix_shell
 		For non-personal users this column must be non-NULL.
 
 */
-CREATE TABLE posix_user
-(
-  account_id	NUMERIC(12,0)
-		CONSTRAINT posix_user_account_id
-		  REFERENCES account(account_id)
-		CONSTRAINT posix_user_pk PRIMARY KEY,
-/* TBD: Bør det tillates at samme `account' gis opphav til flere
-        `posix_user's, f.eks. dersom man opererer med multiple
-        NIS-domener?  Hvis ja: Hva bør da primærnøkkelen for
-        posix_user være? */
-/* TBD: Bør vi støtte POSIX-brukernavn på mer enn 8 tegn? */
-  name		CHAR VARYING(16)
+CREATE TABLE posix_user (
+  account_id    NUMERIC(12,0)
+		CONSTRAINT posix_user_pk PRIMARY KEY
+                CONSTRAINT posix_user_account_id
+                  REFERENCES account_info(account_id),
+  uid           NUMERIC(12,0)
 		NOT NULL
-		CONSTRAINT posix_user_name_length CHECK (LENGTH(name) <= 8),
-  name_domain	CHAR VARYING(16)
-		CONSTRAINT posix_user_name_domain
-		  REFERENCES value_domain_code(code),
-  uid		NUMERIC(10,0)
-		NOT NULL,
-  uid_domain	CHAR VARYING(16)
-		CONSTRAINT posix_user_uid_domain
-		  REFERENCES value_domain_code(code),
-  gid		NUMERIC(10,0)
-		NOT NULL,
-  gid_domain	CHAR VARYING(16)
-		CONSTRAINT posix_user_gid_domain
-		  REFERENCES value_domain_code(code),
-  gecos		CHAR VARYING(128),
-  dir		CHAR VARYING(64)
-		NOT NULL,
-  shell		CHAR VARYING(16)
+                CONSTRAINT posix_user_uid_chk
+                  CHECK (uid >= 0 AND uid < 2147483648)
+		CONSTRAINT posix_user_uid_unique UNIQUE,
+  gid           NUMERIC(12,0)
 		NOT NULL
-		CONSTRAINT posix_user_shell REFERENCES posix_shell(shell),
-  CONSTRAINT posix_user_name_unique UNIQUE(name, name_domain),
-  CONSTRAINT posix_user_uid_unique UNIQUE(uid, uid_domain),
-  CONSTRAINT posix_user_gid_unique UNIQUE(gid, gid_domain)
-);
+                CONSTRAINT posix_user_gid
+                  REFERENCES posix_group(group_id),
+  /* Longer GECOS strings are possible, but not very likely... */
+  gecos		CHAR VARYING(512),
+  /* Longer home dirs are possible, but not very likely...
 
-
-/* TBD: Spread for brukere; bør dette implementeres ved hjelp av en
-	"REFERENCES account(account_id)"-type tabell, eller som
-	separat spread-tabell for hver enkelt variant av bruker det
-	finnes andre tabeller for (som f.eks. posix_user)? */
-
-
-/*
-
-Extra information for groups exported as NIS filegroups.
-
-Note that the names of NIS filegroups can't be longer than 8
-character; if a group with too long a name is referenced in this
-table, it should be ignored by the export machinery.
-
-   gid
-	Unix numeric filegroup ID.
- */
-CREATE TABLE nis_filegroup
-(
-  gkey		NUMERIC(12,0)
-		CONSTRAINT nis_filegroup_pk PRIMARY KEY
-		CONSTRAINT nis_filegroup_gkey REFERENCES group_info(group_id),
-  gid		NUMERIC(5,0)
-		CONSTRAINT nis_filegroup_gid UNIQUE
+     Not sure if NULL should should be allowed, but we're allowing it
+     for now; maybe someone needs to keep "home == '/'" and "home not
+     specified" cases separate. */
+  home		CHAR VARYING(512),
+  shell		NUMERIC(6,0)
+		NOT NULL
+		CONSTRAINT posix_user_shell REFERENCES posix_shell_code(code)
 );
