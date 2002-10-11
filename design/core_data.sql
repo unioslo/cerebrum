744a88f3f6e8306632172ffc55abc583dca43d8b
Rune Frøysa <runefro@usit.uio.no>
2002-10-11 11:56:55 +0000

    Added some needed default data.
    The stuff that has been commented out is just temporary to get some
    initial data for the posix tables.

--- design/core_data.sql
+++ design/core_data.sql
@@ -44,6 +44,45 @@ INSERT INTO person_affiliation_code (code, code_str, description) VALUES
 INSERT INTO person_aff_status_code (affiliation, status, status_str, description) VALUES
   ([:sequence schema=cerebrum name=code_seq op=current], [:sequence schema=cerebrum name=code_seq op=current], 'VALID', 'Valid');
 
+INSERT INTO account_code (code, code_str, description) VALUES
+  ([:sequence schema=cerebrum name=code_seq op=next], 'U', 'Personlig konto');
+
+INSERT INTO account_code (code, code_str, description) VALUES
+  ([:sequence schema=cerebrum name=code_seq op=next], 'P', 'Programvare konto');
+
+INSERT INTO group_visibility_code (code, code_str, description) VALUES
+  ([:sequence schema=cerebrum name=code_seq op=next], 'A', 'All');
+
+INSERT INTO posix_shell_code(code, code_str, description) VALUES
+  ([:sequence schema=cerebrum name=code_seq op=next], 'bash', '/bin/bash');
+
+INSERT INTO value_domain_code(code, code_str, description) VALUES
+  ([:sequence schema=cerebrum name=code_seq op=next], 'def_accname_dom', 'Default domain for account names');
+
+INSERT INTO authentication_code(code, code_str, description) VALUES
+  ([:sequence schema=cerebrum name=code_seq op=next], 'md5', 'MD5 password');
+
+/*
+
+***** Account som er laget av seg selv:
+
+insert into entity_info values(888888, 2003);
+INSERT INTO cerebrum.account_info (entity_type, account_id, owner_type,
+            owner_id, np_type, create_date, creator_id, expire_date)
+VALUES (2003, 888888, 2002, 2732, NULL, SYSDATE, 888888, SYSDATE);
+
+***** En filgruppe i påvente av at det kommer på plass:
+
+insert into entity_info values(999999, 2004);
+insert into group_info (entity_type, group_id, description, visibility, 
+  creator_id, create_date, expire_date ) 
+VALUES (2004, 999999, 'test da vi ikke har gruppe ting enda', 19, 888888, SYSDATE, SYSDATE);
+
+insert into posix_group values (999999, 0);
+
+*/
+
+
 /*
   UIO specific systems, will be moved to a separate file later
 */
