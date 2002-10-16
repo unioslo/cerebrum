17873e34f030178269d4e21e6e8151d1901b127e
Harald Meland <hmeland@ulrik.uio.no>
2002-10-16 04:31:22 +0000

    Removed hashbang line; this is a module, not a script.
    Added copyright.
    
    Renamed BofhdExtention -> BofhdExtension, and made a subclass of
    'object'
    
    Removed user argument to Database.connect() call.

--- Cerebrum/modules/no/bofhd_module_example.py
+++ Cerebrum/modules/no/bofhd_module_example.py
@@ -1,16 +1,32 @@
-#!/usr/bin/env python2
+# Copyright 2002 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 # $Id$
 
-# Samlple module that extends bofhd with a set of commands
+# Sample module that extends bofhd with a set of commands
 
 from Cerebrum import Database
 from Cerebrum.modules.no.uio import OU
 
-class BofhdExtention:
+class BofhdExtension(object):
     def __init__(self, Cerebrum):
         self.all_commands = {
-            'get_stedkode_info' : ['ou', 'stedkode', 'number', 1]
+            'get_stedkode_info': ['ou', 'stedkode', 'number', 1]
             }
         self.ou = OU.OU(Cerebrum)
 
@@ -26,20 +42,21 @@ class BofhdExtention:
         self.ou.find_stedkode(fakultetnr, instituttnr, gruppenr)
         self.ou.find(self.ou.ou_id)
         return {
-            'name' : self.ou.name,
-            'acronym' : self.ou.acronym,
-            'short_name' : self.ou.short_name,
-            'display_name' : self.ou.display_name,
-            'sort_name' : self.ou.sort_name
+            'name': self.ou.name,
+            'acronym': self.ou.acronym,
+            'short_name': self.ou.short_name,
+            'display_name': self.ou.display_name,
+            'sort_name': self.ou.sort_name
             }
 
     def get_format_suggestion(self, cmd):
         suggestions = {
-            'get_stedkode_info' : "Navn: %s\nAconym: %s\nShort: %s¤name;acronym;short_name"
+            'get_stedkode_info':
+            "Navn: %s\nAcronym: %s\nShort: %s¤name;acronym;short_name"
             }
         return suggestions.get(cmd)
 
 if __name__ == '__main__':
-    Cerebrum = Database.connect(user="cerebrum")
-    sm = BofhdExtention(Cerebrum)
+    Cerebrum = Database.connect()
+    sm = BofhdExtension(Cerebrum)
     print "Ret: %s" % sm.get_stedkode_info('user', '900547')
