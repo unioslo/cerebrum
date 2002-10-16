d28e5c15eef0ef0376cfee7ca5337eb0bf749ec7
Harald Meland <hmeland@ulrik.uio.no>
2002-10-16 03:15:04 +0000

    Use [:table] portability function.
    Entity type renames:
      o -> ou
      p -> person
      a -> account
      g -> group
    
    Removed account_code ('U', 'Personlig konto') entry; personal accounts
    should not have NULL np_type, as their type(s) are indicated through
    its entries in the `account_type' table.
    
    Added insertion of standard values in 'group_membership_op_code'.

--- design/core_data.sql
+++ design/core_data.sql
@@ -1,65 +1,121 @@
-INSERT INTO entity_type_code (code, code_str, description) VALUES
-  (2001, 'o', 'Organizational Unit - see table "cerebrum.ou_info" and friends.');
-INSERT INTO entity_type_code (code, code_str, description) VALUES
-  (2002, 'p', 'Person - see table "cerebrum.person_info" and friends.');
-INSERT INTO entity_type_code (code, code_str, description) VALUES
-  (2003, 'a', 'User Account - see table "cerebrum.account_info" and friends.');
-INSERT INTO entity_type_code (code, code_str, description) VALUES
-  (2004, 'g', 'Group - see table "cerebrum.group_info" and friends.');
+/*
 
-INSERT INTO contact_info_code (code, code_str, description) VALUES
+Create entries for the core entity types.
+
+TBD: Each of these statements should be done before the root table
+     of the corresponding entity is created, so that it's possible to
+     get the entity_type column check correct.  Or maybe this is
+     better to fix later with an 'ALTER TABLE' statement?
+*/
+INSERT INTO [:table schema=cerebrum name=entity_type_code]
+  (code, code_str, description) VALUES
+  (2001, -- [:sequence schema=cerebrum name=code_seq op=next],
+   'ou', 'Organizational Unit - see table "cerebrum.ou_info" and friends.');
+INSERT INTO [:table schema=cerebrum name=entity_type_code]
+  (code, code_str, description) VALUES
+  (2002, -- [:sequence schema=cerebrum name=code_seq op=next],
+   'person', 'Person - see table "cerebrum.person_info" and friends.');
+INSERT INTO [:table schema=cerebrum name=entity_type_code]
+  (code, code_str, description) VALUES
+  (2003, -- [:sequence schema=cerebrum name=code_seq op=next],
+   'account', 'User Account - see table "cerebrum.account_info" and friends.');
+INSERT INTO [:table schema=cerebrum name=entity_type_code]
+  (code, code_str, description) VALUES
+  (2004, --[:sequence schema=cerebrum name=code_seq op=next],
+   'group', 'Group - see table "cerebrum.group_info" and friends.');
+
+INSERT INTO [:table schema=cerebrum name=contact_info_code]
+  (code, code_str, description) VALUES
   ([:sequence schema=cerebrum name=code_seq op=next], 'PHONE', 'Phone');
-INSERT INTO contact_info_code (code, code_str, description) VALUES
+INSERT INTO [:table schema=cerebrum name=contact_info_code]
+  (code, code_str, description) VALUES
   ([:sequence schema=cerebrum name=code_seq op=next], 'FAX', 'Fax');
 
-INSERT INTO address_code (code, code_str, description) VALUES
-  ([:sequence schema=cerebrum name=code_seq op=next], 'POST', 'Post address');
-INSERT INTO address_code (code, code_str, description) VALUES
-  ([:sequence schema=cerebrum name=code_seq op=next], 'STREET', 'Street address');
+INSERT INTO [:table schema=cerebrum name=address_code]
+  (code, code_str, description) VALUES
+  ([:sequence schema=cerebrum name=code_seq op=next], 'POST',
+   'Post address');
+INSERT INTO [:table schema=cerebrum name=address_code]
+  (code, code_str, description) VALUES
+  ([:sequence schema=cerebrum name=code_seq op=next], 'STREET',
+   'Street address');
 
-INSERT INTO gender_code (code, code_str, description) VALUES
+INSERT INTO [:table schema=cerebrum name=gender_code]
+  (code, code_str, description) VALUES
   ([:sequence schema=cerebrum name=code_seq op=next], 'F', 'Female');
-INSERT INTO gender_code (code, code_str, description) VALUES
+INSERT INTO [:table schema=cerebrum name=gender_code]
+  (code, code_str, description) VALUES
   ([:sequence schema=cerebrum name=code_seq op=next], 'M', 'Male');
 
-INSERT INTO person_external_id_code (code, code_str, description) VALUES
-  ([:sequence schema=cerebrum name=code_seq op=next], 'NO_BIRTHNO', 'Norwegian birth number');
+INSERT INTO [:table schema=cerebrum name=person_external_id_code]
+  (code, code_str, description) VALUES
+  ([:sequence schema=cerebrum name=code_seq op=next], 'NO_BIRTHNO',
+   'Norwegian birth number');
 
-INSERT INTO person_name_code (code, code_str, description) VALUES
+INSERT INTO [:table schema=cerebrum name=person_name_code]
+  (code, code_str, description) VALUES
   ([:sequence schema=cerebrum name=code_seq op=next], 'FIRST', 'First name');
-INSERT INTO person_name_code (code, code_str, description) VALUES
+INSERT INTO [:table schema=cerebrum name=person_name_code]
+  (code, code_str, description) VALUES
   ([:sequence schema=cerebrum name=code_seq op=next], 'LAST', 'Last name');
-INSERT INTO person_name_code (code, code_str, description) VALUES
+INSERT INTO [:table schema=cerebrum name=person_name_code]
+  (code, code_str, description) VALUES
   ([:sequence schema=cerebrum name=code_seq op=next], 'FULL', 'Full name');
 
-INSERT INTO person_affiliation_code (code, code_str, description) VALUES
-  ([:sequence schema=cerebrum name=code_seq op=next], 'EMPLOYEE', 'Employed');
+INSERT INTO [:table schema=cerebrum name=person_affiliation_code]
+  (code, code_str, description) VALUES
+  ([:sequence schema=cerebrum name=code_seq op=next], 'EMPLOYEE',
+   'Employed');
 
-INSERT INTO person_aff_status_code (affiliation, status, status_str, description) VALUES
-  ([:sequence schema=cerebrum name=code_seq op=current], [:sequence schema=cerebrum name=code_seq op=current], 'VALID', 'Valid');
+INSERT INTO [:table schema=cerebrum name=person_aff_status_code]
+  (affiliation, status, status_str, description) VALUES
+  ([:sequence schema=cerebrum name=code_seq op=current],
+   [:sequence schema=cerebrum name=code_seq op=current],
+   'VALID', 'Valid');
 
-INSERT INTO person_affiliation_code (code, code_str, description) VALUES
+INSERT INTO [:table schema=cerebrum name=person_affiliation_code]
+  (code, code_str, description) VALUES
   ([:sequence schema=cerebrum name=code_seq op=next], 'STUDENT', 'Student');
 
-INSERT INTO person_aff_status_code (affiliation, status, status_str, description) VALUES
-  ([:sequence schema=cerebrum name=code_seq op=current], [:sequence schema=cerebrum name=code_seq op=current], 'VALID', 'Valid');
+INSERT INTO [:table schema=cerebrum name=person_aff_status_code]
+  (affiliation, status, status_str, description) VALUES
+  ([:sequence schema=cerebrum name=code_seq op=current],
+   [:sequence schema=cerebrum name=code_seq op=current],
+   'VALID', 'Valid');
 
-INSERT INTO account_code (code, code_str, description) VALUES
-  ([:sequence schema=cerebrum name=code_seq op=next], 'U', 'Personlig konto');
+INSERT INTO [:table schema=cerebrum name=account_code]
+  (code, code_str, description) VALUES
+  ([:sequence schema=cerebrum name=code_seq op=next],
+   'P', 'Programvarekonto');
 
-INSERT INTO account_code (code, code_str, description) VALUES
-  ([:sequence schema=cerebrum name=code_seq op=next], 'P', 'Programvare konto');
-
-INSERT INTO group_visibility_code (code, code_str, description) VALUES
+INSERT INTO [:table schema=cerebrum name=group_visibility_code]
+  (code, code_str, description) VALUES
   ([:sequence schema=cerebrum name=code_seq op=next], 'A', 'All');
 
-INSERT INTO posix_shell_code(code, code_str, shell) VALUES
+INSERT INTO [:table schema=cerebrum name=group_membership_op_code]
+  (code, code_str, description) VALUES
+  ([:sequence schema=cerebrum name=code_seq op=next], 'union',
+   'Union');
+INSERT INTO [:table schema=cerebrum name=group_membership_op_code]
+  (code, code_str, description) VALUES
+  ([:sequence schema=cerebrum name=code_seq op=next], 'intersection',
+   'Intersection');
+INSERT INTO [:table schema=cerebrum name=group_membership_op_code]
+  (code, code_str, description) VALUES
+  ([:sequence schema=cerebrum name=code_seq op=next], 'difference',
+   'Difference');
+
+INSERT INTO [:table schema=cerebrum name=posix_shell_code]
+  (code, code_str, shell) VALUES
   ([:sequence schema=cerebrum name=code_seq op=next], 'bash', '/bin/bash');
 
-INSERT INTO value_domain_code(code, code_str, description) VALUES
-  ([:sequence schema=cerebrum name=code_seq op=next], 'def_accname_dom', 'Default domain for account names');
+INSERT INTO [:table schema=cerebrum name=value_domain_code]
+  (code, code_str, description) VALUES
+  ([:sequence schema=cerebrum name=code_seq op=next], 'def_accname_dom',
+   'Default domain for account names');
 
-INSERT INTO authentication_code(code, code_str, description) VALUES
+INSERT INTO [:table schema=cerebrum name=authentication_code]
+  (code, code_str, description) VALUES
   ([:sequence schema=cerebrum name=code_seq op=next], 'md5', 'MD5 password');
 
 /*
@@ -87,12 +143,16 @@ insert into posix_group values (999999, 0);
   UIO specific systems, will be moved to a separate file later
 */
 
-INSERT INTO authoritative_system_code (code, code_str, description) VALUES
+INSERT INTO [:table schema=cerebrum name=authoritative_system_code]
+  (code, code_str, description) VALUES
   ([:sequence schema=cerebrum name=code_seq op=next], 'LT', 'LT');
-INSERT INTO authoritative_system_code (code, code_str, description) VALUES
+INSERT INTO [:table schema=cerebrum name=authoritative_system_code]
+  (code, code_str, description) VALUES
   ([:sequence schema=cerebrum name=code_seq op=next], 'FS', 'FS');
 
-INSERT INTO ou_perspective_code (code, code_str, description) VALUES
+INSERT INTO [:table schema=cerebrum name=ou_perspective_code]
+  (code, code_str, description) VALUES
   ([:sequence schema=cerebrum name=code_seq op=next], 'LT', 'LT');
-INSERT INTO ou_perspective_code (code, code_str, description) VALUES
+INSERT INTO [:table schema=cerebrum name=ou_perspective_code]
+  (code, code_str, description) VALUES
   ([:sequence schema=cerebrum name=code_seq op=next], 'FS', 'FS');
