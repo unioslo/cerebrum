e67b3c21ad532665e78e2aaafae4a1d16002a30e
Rune Frøysa <runefro@usit.uio.no>
2002-10-31 10:16:10 +0000

    Use XMLHelper

--- contrib/no/uio/import_from_FS.py
+++ contrib/no/uio/import_from_FS.py
@@ -7,15 +7,14 @@ from Cerebrum import cereconf
 
 from Cerebrum.modules.no.uio.access_FS import FSPerson
 from Cerebrum import Database,Errors
+from Utils import XMLHelper
 
 default_personfile = "/u2/dumps/FS/persons.dat.2"
 
 cereconf.DATABASE_DRIVER='Oracle'
 Cerebrum = Database.connect(user="ureg2000", service="FSDEMO.uio.no")
 FSP = FSPerson(Cerebrum)
-xml_hdr = '<?xml version="1.0" encoding="ISO-8859-1"?>\n'
-
-# TODO: Some of the XML encoding should be placed in a separate file
+xml = XMLHelper()
 
 def get_person_info():
     # KURS & EVU fagpersoner
@@ -24,11 +23,11 @@ def get_person_info():
     # !!! Mulig at dette er den eneste spørringen som trengs !!!
 
     f=open(default_personfile, 'w')
-    f.write(xml_hdr + "<data>\n")
+    f.write(xml.xml_hdr + "<data>\n")
     # Her kan stedkodeinfo utledes direkte ...
     cols, fagpersoner = FSP.GetKursFagpersonundsemester()
     for p in fagpersoner:
-        f.write(_xmlify_dbrow(p, _conv_colnames(cols), 'fagperson') + "\n")
+        f.write(xml.xmlify_dbrow(p, xml.conv_colnames(cols), 'fagperson') + "\n")
 
     # STUDENTS
     # Studensts from FS
@@ -36,15 +35,15 @@ def get_person_info():
 
     cols, students = FSP.GetStudinfRegkort()
     for s in students:
-        f.write(_xmlify_dbrow(s, _conv_colnames(cols), 'student') + "\n")
+        f.write(xml.xmlify_dbrow(s, xml.conv_colnames(cols), 'student') + "\n")
 
     cols, students = FSP.GetStudinfNaaKlasse()
     for s in students:
-        f.write(_xmlify_dbrow(s, _conv_colnames(cols), 'student') + "\n")
+        f.write(xml.xmlify_dbrow(s, xml.conv_colnames(cols), 'student') + "\n")
 
     cols, students = FSP.GetStudinfStudierett()
     for s in students:
-        f.write(_xmlify_dbrow(s, _conv_colnames(cols), 'student') + "\n")
+        f.write(xml.xmlify_dbrow(s, xml.conv_colnames(cols), 'student') + "\n")
 
     # EVU STUDENTS
     # Evu students from FS
@@ -52,24 +51,8 @@ def get_person_info():
 
     cols, evustud = FSP.GetStudinfEvuKurs()
     for e in evustud:
-        f.write(_xmlify_dbrow(e, _conv_colnames(cols), 'evu') + "\n")
+        f.write(xml.xmlify_dbrow(e, xml.conv_colnames(cols), 'evu') + "\n")
     f.write("</data>\n")
-
-def _conv_colnames(cols):
-    "Strip tablename prefix from column name"
-    prefix = re.compile(r"[^.]*\.")
-    for i in range(len(cols)):
-        cols[i] = re.sub(prefix, "", cols[i]).lower()
-    return cols
-
-def _xmlify_dbrow(row, cols, tag):
-    return "<%s " % tag + (
-        " ".join(["%s=%s" % (cols[i], _escape_xml_attr(row[i]))
-                  for i in range(len(cols))])+"/>")
-
-def _escape_xml_attr(a):
-    # TODO:  Check XML-spec to find out what to quote
-    return '"%s"' % a
 
 def main():
     get_person_info()
