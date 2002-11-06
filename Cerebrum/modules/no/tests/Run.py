c892070c9197b663fca4ef454defb2eebab78b34
Petter Reinholdtsen <pre@ulrik.uio.no>
2002-11-06 13:30:55 +0000

    Add test to check fødselsnr parsing.

---
+++ Cerebrum/modules/no/tests/Run.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2.2
+#
+# $Id$
+
+import unittest
+
+from Cerebrum.modules.no.tests.fodselsnrTestCase import fodselsnrTestCase
+
+def suite():
+    """Returns a suite containing all the test cases in this module.
+       It can be a good idea to put an identically named factory function
+       like this in every test module. Such a naming convention allows
+       automation of test discovery.
+    """
+
+    suite1 = fodselsnrTestCase.suite()
+    return unittest.TestSuite((suite1,))
+
+
+if __name__ == '__main__':
+    # When this module is executed from the command-line, run all its tests
+    unittest.main(defaultTest='suite')
---
+++ Cerebrum/modules/no/tests/fodselsnrTestCase.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python2.2
+#
+# Author: Petter Reinholdtsen <pere@hungry.com>
+# Date:   2002-11-06
+#
+# Test the fødselsnummer class
+
+import unittest
+
+from Cerebrum.modules.no import fodselsnr
+
+class fodselsnrTestCase(unittest.TestCase):
+    def setUp(self):
+        self.invalidnumbers = [
+            "01013338728",
+            "19055433813",
+            ]
+        
+        self.validnumbers = [
+            # (number, sex, birth date)
+            ("01013638728", 0, (1936, 01, 01)),
+            ("71057107345", 0, (1971, 05, 31)),
+            ("20067647972", 0, (1976, 06, 20)),
+            ("30535890168", 0, (1958, 03, 30)),
+            ("01015449256", 1, (1954, 01, 01)),
+            ("20035322052", 1, (1953, 03, 20)),
+            ("19055430813", 1, (1954, 05, 19))
+            ]
+
+        self.invalidinfo = [
+            ("01013638728", 1, (1867, 01, 01)),
+            ("19055430813", 0, (1954, 05, 12))
+            ]
+        
+    def testIsNumberOK(self):
+        "Check if able to separate valid fødelsnummers from invalid"
+        for number in self.validnumbers:
+            assert( fodselsnr.personnr_ok(number[0]) )
+        for number in self.invalidnumbers:
+            try:
+                nr = fodselsnr.personnr_ok(number)
+                raise "Invalid fødelsnr accepted"
+            except:
+                pass
+
+    def testDate(self):
+        "Check if the Date is correctly extracted from fødelsnummers"
+        for number in self.validnumbers:
+            year, month, day = fodselsnr.fodt_dato(number[0])
+            assert( (year, month, day) == number[2] )
+
+        for number in self.invalidinfo:
+            year, month, day = fodselsnr.fodt_dato(number[0])
+            assert( (year, month, day) != number[2] )
+
+    def testSex(self):
+        "Check if the sex is correctly extracted from fødelsnummers"
+        for number in self.validnumbers:
+            assert( fodselsnr.er_kvinne(number[0]) == number[1] )
+
+        for number in self.invalidinfo:
+            assert( fodselsnr.er_kvinne(number[0]) != number[1] )
+
+    def suite():
+        suite = unittest.TestSuite()
+        suite.addTest(fodselsnrTestCase("testIsNumberOK"))
+        suite.addTest(fodselsnrTestCase("testDate"))
+        suite.addTest(fodselsnrTestCase("testSex"))
+        return suite
+    suite=staticmethod(suite)
+
+def suite():
+    return fodselsnrTestCase.suite()
+
+if __name__ == "__main__":
+    unittest.main()
