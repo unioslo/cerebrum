a4fa48609cebfbf26039bad7df7767dce32684a7
Harald Meland <hmeland@ulrik.uio.no>
2002-11-13 20:17:04 +0000

    Rewritten to work with `mark_update' metaclass.
    Appears to be functional (with a bunch of changes to other modules,
    soon to be checked in).

--- Cerebrum/modules/PosixGroup.py
+++ Cerebrum/modules/PosixGroup.py
@@ -19,66 +19,78 @@
 """Access to Cerebrum groups that are also POSIX file groups."""
 
 from Cerebrum import Group
+from Cerebrum import Errors
 
 class PosixGroup(Group.Group):
 
+    __read_attr__ = ('__in_db',)
+    __write_attr__ = ('posix_gid',)
+
     # We do not allow giving PosixGroups names that differ from the
     # names of the Groups they are based on, as we anticipate that
-    # would quickly lead to chaos.
+    # doing so would lead to chaos.
     #
     # If one wants to build a PosixGroup on top of a Group whose name
     # is not proper for PosixGroup, one might define a _new_
     # PosixGroup and add the Group as its only (union) member.
-    def populate(self, creator, visibility, name, description=None,
-                 create_date=None, expire_date=None, gid=None):
-        self._check_name(name)
+    def populate(self, creator=None, visibility=None, name=None,
+                 description=None, create_date=None, expire_date=None,
+                 gid=None, parent=None):
+        if parent is not None:
+            self.__xerox__(parent)
+        else:
+            Group.populate(creator, visibility, name, description,
+                           create_date, expire_date)
+        self.__in_db = False
+        if gid is None:
+            gid = self._get_gid()
         self.posix_gid = gid
-        super(PosixGroup, self).populate(
-            creator, visibility, name, description, create_date, expire_date)
 
-    def write_db(self, as_object=None):
-        assert self.__write_db
-        super(PosixGroup, self).write_db(self, as_object)
-        if as_object is None:
-            if self.posix_gid is None:
-                self.posix_gid = self._get_gid()
+    def write_db(self):
+        self.__super.write_db()
+        if not self.__updated:
+            return
+        if not self.__in_db:
             self.execute("""
             INSERT INTO [:table schema=cerebrum name=posix_group]
               (group_id, posix_gid)
-            VALUES (:g_id, :posix_gid)""", {'g_id': self.group_id,
+            VALUES (:g_id, :posix_gid)""", {'g_id': self.entity_id,
                                             'posix_gid': self.posix_gid})
         else:
             self.execute("""
             UPDATE [:table schema=cerebrum name=posix_group]
             SET posix_gid=:posix_gid
-            WHERE group_id=:g_id""", {'g_id': as_object.group_id,
-                                      'posix_gid': as_object.posix_gid})
-        self.__write_db = False
+            WHERE group_id=:g_id""", {'g_id': self.entity_id,
+                                      'posix_gid': self.posix_gid})
+        del self.__in_db
+        self.__in_db = True
+        self.__updated = False
 
     def __eq__(self, other):
         assert isinstance(other, PosixGroup)
-        if self.posix_gid <> other.posix_gid:
-            return False
-        return super(PosixGroup, self).__eq__(other)
+        if self.posix_gid == other.posix_gid:
+            return self.__super.__eq__(other)
+        return False
 
-    def new(self, creator, visibility, name=None, gid=None,
-            description=None, create_date=None, expire_date=None):
-        PosixGroup.populate(self, creator, visibility, name, gid,
-                            description, create_date, expire_date)
+    def new(self, creator, visibility, name, description=None,
+            create_date=None, expire_date=None, gid=None):
+        PosixGroup.populate(self, creator, visibility, name, description,
+                            create_date, expire_date, gid)
         PosixGroup.write_db(self)
-        PosixGroup.find(self, self.group_id)
+        PosixGroup.find(self, self.entity_id)
 
     def find(self, group_id):
         super(PosixGroup, self).find(group_id)
         self.posix_gid = self.query_1("""
         SELECT posix_gid
-        FROM [:table schema=cerebrum name=posix_gid]
-        WHERE group_id=:g_id""", {'g_id':self.group_id})
+        FROM [:table schema=cerebrum name=posix_group]
+        WHERE group_id=:g_id""", {'g_id': self.entity_id})
+        self.__in_db = True
 
     def find_by_gid(self, gid):
         group_id = self.query_1("""
         SELECT group_id
-        FROM [:table schema=cerebrum name=posix_group
+        FROM [:table schema=cerebrum name=posix_group]
         WHERE posix_gid=:gid""", locals())
         self.find(group_id)
 
@@ -93,15 +105,22 @@ class PosixGroup(Group.Group):
             except Errors.NotFoundError:
                 return gid
 
-    def _check_name(self, name):
-        name_len = len(name)
-        if name_len == 0:
-            raise ValueError, "PosixGroup can't have empty name."
-        if name_len > 8:
-            raise ValueError,\
-                  "PosixGroup name '%s' longer than 8 characters." % name
-        for c in name:
-            if c not in tuple('abcdefghijklmnopqrstuvwxyz0123456789'):
-                raise ValueError, \
-                      "PosixGroup name '%s' contains illegal char '%s'." % \
-                      (name, c)
+    def delete(self):
+        if self.__in_db:
+            # Remove entry in table `posix_group'.
+            self.execute("""
+            DELETE FROM [:table schema=cerebrum name=posix_group]
+            WHERE group_id=:g_id""", {'g_id': self.entity_id})
+
+##     def _check_name(self, name):
+##         name_len = len(name)
+##         if name_len == 0:
+##             raise ValueError, "PosixGroup can't have empty name."
+##         if name_len > 8:
+##             raise ValueError,\
+##                   "PosixGroup name '%s' longer than 8 characters." % name
+##         for c in name:
+##             if c not in tuple('abcdefghijklmnopqrstuvwxyz0123456789'):
+##                 raise ValueError, \
+##                       "PosixGroup name '%s' contains illegal char '%s'." % \
+##                       (name, c)
