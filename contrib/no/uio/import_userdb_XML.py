a3ae364e4e4e8c0c96dc769d7ca4cb65334136c6
Rune Frøysa <runefro@usit.uio.no>
2002-11-22 09:43:43 +0000

    Initial version.  The current version is able to import persons, users, filegroups and filegroupmembers.  It does, however, take a number of shortcuts which should be fixed in the near future.

---
+++ contrib/no/uio/import_userdb_XML.py
@@ -0,0 +1,310 @@
+#!/usr/bin/env python2.2
+# $Id$
+
+## Import from Ureg2000 (Specific to uio.no)
+## -----------------------------------------
+##   STATUS: 2002-11-19: Not started, but will be very useful for testing
+## 		      of other components; hence, start is scheduled
+## 		      before 2002-11-22.
+##
+##   RESPONSIBILITY OF: RF
+##
+##   Scripts for importing user, email, filegroup and and netgroup data
+##   from Ureg2000.  Person data not appearing in any of Cerebrum's
+##   authoritative sources must also be imported.
+##
+##   CODE DEPENDENCIES: Core API
+## 		     POSIX users and groups
+## 		     Email API
+##
+##   DATA DEPENDENCIES: Import LT+FS
+##
+##   FILES:
+
+
+"""
+Reads XML files with user and person data from file, and imports them
+into cerebrum.  Contains some UREG specific parts, but attempt has
+been made to isolate these so that this script also can be used with
+other sources.
+"""
+
+## HOWTO:
+##
+## - importer alle filgrupper m/creator_id=ureg2000
+## - for hver person:
+##   - hvis personen ikke finnes, legg vedkommende inn med ss=ureg2000
+##   - bygg brukeren
+##     - ignorer spread (foreløbig)
+##     - husk create_date og creator_id i en hash, samt alle byggedes id
+## - sett create_date og creator_id direkte i SQL (API støtter det ikke)
+
+import pprint
+import xml.sax
+import sys
+import getopt
+from Cerebrum import Database
+from Cerebrum import Account
+from Cerebrum import Group
+from Cerebrum import Person
+from Cerebrum import Constants
+from Cerebrum import Errors
+from Cerebrum.modules import PosixGroup
+from Cerebrum.modules import PosixUser
+
+default_personfile = '/local2/home/runefro/usit/cerebrum/contrib/no/uio/users.xml'
+default_groupfile = '/local2/home/runefro/usit/cerebrum/contrib/no/uio/filegroups.xml'
+Cerebrum = Database.connect()
+personObj = Person.Person(Cerebrum)
+co = Constants.Constants(Cerebrum)
+pp = pprint.PrettyPrinter(indent=4)
+
+class PersonUserData(object):
+    """This class is used to iterate over all users/persons in the ureg dump. """
+
+    def __init__(self, filename):
+        # Ugly memory-wasting, inflexible way:
+        self.tp = PersonUserParser()
+        xml.sax.parse(filename, self.tp)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        """Returns a dict with data about the next person in ureg."""
+        try:
+            return self.tp.personer.pop(0)
+        except IndexError:
+            raise StopIteration, "End of file"
+
+class PersonUserParser(xml.sax.ContentHandler):
+    """Parser for the users.xml file.  Stores recognized data in an
+    internal datastructure"""
+
+    def __init__(self):
+        self.personer = []
+        self.elementstack = []
+
+    def startElement(self, name, attrs):
+        tmp = {}
+        for k in attrs.keys():
+            tmp[k.encode('iso8859-1')] = attrs[k].encode('iso8859-1')
+        name = name.encode('iso8859-1')
+        if name == "persons":
+            pass
+        elif name == "person":
+            self.person = {'bdate': tmp['bdate']}
+        elif self.elementstack[-1] == "user":
+            if name in ("auth", "spread", "name"):
+                self.user[name] = self.user.get(name, []) + [tmp]
+            elif name in ("uio",):
+                self.user[name] = tmp
+            else:
+                print "WARNING: unknown user element: %s" % name
+        elif self.elementstack[-1] == "person":
+            if name in ("contact", "name", "extid"):
+                self.person[name] = self.person.get(name, []) + [tmp]
+            elif name in ("uio",):
+                self.person[name] = tmp
+            elif name == "user":
+                self.user = {}
+                for k in tmp.keys():
+                    self.user[k] = tmp[k]
+            else:
+                print "WARNING: unknown person element: %s" % name
+        else:
+            print "WARNING: unknown element: %s" % name
+        self.elementstack.append(name)
+
+    def endElement(self, name): 
+        if name == "person":
+            self.personer.append(self.person)
+            pass
+        elif name == "user":
+            self.person['user'] = self.person.get('user', []) + [self.user]
+        self.elementstack.pop()
+
+
+class GroupData(object):
+    """This class is used to iterate over all groups from ureg."""
+
+    def __init__(self, filename):
+        # Ugly memory-wasting, inflexible way:
+        self.tp = FilegroupParser()
+        xml.sax.parse(filename, self.tp)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        """Returns a dict with data about the next group in ureg."""
+        try:
+            return self.tp.groups.pop(0)
+        except IndexError:
+            raise StopIteration, "End of file"
+
+class FilegroupParser(xml.sax.ContentHandler):
+    """Parser for the filegroups.xml file.  Stores recognized data in an
+    internal datastructure"""
+
+    def __init__(self):
+        self.groups = []
+        self.elementstack = []
+
+    def startElement(self, name, attrs):
+        tmp = {}
+        for k in attrs.keys():
+            tmp[k.encode('iso8859-1')] = attrs[k].encode('iso8859-1')
+        name = name.encode('iso8859-1')
+        if name == "groups":
+            pass
+        elif name == "group":
+            self.group = {}
+            for k in attrs.keys():
+                self.group[k] = tmp[k]
+        elif self.elementstack[-1] == "group":
+            if name == "member":
+                self.group[name] = self.group.get(name, []) + [tmp]
+            else:
+                print "WARNING: unknown group element: %s" % name
+        else:
+            print "WARNING: unknown element: %s" % name
+        self.elementstack.append(name)
+
+    def endElement(self, name): 
+        if name == "group":
+            self.groups.append(self.group)
+        self.elementstack.pop()
+
+def import_groups(groupfile, fill=0):
+    account = Account.Account(Cerebrum)
+    # insert into entity_name values(888888, 21, 'ureg2000');
+    account.find(888888)
+
+    for group in GroupData(groupfile):
+        # pp.pprint(group)
+        print ".",
+        groupObj = Group.Group(Cerebrum)
+        pg = PosixGroup.PosixGroup(Cerebrum)
+        if not fill:
+            groupObj.populate(account, co.group_visibility_all,
+                              group['name'], group['comment'])
+            groupObj.write_db()
+            # groupObj.find(groupObj.entity_id)
+
+            # pg = PosixGroup.PosixGroup(Cerebrum)
+            pg.populate(parent=groupObj, gid=group['gid'])
+            pg.write_db()
+        else:
+            try:
+                pg.find_by_gid(group['gid'])
+            except Errors.NotFoundError:
+                print "G",
+                continue
+            for m in group['member']:
+                try:
+                    account.find_account_by_name(co.account_namespace, m['uname'])
+                    pg.add_member(account, co.group_memberop_union);
+                    print "A",
+                except Errors.NotFoundError:
+                    print "n",
+                    continue
+
+    Cerebrum.commit()
+
+def import_person_users(personfile):
+    namestr2const = {'lname': co.name_last, 'fname': co.name_first}
+
+    for person in PersonUserData(personfile):
+        # pp.pprint(person)
+
+        personObj.clear()
+        fnr = None
+        for e in person['extid']:
+            if e['type'] == 'fnr':
+                fnr = e['val']
+        person_id = None
+        if fnr is not None:
+            try:
+                print "Fnr: %s" % fnr,
+                personObj.find_by_external_id(co.externalid_fodselsnr, fnr)
+                person_id = personObj.person_id
+                print " ********* OLD *************"
+            except Errors.NotFoundError:
+                print " ********* NEW *************"
+                pass
+        if person_id is None:
+            # Personen fantes ikke, lag den
+            bdate = [int(x) for x in person['bdate'].split('-')]
+            personObj.populate(Cerebrum.Date(*bdate),
+                                co.gender_unknown)
+            personObj.affect_names(co.system_ureg, *(namestr2const.values()))
+
+            for k in person['name']:
+                if namestr2const.has_key(k['type']):
+                    personObj.populate_name(namestr2const[k['type']], k['val'])
+            if fnr is not None:
+                personObj.populate_external_id(co.system_ureg, co.externalid_fodselsnr, fnr)
+            # personObj.populate_contact_info(co.contact_phone, tlf)
+            # personObj.affect_addresses(co.system_lt, co.address_post)
+            # personObj.populate_address(co.address_post, addr="%s\n%s" %
+
+            # if stedkode <> '':
+            #             try:
+            #                 fak, inst, gruppe = stedkode[0:2], stedkode[2:4], stedkode[4:6]
+            #                 ou.find_stedkode(int(fak), int(inst), int(gruppe))
+            #                 personObj.affect_affiliations(co.system_lt, co.affiliation_employee)
+            #                 personObj.populate_affiliation(ou.ou_id, co.affiliation_employee,
+            #                                                 co.affiliation_status_employee_valid)
+            #             except:
+            #                 print "Error setting stedkode"
+
+            personObj.write_db()
+            person_id = personObj.person_id
+
+        # Bygg brukeren
+        # print "Building user for person_id=%s" % person_id
+        for u in person['user']:
+            print "%s" % u['uname']
+            expire_date = None  # TODO
+            gecos = None        # TODO
+            shell = co.posix_shell_bash # TODO: shell2shellconst[u['shell']]
+            
+            group=PosixGroup.PosixGroup(Cerebrum)
+            group.find_by_gid(u['dfg'])
+            account = Account.Account(Cerebrum)
+            account.clear()
+            account.populate(u['uname'],
+                             co.entity_person,  # Owner type TODO
+                             person_id,
+                             None, 
+                             888888, expire_date)
+            account.write_db()
+
+            posix_user = PosixUser.PosixUser(Cerebrum)
+            posix_user.populate(account.entity_id, u['uid'], group.entity_id, gecos,
+                                u['home'], shell)
+            posix_user.set_password('TODO')    # TODO
+            posix_user.write_db(account)
+            Cerebrum.commit()
+
+if __name__ == '__main__':
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "p:g:PGM",
+                                   ["pfile=", "gfile=", "persons", "groups", "groupmembers"])
+    except getopt.GetoptError:
+        print "usage()"
+        sys.exit(2)
+    pfile = default_personfile
+    gfile = default_groupfile
+    for o, a in opts:
+        if o in ('-p', '--pfile'):
+            pfile = a
+        elif o in ('-g', '--gfile'):
+            gfile = a
+        elif o in ('-P', '--persons'):
+            import_person_users(pfile)
+        elif o in ('-G', '--groups'):
+            import_groups(gfile, 0)
+        elif o in ('-M', '--groupmembers'):
+            import_groups(gfile, 1)
