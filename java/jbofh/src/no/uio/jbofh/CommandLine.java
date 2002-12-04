623238b81436b395a3819ccfc7bb124a3bc0923a
Rune Frøysa <runefro@usit.uio.no>
2002-12-04 08:59:09 +0000

    Extended the parsing mecanism to allow () enclosed sub-groups of parameters.

--- java/jbofh/src/no/uio/jbofh/CommandLine.java
+++ java/jbofh/src/no/uio/jbofh/CommandLine.java
@@ -11,6 +11,8 @@ import java.util.Enumeration;
 import org.gnu.readline.*;
 import java.io.EOFException;
 import org.apache.log4j.Category;
+import java.io.IOException;
+import java.text.ParseException;
 
 /**
  *
@@ -31,21 +33,28 @@ public class CommandLine {
     }
 
     /**
+     * Split string into tokens, using whitespace as delimiter.
+     * Matching '/" pairs can be used to include whitespace in the
+     * tokens.  Sub-groups marked by matching parenthesis are returned
+     * as sub-vectors. Sub-sub groups are not allowed.
+     *
      * @param str
-     * @return An array consisting of commands separated by whitespace. Pairs of '/" signs will be grouped
+     * @return A vector of parsed tokens.
      */    
-    String [] splitCommand(String str) {
+    Vector splitCommand(String str) throws ParseException {
         /* This could probably be done easier by using String.parse(), but that would require JDK1.4 */
         
         char chars[] = (str+" ").toCharArray();
         Vector ret = new Vector();
+	Vector subCmd = null, curApp = ret;
         int i = 0, pstart = 0;
         Character quote = null;
         while(i < chars.length) {
             if(quote != null) {
                 if(chars[i] == quote.charValue()) {                
-                    if(i > pstart) 
-                        ret.add(new String(str.substring(pstart, i)));
+                    if(i > pstart) {
+                        curApp.add(new String(str.substring(pstart, i)));
+		    }
                     pstart = i+1;
                     quote = null;
                 }                
@@ -53,46 +62,73 @@ public class CommandLine {
                 if(chars[i] == '\'' || chars[i] == '"') {
                     pstart = i+1;
                     quote = new Character(chars[i]);
-                } else if(chars[i] == ' ') {                
-                    if(i > pstart) 
-                        ret.add(new String(str.substring(pstart, i)));
+                } else if(chars[i] == ' ' || chars[i] == '(' || chars[i] == ')') {                
+                    if(i > pstart) {
+                        curApp.add(new String(str.substring(pstart, i)));
+		    }
                     pstart = i+1;
+		    if(chars[i] == ')') {
+			if(subCmd == null) 
+			    throw new ParseException(") with no (", i);
+			ret.add(curApp);
+			curApp = ret;
+			subCmd = null;
+		    } else if(chars[i] == '(') {
+			if(subCmd != null) 
+			    throw new ParseException("nested paranthesis detected", i);
+			subCmd = new Vector();
+			curApp = subCmd;
+		    }
                 }
             }
             i++;
         }
-        String r[] = new String[ret.size()];
-        for(i = 0; i < ret.size(); i++) r[i] = (String) ret.get(i);
-        return r; // (String []) ret.toArray();
+	if(quote != null)
+	    throw new ParseException("Missing end-quote", i);
+	if(subCmd != null)
+	    throw new ParseException("Missing end )", i);
+        return ret;
     }
     
-    String promptArg(String prompt, boolean addHist) {
+    String promptArg(String prompt, boolean addHist) throws IOException {
         while (true) {
-            try {
-                Vector oldHist = new Vector();
-                // A readline thingy where methods were non-static would have helped a lot.
-                if(! addHist) Readline.getHistory(oldHist);
-                String ret = Readline.readline(prompt);
-                if(! addHist) {
-                    Readline.clearHistory();
-                    for (Enumeration e = oldHist.elements() ; e.hasMoreElements() ;) 
-                        Readline.addToHistory((String) e.nextElement());
-                }
-                return ret;
-            } catch (EOFException e) {
-                return null;
-            } catch (Exception e) {
-                logger.error("Unexpected exception reading commandline", e);
-                System.out.println("Unexpected error: "+e);
-                return "";
-            }
+	    Vector oldHist = new Vector();
+	    // A readline thingy where methods were non-static would have helped a lot.
+	    if(! addHist) Readline.getHistory(oldHist);
+	    String ret = Readline.readline(prompt);
+	    if(! addHist) {
+		Readline.clearHistory();
+		for (Enumeration e = oldHist.elements() ; e.hasMoreElements() ;) 
+		    Readline.addToHistory((String) e.nextElement());
+	    }
+	    if(ret == null) ret = "";
+	    return ret;
         }
     }
     
-    String [] getSplittedCommand() {
-        String line = promptArg("jbofh> ", true);
-        if(line != null) 
-            return splitCommand(line);
-        return null;
+    Vector getSplittedCommand() throws IOException, ParseException {
+	return splitCommand(promptArg("jbofh> ", true));
+    }
+
+    public static void main(String[] args) {
+	String tests[] = {
+	    "dette er en test",
+	    "en 'noe annerledes' test",
+	    "en (parantes test 'med quote' test) hest",
+	    "mer(test hei)du morn ",
+	    "en liten (test av) dette) her",
+	    "mer (enn du(skulle tro))"
+	};
+        CommandLine cLine = new CommandLine(Category.getInstance(CommandLine.class));
+	for(int j = 0; j < tests.length; j++) {
+	    System.out.println("split: --------"+tests[j]+"-----------");
+	    try {
+		Vector v = cLine.splitCommand(tests[j]);
+		for(int i = 0; i < v.size(); i++)
+		    System.out.println(i+": '"+v.get(i)+"'");
+	    } catch (ParseException ex) {
+		System.out.println("got: "+ex);
+	    }
+	}	
     }
 }
