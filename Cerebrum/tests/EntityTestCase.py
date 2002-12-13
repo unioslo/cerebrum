149ed61218e218f2e42aebfbf7f9fe33e4d4dca6
Rune Frøysa <runefro@usit.uio.no>
2002-12-13 11:36:17 +0000

    Added EntityContactInfoTestCase

--- Cerebrum/tests/EntityTestCase.py
+++ Cerebrum/tests/EntityTestCase.py
@@ -5,7 +5,7 @@
 import unittest
 from Cerebrum import Database
 from Cerebrum.Entity import \
-     Entity, EntityName
+     Entity, EntityName, EntityContactInfo
 from Cerebrum import Constants
 from Cerebrum import cereconf
 import traceback
@@ -79,7 +79,11 @@ class EntityName_createTestCase(Entity_createTestCase, EntityName):
     test_name = "foobar3"
     def setUp(self):
         super(EntityName_createTestCase, self).setUp()
-        self.add_name(self.co.account_namespace, self.test_name)
+        try:
+            self.add_name(self.co.account_namespace, self.test_name)
+        except:
+            print "Error: unable to create EntityName"
+            traceback.print_exc()            
 
     def tearDown(self):
         self.delete_name(self.co.account_namespace)
@@ -117,6 +121,53 @@ class EntityNameTestCase(EntityName_createTestCase):
         return suite
     suite = staticmethod(suite)
 
+class EntityContactInfo_createTestCase(Entity_createTestCase, EntityContactInfo):
+    test_ci = {'src': 'system_manual', 'type': 'contact_phone',
+               'pref': 10, 'value': '+47 12345678',
+               'desc': 'some description'}
+
+    def setUp(self):
+        super(EntityContactInfo_createTestCase, self).setUp()
+        try:
+            for k in ('src', 'type'):
+                if isinstance(self.test_ci[k], str):
+                    self.test_ci[k] = getattr(self.co, self.test_ci[k])
+            self.add_contact_info(self.test_ci['src'], self.test_ci['type'], self.test_ci['value'],
+                                  self.test_ci['pref'],  self.test_ci['desc'])
+        except:
+            print "Error: unable to create EntityContactInfo"
+            traceback.print_exc()
+
+    def tearDown(self):
+        self.delete_contact_info(self.test_ci['src'], self.test_ci['type'])
+        super(EntityContactInfo_createTestCase, self).tearDown()
+
+class EntityContactInfoTestCase(EntityContactInfo_createTestCase):
+    def testEntityGetContactInfo(self):
+        "Test that one can get the created EntityContactInfo"
+
+        ci = self.get_contact_info(self.test_ci['src'], self.test_ci['type'])
+        ci = ci[0]
+        self.failIf(ci['contact_value'] <> self.test_ci['value'] or \
+                    ci['description'] <> self.test_ci['desc'], "EntityContactInfo should be equal")
+
+    def testEntityDeleteContactInfo(self):
+        "Test that the EntityContactInfo can be deleted"
+        
+        self.delete_contact_info(self.test_ci['src'], self.test_ci['type'])
+        try:
+            ci = self.get_contact_info(self.test_ci['src'], self.test_ci['type'])
+            fail("Error: should no longer exist")
+        except:
+            pass
+
+    def suite():
+        suite = unittest.TestSuite()
+        suite.addTest(EntityContactInfoTestCase("testEntityGetContactInfo"))
+        suite.addTest(EntityContactInfoTestCase("testEntityDeleteContactInfo"))
+        return suite
+    suite = staticmethod(suite)
+
 def suite():
     """Returns a suite containing all the test cases in this module.
        It can be a good idea to put an identically named factory function
@@ -126,8 +177,9 @@ def suite():
 
     suite1 = EntityTestCase.suite()
     suite2 = EntityNameTestCase.suite()
+    suite3 = EntityContactInfoTestCase.suite()
 
-    return unittest.TestSuite((suite1, suite2))
+    return unittest.TestSuite((suite1, suite2, suite3))
 
 
 if __name__ == '__main__':
