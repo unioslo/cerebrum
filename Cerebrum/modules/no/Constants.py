c080f68e59382a23f2da5fcb73b94755c1bcad00
Rune Frøysa <runefro@usit.uio.no>
2002-12-16 13:26:20 +0000

    some sats constants

---
+++ Cerebrum/modules/no/Constants.py
@@ -0,0 +1,12 @@
+"""Access to Cerebrum code values.
+
+"""
+
+from Cerebrum import Constants
+from Cerebrum.Constants import _AuthoritativeSystemCode
+from Cerebrum.Constants import _PersonExternalIdCode
+
+class Constants(Constants.Constants):
+    system_sats = _AuthoritativeSystemCode('SATS')
+
+    externalid_personoid = _PersonExternalIdCode('SATS_PERSONOID')
