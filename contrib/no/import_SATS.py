07050b273888172da830f0de1f12251d586556f9
Rune Frøysa <runefro@usit.uio.no>
2002-12-16 12:25:37 +0000

    Import av personer

--- contrib/no/import_SATS.py
+++ contrib/no/import_SATS.py
@@ -2,14 +2,19 @@
 
 import pprint
 import sys
+import string
 from Cerebrum import Database
 from Cerebrum.Utils import Factory
+from Cerebrum import Person
+from Cerebrum.modules.no import fodselsnr
 
 pp = pprint.PrettyPrinter(indent=4)
 
 Cerebrum = Database.connect()
 co = Factory.getConstants()(Cerebrum)
 OU_class = Factory.get('OU')
+
+source_system = co.system_manual
 
 def read_inputfile(filename):
     print "Processing %s" % filename
@@ -88,18 +93,96 @@ def import_OU(gs_vg):
             pp.pprint(skole)
         else:
             postno, city = skole[loc['address3']].split()
-            ou.add_entity_address(co.system_manual, co.address_post,
+            ou.add_entity_address(source_system, co.address_post,
                                   address_text=skole[loc['address1']],
                                   postal_number=postno, city=city)
         if skole[loc['phoneno']] <> '':
-            ou.add_contact_info(co.system_manual, co.contact_phone, skole[loc['phoneno']])
+            ou.add_contact_info(source_system, co.contact_phone, skole[loc['phoneno']])
         if skole[loc['faxno']] <> '':
-            ou.add_contact_info(co.system_manual, co.contact_fax, skole[loc['faxno']])
+            ou.add_contact_info(source_system, co.contact_fax, skole[loc['faxno']])
+    Cerebrum.commit()
+    print
+
+def import_elever(gs_vg):
+
+    # TODO: Ved en triviell omskriving av denne vil den også kunne
+    # importere foreldre og ansatte.  Tipper den da bør returnere en
+    # dict som mapper personoid -> entityid.
+    # 
+    # En spesialisert rutine kan deretter gjøre nødvendig populering
+    # av foreldre -> barn, samt melde inn i grupper.  Hvordan dette
+    # skal gjøres må diskuteres nærmere, da jeg ikke vet hva som er
+    # ønsket.
+
+    person = Person.Person(Cerebrum)
+    spec, dta = read_inputfile("sats/person_elev_%s.txt" % gs_vg)
+    loc = {}
+    n = 0
+    for k in spec:
+        loc[k.lower()] = n
+        n += 1
+
+    # TODO: mange records har ikke SSN.  Skal man lagre personoid som
+    # external_id?
+
+    for elev in dta:
+        sys.stdout.write('.')
+        sys.stdout.flush()
+        person.clear()
+        gender = co.gender_female
+        if elev[loc['sex']] == 1:
+            gender = co.gender_male
+
+        date = Cerebrum.Date(1970,1,1)
+        try:
+            day, mon, year = [int(x) for x in elev[loc['birthday']].split('.')]
+            # if year < 1990: continue  # Speedup while testing
+            date = Cerebrum.Date(year, mon, day)
+        except:
+            print "\nWARNING: Bad date %s for %s" % (elev[loc['birthday']],
+                                                     elev[loc['personoid']])
+        person.populate(date, gender)
+        person.affect_names(source_system, co.name_first, co.name_last)
+        person.populate_name(co.name_first, elev[loc['firstname']])
+        person.populate_name(co.name_last, elev[loc['lastname']])
+        if elev[loc['socialsecno']] <> '':
+            person.populate_external_id(source_system, co.externalid_fodselsnr,
+                                        elev[loc['socialsecno']])
+        else:
+            print "\nWARNING: no ssid for %s" % elev[loc['personoid']]
+        ##person.populate_external_id(source_system, co.externalid_sats_oid,
+        ##                            elev[loc['personoid']])
+
+        try:
+            person.write_db()
+        except:
+            print "Error"  # TODO: log
+            continue
+        if elev[loc['address3']] == '': # or elev[loc['address1']] == '':
+            print "\nWARNING: Bad address for %s" % elev[loc['socialsecno']]
+            pp.pprint(elev)
+        else:
+            postno, city = string.split(elev[loc['address3']], maxsplit=1)
+            if postno.isdigit():
+                person.add_entity_address(source_system, co.address_post,
+                                          address_text=elev[loc['address1']],
+                                          postal_number=postno, city=city)
+            else:
+                print "\nWARNING: Bad address for %s" % elev[loc['personoid']]
+        if elev[loc['phoneno']] <> '':
+            person.add_contact_info(source_system, co.contact_phone, elev[loc['phoneno']])
+        if elev[loc['faxno']] <> '':
+            person.add_contact_info(source_system, co.contact_fax, elev[loc['faxno']])
+        ##if elev[loc['email']] <> '':
+        ##    person.add_contact_info(source_system, co.contact_email, elev[loc['email']])
+
     Cerebrum.commit()
     print
     
 def main():
-    import_OU('vg')
+    # convert_all()
+    # import_OU('vg')
+    import_elever('vg')
 
 if __name__ == '__main__':
     main()
