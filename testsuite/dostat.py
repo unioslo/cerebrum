8f6b0e8fc731e7f912a838bf02099bba76d3b0ff
Rune Frøysa <runefro@usit.uio.no>
2002-12-17 13:40:06 +0000

    Simple profiling example

---
+++ testsuite/dostat.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2.2
+
+#
+# This example shows how one can perform profiling in python
+#
+
+import profile
+import pstats
+import import_SATS
+
+pr = profile.Profile()
+pr.calibrate(1000)
+
+pdta = pr.run('import_SATS.main()')
+
+pdta.dump_stats('profile')
+
+s = pstats.Stats('profile')
+s.strip_dirs()
+s.sort_stats('time').print_stats(20)
