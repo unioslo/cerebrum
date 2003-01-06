b48344d8d0a9f39768d7133ac5fc0de42e89f1db
Harald Meland <hmeland@ulrik.uio.no>
2003-01-06 13:08:19 +0000

    Removed some superfluous commit() and clear() calls.
    Moved SQL for cleaning up table ou_info to the tearDown() method, so
    that each test starts with the same state (as set up in setUp()).
    
    When testing for attribute existence, use hasattr() instead of
    getattr().
    
    Revised testDeleteOU() to use self.assertRaises(); previously the test
    would always succeed.

--- Cerebrum/tests/OUTestCase.py
+++ Cerebrum/tests/OUTestCase.py
@@ -5,22 +5,30 @@
 import unittest
 import traceback
 from Cerebrum import Database
+from Cerebrum import Errors
 from Cerebrum import OU
 from Cerebrum import Person
 from Cerebrum import Account
 from Cerebrum import Constants
 
-class OU_createTestCase(unittest.TestCase, object): # 'object' because
-    # unittest isn't an object, and we want to be allowed to call super
+
+# Explicitly inherit from `object` because unittest isn't a new-style
+# class, and we want to be allowed to call super.
+class OU_createTestCase(unittest.TestCase, object):
 
     Cerebrum = Database.connect()
     co = Constants.Constants(Cerebrum)
 
-    ou_dta = {'stednavn': "Stednavn", 'acronym': 'Acronym',
-         'short_name': 'short_name', 'display_name': 'display_name',
-         'sort_name': 'sort_name',
-         'addr': 'addr', 'zip':'zip', 'city': 'city'
-         }
+    ou_dta = {
+        'stednavn': "Stednavn",
+        'acronym': 'Acronym',
+        'short_name': 'short_name',
+        'display_name': 'display_name',
+        'sort_name': 'sort_name',
+        'addr': 'addr',
+        'zip':'zip',
+        'city': 'city'
+        }
 
     def _myPopulateOU(self, ou):
         ou.populate(self.ou_dta['stednavn'], acronym=self.ou_dta['acronym'],
@@ -34,10 +42,8 @@ class OU_createTestCase(unittest.TestCase, object): # 'object' because
 
     def setUp(self):
         try:
-            self.Cerebrum.commit()
             # print "OU_createTestCase.setUp()"
             new_ou = OU.OU(self.Cerebrum)
-            new_ou.clear()
             self._myPopulateOU(new_ou)
             new_ou.write_db()
             self.ou_id = new_ou.entity_id
@@ -47,19 +53,22 @@ class OU_createTestCase(unittest.TestCase, object): # 'object' because
 
     def tearDown(self):
         # print "OU_createTestCase.tearDown()"
-        pass
+        self.Cerebrum.execute("""
+        DELETE FROM [:table schema=cerebrum name=ou_info]
+        WHERE ou_id=:id""", {'id': self.ou_id})
+
 
 class OUTestCase(OU_createTestCase):
+
     def testCreateOU(self):
         "Test that one can create an OU"
-        self.failIf(getattr(self, "ou_id", None) is None)
+        self.failIf(not hasattr(self, "ou_id"))
 
     def testCompareOU(self):
         "Compare created OU from database with set values"
         ou = OU.OU(self.Cerebrum)
         ou.find(self.ou_id)
         new_ou = OU.OU(self.Cerebrum)
-        new_ou.clear()
         self._myPopulateOU(new_ou)
         self.failIf(new_ou <> ou, "Error: should be equal")
         ou.populate('test')
@@ -67,17 +76,11 @@ class OUTestCase(OU_createTestCase):
 
     def testDeleteOU(self):
         "Delete the OU"
-        # This is actually a clean-up method, as we don't support deletion of OUs
-        self.Cerebrum.execute(
-            """DELETE FROM [:table schema=cerebrum name=ou_info]
-               WHERE ou_id=:id""", {'id': self.ou_id})
+        # This is actually a clean-up method, as we don't support
+        # deletion of OUs
+        self.tearDown()
         ou = OU.OU(self.Cerebrum)
-        try:
-            ou.find(self.ou_id)
-            fail("Error: Should no longer exist")
-        except:
-            # OK
-            pass
+        self.assertRaises(Errors.NotFoundError, ou.find, self.ou_id)
 
     def suite():
         suite = unittest.TestSuite()
@@ -87,18 +90,20 @@ class OUTestCase(OU_createTestCase):
         return suite
     suite = staticmethod(suite)
 
+
 def suite():
     """Returns a suite containing all the test cases in this module.
-       It can be a good idea to put an identically named factory function
-       like this in every test module. Such a naming convention allows
-       automation of test discovery.
+
+    It can be a good idea to put an identically named factory function
+    like this in every test module. Such a naming convention allows
+    automation of test discovery.
+
     """
 
     suite1 = OUTestCase.suite()
-
     return unittest.TestSuite((suite1,))
 
 
 if __name__ == '__main__':
-    # When this module is executed from the command-line, run all its tests
+    # When executed as a script, perform all tests in suite().
     unittest.main(defaultTest='suite')
