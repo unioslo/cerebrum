ae0f03ec6b5341c88480fb3df1b39fcccee27f40
Harald Meland <hmeland@ulrik.uio.no>
2003-01-06 13:18:52 +0000

    setUp(): Set self.account to an Account instance associated with the account we use for testing.  This saves some work in GroupTestCase.py.
    _myPopulateAccount(): Keys 'owner_id' and 'creator_id' in
      `account_dta` is only initialized once.
    
    Removed some superfluous clear() calls.
    
    When testing for attribute existence, use hasattr() instead of
    getattr().
    
    Revised testDeleteAccount() to use self.assertRaises(); previously the
    test would always succeed.

--- Cerebrum/tests/AccountTestCase.py
+++ Cerebrum/tests/AccountTestCase.py
@@ -5,86 +5,79 @@
 import unittest
 import cereconf
 from Cerebrum import Database
+from Cerebrum import Errors
 from Cerebrum import OU
 from Cerebrum import Person
 from Cerebrum import Account
 from Cerebrum import Constants
 
-
 from Cerebrum.tests.PersonTestCase import Person_createTestCase
+
 
 class Account_createTestCase(Person_createTestCase):
 
     account_dta = {
         'name': 'name',
         'owner_type': Person_createTestCase.co.entity_person,
-        'owner_id': None,  # Set during setUp
+        'owner_id': None,               # Set during setUp
         'np_type' : None,
-        'creator_id': 888888,
+        'creator_id': None,
         'expire_date': None
         }
 
     def setUp(self):
         super(Account_createTestCase, self).setUp()
-
         account = Account.Account(self.Cerebrum)
-        account.clear()
         self._myPopulateAccount(account)
-
         account.write_db()
-        self.account_id = int(account.entity_id)
+        self.account_id = account.entity_id
+        self.account = account
 
     def _myPopulateAccount(self, account):
         ad = self.account_dta
-        ad['owner_id'] = self.person_id
-        account.clear()
-        account.find_by_name(cereconf.INITIAL_ACCOUNTNAME)
-        ad['creator_id'] = account.entity_id
-        account.clear()
+        if ad.get('owner_id', None) is None:
+            ad['owner_id'] = self.person_id
+        if ad.get('creator_id', None) is None:
+            account.find_by_name(cereconf.INITIAL_ACCOUNTNAME)
+            ad['creator_id'] = account.entity_id
+            account.clear()
         account.populate(ad['name'], ad['owner_type'], ad['owner_id'],
                          ad['np_type'], ad['creator_id'], ad['expire_date'])
 
     def tearDown(self):
         # print "Account_createTestCase.tearDown()"
-        self.Cerebrum.execute(
-            """DELETE FROM [:table schema=cerebrum name=entity_name]
-               WHERE entity_id=:id""", {'id': self.account_id})
-        self.Cerebrum.execute(
-            """DELETE FROM [:table schema=cerebrum name=account_info]
-               WHERE account_id=:id""", {'id': self.account_id})
-        super(Person_createTestCase, self).tearDown()
+        self.Cerebrum.execute("""
+        DELETE FROM [:table schema=cerebrum name=entity_name]
+        WHERE entity_id=:id""", {'id': self.account_id})
+        self.Cerebrum.execute("""
+        DELETE FROM [:table schema=cerebrum name=account_info]
+        WHERE account_id=:id""", {'id': self.account_id})
+        super(Account_createTestCase, self).tearDown()
 
 
 class AccountTestCase(Account_createTestCase):
+
     def testCreateAccount(self):
         "Test that one can create an Account"
-        self.failIf(getattr(self, "account_id", None) is None)
+        self.failIf(not hasattr(self, "account_id"))
 
     def testCompareAccount(self):
         "Check that created database Account object has correct values"
         account = Account.Account(self.Cerebrum)
-        account.clear()
         account.find(self.account_id)
         new_account = Account.Account(self.Cerebrum)
-        new_account.clear()
         self._myPopulateAccount(new_account)
-
         self.failIf(new_account <> account, "Error: should be equal")
         new_account.account_name = 'foobar'
         self.failIf(new_account == account, "Error: should be different")
 
     def testDeleteAccount(self):
         "Delete the Account"
-        # This is actually a clean-up method, as we don't support deletion of Persons
-        old_id = self.account_id
+        # This is actually a clean-up method, as we don't support
+        # deletion of Accounts.
         self.tearDown()
         account = Account.Account(self.Cerebrum)
-        try:
-            account.find(self.account_id)
-            fail("Error: Should no longer exist")
-        except:
-            # OK
-            pass
+        self.assertRaises(Errors.NotFoundError, account.find, self.account_id)
 
     def suite():
         suite = unittest.TestSuite()
@@ -94,18 +87,20 @@ class AccountTestCase(Account_createTestCase):
         return suite
     suite = staticmethod(suite)
 
+
 def suite():
     """Returns a suite containing all the test cases in this module.
-       It can be a good idea to put an identically named factory function
-       like this in every test module. Such a naming convention allows
-       automation of test discovery.
+
+    It can be a good idea to put an identically named factory function
+    like this in every test module. Such a naming convention allows
+    automation of test discovery.
+
     """
 
     suite1 = AccountTestCase.suite()
-
     return unittest.TestSuite((suite1,))
 
 
 if __name__ == '__main__':
-    # When this module is executed from the command-line, run all its tests
+    # When executed as a script, perform all tests in suite().
     unittest.main(defaultTest='suite')
