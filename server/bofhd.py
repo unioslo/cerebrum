8b05a7b737bc35ae2994cc84b840a4c2b680352c
Rune Frøysa <runefro@usit.uio.no>
2003-01-23 11:23:38 +0000

    added session handling on login

--- server/bofhd.py
+++ server/bofhd.py
@@ -29,22 +29,83 @@
 import cerebrum_path
 
 from SimpleXMLRPCServer import SimpleXMLRPCServer
-import re
-import socket
-# import xmlrpclib
-import cereconf
-from Cerebrum import Person
-from Cerebrum import Utils
-from Cerebrum import Account
-from Cerebrum import Errors
-from Cerebrum.Utils import Factory
-from Cerebrum.modules import PosixUser
-import sys
-import traceback
-# import dumputil
-import pprint
 
-pp = pprint.PrettyPrinter(indent=4)
+import socket
+import cereconf
+from Cerebrum import Errors
+from Cerebrum import Account
+from Cerebrum.Utils import Factory
+from Cerebrum import Utils
+from bofhd_errors import CerebrumError
+
+import sys
+import crypt
+from random import Random
+import md5
+
+class HelperFuncs(object):
+    """Internal helper functions that are not remotely callable"""
+
+    def __init__(self, Cerebrum, ef):
+        self.db = Cerebrum
+        self.ef = ef
+
+    def create_user_session(self, account_id):
+        r = Random().random()
+        session_id = "%s-ok%s" %  (account_id, r)  # TODO: strong-random
+        m = md5.md5()
+        m.update(session_id)
+        session_id = m.hexdigest()
+        self.db.execute("""
+        INSERT INTO [:table schema=cerebrum name=bofhd_session] (session_id,
+            account_id, auth_time)
+        VALUES (:session_id, :account_id, [:now])""", {
+            'session_id': session_id,
+            'account_id': account_id
+            })
+        return session_id
+
+    def get_user_from_session(self, session_id):
+        """Map sessionid to an existing authenticated user"""
+        try:
+            entity_id = self.db.query_1("""
+            SELECT account_id
+            FROM [:table schema=cerebrum name=bofhd_session]
+            WHERE session_id=:session_id""",
+                                     {'session_id': session_id})
+        except Errors.NotFoundError:
+            raise CerebrumError, "Authentication failure"
+        return entity_id
+
+    def store_session_state(self, session_id, state_type, entity_id, state_data):
+        self.db.execute("""
+        INSERT INTO [:table schema=cerebrum name=bofhd_session_state]
+          (session_id, state_type, entity_id, state_data, set_time)
+        VALUES (:session_id, :state_type, :entity_id, :state_data, [:now])""" % {
+            'session_id': session_id,
+            'state_type': state_type,
+            'entity_id': entity_id,
+            'state_data': state_data
+            })
+
+    def get_session_state(self, session_id, state_type):
+        return self.db.query("""
+        SELECT entity_id, state_data
+        FROM  [:table schema=cerebrum name=bofhd_session_state]
+        WHERE session_id=:session_id
+        ORDER BY set_time""", {'session_id': session_id})
+
+    def lookupParamInfo(self, cmd, fext, nargs):
+        modref = self.ef.modules[ self.ef.command2module[cmd] ]
+        try:
+            func = getattr(modref, cmd+fext)
+            return (func, None, None, None)
+        except AttributeError:
+            pass
+        cmdspec = modref.all_commands[cmd]
+        assert(nargs < len(cmdspec._params)) # prompt skal ikke kalles hvis for mange argumenter(?)
+        return (None, modref, cmdspec._params[nargs])
+    
 
 class ExportedFuncs(object):
 
@@ -57,13 +118,13 @@ class ExportedFuncs(object):
     """
 
     def __init__(self, Cerebrum, fname):
-        self.defaultSessionId = 'secret'
         self.modules = {}           # Maps modulenames to a module reference
         self.command2module = {}    # Maps a command to a modulename
         self.module_order = []
         self.Cerebrum = Cerebrum
-        self.person = Person.Person(self.Cerebrum)
-        self.const = self.person.const
+        self.const = Factory.get('Constants')(Cerebrum)
+
+        self.helper = HelperFuncs(Cerebrum, self)
             
         f = file(fname)
         while 1:
@@ -89,25 +150,35 @@ class ExportedFuncs(object):
                 print "Warning, function '%s' is not implemented" % k
         
     def login(self, uname, password):
-        if(uname != 'runefro'):
-            raise "CerebrumError", "Invalid user"
-        return self.defaultSessionId
+        account = Account.Account(self.Cerebrum)
+        try:
+            account.find_by_name(uname)
+            enc_pass = account.get_account_authentication(self.const.auth_type_md5)
+        except Errors.NotFoundError:
+            raise CerebrumError, "Invalid user"
+        if enc_pass <> crypt.crypt(password, enc_pass):
+            raise CerebrumError, "Invalid user"  # 'hides' legal usernames
+        try:
+            sessionId = self.helper.create_user_session(account.entity_id)
+            self.Cerebrum.commit()
+            return sessionId
+        except Exception:
+            self.Cerebrum.rollback()
+            raise
 
     def get_commands(self, sessionid):
         # Build a tuple of tuples describing the commands available to
         # the client
 
-        if(sessionid != self.defaultSessionId):
-            raise "CerebrumError", "Authentication failure"
-        else:
-            # TODO: This may potentially return a different command
-            # than self.command2module
-            commands = {}
-            for mn in self.module_order:
-                newcmd = self.modules[mn].get_commands("uname")
-                for k in newcmd.keys():
-                    commands[k] = newcmd[k]
-            return commands
+        user = self.helper.get_user_from_session(sessionid)
+        # TODO: This may potentially return a different command
+        # than self.command2module
+        commands = {}
+        for mn in self.module_order:
+            newcmd = self.modules[mn].get_commands("uname")
+            for k in newcmd.keys():
+                commands[k] = newcmd[k]
+        return commands
 
     def get_format_suggestion(self, cmd):
         modfile = self.command2module[cmd]
@@ -140,7 +211,7 @@ class ExportedFuncs(object):
         """Execute the callable function (in the correct module) with
         the given name after mapping sessionid to username"""
 
-        user = self.get_user_from_session(sessionid)
+        user = self.helper.get_user_from_session(sessionid)
 
         print "Run command: %s (%s)" % (args[0], args)
         modfile = self.command2module[args[0]]
@@ -157,8 +228,6 @@ class ExportedFuncs(object):
                 if isinstance(args[n], list) or isinstance(args[n], tuple):
                     raise NotImplemetedError, "tuple argumenter ikke implemetert enda"
             ret = func(user, *new_args)
-            print "process ret: "
-            pp.pprint(ret)
             self.Cerebrum.commit()
             return self.process_returndata(self.Cerebrum.pythonify_data(ret))
         except Exception:
@@ -177,8 +246,8 @@ class ExportedFuncs(object):
     def tab_complete(self, sessionid, *args):
         "Atempt to tab-complete the command."
         
-        user = self.get_user_from_session(sessionid)
-        func, modref, param = self._lookupParamInfo(args[0], "_tab", len(args)-1)
+        user = self.helper.get_user_from_session(sessionid)
+        func, modref, param = self.helper.lookupParamInfo(args[0], "_tab", len(args)-1)
         if func is not None:
             ret = func(user, *args[1:])
         else:
@@ -191,8 +260,8 @@ class ExportedFuncs(object):
     def prompt_next_param(self, sessionid, *args):
         "Prompt for next parameter."
 
-        user = self.get_user_from_session(sessionid)
-        func, modref, param = self._lookupParamInfo(args[0], "_prompt", len(args)-1)
+        user = self.helper.get_user_from_session(sessionid)
+        func, modref, param = self.helper.lookupParamInfo(args[0], "_prompt", len(args)-1)
         if func is not None:
             ret = func(user, *args[1:])
             return self.process_returndata(ret)
@@ -201,17 +270,6 @@ class ExportedFuncs(object):
                 return param.getPrompt()
             else:
                 return getattr(modref, param._prompt_func)(user, *args[1:])
-
-    def _lookupParamInfo(self, cmd, fext, nargs):
-        modref = self.modules[ self.command2module[cmd] ]
-        try:
-            func = getattr(modref, cmd+fext)
-            return (func, None, None, None)
-        except AttributeError:
-            pass
-        cmdspec = modref.all_commands[cmd]
-        assert(nargs < len(cmdspec._params)) # prompt skal ikke kalles hvis for mange argumenter(?)
-        return (None, modref, cmdspec._params[nargs])
 
     def process_returndata(self, ret):
         """Encode the returndata so that it is a legal XML-RPC structure."""
@@ -234,12 +292,6 @@ class ExportedFuncs(object):
             if isinstance(ret, str):
                 ret = unicode(ret, 'iso8859-1')
             return ret
-
-    def get_user_from_session(self, sessionid):
-        """Map sessionid to an existing authenticated user"""
-        if(1 == 0):
-            raise "CerebrumError", "Authentication failure"
-        return 'runefro'
 
 def find_config_dat():
     # XXX This should get the path from configure
