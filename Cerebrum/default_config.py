e2299f201fb50031ef0d36f16f7848102e9ac7a4
Rune Frøysa <runefro@usit.uio.no>
2003-01-30 14:09:34 +0000

    reflect changes in Factory

--- Cerebrum/default_config.py
+++ Cerebrum/default_config.py
@@ -64,21 +64,22 @@ INITIAL_ACCOUNTNAME = "bootstrap_account"
 # Specify the class this installation should use when working with
 # various entities.
 #
-# The specification must be a two-element tuple; the first element
-# specifies the module, and the second the name of the class to use in
-# that module.
-CLASS_OU = ('Cerebrum.OU', 'OU')
-CLASS_PERSON = ('Cerebrum.Person', 'Person')
-CLASS_ACCOUNT = ('Cerebrum.Account', 'Account')
-CLASS_GROUP = ('Cerebrum.Group', 'Group')
-CLASS_CONSTANTS = ('Cerebrum.Constants', 'Constants')
-CLASS_CL_CONSTANTS = ('Cerebrum.modules.CLConstants', 'CLConstants')
 
-CLASS_DBDRIVER = ('Cerebrum.Database', 'PostgreSQL')
-CLASS_DATABASE = ('Cerebrum.CLDatabase', 'CLDatabase')
+# The specification has the format [modulename/classname].  To use
+# multiple constant classes, list them
+
+CLASS_OU = ['Cerebrum.OU/OU']
+CLASS_PERSON = ['Cerebrum.Person/Person']
+CLASS_ACCOUNT = ['Cerebrum.Account/Account']
+CLASS_GROUP = ['Cerebrum.Group/Group']
+CLASS_CONSTANTS = ['Cerebrum.Constants/Constants']
+CLASS_CL_CONSTANTS = ['Cerebrum.modules.CLConstants/CLConstants']
+
+CLASS_DBDRIVER = ['Cerebrum.Database/PostgreSQL']
+CLASS_DATABASE = ['Cerebrum.CLDatabase/CLDatabase']
 # To enable logging, use this:
-#CLASS_CHANGELOG = ('Cerebrum.modules.ChangeLog', 'ChangeLog')
-CLASS_CHANGELOG = ('Cerebrum.ChangeLog', 'ChangeLog')
+#CLASS_CHANGELOG = ['Cerebrum.modules.ChangeLog/ChangeLog']
+CLASS_CHANGELOG = ['Cerebrum.ChangeLog/ChangeLog']
 
 # Path to templates for passweb.py et. al.
 TEMPLATE_DIR='/path'
