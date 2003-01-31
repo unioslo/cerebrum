44c997984ec90cede9e2857767b3dfa0098b4498
Rune Frøysa <runefro@usit.uio.no>
2003-01-31 12:54:56 +0000

    oboleted by changes in makedb.py

--- design/drop_mod_nis.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE nis_netgroup;
-DROP TABLE nis_domain_code;
-
--- design/mod_drop_posix_user.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE posix_user;
-DROP TABLE posix_group;
-DROP TABLE posix_shell_code;
-DROP SEQUENCE posix_uid_seq;
-
