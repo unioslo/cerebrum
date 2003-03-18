0e77ab5555a8d45c1f7ee724e167485705a1159f
Rune Frøysa <runefro@usit.uio.no>
2003-03-18 13:39:38 +0000

    Has successfully ran process_students to create student accounts.

--- contrib/no/uio/process_students.py
+++ contrib/no/uio/process_students.py
@@ -2,40 +2,49 @@
 
 import getopt
 import sys
-from Cerebrum.modules.no.uio import AutoStud
-from Cerebrum.modules.no import fodselsnr
+import cereconf
 from Cerebrum import Account
+from Cerebrum import Errors
+from Cerebrum import Group
 from Cerebrum import Person
 from Cerebrum.Utils import Factory
+from Cerebrum.modules import PosixUser
+from Cerebrum.modules.no import fodselsnr
+from Cerebrum.modules.no.uio import AutoStud
 
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
 db = Factory.get('Database')()
-co = Factory.get('Constants')(db)
+db.cl_init(change_program='process_students')
+const = Factory.get('Constants')(db)
 
 debug = 0
 
 def bootstrap():
-    global default_creator_id, default_expire_date
+    global default_creator_id, default_expire_date, default_shell
     account = Factory.get('Account')(db)
     account.find_by_name(cereconf.INITIAL_ACCOUNTNAME)
     default_creator_id = account.entity_id
     default_expire_date = None
-    default_shell = co.posix_shell_bash
+    default_shell = const.posix_shell_bash
 
-def create_user(fnr, profile, reserve=1):
+def create_user(fnr, profile, reserve=0):
     print "Create %s: %s" % (fnr, profile)
     person = Person.Person(db)
-    person.find_by_external_id(co.externalid_fodselsnr, fnr, co.system_fs)
+    try:
+        person.find_by_external_id(const.externalid_fodselsnr, fnr, const.system_fs)
+    except Errors.NotFoundError:
+        print "OUCH! person %s not found" % fnr
+        return None
     posix_user = PosixUser.PosixUser(db)
-    full_name = person.get_name(co.system_fs, co.name_full)
-    first_name, last_name = full_name.split(" ", 2) # TODO: fs-import bør lagre begge navn
-    uname = posix_user.suggest_unames(co.account_namespace,
-                                      firstname, lastname)[0]
+    full_name = person.get_name(const.system_fs, const.name_full)
+    first_name, last_name = full_name.split(" ", 1) # TODO: fs-import bør lagre begge navn
+    uname = posix_user.suggest_unames(const.account_namespace,
+                                      first_name, last_name)[0]
     account = Factory.get('Account')(db)
     account.populate(uname,
-                     co.entity_person,
+                     const.entity_person,
                      person.entity_id,
                      None,
                      default_creator_id, default_expire_date)
@@ -44,7 +53,7 @@ def create_user(fnr, profile, reserve=1):
     account.set_password(password)
     account.write_db()
     if not reserve:
-        update_account(profile, [posix_user.entity_id])
+        update_account(profile, [account.entity_id])
     return account.entity_id
 
 def update_account(profile, account_ids, do_move=0, rem_grp=0):
@@ -66,8 +75,9 @@ def update_account(profile, account_ids, do_move=0, rem_grp=0):
                 posix_user.disk_id = disk
             posix_user.gid = profile.get_dfg()
             posix_user.write_db()
-        except NotFoundError:
+        except Errors.NotFoundError:
             disk_id=profile.get_disk()
+            profile.notify_used_disk(old=None, new=disk_id)
             uid = posix_user.get_free_uid()
             gid = profile.get_dfg()
             shell = default_shell
@@ -76,22 +86,23 @@ def update_account(profile, account_ids, do_move=0, rem_grp=0):
             posix_user.write_db()
         already_member = {}
         for r in group.list_groups_with_entity(account.entity_id):
-            if r['operation'] == self.const.group_memberop_union:
-                already_member[r['group_id']] = 1
+            if r['operation'] == const.group_memberop_union:
+                already_member[int(r['group_id'])] = 1
         for g in profile.get_filgrupper():  # TODO: Vil antagelig være get_groups()
             if not already_member.get(g, 0):
                 group.clear()
-                group.find_by_name(g)
-                group.add_memeber(a, self.const.group_memberop_union)
+                group.find(g)
+                group.add_member(account.entity_id, account.entity_type,
+                                 const.group_memberop_union)
             else:
                 del(already_member[g])
         if rem_grp:
             for g in already_member.keys():
-                if g in profile.autogroups:
+                if g in autostud.autogroups:
                     pass  # TODO:  studxonfig.xml should have the list...
         for aff in profile.get_stedkoder():
-            person.populate_affiliation(co.system_fs, aff, co.affiliation_student,
-                                        co.affiliation_status_student_valid)
+            person.populate_affiliation(const.system_fs, aff, const.affiliation_student,
+                                        const.affiliation_status_student_valid)
         # TODO: update default e-mail address
         # TODO: spread
 
@@ -99,42 +110,54 @@ def get_student_accounts():
     account = Account.Account(db)
     person = Person.Person(db)
     ret = {}
-    for a in account.find_accounts_by_type(affiliation=co.affiliation_student):
+    for a in account.find_accounts_by_type(affiliation=const.affiliation_student):
         account.clear()
         account.find(a)
-        if account.owner_type == co.entity_person:
+        if account.owner_type == const.entity_person:
             person.find(account.owner_id)
-            for it, ss, eid in person.get_external_id(id_type=co.externalid_fodselsnr):
+            for it, ss, eid in person.get_external_id(id_type=const.externalid_fodselsnr):
                 eid = fodselsnr.personnr_ok(eid)
-                ret.set_default(eid, []).append(account.entity_id)
+                ret.setdefault(eid, []).append(account.entity_id)
     return ret
 
 def process_students(update_accounts=0, create_users=0):
     students = get_student_accounts()
-    autostud = AutoStud.AutoStud()
+    global autostud
+    autostud = AutoStud.AutoStud(db, debug=debug)
 
     # First process active students
     for t in autostud.get_topics_list():
-        profile = autostud.get_profile(t)
         fnr = fodselsnr.personnr_ok("%06d%05d" % (int(t[0]['fodselsdato']),
                                                   int(t[0]['personnr'])))
-        if create_users and not students.has_key(fnr):
-            students.set_default(fnr, []).append(create_user(fnr, profile))
-        elif update_account and students.has_key(fnr):
-            update_accounts(profile, students[fnr])
-
+        try:
+            profile = autostud.get_profile(t)
+        except Errors.NotFoundError, msg:
+            print "Error for %s: %s" %  (fnr, msg)
+            continue
+        try:
+            if create_users and not students.has_key(fnr):
+                students.setdefault(fnr, []).append(create_user(fnr, profile))
+            elif update_account and students.has_key(fnr):
+                update_accounts(profile, students[fnr])
+        except ValueError, msg:  # TODO: Bad disk should throw a spesific class
+            print "Error for %s: %s" % (fnr, msg)
+    db.commit()
+    return
     # Deretter de som bare har et gyldig opptak
     for s in autostud.get_studprog_students():
         fnr = fodselsnr.personnr_ok("%06d%05d" % (int(s[0]['fodselsdato']),
                                                   int(s[0]['personnr'])))
         if create_users and not students.has_key(fnr):
-            create_user(fnr, profile, reserve=1)
-
+            try:
+                create_user(fnr, profile, reserve=1)
+            except ValueError, msg:  # TODO: Bad disk should throw a spesific class
+                print "Error for %s: %s" % (fnr, msg)
+            
 def main():
     opts, args = getopt.getopt(sys.argv[1:], 'dcu',
                                ['debug', 'create-users', 'update-accounts'])
     global debug
-    update_account = create_users = 0
+    update_accounts = create_users = 0
     bootstrap()
     for opt, val in opts:
         if opt in ('-d', '--debug'):
@@ -148,7 +171,7 @@ def main():
     if(not update_accounts and not create_users):
         usage()
     process_students(update_accounts, create_users)
-
+    
 def usage():
     print """Usage: process_students.py -d | -a
     """
