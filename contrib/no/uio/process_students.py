367b1530940c1e109f598c4bbc89ba411d859387
Rune Frøysa <runefro@usit.uio.no>
2003-03-20 09:58:19 +0000

    added code framework for printing password letters for snail-mail
    made creation of reserved users work

--- contrib/no/uio/process_students.py
+++ contrib/no/uio/process_students.py
@@ -11,6 +11,7 @@ from Cerebrum.Utils import Factory
 from Cerebrum.modules import PosixUser
 from Cerebrum.modules.no import fodselsnr
 from Cerebrum.modules.no.uio import AutoStud
+from server.templates.letters import TemplateHandler
 
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
@@ -18,7 +19,7 @@ pp = pprint.PrettyPrinter(indent=4)
 db = Factory.get('Database')()
 db.cl_init(change_program='process_students')
 const = Factory.get('Constants')(db)
-
+all_passwords = {}
 debug = 0
 
 def bootstrap():
@@ -48,10 +49,11 @@ def create_user(fnr, profile, reserve=0):
                      person.entity_id,
                      None,
                      default_creator_id, default_expire_date)
-    # TODO: Brukernavn + passord må lagres slik at vi kan lage passord ark
     password = posix_user.make_passwd(uname)
     account.set_password(password)
+    # TODO: må også lage brev-info fra profil (språk?)
     show_update('a', account.write_db())
+    all_passwords[int(account.entity_id)] = [password, reserve]
     if not reserve:
         update_account(profile, [account.entity_id])
     return account.entity_id
@@ -123,12 +125,17 @@ def get_student_accounts():
     account = Account.Account(db)
     person = Person.Person(db)
     ret = {}
+    if debug:
+        print "Finding student accounts...",
+        sys.stdout.flush()
     for a in account.list_accounts_by_type(affiliation=const.affiliation_student):
         person.clear()
         person.find(a['person_id'])
         for it, ss, eid in person.get_external_id(id_type=const.externalid_fodselsnr):
             eid = fodselsnr.personnr_ok(eid)
             ret.setdefault(eid, {}).setdefault(int(a['account_id']), []).append([ int(a['ou_id']), int(a['affiliation']) ])
+    if debug:
+        print " found %i entires" % len(ret)
     return ret
 
 def show_update(char, func_ret):
@@ -139,56 +146,111 @@ def show_update(char, func_ret):
     else:
         print "%sM" % char,
 
-    
+def make_letters():
+    person = Person.Person(db)
+    account = Account.Account(db)
+    # TODO: remember that things can go wrong in printing etc, so info
+    #       should probably be saved somewhere
+    dta = {}
+    for account_id in all_passwords.keys():
+        account_id
+        try:
+            account.clear()
+            account.find(account_id)
+            person.clear()
+            person.find(account.owner_id)  # should be account.owner_id
+        except Errors.NotFoundError:
+            print "NotFoundError for account_id=%s" % account_id
+            continue
+        tpl = {}
+        address = person.get_entity_address(source=const.system_fs, type=const.address_post)
+        if address is None:
+            print "Bad address for %s" % account_id
+            continue
+        address = address[0]
+        alines = address['address_text'].split("\n")+[""]
+        tpl['address_line1'] = alines[0]
+        tpl['address_line2'] = alines[1]
+        tpl['address_line3'] = address['p_o_box']
+        tpl['zip'] = address['postal_number']
+        tpl['city'] = address['city']
+        tpl['country'] = address['country']
+
+        tpl['uname'] = account.account_name
+        tpl['password'] =  all_passwords[account_id][0]
+        tpl['fullname'] =  person.get_name(const.system_fs, const.name_full)
+        tmp = person.get_external_id(id_type=const.externalid_fodselsnr,
+                                     source_system=const.system_fs)
+        tpl['birthno'] =  tmp[0]['external_id']
+        tpl['emailadr'] =  "TODO"  # We probably don't need to support this...
+        dta[account_id] = tpl
+
+    keys = dta.keys()
+    keys.sort(lambda x,y: cmp(dta[x]['zip'], dta[y]['zip']))
+    num = 1
+    th_new = TemplateHandler('no', 'new_user', 'txt')
+    th_reserved = TemplateHandler('no', 'new_user', 'txt')  # TODO: write this template
+    for k in keys:
+        dta[k]['lopenr'] = num
+        if all_passwords[k][1]:
+            th = th_reserved
+        else:
+            th = th_new
+        print th._hdr
+        print th.apply_template('body', dta[k])
+        print th._footer
+        num += 1
 
 def process_students(update_accounts=0, create_users=0):
     students = get_student_accounts()
     global autostud
     autostud = AutoStud.AutoStud(db, debug=debug)
 
-    # First process active students
-    for t in autostud.get_topics_list(topics_file=topics_file):
-        fnr = fodselsnr.personnr_ok("%06d%05d" % (int(t[0]['fodselsdato']),
-                                                  int(t[0]['personnr'])))
-        if debug:
-            print "%s" % fnr,
-        try:
-            profile = autostud.get_profile(t)
-        except Errors.NotFoundError, msg:
-            print "  Error for %s: %s" %  (fnr, msg)
-            continue
-        if debug:
-            print " disk=%s, dfg=%s, def_sko=%s, fg=%s sko=%s" % \
-                  (profile._disk, profile.get_dfg(),
-                  profile.get_email_sko(), profile.get_filgrupper(),
-                  profile.get_stedkoder())
-        try:
-            if create_users and not students.has_key(fnr):
-                students.setdefault(fnr, []).append(create_user(fnr, profile))
-            elif update_account and students.has_key(fnr):
-                update_account(profile, students[fnr].keys(), account_info=students[fnr])
+    for run_no in range(2):
+        if run_no == 0:
+            # First process active students
+            lst = autostud.get_topics_list(topics_file=topics_file) 
+        else:
+            # Deretter de som bare har et gyldig opptak
+            lst = autostud.get_studieprog_list(studieprogs_file=studieprogs_file)
+
+        for t in lst:
+            fnr = fodselsnr.personnr_ok("%06d%05d" % (int(t[0]['fodselsdato']),
+                                                      int(t[0]['personnr'])))
+            if run_no == 1 and students.has_key(fnr):
+                continue    # don't need to reserve if person already has an account
             if debug:
-                print
-        except ValueError, msg:  # TODO: Bad disk should throw a spesific class
-            print "  Error for %s: %s" % (fnr, msg)
-    db.commit()
-    return
-    # Deretter de som bare har et gyldig opptak
-    for s in autostud.get_studprog_students():
-        fnr = fodselsnr.personnr_ok("%06d%05d" % (int(s[0]['fodselsdato']),
-                                                  int(s[0]['personnr'])))
-        if create_users and not students.has_key(fnr):
+                print "%s" % fnr,
             try:
-                create_user(fnr, profile, reserve=1)
+                profile = autostud.get_profile(t)
+            except Errors.NotFoundError, msg:
+                print "  Error for %s: %s" %  (fnr, msg)
+                continue
+            if debug:
+                print " disk=%s, dfg=%s, def_sko=%s, fg=%s sko=%s" % \
+                      (profile._disk, profile.get_dfg(),
+                      profile.get_email_sko(), profile.get_filgrupper(),
+                      profile.get_stedkoder())
+            try:
+                if create_users and not students.has_key(fnr):
+                    students.setdefault(fnr, []).append(create_user(fnr, profile, reserve=run_no))
+                elif update_account and run_no == 0 and students.has_key(fnr):
+                    # update_account must only be done on run_no = 0
+                    update_account(profile, students[fnr].keys(), account_info=students[fnr])
+                if debug:
+                    print
             except ValueError, msg:  # TODO: Bad disk should throw a spesific class
-                print "Error for %s: %s" % (fnr, msg)
+                print "  Error for %s: %s" % (fnr, msg)
+    db.commit()  # TBD: should we commit more frequently?
+    make_letters()
             
 def main():
-    opts, args = getopt.getopt(sys.argv[1:], 'dcut:',
-                               ['debug', 'create-users', 'update-accounts', 'topics_file='])
-    global debug, topics_file
+    opts, args = getopt.getopt(sys.argv[1:], 'dcut:s:',
+                               ['debug', 'create-users', 'update-accounts',
+                                'topics-file=', 'studieprogs-file='])
+    global debug, topics_file, studieprogs_file
     update_accounts = create_users = 0
-    topics_file = None
+    studieprogs_file = topics_file = None
     bootstrap()
     for opt, val in opts:
         if opt in ('-d', '--debug'):
@@ -199,6 +261,8 @@ def main():
             update_accounts = 1
         elif opt in ('-t', '--topics-file'):
             topics_file = val
+        elif opt in ('-s', '--studieprogs-file'):
+            studieprogs_file = val
         else:
             usage()
     if(not update_accounts and not create_users):
