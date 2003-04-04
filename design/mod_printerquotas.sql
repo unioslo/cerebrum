1dfadbc47626f2e0a10608da0c3e2897cd2262b5
Rune Frøysa <runefro@usit.uio.no>
2003-04-04 11:06:10 +0000

    mod_printerquotas.sql

---
+++ design/mod_printerquotas.sql
@@ -0,0 +1,15 @@
+category:main;
+CREATE TABLE printerquotas (
+ account_id           NUMERIC(12,0)
+                      CONSTRAINT printerquotas_pk PRIMARY KEY
+                      CONSTRAINT printerquotas_account_id
+                      REFERENCES account_info(account_id),
+ printer_quota        NUMERIC(8),
+ pages_printed        NUMERIC(8),
+ pages_this_semester  NUMERIC(8),
+ termin_quota         NUMERIC(8),
+ has_printerquota     CHAR(1),
+ weekly_quota         NUMERIC(8),
+ max_quota            NUMERIC(8)
+);
+
