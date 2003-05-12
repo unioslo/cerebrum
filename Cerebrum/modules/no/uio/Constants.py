b79161629a1a6152209dfb0c0ae76b05a62d13fc
Rune Frøysa <runefro@usit.uio.no>
2003-05-12 11:13:15 +0000

    added spread constants

--- Cerebrum/modules/no/uio/Constants.py
+++ Cerebrum/modules/no/uio/Constants.py
@@ -23,7 +23,7 @@ get the actual database code/code_str representing a given Entity,
 Address, Gender etc. type."""
 
 from Cerebrum import Constants
-from Cerebrum.Constants import _AuthoritativeSystemCode,_OUPerspectiveCode
+from Cerebrum.Constants import _AuthoritativeSystemCode,_OUPerspectiveCode, _SpreadCode
 
 class Constants(Constants.Constants):
     system_lt = _AuthoritativeSystemCode('LT', 'LT')
@@ -32,3 +32,21 @@ class Constants(Constants.Constants):
 
     perspective_lt = _OUPerspectiveCode('LT', 'LT')
     perspective_fs = _OUPerspectiveCode('FS', 'FS')
+
+    
+    spread_uio_nis_user = _SpreadCode('NIS_user@uio', Constants.Constants.entity_account,
+                                      'User in NIS domain "uio"')
+    spread_uio_nis_fg = _SpreadCode('NIS_fg@uio', Constants.Constants.entity_group,
+                                    'File group in NIS domain "uio"')
+    spread_uio_nis_ng = _SpreadCode('NIS_ng@uio', Constants.Constants.entity_group,
+                                    'Net group in NIS domain "uio"')
+    spread_ifi_nis_user = _SpreadCode('NIS_user@ifi', Constants.Constants.entity_account,
+                                      'User in NIS domain "ifi"')
+    spread_ifi_nis_fg = _SpreadCode('NIS_fg@ifi', Constants.Constants.entity_group,
+                                    'File group in NIS domain "ifi"')
+    spread_ifi_nis_ng = _SpreadCode('NIS_ng@ifi', Constants.Constants.entity_group,
+                                    'Net group in NIS domain "ifi"')
+    spread_uio_ldap_person = _SpreadCode('LDAP_person', Constants.Constants.entity_person,
+                                         'Person included in LDAP directory')
+    spread_uio_ldap_ou = _SpreadCode('LDAP_OU', Constants.Constants.entity_ou,
+                                     'OU included in LDAP directory')
