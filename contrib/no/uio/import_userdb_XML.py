c381dfc2997ba691193246b380a5a7d88c036fb8
Rune Frøysa <runefro@usit.uio.no>
2003-05-12 06:59:50 +0000

    preliminary checkin.  Numerous changes including: - person/user creation is now callback based which reduces memory   usage and provides quicker startup. - performs some checking on spread to determine if Account or   PosixUser should be built. - parser recognizes pwdhist, printerquota and quatantine

--- contrib/no/uio/import_userdb_XML.py
+++ contrib/no/uio/import_userdb_XML.py
@@ -50,10 +50,17 @@ default_personfile = ''
 default_groupfile = ''
 db = Factory.get('Database')()
 db.cl_init(change_program='migrate_iux')
-personObj = Factory.get('Person')(db)
 co = Factory.get('Constants')(db)
 pp = pprint.PrettyPrinter(indent=4)
 prev_msgtime = time()
+
+namestr2const = {'lname': co.name_last, 'fname': co.name_first}
+personObj = Factory.get('Person')(db)
+posix_user = PosixUser.PosixUser(db)
+disk2id = {}
+account = Account.Account(db)
+account.find_by_name(cereconf.INITIAL_ACCOUNTNAME)
+acc_creator_id = account.entity_id
 
 shell2shellconst = {
     'bash': co.posix_shell_bash,
@@ -70,35 +77,20 @@ shell2shellconst = {
     'nologin.stengt': co.posix_shell_nologin,
     'nologin.teppe': co.posix_shell_nologin,
     'puberos': co.posix_shell_nologin,
+    'sftp-server': co.posix_shell_nologin,
     'sh': co.posix_shell_sh,
     'tcsh': co.posix_shell_tcsh,
     'zsh': co.posix_shell_zsh,
     }
-class PersonUserData(object):
-    """This class is used to iterate over all users/persons in the ureg dump. """
-
-    def __init__(self, filename):
-        # Ugly memory-wasting, inflexible way:
-        self.tp = PersonUserParser()
-        xml.sax.parse(filename, self.tp)
-
-    def __iter__(self):
-        return self
-
-    def next(self):
-        """Returns a dict with data about the next person in ureg."""
-        try:
-            return self.tp.personer.pop(0)
-        except IndexError:
-            raise StopIteration, "End of file"
 
 class PersonUserParser(xml.sax.ContentHandler):
     """Parser for the users.xml file.  Stores recognized data in an
     internal datastructure"""
 
-    def __init__(self):
+    def __init__(self, call_back_function):
         self.personer = []
         self.elementstack = []
+        self.call_back_function = call_back_function
 
     def startElement(self, name, attrs):
         tmp = {}
@@ -110,9 +102,9 @@ class PersonUserParser(xml.sax.ContentHandler):
         elif name == "person":
             self.person = {'bdate': tmp['bdate']}
         elif self.elementstack[-1] == "user":
-            if name in ("auth", "spread", "name"):
+            if name in ("auth", "spread", "name", "pwdhist"):
                 self.user[name] = self.user.get(name, []) + [tmp]
-            elif name in ("uio",):
+            elif name in ("uio", "printerquota", "quarantine"):
                 self.user[name] = tmp
             else:
                 print "WARNING: unknown user element: %s" % name
@@ -133,8 +125,8 @@ class PersonUserParser(xml.sax.ContentHandler):
 
     def endElement(self, name):
         if name == "person":
-            self.personer.append(self.person)
-            pass
+            #self.personer.append(self.person)
+            self.call_back_function(self.person)
         elif name == "user":
             self.person['user'] = self.person.get('user', []) + [self.user]
         self.elementstack.pop()
@@ -273,129 +265,141 @@ def import_groups(groupfile, fill=0):
     db.commit()
 
 def import_person_users(personfile):
-    namestr2const = {'lname': co.name_last, 'fname': co.name_first}
-    account = Account.Account(db)
-    account.find_by_name(cereconf.INITIAL_ACCOUNTNAME)
-    acc_creator_id = account.entity_id
+    global gid2entity_id, stedkode2ou_id
 
     showtime("Preparing")
     group=PosixGroup.PosixGroup(db)
     gid2entity_id = {}
-    for row in group.list_all():
-        group.clear()
-        try:
-            group.find(row['group_id'])
-            gid2entity_id[int(group.posix_gid)] = group.entity_id
-        except Errors.NotFoundError:
-            pass  # Not a PosixGroup: OK
-
-    ou = Stedkode.Stedkode(db)
-    stedkode2ou_id = {}
-    for row in ou.list_all():
-        ou.find(row['ou_id'])
-        stedkode2ou_id["%02i%02i%02i" % (
-            ou.fakultet, ou.institutt, ou.avdeling)] = ou.entity_id
-
-    posix_user = PosixUser.PosixUser(db)
-    disk2id = {}
-    showtime("Parsing")
-    for person in PersonUserData(personfile):
-        # pp.pprint(person)
-
-        personObj.clear()
-        fnr = None
-        for e in person['extid']:
-            if e['type'] == 'fnr':
-                if e['val'] <> '00000000000':
-                    fnr = e['val']
-        person_id = None
-        if fnr is not None:
+    if 0:
+        for row in group.list_all():
+            group.clear()
             try:
-                print "Fnr: %s" % fnr,
-                personObj.find_by_external_id(co.externalid_fodselsnr, fnr)
-                person_id = personObj.entity_id
-                print " ********* OLD *************"
+                group.find(row['group_id'])
+                gid2entity_id[int(group.posix_gid)] = group.entity_id
             except Errors.NotFoundError:
-                print " ********* NEW *************"
-                pass
-        else:
-            print "No fnr",
-        if person_id is None:
-            # Personen fantes ikke, lag den
-            if person['bdate'] == '999999':
-                bdate = None
-            else:
-                try:
-                    bdate = [int(x) for x in person['bdate'].split('-')]
-                    bdate = db.Date(*bdate)
-                except:
-                    print "Warning, %s is an illegal date" % person['bdate']
-                    #bdate = [1970, 1, 1]
-                    #bdate = db.Date(*bdate)
-                    bdate = None
-            personObj.populate(bdate,
-                                co.gender_unknown)
-            personObj.affect_names(co.system_ureg, *(namestr2const.values()))
+                pass  # Not a PosixGroup: OK
+    else:
+        for row in group.list_all_ext():
+            gid2entity_id[int(row['posix_gid'])] = row['group_id']
+        
+##     ou = Stedkode.Stedkode(db)
+    stedkode2ou_id = {}
+##     for row in ou.list_all():
+##         ou.clear()
+##         ou.find(row['ou_id'])
+##         stedkode2ou_id["%02i%02i%02i" % (
+##             ou.fakultet, ou.institutt, ou.avdeling)] = ou.entity_id
 
-            for k in person['name']:
-                if namestr2const.has_key(k['type']):
-                    personObj.populate_name(namestr2const[k['type']], k['val'])
-            if fnr is not None:
-                personObj.populate_external_id(co.system_ureg,
-                                               co.externalid_fodselsnr, fnr)
-            for c in person['contact']:
-                if c['type'] == 'workphone':
-                    personObj.populate_contact_info(co.system_ureg,
-                                                    co.contact_phone, c['val'],
-                                                    contact_pref=1)
-                elif c['type'] == 'privphone':
-                    personObj.populate_contact_info(co.system_ureg,
-                                                    co.contact_phone, c['val'],
-                                                    contact_pref=2)
-                elif c['type'] == 'workfax':
-                    personObj.populate_contact_info(co.system_ureg,
-                                                    co.contact_phone, c['val'])
-                elif c['type'] == 'privaddress':
-                    a = c['val'].split('$', 2)
-                    personObj.populate_address(co.system_ureg, co.address_post,
-                                               address_text="\n".join(a))
-            if person.has_key('uio'):
-                if person['uio'].has_key('psko'):
-                    # Typer v/uio: A B M S X a b s
-                    aff = co.affiliation_student   # TODO: define all const
-                    affstat = co.affiliation_status_student_valid
-                    if person['uio']['ptype'] in ('A', 'a'):
-                        aff = co.affiliation_employee
-                        affstat = co.affiliation_status_employee_valid
+    showtime("Parsing")
+    xml.sax.parse(personfile, PersonUserParser(person_callback))
+    db.commit()
 
-                    try:
-                        ou_id = stedkode2ou_id[ person['uio']['psko'] ]
-                        personObj.populate_affiliation(co.system_ureg, ou_id,
-                                                       aff, affstat)
-                    except KeyError:
-                        print "Unknown stedkode: %s" % person['uio']['psko']
+def person_callback(person):
+    # - Hvis personen har spread=u/i, bygg PosixUser.
+    # - Hvis deleted_date ikke er satt, bygg Account.
+    # - Hvis reserved="1", skal brukeren ikke ha spread.
+    # Etter at hele filen er prosessert, gå gjennom alle som har
+    # deleted_date, og bygg PosixUser til de, men sett
+    # expire_date=deleted_date
 
-            personObj.write_db()
+    personObj.clear()
+    fnr = None
+    for e in person['extid']:
+        if e['type'] == 'fnr':
+            if e['val'] <> '00000000000':
+                fnr = e['val']
+    person_id = None
+    if fnr is not None:
+        try:
+            print "Fnr: %s" % fnr,
+            personObj.find_by_external_id(co.externalid_fodselsnr, fnr)
             person_id = personObj.entity_id
+            print " ********* OLD *************"
+        except Errors.NotFoundError:
+            print " ********* NEW *************"
+            pass
+    else:
+        print "No fnr",
+    if person_id is None:
+        # Personen fantes ikke, lag den
+        if person['bdate'] == '999999':
+            bdate = None
+        else:
+            try:
+                bdate = [int(x) for x in person['bdate'].split('-')]
+                bdate = db.Date(*bdate)
+            except:
+                print "Warning, %s is an illegal date" % person['bdate']
+                #bdate = [1970, 1, 1]
+                #bdate = db.Date(*bdate)
+                bdate = None
+        personObj.populate(bdate,
+                            co.gender_unknown)
+        personObj.affect_names(co.system_ureg, *(namestr2const.values()))
 
-        # Bygg brukeren
+        for k in person['name']:
+            if namestr2const.has_key(k['type']):
+                personObj.populate_name(namestr2const[k['type']], k['val'])
+        if fnr is not None:
+            personObj.populate_external_id(co.system_ureg,
+                                           co.externalid_fodselsnr, fnr)
+        for c in person['contact']:
+            if c['type'] == 'workphone':
+                personObj.populate_contact_info(co.system_ureg,
+                                                co.contact_phone, c['val'],
+                                                contact_pref=1)
+            elif c['type'] == 'privphone':
+                personObj.populate_contact_info(co.system_ureg,
+                                                co.contact_phone, c['val'],
+                                                contact_pref=2)
+            elif c['type'] == 'workfax':
+                personObj.populate_contact_info(co.system_ureg,
+                                                co.contact_phone, c['val'])
+            elif c['type'] == 'privaddress':
+                a = c['val'].split('$', 2)
+                personObj.populate_address(co.system_ureg, co.address_post,
+                                           address_text="\n".join(a))
+        if person.has_key('uio'):
+            if person['uio'].has_key('psko'):
+                # Typer v/uio: A B M S X a b s
+                aff = co.affiliation_student   # TODO: define all const
+                affstat = co.affiliation_status_student_valid
+                if person['uio']['ptype'] in ('A', 'a'):
+                    aff = co.affiliation_employee
+                    affstat = co.affiliation_status_employee_valid
 
-        # TODO:
-        # - ta hensyn til spread
-        # - sette rett create_date og creator_id
-        # - hvis disken ikke finnes, registrer den
+                try:
+                    ou_id = stedkode2ou_id[ person['uio']['psko'] ]
+                    personObj.populate_affiliation(co.system_ureg, ou_id,
+                                                   aff, affstat)
+                except KeyError:
+                    print "Unknown stedkode: %s" % person['uio']['psko']
 
-        for u in person['user']:
-            if int(u['uid']) <= 0:
-                print "WARNING, skipped %s" % u
-                continue
-            expire_date = None  # TODO
+        personObj.write_db()
+        person_id = personObj.entity_id
+
+    # Bygg brukeren
+
+    # TODO:
+    # - sette rett create_date og creator_id
+
+    for u in person['user']:
+        if u.has_key('deleted_date'):  # TODO
+            continue
+        expire_date = None  # TODO
+
+        is_posix = 0
+        for tmp in u.get('spread', []):
+            if tmp['domain'] in ('u', 'i'):
+                is_posix = 1
+
+        home = disk_id = None
+        if is_posix:
             gecos = None        # TODO
             shell = shell2shellconst[u['shell']]
             posix_user.clear()
-
+            accountObj = posix_user
             tmp = u['home'].split("/")
-            disk_id = None
             if len(tmp) == 5:
                 disk_id = disk2id.get("/".join(tmp[:4]), None)
                 if disk_id is None:
@@ -403,34 +407,47 @@ def import_person_users(personfile):
                     host = tmp[2]
                     disk_id = make_disk(host, disk, "/".join(tmp[:4]))
                     disk2id["/".join(tmp[:4])] = disk_id
-            if disk_id is None:
-                u['home'] = None
+            if disk_id is not None:  # Only set home if not on a normal disk
+                home = None
+        else:
+            account.clear()
+            accountObj = account
 
-            posix_user.affect_auth_types(co.auth_type_md5_crypt,
-                                      co.auth_type_crypt3_des)
-            for au in u.get('auth', []):
-                if au['type'] == 'plaintext':   # TODO: Should be called last?
-                    posix_user.set_password(au['val'])
-                elif au['type'] == 'md5':
-                    posix_user.populate_authentication_type(
-                        co.auth_type_md5_crypt, au['val'])
-                elif au['type'] == 'crypt':
-                    posix_user.populate_authentication_type(
-                        co.auth_type_crypt3_des, au['val'])
-            posix_user.populate(u['uid'],
+        accountObj.affect_auth_types(co.auth_type_md5_crypt,
+                                     co.auth_type_crypt3_des)
+        for au in u.get('auth', []):
+            if au['type'] == 'plaintext':   # TODO: Should be called last?
+                accountObj.set_password(au['val'])
+            elif au['type'] == 'md5':
+                accountObj.populate_authentication_type(
+                    co.auth_type_md5_crypt, au['val'])
+            elif au['type'] == 'crypt':
+                accountObj.populate_authentication_type(
+                    co.auth_type_crypt3_des, au['val'])
+        if is_posix:
+            accountObj.populate(u['uid'],
                                 gid2entity_id[int(u['dfg'])],
                                 gecos,
                                 shell,
-                                home=u['home'], # TODO: disk_id
+                                home=home, # TODO: disk_id
                                 name=u['uname'],
                                 disk_id=disk_id,
                                 owner_type=co.entity_person,
                                 owner_id=person_id,
                                 creator_id=acc_creator_id,
                                 expire_date=expire_date)
-
-            posix_user.write_db()
-    db.commit()
+        else:
+            accountObj.populate(u['uname'],
+                                co.entity_person,
+                                person_id,
+                                None,
+                                acc_creator_id,
+                                expire_date,
+                                home,
+                                disk_id)
+        accountObj.write_db()
+        for tmp in u.get('spread', []):
+            pass             # TODO
 
 def make_disk(hostname, disk, diskname):
     host = Disk.Host(db)
