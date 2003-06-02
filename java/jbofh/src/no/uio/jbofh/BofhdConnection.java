57d884220a0f03b59a9e95a9eaa3fb5f9615cd98
Rune Frøysa <runefro@usit.uio.no>
2003-06-02 10:18:33 +0000

    Added https support.  Java by default refuses to talk https to machine when the ca can't be verified.  This can be remedied by adding the ca using keytool, which is awkward when distributing the aplication. Thus we implement a specific X509TrustManager that allows us to put the pem file inside the jbofh jar.  Unfortunately Suns documentation for this is rather thin, but I guess this should work.

--- java/jbofh/src/no/uio/jbofh/BofhdConnection.java
+++ java/jbofh/src/no/uio/jbofh/BofhdConnection.java
@@ -7,12 +7,70 @@
 package no.uio.jbofh;
 import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.XmlRpcClient;
+import org.apache.xmlrpc.XmlRpc;
 import org.apache.log4j.Category;
 import java.util.Arrays;
 import java.util.Vector;
 import java.util.Hashtable;
 import java.util.Enumeration;
 import java.io.*;
+
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+/**
+ * Specialized TrustManager called by the SSLSocket framework when
+ * validating server certificate.
+ */
+class InternalTrustManager implements X509TrustManager {
+    static X509Certificate serverCert = null;
+
+    InternalTrustManager() throws IOException, CertificateException {
+	readServerCert();
+    }
+
+    private void readServerCert() throws IOException, CertificateException {
+	InputStream inStream = ResourceLocator.getResource(this, "/cacert.pem").openStream();
+	CertificateFactory cf = CertificateFactory.getInstance("X.509");
+	X509Certificate cert = (X509Certificate)cf.generateCertificate(inStream);
+	inStream.close();
+	serverCert = cert;
+    }
+
+    public void checkClientTrusted( X509Certificate[] cert, String str) {
+	// Not implemented (not called by framework for this client)
+    }
+    
+    public void checkServerTrusted( X509Certificate[] cert, String str) 
+	throws CertificateException {
+	/* This implementation is rather primitive in that we only
+	 * allow a keychain length of one.
+	 */
+	if(cert.length != 1)
+	    throw new CertificateException("Unexpected keychain length");
+
+	if(! serverCert.getSubjectDN().equals(cert[0].getIssuerDN()))
+	    throw new CertificateException("Incorrect issuer for server cert");
+
+	try {
+	    cert[0].verify(serverCert.getPublicKey());
+	} catch (Exception e) {
+	    throw new CertificateException("Bad server certificate: "+e);
+	}
+    }
+    
+    public X509Certificate[] getAcceptedIssuers() {
+	X509Certificate[] ret = new X509Certificate[1];
+	ret[0] = serverCert;
+	return ret;
+    }
+}
 
 /**
  *
@@ -29,6 +87,36 @@ public class BofhdConnection {
     }
 
     void connect(String host_url) {
+	//XmlRpc.setDebug(true);
+	/*
+	  The SecurityTool overrides the default key_store and
+	  trust_store.  The latter is used by the client to validate
+	  the server certificate.
+	 */
+	/*  This works, but requiers server-cert on local filesystem:
+	SecurityTool st = new SecurityTool();
+	st.setTrustStore("jbofh.truststore");
+	try {
+	    st.setup();
+	} catch (Exception e) {
+	    System.out.println("Error setting SecurityTool: "+e);
+	    e.printStackTrace();
+	}
+	*/
+	if(host_url.startsWith("https:")) {
+	    try {
+		InternalTrustManager tm = new InternalTrustManager();
+		TrustManager []tma = {tm};
+		SSLContext sc = SSLContext.getInstance("SSL");  // TLS?
+		sc.init(null,tma, null);
+		SSLSocketFactory sf1 = sc.getSocketFactory();
+		HttpsURLConnection.setDefaultSSLSocketFactory(sf1);	
+	    } catch (Exception e) {
+		System.out.println("Error setting up SSL cert handling: "+e);
+		e.printStackTrace();
+		System.exit(0);
+	    }
+	}
         try {
             xmlrpc = new XmlRpcClient(host_url);
         } catch (java.net.MalformedURLException e) {
