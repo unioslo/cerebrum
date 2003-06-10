45acdb4e0ec67f0548903eb3697c34b9f30170fb
Rune Frøysa <runefro@usit.uio.no>
2003-06-10 20:39:09 +0000

    added tables for storing move_requests

--- design/bofhd_tables.sql
+++ design/bofhd_tables.sql
@@ -56,3 +56,46 @@ CREATE TABLE bofhd_session_state
   set_time     DATE
                NOT NULL
 );
+
+/* bofhd request operation types */
+category:code;
+CREATE TABLE bofhd_request_code
+(
+  code          NUMERIC(6,0)
+                CONSTRAINT bofhd_request_code_pk PRIMARY KEY,
+  code_str      CHAR VARYING(16)
+                NOT NULL
+                CONSTRAINT bofhd_request_codestr_u UNIQUE,
+  description   CHAR VARYING(512)
+                NOT NULL
+);
+
+/* bofhd_request contains pending requests like "move batch". 
+  - account_id of "requester"
+  - run_at not-null iff action should be executed (unlike "move
+    request")
+  - entity_id - identifies the entity affected by the request
+  - destination_id :
+     disk_id for: br_move_user, br_move_request
+     None for: br_move_student
+     group_id for: br_move_give
+TBD: It may be desireable to have a request_id to prevent things like
+     "user move cancel" from removing all entries.
+TBD: It may or may not be a better idea to store the command vector
+sent to bofhd to execute the command.
+*/
+
+category:main;
+CREATE TABLE bofhd_request
+(
+  requestee_id  NUMERIC(12,0),
+  run_at        TIMESTAMP NULL,
+  operation     NUMERIC(6,0)
+                NOT NULL
+                CONSTRAINT operation_code
+                  REFERENCES bofhd_request_code(code),
+  entity_id     NUMERIC(12,0),
+  destination_id NUMERIC(12,0),
+  state_data   CHAR VARYING(80)
+               NULL
+);
