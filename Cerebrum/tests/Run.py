f7ce2fc41e976845482211cf1c7281605d42876f
Rune Frøysa <runefro@usit.uio.no>
2003-06-10 09:13:44 +0000

    don't include the SQLDriverTestCase.suite

--- Cerebrum/tests/Run.py
+++ Cerebrum/tests/Run.py
@@ -34,13 +34,13 @@ def suite():
        automation of test discovery.
     """
 
-    suite1 = SQLDriverTestCase.suite()
+    #suite1 = SQLDriverTestCase.suite()
     suite2 = OUTestCase.suite()
     suite3 = PersonTestCase.suite()
     suite4 = AccountTestCase.suite()
     suite5 = GroupTestCase.suite()
     suite6 = EntityTestCase.suite()
-    return unittest.TestSuite((suite6, suite1, suite2, suite3, suite4, suite5))
+    return unittest.TestSuite((suite6, suite2, suite3, suite4, suite5))
 
 if __name__ == '__main__':
     # When this module is executed from the command-line, run all its tests
