67af62fbe883c0d91c8be366263397b0b6370b12
Harald Meland <hmeland@ulrik.uio.no>
2003-06-11 18:52:21 +0000

    Missing comma.

--- setup.py
+++ setup.py
@@ -268,7 +268,7 @@ setup (name = "Cerebrum", version = Cerebrum.__version__,
                        'owner': cerebrum_user,
                        'mode': 0750},
                       [('cereconf.py', 0644),
-                       ('server/config.dat', 0644)
+                       ('server/config.dat', 0644),
                        ('design/logging.ini', 0644)
                        ]),
                      ({'path': logdir,
