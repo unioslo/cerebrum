82b1e26a6b75283c2efbc261ca030ea87da839d8
rikfred <rikfred@unknown>
2003-06-24 10:28:08 +0000

    Big changes from the first version, rewritten the AD connection routine.

--- contrib/adsync.py
+++ contrib/adsync.py
@@ -22,10 +22,9 @@ import sys
 import time
 import re
 
-# TODO: Should probably avoid "import *" here.
-from socket import *
+import socket 
 
-import cerebrum_path
+#import cerebrum_path
 # TODO: Should probably avoid "import *" here.
 from cereconf import *
 
@@ -51,40 +50,84 @@ account = Account.Account(Cerebrum)
 
 delete_users = 0
 delete_groups = 0
-doit = 0
+max_nmbr_users = 20
+domainusers = []
+
 
 class SocketCom(object):
     """Class for Basic socket communication"""
 
+    p = re.compile('210 OK')
+    
     def __init__(self):
+        self.connect()
+
+        
+    def connect(self):    
         try:
-	    self.sockobj = socket(AF_INET, SOCK_STREAM)
+	    self.sockobj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	    self.sockobj.connect((AD_SERVER_HOST, AD_SERVER_PORT))
-	    print ">>", self.sockobj.recv(1024),
+            print ">>", self.sockobj.recv(8192),
 	    print "<< Authenticating"
 	    self.sockobj.send(AD_PASSWORD)
 	    self.read()
         except:
-	    print 'Error connecting to:', AD_SERVER_HOST, AD_SERVER_PORT
+	    print 'FAILURE connecting to:', AD_SERVER_HOST, AD_SERVER_PORT
+            raise 
 
     def send(self, message):
-        self.sockobj.send(message)
         print "<<", message,
-
-    def read(self):
+        self.last_send=message
+        self.sockobj.send(message)
+        
+    def read(self,out=1):
         received = []
-	data = self.sockobj.recv(1024)
-	received.extend(data.split('\n'))
-	received.remove('')
-	while received[-1][3] == '-':
-	    data = self.sockobj.recv(1024)
-	    received.extend(data.split('\n'))
-	    received.remove('')
-	for elem in received:
-	    print '>>', elem
-	return received
+        rec = []
+        while 1:
+            data = self.sockobj.recv(8192)
+            if data[3] != '-': break
+            m=self.p.search(data)
+            if m: break
+            received.append(data)            
+        received.append(data)
+        #process data
+        for i in received:
+            rec.append(i.strip())
+        if out:     
+            for elem in rec:
+                 print '>>', elem
+        return rec    
 
+            
+##    def read(self,out=1):
+##        received = []
+##	data = self.sockobj.recv(8192)                    
+        #Betyr at oppkoblingen har timet ut, prøver igjen.    
+##        if data == '':
+##            self.connect()
+##           print "WARNING: reconnect, last_send:",self.last_send
+##            self.send(self.last_send)
+##            data = self.sockobj.recv(8192)                    
+##	received.extend(data.split('\n'))
+##        if '' in received:
+##            received.remove('')
+##        while received[-1][3]== '-':
+##        while data:
+##            data = self.sockobj.recv(8192)   
+##            received.extend(data.split('\n'))
+##            if '' in received:
+##                received.remove('')
+##        if out:     
+##            for elem in received:
+##                print '>>', elem
+##	return received
+
+        
+
+    
     def close(self):
+        print 'INFO: Finished, ending session', now()
+        self.sockobj.send("QUIT")
         self.sockobj.close()
 
 
@@ -93,42 +136,40 @@ def full_user_sync():
     # TODO: mangler setting av passord ved oppretting av nye brukere.
     # Mangler en mer stabil sjekk på hvilken OU brukeren hører hjemme
     # utfra cerebrum
-    print 'Starting full_user_sync at', now(), ' doit=', doit
+    # Mangler spread.
+    global domainusers
+    print 'INFO: Starting full_user_sync at', now()
     adusers = {}
     adusers = get_ad_objects('user')
     sock.send('LUSERS&LDAP://%s&1\n' % (AD_LDAP))
     receive = sock.read()
     for line in receive[1:-1]:
         fields = line.split('&')
+        domainusers.append(fields[3])
         if fields[3] in adusers:
             user_id = adusers[fields[3]]
-            ou_seq = get_cere_ou(user_id[1])
-            # Checking if user is in correct OU.
-            #
-            # TBD: In this case two OUs should not have the same name,
-            # better method would be to compare two lists.
-            if ou_seq not in get_ad_ou(fields[1]):
-                sock.send('MOVEOBJ&%s&LDAP://OU=%s,%s\n' % (
-                    fields[1], ou_seq, AD_LDAP))
-                if sock.read() != ['210 OK']:
-                    print "move user failed:", fields[3], 'to', ou_seq
+##          OU sjekk er ikke et krav til UiO Cerebrum. 
+##          ou_seq = get_cere_ou(user_id[1])
+##          if ou_seq not in get_ad_ou(fields[1]):
+##              sock.send('MOVEOBJ&%s&LDAP://OU=%s,%s\n' % (
+##                    fields[1], ou_seq, AD_LDAP))
+##                if sock.read() != ['210 OK']:
+##                    print "WARNING: move user failed, ", fields[3], 'to', ou_seq
             (full_name, account_disable, home_dir, AD_HOME_DRIVE,
-             login_script) = get_user_info(user_id[0])
+             login_script) = get_user_info(user_id[0])            
             # TODO: AD_CANT_CHANGE_PW gir feil output på query,
             # skyldes antageligvis problemer med AD service. Mulige
             # problemer med password expire flag.
-            if ((full_name, account_disable, AD_HOME_DRIVE, home_dir,
-                 login_script, AD_PASSWORD_EXPIRE)
-                !=
-                (fields[9], fields[17], fields[15], fields[7], fields[13],
-                 fields[21])):
-                sock.send('ALTRUSR&%s/%s&fn&%s&dis&%s&'
-                          'hdir&%s&hdr&%s&ls&%s&pexp&%s&ccp&%s\n' % (
-                    AD_DOMAIN, fields[3], full_name, account_disable,
-                    home_dir, AD_HOME_DRIVE, login_script,
-                    AD_PASSWORD_EXPIRE, AD_CANT_CHANGE_PW))
-                if sock.read() != ['210 OK']:
-                    print 'Error updating fields for', fields[3]
+            try:
+                if ((full_name, account_disable, AD_HOME_DRIVE, home_dir,login_script, AD_PASSWORD_EXPIRE) != (fields[9],fields[17],fields[15], fields[7], fields[13], fields[21])):
+                     sock.send('ALTRUSR&%s/%s&fn&%s&dis&%s&hdir&%s&hdr&%s&ls&%s&pexp&%s&ccp&%s\n' % ( AD_DOMAIN, fields[3], full_name, account_disable, home_dir, AD_HOME_DRIVE, login_script, AD_PASSWORD_EXPIRE, AD_CANT_CHANGE_PW))
+                     
+                     if sock.read() != ['210 OK']:
+                         print 'WARNING: Error updating fields for', fields[3]
+                else:
+                    print "INFO:Bruker ",fields[3]," OK"
+            except IndexError:
+                print "WARNING: list index out of range, fields:", full_name, account_disable, AD_HOME_DRIVE, home_dir, login_script, AD_PASSWORD_EXPIRE 
             del adusers[fields[3]]
         elif fields[3] in AD_DONT_TOUCH:
             pass
@@ -136,33 +177,34 @@ def full_user_sync():
             if delete_users:
                 sock.send('DELUSR&%s/%s\n' % (AD_DOMAIN, fields[3]))
                 if sock.read() != ['210 OK']:
-                    print 'Error deleting:', fields[3]
+                    print 'WARNING: Error deleting, ', fields[3]
             else:
                 if AD_LOST_AND_FOUND not in get_ad_ou(fields[1]):
-                    sock.send('MOVEOBJ&%s&LDAP://OU=%s,%s\n' % (
-                        fields[1], AD_LOST_AND_FOUND, AD_LDAP))
+                    sock.send('ALTRUSR&%s/%s&dis&1\n' % ( AD_DOMAIN, fields[3]))
                     if sock.read() != ['210 OK']:
-                        print 'Error moving:', fields[3], 'to', \
-                              AD_LOST_AND_FOUND
+                        print 'WARNING: Error disabling account', fields[3]
+                    sock.send('MOVEOBJ&%s&LDAP://OU=%s,%s\n' % (fields[1], AD_LOST_AND_FOUND, AD_LDAP))
+                    if sock.read() != ['210 OK']:
+                        print 'WARNING: Error moving, ', fields[3], 'to', AD_LOST_AND_FOUND
+                    
+
     for user in adusers:
         # The remaining accounts in the list should be created.
         user_id = adusers[user]
-        ou_struct = get_cere_ou(user_id[1])
-        sock.send('NEWUSR&LDAP://OU=%s,%s&%s&%s\n' % (ou_struct, AD_LDAP,
-                                                      user, user))
+##      OUer ikke et krav i UiO Cerebrum...enda  
+##        ou_struct = get_cere_ou(user_id[1])
+        sock.send('NEWUSR&LDAP://CN=Users,%s&%s&%s\n' % (AD_LDAP,user, user))
         if sock.read() == ['210 OK']:
-            print 'created user:', user, 'in', ou_struct
+            print 'INFO: created user, ', user, 'in Users'
             passw = get_password(user_id[0])
-            (full_name, account_disable, home_dir, AD_HOME_DRIVE,
-             login_script) = get_user_info(user_id[0])
-            sock.send('ALTRUSR&%s/%s&pass&%s&fn&%s&dis&%s&hdir&%s&hdr&%s&'
-                      'ls&%s&pexp&%s&ccp&%s\n' % (
-                AD_DOMAIN, user, passw, full_name, account_disable, home_dir,
-                AD_HOME_DRIVE, login_script, AD_PASSWORD_EXPIRE,
-                AD_CANT_CHANGE_PW))
+            (full_name,account_disable,home_dir,AD_HOME_DRIVE,login_script)\
+                    = get_user_info(user_id[0])
+            sock.send('ALTRUSR&%s/%s&pass&%s&fn&%s&dis&%s&hdir&%s&hdr&%s&ls&%s&pexp&%s&ccp&%s\n' % (AD_DOMAIN,user,passw,full_name,\
+                    account_disable,home_dir,AD_HOME_DRIVE,login_script,\
+                    AD_PASSWORD_EXPIRE,AD_CANT_CHANGE_PW))            
             sock.read()
         else:
-            print 'create user failed:', user, 'in', ou_struct
+            print 'WARNING: create user failed, ', user, 'in Users'
 
 def get_password(user_id):
     # TODO: get correct plaintext password.
@@ -172,10 +214,15 @@ def get_password(user_id):
 
 def get_user_info(user_id):
     account.clear()
-    account.find(user_id)
-    person_id = account.owner_id
-    person.clear()
-    person.find(person_id)
+    try:
+        account.find(user_id)
+        person_id = account.owner_id
+        person.clear()
+        person.find(person_id)
+    except Errors.NotFoundError:
+        print "WARNING: find on person or account failed, aduser_id:", user_id
+        pass
+    
     for ss in AD_SOURCE_SEARCH_ORDER:
         try:
             first_n = person.get_name(int(getattr(co, ss)), int(co.name_first))
@@ -183,6 +230,7 @@ def get_user_info(user_id):
             full_name = first_n +' '+ last_n
             break
         except Errors.NotFoundError:
+            print "WARNING: getting persons name failed, account.owner_id:",person_id
             pass
     # TODO:sjekk også disable mot karantene.
     if account.get_account_expired():
@@ -212,7 +260,8 @@ def get_ad_ou(ldap_path):
 
 def full_group_sync():
     """Checking each group in AD, and compare with cerebrum"""
-    print 'Starting full_group_sync at', now(), ' doit=', doit
+    print 'INFO: Starting full_group_sync at', now()
+    global domainusers
     adgroups = {}
     adgroups = get_ad_objects(int(co.entity_group))
     sock.send('LGROUPS&LDAP://%s\n' % (AD_LDAP))
@@ -220,7 +269,7 @@ def full_group_sync():
     for line in receive[1:-1]:
         fields = line.split('&')
         if fields[3] in adgroups:
-            print 'updating group:', fields[3]
+            print 'INFO: updating group:', fields[3]
             sock.send('LGROUP&%s/%s\n' % (AD_DOMAIN, fields[3]))
             res = sock.read()
             # AD service only list user members not group members of
@@ -231,26 +280,32 @@ def full_group_sync():
             memblist = []
             for grpmemb in group.get_members():
                 ad_object.clear()
-                ad_object.find(grpmemb)
+                try:
+                    ad_object.find(grpmemb)
+                except Errors.NotFoundError:
+                    print "WARNING: Could not find groupmemb,", grpmemb
+                    pass
                 name = ad_object.get_name(int(co.account_namespace))
-                memblist.append(name.entity_name)
-            for l in res[:-1]:
-                print l
+                memblist.append(name)
+            for l in res:
+                if l=='210 OK': break
                 p = re.compile(AD_DOMAIN+'/(.+)')
                 m = p.search(l)
                 member = m.group(1)
                 if member not in memblist:
-                    sock.send('DELUSRGR&%s/%s&%s/%s\n' % (
-                        AD_DOMAIN, m.group(1), AD_DOMAIN, fields[3]))
+                    sock.send('DELUSRGR&%s/%s&%s/%s\n' % (AD_DOMAIN, m.group(1), AD_DOMAIN, fields[3]))
                     if sock.read() != ['210 OK']:
-                        print 'Failed delete', member, 'from', fields[1]
+                        print 'WARNING: Failed delete', member, 'from', fields[1]
                 else:
-                    memblist.remove(member)
+                    memblist.remove(member)                    
             for memb in memblist:
-                sock.send('ADDUSRGR&%s/%s&%s/%s\n' % (AD_DOMAIN, memb,
+                if memb in domainusers:
+                    sock.send('ADDUSRGR&%s/%s&%s/%s\n' % (AD_DOMAIN, memb,
                                                       AD_DOMAIN, fields[3]))
-                if sock.read() != ['210 OK']:
-                    print 'Failed add', memb, 'to', fields[3]
+                    if sock.read() != ['210 OK']:
+                        print 'WARNING: Failed add', memb, 'to', fields[3] 
+                else:
+                    print "WARNING:groupmember",memb,"in group",fields[3],"not in AD"
             del adgroups[fields[3]]
         elif fields[3] in AD_DONT_TOUCH:
             pass
@@ -258,20 +313,21 @@ def full_group_sync():
             if delete_groups:
                 sock.send('DELGR&%s/%s\n' % (AD_DOMAIN, fields[3]))
                 if sock.read() != ['210 OK']:
-                    print 'Error deleting:', fields[3]
+                    print 'WARNING: Error deleting ', fields[3]
             else:
                 if AD_LOST_AND_FOUND not in get_ad_ou(fields[1]):
                     sock.send('MOVEOBJ&%s&LDAP://OU=%s,%s\n' % (
                         fields[1], AD_LOST_AND_FOUND, AD_LDAP))
                     if sock.read() != ['210 OK']:
-                        print 'Error moving:', fields[3], 'to', \
+                        print 'WARNING: Error moving:', fields[3], 'to', \
                               AD_LOST_AND_FOUND
     for grp in adgroups:
         # The remaining is new groups and should be created.
-        grp_ou = adgroups[grp]
-        ou_struct = get_cere_ou(grp_ou[1])
-        sock.send('NEWGR&LDAP://OU=%s,%s&%s&%s\n' % (ou_struct, AD_LDAP,
-                                                     grp, grp))
+
+##      Ikke OU støtte i UIO versjonen.
+##        grp_ou = adgroups[grp]
+##        ou_struct = get_cere_ou(grp_ou[1])
+        sock.send('NEWGR&LDAP://CN=Users,%s&%s&%s\n' % ( AD_LDAP, grp, grp))
         if sock.read() == ['210 OK']:
             group.clear()
             group.find(adgroups[grp][0])
@@ -279,36 +335,27 @@ def full_group_sync():
                 ad_object.clear()
                 ad_object.find(grpmemb)
                 name = ad_object.get_name(int(co.account_namespace))
-                name.entity_name
-                sock.send('ADDUSRGR&%s/%s&%s/%s\n' % (
-                    AD_DOMAIN, name.entity_name, AD_DOMAIN, grp))
+                print 'INFO:Add', name, 'to', grp
+                sock.send('ADDUSRGR&%s/%s&%s/%s\n' % (AD_DOMAIN, name, AD_DOMAIN, grp))
                 if sock.read() != ['210 OK']:
-                    print 'Failed add', memb, 'to', grp
+                    print 'WARNING: Failed add', name, 'to', grp
         else:
-            print 'create group failed:', grp, 'in', ou_struct
+            print 'WARNING: create group failed ', grp,'in Users'
 
 def get_args():
-    global doit
     global delete_users
     global delete_groups
-    val = 'none'
     for arrrgh in sys.argv:
-        if arrrgh == '--quick':
-            val = 'quick'
-        elif arrrgh == '--full':
-            val = 'full'
-        elif arrrgh == '--doit':
-            doit = 1
-        elif arrrgh == '--delete_users':
+        if arrrgh == '--delete_users':
             delete_users = 1
         elif arrrgh == '--delete_groups':
             delete_groups = 1
-    return val
 
 def now():
     return time.ctime(time.time())
 
 def get_ad_objects(entity_type):
+    global max_nmbr_users
     grp_postfix = ''
     if entity_type == 'user':
         e_type = int(co.entity_account)
@@ -321,36 +368,22 @@ def get_ad_objects(entity_type):
     count = 0
     for row in ad_object.list_ad_objects(e_type):
         count = count+1
-        if count > 100: break
+        if count > max_nmbr_users: break
         id = row['entity_id']
         ou_id = row['ou_id']
         id_and_ou = id, ou_id
         ad_object.find(id)
         name = ad_object.get_name(namespace)
-        obj_name = name['entity_name'] + grp_postfix
+        obj_name = "%s%s" % (name,grp_postfix)
         ulist[obj_name]=id_and_ou
         ad_object.clear()
+    print "INFO: Found %s nmbr of objects" % (count)    
     return ulist
 
-def full_ou_sync():
-	print 'Starting full_ou_sync at', now()
-##	print """Expecting that all necessary OUs is already created, will fail
-## if an OU specified in Cerebrum is missing in AD.
-## The correspondence of OU structure in AD and at UIO limit the OU tree in
-## AD to one level from the root, until an OU module to AD is made."""
-
-def main():
-	arg = get_args()
-	if arg == 'quick':
-		quick_sync()
-	elif arg == 'full':
-		full_ou_sync()
-		full_user_sync()
-		full_group_sync()
-	else:
-		print 'Wrong arguments supplied'
-	sock.close()
-
 if __name__ == '__main__':
-	sock = SocketCom()
-	main()
+    sock = SocketCom()    
+    arg = get_args()
+    full_user_sync()
+    full_group_sync()
+    sock.close()
+
