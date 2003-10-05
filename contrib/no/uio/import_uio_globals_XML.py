6b737aac86b500e0dbeb09f8f8b6119f16f4a940
Harald Meland <hmeland@ulrik.uio.no>
2003-10-05 12:43:41 +0000

    Rewritten to be XML-dump-independent.

--- contrib/no/uio/import_uio_globals_XML.py
+++ contrib/no/uio/import_uio_globals_XML.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2.2
 # -*- coding: iso-8859-1 -*-
 
-# Copyright 2002, 2003 University of Oslo, Norway
+# Copyright 2003 University of Oslo, Norway
 #
 # This file is part of Cerebrum.
 #
@@ -20,17 +20,13 @@
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 import cerebrum_path
-import xml.sax
-import sys
-import getopt
-import cereconf
-
 from Cerebrum import Errors
 from Cerebrum.Utils import Factory
 from Cerebrum.modules import Email
 
 db = Factory.get('Database')()
 co = Factory.get('Constants')(db)
+dom = Email.EmailDomain(db)
 
 def is_uio_domain(domain):
     domain = "." + domain
@@ -38,61 +34,25 @@ def is_uio_domain(domain):
             (domain.endswith('.uio.no') and
              not domain.endswith('.ifi.uio.no')))
 
-def create_uio_globals(data):
-    dom = Email.EmailDomain(db)
-    for d in data:
-        dom.clear()
-        if d == "domain":
-            if is_uio_domain(data[d]):
-                try:
-                    dom.find_by_domain(data[d])
+def main():
+    for row in dom.list_email_domains():
+        domain = row['domain']
+        if is_uio_domain(domain):
+            try:
+                dom.clear()
+                dom.find_by_domain(row['domain'])
+                for ctg in dom.get_categories():
+                    if ctg == co.email_domain_category_uio_globals:
+                        break
+                else:
                     dom.add_category(co.email_domain_category_uio_globals)
-                    print "Added: %d: %s" % (dom.email_domain_id, data[d])
-                except Errors.NotFoundError:
-                    print "Not fround: %s" % data[d]
-            else:
-                print "Omitted: %s" % data[d]
-                
-class MailDataParser(xml.sax.ContentHandler):
-
-    def __init__(self, callback):
-        self.callback = callback
-
-    def startElement(self, name, attrs):
-        tmp = {}
-        for k in attrs.keys():
-            tmp[k.encode('iso8859-1')] = attrs[k].encode('iso8859-1')
-        if name == 'email':
-            pass
-        elif name == 'emaildomain':
-            self.callback(tmp)
+                    print "Added: %d: %s" % (dom.email_domain_id, domain)
+            except Errors.NotFoundError:
+                print "Not fround: %s" % domain
         else:
-            pass
-
-def import_email(filename, callback):
-    try:
-        xml.sax.parse(filename, MailDataParser(callback))
-    except StopIteration:
-        pass
+            print "Omitted: %s" % domain
     db.commit()
 
 
 if __name__ == '__main__':
-    try:
-        opts, args = getopt.getopt(sys.argv[1:],"m:","mail-file=")
-    except getopt.GetoptError:
-        usage()
-        sys.exit(2)
-
-    mfile = ""
-        
-    for o, a in opts:
-        if o in ('-m', '--mail-file'):
-            mfile = a
-
-    if mfile == "":
-        usage()
-        
-    import_email(mfile, create_uio_globals)
-
-
+    main()
