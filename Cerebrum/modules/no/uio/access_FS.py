fd523464c647fb4a118d245f40faf2f716412ace
Harald Meland <hmeland@ulrik.uio.no>
2003-10-23 13:28:20 +0000

    GetEmneinf():  * Hvis mulig bør SQL bind variables brukes i stedet for string    interpolation.  * Python bruker innrykk til å avgjøre blokknivå; det synes derfor mer    ryddig om multiline-strenger inne i en blokk ikke har tekst    allerede på kolonne 1.  * Bugfix: '<' skulle være '<='.

--- Cerebrum/modules/no/uio/access_FS.py
+++ Cerebrum/modules/no/uio/access_FS.py
@@ -413,14 +413,13 @@ FROM fs.studieprogram"""
 	"""For hvert definerte Emne henter vi informasjon om 
            ansvarlig sted."""
         qry = """
-
-SELECT e.emnekode, e.versjonskode, e.institusjonsnr_reglement,
-       e.faknr_reglement, e.instituttnr_reglement, e.gruppenr_reglement, 
-       e.studienivakode
-FROM fs.emne e
-WHERE e.arstall_eks_siste is NULL OR
-      e.arstall_eks_siste < %s""" % self.year
-        return (self._get_cols(qry), self.db.query(qry))
+        SELECT e.emnekode, e.versjonskode, e.institusjonsnr_reglement,
+               e.faknr_reglement, e.instituttnr_reglement,
+               e.gruppenr_reglement, e.studienivakode
+        FROM fs.emne e
+        WHERE e.arstall_eks_siste is NULL OR
+              e.arstall_eks_siste <= :year"""
+        return (self._get_cols(qry), self.db.query(qry, {'year': self.year}))
 
 
     def GetStudinfRegkort(self):
