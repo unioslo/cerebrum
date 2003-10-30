4b17ca4d04636344a6437b9b168a4d1ad4b44a6f
Kjetil Homme <kjetilho@ulrik.uio.no>
2003-10-30 09:28:54 +0000

    no need for cereconf.SSH when we already have cereconf.RSH_CMD ...

--- contrib/no/uio/subscribe_imap.py
+++ contrib/no/uio/subscribe_imap.py
@@ -7,5 +7,5 @@ import os
 import sys
 
 # Run: ssh server /bin/sub action server user
-os.execv(cereconf.SSH, [cereconf.SSH, sys.argv[2],
-                        cereconf.SUBSCRIBE_SCRIPT_REMOTE] + sys.argv[1:])
+os.execv(cereconf.RSH_CMD, [cereconf.RSH_CMD, sys.argv[2],
+                            cereconf.SUBSCRIBE_SCRIPT_REMOTE] + sys.argv[1:])
