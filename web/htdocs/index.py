f4e3f973da26d1255ed4ad198b3f19653d66aba5
Stian Soiland-Reyes <stain@unknown>
2003-11-11 08:47:15 +0000

    Include some elements (menu, worklist, activitylog)

--- web/htdocs/index.py
+++ web/htdocs/index.py
@@ -4,11 +4,10 @@ html.Element.__call__ = html.Element.__str__
 from Cerebrum.web.templates.MainTemplate import MainTemplate
 from Cerebrum.web.ActivityLog import ActivityLog
 from Cerebrum.web.WorkList import WorkList
-#from mod_python.Session import Session
+from Cerebrum.web.SideMenu import SideMenu
 
 def index(req, tag="p"):
     req.content_type="text/html"
-#    session = Session(req)
     body = MainTemplate()
     body.title = "Heia du der"
     table = html.SimpleTable(header="row")
@@ -16,15 +15,19 @@ def index(req, tag="p"):
     table.add("Dette", "er")
     table.add("Jævlig", "bra", "altså")
     body.content = table
-    #body.bottom = lambda: "<%s>Heia du</%s>" % (tag, tag)
-#    log = session.setdefault('log', ActivityLog())
+
     log = ActivityLog()
     log.add("Retrieved session")
     log.add("Said hello")
     log.add("And finally goodbye")
+    body.activitylog = log
 
     worklist = WorkList()
-    
-    body.bottom = worklist
-    body.menu = lambda: "Heia du<br/>\n" * 10
+    body.worklist = worklist
+
+
+    sidemenu = SideMenu()
+    body.menu = sidemenu
+
+
     return body
