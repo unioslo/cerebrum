29a7ed3aebe59c75ff99494fd51be535b2a88f4b
Stian Soiland-Reyes <stain@unknown>
2003-11-11 08:46:06 +0000

    Our menu.. at least first draft

---
+++ web/lib/cereweb/SideMenu.py
@@ -0,0 +1,21 @@
+from Menu import Menu
+
+class SideMenu(Menu):
+    def __init__(self):
+        Menu.__init__(self)
+        self.makePerson()
+        self.makeGroup()
+    def makePerson(self):
+        self.person = self.addItem("person", "Person", "/person")
+        self.person.addItem("search", "Search", "/person/search")
+        self.person.addItem("list", "List", "/person/list")
+        self.person.addItem("view", "View", "/person/view?id=%s")
+        self.person.addItem("edit", "Edit", "/person/edit?id=%s")
+        
+    def makeGroup(self):    
+        self.group = self.addItem("group", "Group", "/group")
+        self.group.addItem("search", "Search", "/group/search")
+        self.group.addItem("list", "List", "/group/list")
+        self.group.addItem("view", "View", "/group/view?id=%s")
+        self.group.addItem("edit", "Edit", "/group/edit?id=%s")
+
