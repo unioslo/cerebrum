31d4e252335817cf63e9427251f7fab236ad1165
Mathias Meisfjordskar <mathiasm@ulrik.uio.no>
2003-11-12 15:53:37 +0000

    Part of a major rewrite of generate_mail_ldif.

---
+++ Cerebrum/modules/EmailLDAP.py
@@ -0,0 +1,247 @@
+# -*- coding: iso-8859-1 -*-
+# Copyright 2003 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""."""
+
+import os
+import re
+import time
+import string
+
+from Cerebrum import Utils
+from Cerebrum import Disk
+from Cerebrum import Account
+from Cerebrum import Errors
+from Cerebrum import Utils
+from Cerebrum.modules import Email
+from Cerebrum.Utils import Factory
+from Cerebrum.DatabaseAccessor import DatabaseAccessor
+from Cerebrum.modules.bofhd.utils import BofhdRequests
+
+class EmailLDAP(DatabaseAccessor):
+    """The EmailLDAP class is used to gather methodes used to generate
+    an ldif for mail-backneds."""
+    
+    __metaclass__ = Utils.mark_update
+
+    __write_attr__ = ('aid2addr', 'targ2addr', 'targ2prim', 'targ2spam',
+                      'targ2quota', 'targ2virus', 'serv_id2server',
+                      'targ2server_id', 'targ2forward', 'targ2vacation',
+                      'acc2name', 'pending')
+
+
+    def __init__(self, db):
+        super(EmailLDAP, self).__init__(db)
+        self.const = Factory.get('Constants')(db)
+        # Internal structure:
+        self.aid2addr = {}
+        self.targ2addr = {}
+        self.targ2prim = {}
+        self.targ2spam = {}
+        self.targ2quota = {}
+        self.targ2virus = {}
+        self.serv_id2server = {}
+        self.targ2server_id = {}
+        self.targ2forward = {}
+        self.targ2vacation = {}
+        self.acc2name = {}
+        self.pending = {}
+       
+
+    def _build_addr(self, local_part, domain):
+        return '@'.join((local_part, domain))
+
+
+    def get_targettype(self, targettype):
+        return str(targettype)
+
+
+    def get_server_info(self, target, entity, home):
+        # Find mail-server settings:
+        uname = self.acc2name[entity][0]
+        sinfo = ""
+        if self.targ2server_id.has_key(target):
+            type, name = self.serv_id2server[int(self.targ2server_id[target])]
+            if type == self.const.email_server_type_nfsmbox:
+                if home:
+                    home = "/home/%s" % uname
+                maildrop = "/var/spool/mail"
+                sinfo += "spoolInfo: home=%s maildrop=%s/%s\n" % (
+                    home, maildrop, uname)
+            elif type == self.const.email_server_type_cyrus:
+                sinfo += "IMAPserver: %s\n" % name
+        return sinfo
+    
+    
+    def read_addr(self):
+        mail_dom = Email.EmailDomain(self._db)
+        mail_addr = Email.EmailAddress(self._db)
+        for row in mail_addr.list_email_addresses_ext():
+            a_id, t_id = int(row['address_id']), int(row['target_id'])
+            lp, dom = row['local_part'], row['domain']
+            addr = self._build_addr(lp, dom)
+            self.targ2addr.setdefault(t_id, []).append(addr)
+            self.aid2addr[a_id] = addr
+
+
+    def read_prim(self):
+        mail_prim = Email.EmailPrimaryAddressTarget(self._db)
+        for row in mail_prim.list_email_primary_address_targets():
+            self.targ2prim[int(row['target_id'])] = int(row['address_id'])
+
+
+    def read_spam(self):
+        mail_spam = Email.EmailSpamFilter(self._db)
+        for row in mail_spam.list_email_spam_filters_ext():
+            self.targ2spam[int(row['target_id'])] = [row['level'],
+                                                     row['code_str']]
+
+
+    def read_quota(self):
+        mail_quota = Email.EmailQuota(self._db)
+        for row in mail_quota.list_email_quota_ext():
+            self.targ2quota[int(row['target_id'])] = [row['quota_soft'],
+                                                      row['quota_hard']]
+
+        
+    def read_virus(self):
+        mail_virus = Email.EmailVirusScan(self._db)
+        for row in mail_virus.list_email_virus_ext():
+            self.targ2virus[int(row['target_id'])] = [row['found_str'],
+                                                      row['removed_str'],
+                                                      row['enable']]
+
+
+    def read_server(self):
+        mail_serv = Email.EmailServer(self._db)
+        for row in mail_serv.list_email_server_ext():
+            self.serv_id2server[int(row['server_id'])] = [row['server_type'],
+                                                          row['name']]
+        mail_targ_serv = Email.EmailServerTarget(self._db)
+        for row in mail_targ_serv.list_email_server_targets():
+            self.targ2server_id[int(row['target_id'])] = int(row['server_id'])
+
+            
+    def read_forward(self):
+        mail_forw = Email.EmailForward(self._db)
+        for row in mail_forw.list_email_forwards():
+            self.targ2forward.setdefault(int(row['target_id']),
+                                         []).append([row['forward_to'],
+                                                     row['enable']])
+
+        
+    def read_vacation(self):
+        mail_vaca = Email.EmailVacation(self._db)
+        cur = self._db.DateFromTicks(time.time())
+        def prefer_row(row, oldval):
+            o_txt, o_sdate, o_edate, o_enable = oldval
+            txt, sdate, edate, enable = [row[x]
+                                         for x in ('vacation_text',
+                                                   'start_date',
+                                                   'end_date', 'enable')]
+            spans_now = (sdate <= cur and (edate is None or edate >= cur))
+            o_spans_now = (o_sdate <= cur and (o_edate is None or o_edate >= cur))
+            row_is_newer = sdate > o_sdate
+            if spans_now:
+                if enable == 'T' and o_enable == 'F': return True
+                elif enable == 'T' and o_enable == 'T':
+                    if o_spans_now:
+                        return row_is_newer
+                    else: return True
+                elif enable == 'F' and o_enable == 'T':
+                    if o_spans_now: return False
+                    else: return True
+                else:
+                    if o_spans_now: return row_is_newer
+                    else: return True
+            else:
+                if o_spans_now: return False
+                else: return row_is_newer
+
+        for row in mail_vaca.list_email_vacations():
+            t_id = int(row['target_id'])
+            insert = False
+            if self.targ2vacation.has_key(t_id):
+                if prefer_row(row, self.targ2vacation[t_id]):
+                    insert = True
+            else:
+                insert = True
+            if insert:
+                # Make sure vacations that doesn't span now aren't marked
+                # as active, even though they might be registered as
+                # 'enabled' in the database.
+                enable = False
+                if row['start_date'] <= cur and (row['end_date'] is None
+                                                 or row['end_date'] >= cur):
+                    enable = (row['enable'] == 'T')
+                self.targ2vacation[t_id] = (row['vacation_text'],
+                                            row['start_date'],
+                                            row['end_date'],
+                                            enable)
+
+
+    def read_accounts(self):
+        acc = Account.Account(self._db)
+        for row in acc.list_account_name_home():
+            self.acc2name[int(row['account_id'])] = [row['entity_name'],
+                                                     row['home'],
+                                                     row['path']]
+
+
+    def read_pending_moves(self):
+        br = BofhdRequests(self._db, self.const)
+        for op in (self.const.bofh_email_create,
+                   self.const.bofh_email_move,
+                   self.const.bofh_email_convert):
+            for r in br.get_requests(operation=op):
+                self.pending[int(r['entity_id'])] = True
+
+
+    def read_multi_target(self, group_id):
+        mail_targ = Email.EmailTarget(self._db)
+        grp = Factory.get('Group')(self._db)
+        grp.clear()
+        try:
+            grp.find(group_id)
+        except Errors.NotFoundError:
+            raise ValueError, "no group found: %d" % group_id
+        u, i, d = grp.list_members()
+        if i or d:
+            raise ValueError, "group has non-union members: %d" % group_id
+        # Iterate over group's union members
+        member_addrs = []
+        for member_type, member_id in u:
+            if member_type <> self.const.entity_account:
+                continue
+            # Verify that the user has its own email target.
+            mail_targ.clear()
+            try:
+                mail_targ.find_by_email_target_attrs(
+                    target_type = self.const.email_target_account,
+                    entity_id = member_id)
+            except Errors.NotFoundError:
+                raise ValueError, "no target for group member: %d" % member_id
+            targ_id = int(mail_targ.email_target_id)
+            if not self.targ2addr.has_key(targ_id):
+                continue
+            addrs = self.targ2addr[targ_id][:]
+            addrs.sort()
+            member_addrs.append(addrs[0])
+        return member_addrs
+
---
+++ Cerebrum/modules/no/uio/EmailLDAP.py
@@ -0,0 +1,280 @@
+# -*- coding: iso-8859-1 -*-
+# Copyright 2003 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+""""""
+
+import os
+import re
+import string
+
+from Cerebrum import Disk
+from Cerebrum.modules import Email
+from Cerebrum.modules.EmailLDAP import EmailLDAP
+
+class EmailLDAPUiOMixin(EmailLDAP):
+    """Methods specific for UiO."""
+
+    __write_attr__ = ('home2spool', 'local_uio_domain')
+
+    def __init__(self, db):
+        self.__super.__init__(db)
+        self.local_uio_domain = {}
+        self.home2spool = {}
+        
+
+    spam_act2dig = {'noaction': 0,
+                    'spamfolder': 1,
+                    'dropspam':2}
+    db_tt2ldif_tt = {'account': 'user',
+                     'forward': 'forwardAddress'}         
+    _translate_domains = {'UIO_HOST': 'ulrik.uio.no',
+                          }
+
+
+    def get_targettype(self, targettype):
+        return self.db_tt2ldif_tt.get(str(targettype), str(targettype))
+
+
+    def get_server_info(self, target, entity, home):
+        # Find mail-server settings:
+        uname = self.acc2name[entity][0]
+        sinfo = ""
+        if self.targ2server_id.has_key(target):
+            type, name = self.serv_id2server[int(self.targ2server_id[target])]
+            if type == self.const.email_server_type_nfsmbox:
+                maildrop = "/uio/mailspool/mail"
+                tmphome = "/hom/%s" % uname
+                r = re.search(r'^(/[^/]+/[^/]+)/', home)
+                if r:
+                    tmphome = r.group(1)
+                if self.home2spool.has_key(tmphome):
+                    maildrop = self.home2spool[tmphome]
+                sinfo += "spoolInfo: home=%s maildrop=%s/%s\n" % (
+                    home, maildrop, uname)
+            elif type == self.const.email_server_type_cyrus:
+                sinfo += "IMAPserver: %s\n" % name
+        return sinfo
+    
+
+    def _build_addr(self, local_part, domain):
+        domain = self._translate_domains.get(domain, domain)
+        return '@'.join((local_part, domain))
+
+    
+    def read_server(self):
+        self.__super.read_server()
+        self.make_home2spool()
+        
+  
+    def list_machines(self):
+        disk = Disk.Disk(self._db)
+        res = []
+        path_pattern = re.compile(r'/(?P<department>[^/]+)/(?P<host>[^/]+)/[^/]+')
+        for d in disk.list():
+            path = d['path']
+            m = path_pattern.match(path)
+            if m:
+                res.append([m.group('department'), m.group('host')])
+        return res
+
+
+    def read_spam(self):
+        mail_spam = Email.EmailSpamFilter(self._db)
+        for row in mail_spam.list_email_spam_filters_ext():
+            self.targ2spam[int(row['target_id'])] = [row['level'],
+                                                     self.spam_act2dig.get(row['code_str'], 0)]
+
+    
+    def read_addr(self):
+        mail_dom = Email.EmailDomain(self._db)
+        mail_addr = Email.EmailAddress(self._db)
+        # Handle "magic" domains.
+        #   local_part@magic_domain
+        # defines
+        #   local_part@[all domains with category magic_domains],
+        # overriding any entry for that local_part in any of those
+        # domains.
+        glob_addr = {}
+        for dom_catg in (self.const.email_domain_category_uio_globals,):
+            domain = str(dom_catg)
+            lp_dict = {}
+            glob_addr[domain] = lp_dict
+            # Fill glob_addr[magic domain][local_part]
+            for row in mail_addr.list_email_addresses_ext(domain):
+                lp_dict[row['local_part']] = row
+            for row in mail_dom.list_email_domains_with_category(dom_catg):
+                # Alias glob_addr[domain name] to glob_addr[magic domain],
+                # for later "was local_part@domain overridden" check.
+                glob_addr[row['domain']] = lp_dict
+                # Update dicts 'targ2addr' and 'aid2addr' with the
+                # override addresses.
+                for lp, row2 in lp_dict.items():
+                    # Use 'row', and not 'row2', for domain.  Using 'dom2'
+                    # would give us 'UIO_GLOBALS'.
+                    addr = self._build_addr(lp, row['domain'])
+                    t_id = int(row2['target_id'])
+                    self.targ2addr.setdefault(t_id, []).append(addr)
+                    # Note: We don't need to update aid2addr here, as
+                    # addresses @UIO_GLOBALS aren't allowed to be primary
+                    # addresses.
+        for row in mail_addr.list_email_addresses_ext():
+            lp, dom = row['local_part'], row['domain']
+            # If this address is in a domain that is subject to overrides
+            # from "magic" domains, and the local_part was overridden, skip
+            # this row.
+            if glob_addr.has_key(dom) and glob_addr[dom].has_key(lp):
+                continue
+            addr = self._build_addr(lp, dom)
+            a_id, t_id = [int(row[x]) for x in ('address_id', 'target_id')]
+            self.targ2addr.setdefault(t_id, []).append(addr)
+            self.aid2addr[a_id] = addr
+            
+                
+    def _validate_primary(self, dom, prim, local_uio_domain):
+        if prim in ['pat', 'mons', 'goggins', 'miss', 'smtp', 'mail-mx1',
+                    'mail-mx2', 'mail-mx3']:
+            self.local_uio_domain[dom] = prim
+
+
+    def make_home2spool(self):
+        spoolhost = {}
+        cname_cache = {}
+        curdom, lowpri, primary = "", "", ""
+        no_uio = r'\.uio\.no'
+    
+        # Define domains in zone uio.no whose primary MX is one of our
+        # mail servers as "local domains".
+        cmd = "/local/bin/host -t mx -l uio.no. nissen.uio.no"
+        out = os.popen(cmd)
+        res = out.readlines()
+        err = out.close()
+        if err:
+            raise RuntimeError, '%s: failed with exit code %d' % (cmd, err)
+
+        pat = r'^(\S+) mail is handled by (\d+) (\S+)\.\n$'
+        for line in res:
+            m = re.search(pat, line)
+            if m:
+                dom = string.lower(m.group(1))
+                pri = int(m.group(2))
+                mx = string.lower(m.group(3))
+                dom = re.sub(no_uio, '', dom)
+                mx = re.sub(no_uio, '', mx)
+                if not curdom:
+                    curdom = dom
+                if curdom and curdom != dom:
+                    self._validate_primary(curdom, primary, self.local_uio_domain)
+                    curdom = dom
+                    lowpri, primary = "", ""
+                if (not lowpri) or (pri < lowpri):
+                    lowpri, primary = pri, mx
+                if int(pri) == 33:
+                    spoolhost[dom] = mx
+
+        if curdom and primary:
+            self._validate_primary(curdom, primary, self.local_uio_domain)
+
+        # We have now defined all "proper" local domains (i.e. ones that
+        # have explicit MX records).  We also want to accept mail for any
+        # CNAME in the uio.no zone pointing to any of these local domains.
+
+        cmd = "/local/bin/host -t cname -l uio.no. nissen.uio.no"
+        out = os.popen(cmd)
+        res = out.readlines()
+        err = out.close()
+        if err:
+            raise RuntimeError, '%s: failed with exit code %d' % (cmd, err)
+
+        pat = r'^(\S+) is an alias for (\S+)\.\n$'
+        for line in res:
+            m = re.search(pat, line)
+            if m:
+                alias, real = string.lower(m.group(1)), string.lower(m.group(2))
+                alias = re.sub(no_uio, '', alias)
+                real = re.sub(no_uio, '', real)
+                if self.local_uio_domain.has_key(real):
+                    self.local_uio_domain[alias] = self.local_uio_domain[real]
+                if spoolhost.has_key(real):
+                    spoolhost[alias] = spoolhost[real]
+
+        # Define domains in zone ifi.uio.no whose primary MX is one of our
+        # mail servers as "local domains".  Cache CNAMEs at the same time.
+
+        cmd = "/local/bin/dig @bestemor.ifi.uio.no ifi.uio.no. axfr"
+        out = os.popen(cmd)
+        res = out.readlines()
+        err = out.close()
+        if err:
+            raise RuntimeError, '%s: failed with exit code %d' % (cmd, err)
+
+        pat = r'^(\S+)\.\s+\d+\s+IN\s+MX\s+(\d+)\s+(\S+)\.'
+        pat2 = r'^(\S+)\.\s+\d+\s+IN\s+CNAME\s+(\S+)\.'
+        for line in res:
+            m = re.search(pat, line)
+            if m:
+                dom = string.lower(m.group(1))
+                pri = int(m.group(2))
+                mx = string.lower(m.group(3))
+                dom = re.sub(no_uio, '', dom)
+                mx = re.sub(no_uio, '', mx)
+                if not curdom:
+                    curdom = dom
+                if curdom and curdom != dom:
+                    self._validate_primary(curdom, primary, self.local_uio_domain)
+                    curdom = dom
+                    lowpri, primary = "", ""
+                if (not lowpri) or (pri < lowpri):
+                    lowpri, primary = pri, mx
+                if pri == 33:
+                    spoolhost[dom] = mx
+            else:
+                m = re.search(pat2, line)
+                if m:
+                    alias = string.lower(m.group(1))
+                    real = string.lower(m.group(2))
+                    alias = re.sub(no_uio, '', alias)
+                    real = re.sub(no_uio, '', real)
+                    cname_cache[alias] = real
+
+        if curdom and primary:
+            self._validate_primary(curdom, primary, self.local_uio_domain)
+
+        # Define CNAMEs for domains whose primary MX is one of our mail
+        # servers as "local domains".
+
+        for alias in cname_cache.keys():
+            real = cname_cache[alias]
+            if self.local_uio_domain.has_key(real):
+                self.local_uio_domain[alias] = self.local_uio_domain[real]
+            if spoolhost.has_key(real):
+                spoolhost[alias] = spoolhost[real]
+
+        for faculty, host in self.list_machines():
+            host = string.lower(host)
+            if host == '*':
+                continue
+            if faculty == "ifi":
+                spoolhost[host] = "ulrik"
+                continue
+            elif not spoolhost.has_key(host):
+                continue
+            if spoolhost[host] == "ulrik":
+                continue
+            self.home2spool["/%s/%s" % (faculty, host)] = "/%s/%s/mail" % (
+                faculty, spoolhost[host])
