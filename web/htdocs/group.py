511ec4c1c907f8f6f9815c79430f5839de943090
Thomas Langås <tlan@unknown>
2003-11-27 14:21:41 +0000

    Now we actually save what you do when you try to add a member to a group.

--- web/htdocs/group.py
+++ web/htdocs/group.py
@@ -4,6 +4,7 @@ from Cerebrum.Utils import Factory
 ClientAPI = Factory.get_module("ClientAPI")
 from Cerebrum.web.templates.GroupSearchTemplate import GroupSearchTemplate
 from Cerebrum.web.templates.GroupViewTemplate import GroupViewTemplate
+from Cerebrum.web.templates.GroupAddMemberTemplate import GroupAddMemberTemplate
 from Cerebrum.web.Main import Main
 from gettext import gettext as _
 from Cerebrum.web.utils import url
@@ -47,13 +48,47 @@ def search(req, name, desc, spread):
     page.content = result
     return page    
 
-def view(req, name="test74"):
+def view(req, name):
     page = Main(req)
     page.menu.setFocus("group/view", name)
     server = req.session['server']
     group = ClientAPI.Group.fetch_by_name(name, server)
     view = GroupViewTemplate()
+    view.add_member = lambda group:_add_box(group)
     page.content = lambda: view.viewGroup(group)
     return page
     
+def _add_box(group):
+    operations = [(ClientAPI.Constants.JOIN, _("Union")), 
+                  (ClientAPI.Constants.INTERSECTION, _("Intersection")), 
+                  (ClientAPI.Constants.DIFFERENCE, _("Difference"))]
+    member_types = [("account", _("Account")),
+                    ("group", _("Group"))]
+    action = url("group/add_member?id=%s" % group.id)
 
+    template = GroupAddMemberTemplate()
+    return template.add_member_box(action, member_types, operations)
+
+def add_member(req, id, name, type, operation):
+    server = req.session['server']
+    
+    page = view(req, name)
+    if operation not in (ClientAPI.Constants.JOIN, 
+                         ClientAPI.Constants.INTERSECTION, 
+                         ClientAPI.Constants.DIFFERENCE):
+        # Display an error-message on top of page.
+        page.add_message(_("%s is not a valid operation." % operation), true)
+        return page
+    
+    group = ClientAPI.Group.fetch_by_id(server, id)
+    if (type == "account"):
+        entity = ClientAPI.Account.fetch_by_name(server, name)
+    elif (type == "group"):
+        entity = ClientAPI.Group.fetch_by_name(server, name)
+
+    #FIXME: Operation should be constants somewhere
+    group.add_member(entity, operation)
+   
+    # Display a message stating that entity is added as group-member
+    page.add_message(_("%s added as a member to group." % name), false)
+    return page
