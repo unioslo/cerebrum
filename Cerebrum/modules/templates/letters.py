388af7479d840b708c09535ca6352ed52088099b
Jasmina Hodzic <jasminah@usit.uio.no>
2003-12-07 22:40:37 +0000

    forgot $ in regexp

--- Cerebrum/modules/templates/letters.py
+++ Cerebrum/modules/templates/letters.py
@@ -96,7 +96,7 @@ tag is present, hdr and footer will be empty.
 	# we should probably pass curren_dir as a parameter 
 	# to spool_job from misc list_passwords
 	# TODO: get Rune or Harald to comment this
-	dir = re.sub(r'(/[a-z]*\.[a-z]*)',r'/',filename)
+	dir = re.sub(r'(/[a-z]*\.[a-z]*$)','/',filename)
 	os.chdir(dir)
 	if type == 'tex':
             status = (os.system("%s --interaction nonstopmode %s >> %s 2>&1" % (
