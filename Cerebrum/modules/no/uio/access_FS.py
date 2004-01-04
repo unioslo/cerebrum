4e7ee588e196a05207e9e6d01a4f228e2361b0ea
Igor Rafienko <igorr@ulrik.uio.no>
2004-01-04 21:48:11 +0000

    Added a few helper functions (and modules) to help update_dbfg along

--- Cerebrum/modules/no/uio/access_FS.py
+++ Cerebrum/modules/no/uio/access_FS.py
@@ -1123,3 +1123,20 @@ WHERE k.etterutdkurskode=:kurs AND
             'kurs': kurs,
             'tid': tid,
             'aktkode': aktkode}))
+
+
+    def list_dbfg_usernames(self, fetchall = False):
+        """
+        Get all usernames and return them as a sequence of db_rows.
+
+        NB! This function does *not* return a 2-tuple. Only a sequence of
+        all usernames (the column names can be obtains from db_row objects)
+        """
+
+        query = """
+                SELECT username as username
+                FROM all_users
+                """
+        return self.db.query(query, fetchall = fetchall)
+    # end list_usernames
+
--- Cerebrum/modules/no/uio/access_LT.py
+++ Cerebrum/modules/no/uio/access_LT.py
@@ -271,3 +271,20 @@ VALUES
         if m == None:
             raise InternalError, "Unreconginzable SQL!"
         return [cols.strip() for cols in m.group(2).split(",")]
+
+
+    def list_dbfg_usernames(self, fetchall = False):
+        """
+        Get all usernames and return them as a sequence of db_rows.
+
+        NB! This function does *not* return a 2-tuple. Only a sequence of
+        all usernames (the column names can be obtains from db_row objects)
+        """
+
+        query = """
+                SELECT username as username
+                FROM all_users
+                """
+        return self.db.query(query, fetchall = fetchall)
+    # end list_usernames
+
