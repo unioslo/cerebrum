f908a2fa5ddf40396ad86e25bdc642c83ac56d2e
Stian Soiland-Reyes <stain@unknown>
2004-01-20 14:00:01 +0000

    ChangeType - a type of change in the history log   (as returned by Entity.get_history() )
    Change - an entry in the history log as returned by Entity.get_history()
      - a Change is of a given ChangeType
      References as subject and dest within Change are instances of
      Entity or None.
    
    Entity added methods __repr__ and __str__. These try to
    resolve self.names or self.id to give a somewhat nice
    representation in the shell.
    
    Entity.get_history() added - returns a list of Change
    
    Group.__str__ returns the name, and so does Account.__str__

--- Cerebrum/client/AbstractModel.py
+++ Cerebrum/client/AbstractModel.py
@@ -21,6 +21,52 @@ class Address:
 
     get_info = classmethod(get_info)
 
+class ChangeType:
+    """A type of change in the history log"""
+    def __init__(self, change_type_id, category, change_type, msg):
+        self.id = change_type_id
+        self.category = category
+        self.type = change_type
+        self.message = msg
+        
+    def __eq__(self, other):
+        return isinstance(other, ChangeType) and self.id == other.id
+        
+    def __hash__(self):
+        return hash(self.id) ^ hash(ChangeType)
+
+    def __str__(self):
+        return "%s %s" % (self.category, self.type)    
+
+class Change:
+    """A change entry in the history log"""
+    def __init__(self, type, date, subject, dest, params, change_by):
+        self.type = type # a ChangeType
+        self.date = date # mx.DateTime
+        self.subject = subject # Entity
+        self.dest = dest # Entity or None
+        self.params = params # dict or None
+        self.change_by = change_by # Entity or string (program name)
+        
+    def __repr__(self):
+        date = self.date.strftime("%Y-%m-%d")
+        if self.dest:
+            dest = " " + self.dest
+        else:
+            dest = ""    
+        return "<Change %s %s %s%s>" % (date, self.type, self.subject, self.dest)
+    def __str__(self):    
+        words = self.__dict__.copy()
+        try:
+            words.update(self.params)
+        except Exception, e:
+            pass
+        try:
+            msg = self.type.message % words
+        except KeyError:
+            msg = "%s %s" % (self.type.message, self.subject)
+        date = self.date.strftime("%Y-%m-%d")    
+        return "%s %s" % (date, msg)
 
 class ContactInfo:
 
@@ -48,6 +94,25 @@ class Entity:
         self.addresses = []
         self.contactinfo = []
         self.spreads = []
+    
+    def __repr__(self):
+        names = ["%s=%s" % (key, value) for (key,value) in self.names]
+        desc = " ".join(names)
+        if desc:
+            desc = " " + desc
+        if hasattr(self, "id"):
+            desc = " id=%s%s" % (self.id, desc)
+        return "<%s%s>" % (self.type, desc)
+    
+    def __str__(self):
+        try:
+            return self.names[0][1]    
+        except:
+            if hasattr(self, "id"):
+                return self.id    
+            else:
+                return repr(self)    
+            
     
     def fetch_by_id(cls, id):
         """Retrieves an instance with given id """
@@ -93,9 +158,15 @@ class Entity:
             set.
         """
         pass
+    
+    def get_history(self):
+        pass    
 
 
 class Group(Entity):
+    
+    def __str__(self):
+        return self.name
     
     def create(cls, server, name, description):
         """ Creates a new group with given name and description.
@@ -149,6 +220,9 @@ class Group(Entity):
 
 
 class Account(Entity):
+    
+    def __str__(self):
+        return self.name
 
     def create(cls, name):
         """Creates an account with given name."""
