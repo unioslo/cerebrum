ed5135090827b5be31c00fe398b4b674ffe01d48
Stian Soiland-Reyes <stain@unknown>
2004-03-02 13:45:42 +0000

    Added view_history_short - only retrieves the latest 5 changes

--- web/lib/cereweb/HistoryLog.py
+++ web/lib/cereweb/HistoryLog.py
@@ -2,9 +2,19 @@ from Cerebrum.web.templates.HistoryLogTemplate import HistoryLogTemplate
 from Cerebrum.web.TableView import TableView
 import types
 
+def view_history_short(entity):
+    # Could use some other template for 'short' view 
+    template = HistoryLogTemplate()
+    table = _history_tableview(entity, 5)
+    return template.viewHistoryLog(table)
+
 def view_history(entity):
     template = HistoryLogTemplate()
-    history = entity.get_history()
+    table = _history_tableview(entity)
+    return template.viewHistoryLog(table)
+
+def _history_tableview(entity, max_entries=None):    
+    history = entity.get_history(max_entries)
     table = TableView("timestamp", "icon", "who", "message")
     for change in history:
         if type(change.change_by) in types.StringTypes:
@@ -18,5 +28,6 @@ def view_history(entity):
                   message=change.message(), 
                   #TODO: should be an icon reference!
                   icon="") 
-    return template.viewHistoryLog(table)
+    return table        
+        
     
