110f4caf5f518e9937ed1881a5db45baa0f488eb
Mathias Meisfjordskar <mathiasm@ulrik.uio.no>
2004-03-11 14:55:24 +0000

    Added a "safe" method for dumping username, entity-id ans passwod-hashes.

--- Cerebrum/Account.py
+++ Cerebrum/Account.py
@@ -564,6 +564,19 @@ class Account(AccountType, AccountHome, EntityName, EntityQuarantine, Entity):
         except Errors.NotFoundError:
             return None
 
+    def list_account_authentication(self, auth_type=None):
+        if auth_type == None:
+            auth_type = self.const.auth_type_md5_crypt
+        return self.query("""
+        SELECT ai.account_id, en.entity_name, aa.auth_data
+        FROM [:table schema=cerebrum name=account_info] ai,
+             [:table schema=cerebrum name=entity_name] en,
+             [:table schema=cerebrum name=account_authentication] aa
+        WHERE ai.account_id=en.entity_id AND
+              ai.account_id=aa.account_id AND
+              aa.method=:method""",
+                          {'method' : int(auth_type)})
+
     def get_account_name(self):
         return self.account_name
 
