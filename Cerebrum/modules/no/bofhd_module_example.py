e7abd240123ecb0ac2b96c86b68206a04893661a
Rune Frøysa <runefro@usit.uio.no>
2004-03-12 18:39:15 +0000

    This script hasn't worked for some time due to changes in bofhd. Updated so that it does.

--- Cerebrum/modules/no/bofhd_module_example.py
+++ Cerebrum/modules/no/bofhd_module_example.py
@@ -22,42 +22,77 @@
 # Sample module that extends bofhd with a set of commands
 
 from Cerebrum.Utils import Factory
+from Cerebrum.modules.bofhd.errors import CerebrumError
+from Cerebrum import Utils
+from Cerebrum import Errors
 
-from Cerebrum.modules.bofhd.cmd_param import Command,Id
+from Cerebrum.modules.bofhd.cmd_param import Command,AccountName,FormatSuggestion
 
+def format_day(field):
+    fmt = "yyyy-MM-dd"                  # 10 characters wide
+    return ":".join((field, "date", fmt))
 
 class BofhdExtension(object):
-    def __init__(self, Cerebrum):
-        self.all_commands = {
-            'get_stedkode_info': Command(('ou', 'stedkode'), Id())
-            }
-        self.ou = Factory.get('OU')(Cerebrum)
+    all_commands = {}
 
+    def __init__(self, server):
+        self.server = server
+        self.logger = server.logger
+        self.db = server.db
+        self.ou = Factory.get('OU')(self.db)
+
+    def get_help_strings(self):
+        group_help = {
+            'user': "User commands",
+            }
+
+        # The texts in command_help are automatically line-wrapped, and should
+        # not contain \n
+        command_help = {
+            'user': {
+            'user_info': 'Show information about a user',
+            },
+            }
+        
+        arg_help = {
+            'account_name':
+            ['uname', 'Enter account name',
+             'Enter the name of the account for this operation'],
+            }
+        return (group_help, command_help,
+                arg_help)
+
+    
     def get_commands(self, uname):
         # TODO: Do some filtering on uname to remove commands
         commands = {}
         for k in self.all_commands.keys():
-            commands[k] = self.all_commands[k].get_struct()
+            commands[k] = self.all_commands[k].get_struct(self)
         return commands
 
-    def get_stedkode_info(self, uname, number):
-        fakultetnr, instituttnr, gruppenr = number[0:2], number[2:4], number[4:6]
-        self.ou.find_stedkode(fakultetnr, instituttnr, gruppenr)
-        self.ou.find(self.ou.ou_id)
-        return {
-            'name': self.ou.name,
-            'acronym': self.ou.acronym,
-            'short_name': self.ou.short_name,
-            'display_name': self.ou.display_name,
-            'sort_name': self.ou.sort_name
-            }
+    # user info
+    all_commands['user_info'] = Command(
+        ("user", "info"), AccountName(),
+        fs=FormatSuggestion([("Entity id:     %i\n"+
+                              "Expire:        %s",
+                              ("entity_id", format_day("expire"))),
+                             ("Quarantined:   %s",
+                              ("quarantined",))]))
+    def user_info(self, operator, accountname):
+        account = Utils.Factory.get('Account')(self.db)
+        try: 
+            account.find_by_name(accountname)
+        except Errors.NotFoundError:
+            raise CerebrumError, "Could not find user %s" % accountname
+
+        ret = {'entity_id': account.entity_id,
+               'expire': account.expire_date}
+        if account.get_entity_quarantine():
+            ret['quarantined'] = 'Yes'
+        return ret
 
     def get_format_suggestion(self, cmd):
-        suggestions = {
-            'get_stedkode_info':
-            "Navn: %s\nAcronym: %s\nShort: %s¤name;acronym;short_name"
-            }
-        return suggestions.get(cmd)
+        return self.all_commands[cmd].get_fs()
 
 if __name__ == '__main__':
     Cerebrum = Factory.get('Database')()
