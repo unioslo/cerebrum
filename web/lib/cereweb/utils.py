2b60af60f1224c28eac2095999c59cee95d59a78
Stian Soiland-Reyes <stain@unknown>
2004-03-29 08:55:35 +0000

    Added HTTP redirect function.
    Taken from the NAV project with permissions:
    
    Copyright (c) 2003 by NTNU, ITEA nettgruppen
    Authors: Morten Vold <morten.vold@itea.ntnu.no>
             Magnar Sveen <magnars@idi.ntnu.no>

--- web/lib/cereweb/utils.py
+++ web/lib/cereweb/utils.py
@@ -8,3 +8,26 @@ def url(path):
        installations, but /~stain/group/search for test installations.
     """
     return cereconf.WEBROOT + "/" + path
+
+
+def redirect(req, url, temporary=False, seeOther=False):
+    """
+    Immediately redirects the request to the given url. If the
+    seeOther parameter is set, 303 See Other response is sent, if the
+    temporary parameter is set, the server issues a 307 Temporary
+    Redirect. Otherwise a 301 Moved Permanently response is issued.
+    """
+    from mod_python import apache
+
+    if seeOther:
+        status = apache.HTTP_SEE_OTHER
+    elif temporary:
+        status = apache.HTTP_TEMPORARY_REDIRECT
+    else:
+        status = apache.HTTP_MOVED_PERMANENTLY
+
+    req.headers_out['Location'] = url
+    req.status = status
+    raise apache.SERVER_RETURN, status
+    
+
