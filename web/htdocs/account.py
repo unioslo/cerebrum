192f08a55bf1e5ac5465956b6415cc17664d1fc6
Thomas Langås <tlan@unknown>
2004-03-30 06:37:06 +0000

    * Added the routine to create accounts, this is a   "under the hood"-routine as it needs to be hooked up   with another webpage (as accounts either belongs to   a person or a group).  Currently only accounts owned by   a person are supported.

--- web/htdocs/account.py
+++ web/htdocs/account.py
@@ -8,6 +8,8 @@ from Cerebrum.web.templates.HistoryLogTemplate import HistoryLogTemplate
 from Cerebrum.web.Main import Main
 from gettext import gettext as _
 from Cerebrum.web.utils import url
+from Cerebrum.web.utils import redirect
+import xmlrpclib
 
 def index(req):
     page = Main(req)
@@ -51,6 +53,23 @@ def search(req, name, owner, expire_date, create_date):
     page.content = result.output().encode("utf8")
     return page    
 
+def create(req, ownerid="", ownertype="", id="", name="", affiliation="", 
+           show_form=None, hide_form=None, create=None):
+    if show_form:
+        req.session['profile']['person']['edit']['show_account_create'] = True
+
+    elif hide_form:
+        req.session['profile']['person']['edit']['show_account_create'] = False
+
+    elif create:
+        server = req.session['server']
+        try:
+            owner = ClientAPI.fetch_object_by_id(server, ownerid)
+            account = ClientAPI.Account.create(server, name, owner, affiliation)
+        except xmlrpclib.Fault, e:
+            req.session['profile']['last_error_message'] = e.faultString.split("CerebrumError: ")[-1]                    
+
+    return redirect(req, url("%s/view?id=%s" % (ownertype, ownerid)), seeOther=True)
 
 def _create_view(req, id):
     """Creates a page with a view of the account given by id, returns
