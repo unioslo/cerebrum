ec0ccb957e9a7985c6ab3eddf60056b2a0cf1f42
Thomas Langås <tlan@unknown>
2004-03-30 06:41:56 +0000

    * Added get_last_error_message() which returns the error_message set in the   profile for a given user.  Also clears the error_message when read,   to prevent repeating it.

--- web/lib/cereweb/profile.py
+++ web/lib/cereweb/profile.py
@@ -5,3 +5,11 @@ def get_profile(req):
     profile = RecursiveDict(default_profile.profile)
     return profile
 
+def get_last_error_message(req):
+    try:
+        msg = req.session['profile']['various']['last_error_message']
+        req.session['profile']['various']['last_error_message'] = ''
+    except:
+        return None
+
+    return msg
