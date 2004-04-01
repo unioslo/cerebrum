8cd3c575b00d4f51d390f7f289e39b65c296e7fc
Mathias Meisfjordskar <mathiasm@ulrik.uio.no>
2004-04-01 09:07:04 +0000

    Added first draft of access_FS for HiA.

---
+++ Cerebrum/modules/no/hia/access_FS.py
@@ -0,0 +1,57 @@
+# -*- coding: iso-8859-1 -*-
+# Copyright 2002, 2003 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+import re
+import os
+import sys
+import time
+
+from Cerebrum.modules.no.uio.access_FS import FS
+
+class HiaFS(FS):
+    """FS klassen definerer et sett med metoder som kan benyttes for å
+    hente ut informasjon om personer og OU-er fra FS. De fleste
+    metodene returnerer en tuple med kolonnenavn fulgt av en tuple med
+    dbrows."""
+
+##################################################################
+# Metoder for OU-er:
+##################################################################
+
+    def GetAlleOUer(self, institusjonsnr=0):
+        """Hent data om stedskoder registrert i FS"""
+        qry = """
+SELECT DISTINCT
+   faknr, instituttnr, gruppenr, stedakronym, stednavn_bokmal,
+   faknr_org_under, instituttnr_org_under, gruppenr_org_under,
+   adrlin1, adrlin2, postnr, telefonnr, faxnr,
+   adrlin1_besok, adrlin2_besok, postnr_besok, url,
+   bibsysbeststedkode
+FROM fs.sted
+WHERE institusjonsnr='%s'
+	 """ % institusjonsnr
+        return (self._get_cols(qry), self.db.query(qry))
+    
+##################################################################
+# Hjelpemetoder  
+##################################################################
+
+    def is_alive(self):	
+	return "NVL(p.status_dod, 'N') = 'N'\n"
+
