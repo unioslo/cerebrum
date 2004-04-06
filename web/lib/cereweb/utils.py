3328675f32b9917bc11e5cec9869f6bdff4d3cba
Stian Soiland-Reyes <stain@unknown>
2004-04-06 09:14:55 +0000

    object_url gives URLs to objects =)

--- web/lib/cereweb/utils.py
+++ web/lib/cereweb/utils.py
@@ -1,5 +1,6 @@
 import cereconf
 import os.path
+from Cerebrum.client import AbstractModel
 
 def url(path):
     """Returns a full path for a path relative to the base installation.
@@ -9,6 +10,24 @@ def url(path):
     """
     return cereconf.WEBROOT + "/" + path
 
+_object_type_url_map = {
+    AbstractModel.Account:      "account",
+    AbstractModel.Group:        "group",
+    AbstractModel.Person:       "person",
+    #AbstractModel.OU:           "ou", 
+    AbstractModel.Quarantine:   "quarantine",
+}
+
+def object_url(object, method="view"):
+    """Returns the full path to a page treating the object.
+       Method could be "view" (the default), "edit" and 
+       other things."""
+    # You might catch special cases here before the for-loop   
+    for (type, path) in _object_type_url_map.items():
+        if isinstance(object, type):
+            return url("%s/%s/?id=%s" % 
+                       (path, method, object.id))
+    raise "Unknown object %r" % object
 
 def redirect(req, url, temporary=False, seeOther=False):
     """
