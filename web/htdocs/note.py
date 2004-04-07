2153ce695189390be79ca0a866d418e73977a7f5
Stian Soiland-Reyes <stain@unknown>
2004-04-07 10:31:58 +0000

    School example of using queue_message and redirect_object.
    Returns to the view of the object, and includes a confirming message

--- web/htdocs/note.py
+++ web/htdocs/note.py
@@ -2,17 +2,21 @@ import cerebrum_path
 from Cerebrum.Utils import Factory
 ClientAPI = Factory.get_module("ClientAPI")
 from gettext import gettext as _
-from Cerebrum.web.utils import url
+from Cerebrum.web.utils import redirect_object
+from Cerebrum.web.utils import queue_message
 
 def add(req, entity, subject, description):
     """Adds a note to some entity"""
     server = req.session['server']
     entity = ClientAPI.fetch_object_by_id(server, entity)
     entity.add_note(subject, description)
-    return "OK, added dings"
+    queue_message(req, _("Added note '%s'") % subject)
+    return redirect_object(req, entity, seeOther=True)
 
 def delete(req, entity, id):
+    """Removes a note"""
     server = req.session['server']
     entity = ClientAPI.fetch_object_by_id(server, entity)
     entity.remove_note(id)
-    return "OK, Gone"
+    queue_message(req, _("Deleted note"))
+    return redirect_object(req, entity, seeOther=True)
