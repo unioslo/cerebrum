cb408a967003e8cd723d81565df3f5657acdb972
Stian Soiland-Reyes <stain@unknown>
2004-04-07 12:33:24 +0000

    International error messages
    Not more of that uri-stuff - uses redirect_object
    
    Templates takes entity, not entity_id

--- web/htdocs/quarantine.py
+++ web/htdocs/quarantine.py
@@ -2,11 +2,11 @@ import cerebrum_path
 import forgetHTML as html
 from Cerebrum.Utils import Factory
 ClientAPI = Factory.get_module("ClientAPI")
-from Cerebrum.web.templates.QuarantineViewTemplate import QuarantineViewTemplate
 from Cerebrum.web.templates.QuarantineTemplate import QuarantineTemplate
 from Cerebrum.web.Main import Main
 from gettext import gettext as _
-from Cerebrum.web.utils import url
+from Cerebrum.web.utils import queue_message
+from Cerebrum.web.utils import redirect_object
 
 def _quarantine_vars():
     fields =[("entity_type", "Entity type"),
@@ -21,7 +21,7 @@ def _quarantine_vars():
         formvalues[name] = ""
     return (fields, formvalues)
 
-def edit(req, entity_id, uri, type, submit=None, why=None, start=None, end=None, disable_until=None):
+def edit(req, entity_id, type, submit=None, why=None, start=None, end=None, disable_until=None):
     server = req.session['server']
     err_msg = ""
     (fields, formvalues) = _quarantine_vars()
@@ -59,7 +59,7 @@ def edit(req, entity_id, uri, type, submit=None, why=None, start=None, end=None,
                 did_add = False
                 err_msg += "Unable to disable quarantine!"
         if (did_add):
-            req.internal_redirect(uri)
+            redirect_object(req, ent, seeOther=True)
         else:
             for name, desc in fields:
                 try:
@@ -70,22 +70,23 @@ def edit(req, entity_id, uri, type, submit=None, why=None, start=None, end=None,
     edit = QuarantineTemplate()
     edit.fields = fields
     edit.formvalues = formvalues
-    page.content = lambda: edit.quarantine_form(entity_id, "quarantine/edit",uri)
+    page.content = lambda: edit.quarantine_form(ent, "quarantine/edit")
     if (err_msg):
         page.add_message(err_msg, True)
     return page
 
-def remove(req, entity_id, type, uri):
+def remove(req, entity_id, type):
     err_msg = ""
     server = req.session['server']
     try:
         ent = ClientAPI.fetch_object_by_id(server, entity_id)
         ent.remove_quarantine(type=type)
     except:
-        err_msg = "Unable to remove quarantine!"
-    req.internal_redirect(uri)
+        err_msg = _("Unable to remove quarantine!")
+    queue_message(req, _("Removed quarantine"))
+    return redirect_object(req, ent, seeOther=True)
 
-def add(req, entity_id, uri, submit=None,entity_type=None,\
+def add(req, entity_id, submit=None,entity_type=None,\
         type=None,why=None,start=None,end=None,disable_until=None):
 
     server = req.session['server']
@@ -98,16 +99,16 @@ def add(req, entity_id, uri, submit=None,entity_type=None,\
             """ Set up a new quarantine with registered values.
                 Return to the entity's form if OK.
             """
-            ent = ClientAPI.fetch_object_by_id(server,entity_id)
             ent.add_quarantine(type,why=why,start=start,end=end)
             if (disable_until):
                 try:
                     ent.disable_quarantine(type=type, until=disable_until)
                 except:
-                    err_msg = "Unable to disable the quarantine."
+                    err_msg = _("Unable to disable the quarantine.")
             if (not err_msg):
-                req.internal_redirect(uri)
-        except:
+                queue_message(req, _("Added quarantine %s" % type))
+                return redirect_object(req, ent, seeOther=True)
+        except "Jeg vil se feilmeldingen":
             """ Save the values given by the user, and set up
                 en error message to be shown.
             """
@@ -116,7 +117,7 @@ def add(req, entity_id, uri, submit=None,entity_type=None,\
                     formvalues[name] = eval(name)
                 except:
                     pass
-            err_msg = "Add new quarantine failed!"
+            err_msg = _("Add new quarantine failed!")
 
     formvalues['entity_type'] = ent.type
     formvalues['entity_name'] = ent.name
@@ -126,8 +127,11 @@ def add(req, entity_id, uri, submit=None,entity_type=None,\
     add.formvalues = formvalues
     all_types = ClientAPI.QuarantineType.get_all(server)
     has_types = [quarantine.type for quarantine in ent.get_quarantines()]
+    # only present types not already set
+    # FIXME: Should only present types appropriate for the
+    # entity_type. (how could we know that?) 
     types = [type for type in all_types if type not in has_types]
-    page.content = lambda: add.quarantine_form(entity_id, "quarantine/add", uri, types)
+    page.content = lambda: add.quarantine_form(ent, "quarantine/add", types)
     if (err_msg):
         page.add_message(err_msg, True)
 
