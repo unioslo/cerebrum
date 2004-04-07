7a505f27befc9f4cafac00786c86098d773cdbc4
Stian Soiland-Reyes <stain@unknown>
2004-04-07 10:34:12 +0000

    Namestyle change for prepareSession() -> prepare_session()
    Added prepare_messages - which takes queued messages from the
    session and displays them on the main page.

--- web/lib/cereweb/Main.py
+++ web/lib/cereweb/Main.py
@@ -7,25 +7,36 @@ class Main(MainTemplate):
     def __init__(self, req):
         MainTemplate.__init__(self)
         self.session = req.session
-        self.prepareSession()
+        self.prepare_session()
         self.menu = SideMenu()
         self.worklist = self.session['worklist']
         #self.activitylog = self.session['activitylog']
         self.activitylog = lambda: ActivityLog.view_operator_history(self.session)
-        self.messages = [] # reset each time =)
 
     def add_message(self, message, error=False):
         """Adds a message on top of page. If error is true, the 
         message will be highlighted as an error"""
         self.messages.append((message, error))
 
-    def prepareSession(self):    
+    def prepare_session(self):    
         #if not self.session.has_key("activitylog"):
         #    self.session['activitylog'] = ActivityLog() 
         if not self.session.has_key("worklist"):
             self.session['worklist'] = WorkList() 
+        self.prepare_messages()
+    
+    def prepare_messages(self):        
+        self.messages = []
+        queued_messages = self.session.get("messages")
+        if not queued_messages:
+            return
+        for (message, error) in queued_messages:
+            self.add_message(message, error)
+        # We've moved them from the queue to be shown on the page    
+        del self.session['messages']    
         
     def setFocus(self, *args):
         self.menu.setFocus(*args)    
         
+
 
