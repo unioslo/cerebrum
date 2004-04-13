e24c7d2813bcee9088e0e9395a1b4b47dab54c92
Stian Soiland-Reyes <stain@unknown>
2004-04-13 17:54:40 +0000

    Renamed CerebrumException to CerebrumError, so that later on Cerebrum.modules.bofhd.errors.CerebrumError and Cerebrum.Errors.CerebrumError could be the same exception.
    (actually, Cerebrum.modules.bofhd.errors should have some
    BofhdError. In addition, all "raise CerebrumError" should be changed,
    adding respective error classes in Cerebrum.modules.bofhd.errors)
    
    
    Errors.CerebrumError extends new DocstringException - which uses the
    docstring in the error class in printouts. (nice!)
    
    
    Exciting exception classes added:
    
       RealityError, UnreachableCodeError, ProgrammingError

--- Cerebrum/Errors.py
+++ Cerebrum/Errors.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 # Copyright 2002 University of Oslo, Norway
 #
@@ -17,26 +18,98 @@
 # along with Cerebrum; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-"""
+"""Generic and specific exception classes for Cerebrum."""
 
-"""
+class DocstringException(Exception):
+    """Makes it easy to define more descriptive error messages.
+       
+       >>> class RealityError(DocstringException):
+       ...     '''Unreachable point'''
 
-class CerebrumException(StandardError):
-    'Base class for all exceptions defined in Cerebrum.'
-    pass
+       >>> raise RealityError
+       Traceback (most recent call last):
+         File "<stdin>", line 1, in ?
+       RealityError: Unreachable point
 
-class DatabaseException(CerebrumException):
-    'Base class for database-related Cerebrum exceptions.'
-    pass
+       With argument: 
+       
+       >>> raise RealityError, "Outside handler"
+       Traceback (most recent call last):
+         File "<stdin>", line 1, in ?
+       RealityError: Unreachable point: Outside handler
+
+       Without docstring: 
+       
+       >>> class SomeError(RealityError):
+       ...     pass    
+       >>> raise SomeError
+       Traceback (most recent call last):
+         File "<stdin>", line 1, in ?
+       SomeError
+       
+       Without docstring, with argument:
+       
+       >>> raise SomeError, 129
+       Traceback (most recent call last):
+         File "<stdin>", line 1, in ?
+       SomeError: 129
+       """
+    def __str__(self):
+        args = Exception.__str__(self) # Get our arguments
+        
+        # We'll only include the docstring if it has been defined in
+        # our direct class. This avoids printing "General Error" when
+        # a class SpecificError(GeneralError) just forgot to specify a
+        # docstring. 
+        doc = self.__class__.__doc__ 
+        if args and doc:
+            return doc + ': ' + args
+        elif args:
+            # don't worry, our class name will be printed
+            return args    
+        else:
+            return doc or ""
+
+
+class CerebrumError(DocstringException):           
+    """Generic Cerebrum error"""
+
+class RealityError(CerebrumError):
+    """This should never happen"""
+    ## example:
+    ##     if 0:
+    ##         raise RealityError, "Not 0"
+
+class UnreachableCodeError(RealityError):
+    """Unreachable code"""
+
+
+class ProgrammingError(CerebrumError):
+    """Programming error"""
+    ## example:
+    ##     def function(arg1=None, arg2=None):
+    ##         if (arg1 and arg2) or not (arg1 or arg2):
+    ##             raise ProgrammingError, "Must specify arg1 OR arg2"
+
+class DatabaseException(CerebrumError):
+    """Database error"""
 
 class DatabaseConnectionError(DatabaseException):
-    pass
+    """Connection error"""
 
 class NotFoundError(DatabaseException):
-    pass
+    """Could not find"""
 
 class TooManyRowsError(DatabaseException):
+    """Too many rows"""
+
+class NoEntityAssociationError(CerebrumError):
+    # What does this mean?
     pass
 
-class NoEntityAssociationError(CerebrumException):
-    pass
+def _test():
+    import doctest,Errors 
+    return doctest.testmod(Errors)
+            
+if __name__ == "__main__":
+    _test()    
