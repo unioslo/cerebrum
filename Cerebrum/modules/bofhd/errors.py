44ee75e0ba6e6568ac6608e9ac7fbd60da7ff65b
Stian Soiland-Reyes <stain@unknown>
2004-05-04 08:00:31 +0000

    Unify CerebrumError with CerebrumError.

--- Cerebrum/modules/bofhd/errors.py
+++ Cerebrum/modules/bofhd/errors.py
@@ -17,9 +17,7 @@
 # along with Cerebrum; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-class CerebrumError(StandardError):
-    """Signal a user-error"""
-    pass
+from Cerebrum.Errors import CerebrumError
 
 class PermissionDenied(CerebrumError):
     pass
