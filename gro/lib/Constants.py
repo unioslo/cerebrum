02c1ef0e6987d08b3d5f57ec72f0670189b4537a
Erik Gorset <erikgors@itea.ntnu.no>
2004-06-16 13:12:15 +0000

    Constants for Gro

---
+++ gro/lib/Constants.py
@@ -0,0 +1,9 @@
+
+#The context name to use in the naming service
+CONTEXT_NAME = "Cerebrum"
+
+# The service to register gro as
+GRO_SERVICE_NAME = "Gro"
+
+# The name to register the Gro object as
+GRO_OBJECT_NAME = "GroServer"
