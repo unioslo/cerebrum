106f7047159f344ab8e47e2f5691312029216b55
Mathias Meisfjordskar <mathiasm@ulrik.uio.no>
2004-06-23 08:18:30 +0000

    * Added support for multiple auth-types.   (Needed by new gen_org_ldif)

--- Cerebrum/Account.py
+++ Cerebrum/Account.py
@@ -585,16 +585,23 @@ class Account(AccountType, AccountHome, EntityName, EntityQuarantine, Entity):
             return None
 
     def list_account_authentication(self, auth_type=None):
+        type = ""
         if auth_type == None:
-            auth_type = self.const.auth_type_md5_crypt
+            type = "= %d" % int(self.const.auth_type_md5_crypt)
+        elif isinstance(auth_type, list):
+            type = "IN (%d" % int(auth_type[0])
+            for at in auth_type[1:]:
+                type += ", %d" % int(at)
+            type += ")"
+        else:
+            type = "= %d" % int(auth_type)
         return self.query("""
-        SELECT ai.account_id, en.entity_name, aa.auth_data
+        SELECT ai.account_id, en.entity_name, aa.method, aa.auth_data
         FROM [:table schema=cerebrum name=entity_name] en,
              [:table schema=cerebrum name=account_info] ai
              LEFT JOIN [:table schema=cerebrum name=account_authentication] aa
-               ON ai.account_id=aa.account_id AND aa.method=:method
-        WHERE ai.account_id=en.entity_id""",
-                          {'method' : int(auth_type)})
+               ON ai.account_id=aa.account_id AND aa.method %s
+        WHERE ai.account_id=en.entity_id""" % type)
 
     def get_account_name(self):
         return self.account_name
