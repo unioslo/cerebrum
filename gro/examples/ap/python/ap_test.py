a8d215ed5db075bd0d9138ce8026de00888210e0
Erik Gorset <erikgors@itea.ntnu.no>
2004-06-24 15:51:11 +0000

    make the example bigger

--- gro/examples/ap/python/ap_test.py
+++ gro/examples/ap/python/ap_test.py
@@ -1,14 +1,51 @@
+#!/usr/bin/env python
+import cerebrum_path
+
 from Cerebrum.gro import ServerConnection
 
 from omniORB.any import from_any, to_any
 
 gro = ServerConnection.connect()
+version = gro.get_version()
+print "Connected to Gro version %s.%s"%(version.major, version.minor)
+print
+
 ap = gro.get_ap_handler('username', 'password')
 
-for group in ap.getTypeByName('EntityType', 'group').getChildren():
-    print 'Group:', from_any(group.get('name'))
+groups = ap.getTypeByName('EntityType', 'group').getChildren()
+print 'found', len(groups), 'groups'
+print
+
+for group in groups:
+    group.lockForReading()
+    print 'Group:', group.getString('name')
+    for groupMember in from_any(group.get('members')):
+        groupMember.lockForReading()
+        member = from_any(groupMember.get('member'))
+        operation = from_any(groupMember.get('operation'))
+        member.lockForReading()
+        print 'member leselås:', member.isReadLockedByMe()
+        a = member.getString('name')
+        b = operation.getString('name')
+        print '\t- %s (%s)' % (a, b)
+        print 'member leselås:', member.isReadLockedByMe()
+        member.unlock()
+        print 'member leselås:', member.isReadLockedByMe()
+        groupMember.unlock()
+        print 'member leselås:', member.isReadLockedByMe()
+    group.unlock()
 
 print '---'
 
-for account in ap.getTypeByName('EntityType', 'account').getChildren():
-    print 'Account:', from_any(account.get('name'))
+persons = ap.getTypeByName('EntityType', 'person').getChildren()
+print 'found', len(persons), 'persons'
+print
+
+for person in persons:
+    person.lockForReading()
+    print 'Person: [%s]' % person.getString('name')
+    for account in from_any(person.get('accounts')):
+        account.lockForReading()
+        print '\t- ', account.getString('name')
+        account.unlock()
+    person.unlock()
