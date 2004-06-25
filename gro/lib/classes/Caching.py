bad988f19f47710b68928ac916aa10828cad7336
eventh <eventh@unknown>
2004-06-25 18:05:33 +0000

    Added comments

--- gro/lib/classes/Caching.py
+++ gro/lib/classes/Caching.py
@@ -1,10 +1,20 @@
 import weakref
 import time
 
+
 class Caching(object):
+    """ Handles caching of nodes.
+    
+    If a client asks for a node wich already exists, he will give him the node
+    instead of creating a new node. When noone is holding a referance to the
+    node, it will be removed from the cache."""
     cache = weakref.WeakValueDictionary()
 
     def __new__(cls, *args, **vargs):
+        """
+        When a new node is attempted to be created, it will be read
+        from the database, or if it exists it will return the already
+        existing node instead."""
         key = cls, cls.getKey(*args, **vargs)
 
         if key in cls.cache:
@@ -16,11 +26,15 @@ class Caching(object):
         return self 
 
     def getPrimaryKey(self):
+        """ Returns the primary key for the node. """
         return self._key
 
     def invalidateObject(cls, obj):
+        """ Remove the node from the cache. """
         del cls.cache[obj]
     invalidateObject = classmethod(invalidateObject)
 
     def invalidate(self):
+        """ Remove the node from the cache. """
         self.invalidateObject(self)
+
