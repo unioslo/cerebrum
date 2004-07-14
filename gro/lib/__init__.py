ddd96a4dc00a05e15a0e1e835d41841caac9cdfe
Erik Gorset <erikgors@itea.ntnu.no>
2004-07-14 07:18:26 +0000

    fjernet Node og Clever, og la til db og Builder

--- gro/lib/__init__.py
+++ gro/lib/__init__.py
@@ -1,10 +1,11 @@
 from classes.Account import *
-from classes.Clever import *
 from classes.Disk import *
 from classes.Entity import *
 from classes.Group import *
 from classes.Host import *
 from classes.Locking import *
-from classes.Node import *
 from classes.Person import *
 from classes.Types import *
+
+from classes.db import db
+from classes.Builder import Builder
