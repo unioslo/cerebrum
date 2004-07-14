93b107d280bde5a6e2eccd280c2f9e1d901fbfb1
eventh <eventh@unknown>
2004-07-14 12:15:15 +0000

    Fixed comments. code_str in auth_op_code has length of 256 instead of 16 now.

--- design/gro_auth.sql
+++ design/gro_auth.sql
@@ -1,7 +1,22 @@
+/* Tables used for authentication in GRO.
+*
+* Authentication of commands performed in gro uses these tables wich are:
+* - auth_op_code        : Operation codes like "set_password"
+* - auth_op_attrs       : Operation attrs used for validation
+* - auth_operation      : Linking codes and attrs with operationsets
+* - auth_operation_set  : A set of auth operations.
+* - auth_op_target      : Targets of the operation, entities.
+* - auth_role           : Linking an operation set and a target with a entity
+*
+* The user trying to perform a command is found as the entity_id in auth_role.
+* The command tried to perform is found as the code_str in auth_op_code.
+* The target the command is to perform on, is the entity_id in auth_op_target.
+*/
 category:metainfo;
 name=gro_auth;
 category:metainfo;
 version=1.0;
+
 
 category:drop;
 DROP TABLE auth_role;
@@ -16,10 +31,15 @@ DROP TABLE auth_operation_set;
 category:drop;
 DROP TABLE auth_op_code;
 
-/* Defines the legal operations that may be performed, such as:
- - "set_password"
- - create user on disk
- - delete user from disk */
+
+/* Defines the legal operations that may be performed.
+*
+* Everything in GRO is methods, and the operations should be the same as
+* the method, to give access to it. Attributes are accessed through get and set
+* methods: "get_description", "set_name". If you want an account to be able to
+* set his own password, you have to give him the operation_code "set_password"
+* and make it target his own account.
+*/
 category:code;
 CREATE TABLE auth_op_code (
   code             NUMERIC(6,0)
@@ -28,11 +48,15 @@ CREATE TABLE auth_op_code (
                      NOT NULL
                      CONSTRAINT auth_op_codestr_u UNIQUE,
   description      CHAR VARYING(512)
-                   NOT NULL
+                     NOT NULL
 );
 
-/* PK for a collection of operations.*/
 
+/* Collection of operations.
+*
+* An operation set contains several operations. You can only give 
+* entities access to a set of operations.
+*/
 category:main;
 CREATE TABLE auth_operation_set (
   op_set_id        NUMERIC(12,0)
@@ -40,7 +64,11 @@ CREATE TABLE auth_operation_set (
   name             CHAR VARYING(30)
 );
 
-/* Contains a set of operations within an auth_operation_set */
+
+/* Contains a set of operations within an auth_operation_set.
+*
+* Links operation codes and operation attrs with operationsets.
+*/
 category:main;
 CREATE TABLE auth_operation (
   op_id            NUMERIC(12,0)
@@ -54,11 +82,15 @@ CREATE TABLE auth_operation (
                      CONSTRAINT auth_operation_op_set_fk
                        REFERENCES auth_operation_set(op_set_id)
 );
+
 category:main;
 CREATE INDEX auth_operation_set_id ON auth_operation(op_set_id);
-/* Defines attributes associated with an auth_operation, such as legal
-   shells */
 
+
+/* Defines attributes associated with an auth_operation.
+* 
+* Attributes can be used for validation,  such as legal shells etc.
+*/
 category:main;
 CREATE TABLE auth_op_attrs (
   op_id            NUMERIC(12,0)
@@ -69,21 +101,22 @@ CREATE TABLE auth_op_attrs (
 );
 
 
-/* Defines rules for finding an entity target. 
-
-Examples:
-
-  Users on a disk:
-    op_target_type = 'disk'     entity_id=<disk.entity_id>
-  Users on a host:
-    op_target_type = 'host'     entity_id=<host.entity_id>
-  Users on a host:/path/host/sv-l*
-    op_target_type = 'host'     entity_id=<host.entity_id> 
-    attr = 'sv-l.*' (note: regular expression, and only leaf directory)
-  Allowed to set/clear spread X
-    op_target_type = 'spread'   entity_id = <spread_code.code>
+/* Defines rules for finding an entity target.
+*
+* The targets is the entity wich the operation is performed on.
+*
+* Examples:
+* 
+*   Users on a disk:
+*     op_target_type = 'disk'     entity_id=<disk.entity_id>
+*   Users on a host:
+*     op_target_type = 'host'     entity_id=<host.entity_id>
+*   Users on a host:/path/host/sv-l*
+*     op_target_type = 'host'     entity_id=<host.entity_id> 
+*     attr = 'sv-l.*' (note: regular expression, and only leaf directory)
+*   Allowed to set/clear spread X
+*     op_target_type = 'spread'   entity_id = <spread_code.code>
 */
-
 category:main;
 CREATE TABLE auth_op_target (
   op_target_id     NUMERIC(12,0)
@@ -97,8 +130,13 @@ CREATE TABLE auth_op_target (
 category:main;
 CREATE INDEX auth_op_target_entity_id ON auth_op_target(entity_id);
 
-/* A role associates an auth_operation_set with an auth_op_target */
 
+/* A role associates an auth_operation_set with an auth_op_target.
+*
+* Links the entity with a set of legal operations and a target.
+* The operationset contains several operations wich got operationcodes wich
+* equals GRO-commands, and attr who can be used for validation.
+*/
 category:main;
 CREATE TABLE auth_role (
   entity_id        NUMERIC(12,0)
