04c4ac71509c17020ff8e15d62f97491967e9b59
Håvard Stranden <havarden@idi.ntnu.no>
2004-07-20 14:50:41 +0000

    Added registration of OU and OUPerspectiveType.

--- gro/lib/__init__.py
+++ gro/lib/__init__.py
@@ -7,6 +7,7 @@ from classes.Entity import *
 from classes.Group import *
 from classes.Host import *
 from classes.Locking import *
+from classes.OU import *
 from classes.Person import *
 from classes.Types import *
 
@@ -14,7 +15,7 @@ from classes.db import db
 from classes.Builder import *
 
 from APHandler import APHandler
-# base classes
+# Base classes
 APHandler.register_gro_class(Account)
 APHandler.register_gro_class(AccountAuthentication)
 APHandler.register_gro_class(Address)
@@ -26,11 +27,12 @@ APHandler.register_gro_class(GroupMember)
 APHandler.register_gro_class(Host)
 APHandler.register_gro_class(NameType)
 APHandler.register_gro_class(Note)
+APHandler.register_gro_class(OU)
 APHandler.register_gro_class(Person)
 APHandler.register_gro_class(PersonName)
 APHandler.register_gro_class(SourceSystem)
 APHandler.register_gro_class(Spread)
-# type classes (or constants)
+# Type classes (or constants)
 APHandler.register_gro_class(AddressType)
 APHandler.register_gro_class(AuthenticationType)
 APHandler.register_gro_class(ContactInfoType)
@@ -38,18 +40,21 @@ APHandler.register_gro_class(EntityType)
 APHandler.register_gro_class(GenderType)
 APHandler.register_gro_class(GroupMemberOperationType)
 APHandler.register_gro_class(GroupVisibilityType)
+APHandler.register_gro_class(OUPerspectiveType)
 APHandler.register_gro_class(QuarantineType)
-# search classes
+# Search classes
 AccountSearch = Account.create_search_class()
 DiskSearch = Disk.create_search_class()
 GroupSearch = Group.create_search_class()
 HostSearch = Host.create_search_class()
+OUSearch = OU.create_search_class()
 PersonSearch = Person.create_search_class()
 
 APHandler.register_gro_class(AccountSearch)
 APHandler.register_gro_class(DiskSearch)
 APHandler.register_gro_class(GroupSearch)
 APHandler.register_gro_class(HostSearch)
+APHandler.register_gro_class(OUSearch)
 APHandler.register_gro_class(PersonSearch)
 
 APHandler.build_classes()
