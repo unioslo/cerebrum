ed8d8e82e10967c945f181c04796765bb4433495
Erik Gorset <erikgors@itea.ntnu.no>
2004-07-20 18:25:38 +0000

    removed old database..

--- gro/lib/__init__.py
+++ gro/lib/__init__.py
@@ -1,6 +1,3 @@
-from omniORB import CORBA, PortableServer, sslTP
-import CosNaming
-
 from classes.Account import *
 from classes.Disk import *
 from classes.Entity import *
@@ -11,7 +8,6 @@ from classes.OU import *
 from classes.Person import *
 from classes.Types import *
 
-from classes.db import db
 from classes.Builder import *
 
 from APHandler import APHandler
