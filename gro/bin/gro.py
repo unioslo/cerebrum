90cbc8f6ec47a35f38cc743fd2e050faa1469790
Erik Gorset <erikgors@itea.ntnu.no>
2004-07-27 20:42:35 +0000

    a nicer way to start up gro

--- gro/bin/gro.py
+++ gro/bin/gro.py
@@ -1,7 +1,33 @@
 #!/usr/bin/env  python
 import cerebrum_path
+import cereconf
 
+import thread
+
+from Cerebrum.gro.classes import Scheduler
+from Cerebrum.gro import Communication
 from Cerebrum.gro import Gro
 
+
+def main():
+    com = Communication.get_communication()
+    sched = Scheduler.get_scheduler()
+
+    # starting the scheduler
+    for i in range(cereconf.GRO_SCHEDULERS):
+        print 'starting scheduler thread'
+        thread.start_new_thread(sched.run, ())
+
+    # creating server
+    server = Gro.GroImpl()
+
+    # binding server to Naming Service
+    com.bind_object(server, cereconf.GRO_OBJECT_NAME)
+
+    # starting communication
+    print 'starting communication'
+    com.start()
+
+
 if __name__ == '__main__':        
-    Gro.main()
+    main()
