41618e9968393ed0465d570ddaae6cbd13871417
Erik Gorset <erikgors@itea.ntnu.no>
2004-07-28 17:58:40 +0000

    commands for Gro

---
+++ gro/lib/classes/Commands.py
@@ -0,0 +1,30 @@
+import Registry
+registry = Registry.get_registry()
+
+Builder = registry.Builder
+Method = registry.Method
+
+Group = registry.Group
+GroupVisibilityType = registry.GroupVisibilityType
+
+__all__ = ['Commands']
+
+class Commands(Builder):
+    primary_key = []
+    slots = []
+    method_slots = [
+        Method('create_group', 'Group', [('name', 'string'), ('visibility', 'GroupVisibilityType')],
+               write=True)]
+
+    def __init__(self):
+        Builder.__init__(self, nocache=True)
+
+    def create_group(self, name, visibility):
+        db = self.get_database()
+        group = Group.cerebrum_class(db)
+        print 'change_by', [db.change_by]
+        group.populate(db.change_by, visibility.get_id(), name)
+        group.write_db()
+
+        id = group.entity_id
+        return Group(id, write_lock=self.get_writelock_holder())
