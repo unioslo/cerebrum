6c7f51f254216b78d63f69975ec65648a424f0b3
Erik Gorset <erikgors@itea.ntnu.no>
2004-07-28 18:07:58 +0000

    changed to GroupMemberSearch

--- gro/lib/LOHandler.py
+++ gro/lib/LOHandler.py
@@ -17,62 +17,77 @@ registry = classes.Registry.get_registry()
 scheduler = classes.Scheduler.get_scheduler()
 
 GroupSearch = registry.GroupSearch
+GroupMemberSearch = registry.GroupMemberSearch
 AccountSearch = registry.AccountSearch
+ChangeEventSearch = registry.ChangeEventSearch
 
 class LOHandler(Cerebrum_core__POA.LOHandler):
-    def get_all(self, group_name, tags):
-        print group_name, tags
-        search = GroupSearch()
-        search.set_name(group_name) # FIXME: dette må være et eksakt søk. her er det mulig med *?%_
-        groups = search.search()
-        assert len(groups) <= 1
+    def __init__(self, client):
+        self.client = client
+
+    def get_all(self, group_name, group_tags, entity_tags):
+        searcher = GroupMemberSearch()
+        searcher.set_include_subgroups(True)
+        searcher.set_include_parentgroups(True)
+
+        # finne gruppe
+        if group_name:
+            group_search = GroupSearch()
+            # FIXME: dette må være et eksakt søk. her er det mulig med *?%_
+            group_search.set_name(group_name)
+            groups = group_search.search()
+            assert len(groups) <= 1
 
         if not groups:
-            raise Exception('group not found') # hmm.. retunere not tomt i stedet.
+            raise Exception('group not found') # hmm.. retunere noe tomt i stedet.
 
-        group = groups[0]
-        print 'group found:', group.get_name()
+        # FIXME: finne tags
+        group_tags = []
+        entity_tags = []
 
-        entity_list = {}
+
+        searcher.set_group(groups[0])
+        if group_tags:
+            searcher.set_group_tags(group_tags)
+        if entity_tags:
+            searcher.set_member_tags(entity_tags)
+
+        entities = {}
+        group_members = []
 
         def add_entity(entity):
-            # sjekk om entity har en av taggene
-            if not 1:
-                return None
             id = entity.get_entity_id()
-            if id in entity_list:
-                return id
 
-            attributes = []
-            for i in entity.slots:
-                if i.data_type in ('long', 'string'):
-                    value = getattr(entity, 'get_' + i.name)()
-                    attributes.append(Cerebrum_core.KeyValue(i.name, str(value)))
+            if id not in entities:
+                attributes = []
+                for i in entity.slots:
+                    if i.data_type in ('long', 'string'): # TODO: fikse støtte for konstanter
+                        value = getattr(entity, 'get_' + i.name)()
+                        attributes.append(Cerebrum_core.KeyValue(i.name, str(value)))
 
-            type = entity.get_entity_type().get_name()
+                entity_type = entity.get_entity_type().get_name()
 
-            print 'adding entity with id:', id
-            entity_list[id] = Cerebrum_core.Entity(id, type, attributes)
+                entities[id] = Cerebrum_core.Entity(id, entity_type, attributes)
+            
+        for group_member in searcher.search():
+            group = group_member.get_group()
+            operation = group_member.get_operation().get_name()
+            member = group_member.get_member()
 
-            if type == 'group':
-                add_group(entity)
+            add_entity(group)
+            add_entity(member)
 
-            return id
-
-        group_member_list = []
-
-        def add_group(group):
-            members = []
-            for i in group.get_members():
-                id = add_entity(i.get_member())
-                if id is not None:
-                    member = Cerebrum_core.GroupMember(group.get_entity_id(), i.get_operation().get_name(), id)
-                    members.append(member)
-
-        add_entity(group)
-
-        entity_list = entity_list.items()
-        entity_list.sort()
+            g = Cerebrum_core.GroupMember(group.get_entity_id(), operation, member.get_entity_id())
+            group_members.append(g)
 
 
-        return 0, [j for i, j in entity_list], group_member_list
+        entities = entities.items()
+        entities.sort()
+
+        change_event = self.get_lastest_change_event()
+
+        return change_event.get_id(), [j for i, j in entities], group_members
+
+    def get_lastest_change_event(self):
+        # FIXME: ChangeEventSearch trenger mer funksjonalitet
+        return ChangeEventSearch('latest').search()[-1]
