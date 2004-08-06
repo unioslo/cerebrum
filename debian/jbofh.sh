da56c34ed27c1ab1573585c31fce87f7bb431e05
Andreas Schuldei <schuldei@unknown>
2004-08-06 01:04:34 +0000

    initial checkin

---
+++ debian/jbofh.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# simple script used to run jbofh in Debian
+
+# The first existing directory is used for JAVA_HOME (if JAVA_HOME is not
+# defined in $DEFAULT)
+JDK_DIRS="$JAVA_HOME /usr/lib/kaffe"
+
+# Look for the right JVM to use
+for jdir in $JDK_DIRS; do
+	if [ -r "$jdir/bin/java" -a -z "${JAVA_HOME}" ]; then
+		JAVA_HOME="$jdir"
+	fi
+done
+export JAVA_HOME
+
+if [ "$JAVA_HOME" ] ; then
+  if [ -z "$JAVACMD" ]; then
+    JAVACMD="$JAVA_HOME/bin/java"
+  fi
+
+  $JAVACMD -jar /usr/share/cerebrum/jbofh/JBofh.jar "$@"
+
+else
+  echo "No JVM found to run jbofh"
+  echo "Please install a JVM to run jbofh or "
+  echo "set JAVA_HOME if it's not a JVM from a Debian Package."
+  exit 1
+fi
