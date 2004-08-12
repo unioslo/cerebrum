7552c070a60b88f245c5fcab0260384ceddf2c27
Andreas Schuldei <schuldei@unknown>
2004-08-12 22:41:16 +0000

    initial checkin

---
+++ debian/scripts/build_orig_tar_gz.sh
@@ -0,0 +1,35 @@
+#!/bin/sh -e
+
+DATE=`date +%Y%m%d`
+
+trap { mv cerebrum-0.0.$DATE cerebrum } 0
+
+if [ -f cerebrum_0.0.$DATE.orig.tar.gz ] ; then
+  rm -f cerebrum_0.0.$DATE.orig.tar.gz
+fi
+
+if [ -f cerebrum/debian/changelog ] ; then
+  rm -f cerebrum/debian/changelog
+fi
+
+(cd cerebrum; cvs up -dPA)
+
+DEBFULLNAME="Andreas Schuldei"
+DEBEMAIL="andreas@debian.org"
+
+export DEBEMAIL DEBFULLNAME
+
+( cd cerebrum-0.0.$DATE ; dch --newversion 0.0.$DATE "new upstream cvs checkout")
+
+mv cerebrum cerebrum-0.0.$DATE
+tar cfzv cerebrum_0.0.$DATE.orig.tar.gz cerebrum-0.0.$DATE \
+ --exclude 'CVS' \
+ --exclude '.cvsignore' \
+ --exclude 'debian/tmp' \
+ --exclude 'debian/files' \
+ --exclude 'debian/substvars' \
+ --exclude 'debian/cerebrum-common/' \
+ --exclude 'debian/cerebrum-server/' \
+ --exclude 'debian/cerebrum-jbofh/' \
+ --exclude 'debian/cerebrum-doc/'
+
