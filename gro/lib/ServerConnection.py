2092568e8c883a2c7b2577fdcb60c39711db438d
Erik Gorset <erikgors@itea.ntnu.no>
2004-09-22 15:15:30 +0000

    added get_orb and string_to_object

--- gro/lib/ServerConnection.py
+++ gro/lib/ServerConnection.py
@@ -4,19 +4,27 @@ import CosNaming
 import cereconf
 import Cerebrum_core
 
+_orb = None
+def get_orb(args=[]):
+    global _orb
+    if _orb is None:
+        # Set up the SSL context
+        sslTP.certificate_authority_file(cereconf.SSL_CA_FILE)
+        sslTP.key_file(cereconf.SSL_KEY_FILE)
+        sslTP.key_file_password(cereconf.SSL_KEY_FILE_PASSWORD)
+
+        # Initialize the ORB
+        _orb = CORBA.ORB_init(args, CORBA.ORB_ID)
+    return _orb
+
+
 def connect(args=[]):
     """ 
     Method for connecting to a CORBA name service
     and fetch the Gro object. The method prefers
     SSL connections.
     """
-    # Set up the SSL context
-    sslTP.certificate_authority_file(cereconf.SSL_CA_FILE)
-    sslTP.key_file(cereconf.SSL_KEY_FILE)
-    sslTP.key_file_password(cereconf.SSL_KEY_FILE_PASSWORD)
-
-    # Initialize the ORB
-    orb = CORBA.ORB_init(args, CORBA.ORB_ID)
+    orb = get_orb()
     # Get the name service and narrow the root context
     obj = orb.resolve_initial_references("NameService")
     rootContext = obj._narrow(CosNaming.NamingContext)
@@ -34,5 +42,8 @@ def connect(args=[]):
     if gro is None:
         raise Exception("Could not narrow the gro object")
     return gro
+
+def string_to_object(string):
+    return get_orb().string_to_object(string)
 
 # arch-tag: 614754e4-465c-47ce-92a0-c375e476d563
