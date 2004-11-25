41aeaff3910078af3405e4835fde22c1d09ca6eb
Andreas Schuldei <schuldei@unknown>
2004-11-25 00:26:04 +0000

    password import works now, gross hack

--- contrib/debian-edu/import/unix-files/Import/password.py
+++ contrib/debian-edu/import/unix-files/Import/password.py
@@ -100,7 +100,7 @@ class PasswdImport(ImportBase):
             commit_count += 1
             print "Processing user %s" % user_name
 
-            user_id = self.processUser(user_name, user_data["uid"], user_data["gid"] )
+            user_id = self.processUser(user_name, user_data["uid"], user_data["gid"], "dummer user" )
 
             if commit_count % commit_limit == 0:
                 self.attemptCommit()
@@ -109,11 +109,12 @@ class PasswdImport(ImportBase):
     # end createUser
 
 
-    def processUser(self, user_name, uid, gid):
+    def processUser(self, user_name, uid, gid, gecos):
         """
         Set uid/gid for user.
         """
 
+        print "types: uid: %s, gid: %s" %(type( uid) , type(gid))
         try:
             self.posixgroup.clear()
             self.posixgroup.find_by_gid(gid)
@@ -125,30 +126,48 @@ class PasswdImport(ImportBase):
             self.posixgroup.find_by_gid(1000)
         # yrt
 
+
         try:
-            self.account.clear()
-            self.account.find_by_name(user_name)
+            self.posixuser.clear()
+            self.posixuser.find_by_name(user_name)
             print "User %s exists in Cerebrum" % user_name
         except Errors.NotFoundError:
+            # we may have an account-object lying around.
+            owner = None
+            try:
+                self.account.clear()
+                self.account.find_by_name(cereconf.INITIAL_ACCOUNTNAME)
+                owner = self.account.entity_id
+            except Errors.NotFoundError:
+                # No account found owner = None still.
+                pass
+            
             self.posixuser.clear()
+            print "entity_id %s" % self.posixgroup.entity_id
             self.posixuser.populate(uid,
                                     self.posixgroup.entity_id,
                                     None,
-                                    self.constants.posix_shell_tcsh,
-                                    parent=self.account)
+                                    self.constants.posix_shell_bash,
+                                    name=user_name,
+                                    creator_id = owner,
+                                    owner_id = self.posixgroup.entity_id,
+                                    owner_type = self.constants.entity_group,
+                                    np_type = int(self.constants.account_system) )
             self.posixuser.write_db()
             self.db.commit()
             print "User %s promoted with uid: %s" % (user_name, uid)
         # yrt
-
+          
         try:
             self.posixuser.clear()
             self.posixuser.find_by_uid(int(uid))
             if self.posixuser.account_name == user_name:
-                print "User %s exists as PosixUser in Cerebrum"% user_name
+                print "User %s exists as PosixUser in Cerebrum" % user_name
             else:
                 print "User %s exists with uid: %s. We have: %s." % \
-                            (self.posixuser.account_name, self.posixuser.posix_uid, uid)
+                            (self.posixuser.account_name,
+                             self.posixuser.posix_uid,
+                             uid)
             return
         except Errors.NotFoundError:
             pass
