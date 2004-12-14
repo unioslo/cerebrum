764024b70ebdd058dd7539d806e40b4de4f17821
Rune Frøysa <runefro@usit.uio.no>
2004-12-14 10:00:54 +0000

    - We now support QUARANTINE_RULES that have effect for spesific   spreads.  This allows users to be splatted in some systems, while   being allowed to log into others. - Improved documentation for some methods - Added check_entity_quarantines utility method for returning an   initiated QuarantineHandler for a given entity-id. - All changes should be compatible with existing code

--- Cerebrum/QuarantineHandler.py
+++ Cerebrum/QuarantineHandler.py
@@ -24,47 +24,156 @@ is reported to have.  This is done by providing methods that can be
 queried for the requested operation.
 
 The format of cereconf.QUARANTINE_RULES is
-  { <quarantine_code_str>: {'lock': <0|1>,  'shell': <shell>, .... },
-    ... }
+  { <quarantine_code_str>: [ {  
+         'lock': <0|1>,  'shell': <shell>, ....,
+         'spread': spread_code|[spread_codes]
+       } ] }
+
+I.e, a dict of quarantine_code_str points to a list of dicts (for
+backwards-compatibility, the list part may be skipped).
+
+The 'spread' attribute in the inner dict is optinal.  If set, the
+quarantine rule will only apply when the user has the spread in
+question.  A spread value '*' or absence of spread attr matches any
+spread.
 
 The module will probably be expanded at a later time to allow for
 ranking between quarantines etc.
 """
 
 import cereconf
-from Cerebrum.Constants import _QuarantineCode
+from Cerebrum import Entity
+from Cerebrum.Constants import _QuarantineCode, _SpreadCode
 
 class QuarantineHandler(object):
-    __slots__ = 'quarantines'
+#    __slots__ = 'quarantines'
 
-    rules = {}
-    def __init__(self, database, quarantines):
-        if len(self.rules) == 0:
+    qc2rules = {}
+    def __init__(self, database, quarantines, spreads=None):
+        """Constructs a QuarantineHandler.  quarantines should point
+        to the quarantines that the user currently has.  Spreads is
+        optional, and points to an optional list of spreads limiting
+        the places where the QuarantineHandler will have effect.
+        """
+        if len(self.qc2rules) == 0:
+            # Initial setup only done once.
+            #
+            # Converting strings to Constants and build:
+            #
+            # self.qc2rules = {'qc': {'spread_code': {settings} } }
             _QuarantineCode.sql = database
-            for k in cereconf.QUARANTINE_RULES.keys():
-                qc = _QuarantineCode(k)
-                self.rules[int(qc)] = cereconf.QUARANTINE_RULES[k]
+            _SpreadCode.sql = database
+            for code, rules in cereconf.QUARANTINE_RULES.items():
+                qc_rules = {}
+                self.qc2rules[int(_QuarantineCode(code))] = qc_rules
+                if isinstance(rules, dict):
+                    rules = (rules,)
+                for r in rules:
+                    settings = r.copy()
+                    if settings.has_key('spread'):
+                        tmp_spreads = settings['spread']
+                        del(settings['spread'])
+                    else:
+                        tmp_spreads = ('*',)
+                    if isinstance(tmp_spreads, str):
+                        tmp_spreads = (tmp_spreads,)
+                    for c in tmp_spreads:
+                        if c != '*':
+                            c = int(_SpreadCode(c))
+                        qc_rules[c] = settings
+                        
         if quarantines is None:
             quarantines = []
         self.quarantines = quarantines
+        if spreads is None:
+            spreads = []
+        self.spreads = [int(s) for s in spreads]
+        # Append the '*' spread last to do the check against settings
+        # for this spread last.
+        self.spreads.append('*')
+
+    def _get_matches(self):
+        ret = []
+        for q in self.quarantines:
+            spread2settings = self.qc2rules[int(q)]
+            # Note that for each spread, we only extract the first
+            # matching setting.  Otherwise it would not be possible to
+            # have a quarantine that did not lock the acount for a
+            # spesific spread.
+            for spread in self.spreads:
+                if spread2settings.has_key(spread):
+                    ret.append(spread2settings[spread])
+                    break
+        return ret
 
     def get_shell(self):
-        for q in self.quarantines:
-            shell = self.rules[int(q)].get('shell', None)
+        for m in self._get_matches():
+            shell = m.get('shell', None)
             if shell is not None:
                 return shell
         return None
     
     def should_skip(self):
-        for q in self.quarantines:
-            if self.rules[int(q)].get('skip', 0):
+        for m in self._get_matches():
+            if m.get('skip', 0):
                 return 1
         return 0
 
     def is_locked(self):
-        for q in self.quarantines:
-            if self.rules[int(q)].get('lock', 0):
+        """The account should be known, but the account locked"""
+        # Note that if any matching quaratine specifies lock, lock
+        # will be used.
+        for m in self._get_matches():
+            if m.get('lock', 0):
                 return 1
         return 0
+
+    def check_entity_quarantines(db, entity_id, spreads=None):
+        """Utility method that returns an initiated QuarantineHandler
+        for a given entity_id"""
+        eq = Entity.EntityQuarantine(db)
+        eq.find(entity_id)
+        return QuarantineHandler(
+            db, [int(row['quarantine_type'])
+                 for row in eq.get_entity_quarantine(only_active=True)],
+            spreads)
+    check_entity_quarantines = staticmethod(check_entity_quarantines)
+
+def _test():
+    # TODO: This should use the unit-testing framework, and use common
+    # constants (which we currently don't have for spreads)    
+    cereconf.QUARANTINE_RULES = {
+        'nologin': {'lock': 1, 'shell': 'nologin-shell'},
+        'system': [{'lock': 1, 'shell': 'nologin-shell'},
+                   {'spread': 'AD_account', 'shell': 'ad-shell'}]
+        }
+    from Cerebrum.Utils import Factory
+    db = Factory.get('Database')()
+    co = Factory.get('Constants')(db)
+
+    # Check with old cereconf syntax
+    qh = QuarantineHandler(db, (co.quarantine_nologin,))
+    print "nolgin: L=", qh.is_locked(), "S=", qh.get_shell()
+
+    # New cereconf syntax, non-spread spesific
+    qh = QuarantineHandler(db, (co.quarantine_system,))
+    print "system: L=", qh.is_locked(), "S=", qh.get_shell()
+
+    # spread-spesific quarantine action, should not be locked
+    qh = QuarantineHandler(db, (co.quarantine_system,),
+                           spreads=(co.spread_uio_ad_account,))
+    print "system & AD: L=", qh.is_locked(), "S=", qh.get_shell()
+
+    # spread-specific quarantine action and another quarantine that
+    # requires lock
+    qh = QuarantineHandler(db, (co.quarantine_system, co.quarantine_nologin),
+                           spreads=(co.spread_uio_ad_account,))
+    print "system & AD & L: L=", qh.is_locked(), "S=", qh.get_shell()
+
+    qh = QuarantineHandler.check_entity_quarantines(db, 67201)
+    print "An entity: L=", qh.is_locked(), "S=", qh.get_shell()
+
+if __name__ == '__main__':
+    _test()
 
 # arch-tag: cfaaa1c8-a42d-4205-bb13-51dd9954ca8e
