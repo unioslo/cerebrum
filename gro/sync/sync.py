bf77dbd5515125d5ba88deb93a4b2acda145f41f
Stian Soiland-Reyes <stain@unknown>
2004-12-15 12:31:16 +0000

    returns account objects

--- gro/sync/sync.py
+++ gro/sync/sync.py
@@ -17,12 +17,14 @@ class LoginError(SyncError):
 
 class Sync:
     def __init__(self):
-        self.connect()
         self._transactions = []
+        self.connect()
    
     def __del__(self):
         # We shouldn't have any transactions now, but if we
         # do, we abort them.
+        if self._transactions:
+            print "WARNING: %s transactions still open" % len(self._transactions)
         self.rollback()
     
     def rollback(self):
@@ -56,17 +58,42 @@ class Sync:
         except GroErrors.ServerError:
             # let's naivly try to reconnect 
             self.connect()
-        # This time it should work!    
-        try:
-            t = self.ap.new_transaction()         
-        except GroErrors.ServerError:
-            raise ServerError
+            # This time it should work!    
+            try:
+                t = self.ap.new_transaction()         
+            except GroErrors.ServerError:
+                raise ServerError
+        self._transactions.append(t)    
         return t    
 
     def get_accounts(self):
         t = self.transaction()
-        search = t.account_search()
-    
+        search = t.get_account_search()
+        accounts = search.search()
+        results = []
+        for account in accounts:
+            a = Account()
+            a.id = account.get_id()
+            a.name = account.get_name()
+            a.passwords = account.get_authentications()
+            # Should select hash
+            a.password = a.passwords[0] 
+            try:
+                a.gecos = account.get_gecos()
+                a.uid = account.get_posix_uid()
+                a.shell = account.get_shell()
+            except Exception, e:
+                print "Not posix", a.name
+                a.gecos = ""
+                a.uid = None
+                a.shell = None
+            results.append(a)
+        t.rollback()
+        return results
+
+class Account:
+    def __repr__(self):
+        return "<Account %s %r>" % (self.name, self.gecos)
     
 class TestConnect(unittest.TestCase):
     def testConnect(self):
@@ -90,7 +117,7 @@ class TestAPHandler(unittest.TestCase):
     def testGetTransactions(self):
         t = self.sync.ap.new_transaction()
         transactions = self.sync.ap.get_transactions()
-        equal = [t1 for t1 in transactions if t1._is_equivalent(t)]
+        equal = [t1 for t1 in transactions if t1._is_equivalent(t) ]
         self.assertEqual(len(equal), 1)
         t.rollback()
 
@@ -99,11 +126,32 @@ class TestTransaction(unittest.TestCase):
     def setUp(self):
         self.sync = Sync()        
         self.t = self.sync.ap.new_transaction()
+    
+    def tearDown(self):
+        self.t.rollback()    
 
     def testAccountSearch(self):
         search = self.t.get_account_search()
         result = search.search()
         assert result
+            
+
+class TestSync(unittest.TestCase):
+    def setUp(self):
+        self.s = Sync()
+    
+    def tearDown(self):
+        del self.s    
+
+    def testTransaction(self):
+        t = self.s.transaction()
+        assert t in self.s._transactions
+        t.rollback()
+
+    def testRollback(self):
+        t = self.s.transaction()
+        #self.s.rollback()
+        #self.assertRaises(GroErrors.TransactionError, t.rollback)
             
 
 if __name__ == "__main__":
