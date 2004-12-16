09066ec17e5fa3e8c9327d5f62da5d66dd2479db
Stian Soiland-Reyes <stain@unknown>
2004-12-16 14:03:46 +0000

    get_accounts and get_groups returns proper lists of Accounts and Groups. test_get_account added, while test_get_groups is missing.

--- gro/sync/sync.py
+++ gro/sync/sync.py
@@ -4,6 +4,7 @@ import Gro
 import Cerebrum_core.Errors as GroErrors
 import config
 import unittest
+import sys
 from doc_exception import *
 
 class SyncError(DocstringException):
@@ -18,16 +19,16 @@ class LoginError(SyncError):
 class Sync:
     def __init__(self):
         self._transactions = []
-        self.connect()
+        self._connect()
    
     def __del__(self):
-        # We shouldn't have any transactions now, but if we
-        # do, we abort them.
-        if self._transactions:
-            print "WARNING: %s transactions still open" % len(self._transactions)
-        self.rollback()
+        # We shouldn't have any open transactions now, but if we do, we
+        # abort them.
+        if self._open_transactions():
+            print >>sys.stderr, "WARNING: %s transactions still open" % len(self._transactions)
+        self._rollback()
     
-    def rollback(self):
+    def _rollback(self):
         """Rolls back any open transactions"""
         for t in self._transactions:
             try:
@@ -36,7 +37,18 @@ class Sync:
                 pass    
         self._transactions = []
 
-    def connect(self):    
+    def _open_transactions(self):
+        """Returns current open transactions opened by self. 
+           Removes closed transactions from self._transactions.
+        """
+        my_transactions = self._transactions
+        open_transactions = self.ap.get_transactions()
+        self._transactions = [my for my in my_transactions 
+                  if filter(my._is_equivalent, open_transactions)]
+        return self._transactions              
+
+    def _connect(self):    
+        """Connect and login to Gro"""
         try:
             self.gro = Gro.connect()
         except GroErrors.ServerError:
@@ -52,12 +64,14 @@ class Sync:
         except GroErrors.LoginError, e:
             raise LoginError, user
    
-    def transaction(self):
+    def _transaction(self):
+        """Get a transaction from Gro. Note that you must
+           commit or rollback the transaction when finished."""
         try:
             t = self.ap.new_transaction()         
         except GroErrors.ServerError:
             # let's naivly try to reconnect 
-            self.connect()
+            self._connect()
             # This time it should work!    
             try:
                 t = self.ap.new_transaction()         
@@ -67,33 +81,94 @@ class Sync:
         return t    
 
     def get_accounts(self):
-        t = self.transaction()
+        """Get all accounts from Gro. Returns a list of Account objects."""
+        t = self._transaction()
         search = t.get_account_search()
         accounts = search.search()
         results = []
         for account in accounts:
-            a = Account()
-            a.id = account.get_id()
-            a.name = account.get_name()
-            a.passwords = account.get_authentications()
-            # Should select hash
-            a.password = a.passwords[0] 
-            try:
-                a.gecos = account.get_gecos()
-                a.uid = account.get_posix_uid()
-                a.shell = account.get_shell()
-            except Exception, e:
-                print "Not posix", a.name
-                a.gecos = ""
-                a.uid = None
-                a.shell = None
+            a = Account(account)
             results.append(a)
         t.rollback()
         return results
 
-class Account:
+    def get_groups(self):
+        t = self._transaction()
+        search = t.get_group_search()
+        groups = search.search()
+        results = []
+        for group in groups:
+            g = Group(group)
+            results.append(g)
+        t.rollback()
+        return results
+ 
+class Group:           
+    """Stub object for representation of an account"""
+    def __init__(self, group):
+        # Internal Cerebrum ID 
+        self.id = group.get_id()
+        # Username
+        self.name = group.get_name()
+        # Description
+        self.description = group.get_description()
+        # Complete list of members (usernames) 
+        members = group.get_members()
+        # Account objects 
+        self.members = [ Account(a) for a in members ]
+        # Just the usernames
+        self.membernames = [ a.name for a in self.members ]
+        try:
+            # Posix 
+            self.gid = group.get_posix_gid()
+            dir(group)
+        except Exception, e:
+            self.gid = None    
+
     def __repr__(self):
-        return "<Account %s %r>" % (self.name, self.gecos)
+        return "<Group %s>" % self.name
+
+class Account:
+    """Stub object for representation of an account"""
+    def __init__(self, account):
+        # Internal Cerebrum ID 
+        self.id = account.get_id()
+        # Username
+        self.name = account.get_name()
+        auths = account.get_authentications()
+        # Dictionary of password hashes, hashmethod as key
+        self.passwords = dict([ (auth.get_method().get_name(), auth.get_auth_data() )
+                            for auth in auths ])
+        # OBSOLETE: Preferred password hash
+        self.password = self.passwords['MD5-crypt'] 
+        # Should get home directory from server.. must be
+        # related to the active spread or something like that.
+        self.home = "/home/%s" % self.name
+        try:
+            # POSIX-enabled features 
+            # Full name
+            self.fullname = account.get_gecos()
+        except Exception, e:
+            #print "Not posix", self.name
+            # owner = account.get_owner() 
+            # Should do owner.get_names() or something.. but that
+            # doesn't work. We'll just write the username as the
+            # fullname instead
+            # Since we don't have POSIX, set to None
+            self.fullname = self.name
+            self.gid = None
+            self.uid = None
+            self.shell = None
+        else:    
+            # Rest of POSIX stuff 
+            # get_gid returns a Group instance, not a gid =)
+            group = account.get_gid() 
+            self.gid = group.get_posix_gid()
+            self.uid = account.get_posix_uid()
+            self.shell = account.get_shell().get_shell()
+
+    def __repr__(self):
+        return "<Account %s %r>" % (self.name, self.fullname)
     
 class TestConnect(unittest.TestCase):
     def testConnect(self):
@@ -102,7 +177,7 @@ class TestConnect(unittest.TestCase):
         
     def testReConnect(self):
         sync = Sync()
-        sync.connect()
+        sync._connect()
     
 class TestAPHandler(unittest.TestCase):
     def setUp(self):
@@ -133,7 +208,7 @@ class TestTransaction(unittest.TestCase):
     def testAccountSearch(self):
         search = self.t.get_account_search()
         result = search.search()
-        assert result
+        assert len(result) > 0
             
 
 class TestSync(unittest.TestCase):
@@ -144,14 +219,34 @@ class TestSync(unittest.TestCase):
         del self.s    
 
     def testTransaction(self):
-        t = self.s.transaction()
+        t = self.s._transaction()
         assert t in self.s._transactions
+        assert t in self.s._open_transactions()
         t.rollback()
+        # open_transactions() should remove it 
+        assert t not in self.s._open_transactions()
+        assert t not in self.s._transactions
 
     def testRollback(self):
-        t = self.s.transaction()
-        #self.s.rollback()
-        #self.assertRaises(GroErrors.TransactionError, t.rollback)
+        t = self.s._transaction()
+        self.s._rollback()
+        # If the t is rolled back, we shouldn't be able to roll back
+        # again 
+        self.assertRaises(GroErrors.TransactionError, t.rollback)
+    
+    def testGetAccounts(self):
+        accounts = self.s.get_accounts()
+        assert accounts
+        # We can assume that bootstrap_account exists for now 
+        has_bootstrap = filter(lambda a: a.name == 'bootstrap_account', accounts)
+        assert has_bootstrap
+        bootstrap = has_bootstrap[0]
+        assert bootstrap.fullname == "bootstrap_account"
+        assert bootstrap.password
+        # And he doesn't have any of those POSIX stuff 
+        assert not bootstrap.uid 
+        assert not bootstrap.gid
+        assert not bootstrap.shell
             
 
 if __name__ == "__main__":
