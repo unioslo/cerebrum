424e2719bda3a5b3e096b6a00158df32e0fbe663
Stian Soiland-Reyes <stain@unknown>
2004-12-16 14:04:55 +0000

    Generates passwd, group and shadow in standard UNIX way or something like that.

---
+++ gro/sync/syncfile.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import sync
+import file
+import config
+
+def main():
+    s = sync.Sync()
+    passwd = file.PasswdFile(config.sync.get("file", "passwd"))
+    shadow = file.ShadowFile(config.sync.get("file", "shadow"))
+    passwd.begin()
+    shadow.begin()
+    try:
+        for account in s.get_accounts():
+            try:
+                passwd.add(account)
+                shadow.add(account)
+            except file.NotSupportedError:
+                pass    
+    except Exception, e:
+        print "Exception %s occured, aborting" % e
+        passwd.abort()
+        shadow.abort()            
+    else:            
+        passwd.close()    
+        shadow.close()
+
+    groupfile = file.GroupFile(config.sync.get("file", "group"))
+    groupfile.begin()
+    try:
+        for group in s.get_groups():
+            try:
+                groupfile.add(group)
+            except file.NotSupportedError:
+                pass    
+    except Exception, e:
+        print "Exception %s occured, aborting" % e
+        groupfile.abort()
+    else:    
+        groupfile.close()
+
+if __name__ == "__main__":
+    main()
