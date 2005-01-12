896ee0b1ca4d504d133bf5d970afc6e22863e79a
Stian Soiland-Reyes <stain@unknown>
2005-01-12 04:33:24 +0000

    except errors from errors.py instead

--- spine/ceresync/syncfile.py
+++ spine/ceresync/syncfile.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
 
+# Copyright 2004, 2005 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+import errors
 import sync
 import file
 import config
@@ -15,7 +35,7 @@ def main():
             try:
                 passwd.add(account)
                 shadow.add(account)
-            except file.NotSupportedError:
+            except errors.NotSupportedError:
                 pass    
     except Exception, e:
         print "Exception %s occured, aborting" % e
@@ -31,7 +51,7 @@ def main():
         for group in s.get_groups():
             try:
                 groupfile.add(group)
-            except file.NotSupportedError:
+            except errors.NotSupportedError:
                 pass    
     except Exception, e:
         print "Exception %s occured, aborting" % e
