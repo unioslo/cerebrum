fd64ac83c275c7f463c1e271455e2542705d3750
Stian Soiland-Reyes <stain@unknown>
2005-01-13 17:05:03 +0000

    docstring and copyright mongo added

--- spine/ceresync/errors.py
+++ spine/ceresync/errors.py
@@ -1,3 +1,28 @@
+# -*- coding: iso-8859-1 -*-
+
+# Copyright 2004, 2005 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""General exception classes for Ceresync.
+Modules might subclass these for module-specific exceptions,
+or add those exceptions that are general (like PosixError)
+"""
+
 from doc_exception import DocstringException, ProgrammingError
 
 class SyncError(DocstringException):
