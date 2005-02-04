4ad9663b9a8c7ed61eec41b6cf7820d93ce79078
Rune Frøysa <runefro@usit.uio.no>
2005-02-04 13:56:36 +0000

    new option -t

--- contrib/no/uio/list_disk_quotas.py
+++ contrib/no/uio/list_disk_quotas.py
@@ -7,10 +7,12 @@ import mx
 import cerebrum_path
 from Cerebrum import Errors
 from Cerebrum.Utils import Factory
+from Cerebrum import Utils
 from Cerebrum.modules.no.uio.DiskQuota import DiskQuota
 db = Factory.get('Database')()
 
-def list_disk_quotas():
+def list_disk_quotas(fname):
+    f = Utils.SimilarSizeWriter(fname, "w")
     now = mx.DateTime.now()
     dq = DiskQuota(db)
     for row in dq.list_quotas():
@@ -23,19 +25,22 @@ def list_disk_quotas():
             home = row['home']
         else:
             home = "%s/%s" % (row['path'], row['entity_name'])
-        print "%s:%s:%s" % (row['entity_name'], home, quota)
+        f.write("%s:%s:%s\n" % (row['entity_name'], home, quota))
+    f.close()
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'l', ['help'])
+        opts, args = getopt.getopt(sys.argv[1:], 'lt:', ['help'])
     except getopt.GetoptError:
         usage(1)
 
     for opt, val in opts:
         if opt in ('--help',):
             usage()
+        elif opt in ('-t',):
+            fname = val
         elif opt in ('-l',):
-            list_disk_quotas()
+            list_disk_quotas(fname)
     if not opts:
         usage(1)
 
@@ -43,6 +48,7 @@ def usage(exitcode=0):
     print """Usage: [options]
     List disk quotas for all users.
 
+    -t target_file
     -l : list disk quotas
     """
     sys.exit(exitcode)
