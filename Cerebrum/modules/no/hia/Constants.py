5f4b08da65eb89d6278bc1be03e3e236ccaefeec
Jasmina Hodzic <jasminah@usit.uio.no>
2005-02-07 08:48:45 +0000

    Ny landskode (Singapore)

--- Cerebrum/modules/no/hia/Constants.py
+++ Cerebrum/modules/no/hia/Constants.py
@@ -267,6 +267,7 @@ class Constants(central_Constants):
     country_au = _CountryCode("AU", "Australia", "61", "Australia")
     country_dk = _CountryCode("DK", "Denmark", "45", "Denmark")
     country_it = _CountryCode("IT", "Italia", "39", "Denmark")
+    country_sg = _CountryCode("SG", "Singapore", "65", "Singapore")
 
 ## Landkoder som forekommer i SAP-dumpen uten å være
 ## definert i Cerebrum
