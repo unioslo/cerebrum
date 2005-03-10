ebf72e4ad0dd0aeeec8c92195a81fdd158775056
Rune Frøysa <runefro@usit.uio.no>
2005-03-10 10:30:18 +0000

    can now also build netgroup.user identical to the map made by contrib/generate_nismaps.py

--- contrib/dns/generate_nismaps.py
+++ contrib/dns/generate_nismaps.py
@@ -51,52 +51,40 @@ class NISGroupUtil(object):
         return ret
 
     def _expand_group(self, gid, flatten=False):
+        """Expand a group and all of its members.  Subgroups are
+        included regardles of spread, but if they are of a different
+        spread, the groups members are expanded.
+
+        If flatten=True, subgroups will always be expanded.
+        """
         ret_groups = []
         ret_non_groups = []
         self._group.clear()
         self._group.find(gid)
+
+        # Direct members
         u, i, d = self._group.list_members(spread=self._member_spread,
                                            member_type=self._member_type)
         for row in u:
-            uname = self._entity2name.get(int(row[1]), None)
-            if not uname:
+            name = self._entity2name.get(int(row[1]), None)
+            if not name:
                 logger.warn("Was %i very recently created?" % int(row[1]))
                 continue
-            ret_non_groups.append(uname)
-        # we include subgroups regardless of their spread, but
-        # if they're from a different spread we need to include
-        # their members explicitly.
+            ret_non_groups.append(name)
+
+        # Subgroups
         u, i, d = self._group.list_members(member_type=co.entity_group)
         for row in u:
             gid = int(row[1])
-            if self._exported_groups.has_key(gid):
+            if (not flatten) and self._exported_groups.has_key(gid):
                 ret_groups.append( self._exported_groups[gid])
             else:
-                t_g, t_ng = self._expand_group(gid)
-                ret_groups.append(t_g)
-                ret_non_groups.append(t_ng)
+                t_g, t_ng = self._expand_group(gid, flatten=flatten)
+                ret_groups.extend(t_g)
+                ret_non_groups.extend(t_ng)
         # TODO: Also process intersection and difference members.
         return ret_groups, ret_non_groups
 
-    def generate_netgroup(self, filename):
-        logger.debug("generate_netgroup: %s" % filename)
-
-        f = Utils.SimilarSizeWriter(filename, "w")
-        f.set_size_change_limit(5)
-
-        n = 0
-        zone = '.uio.no.'
-        len_zone = len(zone)
-        for group_id in self._exported_groups.keys():
-            group_members, user_members = self._expand_group(group_id)
-            line = " ".join((" ".join(group_members),
-                             " ".join(["(%s,-,)" % m[:-len_zone] for m in user_members
-                                       if m.endswith(zone)]),
-                             " ".join(["(%s,-,)" % m[:-1] for m in user_members]),
-                             ))
-            f.write(self._wrap_line(self._exported_groups[group_id], line))
-        f.close()
-        
     def _wrap_line(self, gname, line):
         ret = ''
         maxlen = MAX_LINE_LENGTH - (len(gname) + 1)
@@ -115,10 +103,61 @@ class NISGroupUtil(object):
             line = "%s %s" % (tmp_gname, line[:pos])
         return ret + "%s %s\n" % (gname, line)
 
+    def generate_netgroup(self, filename):
+        logger.debug("generate_netgroup: %s" % filename)
+
+        f = Utils.SimilarSizeWriter(filename, "w")
+        f.set_size_change_limit(5)
+
+        n = 0
+        for group_id in self._exported_groups.keys():
+            group_name = self._exported_groups[group_id]
+            group_members, user_members = self._expand_group(group_id)
+            logger.debug("%s -> g=%s, u=%s" % (group_id, group_members, user_members))
+            f.write(self._wrap_line(group_name,
+                                    self._format_members(
+                group_members, user_members, group_name)))
+        f.close()
+        
+
+class MachineNetGroup(NISGroupUtil):
+    def __init__(self, group_spread, member_spread, zone):
+        super(MachineNetGroup, self).__init__(
+            co.dns_owner_namespace, co.entity_dns_owner, group_spread, member_spread)
+        self.zone = zone
+        self.len_zone = len(zone)
+
+    def _format_members(self, group_members, user_members, group_name):
+        return " ".join((" ".join(group_members),
+                         " ".join(["(%s,-,)" % m[:-self.len_zone] for m in user_members
+                                   if m.endswith(self.zone)]),
+                         " ".join(["(%s,-,)" % m[:-1] for m in user_members])))
+
+class UserNetGroup(NISGroupUtil):
+    def __init__(self, group_spread, member_spread):
+        super(UserNetGroup, self).__init__(
+            co.account_namespace, co.entity_account, group_spread, member_spread)
+
+    def _format_members(self, group_members, user_members, group_name):
+        tmp_users = []
+        for uname in user_members:
+            tmp = posix_user.illegal_name(uname)
+            if tmp:
+                logger.warn("Bad username %s in %s" % (tmp, group_name))
+            elif len(uname) > 8:
+                logger.warn("Bad username %s in %s" % (uname, group_name))
+            else:
+                tmp_users.append(uname)
+
+        return " ".join((" ".join(group_members),
+                         " ".join(["(,%s,)" % m for m in tmp_users])))
+
 def main():
-    ngu = NISGroupUtil(co.dns_owner_namespace, co.entity_dns_owner,
-                       _SpreadCode('NIS_mng@uio'), None)
-    ngu.generate_netgroup('test.dump')
+    ngu = MachineNetGroup(_SpreadCode('NIS_mng@uio'), None, '.uio.no.')
+    ngu.generate_netgroup('mng.dump')
+
+    ung = UserNetGroup(_SpreadCode('NIS_ng@uio'), _SpreadCode('NIS_user@uio'))
+    ung.generate_netgroup('ung.dump')
 
 if __name__ == '__main__':
     main()
