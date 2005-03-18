4b6abbe057237aee524bb544f6f7ef38b1dcacfb
Erik Gorset <erikgors@itea.ntnu.no>
2005-03-18 21:15:03 +0000

    added promote_posix

--- spine/lib/cerebrum/PosixGroup.py
+++ spine/lib/cerebrum/PosixGroup.py
@@ -18,6 +18,7 @@
 # along with Cerebrum; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
+from SpineLib.Builder import Method
 from SpineLib.DatabaseClass import DatabaseAttr
 
 from Group import Group
@@ -25,9 +26,20 @@ from Group import Group
 from SpineLib import Registry
 registry = Registry.get_registry()
 
+import Cerebrum.modules.PosixGroup
+
 Group.register_attribute(DatabaseAttr('posix_gid', 'posix_group', int, optional=True))
 Group.db_attr_aliases['posix_group'] = {'id':'group_id'}
 Group.build_methods()
 Group.build_search_class()
+
+def promote_posix(self):
+    obj = self._get_cerebrum_obj()
+    p = Cerebrum.modules.PosixGroup.PosixGroup(self.get_database())
+    p.populate(parent=obj)
+    p.write_db()
+
+Group.register_method(Method('promote_posix', None, write=True), promote_posix)
+
 
 # arch-tag: 1d1d6cc7-0222-42b1-9e43-4953ad046987
