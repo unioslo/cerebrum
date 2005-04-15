5553772d25a64a218c6ed7dbdea07ee3c12a4b8f
Rune Frøysa <runefro@usit.uio.no>
2005-04-15 09:56:14 +0000

    utdypet spec'en noe

--- contrib/no/uio/fs2ltPerson.py
+++ contrib/no/uio/fs2ltPerson.py
@@ -19,45 +19,104 @@ from Cerebrum.modules.no import fodselsnr
 Overføring av data fra FS til LT
 ==================================
 
+Basert på 'LT-03-020 Gjest til Frida.doc'
+
 Data som overføres
 ===========================
 
-Personer
---------
+Aktuelle personer
+-------------------
 
-Alle doktorgrads-studenter som ligger i FS skal overføres.
+* doktorgrads-studenter: Alle ekstern finansierte
+  doktorgrads-studenter som ligger i FS skal overføres (LT er
+  autoritativ for stipendiater som har UiO som arbeidsgiver, og slike
+  personer skal det således ikke overføres informasjon om).
 
-Personen skal kun registreres i LT.PERSON dersom vedkommende ikke
-finnes der fra før.  Det gjøres ingen oppdatering/sletting.  Personen
-registreres også i LT.GJEST med gjestetypekode EF-STIP.
+Spørsmål:
+  * ingen øvrige personer er av interesse? Fagperson?
 
-Fra FS:
-  Student.list_drgrad returnerer relevante personer.  Aktuelle kolonner:
+Fra FS
+-------
+  Vi er interessert i følgende data om personene:
+
+  * Fødselsnummer
+  * Navn
+  * Stedkoden(e) til dogtorgrads-studieprogramme(t/ne) der personen
+    har opptak
+  * tidsrom for studieretten
+  * 1 adresse: primært fs.student.*_semadr, sekundært
+    fs.person.*_hjemsted
+
+  Student.list_drgrad returnerer de personer vi ønsker å overføre.
+  Aktuelle kolonner:
+
    - dato_studierett_tildelt
    - dato_studierett_gyldig_til
-   - institusjonsnr, faknr, instituttnr, gruppenr
+   - institusjonsnr, faknr, instituttnr, gruppenr (fra *_studieansv)
 
-Til LT:
+Spørsmål:
+  * skal fs.person.status_reserv_nettpubl benyttes til noe?
+  * telefon, f.eks fs.person.telefonnr_hjemsted?
+  * det må verifiseres at Student.list_drgrad ikke returnerer flere
+    personer enn vi er interessert i.
+
+
+Til LT
+--------
+  Kolonner ikke beskrevet her skal ha sine default-verdier fra
+  databaseskjemaet, normalt NULL.
+
   LT.PERSON:
-
-   - fodtdag, fodtmnd, fodtar, personnr
-   - navn : fulltnavn 'etternavn fornavn'
-   - pentretypkode : ??
+    - fodtdag, fodtmnd, fodtar, personnr
+    - navn: fulltnavn på formen 'etternavn fornavn'
+    - pentretypkode: 'JA'
+    - opprettemerke_ka_oppforing: NULL
    
+    Spørsmål:
+      - *_privatadresse: fra FS
+      - telefonnr_privattelefon: fra FS?
+      - tittel: brukes ikke?  Kun fagpersoner har i FS?
 
   LT.GJEST:
-   - DATO_FRA
-   - DATO_TIL
-   - FAKULTETNR/INSTITUTTNR/GRUPPENR
-  
-TBD: Oppdatering/sletting i lt.gjest?  Hva med øvrige tabeller?
+    - fodtdag, fodtmnd, fodtar, personnr
+    - dato_fra: FS: dato_studierett_tildelt
+    - dato_til: FS: dato_studierett_gyldig_til eller 3000-01-01 hvis
+      NULL i FS.
+    - fakultetnr/instituttnr/gruppenr: fra studieprogram
+    - gjestetypekode: 'EF-STIP'
 
+    Spørsmål:
+      - gjesteanskode: er NOT NULL.  Skal 'USPES' brukes?
+
+  LT.ARBSTEDTELEFON:
+    - fodtdag, fodtmnd, fodtar, personnr
+    Spørsmål:
+      - skal denne benyttes?
+
+  LT.PERSKOMM:
+    - fodtdag, fodtmnd, fodtar, personnr
+    Spørsmål:
+      - Skal den benyttes? LT-03-020 antyder at et telefonnummer og
+        e-mail adresse skal lagres her.
+
+  LT.RESERVASJON:
+    - fodtdag, fodtmnd, fodtar, personnr
+    - katalogkode: ELKAT
+    - felttypekode: TOTAL
+    - resnivakode: TOTAL
+
+Oppdatering/Sletting
+---------------------
+  Det gjøres ingen oppdatering/sletting av eksisterende data utover
+  det som er beskrevet i denne seksjonen.
+
+  * LT.GJEST: dato_fra/dato_til oppdateres fortløpende
 
 Spesielle merknader
 --------------------
   Dersom personen i følge Cerebrum finnes i FS og LT, men har
-  forskjellig fødselsnummer, skal logges en feilmelding.  Ingen data
-  skal endres for slike personen.
+  forskjellig fødselsnummer, skal logges en feilmelding og personen
+  ignoreres.
 
 """
 
@@ -69,12 +128,12 @@ logger = Factory.get_logger("cronjob")
 def _conv_date(d):
     if type(d) == lt.db._db_mod.dco2.OracleDateType:
         return time.strftime('%Y-%m-%d', time.localtime(long(d)))
-    elif type(d) == 'str':
+    elif isinstance(d, str):
         if len(d) == 8:
             return '%s-%s-%s' % (d[0:4], d[4:6], d[6:8])
         logger.warn("wierd date format: %s" % (d))
     elif d:
-        logger.warn("wierd date format: %s" % (d))
+        logger.warn("wierd date format: %s (type: %s)" % (d, type(d)))
     return None
 
 def prefetch_person_info():
