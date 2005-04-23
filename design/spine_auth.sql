990f133335dade741d4e872f250de840dad217c0
Stian Soiland-Reyes <stain@unknown>
2005-04-23 13:52:14 +0000

    more docs

--- design/spine_auth.sql
+++ design/spine_auth.sql
@@ -55,17 +55,99 @@ DROP TABLE auth_operation_set;
 category:drop;
 DROP TABLE auth_op_code;
 category:drop;
-DROP TABLE auth_op_target_attrs_key_code;
+DROP TABLE auth_op_target_attrs_key;
 category:drop;
 DROP TABLE auth_op_target_type_code;
 
 
 /* Maps [object_type][.method] to authentication function
+*   
+* When authenticating a method call, Spine will search this 
+* mapping to find a valid match. The search will be done 
+* something like this:
 * 
+*    for mapping sorted by descending priority:
+*       if object_type matches the requested object
+*             or is NULL:
+*           if method matches the requested method
+*                 or is NULL:
+*             call auth_func_name  
+*             if result is True or False:
+*                 return result
+*             else:
+*                 continue    
+*
+* This means that each mapping function will be called if
+* the object_type and method matches (or are NULL). The first function
+* to return True (ACCEPT) or False (DROP) will take responsibility, 
+* the search will stop, and the result will determine if the method call 
+* will proceed or not.
+*
+* If the function returns None (RETURN), this indicates that the
+* function did not take responsibility for this specific case (for
+* instance a priority 0 handler for set_quarantine can return True or
+* False on a specific quarantine value it is set to handle, but return
+* None on all other quarantines). When a mapping matches, but the
+* function handler does not take responsability, the search will
+* continue - eventually matching a default handler. 
+* 
+* If no function mapping take responsability, the default action will be
+* DENY.
+*
+* Users familiar with iptables/ipchains will recognize this
+* ACCEPT/DROP/RETURN procedure.
 *
 *
+* priority represents the importance of this mapping.
+* The lower number, the earlier the auth_func_name will
+* be called. Priority 0 is the highest priority.
+* Recommendations:
+* 
+*     0   specific overrides for superadmins
+*            (ie. *.set_quarantine for some quarantines)
+*    10   general overrides for local admins
+*            (ie. Account.*, *.get_quarantine(), ..)
+*    30   specific type, specific method
+*            (ie. Account.set_password)
+*    50   specific type, all methods
+*            (ie. Account.*) 
+*    70   specific methods, all types
+*            (ie. *.set_quarantine)
+*   100   default for all types, all methods
+*            (ie *.*)
+*   
+* object_type is the name of the Spine type to match, 
+* for instance Account, Person, Spread. Note that this must be
+* the direct name, as superclasses (ie. Entity)
+* will not be searched. If object_type is NULL, this mapping
+* will apply to any Spine types given the method.
+*
+* method is the name of the Spine method to match,
+* for instance set_password, get_name, create_group.
+* Note that the general Spine commands like create_group
+* resides in the Spine type Commands. If method is
+* NULL, this mapping will apply to any methods given
+* the object_type.
+* 
+* If both object_type and method is NULL, the mapping will always match.
+*
+* auth_func_name is the name of the authentication function
+* for Spine to call. 
+* 
+* The function will be called as 
+* authfunc(operator, object, method, arguments) and must return either
+* True, False or None, indicating ACCEPT (authorized), DROP (not
+* authorized by rule) or RETURN (not authorized, but lower priority
+* rules might authorize). 
+* 
+* Note that any other return values than None will be
+* interpreted as True or False by their boolean value.
+*
+* FIXME: Name the (factory-compiled) class of authentication functions
+*        and check the parameters
+* 
 */
-category:main;
+ category:main;
 CREATE TABLE auth_func_map (
   auth_func_id      NUMERIC(6,0)
                         CONSTRAINT auth_func_map_pk PRIMARY KEY,
@@ -80,14 +162,26 @@ CREATE TABLE auth_func_map (
 
 /* Defines the legal operations that may be performed.
 *
-* Everything in Spine is methods, and the operations should be the same as
-* the method, to give access to it. Attributes are accessed through get and set
-* methods: "get_description", "set_name". If you want an account to be able to
-* set his own password, you have to give him the operation_code "set_password"
-* and make it target his own account.
-*
+* These codes will be used in two different ways:
+*   a) Simple authorative "flags" used by 
+*      one or several specific auth_funcs. 
+*      For instance:
+*           invite_members
+*           remove_quarantine_teppe 
+*      Such codes will have is_method=0
+
+*   b) Used by a default auth_func handler to give permission for a
+*      specific Spine method by their type and name.
+*      For instance:
+*           Group.add_member
+*           Person.get_name
+*           Account.set_password      
+*      Such codes will have is_method=1
+*             
 *
 * is_method set to true if code_str is objtype.method
+* and this op_code is generated from the available
+* Spine types and methods.
 */
 category:code;
 CREATE TABLE auth_op_code (
@@ -108,12 +202,26 @@ CREATE TABLE auth_op_code (
 *
 * An operation set contains several operations. You can only give 
 * entities access to a set of operations.
+*
+* For instance, one auth_operation_set might be called
+* 'Own account' and contain operations normally allowed
+* for a user to do on his own account. (ie.
+* auth_role.entity_id==auth_op_target.entity_id)
+*
+* Another example is an auth_operation_set of operations
+* on other accounts.
+*
+* A set of operations can be used by several entities,
+* and against several targets.
+*  
 */
 category:main;
 CREATE TABLE auth_operation_set (
   op_set_id        NUMERIC(12,0)
                      CONSTRAINT auth_operation_set_pk PRIMARY KEY,
-  name             CHAR VARYING(30)
+  name             CHAR VARYING(30) NOT NULL,
+  description      CHAR VARYING(512)
+                     NOT NULL DEFAULT '',
 );
 
 
@@ -142,6 +250,10 @@ CREATE INDEX auth_operation_set_id ON auth_operation(op_set_id);
 /* Defines attributes associated with an auth_operation.
 * 
 * Attributes can be used for validation,  such as legal shells etc.
+*
+* The specific meaning of a list of attributes associated with
+* a auth_op_code is defined by the using auth_func.
+* 
 */
 category:main;
 CREATE TABLE auth_op_attrs (
@@ -154,7 +266,16 @@ CREATE TABLE auth_op_attrs (
 
 
 /*
-*
+* Possible auth_op_target target_types. 
+* 
+* The target type will usually be the same as
+* the Spine type attached to the auth_op_target entity_id,
+* for instance Account, Person, Spread.
+* 
+* Attached to a auth_op_target_type_code are also different
+* auth_op_target_attrs_key that are valid keys for 
+* this target_type in auth_op_target_attrs.
+* 
 */
 category:code;
 CREATE TABLE auth_op_target_type_code (
@@ -170,19 +291,27 @@ CREATE TABLE auth_op_target_type_code (
 
 /* Defines rules for finding an entity target.
 *
-* The targets is the entity wich the operation is performed on.
-*
-* Examples:
+* The targets is the object the operation is performed on.
 * 
-*   Users on a disk:
-*     op_target_type = 'disk'     entity_id=<disk.entity_id>
-*   Users on a host:
-*     op_target_type = 'host'     entity_id=<host.entity_id>
-*   Users on a host:/path/host/sv-l*
-*     op_target_type = 'host'     entity_id=<host.entity_id> 
-*     attr = 'sv-l.*' (note: regular expression, and only leaf directory)
-*   Allowed to set/clear spread X
-*     op_target_type = 'spread'   entity_id = <spread_code.code>
+* A target must be of a valid target_type, for instance
+* auth_op_target_type_code('Group') or
+* auth_op_target_type_code('Person').
+*
+* If entity_id is set, this target represents a 
+* single entity. For instance, an account in auth_role.entity_id might
+* have his own entity_id as a target for operations on his own user, 
+* such as Account.set_password.
+*
+* If no entity_id is given, the rules of the auth_op_target_type_code
+* applies for matching objects. 0 or more auth_op_target_attrs may
+* be attached to the target limiting the search.
+*
+* For instance, an attribute 
+* auth_op_target_attrs_key('spread')=spread_code('payroll')
+* on an auth_op_target with
+* target_type=auth_op_target_type_code('Account')
+* will apply to accounts with the spread 'payroll'. 
+*
 */
 category:main;
 CREATE TABLE auth_op_target (
@@ -192,8 +321,7 @@ CREATE TABLE auth_op_target (
   target_type      NUMERIC(6,0)
                     NOT NULL
                     CONSTRAINT auth_op_target_type_fk
-                        REFERENCES auth_op_target_type_code(code),
-  attr             CHAR VARYING(50)
+                        REFERENCES auth_op_target_type_code(code)
 );
 
 category:main;
@@ -201,26 +329,64 @@ CREATE INDEX auth_op_target_entity_id ON auth_op_target(entity_id);
 
 
 /*
+* A valid key for a auth_op_target_attrs key/value pair.
+* 
+* Each attribute in auth_op_target_attrs will normally
+* limit the matching against objects.
+* 
+* Example keys:  spread, ou, affiliation_status 
+*
+* A auth_op_target_attrs_key is valid
+* for a specific target_type. For instance,
+* with target_type='Account' a valid key might
+* be 'member_of' - indicating that the account
+* must be member of the given group.
+*
+* The main use of target_type here is to 
+* limit the view of possible keys for a 
+* target_type in GUI interfaces. 
+* 
+* Note that several target_types might use the
+* same key name, for instance "spread" might
+* apply to both Groups and Accounts.
 *
 */
-category:code;
-CREATE TABLE auth_op_target_attrs_key_code (
-  code             NUMERIC(6,0)
-                     CONSTRAINT auth_op_target_attrs_key_code_pk PRIMARY KEY,
-  code_str         CHAR VARYING(64)
-                     NOT NULL
-                     CONSTRAINT auth_op_target_attrs_key_codestr_u UNIQUE,
+category:main;
+CREATE TABLE auth_op_target_attrs_key (
+  key              CHAR VARYING(64)
+                     NOT NULL,
   description      CHAR VARYING(512)
                      NOT NULL,
   target_type      NUMERIC(6,0)
-                    NOT NULL
-                    CONSTRAINT auth_op_target_attrs_key_code_target_type_fk
-                        REFERENCES auth_op_target_type_code(code)
+                     NOT NULL
+                     CONSTRAINT auth_op_target_attrs_key_target_type_fk
+                        REFERENCES auth_op_target_type_code(code),
+  CONSTRAINT auth_op_target_attrs_key_pk PRIMARY KEY (key, target_type)
 );
 
 
 /*
+* A list of attributes attached to a auth_op_target
+* in the form of key/value pairs. 
 *
+* Keys must be in auth_op_target_attrs_key.
+* 
+* The meaning of a value is specific to the
+* combination of a target_type and key. 
+* 
+* For instance, with target_type=Account,
+* the key "member_of" could mean that the  
+* value is the group_id of a group the account
+* must be a member of.
+* 
+* Or a more complicated example, i both "ou"
+* and "affiliation" is given on a target_type=Person,
+* the person must have the given affiliation_id to the
+* given ou_id. 
+*
+* The exact interpretation is done by the code handling
+* the given target_type.
+* 
 */
 category:main;
 CREATE TABLE auth_op_target_attrs (
@@ -228,10 +394,10 @@ CREATE TABLE auth_op_target_attrs (
                         NOT NULL
                         CONSTRAINT auth_op_target_attrs_op_target_id_fk
                             REFERENCES auth_op_target(op_target_id),
-  key               NUMERIC(6,0)
+  key               CHAR VARYING(64)
                         NOT NULL
                         CONSTRAINT auth_op_target_attrs_key_fk
-                            REFERENCES auth_op_target_attrs_key_code(code),
+                            REFERENCES auth_op_target_attrs_key(key),
   value             CHAR VARYING(256)
                         NOT NULL,
 
@@ -241,9 +407,31 @@ CREATE TABLE auth_op_target_attrs (
 
 /* A role associates an auth_operation_set with an auth_op_target.
 *
-* Links the entity with a set of legal operations and a target.
-* The operationset contains several operations wich got operationcodes wich
-* equals Spine-commands, and attr who can be used for validation.
+* Links the operating entity with a set of legal operations and a
+* target.  The operation set contains several operations that are
+* treated at the "same security level" for common targets. 
+*
+* For instance, normal users will have the operation set
+* "Normal user" attached to their own account, allowing them
+* to change their own password and login shell, plus viewing
+* their own status.
+* 
+* Another example could be "Group administrator" giving permissions on a
+* specific group for adding and removing members, updating the
+* description, but not changing the name.
+*
+* Targets could also be general, as when auth_op_target.entity_id is
+* NULL and filters are given by auth_op_target_attrs. This allows for
+* instance the auth_operation_set "View other persons" on the
+* target_type "Person" - allowing users to see limited attributes on
+* other persons for finding their username, for instance First and Last
+* name.
+*
+* The entity_id can be the direct entity_id of the operators account, or
+* the entity_id of a group the operator is a member of. That way,
+* general (mostly auto-generated) groups like "Students" can get general
+* roles such as "View other persons".
+*
 */
 category:main;
 CREATE TABLE auth_role (
