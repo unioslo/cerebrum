13df606c693aaaf0cf2f8be1df10f39925f9aa8e
Håvar Larsen <havlar@usit.uio.no>
2005-05-11 12:52:46 +0000

    Retrieving notes-id-files from the domino server

---
+++ contrib/get_id_files.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+#
+# Copyright 2002, 2003 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+import binascii
+import string
+
+import notesutils
+
+sock = notesutils.SocketCom()
+users=[]
+IDFILEDIR="/cerebrum/dumps/Notes-IDs/"
+
+sock.send('LISTNEWIDFILES\n')
+line=sock.readline()
+
+while line[3]=='-':
+   (rest,uname)=string.split(line,'&')
+   users.append(uname)
+   line=sock.readline()
+
+for user in users:
+   sock.send('GETIDFILE&'+user+'\n')
+   (rest,idfile)=string.split(sock.readline(),'&')
+   idfile=binascii.a2b_hex(idfile)
+   filename=IDFILEDIR+user+".id"
+   file=open(filename, 'w')
+   file.write(idfile)
+   file.close
+
+sock.send('QUIT\n')
+sock.close()
