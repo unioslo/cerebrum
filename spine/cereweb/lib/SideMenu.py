298c53200e43c58e17efac54bc38b8556400fbbc
Stian Soiland-Reyes <stain@unknown>
2005-08-03 09:14:49 +0000

    email menu items

--- spine/cereweb/lib/SideMenu.py
+++ spine/cereweb/lib/SideMenu.py
@@ -24,8 +24,7 @@ from gettext import gettext as _
 
 class FixUrlMixin:
     def addItem(self, name, label, url):
-            # i18n and fix url
-            label = _(label)
+            # fix url
             url = utils.url(url)    
             item = MenuItem(name, label, url) 
             self.addMenuItem(item) 
@@ -43,60 +42,68 @@ class SideMenu(FixUrlMixin, Menu.Menu):
         self.makeAccount()
         self.makeGroup()
         self.makeOU()
+        self.makeEmail()
         #self.makeRoles()
         #self.makeSpread()
         self.makeOptions()
 
     def makeMain(self):
-        self.main = self.addItem("main", "Main", "index")
+        self.main = self.addItem("main", _("Main") ,"index")
 
     def makePerson(self):
-        self.person = self.addItem("person", "Person", "person")
-        self.person.addItem("search", "Search", "person/search")
-        self.person.addItem("list", "List", "person/list")
-        self.person.addItem("create", "Create", "person/create")
-        self.person.addItem("view", "View", "person/view?id=%s")
-        self.person.addItem("edit", "Edit", "person/edit?id=%s")
+        self.person = self.addItem("person", _("Person") ,"person")
+        self.person.addItem("search", _("Search") ,"person/search")
+        self.person.addItem("list", _("List") ,"person/list")
+        self.person.addItem("create", _("Create") ,"person/create")
+        self.person.addItem("view", _("View") ,"person/view?id=%s")
+        self.person.addItem("edit", _("Edit") ,"person/edit?id=%s")
     
     def makeAccount(self):
-        self.account = self.addItem("account", "Account", "account")
-        self.account.addItem("search", "Seach", "account/search")
-        self.account.addItem("list", "List", "account/list")
+        self.account = self.addItem("account", _("Account") ,"account")
+        self.account.addItem("search", _("Seach") ,"account/search")
+        self.account.addItem("list", _("List") ,"account/list")
 
     def makeGroup(self):    
-        self.group = self.addItem("group", "Group", "group")
-        self.group.addItem("search", "Search", "group")
-        self.group.addItem("list", "List", "group/list")
-        self.group.addItem("create", "Create", "group/create")
-        self.group.addItem("view", "View", "group/view?id=%s")
-        self.group.addItem("edit", "Edit", "group/edit?id=%s")
+        self.group = self.addItem("group", _("Group") ,"group")
+        self.group.addItem("search", _("Search") ,"group")
+        self.group.addItem("list", _("List") ,"group/list")
+        self.group.addItem("create", _("Create") ,"group/create")
+        self.group.addItem("view", _("View") ,"group/view?id=%s")
+        self.group.addItem("edit", _("Edit") ,"group/edit?id=%s")
 
     def makeOU(self):
-        self.ou = self.addItem("ou", "OU", "ou")
-        self.ou.addItem("search", "Search", "ou")
-        self.ou.addItem("list", "List", "ou/list")
-        self.ou.addItem("tree", "Tree", "ou/tree")
-        self.ou.addItem("create", "Create", "ou/create")
-        self.ou.addItem("view", "View", "ou/view?id=%s")
-        self.ou.addItem("edit", "Edit", "ou/edit?id=%s")
+        self.ou = self.addItem("ou", _("OU") ,"ou")
+        self.ou.addItem("search", _("Search") ,"ou")
+        self.ou.addItem("list", _("List") ,"ou/list")
+        self.ou.addItem("tree", _("Tree") ,"ou/tree")
+        self.ou.addItem("create", _("Create") ,"ou/create")
+        self.ou.addItem("view", _("View") ,"ou/view?id=%s")
+        self.ou.addItem("edit", _("Edit") ,"ou/edit?id=%s")
+    
+    def makeEmail(self):    
+        self.email = self.addItem("email", _("Email") ,"email")
+        domains = self.email.addItem("domain", _("Domain") ,"emaildomain")
+        domains.addItem("list", _("List"), "emaildomain/list")
+        domains.addItem("view", _("View"), "emaildomain/view?id=%s")
+        domains.addItem("edit", _("Edit"), "emaildomain/edit?id=%s")
 
     def makeRoles(self):
-        self.group = self.addItem("roles", "Roles", "roles")
-        self.group.addItem("search", "Search", "roles/search")
-        self.group.addItem("list", "List", "roles/list")
-        self.group.addItem("view", "View", "roles/view?id=%s")
-        self.group.addItem("edit", "Edit", "roles/edit?id=%s")
+        self.roles = self.addItem("roles", _("Roles") ,"roles")
+        self.roles.addItem("search", _("Search") ,"roles/search")
+        self.roles.addItem("list", _("List") ,"roles/list")
+        self.roles.addItem("view", _("View") ,"roles/view?id=%s")
+        self.roles.addItem("edit", _("Edit") ,"roles/edit?id=%s")
 
     def makeSpread(self):
-        self.group = self.addItem("spread", "Spread", "spread")
-        self.group.addItem("search", "Search", "spread/search")
-        self.group.addItem("list", "List", "spread/list")
-        self.group.addItem("view", "View", "spread/view?id=%s")
-        self.group.addItem("edit", "Edit", "spread/edit?id=%s")
+        self.spread = self.addItem("spread", _("Spread") ,"spread")
+        self.spread.addItem("search", _("Search") ,"spread/search")
+        self.spread.addItem("list", _("List") ,"spread/list")
+        self.spread.addItem("view", _("View") ,"spread/view?id=%s")
+        self.spread.addItem("edit", _("Edit") ,"spread/edit?id=%s")
 
     def makeOptions(self):
-        self.group = self.addItem("options", "Options", "options")
-        self.group.addItem("view", "View", "options/view")
-        self.group.addItem("edit", "Edit", "options/edit")
+        self.options = self.addItem("options", _("Options") ,"options")
+        self.options.addItem("view", _("View") ,"options/view")
+        self.options.addItem("edit", _("Edit") ,"options/edit")
 
 # arch-tag: 6af7ba3d-76dc-46e1-8327-1ed3e307e9e8
