93fadebd9867da35c99dc65d5d4651376a3ff3a1
Jasmina Hodzic <jasminah@usit.uio.no>
2005-11-07 11:16:28 +0000

    Glemte en AND.

--- Cerebrum/modules/no/access_FS.py
+++ Cerebrum/modules/no/access_FS.py
@@ -490,7 +490,7 @@ class Student(FSObject):
               NVL(sps.dato_studierett_gyldig_til, sysdate) >= SYSDATE AND
               sps.status_sperr_aut_oppd = 'N' AND
               sps.studieprogramkode = sp.studieprogramkode AND
-              %s
+              %s AND
               sp.studienivakode >= 980""" % self._is_alive()
         return self.db.query(qry)
 
