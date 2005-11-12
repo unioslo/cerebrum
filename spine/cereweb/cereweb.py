bb4bb5d0bbbc5988f560604b73f408c28b4f7c47
Cerebrum Arch <cerebrum-commits@usit.uio.no>
2005-11-12 18:19:21 +0000

    Changes from arch/CVS synchronization

--- spine/cereweb/cereweb.py
+++ spine/cereweb/cereweb.py
@@ -23,3 +23,5 @@ if __name__ == '__main__':
     file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'cherrypy.conf')
     cherrypy.config.update(file = file)
     cherrypy.server.start()
+
+# arch-tag: 298feaea-53a6-11da-9370-e22a2f127752
