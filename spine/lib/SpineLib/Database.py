32a3c6e49643f4e05e76c0ccb501004a33622404
Erik Gorset <erikgors@itea.ntnu.no>
2005-11-14 18:38:40 +0000

    Wrapping exception was a mistake.. removing it for now.

--- spine/lib/SpineLib/Database.py
+++ spine/lib/SpineLib/Database.py
@@ -18,11 +18,8 @@
 # along with Cerebrum; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-from threading import RLock
 from Cerebrum.Utils import Factory
-import Cerebrum.Errors
 import Cerebrum.Database
-import SpineExceptions
 Database = Factory.get('Database')
 
 class SpineDatabase(Database):
@@ -32,7 +29,6 @@ class SpineDatabase(Database):
     """
     
     def __init__(self, entity_id=None):
-        self._lock = RLock()
         Database.__init__(self)
         if entity_id is None:
             self.cl_init(change_program='Spine')
@@ -41,54 +37,5 @@ class SpineDatabase(Database):
 
         # i hope this is the correct way to do it
         self.execute('SET TRANSACTION ISOLATION LEVEL SERIALIZABLE')
-
-    def lock(self):
-        self._lock.acquire()
-
-    def release(self):
-        self.rollback_log() # just in case
-        self.rollback() # will this break the database?
-        self._lock.release()
-
-    def query(self, sql, keys={}):
-        """Wraps around the query call on the database to catch exceptions
-        and rethrow proper Spine exceptions."""
-        try:
-            return super(SpineDatabase, self).query(sql, keys)
-        except Cerebrum.Errors.DatabaseConnectionError, e:
-            raise SpineExceptions.DatabaseError('Connection to the database failed', str(e), sql)
-        except Cerebrum.Errors.DatabaseException, e:
-            raise SpineExceptions.DatabaseError('Error during query', str(e), sql)
-        except Cerebrum.Database.DatabaseError, e:
-            raise SpineExceptions.DatabaseError('Error during query', str(e), sql)
-
-    def execute(self, sql, keys={}):
-        """Wraps around the execute call on the database to catch exceptions
-        and rethrow proper Spine exceptions."""
-        try:
-            return super(SpineDatabase, self).execute(sql, keys)
-        except Cerebrum.Errors.DatabaseConnectionError, e:
-            raise SpineExceptions.DatabaseError('Connection to the database failed', str(e), sql)
-        except Cerebrum.Errors.DatabaseException, e:
-            raise SpineExceptions.DatabaseError('Error during query', str(e), sql)
-        except Cerebrum.Database.DatabaseError, e:
-            raise SpineExceptions.DatabaseError('Error during query', str(e), sql)
-    
-    def query_1(self, sql, keys={}):
-        """Wraps around the query_1 call on the database to catch exceptions
-        and rethrow proper Spine exceptions."""
-        try:
-            return super(SpineDatabase, self).query_1(sql, keys)
-        except Cerebrum.Errors.NotFoundError, e:
-            raise SpineExceptions.NotFoundError(*e.args)
-        except Cerebrum.Errors.TooManyRowsError, e:
-            raise SpineExceptions.TooManyMatchesError(*e.args)
-        except Cerebrum.Errors.DatabaseConnectionError, e:
-            raise SpineExceptions.DatabaseError('Connection to the database failed', str(e), sql)
-        except Cerebrum.Errors.DatabaseException, e:
-            raise SpineExceptions.DatabaseError('Error during query', str(e), sql)
-        except Cerebrum.Database.DatabaseError, e:
-            raise SpineExceptions.DatabaseError('Error during query', str(e), sql)
-
 
 # arch-tag: 3a36a882-0fd8-4a9c-9889-9540095f93e3
