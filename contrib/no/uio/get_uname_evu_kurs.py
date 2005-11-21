9bcddb8e50aa9d29b6ea0042b3ac8179da85c5ed
Cerebrum Arch <cerebrum-commits@usit.uio.no>
2005-11-21 09:21:53 +0000

    Revision: cerebrum@usit.uio.no--2005/Cerebrum--cvs--0--patch-787 Creator:  Harald Meland <hmeland@usit.uio.no>
    Update db_row usage.

--- contrib/no/uio/get_uname_evu_kurs.py
+++ contrib/no/uio/get_uname_evu_kurs.py
@@ -44,8 +44,8 @@ import time
 
 def fetch_primary_uname(row, person, account, constants):
 
-    birth_date = str(row.fodselsdato).zfill(6)
-    no_ssn = birth_date + str(row.personnr).zfill(5)
+    birth_date = str(row['fodselsdato']).zfill(6)
+    no_ssn = birth_date + str(row['personnr']).zfill(5)
     sources = map(lambda x: getattr( constants, x ),
                   cereconf.SYSTEM_LOOKUP_ORDER)
     sources.append(None)
@@ -89,10 +89,10 @@ def main():
     for row in fs.evu.list_kurs_deltakere(evukode, tidskode):
         uname = fetch_primary_uname(row, person, account, constants)
         
-        print "%06d %05d %-20s%-30s --> %-10s" % (row.fodselsdato,
-                                                  row.personnr,
-                                                  row.etternavn,
-                                                  row.fornavn,
+        print "%06d %05d %-20s%-30s --> %-10s" % (row['fodselsdato'],
+                                                  row['personnr'],
+                                                  row['etternavn'],
+                                                  row['fornavn'],
                                                   uname)
     # od
 # end main
