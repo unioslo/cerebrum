92a92f9f8ddd56f7819c052a9908dfda6622ae8f
Jasmina Hodzic <jasminah@usit.uio.no>
2005-11-24 06:08:37 +0000

    Initial commit, new eDirectory-stuff

---
+++ Cerebrum/modules/no/hia/EdirLDAP.py
@@ -0,0 +1,205 @@
+# -*- coding: iso-8859-1 -*-
+#
+# Copyright 2003, 2004, 2005 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+import ldap
+
+class LDAPConnection:
+
+    def __init__(self, host=None, port=389,
+                 binddn=None, password=None,
+                 scope= 'SUB', db):
+
+	self.db = db
+        self.__host = host or cereconf.NW_LDAPHOST
+        self.__port = port or cereconf.NW_LDAPPORT
+        self.__binddn = binddn or cereconf.NW_ADMINUSER
+        
+	if not password:
+	    user = cereconf.NW_ADMINUSER.split(',')[:1][0]
+	    self.__password = self.db._read_password(cereconf.NW_LDAPHOST,
+                                                     user)
+	else:
+	    self.__password = password
+            
+        if scope.upper() == 'SUB':
+            self.__scope = ldap.SCOPE_SUBTREE
+        elif scope.upper() == 'ONE':
+            self.__scope = ldap.SCOPE_ONE
+        else:
+            self.__scope = ldap.SCOPE_BASE
+
+        self.__ldap_connection_handle = None
+    
+    def __connect( self, host, binddn, password, port, crypted=True):
+        """Try to establish a (per default encrypted) connection
+           to a given host."""
+
+        ldap.set_option(ldap.OPT_PROTOCOL_VERSION, ldap.VERSION3)
+
+        if crypted:  
+            try:
+                if cereconf.TLS_CACERT_FILE is not None:
+                    ldap.set_option(ldap.OPT_X_TLS_CACERTFILE,
+                                    cereconf.TLS_CACERT_FILE)
+                else:
+                    if cereconf.TLS_CACERT_DIR is not None:
+                        ldap.set_option(ldap.OPT_X_TLS_CACERTDIR,
+                                        cereconf.TLS_CACERT_DIR)
+            except ldap.LDAPError:
+                logger.error("Could not find appropriate certificate, can't start encrypted connection!")
+                return False
+             try:
+                 handle = ldap.open(host)
+            except ldap.LDAPError:
+                logger.error( "Could not do ldap.open to %s" % host)
+                return False
+            try:
+                handle.start_tls_s()
+                logger.info("TLS connection established to %s" % host)
+            except ldap.LDAPError:
+                logger.error("Could not open TLS-connection to %s" % host)
+                return False
+            try:
+                handle.simple_bind_s(binddn,password)
+                logger.debug("Successfully binded '%s' to '%s'" % (binddn, host))
+            except ldap.LDAPError:
+                logger.info( "Could not bind '%s' to '%s'." % (binddn, host))
+                return False
+            return handle
+        else:
+            try:
+                handle.simple_bind_s(binddn, password)
+                logger.info("Opened unencrypted connection to %s" % host)
+            except:
+                logger.info("Could not open unencrypted connection to %s" % host)
+                return False
+            return handle
+    
+    def __unbind(self):
+        """Close connection to a host"
+	self.__ldap_connection_handle.unbind()
+	self.__ldap_connection_handle = None
+
+    def __search(self, handle, basedn, filter,
+                 scope=ldap.SCOPE_SUBTREE, attrs=None):
+        if not handle:
+            return False
+        return handle.search_s(basedn, scope, filter, attrlist=attrs)
+    
+    def __create(self, handle, dn, attrs):
+        if not handle:
+            return False
+        handle.add_s(dn, attrs)
+        
+    def __delete(self, handle, dn):
+        if not handle:
+            return False
+        handle.delete_s(dn)
+    
+    def __rename(self, handle, olddn, newdn, delete_olddn=1):
+        if not handle:
+            return False
+        handle.modrdn_s(olddn, newdn, delete_olddn)
+        
+    def __modify(self, handle, dn, attrs):
+        if not handle:
+            return False
+        handle.modify_s(dn, attrs)
+
+    def get_objects(self, basedn, filter, attrlist=None):
+        if not self.__ldap_connection_handle:
+            return None
+        res = self.__search(self.__ldap_connection_handle, basedn, filter, self.__scope, attrlist)
+        return res
+    
+    def create_object(self, dn, attrlist):
+        ok = True
+        if not self.__ldap_connection_handle:
+            ok = False
+        else:
+            self.__create(self.__ldap_connection_handle, dn, attrs)
+        return ok
+    
+    def delete_object(self, dn):
+        ok = True
+        if not self.__ldap_connection_handle:
+            ok = False
+        else:
+            self.__delete( self.__ldap_connection_handle, dn )
+        return ok
+
+    def rename_object(self, olddn, newdn, del_olddn=True ):
+        if not self.__ldap_connection_handle:
+            return False
+        if del_olddn:
+            self.__rename(self.__ldap_connection_handle, olddn, newdn)
+        else:
+            self.__rename(self.__ldap_connection_handle, olddn, newdn, 0)
+        return ok
+            
+    def add_attributes(self, dn, newattrs):
+        ok = True
+        if not self.__ldap_connection_handle:
+            ok = False
+        else:
+            attrs = []
+            for type, value in newattrs:
+                attrs.append((ldap.MOD_ADD, type, value))
+                self.__modify(self.__ldap_connection_handle, dn, attrs)
+        return ok
+        
+    def modify_attributes(self, dn, changedattrs):
+        ok = True
+        if not self.__ldap_connection_handle:
+            ok = False
+        else:
+            attrs = []
+            for type, value in changedattrs:
+                attrs.append((ldap.MOD_REPLACE, type, value))
+                self.__modify(self.__ldap_connection_handle, dn, attrs)
+        return ok
+
+    def raw_modify_attributes(self, dn, changedattrs):
+        ok = True
+        if not self.__ldap_connection_handle:
+            ok = False
+        else:
+            self.__modify(self.__ldap_connection_handle, dn, changedattrs)
+        return ok
+
+        
+    def delete_attributes(self, dn, delattrs):
+        ok = True
+        if not self.__ldap_connection_handle:
+            ok = False
+        else:
+            attrs = []
+            for type,value in delattrs:
+                attrs.append((ldap.MOD_DELETE, type, value))
+                self.__modify(self.__ldap_connection_handle, dn, attrs)
+        return ok
+        
+    def modify_object(self, dn, attrs):
+        ok = True
+        if not self.__ldap_connection_handle:
+            ok = False
+        else:
+            self.__modify(self.__ldap_connection_handle, dn, attrs)
+        return ok
---
+++ Cerebrum/modules/no/hia/EdirUtils.py
@@ -0,0 +1,178 @@
+# -*- coding: iso-8859-1 -*-
+#
+# Copyright 2003, 2004, 2005 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+import cerebrum_path
+import cereconf
+
+import ldap
+
+from mx import DateTime
+from Cerebrum.Utils import Factory
+from Cerebrum import Entity
+from Cerebrum import Errors
+from Cerebrum import QuarantineHandler
+from Cerebrum.extlib import logging
+from Cerebrum.modules.no.hia import NWLdap
+
+
+class EDirUtils:
+
+    def __init__(self, db):
+        self.db = db
+        self.constants = Factory.get('Constants')(self.db)
+        self.ldap_handle = NWLdap.LDAPConnection(self.db)
+        self.logger = Factory.get_logger('cronjob')(self.db)
+
+    def add_ldap_object(object_dn, attrlist):
+        for kind, value in attrs:
+	    if kind == 'userPassword':
+		attrlist[kind] = 'xxxxxxxx'
+        try:
+            self.ldap_handle.create_object(object_dn, attrlist)
+        except ldap.LDAPError:
+            logger.warn("Could not add object %s." % object_dn)
+
+    def delete_ldap_object(object_dn):
+        try:
+            self.ldap_handle.delete_object(object_dn)
+        except ldap.LDAPError:
+            logger.warn("Could not delete %s." % object_dn)
+
+    def attr_add_ldap_object(object_dn, attrlist):
+        attr_str = string.join(attrlist.values(),', ')
+        try:
+            self.ldap_handle.add_attributes(object_dn, attrlist)
+        except ldap.LDAPError:
+            logger.warn("Could not add attr %s for %s." % (object_dn,
+                                                           attr_str))
+            
+    def attr_del_ldap_object(object_dn, attrlist):
+        attr_str = string.join(attrlist.values(),', ')
+        try:
+            self.ldap_handle.delete_attributes(object_dn, attrlist)
+        except ldap.LDAPError:
+            logger.warn("Could not delete attr %s for %s." % (attr_str, object_dn))
+
+    def attr_mod_ldap_object(object_dn, attrlist):
+        for kind, value in attrlist:
+            if kind == 'userPassword':
+                attrlist[kind] = 'xxxxxxxx'
+        attr_str = string.join(attrlist.values(),', ')
+        try:
+            self.ldap_handle.modify_attributes(object_dn, attrlist)
+        except ldap.LDAPError:
+            logger.warn("Could not modify object %s with %s." % (object_dn,
+                                                                 attr_str))
+
+    def person_set_name(account_name, name_first, name_last, name_full):
+
+        ldap_object = self._find_object(account_name,
+                                        object_class='person')
+
+        attrs = {'givenName':name_first,
+                 'sn':name_last,
+                 'fullName':name_full}
+
+        if ldap_object:
+            (ldap_user, ldap_attrs) = ldap_object[0]
+            for k, v in attrs.iteritems():
+                ldap_update = [((k, [v]))]
+                if a in ldap_attrs.keys():
+                    self.attr_mod_ldap_object(ldap_user, ldap_update)
+                else:
+                    self.attr_add_ldap_object(ldap_user, ldap_update)
+        else:
+            return
+
+    def _find_object(object_name, object_class=None, attrlist=None):
+        tmp_str = ""
+        if object_class == 'person':
+            tmp_str = "(objectClass=inetOrgPerson)"
+        elif object_class == 'group':
+            tmp_str = "(objectclass=group)"
+        else:
+            self.logger.error("No such object class %s" % object_class)
+            return None
+        search_str = "(&(cn=%s)%s)" % (object_name, tmp_str)
+        ldap_object = self.ldap_handle.get_objects(cereconf.NW_LDAP_ROOT,
+                                                   search_str, attrlist)
+        return ldap_object
+    
+    def get_pq_balance(account_name):
+        ldap_object = self._find_object(account_name,
+                                        object_class='person',
+                                        attrlist = ['accountBalance',
+                                                    'allowUnlimitedCredit'])
+        if ldap_object:
+            (ldap_user, ldap_attr) = ldap_object[0]
+            if ldap_attr['allowUnlimitedCredit'] == True:
+                return False
+            else:
+                return ldap_attr['accountBalance'][0]
+        
+    def set_pq_info(account_name, pquota=cereconf.NW_PR_QUOTA):
+        ldap_object = self._find_object(account_name,
+                                        object_class='person')
+        if ldap_object:
+            (ldap_object_dn, ldap_attr) = ldap_object[0]
+            if 'accountBalance' in ldap_attr.keys():
+                tot = int(ldap_attr['accountBalance'][0]) + pquota
+                self.attr_mod_ldap_object(ldap_object_dn, [('accountBalance',
+                                                            tot)])
+            else:
+                self.attr_mod_ldap_object(ldap_object_dn, [('accountBalance',
+                                                            pquota)])
+                
+    def set_description(object_name, object_class='person', description):
+        desc = []
+        ldap_object = self._find_object(object_name,
+                                        object_class)
+
+        if ldap_object:
+            desc = self.get_description(object_name, object_class)
+
+        if len(desc) <= 4:
+            desc.append(description)
+        else:
+            temp = desc[0]
+            desc = desc[1:]
+            desc.insert(temp, 0)
+            desc.append(description)
+
+        (ldap_object_dn, attrs) = ldap_object[0]
+        self.attr_mod_ldap_object(ldap_object_dn, [('description',
+                                                    desc)])
+        
+    def get_description(object_name, object_class='person'):
+        desc = []
+        
+        if object_class == 'person':
+            ldap_object = self._find_object(object_name,
+                                            object_class='person',
+                                            attrlist=['decription',])
+        elif object_class == 'group':
+            ldap_object = self._find_object(object_name,
+                                            object_class='group',
+                                            attrlist=['decription',])
+
+        if ldap_object:
+            (foo, ldap_attr) = ldap_object[0]
+            desc = ldap_attr['description']
+        return desc
