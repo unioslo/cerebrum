0ac481c42a9b56576de46fefdcd9f1c11098a3d7
Cerebrum Arch <cerebrum-commits@usit.uio.no>
2005-12-08 14:59:39 +0000

    Changes from arch/CVS synchronization

--- Cerebrum/modules/no/Indigo/Constants.py
+++ Cerebrum/modules/no/Indigo/Constants.py
@@ -110,3 +110,4 @@ class Constants(Constants.Constants):
     account_test = _AccountCode('test', 'Testkonto')
     account_pvare = _AccountCode('programvare','Kurskonto')
 
+# arch-tag: 82109000-67f8-11da-8454-871df49a59c9
