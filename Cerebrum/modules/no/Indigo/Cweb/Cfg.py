4cd0f7d0640c0d52d23c71390a02a5d9ad7b3523
Jasmina Hodzic <jasminah@usit.uio.no>
2005-12-29 12:33:34 +0000

    We want to use cereconf-variables for CWEB templates and logs for now.

--- Cerebrum/modules/no/Indigo/Cweb/Cfg.py
+++ Cerebrum/modules/no/Indigo/Cweb/Cfg.py
@@ -1,8 +1,10 @@
 # TBD: Where should we put these variables.  Is cereconf sufficient,
 # or do we need more dynamic behaviour?
+import cerebrum_path
+import cereconf
 
-__root_dir = '/local/home/rune/usit/cerebrum/contrib/no/Indigo/web'
-template_dir = __root_dir+'/templates'
+__root_dir = cereconf.CWEB_LOG_DIR
+template_dir = cereconf.CWEB_TPL_DIR+'/templates'
 state_file = __root_dir+'/state.db'
 log_file = __root_dir+'/www.log'
 bofh_server_url = 'http://127.0.0.1:8008'
