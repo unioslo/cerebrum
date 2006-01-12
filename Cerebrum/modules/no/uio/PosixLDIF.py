d73d0650341c2b63f9259e2698c8848cc21b3b4d
Hallvard Breien Furuseth <hallvard@usit.uio.no>
2006-01-12 09:24:59 +0000

    Rename SambaMixin to PosixLDIF_UiOMixin: Add attribute eduPersonAffiliation and class uioAccountObject to user entries.
    Requires OrgLDIF.py 1.10, PosixUser.py 1.71, cereconf.CLASS_POSIXLDIF update.

--- Cerebrum/modules/no/uio/PosixLDIF.py
+++ Cerebrum/modules/no/uio/PosixLDIF.py
@@ -1,5 +1,5 @@
 # -*- coding: iso-8859-1 -*-
-# Copyright 2004 University of Oslo, Norway
+# Copyright 2004-2006 University of Oslo, Norway
 #
 # This file is part of Cerebrum.
 #
@@ -18,28 +18,50 @@
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 from Cerebrum.modules.PosixLDIF import PosixLDIF
+from Cerebrum.Utils import Factory
 
-class SambaMixin(PosixLDIF):
-    """
-    Mixin class for sambaAccount. 
-    Only partial sambaAccount to suppport NT-password used for freeradius 
-    """
+class PosixLDIF_UiOMixin(PosixLDIF):
+    """PosixLDIF mixin class providing functionality specific to UiO."""
+
+    def init_user(self, *args, **kwargs):
+	# Prepare to include eduPersonAffiliation, taken from OrgLDIF.
+	self.org_ldif = Factory.get('OrgLDIF')(self.db, self.logger)
+	self.org_ldif.init_eduPersonAffiliation_lookup()
+	return self.__super.init_user(*args, **kwargs)
 
     def auth_methods(self, auth_meth= None):
+	# Also fetch NT password, for attribute sambaNTPassword.
 	meth = self.__super.auth_methods(auth_meth)
-	#meth = super(UserLdif, self).auth_methods() or []
 	meth.append(int(self.const.auth_type_md4_nt))
 	return meth
 
     def update_user_entry(self, account_id, entry, row):
+	# Add eduPersonAffiliation and sambaNTPassword
+
+	# eduPersonAffiliation (taken from OrgLDIF)
+	owner_id = int(row['owner_id'])
+	added = self.org_ldif.affiliations.get(owner_id)
+	if added:
+	    added = self.org_ldif.attr_unique(self.org_ldif.select_list(
+		self.org_ldif.eduPersonAff_selector, owner_id, added))
+	    if added:
+		entry['eduPersonAffiliation'] = added
+
+	# sambaNTPassword (used by FreeRadius)
 	try:
 	    hash = self.auth_data[account_id][int(self.const.auth_type_md4_nt)]
 	except KeyError:
 	    pass
-        else:
+	else:
 	    entry['sambaNTPassword'] = (hash,)
+	    # TODO: Remove sambaSamAccount and sambaSID after Radius-testing
 	    entry['objectClass'].append('sambaSamAccount')
 	    entry['sambaSID'] = entry['uidNumber']
+	    added = True
+
+	# Object class which allows the additional attributes
+	if added:
+	    entry['objectClass'].append('uioAccountObject')
 	return self.__super.update_user_entry(account_id,entry, row)
 
 # arch-tag: e2f10a69-807b-4b18-8893-530a4cae1a38
