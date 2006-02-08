011be8859f48ea76783e8b0c2f83768c519a838c
Kjetil Homme <kjetilho@ulrik.uio.no>
2006-02-08 22:22:19 +0000

    * write_db doesn't return a value, simplified code. * populate_trait now returns 'INSERT' or 'UPDATE' depending on prior   state in database. * list_traits now accepts strval_like to do pattern matching against   strval (requires Database.py 1.42), and fetchall to return a list   of all values rather than an iterator.

--- Cerebrum/modules/EntityTrait.py
+++ Cerebrum/modules/EntityTrait.py
@@ -62,9 +62,8 @@ class EntityTrait(Entity):
         self.__traits = {}
         self.__trait_updates = {}
 
-
     def write_db(self):
-        super_return = self.__super.write_db()
+        self.__super.write_db()
 
         def pickle_fixup(params):
             """pickle can't handle datetime objects"""
@@ -97,8 +96,6 @@ class EntityTrait(Entity):
                 self._db.log_change(self.entity_id, self.const.trait_add, None,
                                     change_params=params)
         self.__trait_updates = {}
-        return super_return
-
 
     def delete_trait(self, code):
         """Remove the entity's trait identified by code value."""
@@ -120,19 +117,22 @@ class EntityTrait(Entity):
         self._db.log_change(self.entity_id, self.const.trait_del, None)
         del self.__traits[code]
 
-
     def delete(self):
-        """To delete this entity we need to remove all traits first."""
+        """To delete this entity we need to remove all traits first.
+        Traits which have this entity as target_id are left alone, and
+        deletion will fail if any such trait exists.
 
+        """
         for code in self.get_traits():
             self.delete_trait(code)
         self.__super.delete()
 
-
     def populate_trait(self, code, target_id=None, date=None, numval=None,
                        strval=None):
-        """Adds or updates an entity trait."""
+        """Adds or updates an entity trait.  Returns 'INSERT' or
+        'UPDATE'.
 
+        """
         code = _EntityTraitCode(code)
         if code in self.get_traits():
             # If the trait is already listed in updates, it may be a
@@ -148,12 +148,13 @@ class EntityTrait(Entity):
                                'date': date,
                                'numval': numval,
                                'strval': strval}
-
+        return self.__trait_updates[code]
 
     def get_traits(self):
         """Returns a dict of traits associated with the current entity
-        keyed by the code constant."""
+        keyed by the code constant.
 
+        """
         if not self.__traits:
             for row in self.query("""
             SELECT entity_id, entity_type, code,
@@ -164,7 +165,6 @@ class EntityTrait(Entity):
                 self.__traits[_EntityTraitCode(row['code'])] = row.dict()
         return self.__traits
 
-
     def get_trait(self, trait):
         """Return the trait value (as a dict), or None."""
         traits = self.get_traits()
@@ -172,16 +172,17 @@ class EntityTrait(Entity):
             return None
         return traits.get(_EntityTraitCode(trait))
 
-
     def list_traits(self, code, target_id=NotSet, date=NotSet,
-                    numval=NotSet, strval=NotSet):
+                    numval=NotSet, strval=NotSet, strval_like=NotSet,
+                    fetchall=False):
         """Returns all the occurences of a specified trait, optionally
         filtered on values. To match SQL NULL, use None.  date should
-        be an mx.DateTime object."""
+        be an mx.DateTime object.  strval_like will apply DWIM case
+        sensitivity (see Utils.prepare_sql_pattern).
 
+        """
         # TBD: we may want additional filtering parameters, ie.
-        # date_before, date_after, numval_lt, numval_gt and
-        # strval_like
+        # date_before, date_after, numval_lt and numval_gt.
 
         def add_cond(col, value, normalise=False):
             if value is None:
@@ -198,14 +199,18 @@ class EntityTrait(Entity):
         add_cond("target_id", target_id)
         add_cond("date", date)
         numval = add_cond("numval", numval, normalise=int)
-        strval = add_cond("strval", strval, normalise=str)
+        if strval_like is not NotSet:
+            expr, strval = self._db.sql_pattern('strval', strval_like)
+            conditions.append(expr)
+        else:
+            # strval_like has precedence over strval
+            strval = add_cond("strval", strval, normalise=str)
+        where = " AND ".join(conditions)
 
         # Return everything but entity_type, which is implied by code
-
-        where = " AND ".join(conditions)
         return self.query("""
         SELECT entity_id, code, target_id, date, numval, strval
         FROM [:table schema=cerebrum name=entity_trait]
-        WHERE """ + where, locals())
+        WHERE """ + where, locals(), fetchall=fetchall)
 
 # arch-tag: a834dc20-402d-11da-9b87-c30b16468bb4
