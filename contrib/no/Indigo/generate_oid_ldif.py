4029dd5d5ee3b2587db9531dedc5d2f08063a54a
Mathias Meisfjordskar <mathiasm@ulrik.uio.no>
2006-02-17 16:06:53 +0000

    Added code to support the latest specs.

--- contrib/no/Indigo/generate_oid_ldif.py
+++ contrib/no/Indigo/generate_oid_ldif.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 
-# Copyright 2005 University of Oslo, Norway
+# Copyright 2006 University of Oslo, Norway
 #
 # This file is part of Cerebrum.
 #
@@ -29,29 +29,91 @@ from Cerebrum.Utils import SimilarSizeWriter
 from Cerebrum.Constants import _PersonAffStatusCode
 
 def get_account_info():
-    global uname2mailaddr, p_id2name, a_id2auth, a_id2owner, const2str
+    global uname2mailaddr, p_id2name, p_id2fnr, a_id2auth, a_id2p_id, p_id2a_id, \
+           ou_id2name, const2str
     
     uname2mailaddr = ac.getdict_uname2mailaddr()
     p_id2name = p.getdict_persons_names(source_system=co.system_cached,
                                         name_types=(co.name_first,co.name_last))
+    p_id2fnr = dict()
+    for row in p.list_external_ids(id_type=co.externalid_fodselsnr):
+        p_id2fnr[int(row['entity_id'])] = row['external_id']
+        
     a_id2auth = dict()
-    for row in ac.list_account_authentication(auth_type=co.auth_type_md5_crypt):
-        a_id2auth[int(row['account_id'])] = (row['entity_name'], row['auth_data'])
+    for row in ac.list_account_authentication(auth_type=(co.auth_type_md4_nt,
+                                                         co.auth_type_plaintext,
+                                                         co.auth_type_ssha)):
+        aid = int(row['account_id'])
+        if row['method'] is None:
+            continue
+        a_id2auth.setdefault(aid, [])
+        if len(a_id2auth[aid]) <> 2:
+            a_id2auth[aid].insert(0, row['entity_name'])
+            a_id2auth[aid].insert(1, dict())
+        a_id2auth[aid][1].setdefault(int(row['method']), row['auth_data'])
 
-    a_id2owner = dict()
+    a_id2p_id = dict()
+    p_id2a_id = dict()
     for row in ac.list():
-        a_id2owner[row['account_id']] = (int(row['owner_type']),
+        a_id2p_id[row['account_id']] = (int(row['owner_type']),
                                          row['owner_id'])
+        if int(row['owner_type']) == int(co.entity_person):
+            p_id2a_id[int(row['owner_id'])] = int(row['account_id'])
+    ou_id2name = dict()
+    for row in ou.search():
+        ou_id2name[int(row['ou_id'])] = str(row['acronym'])
 
     const2str = dict()
-    const2str[int(co.affiliation_ansatt)] = "cn=ANSATTE"
-    const2str[int(co.affiliation_elev)] = "cn=ELEVER"
+    const2str[int(co.affiliation_ansatt)] = "ANSATTE"
+    const2str[int(co.affiliation_elev)] = "ELEVER"
     
     
+def process_txt_file(file):
+    users_ou = dict()
 
+    for row in p.list_affiliations():
+        id = int(row['person_id'])
+        aff = int(row['affiliation'])
+        ou_id = int(row['ou_id'])
+        if not p_id2a_id.has_key(id):
+            # no user
+            logger.warning("No user found for '%s'" % id)
+            continue
+        a_id = p_id2a_id[id]
+        if not (a_id2auth.has_key(a_id) and a_id2auth[a_id][0]):
+            # No username
+            logger.warning("No username found for '%s'" % id)
+            continue
+        if not p_id2fnr.has_key(id):
+            logger.warning("Fnr not found for '%s'" % id)
+            continue
+        uname = a_id2auth[a_id][0]
+        pwd = ""
+        if a_id2auth[a_id][1] is not None:
+            pwd = a_id2auth[a_id][1][int(co.auth_type_plaintext)]
+        if not (p_id2name.has_key(id) or \
+                p_id2name[id].get(int(co.name_first)) or \
+                p_id2name[id].get(int(co.name_last))):
+            logger.warning("Names not found for '%s'" % id)
+            continue
+        if not ou_id2name.has_key(ou_id):
+            logger.warning("OU name not found for '%s'" % ou_id)
+            continue
+        p_names = p_id2name[id]
+        first = p_names[int(co.name_first)]
+        last = p_names[int(co.name_last)]
+        if not uname2mailaddr.has_key(uname):
+            logger.warning("Mail-addr not found for '%s', '%s'" % (id, uname))
+            continue
+        
+        file.write("%s:%s:%s:%s:%s:%s:%s\n" % (p_id2fnr[id], uname, pwd,
+                                             uname2mailaddr[uname],
+                                             ou_id2name[ou_id], first, last))
+        users_ou.setdefault(ou_id2name[ou_id], {}).setdefault(aff, []).append(uname)
+    return users_ou
+        
 
 def process_users(affiliation, file):    
-
     known_dns = dict()
     
     for row in ac.list_accounts_by_type(affiliation=int(affiliation)):
@@ -61,16 +123,19 @@ def process_users(affiliation, file):
             logger.warning("No username found for '%s'" % id)
             continue
         uname = a_id2auth[id][0]
+        plain = "*notfound"
+        md4 = "*notfound"
+        ssha = "*notfound"
         if a_id2auth[id][1] is not None:
-            pwd = a_id2auth[id][1]
-        else:
-            pwd = "*notfound"
+            plain = a_id2auth[id][1][int(co.auth_type_plaintext)]
+            md4 = a_id2auth[id][1][int(co.auth_type_md4_nt)]
+            ssha = a_id2auth[id][1][int(co.auth_type_ssha)]
         first = last = None
-        if not a_id2owner.has_key(id):
+        if not a_id2p_id.has_key(id):
             logger.warning("No owner-ID and type for '%s'" % id)
             continue
-        if a_id2owner[id][0] == int(co.entity_person):
-            p_names = p_id2name[a_id2owner[id][1]]
+        if a_id2p_id[id][0] == int(co.entity_person):
+            p_names = p_id2name[a_id2p_id[id][1]]
             if not (p_names.get(int(co.name_first)) or \
                     p_names.get(int(co.name_last))):
                 logger.warning("Names not found for '%s'" % id)
@@ -79,6 +144,9 @@ def process_users(affiliation, file):
             last = p_names[int(co.name_last)]
         else:
             logger.warning("Wrong owner-type for '%s'" % id)
+            continue
+        if not p_id2fnr.has_key(a_id2p_id[id][1]):
+            logger.warning("Fnr not found for '%s'" % id)
             continue
         # Skip duplicates
         if known_dns.has_key(uname):
@@ -90,11 +158,21 @@ def process_users(affiliation, file):
                "givenname: %s" % first]
         if uname2mailaddr.has_key(uname):
             txt += ["mail: %s" % uname2mailaddr[uname]]
-        txt += ["orcldefaultprofilegroup: %s,cn=groups,dc=ovgs,dc=no" % const2str[int(affiliation)], 
-                "orcltimezone: Europe/Oslo", 
+        txt += ["orcldefaultprofilegroup: cn=%s,cn=groups,dc=ovgs,dc=no" % const2str[int(affiliation)], 
+                "orcltimezone: Europe/Oslo",
+                "objectclass: top",
+                "objectclass: person",
+                "objectclass: inetorgperson",
+                "objectclass: organizationalperson",
+                "objectclass: orcluser",
+                "objectclass: orcluserv2",
+                "orclsamaccountname: %s" % uname,
                 "sn: %s" % last, 
                 "uid: %s" % uname, 
-                "userpassword: %s" % pwd,
+                "userpassword: %s" % ssha,
+                "ofkpassword: %s" % md4,
+                "userpasswordct: %s" % plain,
+                "fnr: %s" % p_id2fnr[a_id2p_id[id][1]],
                 "\n"]
         file.write('\n'.join(txt))
     return known_dns.keys()
@@ -110,15 +188,29 @@ def process_prof_group(dn, users, file):
         txt += ["uniquemember: cn=%s,cn=users,dc=ovgs,dc=no" % u]
     file.write('\n'.join(txt))
 
+
+def process_aff_groups(users, file):
+    for ou in users.keys():
+        for aff in users[ou].keys():
+            txt = ["dn: cn=%s:%s,cn=groups,dc=ovgs,dc=no" % (ou, const2str[int(aff)]),
+                   "objectclass: top",
+                   "objectclass: groupOfUniqueNames",
+                   "objectclass: orclGroup"]
+            for u in users[ou][aff]:
+                txt += ["uniquemember: cn=%s,cn=users,dc=ovgs,dc=no" % u]
+            file.write('\n'.join(txt))
+        file.write('\n\n')
+
+
 def process_groups(spread, file):
     g = Factory.get('Group')(db)
     for row in g.search(spread=int(spread)):
         id = row['group_id']
-        name = row['group_id']
+        name = row['name']
         desc = row['description']
         g.clear()
         g.find(id)
-        txt = ["dn: cn=%s,cn=groups,dc=ovgs,dc=no" % name,
+        txt = ["dn: cn=%s,cn=groups,dc=ovgs,dc=no" % desc,
                "description: %s" % desc,
                "displayname: %s" % desc,
                "objectclass: top",
@@ -141,7 +233,7 @@ def process_groups(spread, file):
         
 
 def main():
-    global db, co, ac, p, logger
+    global db, co, ac, p, ou, logger
 
     logger = Factory.get_logger("cronjob")
     
@@ -149,45 +241,61 @@ def main():
     co = Factory.get('Constants')(db)
     ac = Factory.get('Account')(db)
     p = Factory.get('Person')(db)
+    ou = Factory.get('OU')(db)
 
-    path = "/cerebrum/dumps/OID"
+    txt_path = "/cerebrum/dumps/txt"
+    oid_path = "/cerebrum/dumps/OID"
     
     options, rest = getopt.getopt(sys.argv[1:],
-                                  "p:",
-                                  ["path=",])
+                                  "o:t:",
+                                  ["oid-path=", "txt-path="])
     for option, value in options:
-        if option in ("-p", "--path"):
-            path = value
+        if option in ("-o", "--oid-path"):
+            oid_path = value
+        elif option in ("-t", "--txt-path"):
+            txt_path = value
 
     # Load dicts with misc info.
     get_account_info()
 
+    # Dump OFK info 
+    f = SimilarSizeWriter("%s/ofk.txt" % txt_path, "w")
+    f.set_size_change_limit(10)
+    users = process_txt_file(f)
+    f.close()
+
+    # Dump info about OU groups
+    f = SimilarSizeWriter("%s/affiliation_groups_oid.ldif" % oid_path, "w")
+    f.set_size_change_limit(10)
+    process_aff_groups(users, f)
+    f.close()
+
     # Dump info about users with co.affiliation_ansatt
-    f = SimilarSizeWriter("%s/ans_user_oid.ldif" % path, "w")
+    f = SimilarSizeWriter("%s/ans_user_oid.ldif" % oid_path, "w")
     f.set_size_change_limit(10)
     users = process_users(co.affiliation_ansatt, f)
     f.close()
 
     # Make a group out of these users
-    f = SimilarSizeWriter("%s/ans_group_oid.ldif" % path, "w")
+    f = SimilarSizeWriter("%s/ans_group_oid.ldif" % oid_path, "w")
     f.set_size_change_limit(10)
     process_prof_group("ANSATTE", users, f)
     f.close()
 
     # Dump info about users with co.affiliation_elev 
-    f = SimilarSizeWriter("%s/elev_user_oid.ldif" % path, "w")
+    f = SimilarSizeWriter("%s/elev_user_oid.ldif" % oid_path, "w")
     f.set_size_change_limit(10)
     users = process_users(co.affiliation_elev, f)
     f.close()
     
     # Make a group out of these users
-    f = SimilarSizeWriter("%s/elev_group_oid.ldif" % path, "w")
+    f = SimilarSizeWriter("%s/elev_group_oid.ldif" % oid_path, "w")
     f.set_size_change_limit(10)
     process_prof_group("ELEVER", users, f)
     f.close()
             
     # Make and populate groups with spread spread_oid_grp
-    f = SimilarSizeWriter("%s/group_oid.ldif" % path, "w")
+    f = SimilarSizeWriter("%s/group_oid.ldif" % oid_path, "w")
     f.set_size_change_limit(10)
     process_groups(co.spread_oid_grp, f)
     f.close()
