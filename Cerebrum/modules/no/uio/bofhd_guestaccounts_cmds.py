27a1877ecc1d29f9b59b160ea488d2dca0556b35
Kjetil Homme <kjetilho@ulrik.uio.no>
2006-02-24 15:57:26 +0000

    lots of small fixes and cleanups

--- Cerebrum/modules/no/uio/bofhd_guestaccounts_cmds.py
+++ Cerebrum/modules/no/uio/bofhd_guestaccounts_cmds.py
@@ -28,11 +28,10 @@ from Cerebrum.Utils import Factory
 from Cerebrum.modules.bofhd.cmd_param import *
 from Cerebrum.modules import PosixUser
 from Cerebrum.modules import PosixGroup
-from Cerebrum.modules.bofhd.errors import CerebrumError
-from Cerebrum.modules.bofhd.errors import PermissionDenied
+from Cerebrum.modules.bofhd.errors import CerebrumError, PermissionDenied
 from Cerebrum.modules.bofhd.auth import BofhdAuth
-from Cerebrum.modules.no.uio import bofhd_guestaccounts_utils
-    
+from Cerebrum.modules.no.uio.bofhd_guestaccounts_utils \
+     import BofhdUtils, GuestAccountException    
 
 class BofhdExtension(object):
     all_commands = {}
@@ -41,15 +40,14 @@ class BofhdExtension(object):
     def __init__(self, server):
         self.server = server
         self.db = server.db
-        self.const = Factory.get('Constants')(self.db)
-        self.bgu = bofhd_guestaccounts_utils.BofhdUtils(server)
+        self.util = server.util
+        self.co = Factory.get('Constants')(self.db)
+        self.bgu = BofhdUtils(server)
         self.ba = BofhdAuth(self.db)
         self._cached_client_commands = Cache.Cache(mixins=[Cache.cache_mru,
                                                            Cache.cache_slots,
                                                            Cache.cache_timeout],
-                                                   size=500,
-                                                   timeout=60*30)
-
+                                                   size=500, timeout=60*30)
 
     def get_help_strings(self):
         group_help = {}
@@ -66,10 +64,12 @@ class BofhdExtension(object):
             }
 
         arg_help = {
-            'release_type': ['release_type', 'Enter release type',
-                             """Enter either a guest user name or a range of names.
-E.g. guest007 or guest010-guest040."""],
-            'guest_owner_group' : ['guest_owner_group', 'Enter name of owner group']
+            'release_type':
+            ['release_type', 'Enter release type',
+             "Enter a guest user name or a range of names, e.g., guest007 "
+             "or guest010-040."],
+            'guest_owner_group':
+            ['guest_owner_group', 'Enter name of owner group']
             }
         return (group_help, command_help, arg_help)
 
@@ -95,17 +95,26 @@ E.g. guest007 or guest010-guest040."""],
         return commands
 
     
-    def not_good(self, end_date, today, max_date):
-        m = re.match('(\d{4})-(\d\d)-(\d\d)', end_date)
-        if not m:
-            return True
-        else:
-            d = DateTime.Date(int(m.group(1)), int(m.group(2)), int(m.group(3)))
-            if d < today or d > max_date:
-                return True
-        return False
+    def time_in_interval(self, date, min_date=None, max_date=None):
+        """Return date as an DateTime object if it is inside the
+        interval defined by min_date and max_date (both DateTime
+        objects).  Raise CerebrumError otherwise.
 
-        
+        """
+        if isinstance(date, str):
+            m = re.match('(\d{4})-(\d\d?)-(\d\d?)', date)
+            if not m:
+                raise CerebrumError, "Couldn't parse date (%s)" % date
+            date = DateTime.Date(int(m.group(1)), int(m.group(2)),
+                                 int(m.group(3)))
+        if min_date and date < min_date:
+            raise CerebrumError, ("Date can't be earlier than %s" %
+                                  str(min_date).split()[0])
+        if max_date and date > max_date:
+            raise CerebrumError, ("Date can't be later than %s" %
+                                  str(max_date).split()[0])
+        return date
+
     def user_create_guest_prompt_func(self, session, *args):
         #print "args = ", args
         all_args = list(args[:])
@@ -141,26 +150,22 @@ E.g. guest007 or guest010-guest040."""],
     def user_create_guest(self, operator, *args):
         "Create <nr> new guest users"
         nr, owner_group_name, filegroup, shell, home = args
-        owner_type = self.const.entity_group
-        owner_id = self._get_group(owner_group_name).entity_id
-        np_type = self.const.account_guest
-        group = self._get_group(filegroup, grtype="PosixGroup")
+        owner_type = self.co.entity_group
+        owner_id = self.util.get_target(owner_group_name,
+                                        default_lookup="group").entity_id
+        np_type = self.co.account_guest
+        group = self.util.get_target(filegroup, default_lookup="group")
         posix_user = PosixUser.PosixUser(self.db)
         shell = self._get_shell(shell)
-        if home[0] != ':':  # Hardcoded path
-            disk_id, home = self._get_disk(home)[1:3]
-        else:
-            if not self.ba.is_superuser(operator.get_entity_id()):
-                raise PermissionDenied("only superusers may use hardcoded path")
-            disk_id, home = None, home[1:]
+        disk_id = self._get_disk(home)[1]
         gecos = None
         expire_date = None
-        self.ba.can_create_user(operator.get_entity_id(), owner_id, disk_id)        
+        self.ba.can_create_user(operator.get_entity_id(), owner_id, disk_id)
 
         ret = []
-        ac = Factory.get('Account')(self.db)    
+        ac = Factory.get('Account')(self.db)
         # Find the names of the next <nr> of guest users
-        for uname in self.bgu.find_new_guestusernames(nr):
+        for uname in self.bgu.find_new_guestusernames(int(nr)):
             posix_user.clear()
             # Create the guest_users
             uid = posix_user.get_free_uid()
@@ -171,36 +176,33 @@ E.g. guest007 or guest010-guest040."""],
                                 expire_date=expire_date)            
             try:
                 posix_user.write_db()
-                for spread in cereconf.GUESTS_USER_SPREADS:
-                    posix_user.add_spread(self.const.Spread(spread))
                 homedir_id = posix_user.set_homedir(
-                    disk_id=disk_id, home=home,
-                    status=self.const.home_status_not_created)
-                posix_user.set_home(self.const.spread_uio_nis_user, homedir_id)
+                    disk_id=disk_id, status=self.co.home_status_not_created)
+                posix_user.set_home(self.co.spread_uio_nis_user, homedir_id)
                 # For correct ordering of ChangeLog events, new users
                 # should be signalled as "exported to" a certain system
                 # before the new user's password is set.  Such systems are
                 # flawed, and should be fixed.
-                passwd = posix_user.make_passwd(uname)
-                posix_user.set_password(passwd)
-                # And, to write the new password to the database, we have
-                # to .write_db() one more time...
-                posix_user.write_db()
+                for spread in cereconf.GUESTS_USER_SPREADS:
+                    posix_user.add_spread(self.co.Spread(spread))
             except self.db.DatabaseError, m:
                 raise CerebrumError, "Database error: %s" % m
             # Add guest_trait and set quarantine
-            ac.clear()
-            ac.find_by_name(uname)
             today = DateTime.today()
-            ac.populate_trait(co.trait_guest_owner, target_id=None)
+            posix_user.populate_trait(self.co.trait_guest_owner, target_id=None)
+            # The password must be set _after_ the trait, or else we
+            # won't store it using the 'PGP-guest_acc' method.
+            passwd = posix_user.make_passwd(uname)
+            posix_user.set_password(passwd)
             #self.logger.debug("Set guest trait for %s" % uname)
-            ac.add_entity_quarantine(co.quarantine_generell, operator,
-                                     "Available guest user.", today.date) 
+            posix_user.add_entity_quarantine(self.co.quarantine_generell,
+                                             operator.get_entity_id(),
+                                             "Available guest user",
+                                             today.date) 
             #self.logger.debug("Set quarantine for %s" % uname)
-            ac.write_db()
+            posix_user.write_db()
             ret.append((uname, uid))
         return "OK, created guest_users:\n %s " % self._pretty_print(ret)
-
 
     def user_request_guest_prompt_func(self, session, *args):
         all_args = list(args[:])
@@ -208,26 +210,12 @@ E.g. guest007 or guest010-guest040."""],
             return {'prompt': 'How many guest users?', 'default': '1'}
         nr_of_guests = int(all_args[0])
         all_args.pop(0)
-        # try:
-        #     nr_of_guests = int(all_args[0])
-        #     all_args.pop(0)
-        # except ValueError:
-        #     all_args.pop(0)  # If not an integer. Ask again
-        #     if not all_args:
-        #         return {'prompt': 'How many guest users?', 'default': '1'}        
         # Date checking
-        today = DateTime.today()
-        default_date = today + DateTime.RelativeDateTime(days=cereconf.GUESTS_DEFAULT_PERIOD)
-        max_date = today + DateTime.RelativeDateTime(days=cereconf.GUESTS_MAX_PERIOD)
+        default_date = DateTime.today() + \
+                       DateTime.RelativeDateTime(days=cereconf.GUESTS_DEFAULT_PERIOD)
         if not all_args:
             return {'prompt': 'Enter end date',
                     'default': default_date.date}
-        # Don't give up until a correct date is entered
-        # while self.not_good(all_args[0], today, max_date):
-        #     all_args.pop(0)
-        #     if not all_args:
-        #         return {'prompt': 'Enter end date (YYYY-MM-DD)',
-        #                 'default': default_date.date}    
         end_date = all_args.pop(0)            
         # Get group name. If name is valid is checked in the other method
         if not all_args:
@@ -242,31 +230,30 @@ E.g. guest007 or guest010-guest040."""],
         perm_filter='can_request_guests')
     def user_request_guest(self, operator, *args):
         nr, end_date, groupname = args
-        try:
-            # Fjern query_run_any=True når ting er klart
-            self.ba.can_request_guests(operator.get_entity_id(), groupname,
-                                       query_run_any=True)
-            owner = self._get_group(groupname)
-        except Errors.NotFoundError:
-            raise CerebrumError("No group with name %s" % groupname)
+        self.ba.can_request_guests(operator.get_entity_id(), groupname)
+        owner = self.util.get_target(groupname, default_lookup="group")
+        today = DateTime.today()
+        end_date = self.time_in_interval(end_date, today,
+            today + DateTime.RelativeDateTime(days=cereconf.GUESTS_MAX_PERIOD))
         try:
             user_list = self.bgu.request_guest_users(int(nr), end_date,
-                                                         self.const.entity_group,
-                                                         owner.entity_id)
+                                                     self.co.entity_group,
+                                                     owner.entity_id)
             for uname, e_id, passwd in user_list:
                 operator.store_state("new_account_passwd",
-                                     {'account_id': entity_id, 'password': passwd})
-            
+                                     {'account_id': e_id,
+                                      'password': passwd})
+
             ret = "OK, reserved guest users:\n%s\n" % self._pretty_print(user_list)
             ret += "Please use misc list_passwords to print or view the passwords."
             return ret
-        except bofhd_guestaccounts_utils.GuestAccountException, e:
+        except GuestAccountException, e:
             raise CerebrumError(str(e))
-        
-    
+
     # user release_guest [<guest> || <range>]+
     all_commands['user_release_guest'] = Command(
-        ('user', 'release_guest'), SimpleString(help_ref='release_type'))              
+        ('user', 'release_guest'),
+        SimpleString(help_ref='release_type', repeat=True))
     def user_release_guest(self, operator, *args):
         guests = []        
         if not args:
@@ -275,27 +262,26 @@ E.g. guest007 or guest010-guest040."""],
         for arg in args:
             if '-' in arg:
                 first, last = arg.split('-')
-                first = int(first[5:])
-                last = int(last[5:])
+                prefix = first.rstrip('0123456789')
+                first = int(first[len(prefix):])
+                last = int(last)
                 for i in range(first, last+1):
-                    guests.append('guest%s' % str(i).zfill(3))
+                    guests.append('%s%03d' % (prefix, i))
             else:
                 guests.append(arg)
 
         for guest in guests:
-            # Fjern query_run_any=True når ting er klart
             try:
                 owner_id = self.bgu.get_owner(guest)
-                owner_group = self._get_group(owner_id, idtype='id')
+                owner_group = self.util.get_target(owner_id)
                 self.ba.can_release_guests(operator.get_entity_id(),
-                                           owner_group.get_name(self.const.group_namespace),
-                                           query_run_any=True)
+                                           owner_group.group_name)
                 self.bgu.release_guest(guest, operator.get_entity_id())
             except Errors.NotFoundError:
                 raise CerebrumError("Could not find guest user with name %s" % guest)
             except PermissionDenied:
                 raise CerebrumError("No permission to release guest user %s" % guest)
-            except bofhd_guestaccounts_utils.GuestAccountException, e:
+            except GuestAccountException, e:
                 raise CerebrumError("Could not release guest user. %s" % str(e))
 
         return "OK, released guests:\n%s" % self._pretty_print(guests)
@@ -305,14 +291,12 @@ E.g. guest007 or guest010-guest040."""],
     all_commands['user_guests'] = Command(
         ('user', 'guests'), GroupName(help_ref="guest_owner_group"))
     def user_guests(self, operator, groupname): 
-        owner = self._get_group(groupname)
-        try:
-            tmp = self.bgu.list_guest_users(owner.entity_id)
-        except:
-            raise CerebrumError("Could not list guest users.")
-
+        owner = self.util.get_target(groupname, default_lookup="group")
+        users = self.bgu.list_guest_users(owner_id=owner.entity_id)
+        if not users:
+            return "No guest users are owned by %s" % groupname
         return "The following guest users is owned by %s:\n%s" % (
-            groupname, self._pretty_print(tmp))
+            groupname, self._pretty_print(users))
 
 
     all_commands['user_guests_status'] = Command(
@@ -321,35 +305,8 @@ E.g. guest007 or guest010-guest040."""],
         return "%d guest users available." % self.bgu.nr_available_accounts()
 
 
-    def _get_account(self, id, idtype=None, actype="Account"):
-        if actype == 'Account':
-            account = self.Account_class(self.db)
-        elif actype == 'PosixUser':
-            account = PosixUser.PosixUser(self.db)
-        account.clear()
-        try:
-            if idtype is None:
-                if id.find(":") != -1:
-                    idtype, id = id.split(":", 1)
-                    if len(id) == 0:
-                        raise CerebrumError, "Must specify id"
-                else:
-                    idtype = 'name'
-            if idtype == 'name':
-                account.find_by_name(id, self.const.account_namespace)
-            elif idtype == 'id':
-                if isinstance(id, str) and not id.isdigit():
-                    raise CerebrumError, "Entity id must be a number"
-                account.find(id)
-            else:
-                raise CerebrumError, "unknown idtype: '%s'" % idtype
-        except Errors.NotFoundError:
-            raise CerebrumError, "Could not find %s with %s=%s" % (actype, idtype, id)
-        return account
-
-
     def _get_shell(self, shell):
-        return self._get_constant(self.const.PosixShell, shell, "shell")
+        return self._get_constant(self.co.PosixShell, shell, "shell")
 
 
     def _get_constant(self, code_cls, code_str, code_type="value"):
@@ -359,29 +316,6 @@ E.g. guest007 or guest010-guest040."""],
         except Errors.NotFoundError:
             raise CerebrumError("Unknown %s: %s" % (code_type, code_str))
         return c
-
-
-    def _get_group(self, id, idtype=None, grtype="Group"):
-        if grtype == "Group":
-            group = self.Group_class(self.db)
-        elif grtype == "PosixGroup":
-            group = PosixGroup.PosixGroup(self.db)
-        try:
-            group.clear()
-            if idtype is None:
-                if id.count(':'):
-                    idtype, id = id.split(':', 1)
-                else:
-                    idtype='name'
-            if idtype == 'name':
-                group.find_by_name(id)
-            elif idtype == 'id':
-                group.find(id)
-            else:
-                raise CerebrumError, "unknown idtype: '%s'" % idtype
-        except Errors.NotFoundError:
-            raise CerebrumError, "Could not find %s with %s=%s" % (grtype, idtype, id)
-        return group
 
 
     def _get_disk(self, path, host_id=None, raise_not_found=True):
