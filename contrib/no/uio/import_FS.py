f5816139c57b73fb46520ab83268228c2b6ff98c
Jasmina Hodzic <jasminah@usit.uio.no>
2006-04-06 02:17:01 +0000

    debugmeldinger burde aldri inneholde info som ikke finnes pÃ¥ nÃ¥vÃ¦rende tidspunkt

--- contrib/no/uio/import_FS.py
+++ contrib/no/uio/import_FS.py
@@ -362,7 +362,7 @@ def process_person_callback(person_info):
                                    (ad_street, co.address_street)):
         # TBD: Skal vi slette evt. eksisterende adresse v/None?
         if address_info is not None:
-            logger.debug("Populating address for person %s" % new_person.entity_id)
+            logger.debug("Populating address...")
             new_person.populate_address(co.system_fs, ad_const, **address_info)
     # if this is a new Person, there is no entity_id assigned to it
     # until written to the database.
