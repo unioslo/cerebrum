49fd933cab30e48b2f0249191745ba6a60d94760
Steinar Hamre <stseiham@extern.uio.no>
2006-04-27 15:29:51 +0000

    Start buisness-logic for NTNU

---
+++ Cerebrum/modules/no/ntnu/Group.py
@@ -0,0 +1,16 @@
+
+from Cerebrum import Group
+from Cerebrum import Errors
+import re
+
+group_name_regex=re.compile("^[a-z][a-z0-9_]*$")
+
+class GroupNTNUMixin(Account.Account):
+    def illegal_name(self, name):
+        if len(name) > 16:
+            return "too long (%s)" % name
+        if not re.match(group_name_regex, name):
+            return "misformed (%s)" % name
+        return self.__super.illegal_name(name)
+
+
