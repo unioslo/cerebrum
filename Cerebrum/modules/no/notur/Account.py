2af26d419607779503424b457c2cc4035358a319
Steinar Hamre <stseiham@extern.uio.no>
2006-04-27 15:42:42 +0000

    Add name-washing for NOTUR.

---
+++ Cerebrum/modules/no/notur/Account.py
@@ -0,0 +1,16 @@
+
+from Cerebrum import Account
+from Cerebrum import Errors
+import re
+
+account_name_regex=re.compile("^[a-z][a-z0-9]*$")
+
+class AccountNoturMixin(Account.Account):
+    def illegal_name(self, name):
+        if len(name) > 8:
+            return "too long (%s)" % name
+        if not re.match(account_name_regex, name):
+            return "misformed (%s)" % name
+        return self.__super.illegal_name(name)
+
+
