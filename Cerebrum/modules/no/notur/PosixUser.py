cbfb00e2ece121d97d45a00c0fd2a26e6ff1c98e
Steinar Hamre <stseiham@extern.uio.no>
2006-05-03 17:18:04 +0000

    Limit UIDs to the range delegated to NOTUR.

---
+++ Cerebrum/modules/no/notur/PosixUser.py
@@ -0,0 +1,28 @@
+
+from Cerebrum.modules import PosixUser
+
+class UidRangeException(Exception):
+    """Exception raissed for Uids out of legal range"""
+    pass
+
+
+# This probably behaves badly if all uids in range are used.
+class PosixUserNoturMixin(PosixUser.PosixUser):
+    minfreeuid = 50000
+    maxfreeuid = 53000
+    def get_free_uid(self):
+        uid=self.__super.get_free_uid()
+        if (uid<=self.minfreeuid or uid>=self.maxfreeuid):
+            # We're resetting the sequence ourselves. Ugly!
+            # Postgres-spesific!
+            self.execute("""ALTER SEQUENCE posix_uid_seq
+            MINVALUE :min MAXVALUE :max
+            RESTART :min INCREMENT 1""",
+                         {'min': self.minfreeuid,
+                          'max': self.maxfreeuid})
+            uid=self.__super.get_free_uid()
+            if (uid<=self.minfreeuid or uid>=self.maxfreeuid):
+                raise UidRangeException(
+                    "Uid out of range: Please reset posix_uid_seq to %d" %
+                    minfreeuid)
+        return uid
