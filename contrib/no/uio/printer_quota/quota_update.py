cffdb491fddba41f214e3c0fe0c7563cea21edc2
Jasmina Hodzic <jasminah@usit.uio.no>
2006-06-12 13:26:30 +0000

    Fixed misspelled guest registration code (POLS-ANSATT -> POLS-ANSAT)

--- contrib/no/uio/printer_quota/quota_update.py
+++ contrib/no/uio/printer_quota/quota_update.py
@@ -291,7 +291,7 @@ def get_bet_fritak_utv_data(lt_person_file):
                                        'ST-ORG FRI', 'EF-FORSKER', 'EF-STIP',
                                        'EMERITUS', 'GJ-FORSKER', 'REG-ANSV',
                                        'EKST. KONS', 'SENIORFORS',
-                                       'POLS-ANSATT', 'ASSOSIERT',
+                                       'POLS-ANSAT', 'ASSOSIERT',
                                        'EKST. PART', 'SIVILARB'):
                 if not fnr2pid.has_key(fnr):
                     logger.warn("Unknown LT-person %s" % fnr)
