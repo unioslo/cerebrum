2c7a2d56f4547a0b43fc17bc8e05227f06cadab6
Jasmina Hodzic <jasminah@usit.uio.no>
2006-09-17 10:49:06 +0000

    Had to override update_email_addresses after all :/

--- Cerebrum/modules/no/Indigo/Account.py
+++ Cerebrum/modules/no/Indigo/Account.py
@@ -77,102 +77,108 @@ class AccountIndigoMixin(Account.Account):
         random.shuffle(pwd)
         return string.join(pwd,'')
 
-    def update_email_addresses(self, set_primary = False):
-        # do nothing while testing
-        pass
-##         # Find, create or update a proper EmailTarget for this
-##         # account.
-##         et = Email.EmailTarget(self._db)
-##         target_type = self.const.email_target_account
-##         if self.is_deleted():
-##             target_type = self.const.email_target_deleted
-##         try:
-##             et.find_by_email_target_attrs(entity_id = self.entity_id)
-##             et.email_target_type = target_type
-##         except Errors.NotFoundError:
-##             # We don't want to create e-mail targets for reserved or
-##             # deleted accounts, but we do convert the type of existing
-##             # e-mail targets above.
-##             if target_type == self.const.email_target_deleted:
-##                 return
-##             # A new target is registered in cerebrum
-##             et.populate(target_type, self.entity_id, self.const.entity_account)
-##         et.write_db()
-##         # For deleted users, set expire_date for all of the
-##         # user's addresses, and don't allocate any new addresses.
-##         ea = Email.EmailAddress(self._db)
-##         if target_type == self.const.email_target_deleted:
-##             expire_date = self._db.DateFromTicks(time.time() +
-##                                                  60 * 60 * 24 * 180)
-##             for row in et.get_addresses():
-##                 ea.clear()
-##                 ea.find(row['address_id'])
-##                 if ea.email_addr_expire_date is None:
-##                     ea.email_addr_expire_date = expire_date
-##                 ea.write_db()
-##             return
-##         # Figure out which domain(s) the user should have addresses
-##         # in.  Primary domain should be at the front of the resulting
-##         # list.
-##         primary_set = False
-##         ed = Email.EmailDomain(self._db)
-##         ed.find(self.get_primary_maildomain())
-##         domains = [ed.email_domain_name]
-## 	epat = Email.EmailPrimaryAddressTarget(self._db)
-##         # Iterate over the available domains, testing various
-##         # local_parts for availability.  Set user's primary address to
-##         # the first one found to be available.
-##         for domain in domains:
-##             if ed.email_domain_name <> domain:
-##                 ed.clear()
-##                 ed.find_by_domain(domain)
-##             # Check for 'cnaddr' category before 'uidaddr', to prefer
-##             # 'cnaddr'-style primary addresses for users in
-##             # maildomains that have both categories.
-##             ctgs = [int(r['category']) for r in ed.get_categories()]
-##             local_parts = []
-##             if int(self.const.email_domain_category_cnaddr) in ctgs:
-##                 local_parts.append(self.get_email_cn_local_part(given_names=1, max_initials=1))
-##                 local_parts.append(self.account_name)
-##             elif int(self.const.email_domain_category_uidaddr) in ctgs:
-##                 local_parts.append(self.account_name)
-## 	    for lp in local_parts:
-## 		lp = self.wash_email_local_part(lp)
-## 		# Is the address taken?
-##  		ea.clear()
-## 		try:
-## 		    ea.find_by_local_part_and_domain(lp, ed.email_domain_id)
-## 		    if ea.email_addr_target_id <> et.email_target_id:
-## 			# Address already exists, and points to a
-## 			# target not owned by this Account.
-##                         continue
-## 		    # Address belongs to this account; make sure
-## 		    # there's no expire_date set on it.
-## 		    ea.email_addr_expire_date = None
-## 		except Errors.NotFoundError:
-## 		    # Address doesn't exist; create it.
-## 		    ea.populate(lp, ed.email_domain_id, et.email_target_id,
-## 				expire=None)
-## 		ea.write_db()
-##                 if not primary_set:
-##                     epat.clear()
-##                     try:
-##                         epat.find(ea.email_addr_target_id)
-##                         epat.populate(ea.email_addr_id)
-##                     except Errors.NotFoundError:
-##                         epat.clear()
-##                         epat.populate(ea.email_addr_id, parent = et)
-##                     epat.write_db()
-##                     primary_set = True
-##             while count < 2:
-##                 pwd.append(string.digits[random.randint(0, len(string.digits)-1)])
-##                 count += 1
-##             while count < 8:
-##                 pwd.append(string.ascii_letters[random.randint(0, len(string.ascii_letters)-1)])
-##                 count += 1
-##             random.shuffle(pwd)
-##             return string.join(pwd,'')
+    def update_email_addresses(self):
+        # Find, create or update a proper EmailTarget for this
+        # account.
+        et = EmailTarget(self._db)
+        target_type = self.const.email_target_account
+        if self.is_expired():
+            target_type = self.const.email_target_deleted
+        changed = False
+        try:
+            et.find_by_email_target_attrs(entity_id = self.entity_id)
+            if et.email_target_type != target_type:
+                changed = True
+                et.email_target_type = target_type
+        except Errors.NotFoundError:
+            # We don't want to create e-mail targets for reserved or
+            # deleted accounts, but we do convert the type of existing
+            # e-mail targets above.
+            if target_type == self.const.email_target_deleted:
+                return
+            et.populate(target_type, self.entity_id, self.const.entity_account)
+        et.write_db()
+        # For deleted/reserved users, set expire_date for all of the
+        # user's addresses, and don't allocate any new addresses.
+        ea = EmailAddress(self._db)
+        if changed and cereconf.EMAIL_EXPIRE_ADDRESSES is not False:
+            if target_type == self.const.email_target_deleted:
+                seconds = cereconf.EMAIL_EXPIRE_ADDRESSES * 86400
+                expire_date = self._db.DateFromTicks(time.time() + seconds)
+            else:
+                expire_date = None
+            for row in et.get_addresses():
+                ea.clear()
+                ea.find(row['address_id'])
+                ea.email_addr_expire_date = expire_date
+                ea.write_db()
+        # Active accounts shouldn't have an alias value (it is used
+        # for failure messages)
+        if changed and target_type == self.const.email_target_account:
+            if et.email_target_alias is not None:
+                et.email_target_alias = None
+                et.write_db()
 
+        if target_type == self.const.email_target_deleted:
+            return
+        # Figure out which domain(s) the user should have addresses
+        # in.  Primary domain should be at the front of the resulting
+        # list.
+        ed = EmailDomain(self._db)
+        ed.find(self.get_primary_maildomain())
+        domains = [ed.email_domain_name]
+        if cereconf.EMAIL_DEFAULT_DOMAIN not in domains:
+            domains.append(cereconf.EMAIL_DEFAULT_DOMAIN)
+        # Iterate over the available domains, testing various
+        # local_parts for availability.  Set user's primary address to
+        # the first one found to be available.
+        primary_set = False
+        epat = EmailPrimaryAddressTarget(self._db)
+        for domain in domains:
+            if ed.email_domain_name <> domain:
+                ed.clear()
+                ed.find_by_domain(domain)
+            # Check for 'cnaddr' category before 'uidaddr', to prefer
+            # 'cnaddr'-style primary addresses for users in
+            # maildomains that have both categories.
+            ctgs = [int(r['category']) for r in ed.get_categories()]
+            local_parts = []
+            if int(self.const.email_domain_category_cnaddr) in ctgs:
+                local_parts.append(self.get_email_cn_local_part())
+                local_parts.append(self.account_name)
+            elif int(self.const.email_domain_category_uidaddr) in ctgs:
+                local_parts.append(self.account_name)
+            for lp in local_parts:
+                lp = self.wash_email_local_part(lp)
+                # Is the address taken?
+                ea.clear()
+                try:
+                    ea.find_by_local_part_and_domain(lp, ed.email_domain_id)
+                    if ea.email_addr_target_id <> et.email_target_id:
+                        # Address already exists, and points to a
+                        # target not owned by this Account.
+                        #
+                        # TODO: An expired address gets removed by a
+                        # database cleaning job, and when it's gone,
+                        # the address will eventually be recreated
+                        # connected to this target.
+                        continue
+                except Errors.NotFoundError:
+                    # Address doesn't exist; create it.
+                    ea.populate(lp, ed.email_domain_id, et.email_target_id,
+                                expire=None)
+                ea.write_db()
+                if not primary_set:
+                    epat.clear()
+                    try:
+                        epat.find(ea.email_addr_target_id)
+                        epat.populate(ea.email_addr_id)
+                    except Errors.NotFoundError:
+                        epat.clear()
+                        epat.populate(ea.email_addr_id, parent = et)
+                    epat.write_db()
+                    primary_set = True
+                    
     def set_password(self, plaintext):
         # Override Account.set_password so that we get a copy of the
         # plaintext password
