d0eb8b90cf67518970f374f4eea33336f7df8b92
Alf B. Lervåg <alfborge@unknown>
2006-10-03 15:07:59 +0000

    Rewritten to use bofhd_auth.sql instead of spine_auth.sql.

--- spine/lib/cerebrum/Auth.py
+++ spine/lib/cerebrum/Auth.py
@@ -20,25 +20,32 @@
 
 from SpineLib.DatabaseClass import DatabaseClass, DatabaseAttr
 from SpineLib.Builder import Method
+from Types import CodeType
 
 from Commands import Commands
 
 from SpineLib import Registry
 registry = Registry.get_registry()
 
-__all__ = ['AuthOperation', 'AuthOperationSet', 'AuthOperationSetMember']
+__all__ = ['AuthOperationCode', 'AuthOperationSet', 'AuthOperation']
 
-table = 'auth_operation'
-class AuthOperation(DatabaseClass):
+table = 'auth_op_code'
+class AuthOperationCode(CodeType):
     primary = (
         DatabaseAttr('id', table, int),
     )
     slots = (
-        DatabaseAttr('op_class', table, str),
-        DatabaseAttr('op_method', table, str)
+        DatabaseAttr('name', table, str),
+        DatabaseAttr('description', table, str)
     )
 
-registry.register_class(AuthOperation)
+    db_attr_aliases = {
+        table: {
+            'id':'code',
+            'name':'code_str'
+        }
+    }
+registry.register_class(AuthOperationCode)
 
 table = 'auth_operation_set'
 class AuthOperationSet(DatabaseClass):
@@ -50,33 +57,35 @@ class AuthOperationSet(DatabaseClass):
         DatabaseAttr('description', table, str, write=True)
     )
 
+    db_attr_aliases = {
+        table: {
+            'id':'op_set_id',
+        }
+    }
     def add_operation(self, operation):
-        AuthOperationSetMember._create(self.get_database(), operation, self)
+        db = self.get_database()
+        obj_id = int(db.nextval('code_seq'))
+        AuthOperation._create(db, obj_id, operation, self)
     add_operation.signature = None
     add_operation.signature_name = 'add_operation'
-    add_operation.signature_args = [AuthOperation]
+    add_operation.signature_args = [AuthOperationCode]
     add_operation.signature_write = True
     
     def remove_operation(self, operation):
-        AuthOperationSetMember(self.get_database(), operation, self).delete()
+        db = self.get_database()
+        ss = registry.AuthOperationSearcher(db)
+        ss.set_op_set(self)
+        ss.set_op(operation)
+        ops = ss.search()
+        for op in ops:
+            op.delete()
     remove_operation.signature = None
     remove_operation.signature_name = 'remove_operation'
-    remove_operation.signature_args = [AuthOperation]
+    remove_operation.signature_args = [AuthOperationCode]
     remove_operation.signature_write = True
     
-
-    def get_operations(self):
-        db = self.get_database()
-        s = registry.AuthOperationSearcher(db)
-        ss = registry.AuthOperationSetMemberSearcher(db)
-        ss.set_op_set(self)
-        s.add_join('', ss, 'op')
-        return s.search()
-    get_operations.signature = [AuthOperation]
-    get_operations.signature_name = 'get_operations'
-
     def delete(self):
-        s = registry.AuthOperationSetMemberSearcher(self.get_database())
+        s = registry.AuthOperationSearcher(self.get_database())
         s.set_op_set(self)
         for i in s.search():
             i.delete()
@@ -88,16 +97,30 @@ class AuthOperationSet(DatabaseClass):
 
 registry.register_class(AuthOperationSet)
 
-table = 'auth_operation_set_member'
-class AuthOperationSetMember(DatabaseClass):
+def create_auth_operation_set(self, name, description):
+    db = self.get_database()
+    obj_id = int(db.nextval('code_seq'))
+    AuthOperationSet._create(db, obj_id, name, description)
+    return AuthOperationSet(db, obj_id)
+create_auth_operation_set.signature = AuthOperationSet
+create_auth_operation_set.signature_args = [str, str]
+create_auth_operation_set.signature_write = True
+
+
+table = 'auth_operation'
+class AuthOperation(DatabaseClass):
     primary = (
-        DatabaseAttr('op', table, AuthOperation),
-        DatabaseAttr('op_set', table, AuthOperationSet)
+        DatabaseAttr('id', table, int),
+    )
+    slots = (
+        DatabaseAttr('op', table, AuthOperationCode),
+        DatabaseAttr('op_set', table, AuthOperationSet),
     )
     db_attr_aliases = {
         table: {
-            'op':'op_id',
-            'op_set':'op_set_id'
+            'id':'op_id',
+            'op':'op_code',
+            'op_set':'op_set_id',
         }
     }
     def delete(self):
@@ -107,24 +130,16 @@ class AuthOperationSetMember(DatabaseClass):
     delete.signature_name = 'delete'
     delete.signature_write = True
 
-registry.register_class(AuthOperationSetMember)
+registry.register_class(AuthOperation)
 
-def create_auth_operation(self, op_class, op_method):
+def get_operations(self):
     db = self.get_database()
-    obj_id = int(db.nextval('auth_seq'))
-    AuthOperation._create(db, obj_id, op_class, op_method)
-    return AuthOperation(db, obj_id)
-m = Method('create_auth_operation', AuthOperation,
-           args=[('op_class', str), ('op_method', str)], write=True)
-Commands.register_method(m, create_auth_operation)
+    aos = registry.AuthOperationSearcher(db)
+    aos.set_op_set(self)
+    return aos.search()
+get_operations.signature = [AuthOperation]
+get_operations.signature_name = 'get_operations'
 
-def create_auth_operation_set(self, name, description):
-    db = self.get_database()
-    obj_id = int(db.nextval('auth_seq'))
-    AuthOperationSet._create(db, obj_id, name, description)
-    return AuthOperationSet(db, obj_id)
-m = Method('create_auth_operation_set', AuthOperationSet,
-           args=[('name', str), ('description', str)], write=True)
-Commands.register_method(m, create_auth_operation_set)
-
+Commands.register_methods([create_auth_operation_set])
+AuthOperationSet.register_methods([get_operations])
 # arch-tag: 3dd57534-233c-4cc1-aa00-b929fd7fb24b
