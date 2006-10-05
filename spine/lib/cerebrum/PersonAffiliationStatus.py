eeda36dc79aa658c5156f6cfa1c6d4dd9e81a513
Steinar Hamre <stseiham@extern.uio.no>
2006-10-05 14:26:41 +0000

    It's not a fix, but it's now a documented bug.

--- spine/lib/cerebrum/PersonAffiliationStatus.py
+++ spine/lib/cerebrum/PersonAffiliationStatus.py
@@ -1,10 +1,15 @@
 from SpineLib.DatabaseClass import DatabaseClass, DatabaseAttr
-from Types import PersonAffiliationType
+from Types import PersonAffiliationType, CodeType
 from SpineLib import Registry
 
 registry = Registry.get_registry()
 
 table = 'person_aff_status_code'
+# This is really a code value table in Cerebrum, and
+# should be CodeType, not DatabaseClass.
+# However, CodeType does not handle this table because status_str
+# is not globally unique. (status_str is unique per affiliation.)
+# care should be taken so that entries are not edited run-time.
 class PersonAffiliationStatus(DatabaseClass):
     primary = (
         DatabaseAttr('id', table, int),
