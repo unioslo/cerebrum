63c3d81ee0e3e215354e7d4afa1582205bbffc81
Kjetil Homme <kjetilho@ulrik.uio.no>
2006-10-25 07:52:09 +0000

    reverting 1.25

--- Cerebrum/Disk.py
+++ Cerebrum/Disk.py
@@ -289,12 +289,11 @@ class Host(EntityName, Entity_class):
         if is_new:
             self.execute("""
             INSERT INTO [:table schema=cerebrum name=host_info]
-              (entity_type, host_id, name, description)
-            VALUES (:e_type, :host_id, :name, :description)
+              (entity_type, host_id, description)
+            VALUES (:e_type, :host_id, :description)
                     """,
                          {'e_type': int(self.const.entity_host),
                           'host_id': self.entity_id,
-                          'name': self.name,
                           'description': self.description})
             self._db.log_change(self.entity_id, self.const.host_add, None,
                                 change_params={'name': self.name})
@@ -302,11 +301,10 @@ class Host(EntityName, Entity_class):
         else:
             self.execute("""
             UPDATE [:table schema=cerebrum name=host_info]
-            SET description=:description, name=:name
+            SET description=:description
             WHERE host_id=:host_id""",
                          {'host_id': self.entity_id,
-                          'description': self.description,
-                          'name': self.name})
+                          'description': self.description})
             self._db.log_change(self.entity_id, self.const.host_mod, None,
                                 change_params={'name': self.name})
             if 'name' in self.__updated:
