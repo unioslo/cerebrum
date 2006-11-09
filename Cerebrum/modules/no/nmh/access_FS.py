484d6c29d9be0114ff434fc75eacbb1f8724705f
Jasmina Hodzic <jasminah@usit.uio.no>
2006-11-09 02:20:04 +0000

    initial commit access_FS for NMH

---
+++ Cerebrum/modules/no/nmh/access_FS.py
@@ -0,0 +1,136 @@
+# -*- coding: iso-8859-1 -*-
+# Copyright 2002, 2003 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+import time
+
+from Cerebrum.modules.no import access_FS
+
+class NMHStudent(access_FS.Student):
+    def list_aktiv(self):
+	""" Hent opplysninger om studenter definert som aktive 
+	ved NMH. En aktiv student er en student som har et gyldig
+        opptak til et studieprogram der studentstatuskode er 'AKTIV'
+        eller 'PERMISJON' og sluttdatoen er enten i fremtiden eller
+        ikke satt."""
+	qry = """
+        SELECT DISTINCT
+          s.fodselsdato, s.personnr, p.etternavn, p.fornavn,
+          s.adrlin1_semadr,s.adrlin2_semadr, s.postnr_semadr,
+          s.adrlin3_semadr, s.adresseland_semadr, p.adrlin1_hjemsted,
+          p.adrlin2_hjemsted, p.postnr_hjemsted, p.adrlin3_hjemsted,
+          p.adresseland_hjemsted, p.status_reserv_nettpubl,
+          p.sprakkode_malform, sps.studieprogramkode, sps.studieretningkode,
+          sps.studierettstatkode, sps.studentstatkode, sps.terminkode_kull,
+          sps.arstall_kull, p.kjonn, p.status_dod, p.telefonnr_mobil,
+          s.studentnr_tildelt
+        FROM fs.studieprogramstudent sps, fs.person p,
+             fs.student s
+        WHERE p.fodselsdato = sps.fodselsdato AND
+          p.personnr = sps.personnr AND
+          p.fodselsdato = s.fodselsdato AND
+          p.personnr = s.personnr AND
+          %s AND
+          sps.status_privatist = 'N' AND
+          sps.studentstatkode IN ('AKTIV', 'PERMISJON') AND
+          NVL(sps.dato_studierett_gyldig_til,SYSDATE)>= SYSDATE
+          """ % (self._is_alive())
+        return self.db.query(qry)
+
+class NMHUndervisning(access_FS.Undervisning):
+    ## TBD: avskaffe UiO-spesifikke søk for list_undervisningsenheter
+    ##      og list_studenter_underv_enhet.
+    ##      Prøve å lage generell list_studenter_kull.
+    ##      Prøve å fjerne behov for override-metoder her 
+    def list_undervisningenheter(self, sem="current"):
+	"""Metoden som henter data om undervisningsenheter
+	i nåverende (current) eller neste (next) semester. Default
+	vil være nåværende semester. For hver undervisningsenhet 
+	henter vi institusjonsnr, emnekode, versjonskode, terminkode + årstall 
+	og terminnr."""
+	qry = """
+        SELECT DISTINCT
+          r.institusjonsnr, r.emnekode, r.versjonskode, e.emnenavnfork,
+          e.emnenavn_bokmal, e.faknr_kontroll, e.instituttnr_kontroll, 
+          e.gruppenr_kontroll, r.terminnr, r.terminkode, r.arstall
+          FROM fs.emne e, fs.undervisningsenhet r
+          WHERE r.emnekode = e.emnekode AND
+          r.versjonskode = e.versjonskode AND """ 
+        if (sem=="current"):
+	    qry +="""%s""" % self._get_termin_aar(only_current=1)
+        else: 
+	    qry +="""%s""" % self._get_next_termin_aar()
+	return self.db.query(qry)
+
+    def list_studenter_underv_enhet(self, institusjonsnr, emnekode, versjonskode,
+                                    terminkode, arstall, terminnr):
+	"""Finn fødselsnumrene til alle studenter på et gitt 
+	undervisningsenhet. Skal brukes til å generere grupper for
+	adgang til CF."""
+	qry = """
+        SELECT DISTINCT
+          fodselsdato, personnr
+        FROM fs.undervisningsmelding
+        WHERE
+          institusjonsnr = :institusjonsnr AND
+          emnekode = :emnekode AND
+          versjonskode = :versjonskode AND
+          terminnr = :terminnr AND
+          terminkode = :terminkode AND
+          arstall = :arstall """
+        return self.db.query(qry, {'institusjonsnr': institusjonsnr,
+                                   'emnekode': emnekode,
+                                   'versjonskode': versjonskode,
+                                   'terminnr': terminnr,
+                                   'terminkode': terminkode,
+                                   'arstall': arstall}
+                             )
+
+    def list_studenter_kull(self, studieprogramkode, terminkode, arstall):
+        """Hent alle studentene som er oppført på et gitt kull."""
+
+        query = """
+        SELECT DISTINCT
+            fodselsdato, personnr
+        FROM
+            fs.studieprogramstudent
+        WHERE
+            studentstatkode IN ('AKTIV', 'PERMISJON') AND
+            NVL(dato_studierett_gyldig_til,SYSDATE)>= SYSDATE AND
+            studieprogramkode = :studieprogramkode AND
+            terminkode_kull = :terminkode_kull AND
+            arstall_kull = :arstall_kull
+        """
+
+        return self.db.query(query, {"studieprogramkode" : studieprogramkode,
+                                     "terminkode_kull"   : terminkode,
+                                     "arstall_kull"      : arstall})
+
+class FS(access_FS.FS):
+
+    def __init__(self, db=None, user=None, database=None):
+        super(FS, self).__init__(db=db, user=user, database=database)
+
+        t = time.localtime()[0:3]
+        self.year = t[0]
+        self.mndnr = t[1]
+        self.dday = t[2]
+        
+        # Override with nmh-spesific classes
+        self.student = NMHStudent(self.db)
+        self.undervisning = NMHUndervisning(self.db)
