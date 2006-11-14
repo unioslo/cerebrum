7448037acc0db4b00cf90942a6c84eb620624a9e
nappy <kenneth.a.johansen@uit.no>
2006-11-14 07:37:08 +0000

    did some changes to comply with schema version 1.4 from FEIDE.
    kenneth

--- Cerebrum/modules/no/uit/OrgLdif.py
+++ Cerebrum/modules/no/uit/OrgLdif.py
@@ -28,9 +28,119 @@ from Cerebrum.Utils             import Factory
 from Cerebrum.modules.LDIFutils import *
 from Cerebrum.modules           import OrgLDIF
 from Cerebrum.QuarantineHandler import QuarantineHandler
+from Cerebrum.modules.OrgLDIF import *
 
-class OrgLdifUitMixin:
+class OrgLdifUitMixin(OrgLDIF):
     
+
+    def __init__(self, db, logger):
+        self.__super.__init__(db, logger)
+        self.norEduOrgUniqueNumber = ("000%05d"  # Note: 000 = Norway in FEIDE.
+                                      % int(cereconf.DEFAULT_INSTITUSJONSNR),)
+        self.norEduOrgNIN = (cereconf.DEFAULT_FORETAKSNR)
+        self.norEduOrgSchemaVersion=(cereconf.norEduOrgSchemaVersion)
+        # '@<security domain>' for the eduPersonPrincipalName attribute.
+        self.eduPPN_domain = '@' + cereconf.INSTITUTION_DOMAIN_NAME
+
+
+    def make_ou_entry(self, ou_id, parent_dn):
+        # Changes from superclass:
+        # Only output OUs with katalog_merke == 'T'.
+        # Add object class norEduOrgUnit and its attributes norEduOrgAcronym,
+        # cn, norEduOrgUnitUniqueNumber, norEduOrgUniqueNumber.
+        # If a DN is not unique, prepend the norEduOrgUnitUniqueNumber.
+        self.ou.clear()
+        self.ou.find(ou_id)
+        if self.ou.katalog_merke != 'T':
+            return parent_dn, None
+        ou_names = [iso2utf((n or '').strip()) for n in (self.ou.acronym,
+                                                         self.ou.short_name,
+                                                         self.ou.display_name)]
+        acronym  = ou_names[0]
+        ou_names = filter(None, ou_names)
+        entry = {
+            'objectClass': ['top', 'organizationalUnit', 'norEduOrg','eduOrg'],
+            #'norEduOrgUnitUniqueNumber': (self.get_orgUnitUniqueNumber(),),
+            #'norEduOrgUniqueNumber':     self.norEduOrgUniqueNumber,
+            #'norEduOrgNIN' : self.norEduOrgNIN,
+            'ou': ou_names,
+            'cn': ou_names[-1:]}
+        if acronym:
+            entry['norEduOrgAcronym'] = (acronym,)
+        dn = self.make_ou_dn(entry, parent_dn or self.ou_dn)
+        if not dn:
+            return parent_dn, None
+        entry['ou'] = self.attr_unique(entry['ou'], normalize_string)
+        self.fill_ou_entry_contacts(entry)
+        self.update_ou_entry(entry)
+        return dn, entry
+
+
+    def generate_person(self, outfile, alias_outfile, use_mail_module):
+        """Output person tree and aliases if cereconf.LDAP_PERSON['dn'] is set.
+
+        Aliases are only output if cereconf.LDAP_PERSON['aliases'] is true.
+
+        If use_mail_module is set, persons' e-mail addresses are set to
+        their primary users' e-mail addresses.  Otherwise, the addresses
+        are taken from contact info registered for the individual persons."""
+        if not self.person_dn:
+            return
+        self.init_person_dump(use_mail_module)
+        if self.person_parent_dn not in (None, self.org_dn):
+            outfile.write(container_entry_string('PERSON'))
+        timer       = self.make_timer("Processing persons...")
+        round_timer = self.make_timer()
+        round       = 0
+        for row in self.list_persons():
+            if round % 10000 == 0:
+                round_timer("...rounded %d rows..." % round)
+            round += 1
+            dn, entry, alias_info = self.make_person_entry(row)
+            if dn:
+                if dn in self.used_DNs:
+                    self.logger.warn("Omitting person_id %d: duplicate DN '%s'"
+                                     % (row['person_id'], dn))
+                else:
+                    self.used_DNs[dn] = True
+                    outfile.write(entry_string(dn, entry, False))
+                    if self.aliases and alias_info:
+                        self.write_person_alias(alias_outfile,
+                                                dn, entry, alias_info)
+        timer("...persons done.")
+        self.generate_system_object(outfile) # this line is the only reason for having generate_persons here
+
+    #UIT: added the system object. This is needed when we add users under it
+    def generate_system_object(self,outfile):
+        entry= {'objectClass':['top','uioUntypedObject']}
+        self.ou_dn = "cn=system,dc=uit,dc=no"
+        outfile.write(entry_string(self.ou_dn,entry))
+
+
+    def generate_org_object(self, outfile):
+        """Output the organization object if cereconf.LDAP_ORG['dn'] is set."""
+        if not self.org_dn:
+            return
+        self.init_org_object_dump()
+        entry = {}
+        self.ou.clear()
+        if self.root_ou_id is not None:
+            self.ou2DN[self.root_ou_id] = None
+            self.ou.find(self.root_ou_id)
+            self.fill_ou_entry_contacts(entry)
+        entry.update(ldapconf('ORG', 'attrs', {}))
+        oc = ['top', 'organization', 'eduOrg']
+        oc.extend(entry.get('objectClass', ()))
+        entry['objectClass'] = (['top', 'organization', 'eduOrg','norEduOrg','dcObject']
+                                + list(entry.get('objectClass', ())))
+        self.update_org_object_entry(entry)
+        entry['o'] = (['University of Tromsoe','Universitetet i Tromso'])
+        entry['eduOrgLegalName']=(['Universitetet i Tromso','University of Tromsoe'])
+        entry['objectClass'] = self.attr_unique(entry['objectClass'],str.lower)
+        #print "entry=%s " % entry
+        outfile.write(entry_string(self.org_dn, entry))
+
+
 
     def make_person_entry(self, row):
         # Return (dn, person entry, alias_info) for a person to output,
@@ -75,6 +185,9 @@ class OrgLdifUitMixin:
             pass
 
         passwd = self.acc_passwd.get(account_id)
+        nt4_passwd=self.acc_passwd_nt4.get(account_id)
+        if nt4_passwd ==None:
+            nt4_passwd='*Invalid'
         qt = self.acc_quarantines.get(account_id)
         if qt:
             qh = QuarantineHandler(self.db, qt)
@@ -83,7 +196,7 @@ class OrgLdifUitMixin:
             if qh.is_locked():
                 passwd = 0
         if passwd:
-            #UIT: Changed {crypt} to {Md5} below
+            #entry['nt4_userPassword'] = (nt4_passwd,)
             entry['userPassword'] = ("{MD5}" + passwd,)
         elif passwd != 0 and entry.get('uid'):
             self.logger.debug("User %s got no password-hash.", entry['uid'][0])
@@ -157,35 +270,32 @@ class OrgLdifUitMixin:
     
     
     def init_account_info(self):
-        # Set self.acc_name        = dict {account_id: user name}.
-        # Set self.acc_passwd      = dict {account_id: password hash}.
-        # Set self.acc_quarantines = dict {account_id: [quarantine list]}.
-        
-        #print "Our init_account_info"
+
         timer = self.make_timer("Fetching account information...")
         timer2 = self.make_timer()
         self.account = Factory.get('Account')(self.db)
         self.acc_name = acc_name = {}
+        self.acc_passwd_nt4={}
         self.acc_passwd = {}
         self.acc_quarantines = acc_quarantines = {}
         fill_passwd = {
-            # Uit: removed md5_crypt and crypt3_des and added md5_b64
+
             int(self.const.auth_type_md5_b64):  self.acc_passwd.__setitem__,
+            int(self.const.auth_type_md4_nt):  self.acc_passwd_nt4.__setitem__,
             int(self.const.auth_type_md5_b64):  self.acc_passwd.setdefault}
-            #int(self.const.auth_type_md5_crypt):  self.acc_passwd.__setitem__,
-            #int(self.const.auth_type_crypt3_des): self.acc_passwd.setdefault }
+            #int(self.const.auth_type_md4_nt): self.acc_passwd.setdefault }
         for row in self.account.list_account_authentication(
-                auth_type = fill_passwd.keys()):
+            auth_type = fill_passwd.keys()):
             account_id           = int(row['account_id'])
             acc_name[account_id] = row['entity_name']
             method               = row['method']
             if method:
                 fill_passwd[int(method)](account_id, row['auth_data'])
-            #print "method: %s , fill=%s" % (method,fill_passwd)
+                #print "method: %s , fill=%s" % (method,fill_passwd)
         timer2("...account quarantines...")
         now = mx.DateTime.now()
         for row in self.account.list_entity_quarantines(
-                entity_types = self.const.entity_account):
+            entity_types = self.const.entity_account):
             if (row['start_date'] <= now
                 and (row['end_date'] is None or row['end_date'] >= now)
                 and (row['disable_until'] is None
@@ -195,77 +305,6 @@ class OrgLdifUitMixin:
                     int(row['quarantine_type']))
         timer("...account information done.")
 
-    
-    
-    def generate_person(self, outfile, alias_outfile, use_mail_module):
-        """Output person tree and aliases if cereconf.LDAP_PERSON['dn'] is set.
-                            
-        Aliases are only output if cereconf.LDAP_PERSON['aliases'] is true.
-        
-        If use_mail_module is set, persons' e-mail addresses are set to
-        their primary users' e-mail addresses.  Otherwise, the addresses
-        are taken from contact info registered for the individual persons."""
-        if not self.person_dn:
-            return
-        self.init_person_dump(use_mail_module)
-        if self.person_parent_dn not in (None, self.org_dn):
-            outfile.write(container_entry_string('PERSON'))
-        timer       = self.make_timer("Processing persons...")
-        round_timer = self.make_timer()
-        round       = 0
-        for row in self.list_persons():
-            if round % 10000 == 0:
-                round_timer("...rounded %d rows..." % round)
-            round += 1
-            dn, entry, alias_info = self.make_person_entry(row)
-            if dn:
-                if dn in self.used_DNs:
-                    self.logger.warn("Omitting person_id %d: duplicate DN '%s'"
-                    % (row['person_id'], dn))
-                else:
-                    self.used_DNs[dn] = True
-                    outfile.write(entry_string(dn, entry, False))
-                    if self.aliases and alias_info:
-                        self.write_person_alias(alias_outfile,
-                        dn, entry, alias_info)
-        timer("...persons done.")
-        
-        # UIT: All above is identical to OrgLDIF.py in modules folder
-        #print "Calling gen_sys_obj"
-        #outfile = ldif_outfile('ORG',ofile)
-        self.generate_system_object(outfile)
-    
-    
-    #UIT: added the system object. This is needed when we add users under it
-    def generate_system_object(self,outfile):
-        entry= {'objectClass':['top','uioUntypedObject']}
-        self.ou_dn = "cn=system,dc=uit,dc=no"
-        outfile.write(entry_string(self.ou_dn,entry))
-        
-    def generate_org_object(self, outfile):
-        """Output the organization object if cereconf.LDAP_ORG['dn'] is set."""
-        if not self.org_dn:
-            return
-        self.init_org_object_dump()
-        entry = {}
-        self.ou.clear()
-        if self.root_ou_id is not None:
-            self.ou2DN[self.root_ou_id] = None
-            self.ou.find(self.root_ou_id)
-            self.fill_ou_entry_contacts(entry)
-        entry.update(ldapconf('ORG', 'attrs', {}))
-        # UIT: added dcObject to to org_object
-        oc = ['top', 'organization', 'eduOrg','dcObject']
-        #oc = ['top', 'organization', 'eduOrg']
-        oc.extend(entry.get('objectClass', ()))
-        # UIT: added dcObject to entry ['objectClass']
-        entry['objectClass'] = (['top', 'organization', 'eduOrg','dcObject']
-                                + list(entry.get('objectClass', ())))
-        self.update_org_object_entry(entry)
-        # Uit: added the next line
-        entry['o'] = (['University of Tromsoe'])
-        entry['objectClass'] = self.attr_unique(entry['objectClass'],str.lower)
-        outfile.write(entry_string(self.org_dn, entry))
 
     def update_person_entry(self,entry, row):
         # Override this to fill in a person entry further before output.
@@ -273,5 +312,22 @@ class OrgLdifUitMixin:
         # If there is no password, store a useless one instead of no password
         # so that a text filter can easily find and replace the password.
         entry.setdefault('userPassword', ("{MD5}*Invalid",)) # UIT: changed to MD5 from crypt
+        #entry.setdefault('nt4_userPassword', ("*Invalid",)) # UIT: changed to MD5 from crypt
+
+
+        self.__super.update_person_entry(entry, row)
+        uname = entry.get('uid')
+        person_id = int(row['person_id'])
+        fnr = self.fodselsnrs.get(person_id)
+        if uname and fnr:
+            #entry['objectClass'].append('norEduPerson')
+            entry['eduPersonPrincipalName'] = (uname[0] + self.eduPPN_domain,)
+            entry['norEduPersonNIN'] = (str(fnr),)
+            birth_date = self.birth_dates.get(person_id)
+            if birth_date:
+                entry['norEduPersonBirthDate'] = ("%04d%02d%02d" % (
+                    birth_date.year, birth_date.month, birth_date.day),)
+
+
 
 # arch-tag: 8543fff4-b4f2-11da-9167-d5a8ad1cd3f2
