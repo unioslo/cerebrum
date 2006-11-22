9d70a9d28468b10436a809500d8d50b99b3fe7c1
Jasmina Hodzic <jasminah@usit.uio.no>
2006-11-22 22:21:19 +0000

    Trying to tie in a few more common constant definitions for the University Colleges

--- Cerebrum/modules/no/nmh/Constants.py
+++ Cerebrum/modules/no/nmh/Constants.py
@@ -25,56 +25,17 @@ get the actual database code/code_str representing a given Entity,
 Address, Gender etc. type."""
 
 from Cerebrum import Constants
-from Cerebrum.Constants import _AuthoritativeSystemCode, \
-                               _SpreadCode, \
-                               _QuarantineCode,\
+from Cerebrum.Constants import _SpreadCode, \
                                _PersonAffiliationCode, \
-                               _PersonAffStatusCode, \
-                               _AccountCode, \
-                               _PersonNameCode, \
-                               _ContactInfoCode, \
-                               _CountryCode
+                               _PersonAffStatusCode
+
 
 class Constants(Constants.Constants):
-## AFFILIATIONS FOR ANSATTE
-    affiliation_ansatt = _PersonAffiliationCode('ANSATT', 'Ansatt ved NMH')
+    ## affiliations for employees
     affiliation_status_ansatt_ovlaerer = _PersonAffStatusCode(
         affiliation_ansatt, 'ans_ovlaerer', 'Ansatt ved NMH, øvingslærer.')
-    affiliation_status_ansatt_vitenskapelig = _PersonAffStatusCode(
-        affiliation_ansatt, 'vitenskapelig', 'Vitenskapelig ansatt ved NMH')
-    affiliation_status_ansatt_tekadm = _PersonAffStatusCode(
-        affiliation_ansatt, 'tekadm', 'Tekniske/administrative ansatte')
     
-## AFFILIATIONS FOR STUDENTER
-    affiliation_student = _PersonAffiliationCode('STUDENT', 'Student ved NMH')
-    affiliation_status_student_evu = _PersonAffStatusCode(
-        affiliation_student, 'evu', 'Student, etter og videre utdanning')
-    affiliation_status_student_aktiv = _PersonAffStatusCode(
-        affiliation_student, 'aktiv', 'Student, aktiv')
-
-## AFFILIATIONS FOR ASSOSIERTE PERSONER
-    affiliation_tilknyttet = _PersonAffiliationCode(
-        'TILKNYTTET',
-        ('Personer tilknyttet NMH og registrert i SAP med utvalgte stillingskoder'))
-    ## Fram til SAP er i produksjon hos NMH er fagpersoner å regne
-    ## som ansatte og skal behandles deretter (ved f.eks. eksport til LDAP)
-    affiliation_status_tilknyttet_fagperson = _PersonAffStatusCode(
-        affiliation_tilknyttet, 'fagperson',
-        'Personer registrert i FS som fagpersoner')
-    affiliation_status_tilknyttet_pensjonist = _PersonAffStatusCode(
-        affiliation_tilknyttet, 'pensjonist',
-        'Personer registrert i SAP som pensjonister')
-    affiliation_status_tilknyttet_bilag = _PersonAffStatusCode(
-        affiliation_tilknyttet, 'bilag',
-        'Personer registrert i SAP som bilagslønnet')
-    affiliation_status_tilknyttet_gjest = _PersonAffStatusCode(
-        affiliation_tilknyttet, 'gjest',
-        'Personer registrert i SAP som gjest')
-    affiliation_status_tilknyttet_gjestefors = _PersonAffStatusCode(
-        affiliation_tilknyttet, 'gjesteforsker',
-        'Personer registrert i SAP som gjesteforskere')    
-
-## AFFILIATIONS FOR ANDRE
+    ## affiliations for others
     affiliation_manuell = _PersonAffiliationCode(
         'MANUELL',
         ('Tilknyttet NMH uten å være registrert i et av de'
@@ -83,59 +44,23 @@ class Constants(Constants.Constants):
         affiliation_manuell, 'inaktiv',
         'Person uten ekte tilknytning til NMH. Bruk med forsiktighet!')
 
-## DEFINISJON AV SPREAD
-    ## Brukerrelaterte spread
+    ## Spread definitions - user related
     spread_ad_account = _SpreadCode(
         'account@ad', Constants.Constants.entity_account,
         'Brukeren kjent i AD ved NMH')
     spread_exchange_account = _SpreadCode(
         'account@exchange', Constants.Constants.entity_account,
         'Brukeren kjent i AD ved NMH')
-    spread_ldap_account = _SpreadCode(
-        'account@ldap', Constants.Constants.entity_account,
-        'Brukeren kjent i LDAP ved NMH')
-    spread_lms_account = _SpreadCode(
-        'account@lms', Constants.Constants.entity_account,
-        'Brukeren kjent i LMSen til NMH')    
 
-    ## Grupperelaterte spread
+    ## Spread definitions - group related
     spread_ad_group = _SpreadCode(
         'group@ad', Constants.Constants.entity_group,
         'Gruppe kjent i AD ved NMH')
     spread_lms_group = _SpreadCode(
         'group@lms', Constants.Constants.entity_group,
-        'Gruppe kjent i LMSen til NMH')    
+        'Gruppe kjent i LMSen til NMH') 
 
-    ## Personrelaterte spread
-    spread_ldap_person = _SpreadCode(
-        'person@ldap', Constants.Constants.entity_person,
-        'Person kjent i organisasjonstreet til NMH (FEIDE-person)')
+    ## Spread definitions - person relates
     spread_adgang_person = _SpreadCode(
         'person@adgang', Constants.Constants.entity_person,
         'Person kjent i adgangssystemet til NMH')
-    spread_lms_person = _SpreadCode(
-        'person@lms', Constants.Constants.entity_person,
-        'Person kjent i LMSen til NMH')
-
-## KARANTENETYPER
-    quarantine_generell = _QuarantineCode('generell',
-                                          'Generell splatt')
-    quarantine_teppe = _QuarantineCode('teppe',
-                                       'Kalt inn på teppe')
-    quarantine_kunepost = _QuarantineCode('kunepost',
-                                           'Tilgang kun til e-post')
-    quarantine_svaktpassord = _QuarantineCode('svaktpassord',
-                                               'For dårlig passord')
-    quarantine_system = _QuarantineCode('system',
-                                        'Systembrukar som ikke skal logge inn')
-    ## Cerebruminternt, skal kun brukes automatisk
-    quarantine_auto_passord = _QuarantineCode('auto_passord',
-                                             'Passord ikke skiftet trass pålegg')
-    quarantine_auto_inaktiv = _QuarantineCode('auto_inaktiv',
-                                              'Ikke aktiv student, utestengt')
-    quarantine_auto_kunepost = _QuarantineCode('auto_kunepost',
-                                               'Privatist, kun tilgang til e-post')
-    quarantine_ou_notvalid = _QuarantineCode('ou_notvalid',
-                                             'Sted ugyldig i autoritativ kildesystem')    
-    quarantine_ou_remove = _QuarantineCode('ou_remove',
-                                           'Sted fjernet fra autoritativ kildesystem')
