1b7b4cb649c7ef40550cb3ec0a428e20268b82f7
nappy <kenneth.a.johansen@uit.no>
2006-11-24 14:48:59 +0000

    updated for current use

--- run_employee.sh
+++ run_employee.sh
@@ -3,9 +3,9 @@
 today=`date +"%Y%m%d"`
 
 cd /cerebrum/share/cerebrum/contrib/no/uit
-python get_user_info.py 
+#python get_user_info.py 
 python get_slp4_data.py 
-python parse_user_info.py -e 
+#python parse_user_info.py -e 
 python import_ad_email.py -i /cerebrum/var/dumps/email/AD_Emaildump.cvs
 
 # source tables
@@ -19,6 +19,6 @@ python import_LT.py -p /cerebrum/var/dumps/employees/uit_persons_$today.xml
 
 
 # finally generate accounts
-python process_employees.py
+python process_employees.py -f /cerebrum/var/dumps/employees/uit_persons_$today.xml
 
 # arch-tag: b9f98060-b426-11da-87cc-a78423d7022d
