2c56e3e80ac08e8f50c6be74739c8dc45f223507
Trond Kandal <trond.kandal@ntnu.no>
2006-11-29 09:19:46 +0000

    added method for changing password.

--- spine/cereweb/htdocs/user_client.py
+++ spine/cereweb/htdocs/user_client.py
@@ -20,6 +20,7 @@
 
 import cherrypy
 
+from lib import utils
 from gettext import gettext as _
 from lib.Main import Main
 from lib.utils import commit, commit_url, queue_message, object_link
@@ -34,7 +35,27 @@ def index(transaction):
     page.account = account
     res = str(page)
     return [res]
-
 index = transaction_decorator(index)
 index.exposed = True
+
+def set_password(transaction, **vargs):
+	myId = vargs.get('id')
+	pass1 = vargs.get('passwd1')
+	pass2 = vargs.get('passwd2')
+	if myId and pass1 and pass2 and pass1 == pass2:
+	  account = transaction.get_account(int(myId))
+	  account.set_password(pass1)
+	  transaction.commit()
+	elif (not myId):
+		print 'not myId'
+	elif (not pass1):
+		print 'not pass1'
+	elif ( not pass2):
+		print 'not pass2'
+	elif ( not pass1 == pass2 ):
+		print 'pass1 != pass2'
+
+	utils.redirect('/user_client')
+set_password = transaction_decorator(set_password)
+set_password.exposed = True
 
