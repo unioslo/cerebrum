76b0555f6ac1a7039603fca4166b33cb226c0790
Rune Frøysa <runefro@usit.uio.no>
2006-12-12 12:33:54 +0000

    initial release.  Expect changes

---
+++ contrib/no/uio/generate_kurs_ldif.py
@@ -0,0 +1,266 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+import getopt
+import sys
+import cerebrum_path
+import locale
+from Cerebrum.Utils import Factory
+from Cerebrum.modules.xmlutils.GeneralXMLParser import GeneralXMLParser
+
+"""
+Generere LDAP tre med uioEduSection (Undervisningsaktivitet - gruppe,
+kollokvia...) eller uioEduOffering (Undervisningsenhet - emne) objekter.
+Hver av disse vil ha en uioEduCourseOffering verdi som inneholder en
+URN som unikt identifiserer dette studie-elementet.
+
+ldap-person-dumpen vil generere eduCourseMember atributter med verdi
+role@eduCourseOffering, der eduCourseOffering er URN-en over, og
+role=Learner for studenter og Instructor for gruppe-lærer/foreleser.
+
+--aktivitetfile fname : xml fil med undervisningsaktiviteter
+--enhetfile fname : xml fil med undervisningsenheter
+--emnefile fname : xml fil med emner
+--destfile fname.ldif : trigger generering av ldif fil med angitt navn
+"""
+
+logger = Factory.get_logger("cronjob")
+db = Factory.get('Database')()
+group = Factory.get('Group')(db)
+
+locale.setlocale(locale.LC_CTYPE, ('en_US', 'iso88591'))  # norsk "Ø".lower()
+
+class CerebrumGroupInfo(object):
+    ## Fra generate_fronter_full.py:214
+    ##             id_seq = (self.EMNE_PREFIX, enhet['institusjonsnr'],
+    ##                       enhet['emnekode'], enhet['versjonskode'],
+    ##                       enhet['terminkode'], enhet['arstall'],
+    ##                       enhet['terminnr'])
+    ##             enhet_id = ":".join([str(x) for x in id_seq]).lower()
+
+    ## 1144:       enhstud = "uio.no:fs:%s:student" % enhet_id.lower()
+    ## 982:        aktstud = "uio.no:fs:%s:student:%s" % (enhet_id.lower(), aktkode.lower())
+
+    ##     752523 |           15 | uio.no:fs:kurs:185:inf-mat2351:1:vår:2007:1:aktivitetsansvar:2-2
+    ##     752521 |           15 | uio.no:fs:kurs:185:inf-mat2351:1:vår:2007:1:enhetsansvar
+    ##     752522 |           15 | uio.no:fs:kurs:185:inf-mat2351:1:vår:2007:1:student
+    ##     752526 |           15 | uio.no:fs:kurs:185:inf-mat2351:1:vår:2007:1:student:2-1
+    PREFIX = "uio.no:fs:kurs:"
+    id_key_seq = ('institusjonsnr', 'emnekode', 'versjonskode',
+                  'terminkode', 'arstall', 'terminnr')
+
+    def __init__(self):
+        self._emne_key2dta = {}
+        for row in group.search(name="%s%%" % CerebrumGroupInfo.PREFIX):
+            name = row['name'][len(CerebrumGroupInfo.PREFIX):]
+            emne_key = name.split(":")[:len(CerebrumGroupInfo.id_key_seq)]
+            emne_val = name.split(":")[len(CerebrumGroupInfo.id_key_seq):]
+            self._emne_key2dta.setdefault(tuple(emne_key), []).append(
+                {'group_id': int(row['group_id']),
+                 'emne_val': emne_val})
+##         for k, v in self._emne_key2dta.items():
+##             logger.debug("Emne "+repr(k)+" -> "+repr(v))
+
+    def find_group_by_undervisningsenhet(
+        self, institusjonsnr, emnekode, versjonskode, terminkode,
+        arstall, terminnr, persontype):
+        """Returnerer entity-id for aktuell gruppe.
+        persontype er en av ('enhetsansvar', 'student').  De øvrige
+        verdiene tilsvarer kolonner i FS
+        """
+        rows = self._emne_key2dta.get(
+            (institusjonsnr, emnekode, versjonskode, terminkode,
+             arstall, terminnr), [])
+##         logger.debug("nokkel: %s" % repr((institusjonsnr, emnekode, versjonskode, terminkode,
+##              arstall, terminnr)))
+##         logger.debug("Leter i %s" % repr(rows))
+        for dta in rows:
+            if(len(dta['emne_val']) == 1):
+                if persontype == dta['emne_val'][0]:
+                    return dta['group_id']
+        return None
+
+    def find_group_by_undervisningsaktivitet(
+        self, institusjonsnr, emnekode, versjonskode, terminkode,
+        arstall, terminnr, aktkode, persontype):
+        """Returnerer entity-id for aktuell gruppe.
+        persontype er en av ('aktivitetsansvar', 'student').  De
+        øvrige verdiene tilsvarer kolonner i FS
+        """
+        rows = self._emne_key2dta.get(
+            (institusjonsnr, emnekode, versjonskode, terminkode,
+             arstall, terminnr), [])
+##         logger.debug("nokkel: %s -- aktkode=%s" % (repr((institusjonsnr, emnekode, versjonskode, terminkode,
+##              arstall, terminnr)), aktkode))
+##         logger.debug("Leter i %s" % repr(rows))
+        for dta in rows:
+            if(len(dta['emne_val']) == 2):
+                if (persontype, aktkode) == tuple(dta['emne_val']):
+                    return dta['group_id']
+        return None
+
+##  1. Lage et "Offerings & sections" tre med informasjon om de
+##  undervisningsenheter og undervisningsaktiviteter som er definert
+##  ved UiO.
+
+class StudinfoParsers(object):
+    def __init__(self, emne_file, aktivitet_file, enhet_file):
+        self.emnekode2info = self._parse_emner(emne_file)
+        self.undervisningsaktiviteter = self._parse_undervisningsaktivitet(aktivitet_file)
+        self.undervisningsenheter = self._parse_undervisningenheter(enhet_file)
+        # The current emne query does not fetch emnenavn_bokmal.  If it did,
+        # we could avoid this pre-parsing and use generators instead
+        for entry in self.undervisningsenheter:
+            tmp =  self.emnekode2info.get(entry['emnekode'])
+            if not tmp:
+                logger.warn("Enhet for ukjent emne: %s" % entry)
+            else:
+                tmp['emnenavn_bokmal'] = entry['emnenavn_bokmal']
+    
+    def _parse_emner(self, fname):
+        logger.debug("Parsing %s" % fname)
+        emnekode2info = {}
+        def got_emne(dta, elem_stack):
+            entry = elem_stack[-1][-1]
+            sko = "%02i%02i%02i" % (int(entry['faknr_reglement']),
+                                    int(entry['instituttnr_reglement']),
+                                    int(entry['gruppenr_reglement']))
+            emnekode2info[entry['emnekode']] = {
+                'sko': sko,
+                'studienivakode': entry['studienivakode']
+                }
+
+        cfg = [(['data', 'emne'], got_emne)]
+        GeneralXMLParser(cfg, fname)
+        return emnekode2info
+
+    def _parse_undervisningsaktivitet(self, fname):
+        logger.debug("Parsing %s" % fname)
+        ret = []
+        def got_aktivitet(dta, elem_stack):
+            entry = elem_stack[-1][-1]
+            ret.append(entry)
+
+        cfg = [(['data', 'aktivitet'], got_aktivitet)]
+        GeneralXMLParser(cfg, fname)
+        return ret
+
+    def _parse_undervisningenheter(self, fname):
+        logger.debug("Parsing %s" % fname)
+        ret = []
+        def got_enhet(dta, elem_stack):
+            entry = elem_stack[-1][-1]
+            ret.append(entry)
+
+        cfg = [(['data', 'enhet'], got_enhet)]
+        GeneralXMLParser(cfg, fname)
+        return ret
+
+def gen_undervisningsaktivitet(cgi, sip, out):
+    # uioEduSection - Undervisningsaktivitet (instansiering av gruppe,
+    #                 kollokvia, lab, skrivekurs, forelesning)
+    # access_FS.py:Undervisning.list_aktiviteter
+    #
+    # uioEduCourseCode - FS.emne.emnekode
+    # uioEduCourseAdministrator - (FS.emne.*_reglement (6 siffer)).
+    # uioEduCourseLevel - (FS.emne.studienivakode)
+    # uioEduCourseName - (FS.emne.emnenavn_bokmal)
+    # uioEduCourseSectionName - (FS.undaktivitet.aktivitetsnavn)
+    # uioEduCourseOffering - urn:mace:uio.no:section:<noe>
+    n = 0
+    for entry in sip.undervisningsaktiviteter:
+        emne = sip.emnekode2info[entry['emnekode']]
+        aktivitet_id = []
+        for persontype in ('aktivitetsansvar', 'student'):
+            args = [entry[x] for x in CerebrumGroupInfo.id_key_seq]
+            args.extend((entry['aktivitetkode'], persontype))
+            args = [x.lower() for x in args]
+            entity_id = cgi.find_group_by_undervisningsaktivitet(*args)
+            if entity_id is None:
+                logger.warn("Ikke cerebrum-data for %s" % repr(args))
+            else:
+                aktivitet_id.append("%i" % entity_id)
+        if len(aktivitet_id) != 2:
+            continue
+        aktivitet_id = "_".join(aktivitet_id)
+        out.write("dn: cn=ua-%i,cn=course,dc=uio,dc=no\n" % n)
+        out.write("objectClass: top\n")
+        out.write("objectClass: uioEduSection\n")
+        out.write("uioEduCourseCode: %s\n" % entry['emnekode'])
+        out.write("uioEduCourseAdministrator: %s\n" % emne['sko'])
+        out.write("uioEduCourseLevel: %s\n" % emne['studienivakode'])
+        out.write("uioEduCourseName: %s\n" % emne['emnenavn_bokmal'])
+        out.write("uioEduCourseSectionName: %s\n" % entry['aktivitetsnavn'])
+        urn = 'urn:mace:uio.no:section:aktivitet-%s' % aktivitet_id
+        out.write("uioEduCourseOffering: %s\n\n" % urn)
+        n += 1
+
+def gen_undervisningsenhet(cgi, sip, out):
+    # uioEduOffering - Undervisningsenhet (instansiering av et emne)
+    # access_FS.py:Undervisning.list_undervisningenheter
+    #
+    # uioEduCourseCode, uioEduCourseAdministrator, uioEduCourseLevel,
+    # uioEduCourseName - som for Undervisningsaktivitet
+    # uioEduCourseOffering - urn:mace:uio.no:offering:<noe>
+    n = 0
+    for entry in sip.undervisningsenheter:
+        emne = sip.emnekode2info.get(entry['emnekode'])
+        if not emne:
+            continue # warned erlier
+        aktivitet_id = []
+        for persontype in ('student', 'enhetsansvar'):
+            args = [entry[x] for x in CerebrumGroupInfo.id_key_seq]
+            args.append(persontype)
+            args = [x.lower() for x in args]
+            entity_id = cgi.find_group_by_undervisningsenhet(*args)
+            if entity_id is None:
+                logger.warn("Ikke cerebrum-data for %s" % repr(args))
+            else:
+                aktivitet_id.append("%i" % entity_id)
+        if len(aktivitet_id) != 2:
+            continue
+        aktivitet_id = "_".join(aktivitet_id)
+        out.write("dn: cn=ue-%i,cn=course,dc=uio,dc=no\n" % n)
+        out.write("objectClass: top\n")
+        out.write("objectClass: uioEduOffering\n")
+        out.write("uioEduCourseCode: %s\n" % entry['emnekode'])
+        out.write("uioEduCourseAdministrator: %s\n" % emne['sko'])
+        out.write("uioEduCourseLevel: %s\n" % emne['studienivakode'])
+        out.write("uioEduCourseName: %s\n" % emne['emnenavn_bokmal'])
+        urn = 'urn:mace:uio.no:offering:enhet-%s' % aktivitet_id
+        out.write("uioEduCourseOffering: %s\n\n" % urn)
+        n += 1
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], '', [
+            'help', 'aktivitetfile=', 'enhetfile=', 'emnefile=', 'destfile='])
+    except getopt.GetoptError:
+        usage(1)
+
+    for opt, val in opts:
+        if opt in ('--help',):
+            usage()
+        elif opt in ('--aktivitetfile',):
+            aktivitetfile = val
+        elif opt in ('--enhetfile',):
+            enhetfile = val
+        elif opt in ('--emnefile',):
+            emnefile = val
+        elif opt in ('--destfile',):
+            destfile = file(val, "w")
+            cgi = CerebrumGroupInfo()
+            sip = StudinfoParsers(emnefile, aktivitetfile, enhetfile)
+            gen_undervisningsaktivitet(cgi, sip, destfile)
+            gen_undervisningsenhet(cgi, sip, destfile)
+            destfile.close()
+    if not opts:
+        usage(1)
+
+def usage(exitcode=0):
+    print _doc__
+    sys.exit(exitcode)
+
+if __name__ == '__main__':
+    main()
+
+# no/uio/import_from_FS.py --db-service FSPROD.uio.no --db-user ureg2000 --misc-func undervisning.list_aktiviteter --misc-tag aktivitet --misc-file undervisningsaktiviteter.xml --misc-func undervisning.list_undervisningenheter --misc-tag enhet --misc-file undervisningenheter.xml
