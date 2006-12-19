9290417a3a5f4d9e6764a5a1ec201d5d0d164c38
Bjørn Ove Grøtan <bgrotan@ulrik.uio.no>
2006-12-19 15:43:13 +0000

    Prompt for a password, with echo turned off - using build-in getpass- module from Python.
    http://www.python.org/doc/current/lib/module-getpass.html
    Will also work for Windows using the msvcrt module as well as
    Mac using the EasyDialogs.AskPassword if available.

--- spine/examples/python/client/passwd.py
+++ spine/examples/python/client/passwd.py
@@ -21,19 +21,21 @@
 import os
 import sys
 import SpineClient
+from getpass import getuser,getpass
 
 ior_file = '/tmp/spine.ior'
 tmp_dir = os.path.expanduser('~/tmp')
 spine = SpineClient.SpineClient(ior_file, idl_path=tmp_dir).connect()
 
-print 'Old password: ',
-passwd = sys.stdin.readline().strip()
-session = spine.login(os.getlogin(), passwd)
-print 'New password: ',
-passwd = sys.stdin.readline().strip()
-print 'Repeat new password: ',
-passwd2 = sys.stdin.readline().strip()
+username = getuser()
+passwd = getpass('Old password: ')
+session = spine.login(username, passwd)
+passwd = getpass('Enter new password: ')
+passwd2 = getpass('Retype new password: ')
 
 if passwd2 == passwd:
     tr = session.new_transaction()
-    tr.get_commands().get_user_by_name(os.getlogin()).set_password(passwd)
+    tr.get_commands().get_user_by_name(username).set_password(passwd)
+else:
+    print 'Password mismatch'
+    sys.exit()
