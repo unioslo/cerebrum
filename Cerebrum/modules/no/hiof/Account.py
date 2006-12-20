34e59c3cc20e4c0114240d4a509775ab27991c65
Roger Hansen <rogerha@usit.uio.no>
2006-12-20 12:38:20 +0000

    Mixin class for HiOf. Method illegal_name is there to make it possible to check if usernames are sane.

---
+++ Cerebrum/modules/no/hiof/Account.py
@@ -0,0 +1,49 @@
+# -*- coding: iso-8859-1 -*-
+# Copyright 2003-2005 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+""""""
+
+import re
+
+from Cerebrum import Account
+
+class AccountHiOfMixin(Account.Account):
+    """Account mixin class providing functionality specific to HiOf.
+
+    The methods of the core Account class that are overridden here,
+    ensure that any Account objects generated through
+    Cerebrum.Utils.Factory.get() provide functionality that reflects
+    the policies at HiOf.
+
+    """
+
+    def illegal_name(self, name):
+        # Avoid circular import dependency
+        from Cerebrum.modules.PosixUser import PosixUser
+
+        if isinstance(self, PosixUser):
+            # TODO: Kill the ARsystem user to limit range og legal characters
+            if len(name) > 8:
+                return "too long (%s)" % name
+            if re.search("^[^A-Za-z]", name):
+                return "must start with a character (%s)" % name
+            if re.search("[^A-Za-z0-9\-_]", name):
+                return "contains illegal characters (%s)" % name
+        return False
+
