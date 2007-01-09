3ffaa17ed3541523382519f3dbaf27134549df28
Alf B. Lervåg <alfborge@unknown>
2007-01-09 14:09:31 +0000

    Skrevet om slik at Run bruker refleksjon for å finne testene.  Dette gjør det mye lettere å legge til nye testklasser samt nye tester.
    Det er ganske mange tester som er fullstendig feilskrevet og som feiler i
    Cerebrum/tests.  Det er på høy tid med en opprydning slik at man kan begynne å
    bruke tester i utviklingsarbeidet.
    
    Call to arms!

--- Cerebrum/tests/Run.py
+++ Cerebrum/tests/Run.py
@@ -21,30 +21,16 @@
 
 import unittest
 
-from Cerebrum.tests import EntityTestCase
-from Cerebrum.tests.OUTestCase import OUTestCase
-from Cerebrum.tests.PersonTestCase import PersonTestCase
-from Cerebrum.tests.AccountTestCase import AccountTestCase
-from Cerebrum.tests import GroupTestCase
-from Cerebrum.tests.SQLDriverTestCase import SQLDriverTestCase
-
-def suite():
-    """Returns a suite containing all the test cases in this module.
-       It can be a good idea to put an identically named factory function
-       like this in every test module. Such a naming convention allows
-       automation of test discovery.
-    """
-
-    #suite1 = SQLDriverTestCase.suite()
-    suite2 = OUTestCase.suite()
-    suite3 = PersonTestCase.suite()
-    suite4 = AccountTestCase.suite()
-    suite5 = GroupTestCase.suite()
-    suite6 = EntityTestCase.suite()
-    return unittest.TestSuite((suite6, suite2, suite3, suite4, suite5))
-
 if __name__ == '__main__':
-    # When this module is executed from the command-line, run all its tests
-    unittest.main(defaultTest='suite')
+    modules = [
+        'Cerebrum.tests.EntityTestCase',
+        'Cerebrum.tests.GroupTestCase',
+        'Cerebrum.tests.OUTestCase',
+        'Cerebrum.tests.PersonTestCase',
+        'Cerebrum.tests.AccountTestCase',
+        'Cerebrum.tests.SQLDriverTestCase',
+    ]
+    suite = unittest.TestLoader().loadTestsFromNames(modules)
+    unittest.TextTestRunner(verbosity=1).run(suite)
 
 # arch-tag: 0a2c5fe2-c56e-43a0-b072-90e6d3fbe5b6
