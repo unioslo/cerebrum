bd42212f4d1bf829cc90668adee8acc88a7dfc08
Jasmina Hodzic <jasminah@usit.uio.no>
2007-01-26 22:22:01 +0000

    a version that work

--- contrib/no/nmh/ADgroupSync.py
+++ contrib/no/nmh/ADgroupSync.py
@@ -1,5 +1,23 @@
 #!/local/bin/python
 # -*- coding: iso-8859-1 -*-
+#
+# Copyright 2006 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 import getopt
 import sys
@@ -10,201 +28,186 @@ import operator
 
 from Cerebrum import Errors
 from Cerebrum.Utils import Factory
-db = Factory.get('Database')()
-db.cl_init(change_program="adfgsync")
-group = Factory.get('Group')(db)
-co = Factory.get('Constants')(db)
-
-PASSORD = 'cerebrum:Cere7est'
-
-server = xmlrpclib.Server("https://%s@%s:%i" % (
-	PASSORD,
-	cereconf.AD_SERVER_HOST,
-	cereconf.AD_SERVER_PORT))
-
-#logger = Factory.get_logger("console")
 
 
 def fetch_cerebrum_data(spread):
-	
-	return group.search(spread)
+    return group.search(spread)
 
 
 def fetch_ad_data():
-	return server.listObjects('group', True)
+    temp = server.listObjects('group', True)
+    ad_groups = []
+    for t in temp:
+        rest, name = t.split(',')[0].split('=')
+        ad_groups.append(name)
+    return ad_groups
 
 
 def fetch_ad_usrdata():
-	#Empty the userattributes dict. Sentence below results in
-	#distinguishedName and the default value sAMAccountName.
-	server.setUserAttributes()
-
-	return server.listObjects('user', True)
-
+    # remove all attributes from userattributes-dict except
+    # distinguishedName and the default value sAMAccountName.
+    #
+    server.setUserAttributes()
+    return server.listObjects('user', True)
 
 
 def full_sync(delete_groups, dry_run):
-    
-	#Fetch AD-data.
-	adgroups = fetch_ad_data()		
-        print "Fetched %i ad-groups" % len(adgroups)
-#	logger.info("Fetched %i ad-groups" % len(adgroups))	
+    # fetch group info from AD
+    #
+    adgroups = fetch_ad_data()
+    logger.debug("Fetched %i groups from AD", len(adgroups))	
 
-        for row in adgroups:
-		print row
-
-	#Fetch cerebrum data.
-	print "group_spread:%i" % int(co.spread_ad_group)
-	cerebrumgroups = fetch_cerebrum_data(int(co.spread_ad_group))
-        print "Fetched %i groups" % len(cerebrumgroups)
-#	logger.info("Fetched %i groups" % len(cerebrumgroups))
-
-
-    #compare cerebrum and ad-data.
-	changelist = compare(delete_groups, cerebrumgroups, adgroups)
-
-	adusers = None
-        print "Found %i number of changes" % len(changelist)
-#	logger.info("Found %i number of changes" % len(changelist))
+    # fetch group info from cerebrum
+    #
+    cerebrumgroups = fetch_cerebrum_data(int(co.spread_ad_group))
+    logger.info("Fetched %i groups from Cerebrum" % len(cerebrumgroups))
+        
+    # compare group data in cerebrum and AD
+    #
+    changelist = compare(delete_groups, cerebrumgroups, adgroups)
+    logger.info("Found %i changes in group info", len(changelist))
 
 
-    #Perform changes. 	
-	perform_changes(changelist, dry_run)	
-	sync_groups(cerebrumgroups, dry_run)
-
-	cerebrumgroups = None
+    # execute change-commands in AD
+    #
+    perform_changes(changelist, dry_run)	
+    sync_groups(cerebrumgroups, dry_run)
 
 
 def perform_changes(changelist, dry_run):
-	for chg in changelist:
-		if chg['type'] == 'createObject':
-			ret = run_cmd(chg['type'], dry_run, 'Group', 
-						'OU=grupper,OU=cerebrum,DC=NMH-TEST,DC=no', 
-						chg['sAMAccountName'])
-			if not ret[0]:
-				print "Warning(creObj): %s" % ret[1]
-				#logger.warn(ret[1])
-		elif chg['type'] == 'deleteObject':
-			run_cmd('bindObject', dry_run, chg['distinguishedName'])
-			ret = run_cmd(chg['type'], dry_run)
-			if not ret[0]:
-				#logger.warn(ret[1])
-				print "Warning(delObj) %s" % ret[1]
-		else:
-			#logger.warn("unknown type: %s" % chg['type'])
-		        print "unknown type: %s" % chg['type']
-			
-	print "perform changes done"
+    if dry_run:
+        logger.info("Would perform %d changes", len(changelist))
+    for chg in changelist:
+        if chg['type'] == 'createObject':
+            ret = run_cmd(chg['type'],
+                          dry_run,
+                          'Group', 
+                          'OU=grupper,OU=cerebrum,DC=NMH-TEST,DC=no', 
+                          chg['distinguishedName'])
+            if not ret[0]:
+                logger.error("Could not create group: %s", chg['distinguishedName'])
+            else:
+                logger.info("Created group %s", chg['distinguishedName']) 
+        elif chg['type'] == 'deleteObject':
+            adname = 'CN=' + chg['distinguishedName'] +',%s,%s' % (cereconf.AD_DEFAULT_GROUP_OU,
+                                                                   cereconf.AD_LDAP)
+            run_cmd('bindObject', dry_run, adname)
+            ret = run_cmd(chg['type'], dry_run)
+            if not ret[0]:
+                logger.error("Could not delete object: %s", ret[1])
+            else:
+                logger.info("Deleted group %s", chg['distinguishedName']) 
+        else:
+            logger.warn("Unknown change-type: %s", chg['type'])
+
 
 def sync_groups(cerebrumgroups, dry_run):
-	#To reduce traffic, we send current list of groupmembers to AD, and the
-	#server ensures that each group have correct members.   
+    # To reduce traffic send current list of groupmembers to AD
+    # The server ensures that each group has correct members.   
 
-	#print "Fetching users in AD"
-	#users_in_ad = fetch_ad_usrdata()
+    for g in cerebrumgroups:
+        # Only interested in union members(this is only type in use)
+        #
+        # logger.debug("Getting group: %s", grp_name)
+        group.clear()
+        group.find(g['group_id'])				 
+        user_memb = group.list_members(spread=co.spread_ad_account, get_entity_name=True)
+        group_memb = group.list_members(spread=co.spread_ad_group, get_entity_name=True)
+        members = []
+        for usr in user_memb[0]:
+            # TODO: How to treat quarantined users???, some exist in AD, 
+            # others do not. They generate errors when not in AD. We still
+            # want to update group membership if in AD.
+            # FIXME: it is rather ugly to assume that user name i in usr[2], this
+            # should be changed (user_memb restructured)
+            #
+            members.append(usr[2])
+            # logger.debug("Registered all member accounts")
 
+        for grp in group_memb[0]:
+            members.append('%s%s' % (grp[2], cereconf.AD_GROUP_POSTFIX))
+            #logger.debug("Registered all member groups")
+        # Try to locate group object in AD
+        #
+        dn = None
+        dn = server.findObject('%s%s' % (g['name'], cereconf.AD_GROUP_POSTFIX))
 
-	for (grp_id, grp_name, grp_desc) in cerebrumgroups:
-		#Only interested in union members(believe this is only type in use)
-		print "group:%s" % grp_name
-
-		group.clear()
-		group.find(grp_id)				 
-		user_memb = group.list_members(spread=co.spread_ad_account, get_entity_name=True)
-		group_memb = group.list_members(spread=co.spread_ad_group, get_entity_name=True)
-		members = []
-		for usr in user_memb[0]:
-		    #TODO: How to treat quarantined users???, some exist in AD, 
-		    #others do not. They generate errors when not in AD. We still
-		    #want to update group membership if in AD.
-
-			#if usr[2] in users_in_ad:
-			members.append(usr[2])
-
-		for grp in group_memb[0]:
-			print grp
-			members.append('%s%s' % (grp[2],''))#cereconf.AD_GROUP_POSTFIX))
-	
-		dn = server.findObject('%s%s' % (grp_name,''))# cereconf.AD_GROUP_POSTFIX))
-		if not dn:
-                        print  "unknown group: %s%s" % (grp_name,'')# cereconf.AD_GROUP_POSTFIX)	
-			#logger.debug("unknown group: %s%s" % (grp_name,'')# cereconf.AD_GROUP_POSTFIX))	
-		else:
-			server.bindObject(dn)
-			res = server.syncMembers(members, False)
-			if not res[0]:
-				print "syncMembers %s failed for:%r" % (dn,res[1:])
-				#logger.debug("syncMembers %s failed for:%r" % (dn,res[1:]))
+        if not dn:
+            logger.warn("No such group: %s%s" % (g['name'], cereconf.AD_GROUP_POSTFIX))
+        else:
+            server.bindObject(dn)
+            res = server.syncMembers(members, False)
+            if not res[0]:
+                logger.warn("Failed to syncronize members for %s", dn)
+            else:
+                logger.info("Succesfully syncronized group %s", dn)
 
 
 def run_cmd(command, dry_run, arg1=None, arg2=None, arg3=None):
+    if dry_run:
+        logger.info('Dryrun: server.%s(%s,%s,%s)' % (command, arg1, arg2, arg3))
+        #Assume success on all changes.
+        #
+        return (True, command)
+    else:
+        cmd = getattr(server, command)
+        if arg1 == None:
+            ret = cmd()
+        elif arg2 == None:
+            ret = cmd(arg1)
+        elif arg3 == None:
+            ret = cmd(arg1, arg2)
+        else:
+            ret = cmd(arg1, arg2, arg3)
 
-	if dry_run:
-		print('server.%s(%s,%s,%s)' % (command, arg1, arg2, arg3))
-		#Assume success on all changes.
-		return (True, command)
-	else:
-		cmd = getattr(server, command)
-		if arg1 == None:
-			ret = cmd()
-		elif arg2 == None:
-			ret = cmd(arg1)
-		elif arg3 == None:
-			ret = cmd(arg1, arg2)
-		else:
-			ret = cmd(arg1, arg2, arg3)
-
-		return ret
+        return ret
 
 
-def compare(delete_groups,cerebrumgrp,adgrp):
-	print "compare started"
-	changelist = []	 	
-
-	for (grp_id, grp, description) in cerebrumgrp:
-
-		if 'CN=%s%s,%s,%s' % (grp,'','OU=grupper','OU=cerebrum,DC=NMH-TEST,DC=no'# Trenger fiks av cereconf.py
-				      #cereconf.AD_GROUP_POSTFIX, 
-				      #cereconf.AD_DEFAULT_GROUP_OU,
-				      #cereconf.AD_LDAP
-					   ) in adgrp:
-			adgrp.remove('CN=%s%s,%s,%s' % (grp,'','OU=grupper','OU=cerebrum,DC=NMH-TEST,DC=no'
-							#cereconf.AD_GROUP_POSTFIX,
-							#cereconf.AD_DEFAULT_GROUP_OU,
-							#cereconf.AD_LDAP
-							))
-		else:
-			#Group not in AD, create.
-			print "match failed"
-			changelist.append({'type': 'createObject',
-					   'sAMAccountName' : '%s%s' % (grp,''),
-									#cereconf.AD_GROUP_POSTFIX),
-					   'description' : description})
- 	
-	#The remaining groups is surplus in AD.
-	for adg in adgrp:
-		#if adg.find(cereconf.AD_DO_NOT_TOUCH) >= 0:			
-		#	pass
-		#elif adg.find('CN=Builtin,%s' % cereconf.AD_LDAP) >= 0:
-		#	pass
-		#else:
-		changelist.append({'type' : 'deleteObject','distinguishedName' : adg}) 
-
-	return changelist
-
+def compare(delete_groups, cerebrumgrp, adgrp):
+    changelist = []
+    for g in cerebrumgrp:
+        adcn = g['name'] +  cereconf.AD_GROUP_POSTFIX
+        if adcn in adgrp:
+            logger.debug("Found group %s, removing from update list", g['name'])
+            adgrp.remove(adcn)
+        else:
+            logger.debug("Could not find group %s, will create", adcn)
+            changelist.append({'type': 'createObject',
+                               'distinguishedName' : '%s%s' % (g['name'], cereconf.AD_GROUP_POSTFIX),
+                               'description' : g['description']})
+    # the remaining groups are not registered in cerebrum and
+    # will be deleted
+    #
+    for adg in adgrp:
+        changelist.append({'type' : 'deleteObject',
+                           'distinguishedName' : adg}) 
+    return changelist
 
 
 def main():
+    global db, co, group, logger, server
+    
+    delete_groups = False
+    dry_run = False	
 
+    db = Factory.get('Database')()
+    db.cl_init(change_program="adgsync")
+    group = Factory.get('Group')(db)
+    co = Factory.get('Constants')(db)
+    logger = Factory.get_logger("cronjob")
+
+    passwd = db._read_password(cereconf.AD_SERVER_HOST,
+                               cereconf.AD_SERVER_UNAME)
+
+    server = xmlrpclib.Server("https://%s@%s:%i" % (passwd,
+                                                    cereconf.AD_SERVER_HOST,
+                                                    cereconf.AD_SERVER_PORT))
     try:
         opts, args = getopt.getopt(sys.argv[1:], '',
                                    ['delete_groups','help', 'dry_run'])
     except getopt.GetoptError:
         usage(1)
 
-    delete_groups = False
-    dry_run = False	
-	
     for opt, val in opts:
         if opt == '--delete_groups':
             delete_groups = True
@@ -213,18 +216,19 @@ def main():
         elif opt == '--dry_run':
             dry_run = True
 
+    logger.info("Syncronizing all groups")
     full_sync(delete_groups, dry_run)
-   
+    logger.info("All done")
 
-
+    
 def usage(exitcode=0):
     print """Usage: [options]
     --delete_groups
     --dry_run
     --help
     """
-
     sys.exit(exitcode)
+
 
 if __name__ == '__main__':
     main()
