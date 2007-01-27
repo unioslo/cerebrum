777c53fd967291b77d02925468a60eb35b837eb7
Jasmina Hodzic <jasminah@usit.uio.no>
2007-01-27 23:25:55 +0000

    bofhd-cmds for NMH

---
+++ Cerebrum/modules/no/nmh/bofhd_nmh_cmds.py
@@ -0,0 +1,238 @@
+# -*- coding: iso-8859-1 -*-
+import cereconf
+
+import mx
+import pickle
+from Cerebrum.Utils import Factory
+from Cerebrum.modules.bofhd.errors import CerebrumError, PermissionDenied
+from Cerebrum import Constants
+from Cerebrum import Utils
+from Cerebrum import Cache
+from Cerebrum import Errors
+from Cerebrum.modules.bofhd.cmd_param import *
+from Cerebrum.modules.no.nmh import bofhd_nmh_help
+from Cerebrum.Constants import _CerebrumCode, _SpreadCode
+from Cerebrum.modules.bofhd.auth import BofhdAuth
+from Cerebrum.modules.bofhd.utils import _AuthRoleOpCode
+
+def format_day(field):
+    fmt = "yyyy-MM-dd"                  # 10 characters wide
+    return ":".join((field, "date", fmt))
+
+class BofhdExtension(object):
+    OU_class = Utils.Factory.get('OU')
+    Account_class = Factory.get('Account')
+    Group_class = Factory.get('Group')
+    all_commands = {}
+
+    copy_commands = (
+        #
+        # copy relevant access-cmds and util methods
+        #
+        'access_disk', 'access_group', 'access_ou', 'access_user',
+        'access_global_group', 'access_global_ou', '_list_access',
+        'access_grant', 'access_revoke', '_manipulate_access',
+        '_get_access_id', '_validate_access', '_get_access_id_disk',
+        '_validate_access_disk', '_get_access_id_group', '_validate_access_group',
+        '_get_access_id_global_group', '_validate_access_global_group',
+        '_get_access_id_ou', '_validate_access_ou', '_get_access_id_global_ou',
+        '_validate_access_global_ou', 'access_list_opsets', 'access_show_opset',
+        'access_list', '_get_auth_op_target', '_grant_auth', '_revoke_auth',
+        '_get_opset',
+        #
+        # copy relevant group-cmds and util methods
+        #
+        'group_add', 'group_gadd', '_group_add', '_group_add_entity',
+        '_group_count_memberships', 'group_add_entity', 'group_create',
+        'group_delete', 'group_remove', 'group_gremove', '_group_remove',
+        '_group_remove_entity', 'group_remove_entity', 'group_info',
+        'group_list', 'group_list_expanded', 'group_search', 'group_set_description',
+        'group_memberships', '_get_group', '_get_group_opcode',
+        #
+        # copy relevant misc-cmds and util methods
+        #
+        'misc_affiliations', 'misc_check_password', 'misc_clear_passwords',
+        'misc_stedkode', 'misc_verify_password',
+        #
+        # copy relevant person-cmds and util methods
+        #
+        'person_accounts', 'person_affiliation_add', '_person_affiliation_add_helper',
+        'person_affiliation_remove', 'person_create',
+        'person_find', 'person_info', 'person_student_info',
+        #
+        # copy relevant quarantine-cmds and util methods
+        #
+        'quarantine_disable', 'quarantine_list', 'quarantine_remove',
+        'quarantine_set', 'quarantine_show',
+        #
+        # copy relevant user-cmds and util methods
+        #
+        'user_affiliation_add', '_user_affiliation_add_helper',
+        'user_affiliation_remove', 'user_history', 'user_info',
+        'user_find', 'user_password', 'user_set_expire',
+        '_user_create_prompt_func_helper', 'user_create_basic_prompt_func',
+        '_user_create_set_account_type',
+        'user_set_owner', 'user_set_owner_prompt_func',
+        #
+        # copy relevant spread-cmds and util methods
+        #
+        'spread_list', 'spread_add', 'spread_remove',
+        #
+        # copy relevant helper-functions
+        #
+         '_find_persons', '_get_account', '_get_ou', '_format_ou_name',
+        '_get_person', '_get_disk', '_get_group', '_map_person_id', '_get_entity',
+         '_get_boolean', '_entity_info', 'num2str', '_get_affiliationid',
+        '_get_affiliation_statusid', '_parse_date', '_today', 'entity_history',
+        '_format_changelog_entry', '_format_from_cl',
+         '_get_entity_name', '_get_group_opcode', '_get_name_from_object',
+        '_get_constant', '_is_yes', '_remove_auth_target',
+        '_remove_auth_role', '_get_cached_passwords', '_parse_date_from_to'
+        )
+
+    def __new__(cls, *arg, **karg):
+        # A bit hackish.  A better fix is to split bofhd_uio_cmds.py
+        # into seperate classes.
+        from Cerebrum.modules.no.uio.bofhd_uio_cmds import BofhdExtension as \
+             UiOBofhdExtension
+
+        non_all_cmds = ('num2str', 'user_set_owner_prompt_func',
+                        'user_create_basic_prompt_func',)
+        for func in BofhdExtension.copy_commands:
+            setattr(cls, func, UiOBofhdExtension.__dict__.get(func))
+            if func[0] != '_' and func not in non_all_cmds:
+                BofhdExtension.all_commands[func] = UiOBofhdExtension.all_commands[func]
+        x = object.__new__(cls)
+        return x
+
+    def __init__(self, server, default_zone='nmh'):
+        self.server = server
+        self.logger = server.logger
+        self.util = server.util
+        self.db = server.db
+        self.const = Factory.get('Constants')(self.db)
+        self.ba = BofhdAuth(self.db)
+
+        # From uio
+        self.num2const = {}
+        self.str2const = {}
+        for c in dir(self.const):
+            tmp = getattr(self.const, c)
+            if isinstance(tmp, _CerebrumCode):
+                self.num2const[int(tmp)] = tmp
+                self.str2const[str(tmp)] = tmp
+        self._cached_client_commands = Cache.Cache(mixins=[Cache.cache_mru,
+                                                           Cache.cache_slots,
+                                                           Cache.cache_timeout],
+                                                   size=500,
+                                                   timeout=60*60)
+
+
+    def get_help_strings(self):
+        return (bofhd_nmh_help.group_help,
+                bofhd_nmh_help.command_help,
+                bofhd_nmh_help.arg_help)
+    
+    def get_commands(self, account_id):
+        try:
+            return self._cached_client_commands[int(account_id)]
+        except KeyError:
+            pass
+        commands = {}
+        for k in self.all_commands.keys():
+            tmp = self.all_commands[k]
+            if tmp is not None:
+                if tmp.perm_filter:
+                    if not getattr(self.ba, tmp.perm_filter)(account_id, query_run_any=True):
+                        continue
+                commands[k] = tmp.get_struct(self)
+        self._cached_client_commands[int(account_id)] = commands
+        return commands
+
+    def get_format_suggestion(self, cmd):
+        return self.all_commands[cmd].get_fs()
+
+
+    # misc list_passwords
+    #
+    all_commands['misc_list_passwords'] = Command(
+        ("misc", "list_passwords"),
+        fs=FormatSuggestion("%-8s %-20s %s", ("account_id", "operation", "password"),
+                            hdr="%-8s %-20s %s" % ("Id", "Operation", "Password")))
+    def misc_list_passwords(self, operator):
+        ret = self._get_cached_passwords(operator)
+        if not ret:
+            return "Password cache for this session is empty."
+        return ret
+
+    # user create prompt
+    #
+    def user_create_prompt_func(self, session, *args):
+        return self._user_create_prompt_func_helper('Account', session, *args)    
+    
+    # user create
+    #
+    # FIXME: we should be able to use uio's user create her
+    #
+    all_commands['user_create'] = Command(
+        ('user', 'create'), prompt_func=user_create_prompt_func,
+        fs=FormatSuggestion("Created account_id=%i", ("account_id",)),
+        perm_filter='is_superuser')
+    def user_create(self, operator, *args):
+        if args[0].startswith('group:'):
+            group_id, np_type, uname = args
+            owner_type = self.const.entity_group
+            owner_id = self._get_group(group_id.split(":")[1]).entity_id
+            np_type = self._get_constant(self.const.Account, np_type,
+                                         "account type")
+            affiliation = None
+            owner_type = self.const.entity_group
+        else:
+            if len(args) == 4:
+                idtype, person_id, affiliation, uname = args
+            else:
+                idtype, person_id, yes_no, affiliation, uname = args
+            person = self._get_person("entity_id", person_id)
+            owner_type, owner_id = self.const.entity_person, person.entity_id
+            np_type = None
+        account = self.Account_class(self.db)
+        account.clear()
+        if not self.ba.is_superuser(operator.get_entity_id()):
+            raise PermissionDenied("only superusers may reserve users")
+        account.populate(uname,
+                         owner_type,  # Owner type
+                         owner_id,
+                         np_type,                      # np_type
+                         operator.get_entity_id(),  # creator_id
+                         None)                      # expire_date
+        account.write_db()
+        for spread in cereconf.BOFHD_NEW_USER_SPREADS:
+            account.add_spread(self.const.Spread(spread))
+        passwd = account.make_passwd(uname)
+        account.set_password(passwd)
+        try:
+            account.write_db()
+            if affiliation is not None:
+                ou_id, affiliation = affiliation['ou_id'], affiliation['aff']
+                self._user_create_set_account_type(
+                    account, person.entity_id, ou_id, affiliation)
+        except self.db.DatabaseError, m:
+            raise CerebrumError, "Database error: %s" % m
+        operator.store_state("new_account_passwd", {'account_id': int(account.entity_id),
+                                                    'password': passwd})
+        return {'account_id': int(account.entity_id)}
+    
+    # user delete
+    #
+    all_commands['user_delete'] = Command(
+        ("user", "delete"), AccountName(), perm_filter='can_delete_user')
+    def user_delete(self, operator, accountname):
+        account = self._get_account(accountname)
+        self.ba.can_delete_user(operator.get_entity_id(), account)
+        if account.is_deleted():
+            raise CerebrumError, "User is already deleted"
+        account.expire_date = mx.DateTime.now()
+        for s in account.get_spread():
+            account.delete_spread(int(s['spread']))
+        account.write_db()
+        return "User %s queued for deletion immediately" % account.account_name
