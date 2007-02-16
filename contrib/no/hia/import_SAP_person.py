dc06e84da01cb41f79846c17b7ec311e5c7e4f90
Igor Rafienko <igorr@ulrik.uio.no>
2007-02-16 04:12:46 +0000

    Shuffling some codes around

--- contrib/no/hia/import_SAP_person.py
+++ contrib/no/hia/import_SAP_person.py
@@ -61,7 +61,7 @@ import cereconf
 from Cerebrum import Errors
 from Cerebrum.Utils import Factory
 from Cerebrum.modules.no.hia.mod_sap_utils import sap_row_to_tuple
-from Cerebrum.modules.no.hia.mod_sap_codes import SAPForretningsOmradeKode
+from Cerebrum.modules.no.Constants import SAPForretningsOmradeKode
 from Cerebrum.modules.no import fodselsnr
 
 import sys
