135ff539986afdb484bc80f6976a23bc58592ec7
Steinar Hamre <stseiham@extern.uio.no>
2007-02-21 19:35:24 +0000

    Run from cron on home servers to create home directories.

---
+++ spine/ceresync/homedir.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+# Copyright 2007 University of Oslo, Norway
+#
+# This filebackend is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+
+
+import config
+import SpineClient
+import os
+
+
+def setup_home(path, uid, gid):
+    os.mkdir(path, 0700)
+    os.chown(path, uid, gid)
+
+
+connection = SpineClient.SpineClient(config=config.conf).connect()
+session = connection.login(config.conf.get('spine', 'login'),
+                           config.conf.get('spine', 'password'))
+tr = session.new_transaction()
+cmd = tr.get_commands()
+
+try:
+    hostname = config.conf.get('homedir', 'hostname')
+except:
+    import os
+    hostname = os.uname()[1]
+
+try:
+    setup_script = config.conf.get('homedir', 'setup_script')
+except:
+    setup_script="/local/skel/bdb-setup"
+
+
+
+
+status_create_failed = tr.get_home_status("create_failed")
+status_on_disk = tr.get_home_status("on_disk")
+status_not_created = tr.get_home_status("not_created")
+
+me = cmd.get_host_by_name(hostname)
+
+hds = tr.get_home_directory_searcher()
+ds = tr.get_disk_searcher()
+ds.set_host(me)
+hds.add_join("disk", ds, "")
+hds.set_status(status_not_created)
+
+
+def get_path(hd):
+    disk = hd.get_disk()
+    home = hd.get_home()
+    if disk:
+        path = disk.get_path()
+        if home:
+            return path + home
+        else:
+            return path + "/" + hd.get_account().get_name()
+    else:
+        return home
+    
+
+
+
+
+def make_homedir(hd):
+    #path = hd.get_path() XXX
+    path = get_path(hd)
+    account = hd.get_account()
+    username = account.get_name()
+    uid = account.get_posix_uid()
+    gid = account.get_primary_group().get_posix_gid()
+    
+    if os.access(setup_script, os.F_OK):
+        r = os.system("%s %d %d %s %s" % (setup_script,
+                                          uid, gid, path, username))
+        if r<0:
+            raise "Create failed"
+    else:
+        setup_home(path, uid, gid)
+
+for hd in hds.search():
+    try:
+        make_homedir(hd)
+        hd.set_status(status_on_disk)
+    except:
+        # XXX log
+        hd.set_status(status_create_failed)
+
+tr.commit()
