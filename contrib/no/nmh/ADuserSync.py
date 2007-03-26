79a5a99dffe8bb76b5bfd52188d91807765d5b5e
Monica Stamnes <mocca@usit.uio.no>
2007-03-26 10:23:45 +0000

    Minor bugfix - employees do not have profilepath set from Cerebrum.

--- contrib/no/nmh/ADuserSync.py
+++ contrib/no/nmh/ADuserSync.py
@@ -261,13 +261,17 @@ def create_user(elem):
         elem['homeDirectory'] = "%s%s" %(cereconf.AD_HOME_DIRECTORY_STUDENT, elem['sAMAccountName'])
         elem['homeDrive'] = cereconf.AD_HOME_DRIVE_STUDENT
         elem['profilePath'] = "%s%s" % (cereconf.AD_PROFILE_PATH_STUDENT, elem['sAMAccountName'])
+    logger.debug("Creating account %s, homedir %s, homedrive %s, profilepath %s" % (elem['sAMAccountName'],
+                                                                                    elem['homeDirectory'],
+                                                                                    elem['homeDrive'],
+                                                                                    elem['profilePath']))
     else:
         elem['homeDirectory'] = "%s%s" %(cereconf.AD_HOME_DIRECTORY_ANSATT, elem['sAMAccountName'])
         elem['homeDrive'] = cereconf.AD_HOME_DRIVE_ANSATT
     logger.debug("Creating account %s, homedir %s, homedrive %s" % (elem['sAMAccountName'],
                                                                     elem['homeDirectory'],
-                                                                    elem['homeDrive'],
-                                                                    elem['profilePath']))
+                                                                    elem['homeDrive']))
+    
     ret = run_cmd('createObject', 'User', ou, elem['sAMAccountName'])
     if ret[0]:
         logger.info("Succesfully created account %s", elem['sAMAccountName'])
