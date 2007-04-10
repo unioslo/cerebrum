4158f9400080c51a4628dd9ac1c31390e7328656
Bjørn Ove Grøtan <bgrotan@ulrik.uio.no>
2007-04-10 10:08:12 +0000

    Refactoring

--- contrib/no/ntnu/clean_affiliations.py
+++ contrib/no/ntnu/clean_affiliations.py
@@ -27,7 +27,10 @@ import cerebrum_path
 from Cerebrum import Utils
 from Cerebrum import Person
 from Cerebrum import Errors
+
 import sets
+import getopt
+import sys
 
 Factory = Utils.Factory
 db = Factory.get('Database')()
@@ -39,24 +42,60 @@ p = Factory.get('Person')(db)
 auto = sets.Set()
 manual = sets.Set()
 
-for person,ou,aff,source,status,dd,cd in p.list_affiliations():
-    if source == co.system_manual:
-        manual.add((person, ou, aff))
-    else:
-        auto.add((person, ou, aff))
-
-count=0
-for person,ou,aff in manual.intersection(auto):
+def main():
     try:
-        # Use delete_date?
-        p.clear()
-        p.find(person)
-        p.delete_affiliation(ou, aff, co.system_manual)
-        p.write_db()
-        count+=1
-    except Errors.NotFoundError:
-        pass
+        opts,args = getopt.getopt(sys.argv[1:],'v:d:h',['verbose','dryrun','help'])
+    except getopt.GetoptError:
+        usage()
 
-print "removed %d manual affiliations" % count
+    dryrun = False
+    verbose = False
+    
 
-db.commit()
+    for opt,val in opts:
+        if opt in ('-d','--dryrun'):
+            dryrun = True
+        if opt in ('-v','--verbose'):
+            verbose = True
+        if opt in ('-h','--help'):
+            usage()
+
+    for person,ou,aff,source,status,dd,cd in p.list_affiliations():
+        if source == co.system_manual:
+            manual.add((person, ou, aff))
+        else:
+            auto.add((person, ou, aff))
+
+    count=0
+    # If you have 2 affiliations of same type and ou and one of them
+    # is added manually, it will be marked for deletion
+    for person,ou,aff in manual.intersection(auto):
+        try:
+            # Use delete_date?
+            p.clear()
+            p.find(person)
+            print "Deleting affiliation %s on %s of type manual for entity %s" % (aff,ou,person)
+            p.delete_affiliation(ou, aff, co.system_manual)
+            p.write_db()
+            count+=1
+        except Errors.NotFoundError:
+            pass
+            
+    if dryrun:
+        print "%d manual affiliations would have been removed" % count
+        db.rollback()
+    else:
+        print "%d manual affiliations removed from database" % count
+        db.commit()
+
+def usage():
+    print """Usage: %s
+    -d | --dry-run : if you only want to show which would have been marked for deletion - use this option.
+    -v | --verbose : print out the affiliation on ou for person that would/will be marked for deletion.
+    -h | --help    : duh
+    """ % sys.argv[0]
+    sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
