412276f5a17eef022eb89ef1867b3421d469f2ff
Igor Rafienko <igorr@ulrik.uio.no>
2007-05-03 22:36:55 +0000

    We need to keep track of people without accounts as well
    (This script is probably much slower, because of the sheer number of db requests)

--- contrib/statistics/generate_user_report.py
+++ contrib/statistics/generate_user_report.py
@@ -19,90 +19,136 @@
 # along with Cerebrum; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-# $Id$
+"""This script produces a report over people and usernames.
 
-import getopt
+This script produces a (daily?) report over people, their user names and their
+activity statuses.
+
+For each person in the database we report the following:
+
+* fnr[1]
+* all user names
+* activity status[2]
+
+[1] Should the person have multiple different fnrs, the one listed first in
+--fnr-systems is used as the selection basis. If none are specified,
+cereconf.SYSTEM_LOOKUP_ORDER is respected.
+
+[2] A *person* with at least one valid affiliation is considered active. All
+others are inactive.
+
+This job has been requested for ØFK, but it could be used at any institution
+using Cerebrum.
+"""
+
+
+
 import sys
+import getopt
+
 import cerebrum_path
-from Cerebrum import Errors
+import cereconf
 from Cerebrum.Utils import Factory
-from Cerebrum.Constants import _AuthoritativeSystemCode
-
-progname = __file__.split("/")[-1]
-
-__doc__ = """
-Usage: %s [options]
-   --fnr-src-sys code_str : identifies authoritative system for fnr [required]
-   --user-info  : dump user info
-
-This script is intended to grow, adding more options for customizing
-both output format, and the selected output fields.
-
-Example:
-  %s --fnr-src-sys FS --user-info
-""" % (progname, progname)
-
-db = Factory.get('Database')()
-co = Factory.get('Constants')(db)
-ac = Factory.get('Account')(db)
-person = Factory.get('Person')(db)
-
 logger = Factory.get_logger("cronjob")
 
-class Fetcher(object):
-    def fetch_acinfo(self):
-        ac_info = {}
-        for row in ac.search():
-            ac_info[int(row['account_id'])] = {
-                'uname': row['name']
-                }
 
-        for row in ac.list_account_home(
-                filter_expired=True, include_nohome=True):
-            if not ac_info.has_key(int(row['account_id'])):
-                continue
-            ac_info[int(row['account_id'])]['owner_id'] = int(row['owner_id'])
 
-        ownerid2fnr = {}
-        for row in person.list_external_ids(self.fnr_src_sys, id_type=co.externalid_fodselsnr):
-            ownerid2fnr[int(row['entity_id'])] = row['external_id']
+def generate_person_uname_report(stream, fnr_src_sys=None):
+    """Display statistics about people/unames.
+
+    Each person in Cerebrum gets as many entries at (s)he has usernames.
+
+    Each entry is formatted thus:
+
+    fnr:uname:aktiv/inaktiv
+
+    ... where
+
+    fnr		constants.externalid_fodselsnr
+    uname	account name owned by that person
+    aktiv	aktiv, if the *person* has at least one valid
+                affiliation. inaktiv otherwise.
+    """
+
+    db = Factory.get("Database")()
+    person = Factory.get("Person")(db)
+    acc = Factory.get("Account")(db)
+    const = Factory.get("Constants")(db)
+
+    #
+    # Get the right source system for external ids. If no source system is
+    # specified, we'll prioritise the IDs as specified in SYSTEM_LOOKUP_ORDER.
+    if fnr_src_sys:
+        source_systems = [int(getattr(const, x))
+                          for x in fnr_src_sys.split(",")]
+    else:
+        source_systems = [int(getattr(const, x))
+                          for x in cereconf.SYSTEM_LOOKUP_ORDER]
+
+    for p in person.list_persons():
+        pid = p["person_id"]
+
+        try:
+            person.clear()
+            person.find(pid)
+        except Errors.NotFoundError:
+            logger.warn("list_persons() reported a person, but person.find() "
+                        "did not find it")
+            continue
+
+        # Select fnr from the list of candidates.
+        fnrs = dict([(int(src), eid) for (junk, src, eid) in
+                     person.get_external_id(id_type=const.externalid_fodselsnr)])
+        fnr = ""
+        for system in source_systems:
+            if system in fnrs:
+                fnr = fnrs[system]
+                break
+
+        # at least one active affiliation => 'aktiv'
+        status = "inaktiv"
+        if person.get_affiliations():
+            status = "aktiv"
+
+        # for all (unexpired) accounts owned by the person...
+        accounts = acc.search(owner_id = pid)
+        for entry in accounts:
+            stream.write(":".join((fnr, entry["name"], status)))
+            stream.write("\n")
+# end generate_person_uname_report
+
+
+
+def main(argv=None):
+    """Main processing hub for program."""
+    if argv is None:
+        argv = sys.argv
         
-        uname2mailaddr = ac.getdict_uname2mailaddr()
-
-        for ac_id, dta in ac_info.items():
-            if ownerid2fnr.has_key(dta.get('owner_id')):
-                dta['fnr'] = ownerid2fnr[dta['owner_id']]
-            if uname2mailaddr.has_key(dta['uname']):
-                dta['email'] = uname2mailaddr[dta['uname']]
-
-        return ac_info
-
-def dump_userinfo(fetcher):
-    ac_info = fetcher.fetch_acinfo()
-    for account_id, dta in ac_info.items():
-        print "%s;%s" % (dta.get('fnr', ''), dta.get('email', ''))
-
-def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], '', ['help', 'user-info', 'fnr-src-sys='])
-    except getopt.GetoptError:
-        usage(1)
+        opts, args = getopt.getopt(argv[1:],
+                                   "f:s:",
+                                   ["file=","fnr-systems="])
+    except getopt.GetoptError, error:
+        usage(message=error.msg)
+        return 1
 
-    fetcher = Fetcher()
-
+    output_stream = sys.stdout
+    fnr_src_sys = None
     for opt, val in opts:
-        if opt in ('--help',):
-            usage()
-        elif opt in ('--fnr-src-sys',):
-            fetcher.fnr_src_sys = int(_AuthoritativeSystemCode(val))
-        elif opt in ('--user-info',):
-            dump_userinfo(fetcher)
-    if not opts:
-        usage(1)
+        if opt in ('-f', '--file',):
+            output_stream = open(val, "w")
+        elif opt in ('-s', '--fnr-systems',):
+            fnr_src_sys = val
 
-def usage(exitcode=0):
-    print >>sys.stderr, __doc__
-    sys.exit(exitcode)
+    generate_person_uname_report(output_stream, fnr_src_sys)
 
-if __name__ == '__main__':
-    main()
+    if output_stream not in (sys.stdout, sys.stderr):
+        output_stream.close()
+
+    return 0
+# end main
+
+
+
+if __name__ == "__main__":
+    sys.exit(main())
