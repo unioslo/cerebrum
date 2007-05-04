1cda816b9c80d2400ce7d1495acb72fdf23bd01e
Alf B. Lervåg <alfborge@unknown>
2007-05-04 10:04:42 +0000

    Owner can be a group, so only do get_affiliations is owner is a person. Fixes mantis #465

--- spine/cereweb/htdocs/user_client.py
+++ spine/cereweb/htdocs/user_client.py
@@ -66,10 +66,12 @@ def get_user_info(transaction,username):
         fullname = owner.get_cached_full_name()
         birthdate = owner.get_birth_date().strftime('%Y-%m-%d')
         is_quarantined = owner.is_quarantined()
+        affiliations = owner.get_affiliations()
     else:
         fullname = owner.get_name()
         birthdate = ''
         is_quarantined = ''
+        affiliations = []
     user = {'id':my_id,'username':username,'fullname':fullname,'expire_date':expire_date,
             'birthdate':birthdate,'quarantined':is_quarantined,'external_id': [], 'spreads': [], 'groups': [], 'affiliations': []}
     if owner_type == 'person':
@@ -86,7 +88,6 @@ def get_user_info(transaction,username):
     groups = account.get_groups()
     for group in groups:
         user['groups'].append((group.get_name(), group.get_description()))
-    affiliations = account.get_owner().get_affiliations()
     for affiliation in affiliations:
         user['affiliations'].append((affiliation.get_affiliation().get_name(), affiliation.get_status().get_description(), affiliation.get_ou().get_name()))
     return user
