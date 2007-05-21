ffc101c790e386ba5557ca92e04670f92f1116ff
Monica Stamnes <mocca@usit.uio.no>
2007-05-21 17:08:34 +0000

    The file role.xml can become *very* small at NMH without there being anything wrong.

--- contrib/no/nmh/import_from_FS.py
+++ contrib/no/nmh/import_from_FS.py
@@ -145,7 +145,7 @@ def write_evukurs_info(outfile):
 def write_role_info(outfile):
     """Skriv data om alle registrerte roller"""
     f = MinimumSizeWriter(outfile)
-    f.set_minimum_size_limit(5*KiB)
+    f.set_minimum_size_limit(KiB/4)
     f.write(xml.xml_hdr + "<data>\n")
     cols, role = _ext_cols(fs.undervisning.list_alle_personroller())
     for r in role:
