b1df623db1e53501358a9d5cfcb5fbd35389f45e
Jasmina Hodzic <jasminah@usit.uio.no>
2007-05-31 03:47:06 +0000

    HiÃf needs to use some of UiOs email_cmds. a few methods had to be copied from bofhd_uio_cmds in order to make this work now, this will be fixed soon.

---
+++ Cerebrum/modules/no/hiof/bofhd_hiof_emailcmds.py
@@ -0,0 +1,240 @@
+# -*- coding: iso-8859-1 -*-
+
+# Copyright 2007 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+
+
+import mx
+import pickle
+
+import cereconf
+from Cerebrum.Utils import Factory
+from Cerebrum.modules.bofhd.errors import CerebrumError, PermissionDenied
+from Cerebrum import Constants
+from Cerebrum import Utils
+from Cerebrum import Cache
+from Cerebrum import Errors
+from Cerebrum import Database
+
+from Cerebrum.modules.bofhd.cmd_param import *
+from Cerebrum.modules.no.nmh import bofhd_nmh_help
+from Cerebrum.Constants import _CerebrumCode, _SpreadCode
+from Cerebrum.modules.bofhd.auth import BofhdAuth
+from Cerebrum.modules.bofhd.utils import _AuthRoleOpCode
+from Cerebrum.modules.no import fodselsnr
+from Cerebrum.modules import Email
+
+def format_day(field):
+    fmt = "yyyy-MM-dd"                  # 10 characters wide
+    return ":".join((field, "date", fmt))
+
+class BofhdExtension(object):
+    OU_class = Utils.Factory.get('OU')
+    Account_class = Factory.get('Account')
+    Group_class = Factory.get('Group')
+    all_commands = {}
+
+    copy_commands = (
+        #
+        # copy relevant email-cmds and util methods
+        #
+        'email_add_address', 'email_remove_address',
+        'email_info', 'email_primary_address',
+        'email_create_domain', 'email_domain_configuration',
+        'email_domain_info', 'email_add_domain_affiliation',
+        'email_remove_domain_affiliation',
+        '_email_info_account', '__get_valid_email_addrs',
+        '_email_info_spam',
+        '_email_info_detail', '_email_info_forwarding',
+        '_email_info_mailman', '_email_info_multi',
+        '_email_info_file', '_email_info_pipe',
+        '_email_info_forward', '_set_email_primary_address',
+        '_get_boolean', '_onoff', '_has_category',
+        '_sync_category', '_update_email_for_ou',
+        '_get_account', '_get_email_domain',
+        '_split_email_address', '_remove_email_address',
+        '_get_ou', '_get_affiliationid'
+        )
+        
+    def __new__(cls, *arg, **karg):
+        # A bit hackish.  A better fix is to split bofhd_uio_cmds.py
+        # into seperate classes.
+        from Cerebrum.modules.no.uio.bofhd_uio_cmds import BofhdExtension as \
+             UiOBofhdExtension
+
+        non_all_cmds = ('num2str', )
+        for func in BofhdExtension.copy_commands:
+            setattr(cls, func, UiOBofhdExtension.__dict__.get(func))
+            if func[0] != '_' and func not in non_all_cmds:
+                BofhdExtension.all_commands[func] = UiOBofhdExtension.all_commands[func]
+        x = object.__new__(cls)
+        return x
+
+    def __init__(self, server, default_zone='hiof'):
+        self.server = server
+        self.logger = server.logger
+        self.util = server.util
+        self.db = server.db
+        self.const = Factory.get('Constants')(self.db)
+        self.ba = BofhdAuth(self.db)
+
+        # From uio
+        self.num2const = {}
+        self.str2const = {}
+        for c in dir(self.const):
+            tmp = getattr(self.const, c)
+            if isinstance(tmp, _CerebrumCode):
+                self.num2const[int(tmp)] = tmp
+                self.str2const[str(tmp)] = tmp
+        self._cached_client_commands = Cache.Cache(mixins=[Cache.cache_mru,
+                                                           Cache.cache_slots,
+                                                           Cache.cache_timeout],
+                                                   size=500,
+                                                   timeout=60*60)
+
+
+    def get_help_strings(self):
+        return (bofhd_nmh_help.group_help,
+                bofhd_nmh_help.command_help,
+                bofhd_nmh_help.arg_help)
+    
+    def get_commands(self, account_id):
+        try:
+            return self._cached_client_commands[int(account_id)]
+        except KeyError:
+            pass
+        commands = {}
+        for k in self.all_commands.keys():
+            tmp = self.all_commands[k]
+            if tmp is not None:
+                if tmp.perm_filter:
+                    if not getattr(self.ba, tmp.perm_filter)(account_id, query_run_any=True):
+                        continue
+                commands[k] = tmp.get_struct(self)
+        self._cached_client_commands[int(account_id)] = commands
+        return commands
+
+    def get_format_suggestion(self, cmd):
+        return self.all_commands[cmd].get_fs()
+
+    def _email_info_basic(self, acc, et):
+        info = {}
+        data = [ info ]
+        if (et.email_target_type != self.const.email_target_Mailman and
+            et.email_target_alias is not None):
+            info['alias_value'] = et.email_target_alias
+        info["account"] = acc.account_name
+        est = Email.EmailServerTarget(self.db)
+        try:
+            est.find_by_entity(acc.entity_id)
+        except Errors.NotFoundError:
+            info["server"] = "<none>"
+            info["server_type"] = "N/A"
+        else:
+            es = Email.EmailServer(self.db)
+            es.find(est.email_server_id)
+            info["server"] = es.name
+            type = int(es.email_server_type)
+            info["server_type"] = str(self.const.EmailServerType(type))
+        return data
+
+
+    def __get_email_target_and_account(self, address):
+        """Returns a tuple consisting of the email target associated
+        with address and the account if the target type is user.  If
+        there is no at-sign in address, assume it is an account name.
+        Raises CerebrumError if address is unknown."""
+        et, ea = self.__get_email_target_and_address(address)
+        acc = None
+        if et.email_target_type in (self.const.email_target_account,
+                                    self.const.email_target_deleted):
+            acc = self._get_account(et.email_target_entity_id, idtype='id')
+        return et, acc
+
+    def __get_email_target_and_address(self, address):
+        """Returns a tuple consisting of the email target associated
+        with address and the address object.  If there is no at-sign
+        in address, assume it is an account name and return primary
+        address.  Raises CerebrumError if address is unknown.
+        """
+        et = Email.EmailTarget(self.db)
+        ea = Email.EmailAddress(self.db)
+        if address.count('@') == 0:
+            acc = self.Account_class(self.db)
+            try:
+                acc.find_by_name(address)
+                # FIXME: We can't use Account.get_primary_mailaddress
+                # since it rewrites special domains.
+                et = Email.EmailTarget(self.db)
+                et.find_by_entity(acc.entity_id)
+                epa = Email.EmailPrimaryAddressTarget(self.db)
+                epa.find(et.email_target_id)
+                ea.find(epa.email_primaddr_id)
+            except Errors.NotFoundError:
+                raise CerebrumError, ("No such address: '%s'" % address)
+        elif address.count('@') == 1:
+            try:
+                ea.find_by_address(address)
+                et.find(ea.email_addr_target_id)
+            except Errors.NotFoundError:
+                raise CerebrumError, "No such address: '%s'" % address
+        else:
+            raise CerebrumError, "Malformed e-mail address (%s)" % address
+        return et, ea
+
+    def __get_address(self, etarget):
+        """The argument can be
+        - EmailPrimaryAddressTarget
+        - EmailAddress
+        - EmailTarget (look up primary address and return that, throw
+        exception if there is no primary address)
+        - integer (use as email_target_id and look up that target's
+        primary address)
+        The return value is a text string containing the e-mail
+        address.  Special domain names are not rewritten."""
+        ea = Email.EmailAddress(self.db)
+        if isinstance(etarget, (int, long, float)):
+            epat = Email.EmailPrimaryAddressTarget(self.db)
+            # may throw exception, let caller handle it
+            epat.find(etarget)
+            ea.find(epat.email_primaddr_id)
+        elif isinstance(etarget, Email.EmailTarget):
+            epat = Email.EmailPrimaryAddressTarget(self.db)
+            epat.find(etarget.email_target_id)
+            ea.find(epat.email_primaddr_id)
+        elif isinstance(etarget, Email.EmailPrimaryAddressTarget):
+            ea.find(etarget.email_primaddr_id)
+        elif isinstance(etarget, Email.EmailAddress):
+            ea = etarget
+        else:
+            raise ValueError, "Unknown argument (%s)" % repr(etarget)
+        ed = Email.EmailDomain(self.db)
+        ed.find(ea.email_addr_domain_id)
+        return ("%s@%s" % (ea.email_addr_local_part,
+                           ed.email_domain_name))
+
+    def __get_valid_email_addrs(self, et, special=False, sort=False):
+        """Return a list of all valid e-mail addresses for the given
+        EmailTarget.  Keep special domain names intact if special is
+        True, otherwise re-write them into real domain names."""
+        addrs = [(r['local_part'], r['domain'])       
+                 for r in et.get_addresses(special=special)]
+        if sort:
+            addrs.sort(lambda x,y: cmp(x[1], y[1]) or cmp(x[0],y[0]))
+        return ["%s@%s" % a for a in addrs]
