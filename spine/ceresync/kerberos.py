0569e5fc03b48b910bebca51a6daece6dfe1d99c
Steinar Hamre <stseiham@extern.uio.no>
2007-07-19 12:53:52 +0000

    Got an updated version of the kerberos client from geirha. - remove principals not present in a bulk sync. - ignore users without password. - more and better tests. - fetch more configuration from the config file.

--- spine/ceresync/kerberos.py
+++ spine/ceresync/kerberos.py
@@ -24,80 +24,76 @@
 
 import unittest
 
-try:
-    import kadm5
-except:
-    kadm5 = None
-
+import kadm5
 import heimdal_error
 import mit_error
-
+import errors
+import config
+import os
+import re
 from sync import Pgp
+from sets import Set as set
+import sys
 
+verbose= '-v' in sys.argv
+
+def info(msg):
+    if verbose:
+        sys.stdout.write(msg)
+        sys.stdout.flush()
 class Account:
 
-    def __init__(self):
+    def __init__(self, principal=None, keytab=None, flavor=None):
         self.k = None # Holds the authenticated kadm object
         self.incr = False
         self.pgp = Pgp()
+        self.principal= principal or config.conf.get('kerberos','principal')
+        self.keytab= keytab or config.conf.get('kerberos', 'keytab')
+
+        flavor= flavor or config.conf.get('kerberos','flavor')
+        if flavor.lower() == 'heimdal':
+            self.flavor= kadm5.KRB_FLAVOR_HEIMDAL
+        elif flavor.lower() == 'mit':
+            self.flavor= kadm5.KRB_FLAVOR_MIT
+        else:
+            raise errors.BackendError("Invalid kerberos flavor '%s'"%flavor)
 
     def begin(self,incr=False):
         """
         Initiate connection to local or remote kadmin
         """
         self.incr = incr
-        if not kadm5:
-            return None
-        haveMIT = False
-        haveHeimdal = False
-
+        #FIXME! Find a better way to authenticate
+        os.system('kinit --keytab=%s %s'%(self.keytab,self.principal))
         try:
-            kadm5.init_libs(kadm5.KRB_FLAVOR_HEIMDAL)
-            haveHeimdal = True
-        except OSError,err:
-            print "info: unable to initialize Heimdal kerberos: %s" % err
+            kadm5.init_libs(self.flavor)
+        except:
+            raise errors.BackendError("Kerberos libs not found")
 
-        # Decomment if using MIT and not Heimdal. Make it configurable. 
-        #try:
-        #    kadm5.init_libs(kadm5.KRB_FLAVOR_MIT)
-        #    haveMIT = True
-        #except OSError,err:
-        #    print "info: unable to initialize MIT kerberos: %s" % err
-
-        if not (haveMIT or haveHeimdal):
-            print "No kreberos libs found"
-            return None
-
-        krb = kadm5.KRB5()
-
-        try:
-            principals = krb.klist()
-        except Exception,err:
-            print err
-            return None
-
-        if (not principals):
-            return None
-
-        # Using first principal as default, or later from config
-        principal = principals[0]
-        assert 'admin' in principal
-
-        # OK.. not far left
         self.k = kadm5.KADM5()
         try:
-            self.k.connect(princ=principal)
+            self.k.connect(princ=self.principal)
         except Exception,e:
-            print "Error connecting. Reason: %s" % e
-            raise SystemExit
+            raise errors.BackendError("Unable to connect: %s"%e)
 
         if (not self.incr):
-            self.added_princs = []
-            
+            self.added_princs = set([])
+
     def close(self,):
         """
         Close connection to local or remote kadmin. 
         """
+        if not self.incr:
+            # Could possibly just check for a '/' instead ...
+            regex= re.compile('^([a-z0-9]+)@%s$' % self.k.realm)
+            for princ in self.k.ListPrincipals():
+                # The default principal should be left alone
+                if princ.startswith('default@'): continue                  
+
+                # remove users that were not added in this bulk
+                m= regex.match(princ)
+                if m and princ not in self.added_princs:
+                    self.delete(User(name=m.group(1), passwd=''))
         self.k = None
 
     def abort(self):
@@ -114,15 +110,18 @@ class Account:
             princ = account.name + '@' + self.k.realm # or from config
             password = self.pgp.decrypt(account.passwd)
             options = None # or some defaults from config in dict-format
+            if not password:
+                info("'%s' has blank password. Not adding.\n"%princ)
+                return
             self.k.CreatePrincipal(princ,password,options)
+            info("'%s' added\n"%princ)
             if (not self.incr):
-                self.added_princs.append(princ)
+                self.added_princs.add(princ)
         except heimdal_error.KADM5_DUP,kdup:
             # FIXME! Fetch override from config. If we set new password
             # the user will get a new kvno and things might break.
-            pass
-        except Exception,err:
-            print err
+            info("'%s' allready exists... "%princ)
+            self.update(account)
 
     def update(self,account):
         """Update account in kerberos database
@@ -132,6 +131,9 @@ class Account:
         password = self.pgp.decrypt(account.passwd)
         try:
             self.k.SetPassword(princ,password)
+            info("password updated for '%s'\n"%princ)
+            if (not self.incr):
+                self.added_princs.add(princ)
         except Exception,err:
             print err
 
@@ -139,65 +141,138 @@ class Account:
         """Delete account from kerberos database
         """
         princ = account.name + '@' + self.k.realm # or from config
-        try:
-            self.k.DeletePrincipal(princ)
-        except Exception,err:
-            print err
-
-    def syncronize(self):
-        """Delete any old account not supposed to be here.
-        """
-        if not self.incr:
-            added = self.added_princs
-            princs = self.k.ListPrincipals()
-            for princ in princs:
-                p = self.k.GetPrincipal(princ)
-                if p['mod_name'] == self.principal:
-                    if princ not in added:
-                        try:
-                            self.k.DeletePrincipal(princ)
-                        except Exception,err:
-                            print err
-
-
+        self.k.DeletePrincipal(princ)
+        info("'%s' removed.\n"%princ)
 
 class User:
-   def __init__(self):
-       self.name = 'testuser'
-       self.passwd= Pgp().encrypt('testpw')
+   def __init__(self, name='testuser', passwd='testpw'):
+       self.name = name
+       self.passwd= Pgp().encrypt(passwd)
 
-class KerberosBackTest(unittest.TestCase):
-
+class HeimdalTestCase(unittest.TestCase):
+    """TestCase that tests adding, changing and removing users from a 
+    heimdal kdc."""
     def setUp(self):
-        self.account = Account()
+        self.account= Account(flavor='heimdal')
 
-    def testBegin(self):
-        self.account = Account()
+    def tearDown(self):
+        self.account= None
+
+    def testConnection(self):
+        """Checks that connection works, and that a simple listing of 
+        principals work"""
         self.account.begin()
+        self.assert_(self.account.principal in self.account.k.ListPrincipals())
+        self.account.close()
 
     def testAdd(self):
-        user = User()
-        self.account = Account()
+        """Checks that adding a user works"""
+        user= User()
+        self.account.begin()
+        self.account.add(user)
+        principal= "%s@%s" % (user.name, self.account.k.realm)
+
+        self.assert_(self.account.k.GetPrincipal(principal))
+
+        self.account.k.DeletePrincipal(principal)
+        self.account.close()
+
+    def testAddTwice(self):
+        """Adding a user that allready exists should not fail"""
+        user= User()
+        self.account.begin()
+        self.account.add(user)
+        self.account.add(user)
+        principal= "%s@%s" % (user.name, self.account.k.realm)
+
+        self.assert_(self.account.k.GetPrincipal(principal))
+
+        self.account.k.DeletePrincipal(principal)
+        self.account.close()
+
+    def testAddBadPassword(self):
+        """Adding a user with blank password should be silently ignored"""
+        user= User(passwd='')
+        self.account.begin()
+        self.account.add(user)
+        principal= "%s@%s" % (user.name, self.account.k.realm)
+
+        self.assertRaises(heimdal_error.KADM5_UNK_PRINC, 
+                self.account.k.GetPrincipal,
+                principal) 
+
+        self.account.close()
+
+    def testDelete(self):
+        """Check that removing a user works"""
+        user= User()
+        self.account.begin()
+        self.account.add(user)
+        self.account.delete(user)
+        principal= "%s@%s" % (user.name, self.account.k.realm)
+
+        self.assertRaises(heimdal_error.KADM5_UNK_PRINC, 
+                self.account.k.GetPrincipal,
+                principal) 
+
+        self.account.close()
+
+    def testDeleteNonExisting(self):
+        """Adds a user then deletes it twice. The last delete should raise
+        an exception"""
+        user= User()
+        self.account.begin()
+        self.account.add(user)
+        self.account.delete(user)
+
+        self.assertRaises(heimdal_error.KADM5_UNK_PRINC,
+                self.account.delete,
+                user)
+        
+        self.account.close()
+
+    def setPassword(self):
+        """Changes password on a user, and checks that the modification 
+        date on the principal has changed"""
+        user= User(passwd='oldpass')
         self.account.begin()
         self.account.add(user)
 
-    def testSetPassword(self):
-        user = User()
-        self.account = Account()
-        self.account.begin()
+        principal= "%s@%s" % (user.name, self.account.k.realm)
+        info1= self.account.k.GetPrincipal(principal)
+
+        user= User(passwd='newpass')
         self.account.update(user)
+        info2= self.account.k.GetPrincipal(principal)
+        assertNotEquals(info1['mod_date'], info2['mod_date'])
 
-    def testDelete(self):
-        user = User()
-        self.account = Account()
-        self.account.begin()
-        self.account.delete(user)
-
-    def testClose(self):
-        self.account = Account()
-        self.account.begin()
+        self.account.k.DeletePrincipal(principal)
         self.account.close()
 
+    def testSync(self):
+        """ Adds two users then simulates a bulk sync with only one of the
+        users. The ommited user should then be deleted when close() is called
+        """
+        user1= User('test1','testpw1')
+        user2= User('test2','testpw2')
+        self.account.begin()
+        self.account.add(user1)
+        self.account.add(user2)
+        self.account.close()
+
+        self.account.begin(incr=False)
+        self.account.add(user1)
+        self.account.close()
+        # user2 should now have been removed
+
+        self.account.begin()
+        principal1= "%s@%s" % (user1.name, self.account.k.realm)
+        principal2= "%s@%s" % (user2.name, self.account.k.realm)
+        princlist= self.account.k.ListPrincipals()
+        self.assert_(principal1 in princlist and principal2 not in princlist)
+
+        self.account.k.DeletePrincipal(principal1)
+        self.account.close()
 
 if __name__ == '__main__':
     unittest.main()
