b07789025de968d5186ff10f25cf4d3933a3ad42
Bjørn Ove Grøtan <bgrotan@ulrik.uio.no>
2007-09-04 09:19:35 +0000

    Damn. Hate duplicating code, but need PGP both here and spine/ceresync. Cleanup to come.

--- contrib/no/ntnu/util.py
+++ contrib/no/ntnu/util.py
@@ -1,3 +1,7 @@
+import config
+from popen2 import Popen3
+import unittest
+
 def stedkode_string_to_tuple(stedkode):
     """
     Converts a stedkode string to a tuple representing
@@ -24,3 +28,58 @@ def find_ou_by_stedkode(stedkode, transaction):
     ou_searcher.set_institutt(institutt)
     ou_searcher.set_avdeling(avdeling)
     return ou_searcher.search()
+
+class Pgp:
+    def __init__(self, pgp_prog=None, enc_opts='', dec_opts='', keyid=None):
+        # Handle NoOptionError?
+        pgp_prog= pgp_prog or config.conf.get('pgp', 'prog')
+        enc_opts= enc_opts or config.conf.get('pgp', 'encrypt_opts')
+        dec_opts= dec_opts or config.conf.get('pgp', 'decrypt_opts')
+        keyid= keyid or config.conf.get('pgp', 'keyid')
+
+        self.pgp_enc_cmd= [ pgp_prog,
+            '--recipient', keyid,
+            '--default-key', keyid,
+        ] + enc_opts.split()
+        self.pgp_dec_cmd= [pgp_prog] + dec_opts.split()
+
+    def decrypt(self, cryptstring):
+        message= cryptstring
+        if cryptstring:
+            child= Popen3(self.pgp_dec_cmd)
+            child.tochild.write(cryptstring)
+            child.tochild.close()
+            message= child.fromchild.read()
+            exit_code= child.wait()
+            if exit_code:
+                raise IOError, "%r exited with %i" % (self.pgp_dec_cmd,exit_code)
+        return message
+
+    def encrypt(self, message):
+        child= Popen3(self.pgp_enc_cmd)
+        child.tochild.write(message)
+        child.tochild.close()
+        cryptstring= child.fromchild.read()
+        exit_code= child.wait()
+        if exit_code:
+            raise IOError, "%r exited with %i" % (self.pgp_enc_cmd,exit_code)
+        return cryptstring
+
+class PgpTestCase(unittest.TestCase):
+    def setUp(self):
+        self.p= Pgp()
+        self.message= 'FooX123.-=/'
+
+    def testEncrypt(self):
+        e= self.p.encrypt(self.message).strip()
+        assert e.startswith('-----BEGIN PGP MESSAGE-----') and \
+            e.endswith('-----END PGP MESSAGE-----')
+
+    def testEncryptDecrypt(self):
+        cryptstring= self.p.encrypt(self.message)
+        assert self.message == self.p.decrypt(cryptstring), \
+                'encryption and decryption yield wrong result'
+
+if __name__ == '__main__':
+    unittest.main()
+
