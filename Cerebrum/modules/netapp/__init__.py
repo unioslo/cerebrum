7ac0131e4a271f231d7605c8a7b9b421cd1d14a2
Arne Vedø-Hansen <rafiki@usit.uio.no>
2007-12-06 12:30:52 +0000

    Basic skeleton for bofhd NetApp-commands

--- Cerebrum/modules/netapp/__init__.py
+++ Cerebrum/modules/netapp/__init__.py
@@ -19,11 +19,13 @@
 # along with Cerebrum; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
+# $Id$
+
 """ 
 This module contains NetApp-specific extensions to Cerebrum.
 
 """
 
 __version__ = "$Revision$"
-# $Source$
+# $URL$
 
