cbfa3936fd0055479580e632b9c79b61b773f887
Steinar Hamre <stseiham@extern.uio.no>
2007-12-07 17:13:40 +0000

    added support for deleting a list of persons added support for deleting persons by external-id (+possibly source) misc fixes.

--- contrib/no/ntnu/delete_person.py
+++ contrib/no/ntnu/delete_person.py
@@ -14,6 +14,7 @@ import logging
 import time
 import os
 import traceback
+import sys
 
 import locale
 locale.setlocale(locale.LC_ALL,'nb_NO')
@@ -24,40 +25,68 @@ const = Factory.get('Constants')(db)
 person = Factory.get('Person')(db)
 account = Factory.get('Account')(db)
 
-def delete_person(entity,cascade=False):
+
+def check_commit(fun, *args, **kw):
+    try:
+        fun(*args, **kw)
+    except db.IntegrityError, e:
+        print 'Error while %s: %s' % (fun.func_name, str(e))
+        db.rollback()
+    else:
+        if dryrun:
+            db.rollback()
+        else:
+            db.commit()
+
+def delete_person(entity):
     print "Deleting Cerebrum-person with entity %s" % entity
+    person.clear()
     try:
         person.find(entity)
-    except Errors.NotFoundError:
-        print "Person with entity %s not found. Exiting.." % entity
-        sys.exit(0)
-    if cascade:
-        # Remove traits
-        for t in person.get_traits():
-            person.delete_trait(t)
-            person.write_db()
-        # Remote spreads
-        for s in person.get_spread():
-            person.delete_spread(s)
-            person.write_db()
-        # Remove address
-        for ea in person.get_entity_address():
-            continue
-            #person.delete_entity_address(ea.get('source_type'), ea.get('a_type'))
-            #person.write_db()
-        # Remove all affiliations
-        for a in person.get_affiliations():
-            ou = a.get('ou_id')
-            affiliation = a.get('affiliation')
-            source = a.get('source_system')
-            person.delete_affiliation(ou_id, affiliation, source)
-            person.write_db()
-        # Remove contact info
-        for c in person.get_contact_info():
-            continue
-            #person.delete_contact_info(source, type, pref='ALL')
-            #person.write_db()
-        person.delete()
+    except Errors.NotFoundError, e:
+        print "Person with entity %s not found" % entity
+        return
+    do_delete_person(person)
+
+
+def delete_person_by_externalid(id, type, source):
+    print id, type, source
+    print "Deleting Cerebrum-person with %s id %s" %  (type, id)
+    person.clear()
+    try:
+        person.find_by_external_id(type, id, source)
+    except Errors.NotFoundError, e:
+        print "Person with id %s not found. Exiting.." % id
+        return
+    do_delete_person(person)
+    
+    
+
+def do_delete_person(person):
+    # Remove traits
+    for t in person.get_traits():
+        person.delete_trait(t)
+        person.write_db()
+    # Remote spreads
+    for s in person.get_spread():
+        person.delete_spread(s)
+        person.write_db()
+    # Remove address
+    for ea in person.get_entity_address():
+        person.delete_entity_address(ea['source_type'], ea['a_type'])
+        person.write_db()
+    # Remove all affiliations
+    for a in person.get_affiliations():
+        ou_id = a['ou_id']
+        affiliation = a['affiliation']
+        source = a['source_system']
+        person.delete_affiliation(ou_id, affiliation, source)
+        person.write_db()
+    # Remove contact info
+    for c in person.get_contact_info():
+        person.delete_contact_info(source, type, pref='ALL')
+        person.write_db()
+    person.delete()
 
 def usage():
     print "RTFC"
@@ -65,28 +94,39 @@ def usage():
 def main():
     global dryrun,verbose,cascade
     opts,args = getopt.getopt(sys.argv[1:],
-                       'hvdc',['help','personid=','cascade'])
+                       'hvdc',['help','dryrun', 'verbose',
+                               'type=', 'source=', 'list=', 'personid='])
+    type=None
+    source=None
+    delete_list=[]
+    
     for opt,val in opts:
         if opt in ('-h','--help'):
             usage()
-        elif opt in ('-c','--cascade'):
-            cascade = True
         elif opt in ('-d','--dryrun'):
             dryrun = True
         elif opt in ('-v','--verbose'):
             verbose = True
+        elif opt in ('--type',):
+            type = const.EntityExternalId(val)
+            int(type)
+        elif opt in ('--source',):
+            source = const.AuthoritativeSystem(val)
+            int(source)
+        elif opt in ('--list',): 
+            for line in open(val).readlines():
+                delete_list.append(line.strip())
         elif opt in ('--personid',):
-            entity = val
-            try:
-                delete_person(entity,cascade)
-            except Exception, e:
-                db.rollback()
-		print e
-	    else:
-                if dryrun:
-                    db.rollback()
-                else:
-                    db.commit()
+            delete_list.append(val)
+
+    if type is not None:
+        for i in delete_list:
+            check_commit(delete_person_by_externalid, i, type, source)
+    else:
+        for i in delete_list:
+            check_commit(delete_person, int(i))
+
+
 
 if __name__ == '__main__':
     main()
