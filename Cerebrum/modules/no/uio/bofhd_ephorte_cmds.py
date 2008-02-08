7eb4334882a7ad866dcd96a38d4c0bb342b4fd3b
Jasmina Hodzic <jasminah@usit.uio.no>
2008-02-08 20:02:58 +0000

    added three new commands, used for Ephorte-permission manipulation.

--- Cerebrum/modules/no/uio/bofhd_ephorte_cmds.py
+++ Cerebrum/modules/no/uio/bofhd_ephorte_cmds.py
@@ -27,6 +27,7 @@ from Cerebrum.modules.bofhd.cmd_param import *
 from Cerebrum.modules.bofhd.auth import BofhdAuth
 from Cerebrum.modules.bofhd.errors import CerebrumError, PermissionDenied
 from Cerebrum.modules.no.uio.Ephorte import EphorteRole
+from Cerebrum.modules.no.uio.Ephorte import EphortePermission
 
 
 class UiOEphorteAuth(BofhdAuth):
@@ -42,6 +43,9 @@ class UiOEphorteAuth(BofhdAuth):
     can_remove_ephorte_role = can_add_ephorte_role
     can_list_ephorte_roles = can_add_ephorte_role
 
+    can_add_ephorte_perm = can_add_ephorte_role
+    can_remove_ephorte_perm = can_add_ephorte_role
+    can_list_ephorte_perm = can_add_ephorte_role
 
 class Journalenhet(Parameter):
     _type = 'journalenhet'
@@ -55,6 +59,10 @@ class Rolle(Parameter):
     _type = 'rolle'
     _help_ref = 'rolle'
 
+class Tilgang(Parameter):
+    _type = 'tilgang'
+    _help_ref = 'tilgang'
+    
 class BofhdExtension(object):
     all_commands = {}
     OU_class = Factory.get('OU')
@@ -77,6 +85,7 @@ class BofhdExtension(object):
         self.util = server.util
         self.const = Factory.get('Constants')(self.db)
         self.ephorte_role = EphorteRole(self.db)
+        self.ephorte_perm = EphortePermission(self.db)
         self.ba = UiOEphorteAuth(self.db)
 
         self._cached_client_commands = Cache.Cache(mixins=[Cache.cache_mru,
@@ -93,7 +102,10 @@ class BofhdExtension(object):
             'ephorte': {
             'ephorte_add_role': 'Add an ePhorte role for a person',
             'ephorte_remove_role': 'Remove an ePhorte role from a person',
-            'ephorte_list_roles': 'List a persons ePhorte roles'
+            'ephorte_list_roles': 'List a persons ePhorte roles',
+            'ephorte_add_perm': 'Add an ePhorte permission for a person',
+            'ephorte_remove_perm': 'Remove an ePhorte permission from a person',
+            'ephorte_list_perm': 'List a persons ePhorte permissions'            
             }
             }
         arg_help = {
@@ -109,6 +121,10 @@ class BofhdExtension(object):
                          'Legal values are: \n%s' % "\n".join(
             ["  %-8s : %s" % (str(c), c.description)
              for c in self.const.fetch_constants(self.const.EphorteRole)])],
+            'tilgang': ['tilgang', 'Enter perm ("tilgang")',
+                         'Legal values are: \n%s' % "\n".join(
+            ["  %-8s : %s" % (str(c), c.description)
+             for c in self.const.fetch_constants(self.const.EphortePermission)])],            
             }
 
         # liste lovlige arkivdel/journalenhet
@@ -138,8 +154,18 @@ class BofhdExtension(object):
             return c
         except Errors.NotFoundError:
             raise CerebrumError("Unknown journalenhet")
-  
 
+    def _get_tilgang(self, code_str):
+        try:
+            c = self.const.EphortePermission(code_str)
+            int(c)
+            return c
+        except Errors.NotFoundError:
+            raise CerebrumError("Unknown ePhorte auth. permission (tilgangskode)")
+
+    ##
+    ##
+    ## Add, remove or list ePhorte-roles
     all_commands['ephorte_add_role'] = Command(("ephorte", "add_role"), PersonId(), Rolle(), OU(), Arkivdel(), Journalenhet(), 
         perm_filter='can_add_ephorte_role')
     def ephorte_add_role(self, operator, person_id, role, sko, arkivdel, journalenhet):
@@ -184,10 +210,8 @@ class BofhdExtension(object):
             journalenhet = self._get_journalenhet(journalenhet)
         else:
             journalenhet = None
-
         self.ephorte_role.remove_role(person.entity_id, self._get_role(role), ou.entity_id,
                                    arkivdel, journalenhet)
-                                   
         return "OK, removed %s role for %s" % (role, person_id)
 
     all_commands['ephorte_list_roles'] = Command(("ephorte", "list_roles"), PersonId(), 
@@ -201,7 +225,6 @@ class BofhdExtension(object):
             person = self.util.get_target(person_id, restrict_to=['Person'])
         except Errors.TooManyRowsError:
             raise CerebrumError("Unexpectedly found more than one person")
-
         ret = []
         for row in self.ephorte_role.list_roles(person_id=person.entity_id):
             ou = self._get_ou(ou_id=row['adm_enhet'])
@@ -218,6 +241,57 @@ class BofhdExtension(object):
                 'adm_enhet': self._format_ou_name(ou),
                 'arkivdel': arkivdel,
                 'journalenhet': journalenhet
+                }
+                )
+        return ret
+    
+    ##
+    ## Add, remove or list auth. permissions ("tilgangskoder") for ePhorte
+    ##
+    all_commands['ephorte_add_perm'] = Command(("ephorte", "add_perm"), PersonId(), Tilgang(), OU(), 
+        perm_filter='can_add_ephorte_perm')
+    def ephorte_add_perm(self, operator, person_id, tilgang, sko):
+        if not self.ba.can_add_ephorte_perm(operator.get_entity_id()):
+            raise PermissionDenied("Currently limited to ephorte admins")
+        try:
+            person = self.util.get_target(person_id, restrict_to=['Person'])
+        except Errors.TooManyRowsError:
+            raise CerebrumError("Unexpectedly found more than one person")
+        ou = self._get_ou(stedkode=sko)
+        self.ephorte_perm.add_permission(person.entity_id, self._get_tilgang(tilgang), ou.entity_id)
+        return "OK, added 'tilgang' %s  for %s" % (tilgang, person_id)
+    
+    all_commands['ephorte_remove_perm'] = Command(("ephorte", "remove_perm"), PersonId(), Tilgang(), OU(), 
+        perm_filter='can_remove_ephorte_perm')
+    def ephorte_remove_perm(self, operator, person_id, tilgang, sko):
+        if not self.ba.can_remove_ephorte_perm(operator.get_entity_id()):
+            raise PermissionDenied("Currently limited to ephorte admins")
+        try:
+            person = self.util.get_target(person_id, restrict_to=['Person'])
+        except Errors.TooManyRowsError:
+            raise CerebrumError("Unexpectedly found more than one person")
+        ou = self._get_ou(stedkode=sko)
+        self.ephorte_perm.remove_permission(person.entity_id, self._get_tilgang(tilgang), ou.entity_id)
+        return "OK, removed 'tilgang' %s for %s" % (tilgang, person_id)    
+
+    all_commands['ephorte_list_perm'] = Command(("ephorte", "list_perm"), PersonId(), 
+        perm_filter='can_list_ephorte_perm', fs=FormatSuggestion(
+        "%-10s %-25s", ('tilgang', 'adm_enhet'),
+        hdr="%-10s %-25s" % ("Tilgang", "Adm.enhet")))
+    def ephorte_list_perm(self, operator, person_id):
+        if not self.ba.can_list_ephorte_perm(operator.get_entity_id()):
+            raise PermissionDenied("Currently limited to ephorte admins")
+        try:
+            person = self.util.get_target(person_id, restrict_to=['Person'])
+        except Errors.TooManyRowsError:
+            raise CerebrumError("Unexpectedly found more than one person")
+
+        ret = []
+        for row in self.ephorte_perm.list_permission(person_id=person.entity_id):
+            ou = self._get_ou(ou_id=row['adm_enhet'])
+            ret.append({
+                'tilgang': str(self._get_tilgang(row['perm_type'])),
+                'adm_enhet': self._format_ou_name(ou),
                 }
                 )
         return ret
