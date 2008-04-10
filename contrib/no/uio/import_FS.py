408c7b9808add7c8ff48f39126c1c99795e10e2f
Jasmina Hodzic <jasminah@usit.uio.no>
2008-04-10 08:57:18 +0000

    the log message was trying to read person.entity_id before write_db for person was done

--- contrib/no/uio/import_FS.py
+++ contrib/no/uio/import_FS.py
@@ -375,7 +375,7 @@ def process_person_callback(person_info):
                                    (ad_street, co.address_street)):
         # TBD: Skal vi slette evt. eksisterende adresse v/None?
         if address_info is not None:
-            logger.debug("Populating address %s for %s", ad_const, new_person.entity_id)
+            logger.debug("Populating address %s for %s", ad_const, studentnr)
             new_person.populate_address(co.system_fs, ad_const, **address_info)
     # if this is a new Person, there is no entity_id assigned to it
     # until written to the database.
