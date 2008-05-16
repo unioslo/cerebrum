89cf57d4609357f1cfe428c1105c97546d66c5b6
Igor Rafienko <igorr@ulrik.uio.no>
2008-05-16 14:02:35 +0000

    Fix decoding/utf-8-conversion issues.
    CerebrumStreamWriter should try harder to convert whatever is fed to it in a
    str object to unicode. We attempt a few educated guesses before giving up.

--- Cerebrum/modules/cerelog.py
+++ Cerebrum/modules/cerelog.py
@@ -69,6 +69,7 @@ References:
 import ConfigParser
 import codecs
 import inspect
+import locale
 import logging
 import os
 import os.path
@@ -636,15 +637,42 @@ class CerelogStreamWriter(codecs.StreamWriter):
 
     ... so that the client code does not have to care about the
     encodings, regardless of the encodings specified in logging.ini.
+
+    The unicode objects can be output quite easily. The problem arises with
+    non-ascii str objects. We have no way of *knowing* their exact encoding,
+    and some guesswork is involved in outputting the strings.
     """
+
+    def __init__(self, stream, errors="strict"):
+        codecs.StreamWriter.__init__(self, stream, errors)
+
+        # What's the expected encoding for strings? 
+        self.incoming_encodings = list()
+        for x in (locale.getpreferredencoding(),
+                  "utf-8",
+                  "iso-8859-1",
+                  "windows-1252"):
+            if x.lower() not in self.incoming_encodings:
+                self.incoming_encodings.append(x.lower())
+    # end __init__
 
     def write(self, obj):
         """Force conversion to self.encoding."""
 
-        # We force strings to unicode. Strings are assumed to be in latin1,
-        # although this should be parametrised. 
-        if self.encoding and isinstance(obj, str):
-            obj = obj.decode(self.encoding)
+        # We force strings to unicode, so we won't have to deal with encoding
+        # crap later.
+        if self.incoming_encodings and isinstance(obj, str):
+            # The problem at this point is: what is the encoding in which obj
+            # is represented? There is no way we can know this for sure, since
+            # we have no idea what the environment of python is...
+            for encoding in self.incoming_encodings:
+                try:
+                    obj = obj.decode(encoding)
+                    break 
+                except UnicodeError:
+                    pass
+
+            # IVR 2008-05-16 TBD: What do we do here, if obj is NOT unicode?
 
         data, consumed = self.encode(obj, self.errors)
         self.stream.write(data)
