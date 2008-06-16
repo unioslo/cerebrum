83ff79172fdfd70eb32f28a3176a5c2363452cee
Roger Hansen <rogerha@usit.uio.no>
2008-06-16 12:24:58 +0000

    * pattern-name must be split into dirname and pattern-name where dirname   is the directory where to look for files/dirs with the pattern given   in name-pattern.
    * tar must work on files with a relative path, not absolute. Thus some
      changes had to be done with file names, etc.
    
    * some cleanup

--- contrib/file_clean.py
+++ contrib/file_clean.py
@@ -17,11 +17,14 @@ Options:
 --name-pattern : The given pattern is the pattern of the file that
                  should be archived or deleted. Format is a python
                  regexp.
+--dirname      : the directory where to look for files matching
+                 name-pattern.
 --filetype     : file type ('file' || 'dir')
 --archive-name : name of archive file
 --archive-age  : archive all files older than the given number of days.
                  If not given, archive all files that match
                  name-pattern.
+--no-delete    : Don't delete files that are archived
 --min-age      : If given, delete files older than the given number of days. 
 
 If --read-config and --archive is given, try to read the following
@@ -29,9 +32,11 @@ data-structure from cereconf:
 
   ARCHIVE_FILES = [
       {'name_pattern' : <string>,   # mandatory
+       'dirname'      : <string>,   # mandatory
        'archive_name' : <string>,   # mandatory
        'file_type'    : <string>,   # default: 'file'
        'archive_age'  : <int>,      # default: 0
+       'no_delete'    : <boolean>   # default: False
        'min_age'      : <int>},     # optional
       ...]
 
@@ -40,6 +45,7 @@ data-structure from cereconf:
 
   DELETE_FILES = [
       {'name_pattern' : <string>,   # mandatory
+       'dirname'      : <string>,   # mandatory
        'file_type'    : <string>,   # default: 'file'
        'min_age'      : <int>},     # optional
       ...]
@@ -48,17 +54,9 @@ If name_pattern, archive_name, archive_age or min_age is given by
 command line options, either --archive or --delete but not both must
 be given.
 
-Age is given in days.
-
 Postfix on archive files are '-%Y-%m-%d.tar.gz'
 """
 
-##
-## Hva som gjenstår
-##
-## - testing. Test alle funksjonene
-## - commit
-## - lag sch_jobs på uio
 
 
 import getopt
@@ -72,18 +70,16 @@ import cerebrum_path
 import cereconf
 from Cerebrum.Utils import Factory
 
-tar_cmd = '/bin/tar --files-from %s --remove-files -czf %s'
-logger = Factory.get_logger("cronjob")
+logger = Factory.get_logger("console")
+#logger = Factory.get_logger("cronjob")
 
 
-def find_files(name_pattern, min_age, file_type):
+def find_files(name_pattern, dirname, min_age=0, file_type='file'):
     """
-    Find all files matching name_pattern that are older than min_age
+    Find all files in dirname matching name_pattern that are older
+    than min_age. Return a list of relative paths.
     """
     matches = []
-    pos = name_pattern.rfind("/")
-    dirname = name_pattern[:pos]
-    regexp = re.compile(name_pattern[pos+1:])
     # min_age, in seconds
     time_threshold = time.time() - min_age*3600*24
     try:
@@ -93,64 +89,76 @@ def find_files(name_pattern, min_age, file_type):
                 continue
             if file_type == 'file' and not os.path.isfile(file_path):
                 continue
-            if regexp.match(name):
+            if re.match(name_pattern, name):
                 mtime = os.stat(file_path).st_mtime
                 # If the files last modification time is less than min_age it's a match
                 if mtime < time_threshold:
-                    matches.append(file_path)
-        logger.debug("Found %d files with pattern %s older than %s days in dir %s" % (
-            len(matches), name_pattern[pos+1:], min_age, dirname))
+                    matches.append(name)
+        logger.debug("Found %d files in dir %s with pattern %s older than %s days" % (
+            len(matches), dirname, name_pattern, min_age))
     except OSError, e:
         logger.error("Error finding files with pattern %s: %s" % (name_pattern, e))
     return matches
 
 
-def delete_files(name_pattern='', file_type='file', min_age=0, dryrun=False):
+def delete_files(name_pattern='', dirname='', file_type='file',
+                 min_age=0, dryrun=False):
     """
     Delete all files matching name_pattern that are older than min_age
     """
     # Find files matching pattern and age threashold
-    for tmp in find_files(name_pattern, min_age, file_type):
+    for name in find_files(name_pattern, dirname, min_age, file_type):
         if not dryrun:
-            logger.info("Unlink file: %s" % tmp)
+            file_path = os.path.join(dirname, name)
+            logger.info("Unlink file: %s" % file_path)
             if file_type == 'file':
-                os.unlink(tmp)
+                os.unlink(file_path)
             else:
-                shutil.rmtree(tmp)
+                shutil.rmtree(file_path)
 
 
-def archive_files(name_pattern='', archive_name='', file_type='file',
-                  archive_age=0, min_age=0, dryrun=False):
+def archive_files(name_pattern='', dirname='', archive_name='',
+                  file_type='file', archive_age=0, min_age=0,
+                  no_delete=False, dryrun=False):
     """
-    Archive all files that match name_pattern and are older than
-    archive_age. Any such files are tared and zipped into one file
-    with name archive_name+postfix.
+    Archive all files in dirname that match name_pattern and are
+    older than archive_age. Any such files are tared and zipped into
+    one file with name archive_name+postfix.
     """
-    if not (name_pattern or archive_name):
-        logger.warning("Both name_pattern and archive_name must be given")
-
+    if not (name_pattern or archive_name or dirname):
+        logger.warning("name_pattern, dirname and archive_name must be given")
+        return
+    
     # Check if old archives should be deleted
     if min_age > 0:
         # FIXME: pattern is postfix dependent
-        archive_pattern = archive_name + '-\d+-\d+-\d+.*\.tar\.gz'
+        archive_pattern = os.path.basename(archive_name) + '-\d+-\d+-\d+.*\.tar\.gz'
         logger.info("Delete archives of type %s older than %d days" % (
             archive_pattern, min_age))
-        delete_files(archive_pattern, file_type, min_age, dryrun=dryrun)
-
+        delete_files(archive_pattern, os.path.dirname(archive_name), file_type,
+                     min_age, dryrun=dryrun)
     # Check if new files should be archived
-    files_to_archive = find_files(name_pattern, archive_age, file_type)
+    files_to_archive = find_files(name_pattern, dirname, archive_age, file_type)
     if not files_to_archive:
         return
+    # Set tar command
+    tar_cmd = '/bin/tar --files-from %s --remove-files -czf %s'
+    if no_delete:
+        tar_cmd = '/bin/tar --files-from %s -czf %s'
+        
     # Create filename of archive file, first postfix
     time_threshold = time.time() - archive_age*3600*24
     postfix = '-' + time.strftime('%Y-%m-%d', time.localtime(time_threshold))
-    # create a temporary file for tar command which list files to archive 
-    tempfile.tempdir = '/tmp/'
+    # Create a temporary file for tar command which list files to archive
+    tempfile.tempdir = dirname
     tmp_os_handle, tmp_name = tempfile.mkstemp('.filelist')
     outfile = open(tmp_name, 'w')
     for f in files_to_archive:
         outfile.write(f+"\n")
     outfile.close()
+    # Change dir to dirname to be able to remove files in tmp_name
+    # after archiving
+    os.chdir(dirname)
     # If a file with the same name exists we add a counter
     count = 0
     format = "%(archive_name)s%(postfix)s.tar.gz"
@@ -177,7 +185,7 @@ def archive_files(name_pattern='', archive_name='', file_type='file',
             if file_type == 'dir':
                 # tar won't delete directories used as argument
                 for f in files_to_archive:
-                    os.rmdir(f)
+                    shutil.rmtree(os.path.join(dirname, f))
     os.unlink(tmp_name)
 
 
@@ -190,8 +198,8 @@ def main():
     try:
         opts, args = getopt.getopt(sys.argv[1:], '', [
             'help', 'archive', 'delete', 'read-config', 'dryrun',
-            'name-pattern=', 'filetype=', 'archive-name=', 'archive-age=',
-            'min-age='])
+            'name-pattern=', 'dirname=', 'filetype=', 'archive-name=',
+            'archive-age=', 'min-age=', 'no-delete'])
     except getopt.GetoptError:
         usage(1)
     if not opts:
@@ -200,9 +208,11 @@ def main():
     archive_mode = False
     delete_mode = False
     read_config = False
+    no_delete = False
     dryrun = False
     name_pattern = None
     archive_name = None
+    dirname = None
     file_type = 'file'
     archive_age = 0
     min_age = 0
@@ -220,6 +230,8 @@ def main():
             dryrun = True
         elif opt in ('--name-pattern',):
             name_pattern = val
+        elif opt in ('--dirname',):
+            dirname = val
         elif opt in ('--filetype',):
             file_type = val
         elif opt in ('--archive-name',):
@@ -228,6 +240,8 @@ def main():
             archive_age = int(val)
         elif opt in ('--min-age',):
             min_age = int(val)
+        elif opt in ('--no-delete',):
+            no_delete = True
 
     # read options from cereconf or cmd line?
     if read_config:
@@ -243,12 +257,15 @@ def main():
                 logger.info("Delete mode, reading DELETE_FILES from cereconf")
             except AttributeError:
                 usage(1)
-    elif name_pattern and delete_mode:
-        delete_actions = [(name_pattern, min_age)]        
-    elif name_pattern and archive_name and archive_mode:
+    elif name_pattern and dirname and delete_mode:
+        tmp = {'name_pattern': name_pattern, 'dirname': dirname,
+               'file_type': file_type, 'min_age': min_age,}
+        delete_actions = [tmp]
+    elif name_pattern and dirname and archive_name and archive_mode:
         tmp = {'name_pattern': name_pattern, 'archive_name': archive_name,
-               'file_type': file_type, 'archive_age': archive_age,
-               'min_age': min_age}
+               'dirname': dirname, 'file_type': file_type,
+               'archive_age': archive_age, 'min_age': min_age,
+               'no_delete': no_delete}
         archive_actions = [tmp]
     else:
         usage()
