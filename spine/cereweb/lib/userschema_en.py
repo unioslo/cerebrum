d3275ea2a212ac8d66116ca12f51ee624c0ccf22
Trond Kandal <trond.kandal@ntnu.no>
2008-10-20 11:50:42 +0000

    language-files for the printing of contracts.  tk.

---
+++ spine/cereweb/lib/userschema_en.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+#
+# $Id: userschema_en.py 9197 2008-09-01 14:26:47Z kandal $
+#
+
+def institutionName():
+    return 'Norwegian University of Science and Technology'
+
+def institutionAcronym():
+    return 'NTNU'
+
+def dateText():
+    return 'Date'
+
+def signatureText():
+    return 'Signature'
+
+def facultyText():
+    return 'Faculty:'
+
+def usernameText():
+    return 'Username:'
+
+def emailText():
+    return 'Email:'
+
+def nameText():
+    return 'Name:'
+
+def birthdateText():
+    return 'Birthdate:'
+
+def greetingText():
+    return 'Best regards'
+
+def itDirector():
+    return 'Tor Holmen/S/'
+
+def positionText():
+    return 'IT Director'
+
+def pageOneTitle():
+    return """<u>Declaration for opening of user account at NTNU's
+    computer systems.</u>"""
+
+def text01():
+    return """This declaration is to be signed and returned to the
+    faculty or department shown in the table below, and is required
+    for opening a new user account at NTNU's computer systems."""
+
+def text11():
+    return """The IT regulations can be collected from the
+    administrators or the system maintainers at each instituion. The
+    IT regulations can also be found on the web:"""
+
+def text12():
+    ## must be on one line or else a space will get inserted
+    return """<link href="https://sikkerhet.ntnu.no/itsikkerhet/itreglement.html"><u>https://sikkerhet.ntnu.no/itsikkerhet/itreglement.html</u></link>"""
+
+def text13():
+    return """The different institutions may have additional
+    instructions or rules that have to be added to the general IT
+    regulations."""
+
+def declarationText():
+    return """Declaration:"""
+
+def bullet10():
+    return  """I hereby declare to have read NTNU's IT regulations and
+    understand that I have to act according to the regulations at any
+    time.  I also accept the means that NTNU may use if the IT
+    regulations are violated, as mentioned in the computer regulations
+    paragraph 4.3, 7.1 and 7.2."""
+
+def bullet11():
+     return """I hereby consent that administrative logs may be used
+     to identify my actions on the computer systems with the
+     limitations specified in the IT regulations paragraph 5.10."""
+
+def pageTwoTitle():
+    return  """<u>Assignment of user account on NTNU's computer systems.</u>"""
+
+def text20():
+    return """The following user account have been created on NTNU's
+    computer systems:"""
+
+def passwordText():
+    return 'Password:'
+
+def emailAddressText():
+    return 'Your e-mail address:'
+
+def bullet20():
+    return """We strongly encourage you to change your password
+    immediately after your account has been activated.  The password
+    is changed at: <link href="https://bdb.itea.ntnu.no/passord.php"><u>https://bdb.itea.ntnu.no/passord.php</u></link>"""
+
+def bullet21():
+    return """We remind you that you by signing have agreed to follow
+    the IT regulations and accepted that the system administrators are
+    allowed to access and view system logs."""
+
+def bullet22():
+    return """You are responsible for keeping yourself updated to the
+    current IT regulations and other regulations concerning the
+    computer systems at any time."""
+
+def text21():
+    return """The IT regulations and other central rules are published
+    at:"""
+
+def bullet23():
+    return """<link href="https://sikkerhet.ntnu.no/itsikkerhet/itreglement.html"><u>https://sikkerhet.ntnu.no/itsikkerhet/itreglement.html</u></link>""" 
+
+def text22():
+    return """If there exists local rules and regulations they will be
+    published by the local computer personell."""
+
+def text23():
+    return  """Requests regarding the user account, please contact 
+    Orakeltjenesten: """
+
+def text24():
+    return """E-mail: orakel@ntnu.no, telephone: (735) 91500, fax: (735)
+        91437, web: <link href="http://www.ntnu.no/adm/it/brukerstotte/"><u>http://www.ntnu.no/adm/it/brukerstotte/</u></link>"""
---
+++ spine/cereweb/lib/userschema_no.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+#
+# $Id: userschema_no.py 9197 2008-09-01 14:26:47Z kandal $
+#
+
+def institutionName():
+    return 'Norges teknisknaturvitenskaplige universitet'
+
+def institutionAcronym():
+    return 'NTNU'
+
+def dateText():
+    return 'Dato'
+
+def signatureText():
+    return 'Underskrift'
+
+def facultyText():
+    return 'Fakultet/avd:'
+
+def usernameText():
+    return 'Brukernavn:'
+
+def emailText():
+    return 'Epost:'
+
+def nameText():
+    return 'Navn:'
+
+def birthdateText():
+    return 'Fødselsdato:'
+
+def greetingText():
+    return 'Vennlig hilsen'
+
+def itDirector():
+    return 'Tor Holmen/S/'
+
+def positionText():
+    return 'IT-Direktør'
+
+def pageOneTitle():
+    return """<u>Erklæring ved åpning av brukerkonto på NTNUs dataanlegg.</u>"""
+
+def text01():
+    return """Denne erklæring skal signeres og returneres til det
+          fakultet eller avdeling som framgår av tabellen under,
+          og er påkrevd for åpning av ny brukerkonto ved NTNUs
+          dataanlegg."""
+
+def text11():
+    return """IT-reglementet kan fåes utlevert fra IT-ansvarlige
+          eller IT-Driftsansvarlige ved den enkelte enhet.
+          IT-Reglementet for NTNU finner du også på nett.""" 
+
+def text12():
+    ## must be on one line or else a space will get inserted
+    return """<link href="https://sikkerhet.ntnu.no/itsikkerhet/itreglement.html"><u>https://sikkerhet.ntnu.no/itsikkerhet/itreglement.html</u></link>"""
+
+def text13():
+    return """Det enkelte driftsmiljø kan ha egne instrukser
+          eller regler som kommer i tillegg til IT-reglementet. Brukeren
+          er selv ansvarlig for å oppsøke disse."""
+
+def declarationText():
+    return """Erklæring:"""
+
+def bullet10():
+    return  """Jeg bekrefter herved å ha lest NTNUs IT-reglement
+          og påtar meg de forpliktelser som reglementet til enhver tid
+          legger på meg.  Jeg aksepterer også de sanksjonsmuligheter
+          som NTNU har ved brudd på IT-reglementet slik det er angitt
+          iIT-reglementets punkt 4.3, 7.1 og 7.2"""
+
+def bullet11():
+     return """Jeg gir herved tillatelse til at det kan benyttes
+          driftslogger for å identifisere min bruk av dataanlegget med
+          de begrensninger som er spesifisert i IT-reglementets punkt 5.10."""
+
+def pageTwoTitle():
+    return  """<u>Tildeling av brukerkonto på NTNUs dataanlegg.</u>"""
+
+def text20():
+    return """Følgende brukerkonto er åpnet for deg på NTNUs dataanlegg:"""
+
+def passwordText():
+    return 'Passord:'
+
+def emailAddressText():
+    return 'Din epostadresse:'
+
+def bullet20():
+    return """Vi oppfordrer deg til å bytte passord etter at
+         kontoen er tatt i bruk. Passord kan byttes på:
+         <link href="https://bdb.itea.ntnu.no/passord.php"><u>https://bdb.itea.ntnu.no/passord.php</u></link>"""
+
+def bullet21():
+    return """Vi minner også om NTNUs IT-reglement som du har
+        bundet deg til å overholde, og har akseptert driftansvarliges
+        innsynsrett i driftslogger ved underskrift."""
+
+def bullet22():
+    return """IT-reglementet forplikter brukerne til å holde
+        seg orientert om det til enhver tid gjeldende IT-reglement og
+        eventuelle andre regler og bestemmelser som gjelder NTNUs
+        dataanlegg."""
+
+def text21():
+    return """IT-reglement og de sentrale regler finner du
+        publisert på:"""
+
+def bullet23():
+    return """<link href="https://sikkerhet.ntnu.no/itsikkerhet/itreglement.html"><u>https://sikkerhet.ntnu.no/itsikkerhet/itreglement.html</u></link>""" 
+
+def text22():
+    return """Dersom lokale regler finnes vil det være
+        offentliggjort av det lokale driftspersonalet."""
+
+def text23():
+    return  """Henvendelser vedrørende brukerkontoen rettes til
+        Orakeltjenesten: """
+
+def text24():
+    return """Epost: orakel@ntnu.no, tlf: (735) 91500, fax: (735)
+        91437, web: <link href="http://www.ntnu.no/adm/it/brukerstotte/"><u>http://www.ntnu.no/adm/it/brukerstotte/</u></link>"""
