b0bcd32afd1530de22a6efde643b2a9dd29e5b4f
Roger Hansen <rogerha@usit.uio.no>
2008-10-27 11:11:53 +0000

    group API change is in again

--- contrib/no/uio/populate_ephorte.py
+++ contrib/no/uio/populate_ephorte.py
@@ -241,12 +241,10 @@ class PopulateEphorte(object):
         # instead
         superusers = []
         group.find_by_name(cereconf.EPHORTE_ADMINS)
-        ## Don't use search_members before new group API is in prod
-        # for account_row in group.search_members(group_id=group.entity_id,
-        #                                         indirect_members=True,
-        #                                         member_type=co.entity_account):
-        #     account_id = int(account_row["member_id"])
-        for account_id in group.get_members():
+        for account_row in group.search_members(group_id=group.entity_id,
+                                                indirect_members=True,
+                                                member_type=co.entity_account):
+            account_id = int(account_row["member_id"])
             ac.clear()
             ac.find(account_id)
             superusers.append(int(ac.owner_id))
