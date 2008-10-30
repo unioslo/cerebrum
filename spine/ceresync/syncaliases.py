ef71b4809a86ba603ba8211737ed4646f767d7f1
Steinar Hamre <stseiham@extern.uio.no>
2008-10-30 13:27:03 +0000

    Ceresync client for alias files.

---
+++ spine/ceresync/syncaliases.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+from ceresync import sync
+
+def main():
+    s= sync.Sync(incr=False)
+    print s.cmd.get_last_changelog_id() 
+    aa=s.view.get_aliases()
+    for a in aa:
+        if a.address_id == a.primary_address_id:
+            print "%s@%s: <> %s@%s" % (
+                a.local_part, a.domain,
+                a.account_name, a.server_name )
+	else:
+	    print "%s@%s: %s@%s" % (
+                a.local_part, a.domain,
+                a.primary_address_local_part, a.primary_address_domain )
+
+if __name__ == "__main__":
+    main()
+
