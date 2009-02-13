6809c3d0462e83100091d3d818af470c33547561
Trond Kandal <trond.kandal@ntnu.no>
2009-02-13 13:58:25 +0000

    added a mixin for DataPerson with specific extensions for NTNU.  tk.

--- Cerebrum/modules/no/ntnu/abcenterprise/ABCDataObjectsMixin.py
+++ Cerebrum/modules/no/ntnu/abcenterprise/ABCDataObjectsMixin.py
@@ -5,6 +5,7 @@ from mx.DateTime import Date
 import abcconf
 
 from Cerebrum.modules.abcenterprise.ABCDataObjects import DataOU
+from Cerebrum.modules.abcenterprise.ABCDataObjects import DataPerson
 
 class DataOUMixin(DataOU):
     def __init__(self):
@@ -13,7 +14,31 @@ class DataOUMixin(DataOU):
         self.stedkodes = []
 
     def __str__(self):
+        kodes = None
+        for kode in self.stedkodes:
+            if not kodes:
+                kodes = kode
+            else:
+                kodes += ", " + kode
         result = ("%sDataOUMixin: \n"
-            "\treplacedby: %s \n" % (super(DataOUMixin, self).__str__(),
-                                      self.replacedby))
+            "\treplacedby: %s Stedkoder: %s\n" % (super(DataOUMixin, self).__str__(),
+                                      self.replacedby, kodes))
+       return result
+
+class DataPersonMixin(DataPerson):
+    def __init__(self):
+        super(DataPersonMixin, self).__init__()
+        self.fnr_old = []
+        self.privacy = None
+
+    def __str__(self):
+        old_fnr = None
+        for old in self.fnr_old:
+            if not old_fnr:
+                old_fnr = old
+            else:
+                old_fnr += ", " + old
+        result = ("%sDataPersonMixin: \n"
+                "privacy: %s Old fnr: %s" % (super(DataPersonMixin, self).__str__(),
+                                    self.privacy, old_fnr))
         return result
