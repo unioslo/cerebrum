75b92e4faaceccf2c4f5312426acbd50fd4d6387
Igor Rafienko <igorr@ulrik.uio.no>
2009-02-20 12:54:48 +0000

    There is no point in converting a DATE to TEXT and then to DATE back again.

--- Cerebrum/Person.py
+++ Cerebrum/Person.py
@@ -345,7 +345,7 @@ class Person(EntityContactInfo, EntityExternalId, EntityAddress,
     def find_persons_by_bdate(self, bdate):
         return self.query("""
         SELECT person_id FROM [:table schema=cerebrum name=person_info]
-        WHERE to_date(CAST(birth_date AS TEXT), 'YYYY-MM-DD')=:bdate""", locals())
+        WHERE birth_date = :bdate""", locals())
 
     # Obsolete, use list_persons_by_name instead.
     def find_persons_by_name(self, name, case_sensitive=True):
