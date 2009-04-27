a90756fde3e9e1ba9f95b15ef51f8f8fe74e8ade
Sten Ingvar Solli <stenso@ulrik.uio.no>
2009-04-27 14:54:38 +0000

    Renamed file to avoid crash with other Indigo sites

---
+++ contrib/no/Indigo/ofk_ad_fullsync.py
@@ -0,0 +1,159 @@
+
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+# Copyright 2006, 2007 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""
+ad-sync script for OFK that uses ADsync.py module to sync users and groups
+to AD and Exchange.
+
+Usage: [options]
+  --user-sync (sync users to AD and Exchange)
+  --group-sync (sync groups to AD)
+  
+  --user_spread spread (overrides cereconf.AD_ACCOUNT_SPREAD)
+  --user_exchange_spread spread (overrides cereconf.AD_EXCHANGE_SPREAD)
+  --group_spread spread (overrides cereconf.AD_GROUP_SPREAD)
+  --store-sid (write sid of new AD objects to cerebrum databse as external ids.
+               default is not to write sid to database)
+  --dryrun (report changes that would have been done without --dryrun)
+  --delete (this option ensures deleting superfluous groups. default
+            is _not_ to delete groups)
+  --logger-level LEVEL (default is INFO)
+  --logger-name name (default is console)
+  
+Example:
+  ad_fullsync.py --user-sync --store-sid
+
+  ad_fullsync.py --user-sync --group-sync  --user_spread 'account@ad' \\
+    --user_exchange_spread 'account@exchange'--group_spread 'group@ad' \\
+    --delete --store-sid --logger-level DEBUG --logger-name console
+
+
+"""
+
+
+import getopt
+import sys
+import xmlrpclib
+import cerebrum_path
+import cereconf
+from Cerebrum.Constants import _SpreadCode
+from Cerebrum import Utils
+from Cerebrum.modules.no.Indigo import OFK_ADsync
+
+
+
+db = Utils.Factory.get('Database')()
+db.cl_init(change_program="ofk_ad_sync")
+co = Utils.Factory.get('Constants')(db)
+ac = Utils.Factory.get('Account')(db)
+
+
+def fullsync(user_sync, group_sync, user_spread, user_exchange_spread,
+             group_spread, dryrun, delete_objects, store_sid, logger_name,
+             logger_level):
+        
+    # initate logger
+    logger = Utils.Factory.get_logger(logger_name)
+    # set logger level ...
+
+    # --- USER SYNC ---
+    if user_sync:
+        # Catch protocolError to avoid that url containing password is
+        # written to log
+        try:
+            # instantiate sync_class and call full_sync
+            OFK_ADsync.ADFullUserSync(db, co, logger).full_sync(
+                delete=delete_objects, spread=user_spread, dry_run=dryrun,
+                store_sid=store_sid, exchange_spread=user_exchange_spread)
+        except xmlrpclib.ProtocolError, xpe:
+            logger.critical("Error connecting to AD service. Giving up!: %s %s" %
+                            (xpe.errcode, xpe.errmsg))
+    
+
+    # --- GROUP SYNC ---
+    if group_sync:
+        # Catch protocolError to avoid that url containing password is
+        # written to log
+        try:
+            # instantiate sync_class and call full_sync
+            OFK_ADsync.ADFullGroupSync(db, co, logger).full_sync(
+                delete=delete_objects, dry_run=dryrun, store_sid=store_sid,
+                user_spread=user_spread, group_spread=group_spread)
+        except xmlrpclib.ProtocolError, xpe:
+            logger.critical("Error connecting to AD service. Giving up!: %s %s" %
+                            (xpe.errcode, xpe.errmsg))
+    
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hug',  [
+            'help', 'user-sync', 'group-sync', 'user_spread=',
+            'user_exchange_spread=', 'dryrun', 'store-sid',
+            'delete', 'group_spread=', 'logger-level=', 'logger-name='])
+    except getopt.GetoptError:
+        usage(1)
+
+    dryrun = False
+    store_sid = False
+    delete_objects = False
+    user_sync = False
+    group_sync = False
+    user_spread = cereconf.AD_ACCOUNT_SPREAD
+    user_exchange_spread = cereconf.AD_EXCHANGE_SPREAD
+    group_spread = cereconf.AD_GROUP_SPREAD
+    logger_name = 'console'
+    logger_level = 'INFO'
+    for opt, val in opts:
+        if opt in ('--help','-h'):
+            usage()
+        elif opt in ('--dryrun',):
+            dryrun = True
+        elif opt in ('--delete',):
+            delete_objects = True
+        elif opt in ('--store-sid',):
+            store_sid = True
+        elif opt in ('--user-sync','-u'):
+            user_sync = True
+        elif opt in ('--group-sync','-g'):
+            group_sync = True
+        elif opt == '--user_spread':
+            user_spread = val
+        elif opt == '--user_exchange_spread':
+            user_exchange_spread = val
+        elif opt == '--group_spread':
+            group_spread = val
+        elif opt == '--logger-name':
+            logger_name = val
+        elif opt == '--logger-level':
+            logger_level = val
+        
+    fullsync(user_sync, group_sync, user_spread, user_exchange_spread,
+             group_spread, dryrun, delete_objects, store_sid, logger_name,
+             logger_level)
+
+def usage(exitcode=0):
+    print __doc__
+    sys.exit(exitcode)
+
+
+if __name__ == '__main__':
+    main()
