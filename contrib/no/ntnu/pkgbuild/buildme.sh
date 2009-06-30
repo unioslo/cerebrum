5aa2db3975aa9ebbbeddd614dcd8a8d73f417ded
Christian H. Toldnes <christht@unknown>
2009-06-30 12:32:47 +0000

    funker for bygging av trunk for rhel5

--- contrib/no/ntnu/pkgbuild/buildme.sh
+++ contrib/no/ntnu/pkgbuild/buildme.sh
@@ -8,6 +8,7 @@ if [ -z "$1" -o -z "$2" -o -z "$3" ]; then
 	echo "usage: $0 PLATFORM TARGET TAG-VERSION [REVISION]"
 	echo "Ex: $0 ubuntu804 ceresync 1.10"
 	echo "Ex: $0 rhel5 cerebrum 1.09 10882"
+	echo "Ex: $0 ubuntu804 ceresync trunk"
         exit 1
 elif [ -z "$4" ]; then
 	VER=$3
@@ -30,6 +31,11 @@ case "$PLATFORM" in
         ;;
 esac
 
+if [ "$VER" == "trunk" ]; then
+   URL="https://cerebrum.svn.sourceforge.net/svnroot/cerebrum/trunk"
+else
+   URL="https://cerebrum.svn.sourceforge.net/svnroot/cerebrum/tags/ntnu-prod-$VER"
+fi
 
 echo "Building $PLATFORM packages for $TARGET: $VER"
 echo -n "Creating temp-dir: "
@@ -38,25 +44,29 @@ echo $TEMPDIR
 pushd $TEMPDIR >/dev/null 2>&1
 if [ "$REV" == "latest" ]; then
 echo "Downloading source for: $VER, latest revision"
-    svn co -q https://cerebrum.svn.sourceforge.net/svnroot/cerebrum/tags/ntnu-prod-$VER/ 
-    pushd ntnu-prod-$VER >/dev/null 2>&1
+    svn co -q $URL cerebrum 
+    pushd cerebrum >/dev/null 2>&1
     REV=`svn log --limit 1 | awk '/^r[0-9]+/ {print substr($1,2); exit}'`
     popd >/dev/null 2>&1
     echo "Detected the following revision as latest for $VER: '$REV'"
 else
     echo "Downloadig source for $VER-$REV"
-    svn co https://cerebrum.svn.sourceforge.net/svnroot/cerebrum/tags/ntnu-prod-$VER -r $REV -q
+    svn co $URL -r $REV -q cerebrum
 fi
-mv ntnu-prod-$VER cerebrum-ntnu-$1-$REV
+pushd cerebrum >/dev/null 2>&1
+mv cerebrum ../cerebrum-ntnu-$VER-$REV
+popd >/dev/null 2>&1
+popd >/dev/null 2>&1
 echo "Source downloaded into $TEMPDIR/cerebrum-ntnu-$VER-$REV"
 
 # Verify existance of platform-specific build infrastructure:
-if [ ! -d "$TEMPDIR/cerebrum-ntnu-$VER-$REV/contrib/no/ntnu/pkgbuild/$TARGET/$PLATFORM" ]; then
+PKGDIR="$TEMPDIR/cerebrum-ntnu-$VER-$REV/contrib/no/ntnu/pkgbuild/$TARGET/$PLATFORM"
+if [ ! -d "$PKGDIR" ]; then
     echo "Unable to locate platform-specific build infrastructure. Looked for directory:"
-    echo "$TEMPDIR/cerebrum-ntnu-$VER-$REV/contrib/no/ntnu/pkgbuild/$TARGET/$PLATFORM"
+    echo "$PKGDIR"
     exit 1
 fi
 
 
 echo "Executing platform and target specific buid of $TARGET for $PLATFORM"
-exec $TEMPDIR/cerebrum-ntnu-$VER-$REV/contrib/no/ntnu/pkgbuild/$TARGET/$PLATFORM/pkgbuild.sh $TEMPDIR $VER $REL
+exec $PKGDIR/pkgbuild.sh $TEMPDIR $VER $REV
