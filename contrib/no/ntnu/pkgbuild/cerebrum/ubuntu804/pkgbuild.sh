7706b2fb9d7fb1e3453c3d0413629511abe89edb
Christian H. Toldnes <christht@unknown>
2009-07-01 06:35:37 +0000

    Support for cerebrum server packages for ubuntu804

---
+++ contrib/no/ntnu/pkgbuild/cerebrum/ubuntu804/pkgbuild.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+NAME=cerebrum-ntnu
+PKGPATH="contrib/no/ntnu/pkgbuild/cerebrum/ubuntu804"
+DEBPATH="$PKGPATH/debian/"
+if [ -z "$1" -o -z "$2" -o -z "$3" ]; then
+	echo "usage: $0 DIRECTORY VERSION REVISION"
+        exit 1
+fi
+DIR=$1
+VER=$2
+REV=$3
+VERSION=$VER-$REV
+BUILDDIR=$DIR/cerebrum-ntnu-$VERSION
+
+echo "Installing build-dep and python-cjson"
+sudo apt-get build-dep cerebrum-ntnu || exit 1
+sudo apt-get install python-cjson || exit 1
+
+echo "Building DEB packages for Ubuntu 8.04 of $NAME version $VER-$REL"
+pushd $BUILDDIR > /dev/null 2>&1
+
+echo "Adding symlink to debian build infrastructure at base of source"
+ln -s $PKGPATH/debian debian
+
+echo "Adding default changelog entry..."
+dch -b --newversion $VERSION "updated to $VERSION"
+
+echo "Building new package..."
+dpkg-buildpackage -rfakeroot
+
+popd > /dev/null 2>&1
+
+echo "Finished build of $NAME:"
+echo ""
+find $DIR -name \*.deb
+echo ""
+echo "You might want to enter $BUILDDIR and run ubuntu-mirror-add..."
+
+
