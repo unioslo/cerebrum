978ebd09faba14c24f159f69aa5a774fe118ae01
Alf B. Lervåg <alfborge@unknown>
2009-08-28 09:25:53 +0000

    Adding an ntnu-specific bofhd_auth.

---
+++ Cerebrum/modules/no/ntnu/bofhd_auth.py
@@ -0,0 +1,34 @@
+# -*- coding: iso-8859-1 -*-
+
+# Copyright 2003 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""
+Site specific auth.py for NTNU
+
+"""
+
+from Cerebrum.modules.bofhd import auth
+
+
+class BofhdAuth(auth.BofhdAuth):
+    """Defines methods that are used by bofhd to determine wheter
+    an operator is allowed to perform a given action.
+
+    This class only contains special cases for NTNU.
+    """
