673f49e96a0fe9cca7a47c62ce005fdba4e83ece
Alf B. Lervåg <alfborge@unknown>
2009-09-07 15:15:48 +0000

    Added more NTNU auth-stuff.

--- Cerebrum/modules/bofhd/utils.py
+++ Cerebrum/modules/bofhd/utils.py
@@ -128,12 +128,14 @@ class Constants(Constants.Constants):
     auth_target_type_maildomain = "maildom"
     auth_target_type_ou = "ou"
     auth_target_type_person = "person"
+    auth_target_type_account = "account"
     # These are wildcards, allowing access to _all_ objects of that type
     auth_target_type_global_group = "global_group"
     auth_target_type_global_host = "global_host" # also "disk"
     auth_target_type_global_maildomain = "global_maildom"
     auth_target_type_global_ou = "global_ou"
     auth_target_type_global_person = "global_person"
+    auth_target_type_global_account = "global_account"
 
     bofh_move_user = _BofhdRequestOpCode('br_move_user', 'Move user (batch)')
     bofh_move_user_now = _BofhdRequestOpCode('br_move_user_now', 'Move user')
--- Cerebrum/modules/no/ntnu/bofhd_auth.py
+++ Cerebrum/modules/no/ntnu/bofhd_auth.py
@@ -25,6 +25,12 @@ Site specific auth.py for NTNU
 
 from Cerebrum.modules.bofhd import auth
 
+from Cerebrum.Utils import Factory
+Person_class = Factory.get("Person")
+Account_class = Factory.get("Account")
+Group_class = Factory.get("Group")
+OU_class = Factory.get("OU")
+
 
 class BofhdAuth(auth.BofhdAuth):
     """Defines methods that are used by bofhd to determine wheter
@@ -32,47 +38,152 @@ class BofhdAuth(auth.BofhdAuth):
 
     This class only contains special cases for NTNU.
     """
+    def _has_person_access(self, operator, target, operation, operation_attr=None):
+        if not isinstance(target, Person_class):
+            raise ProgrammingError(
+                "Can't check person access on non-person (%s)" % target.entity_id)
+
+        if self.is_superuser(operator):
+            return True
+
+        if self._has_global_access(operator, operation,
+                                   self.const.auth_target_type_global_person,
+                                   target.entity_id, operation_attr=operation_attr):
+            return True
+
+        return self._has_access_to_entity_via_ou(operator, operation,
+                                                 target, operation_attr=operation_attr)
+
+    def _has_account_access(self, operator, target, operation, operation_attr=None):
+        if not isinstance(target, Account_class):
+            raise ProgrammingError(
+                "Can't check account access on non-account (%s)" % target.entity_id)
+
+        if self.is_superuser(operator):
+            return True
+
+        if self._has_global_access(operator, operation,
+                                   self.const.auth_target_type_global_account,
+                                   target.entity_id, operation_attr=operation_attr):
+            return True
+
+        return self._has_access_to_entity_via_ou(operator, operation,
+                                                 target, operation_attr=operation_attr)
+
+    def _has_group_access(self, operator, target, operation, operation_attr=None):
+        """TODO: Decide what this is."""
+        return self.is_superuser(operator)
+
+    def _has_ou_access(self, operator, ou, operation, operation_attr=None):
+        return self.is_superuser(operator)
+
+    def _has_entity_access(self, operator, target, operation, operation_attr=None):
+        if isinstance(target, Person_class):
+            return self._has_person_access(
+                operator, target, operation, operation_attr=operation_attr)
+        elif isinstance(target, Account_class):
+            return self._has_account_access(
+                operator, target, operation, operator_attr=operation_attr)
+        elif isinstance(target, OU_class):
+            return self._has_ou_access(
+                operator, target, operation, operator_attr=operation_attr)
+        elif isinstance(target, Group_class):
+            return self._has_group_access(
+                operator, target, operation, operator_attr=operation_attr)
+        else:
+            raise ProgrammingError(
+                "Can't check entity access on invalid target (%s)" % target.entity_id)
+
+    def _has_global_access(self, operator, operation, global_type, victim_id,
+                           operation_attr=None):
+        if self.is_superuser(operator):
+            return True
+        
+        return self.__super._has_global_access(
+            operator, operation, global_type, victim_id, operation_attr)
+
     def can_set_password(self, operator, target):
-        return True
+        operation = self.const.auth_set_password
+        return self._has_account_access(operator, target, operation)
 
-    def can_read_account(self, operator, account):
-        return True
+    def can_read_account(self, operator, target):
+        operation = self.const.auth_account_read
+        return self._has_account_access(operator, target, operation)
 
-    def can_create_account(self, operator):
-        return True
+    def can_create_account(self, operator, target):
+        operation = self.const.auth_account_create
+
+        if isinstance(target, Person_class):
+            return self._has_person_access(operator, target, operation)
+        elif isinstance(target, Group_class):
+            return self._has_group_access(operator, target, operation)
+        else:
+            raise ProgrammingError(
+                "Can't check create_account access on invalid target (%s)" % target.entity_id)
 
     def can_edit_account(self, operator, target):
-        return True
+        operation = self.const.auth_account_edit
+        return self._has_account_access(operator, target, operation)
 
     def can_delete_account(self, operator, target):
-        return True
+        operation = self.const.auth_account_delete
+        return self._has_account_access(operator, target, operation)
+
+    def _get_ou(self, ou_id):
+        ou = ou_id
+        if isinstance(ou_id, int):
+            ou = OU_class(self._db)
+            ou.find(ou_id)
+        return ou
 
     def can_edit_affiliation(self, operator, target, ou_id, affiliation_id):
-        return True
+        operation = self.const.auth_affiliation_edit
+
+        ou = self._get_ou(ou_id)
+        affiliation = self.const.PersonAffiliation(affiliation_id)
+
+        if not self._has_ou_access(operator, ou, operation, operation_attr=affiliation):
+            return False
+
+        if isinstance(target, Person_class):
+            return self._has_person_access(operator, target, operation)
+        elif isinstance(target, Account_class):
+            return self._has_account_access(operator, target, operation)
+        else:
+            raise ProgrammingError(
+                "Can't check edit_affiliation access on invalid target (%s)" % target.entity_id)
 
     def can_edit_external_id(self, operator, target, external_id_type):
-        return True
+        operation = self.const.auth_external_id_edit
+
+        return self._has_entity_access(
+                operator, target, operation, operation_attr=external_id_type)
 
     def can_read_external_id(self, operator, target, external_id_type):
-        return True
+        operation = self.const.auth_external_id_read
+
+        return self._has_entity_access(
+                operator, target, operation, operation_attr=external_id_type)
 
     def can_edit_homedir(self, operator, target, spread_id):
         return True
 
-    def can_read_person(self, operator, person):
-        return True
+    def can_read_person(self, operator, target):
+        operation = self.const.auth_person_read
+        return self._has_person_access(operator, target, operation)
 
-    def can_edit_person(self, operator, person):
-        return True
+    def can_edit_person(self, operator, target):
+        operation = self.const.auth_person_edit
+        return self._has_person_access(operator, target, operation)
 
     def can_create_person(self, operator):
-        return True
+        return self._has_global_access(
+            operator, self.const.auth_person_create,
+            self.const.auth_target_type_global_person, None)
 
-    def can_delete_person(self, operator, person):
-        return True
-
-    def can_haz_cheezeburger(self, lol, cat):
-        return True
+    def can_delete_person(self, operator, target):
+        operation = self.const.auth_person_delete
+        return self._has_person_access(operator, target, operation)
 
     def can_syncread_account(self, operator, spread, auth_method):
         return self._query_target_permissions(
