881ae5b8e637537fb2e5b485191c877e45b4826c
Trond Kandal <trond.kandal@ntnu.no>
2009-10-22 12:15:05 +0000

    first part of cleaning up duplicate code in ABCXmlParsers.py.
    start_abc_import.sh is not needed anymore.

--- Cerebrum/modules/no/ntnu/abcenterprise/ABCXmlParsers.py
+++ Cerebrum/modules/no/ntnu/abcenterprise/ABCXmlParsers.py
@@ -26,197 +26,26 @@ import abcconf
 from Cerebrum.modules.abcenterprise.ABCUtils import ABCTypes
 from Cerebrum.modules.abcenterprise.ABCUtils import ABCTypesError
 from Cerebrum.modules.abcenterprise.ABCUtils import ABCFactory
-#from Cerebrum.modules.abcenterprise.ABCDataObjects import DataPerson
-from Cerebrum.modules.no.ntnu.abcenterprise.ABCDataObjectsMixin import DataPersonMixin
-##from Cerebrum.modules.abcenterprise.ABCDataObjects import DataOU
-from Cerebrum.modules.no.ntnu.abcenterprise.ABCDataObjectsMixin import DataOUMixin
 from Cerebrum.modules.abcenterprise.ABCDataObjects import DataGroup
 from Cerebrum.modules.abcenterprise.ABCDataObjects import DataRelation
 from Cerebrum.modules.abcenterprise.ABCDataObjects import DataAddress
-
-class XMLEntityIterator:
-    """Iterate over an XML file and return complete elements of a given kind.
-
-    Iterative parsing in ElementTree is based on (event, element) pairs. For
-    dealing with people, OUs and the like, we need look at ('end',<ElementTree
-    'something'>) only, where 'something' represents the entity we are
-    interested in.
-    """
-
-    def __init__(self, filename, element):
-        # From version 2.5 (c)ElementTree is in Python's standard
-        # library, under xml.etree
-        if abcconf.CLASS_XMLPARSER == 'ElementTree':
-            try:
-                from xml.etree.ElementTree import parse, iterparse
-            except ImportError:
-                from elementtree.ElementTree import parse, iterparse
-        elif abcconf.CLASS_XMLPARSER == 'cElementTree':
-            try:
-                from xml.etree.cElementTree import parse, iterparse
-            except ImportError:
-                from cElementTree import parse, iterparse
-        else:
-            print abcconf.CLASS_XMLPARSER
-            print """
-CLASS_XMLPARSER in abcconf only supports 'ElementTree' and cElementTree'."""
-            sys.exit(1)
-
-        self.it = iter(iterparse(filename, ("start", "end")))
-        self.element_name = element
-
-        # Keep track of the root element (to prevent element caching)
-        junk, self._root = self.it.next()
-        
-
-    def next(self):
-        """Return next specified element, ignoring all else."""
-        
-        # Each time next is called, we drop whatever is dangling under
-        # root. It might be problematic if there is *a lot* of elements
-        # between two consecutive self.element_name elements.
-        self._root.clear()
-        
-        for event, element in self.it:
-            if event == "end" and element.tag == self.element_name:
-                return element
-        raise StopIteration
-                
-
-    def __iter__(self):
-        return self
+from Cerebrum.modules.abcenterprise.ABCXmlParsers import XMLOrg2Object
+from Cerebrum.modules.abcenterprise.ABCXmlParsers import XMLOU2Object
+from Cerebrum.modules.abcenterprise.ABCXmlParsers import XMLPerson2Object
 
 
-class XMLPropertiesParser(object):
-
-    def __init__(self, xmliter):
-        """Constructs an iterator supplying settings."""
-        self._xmliter = xmliter
+from Cerebrum.modules.no.ntnu.abcenterprise.ABCDataObjectsMixin import DataPersonMixin
+from Cerebrum.modules.no.ntnu.abcenterprise.ABCDataObjectsMixin import DataOUMixin
 
 
-    def __iter__(self):
-        return self
-
-    def _check_type(self, type, args):
-        if not ABCTypes.get_type(type, args):
-            #TODO: cleanup
-            raise ABCTypesError, "wrong type and/or argument: '%s' '%s'" % (type,
-                                                                            args)
-            
-    def next(self):
-        """ """
-        # This call with propagate StopIteration when all the (XML) elements
-        # are exhausted.
-        # Iterate over *all* subelements
-        element = self._xmliter.next()
-        datasource = target = timestamp = ""
-        
-        for sub in element.getiterator():
-            value = None
-            if sub.text:
-                value = sub.text.strip().encode("latin1")
-
-            if sub.tag == "datasource":
-                datasource = value
-            elif sub.tag == "target":
-                target = value
-            elif sub.tag == "timestamp":
-                timestamp = value
-            elif sub.tag == "types":
-                for n in sub.getiterator():
-                    if not n.text:
-                        continue
-                    value = n.text.strip().encode("latin1")
-                    if n.tag in ("orgidtype",):
-                        self._check_type("orgidtype", (value,))
-                    elif n.tag in ("orgnametype",):
-                        self._check_type("orgnametype", (value,))
-                    elif n.tag in ("ouidtype",):
-                        self._check_type("ouidtype", (value,))
-                    elif n.tag in ("ounametype",):
-                        self._check_type("ounametype", (value,))
-                    elif n.tag in ("personidtype",):
-                        self._check_type("personidtype", (value,))
-                    elif n.tag in ("partnametype",):
-                        self._check_type("partnametype", (value,))
-                    elif n.tag in ("groupidtype",):
-                        self._check_type("groupidtype", (value,))
-
-                    # Get the "strange" types
-                    elif n.tag in ("addresstype", "contacttype", "relationtype"):
-                        for t, v in (("addresstype", 1),
-                                     ("contacttype", 1),
-                                     ("relationtype", 2)):
-                            if not n.tag == t:
-                                continue
-                            if len(n.attrib) <> v:
-                                raise ABCTypesError, "wrong number of attributes"
-                            attr = None
-                            subj = n.attrib.get("subject")
-                            if v > 1:
-                                obj =  n.attrib.get("object")
-                                attr = (subj, obj, value)
-                            else:
-                                attr = (subj, value)
-                            self._check_type(t, attr)
-                    elif n.tag <> "types":
-                        raise ABCTypesError, "got unknown type: %s" % n.tag
-        # NB! This is crucial to save memory on XML elements
-        element.clear()
-        return (datasource, target, timestamp)
-    
-
-class XMLEntity2Object(object):
-
-    def __init__(self, xmliter):
-        """Constructs an iterator supplying DataEntity objects.
-
-        xmliter is the the underlying ElementTree iterator (here we do not
-        care if it is in-memory or on file).
-        """
-        self._xmliter = xmliter
-
-
-    def next(self):
-        tmp = self._xmliter.next()
-        return tmp
-
-
-    def _make_mxdate(self, text, format = "%Y-%m-%d"):
-        try:
-            year, month, day = time.strptime(text, format)[:3]
-        except ValueError:
-            return None
-        return Date(year, month, day)
-
-
-    def __iter__(self):
-        return self
-
-
-    def _make_address(self, addr_element):
-        """Make a list of tuples out of an <adresse>."""
-        assert addr_element.tag == "address"
-        result = DataAddress()
-        for sub in addr_element.getiterator():
-            if not sub.text:
-                continue
-            value = sub.text.strip().encode("latin1")
-            for tag in ("pobox", "street", "postcode", "city", "country"):
-                if not tag == sub.tag:
-                    continue
-                setattr(result, tag, value)
-        return result
-
-
-class XMLOrg2Object(XMLEntity2Object):
+class XMLOrg2ObjectMixIn(XMLOrg2Object):
     """A converter class that maps ElementTree's Element to DataOU.
     An Organization is represented as an OU."""
 
     def __init__(self, xmliter):
         """Constructs an iterator supplying DataPerson objects."""
 
-        super(XMLOrg2Object, self).__init__(xmliter)
+        super(XMLOrg2ObjectMixIn, self).__init__(xmliter)
 
     def next(self):
         """Return the next DataOU object. Returns a ABCTypesError
@@ -282,12 +111,12 @@ class XMLOrg2Object(XMLEntity2Object):
         return result
 
 
-class XMLOU2Object(XMLEntity2Object):
+class XMLOU2ObjectMixIn(XMLOU2Object):
     """A converter class that maps ElementTree's Element to DataOU."""
 
     def __init__(self, xmliter):
         """Constructs an iterator supplying DataPerson objects."""
-        super(XMLOU2Object, self).__init__(xmliter)
+        super(XMLOU2ObjectMixIn, self).__init__(xmliter)
 
     def next(self):
         """Return the next DataOU object. Returns a ABCTypesError
@@ -363,47 +192,13 @@ class XMLOU2Object(XMLEntity2Object):
         return result
     
 
-class XMLPerson2Object(XMLEntity2Object):
+class XMLPerson2ObjectMixIn(XMLPerson2Object):
     """A converter class that maps ElementTree's Element to DataPerson."""
 
     def __init__(self, xmliter):
         """Constructs an iterator supplying DataPerson objects."""
 
-        super(XMLPerson2Object, self).__init__(xmliter)
-
-
-    def _make_person_name(self, name_element):
-        """Make a list out of <name>. Names are type+value in
-        Cerebrum."""
-
-        result = []
-        for sub in name_element.getiterator():
-            if not sub.text:
-                continue
-            value = sub.text.strip().encode("latin1")
-            for tag in ("fn", "sort", "nickname"):
-                if not tag == sub.tag:
-                    continue
-                if value:
-                    result.append((ABCTypes.get_name_type(tag), value))
-            if sub.tag == "n":
-                for n in sub.getiterator():
-                    if not n.text:
-                        continue
-                    value = n.text.strip().encode("latin1")
-                    for tag in ("family", "given", "other", "prefix", "suffix"):
-                        if not tag == n.tag:
-                            continue
-                        if value:
-                            result.append((ABCTypes.get_name_type(tag), value))
-                        if n.tag in ("partname",):
-                            if len(n.attrib) <> 1:
-                                raise ABCTypesError, "error in partname: %s" % value
-                            type = ABCTypes.get_type("partname",
-                                                     (n.attrib.get("partnametype"),))
-                            if value:
-                                result.append((type, value))          
-        return result
+        super(XMLPerson2ObjectMixIn, self).__init__(xmliter)
 
 
     def next(self):
@@ -430,7 +225,7 @@ class XMLPerson2Object(XMLEntity2Object):
                     raise ABCTypesError, "error in personid: %s" % value
                 type = sub.attrib.get("personidtype")
                 if type == "fnr_closed":
-                    result.fnr_closed.append(value)
+                    result.fnr_closed.append(int(value))
                 else:
                     result.add_id(ABCTypes.get_type("personidtype",(type,)),
                               value)
@@ -470,144 +265,6 @@ class XMLPerson2Object(XMLEntity2Object):
             elif sub.tag == "reserv_publish":
                 if value:
                     result.reserv_publish = value.lower()
-        # NB! This is crucial to save memory on XML elements
-        element.clear()
-        return result
-
-
-class XMLGroup2Object(XMLEntity2Object):
-    """A converter class that maps ElementTree's Element to DataGroup."""
-
-    def __init__(self, xmliter):
-        """Constructs an iterator supplying DataGroup objects."""
-
-        super(XMLGroup2Object, self).__init__(xmliter)
-
-    def next(self):
-        """Return the next DataGroup object. Returns a ABCTypesError
-        exception if object has errors in types. These objects should be
-        skipped.
-
-        Consume the next XML-element describing an OU, and return a
-        suitable representation (DataGroup).
-        """
-
-        # This call with propagate StopIteration when all the (XML) elements
-        # are exhausted.
-        element = super(XMLGroup2Object, self).next()
-        result = DataGroup()
-       
-        # Iterate over *all* subelements
-        for sub in element.getiterator():
-            value = None
-            if sub.text:
-                value = sub.text.strip().encode("latin1")
-
-            if sub.tag == "groupid":
-                if len(sub.attrib) <> 1:
-                    raise ABCTypesError, "error in groupid: %s" % value
-                type = sub.attrib.get("groupidtype")
-                result.add_id(ABCTypes.get_type("groupidtype",(type,)),
-                              value)
-            elif sub.tag == "description":
-                result.desc = value
-           
-        # NB! This is crucial to save memory on XML elements
-        element.clear()
-        return result
-
-
-class XMLRelation2Object(XMLEntity2Object):
-    """A converter class that maps ElementTree's Element to DataRelation."""
-
-    def __init__(self, xmliter):
-        """Constructs an iterator supplying DataRelation objects."""
-
-        super(XMLRelation2Object, self).__init__(xmliter)
-
-    def _get_subvalues(self, iter):
-        value = None
-        result = []
-        for s in iter:
-            if s.text:
-                value = s.text.strip().encode("latin1")
-            if s.tag == "personid":
-                if len(s.attrib) <> 1:
-                    raise ABCTypesError, "error in personid: %s" % value
-                type = s.attrib.get("personidtype")
-                result.append(("person", ABCTypes.get_type("personidtype",
-                                                 (type,)),
-                               value))
-            elif s.tag == "groupid":
-                if len(s.attrib) <> 1:
-                    raise ABCTypesError, "error in groupid: %s" % value
-                type = s.attrib.get("groupidtype")
-                result.append(("group", ABCTypes.get_type("groupidtype",
-                                                          (type,)),
-                               value))
-            elif s.tag == "org":
-                org = ou = None
-                for o in s.getiterator():
-                    if o.text:
-                        value = o.text.strip().encode("latin1")
-                    if o.tag == "orgid":
-                        if len(o.attrib) <> 1:
-                            raise ABCTypesError, "error in org: %s" % value
-                        type = o.attrib.get("orgidtype")
-                        org = (ABCTypes.get_type("orgidtype",
-                                                 (type,)),
-                               value)
-                    elif o.tag == "ouid":
-                        if len(o.attrib) <> 1:
-                            raise ABCTypesError, "error in ouid: %s" % value
-                        type = o.attrib.get("ouidtype")
-                        ou = (ABCTypes.get_type("ouidtype",
-                                                (type,)),
-                              value)
-                # Org is required
-                if not org:
-                    # raise ABCTypesError, "no org"
-                    # TODO: enable again.
-                    pass
-                if org and ou:
-                    result.append(("org", org, ou))
-                elif org:
-                    result.append(("org", org))
-                else:
-                    result.append(("ou", ou))
-        return result
-
-
-    def next(self):
-        """Return the next DataRelation object. Returns a ABCTypesError
-        exception if object has errors in types. These objects should be
-        skipped.
-
-        Consume the next XML-element describing an OU, and return a
-        suitable representation (DataRelation).
-        """
-
-        # This call with propagate StopIteration when all the (XML) elements
-        # are exhausted.
-        element = super(XMLRelation2Object, self).next()
-        result = DataRelation()
-       
-        # Iterate over *all* subelements
-        for sub in element.getiterator():
-            if not result.type:
-                result.type = sub.attrib.get("relationtype")
-
-            if sub.tag == "subject":
-                res = self._get_subvalues(sub.getiterator())
-                if (not isinstance(res, list)) or len(res) <> 1:
-                    raise ABCTypesError, "res is '%s'" % res
-                result.subject = res
-            elif sub.tag == "object":
-                res = self._get_subvalues(sub.getiterator())
-                if not isinstance(res, list):
-                    raise ABCTypesError, "object is '%s' not a list" % res
-                result.object = res                
-                
         # NB! This is crucial to save memory on XML elements
         element.clear()
         return result
