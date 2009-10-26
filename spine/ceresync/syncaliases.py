3b382deb9de73b8e9ed0516ab963051b803a9d87
Alf B. Lervåg <alfborge@unknown>
2009-10-26 14:27:06 +0000

    syncaliases is now able to load/save data to/from file.  added some xml files that can be used for testing

--- spine/ceresync/syncaliases.py
+++ spine/ceresync/syncaliases.py
@@ -1,32 +1,57 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 
-from ceresync import syncws as sync
-import ceresync.backend.file as filebackend
-from sys import exit
+# Copyright 2004, 2005, 2006 University of Oslo, Norway
+#
+# This filebackend is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
+import sys
+
+from ceresync import syncws as sync
 from ceresync import config
+
 log = config.logger
 
 def main():
-    config.parse_args()
+    sync_options = {}
+    config.parse_args(config.make_testing_options())
+    config.set_testing_options(sync_options)
 
     try:
         s = sync.Sync()
     except sync.AlreadyRunningWarning, e:
         log.warning(str(e))
-        exit(1)
+        sys.exit(1)
     except sync.AlreadyRunning, e:
         log.error(str(e))
-        exit(1)
+        sys.exit(1)
+
+    if config.getboolean('args', 'use_test_backend'):
+        import ceresync.backend.test as filebackend
+    else:
+        import ceresync.backend.file as filebackend
 
     aliases = filebackend.Alias()
     
     log.debug("Syncronizing aliases")
-    aliases.begin(False, unicode=True)
+    aliases.begin(unicode=True)
 
     try:
-        for alias in s.get_aliases():
+        for alias in s.get_aliases(**sync_options):
             log.debug("Processing account '%s@%s'", alias.local_part, alias.domain)
             aliases.add(alias)
     except IOError,e:
@@ -34,7 +59,6 @@ def main():
         aliases.abort()
     else:
         aliases.close()
-
 
 if __name__ == "__main__":
     main()
