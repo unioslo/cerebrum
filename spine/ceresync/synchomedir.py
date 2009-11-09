746fc7456072274495807679d03d5221a690189f
Alf B. Lervåg <alfborge@unknown>
2009-11-09 09:18:47 +0000

    Refactored a bit.  Moved all actual work out to the setup script to ease testing and increase flexibility.

--- spine/ceresync/synchomedir.py
+++ spine/ceresync/synchomedir.py
@@ -20,11 +20,15 @@
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 import os, sys
+import subprocess
 
 from ceresync import config
 from ceresync import syncws as sync
 
 log = config.logger
+
+class InconsistencyError(Exception):
+    pass
 
 class HomedirSync(object):
     statuses = [
@@ -35,12 +39,10 @@ class HomedirSync(object):
         'pending_restore'
     ]
 
-    def __init__(self, hostname, show_db, lint, dryrun, no_report,
-                 retry_failed, setup_script, testing_options, root):
+    def __init__(self, hostname, dryrun, no_report, retry_failed,
+                 setup_script, testing_options, root):
         self.cerews = sync.Sync()
         self.hostname = hostname
-        self.show_db = show_db
-        self.lint = lint
         self.dryrun = dryrun
         self.no_report = no_report
         self.retry_failed = retry_failed
@@ -48,75 +50,72 @@ class HomedirSync(object):
         self.testing_options = testing_options
         self.root = root
 
-    def execute(self):
-        if self.lint:
-            self.lint_homedirs()
-        elif self.show_db:
-            self.show_homedirs()
-        else:
-            self.make_homedirs()
-
     def lint_homedirs(self):
         status='on_disk'
         print "Status %s in cerebrum, but does not exist on disk:" % (status,)
-        for homedir in self.get_homedirs(status):
-            path = self.get_path(homedir)
+        for homedir in self._get_homedirs(status):
+            path = self._get_path(homedir)
             if not os.path.isdir(path):
                 print path
 
         status='not_created'
         print "Status %s in cerebrum, but does exist on disk:" % (status,)
-        for homedir in self.get_homedirs(status):
-            path = self.get_path(homedir)
+        for homedir in self._get_homedirs(status):
+            path = self._get_path(homedir)
             if os.path.isdir(path):
                 print path
 
     def show_homedirs(self):
         for status in self.statuses:
             print "Status: %s" % (status,)
-            for homedir in self.get_homedirs(status):
-                path = self.get_path(homedir)
+            for homedir in self._get_homedirs(status):
+                path = self._get_path(homedir)
                 print "  %-9s %s" % (homedir.account_name, path)
 
     def make_homedirs(self):
-        status = self.get_create_status()
-        for homedir in self.get_homedirs(status):
-            self.make_homedir(homedir)
+        status = self._get_create_status()
+        for homedir in self._get_homedirs(status):
+            self._make_homedir(homedir)
 
-    def get_homedirs(self, status):
-        testing_options = self.get_testing_options(status)
+    def _get_path(self, homedir):
+        return self._root_path(homedir.homedir)
+
+    def _get_homedirs(self, status):
+        testing_options = self._get_testing_options(status)
         return self.cerews.get_homedirs(status, self.hostname, **testing_options)
 
-    def get_create_status(self):
+    def _get_create_status(self):
         if self.retry_failed:
             return 'create_failed'
         return 'not_created'
 
-    def make_homedir(self, homedir):
-        path = self.get_path(homedir)
+    def _make_homedir(self, homedir):
+        path = self._get_path(homedir)
         log.debug("Creating homedir for %s: %s",
             homedir.account_name, path)
 
-        try:
-            if not os.path.isdir(path):
-                self.create_homedir(path, homedir)
-                self.run_setup_script(path, homedir)
+        result_status = 'on_disk'
+        if not os.path.isdir(path):
+            try:
+                self._run_create_script(path, homedir)
                 log.info("Created homedir %s for %s" % (
                     path, homedir.account_name))
-            else:
-                log.debug("Homedir %s for %s is ok" % (
-                    path, homedir.account_name))
-            result_status = 'on_disk'
-        except Exception, e:
-            log.exception("Failed creating homedir for %s: %s" % (
-                homedir.account_name, e))
-            result_status= 'create_failed'
+            except Exception, e:
+                result_status = 'create_failed'
+                log.exception("Failed creating homedir for %s: %s" % (
+                    homedir.account_name, e))
+        else:
+            log.debug("Homedir %s for %s already exists" % (
+                path, homedir.account_name))
 
         log.info("Setting status for homedir for %s to %s",
             homedir.account_name, result_status)
-        self.set_homedir_status(homedir, result_status)
+        self._set_homedir_status(homedir, result_status)
 
-    def get_testing_options(self, status):
+    def _root_path(self, path):
+        return os.path.join(self.root, path.lstrip("/"))
+
+    def _get_testing_options(self, status):
         in_file = '%s_homedir_xml_in' % status
         out_file = '%s_homedir_xml_out' % status
         return {
@@ -124,53 +123,47 @@ class HomedirSync(object):
             'homedir_xml_out': self.testing_options.get(out_file),
         }
 
-    def create_homedir(self, path, homedir):
-        parent = os.path.dirname(path)
-        if not os.path.isdir(parent):
-            self.create_parent_directory(parent)
+    def _run_create_script(self, path, homedir):
+        if self.dryrun:
+            return
 
-        self.create_directory(
-            path, homedir.posix_uid, homedir.posix_gid)
+        cmd = (
+            str(self.setup_script),
+            str(homedir.posix_uid),
+            str(homedir.posix_gid),
+            str(path),
+        )
 
-    def run_setup_script(self, path, homedir):
-        cmd = "echo %s %d %d %s %s" % (
-                    self.setup_script, homedir.posix_uid, homedir.posix_gid,
-                    path, homedir.account_name)
-        log.info("Running setup script: %s", cmd)
-        if not self.dryrun:
-            r = os.system(cmd)
-            if r != 0:
-                raise Exception("\"%s\" failed" % self.setup_script)
+        log.info("Running create script: %s", cmd)
 
-    def set_homedir_status(self, homedir, status):
-        if not self.no_report and not self.dryrun:
-            self.cerews.set_homedir_status(homedir.homedir_id, status)
+        returncode = subprocess.call(cmd)
+        if returncode != 0:
+            raise Exception("\"%s\" failed" % self.setup_script)
 
-    def create_parent_directory(self, path):
-        log.debug("Creating parent dir: %s", path)
-        if not self.dryrun:
-            os.mkdir(path, 0755)
+    def _set_homedir_status(self, homedir, status):
+        if self.no_report or self.dryrun:
+            return
 
-    def create_directory(self, path, uid, gid):
-        log.debug("Creating dir: %s", path)
-        if not self.dryrun:
-            os.mkdir(path, 0700)
-            os.chown(path, uid, gid)
+        self._verify_consistency(homedir, status)
+        self.cerews.set_homedir_status(homedir.homedir_id, status)
 
-    def get_path(self, homedir):
-        return self.root_path(homedir.homedir)
+    def _verify_consistency(self, homedir, status):
+        if status == "on_disk" and not self._homedir_exists(homedir):
+            raise InconsistencyError(
+                "Would report non-existing homedir as %s: %s" % (
+                    status, homedir.homedir))
 
-    def root_path(self, path):
-        return os.path.join(self.root, path.lstrip("/"))
+        if status == "create_failed" and self._homedir_exists(homedir):
+            raise InconsistencyError(
+                "Would report existing homedir as %s: %s" % (
+                    status, homedir.homedir))
 
 def get_option_dict(config):
     return {
-        'show_db': config.getboolean('args', 'show_db'),
-        'lint': config.getboolean('args', 'lint'),
         'dryrun': config.getboolean('args', 'dryrun'),
         'no_report': config.getboolean('args', 'no_report'),
         'retry_failed': config.getboolean('args', 'retry_failed'),
-        'setup_script': config.get('homedir', 'setup_script', default="/local/skel/bdb-setup"),
+        'setup_script': get_setup_script(config),
         'hostname': get_hostname(config),
         'root': config.get('args', 'root', default="/"),
     }
@@ -207,13 +200,21 @@ def get_hostname(config):
     hostname = config.get('homedir', 'hostname', default=os.uname()[1]),
     return config.get('args', 'hostname', default=hostname) # Allow command-line override
 
+def get_setup_script(config):
+    setup_script = config.get('homedir', 'setup_script', default="/local/skel/bdb-setup")
+    return config.get('args', 'setup_script', default=setup_script) # Allow command-line override
+
 def main():
     config.parse_args([
         config.make_option(
             "-H", "--hostname",
             action="store",
             type="string",
-            metavar="HOSTNAME",
+            help="pretend to be file server HOSTNAME"),
+        config.make_option(
+            "-S", "--setup_script",
+            action="store",
+            type="string",
             help="pretend to be file server HOSTNAME"),
         config.make_option(
             "-n", "--no-report",
@@ -248,8 +249,14 @@ def main():
 
     options = get_option_dict(config)
     options['testing_options'] = get_testing_option_dict(config)
+
     sync = HomedirSync(**options)
-    sync.execute()
+    if config.getboolean('args', 'lint'):
+        sync.lint_homedirs()
+    elif config.getboolean('args', 'show_db'):
+        sync.show_homedirs()
+    else:
+        sync.make_homedirs()
 
 if __name__ == "__main__":
     main()
