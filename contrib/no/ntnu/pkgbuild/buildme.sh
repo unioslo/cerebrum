344bbd3c77090d7407b78e2ad179fc67dc5f87ab
Alf B. Lervåg <alfborge@unknown>
2009-11-30 13:35:24 +0000

    Merged in changes from ntnu-prod-2.0@12021,12038,12040,12169.

--- contrib/no/ntnu/pkgbuild/buildme.sh
+++ contrib/no/ntnu/pkgbuild/buildme.sh
@@ -3,39 +3,45 @@
 # Main script for building server and client packages on various platforms
 #
 
-if [ -z "$1" -o -z "$2" -o -z "$3" ]; then
+if (( $# < 3 )); then
         echo "Build cerebrum or ceresync packages for various platforms."
         echo "usage: $0 PLATFORM TARGET TAG-VERSION [REVISION]"
-        echo "Ex: $0 ubuntu804 ceresync 1.10"
+        echo "Ex: $0 ubuntu804 ceresync 2.0.1 #tag"
         echo "Ex: $0 rhel5 cerebrum 1.09 10882"
-        echo "Ex: $0 ubuntu804 ceresync trunk"
+        echo "Ex: $0 solaris10 ceresync trunk"
+        echo "Ex: $0 ubuntu804 cerebrum 2.0 #branch"
         exit 1
-elif [ -z "$4" ]; then
-        VER=$3
-        REV="latest"
-else
-        VER=$3
-        REV=$4
 fi
 PLATFORM=$1
 TARGET=$2
+VER=$3
+REV=${4:-latest}
 TRUNK=0
+
 # Qualified quess of correctness of PLATFORM argument:
 case "$PLATFORM" in
     ubuntu804)
-        if [ -z "`grep -i 'Ubuntu 8.04' /etc/issue`" ]; then
+        if grep -iq 'Ubuntu 8.04' /etc/issue; then
+            echo "System seems to qualify as '$PLATFORM'"
+        else
             echo "/etc/issue did not identify this system as $PLATFORM"
             exit 1
-        else
-            echo "System seems to qualify as '$PLATFORM'"
         fi
         ;;
     rhel5)
-        if [ -z "`grep -i 'Red Hat Enterprise Linux Server release 5' /etc/issue`" ]; then
+        if grep -iq 'Red Hat Enterprise Linux Server release 5' /etc/issue; then
+            echo "System seems to qualify as '$PLATFORM'"
+        else
             echo "/etc/issue did not identify this system as $PLATFORM"
             exit 1
-        else
+        fi
+        ;;
+    solaris10)
+        if uname -a | grep 'SunOS .* 5\.10 .* sparc' > /dev/null 2>&1; then
             echo "System seems to qualify as '$PLATFORM'"
+        else
+            echo "uname did not identify this system as $PLATFORM"
+            exit 1
         fi
         ;;
     *)
@@ -44,38 +50,38 @@ case "$PLATFORM" in
         ;;
 esac
 
-if [ "$VER" == "trunk" ]; then
-   URL="https://cerebrum.svn.sourceforge.net/svnroot/cerebrum/trunk"
+
+if [ "$VER" = "trunk" ]; then	# trunk
+   URL="https://cerebrum.svn.sourceforge.net/svnroot/cerebrum/trunk/cerebrum"
    echo "Setting version to 0 for trunk."
    VER=0
-   TRUNK=1
-else
-   URL="https://cerebrum.svn.sourceforge.net/svnroot/cerebrum/tags/ntnu-prod-$VER"
+else	# branch or tag
+   IFS=. read major medium minor <<< "$VER"
+
+   if [[ -n $minor ]]; then	#tag
+       URL="https://cerebrum.svn.sourceforge.net/svnroot/cerebrum/tags/ntnu-prod-$VER/cerebrum"
+       echo "Setting version to tag $VER"
+   else	# branch
+       URL="https://cerebrum.svn.sourceforge.net/svnroot/cerebrum/branches/ntnu-prod-$VER/cerebrum"
+       echo "Setting version to branch $VER"
+   fi
 fi
 
 echo "Building $PLATFORM packages for $TARGET: $VER"
-echo -n "Creating temp-dir: "
 TEMPDIR="`mktemp -d /tmp/cerebuild.XXXXXXXXX`"
-echo $TEMPDIR
-pushd $TEMPDIR >/dev/null 2>&1
-if [ "$REV" == "latest" ]; then
-echo "Downloading source for: $VER, latest revision"
-    svn co -q $URL
-    if [ "$TRUNK" == "1" ]; then
-        mv trunk/cerebrum cerebrum
-        rm -rf trunk
-    else
-        mv * cerebrum
-    fi
-    pushd cerebrum >/dev/null 2>&1
-    REV=`svn log --limit 1 | awk '/^r[0-9]+/ {print substr($1,2); exit}'`
-    popd >/dev/null 2>&1
+echo "Created temp-dir: $TEMPDIR"
+pushd $TEMPDIR >/dev/null 2>&1 || exit
+if [ "$REV" = "latest" ]; then
+    echo "Downloading source for: $VER, latest revision from $URL"
+    svn co -q $URL cerebrum
+
+    REV=`svn log cerebrum --limit 1 --quiet | awk '/^r[0-9]+/ {print substr($1,2); exit}'`
     echo "Detected the following revision as latest for $VER: '$REV'"
+    mv cerebrum cerebrum-ntnu-$VER-$REV
 else
-    echo "Downloadig source for $VER-$REV"
-    svn co $URL -r $REV -q
+    echo "Downloadig source for $VER-$REV from $URL"
+    svn co -q -r $REV $URL cerebrum-ntnu-$VER-$REV
 fi
-mv * cerebrum-ntnu-$VER-$REV
 popd >/dev/null 2>&1
 echo "Source downloaded into $TEMPDIR/cerebrum-ntnu-$VER-$REV"
 
