20151325ba1ae70851a1d0b8fcd903bb9cfcabea
Trond Kandal <trond.kandal@ntnu.no>
2010-02-12 07:56:44 +0000

    added some comments. i have not deleted the code for getting affiliationstatus because it may be used later.

--- contrib/no/ntnu/export_kjernen.py
+++ contrib/no/ntnu/export_kjernen.py
@@ -23,7 +23,7 @@ class Export2Kjernen(object):
         self._person = Factory.get('Person')(self.db)
         self._ou = Factory.get('OU')(self.db)
         self._account = Factory.get('Account')(self.db)
-        self._iso_format = '%Y-%m-%d'
+        self._iso_format = '%Y.%m.%d'
         self.outfile = outfile
         self.verbose = verbose
         self.doTiming = doTiming
@@ -33,6 +33,7 @@ class Export2Kjernen(object):
             self.co.affiliation_alumni:         'alumni',
             self.co.affiliation_student:        'student'
         }
+        ## not used at the moment, maybe later...
         self.affiliations_status = {
             self.co.affiliation_status_tilknyttet_bilag:        'bilag',
             self.co.affiliation_status_ansatt_ansatt:           'ansatt',
@@ -68,7 +69,6 @@ class Export2Kjernen(object):
         if self.verbose:
             print 'Fetching nins...'
         nins = {}
-        ## for row in self._person.list_external_ids(self.co.system_kjernen, self.co.externalid_fodselsnr):
         for row in self._person.list_external_ids(id_type=self.co.externalid_fodselsnr):
             nins[row["entity_id"]]=row["external_id"]
         return nins
@@ -77,7 +77,6 @@ class Export2Kjernen(object):
         if self.verbose:
             print 'Fetching emailaddreses...'
         emails = {}
-        ## for row in self._person.list_contact_info(self.co.system_kjernen, self.co.contact_email):
         for row in self._person.list_contact_info(contact_type=self.co.contact_email):
             emails[row["entity_id"]]=row["contact_value"]
         return emails
@@ -121,14 +120,16 @@ class Export2Kjernen(object):
         affs_status = {}
         ous = {}
         aff_created = {}
-        ## for row in self._person.list_affiliations(source_system=self.co.system_kjernen):
         for row in self._person.list_affiliations():
             affs[row['person_id']] = row['affiliation']
-            affs_status[row['person_id']] = row['status']
+            ## affiliation status is not used at the moment, maybe later...
+            ## affs_status[row['person_id']] = row['status']
             ous[row['person_id']] = row['ou_id']
             aff_created[row['person_id']] = row['create_date'].strftime(self._iso_format)
             
-        return (affs, ous, affs_status, aff_created)
+        ## affiliation status is not used at the moment, maybe later...
+        ## return (affs, ous, affs_status, aff_created)
+        return (affs, ous, aff_created)
  
     def write_file(self):
         if self.verbose:
@@ -136,7 +137,9 @@ class Export2Kjernen(object):
         i = 0
         f = open(self.outfile, fileMode, bufferSize)
         for k in self.nins.keys():
-            out_line = '%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;\n' % \
+            ## affiliation status is not used at the moment, maybe later...
+            ## out_line = '%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;\n' % \
+            out_line = '%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;\n' % \
                     (k,
                     self.nins[k][:6],
                     self.nins[k][6:],
@@ -145,7 +148,7 @@ class Export2Kjernen(object):
                     self.lastnames.get(k, ''),
                     self.emails.get(k, ''),
                     self.affiliations.get(self.affs.get(k, ''), ''),
-                    self.affiliations_status.get(self.affs_status.get(k, ''),''),
+                    ## self.affiliations_status.get(self.affs_status.get(k, ''),''),
                     self.stedkoder.get(self.ous.get(k, ''), ''),
                     self.accounts.get(k, ''),
                     self.created.get(k, ''))
@@ -185,7 +188,9 @@ class Export2Kjernen(object):
         if self.doTiming and self.verbose:
             self.print_time(before)
             before = time.time()
-        (self.affs, self.ous, self.affs_status, self.created) = self.get_affiliations()
+        ## affiliation status is not used at the moment, maybe later...
+        ## (self.affs, self.ous, self.affs_status, self.created) = self.get_affiliations()
+        (self.affs, self.ous, self.created) = self.get_affiliations()
         if self.doTiming and self.verbose:
             self.print_time(before)
             before = time.time()
