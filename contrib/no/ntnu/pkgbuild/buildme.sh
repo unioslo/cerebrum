4b99f12d7329174cd6d3da274cd8542b8f4b84d3
Leiv Arild Andenes <arileiv@unknown>
2010-02-25 12:39:21 +0000

    endre default til lokal byggjeskriptkatalog. Litt finpuss av loggmeldingar

--- contrib/no/ntnu/pkgbuild/buildme.sh
+++ contrib/no/ntnu/pkgbuild/buildme.sh
@@ -2,30 +2,27 @@
 #
 # Main script for building server and client packages on various platforms
 #
-# If you didn't write this script. Please don't modify it...
-#
-#
 
 #
-# Hinder idiots trying to build as root...
+# Dont build as root...
 #
 if [ "`id -u`" -eq "0" ]; then
 	echo "You should never run this script as root"
 	exit 1
 fi
 
-
+# Parse parameters
 if (( $# < 3 )); then
         echo "Build cerebrum or ceresync packages for various platforms."
-        echo "usage: $0 PLATFORM TARGET TAG-VERSION [REVISION]"
-        echo "Ex: $0 ubuntu804 ceresync 2.0.1 #tag"
-        echo "Ex: $0 rhel5 cerebrum 1.09 10882"
-        echo "Ex: $0 ubuntu804 cerebrum 2.0 #branch"
+        echo "usage: $0 PLATFORM TARGET VERSION [REVISION]"
+        echo "Ex: $0 ubuntu804 ceresync 2.0.1   #tag, last revision"
+        echo "Ex: $0 rhel5 ceresync trunk 12472 #trunk, specific revision"
+        echo "Ex: $0 ubuntu804 cerebrum 2.0     #branch, last revision"
         exit 1
 fi
 PLATFORM=$1
 TARGET=$2
-VER=$3
+VER=${3:-trunk}
 REV=${4:-latest}
 TRUNK=0
 
@@ -33,7 +30,7 @@ TRUNK=0
 case "$PLATFORM" in
     ubuntu804)
         if grep -iq 'Ubuntu 8.04' /etc/issue; then
-            echo "System seems to qualify as '$PLATFORM'"
+            echo "System seems to qualify as $PLATFORM"
         else
             echo "/etc/issue did not identify this system as $PLATFORM"
             exit 1
@@ -41,22 +38,21 @@ case "$PLATFORM" in
         ;;
     rhel5)
         if grep -iq 'Red Hat Enterprise Linux Server release 5' /etc/issue; then
-            echo "System seems to qualify as '$PLATFORM'"
+            echo "System seems to qualify as $PLATFORM"
         else
             echo "/etc/issue did not identify this system as $PLATFORM"
             exit 1
         fi
         ;;
     *)
-        echo "Unknown platform: $PLATFORM"
+        echo "Unknown platform '$PLATFORM'"
         exit 1
         ;;
 esac
 
-
 if [ "$VER" = "trunk" ]; then	# trunk
    URL="https://cerebrum.svn.sourceforge.net/svnroot/cerebrum/trunk/cerebrum"
-   echo "Setting version to 0 for trunk."
+   echo "Setting version to 0 for trunk"
    VER=0
 else	# branch or tag
    IFS=. read major medium minor <<< "$VER"
@@ -70,38 +66,36 @@ else	# branch or tag
    fi
 fi
 
-echo "Building $PLATFORM packages for $TARGET: $VER"
 TEMPDIR="`mktemp -d /tmp/cerebuild.XXXXXXXXX`"
-echo "Created temp-dir: $TEMPDIR"
+echo "Building $PLATFORM packages for $TARGET, version $VER, using temp-dir $TEMPDIR"
 pushd $TEMPDIR >/dev/null 2>&1 || exit
 if [ "$REV" = "latest" ]; then
-    echo "Downloading source for: $VER, latest revision from $URL"
+    echo "Downloading source for $VER, latest revision from $URL"
     svn co -q $URL cerebrum
 
     REV=`svn log cerebrum --limit 1 --quiet | awk '/^r[0-9]+/ {print substr($1,2); exit}'`
-    echo "Detected the following revision as latest for $VER: '$REV'"
+    echo "Detected revision '$REV' as latest for $VER"
     if [ -d cerebrum/cerebrum ]; then
         mv cerebrum/cerebrum cerebrum-ntnu-$VER-$REV
     else
         mv cerebrum cerebrum-ntnu-$VER-$REV
     fi
 else
-    echo "Downloadig source for $VER-$REV from $URL"
+    echo "Downloading source for $VER-$REV from $URL"
     svn co -q -r $REV $URL cerebrum-ntnu-$VER-$REV
 fi
 popd >/dev/null 2>&1
 echo "Source downloaded into $TEMPDIR/cerebrum-ntnu-$VER-$REV"
 
 # Verify existance of platform-specific build infrastructure:
-PKGSCRIPT="$TEMPDIR/cerebrum-ntnu-$VER-$REV/contrib/no/ntnu/pkgbuild/$TARGET/$PLATFORM/pkgbuild.sh"
+PKGSCRIPT="`dirname $0`/$TARGET/$PLATFORM/pkgbuild.sh"
+[ -f "$PKGSCRIPT" ] || PKGSCRIPT="$TEMPDIR/cerebrum-ntnu-$VER-$REV/contrib/no/ntnu/pkgbuild/$TARGET/$PLATFORM/pkgbuild.sh"
+
 COMMAND="$PKGSCRIPT $TEMPDIR $VER $REV"
-if [ ! -f "$PKGSCRIPT" ]; then
-    echo "Could not find build script for next step:"
-    echo "$PKGSCRIPT"
-    echo "Wanted to run command:"
+if [ ! -x "$PKGSCRIPT" ]; then
+    echo "Could not find build script for next step, wanted to execute:"
     echo $COMMAND
 else
-    echo "Executing platform and target specific buid of $TARGET for $PLATFORM"
+    echo "Executing platform and target specific build of $TARGET for $PLATFORM"
     exec $COMMAND
 fi
-
--- contrib/no/ntnu/pkgbuild/ceresync/rhel5/pkgbuild.sh
+++ contrib/no/ntnu/pkgbuild/ceresync/rhel5/pkgbuild.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-PKGPATH="contrib/no/ntnu/pkgbuild/ceresync/rhel5"
-SPECPATH="$PKGPATH/specs/"
-SRCPATH="$PKGPATH/src/"
 BUILDPATH="/usr/src/redhat/"
 SPECDEST="$BUILDPATH/SPECS"
 SRCDEST="$BUILDPATH/SOURCES"
@@ -13,6 +10,10 @@ fi
 DIR=$1
 VER=$2
 REV=$3
+PKGPATH="`dirname $0`"
+[ -d "$PKGPATH/specs" -a -d "$PKGPATH/src" ] || PKGPATH="cerebrum-ntnu-$VER-$REV/contrib/no/ntnu/pkgbuild/ceresync/rhel5"
+SPECPATH="$PKGPATH/specs/"
+SRCPATH="$PKGPATH/src/"
 	
 echo "Building RPM packages for RHEL 5 of ceresync version $VER-$REL"
 pushd $DIR > /dev/null 2>&1
@@ -21,14 +22,13 @@ echo "Creating tarball cerebrum-ntnu-$VER-$REV.tar.gz"
 tar --exclude=.svn -czf cerebrum-ntnu-$VER-$REV.tar.gz cerebrum-ntnu-$VER-$REV
 
 echo "Modifying SPEC to use correct version and release"
-sed "s|VERSION|$VER|g" < cerebrum-ntnu-$VER-$REV/$SPECPATH/ceresync-common.spec |\
-sed "s|RELEASE|$REV|g" > $SPECDEST/ceresync-common.spec
+sed "s|VERSION|$VER|g;s|RELEASE|$REV|g" < $SPECPATH/ceresync-common.spec > $SPECDEST/ceresync-common.spec
 
 echo "Copying sources and patches to correct location"
 cp cerebrum-ntnu-$VER-$REV/$SRCPATH/* $SRCDEST/
 cp cerebrum-ntnu-$VER-$REV.tar.gz $SRCDEST/
 
-echo "Building packages"
+echo "Building packages in $BUILDPATH"
 rpmbuild -ba $SPECDEST/ceresync-common.spec
 popd >/dev/null 2>&1
 
