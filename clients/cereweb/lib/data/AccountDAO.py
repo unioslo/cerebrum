4a806db4fd690cde7f3908e5f75905da8267784d
Alf B. Lervåg <alfborge@unknown>
2010-03-24 14:30:52 +0000

    Delete affiliations when trying to delete an account.

--- clients/cereweb/lib/data/AccountDAO.py
+++ clients/cereweb/lib/data/AccountDAO.py
@@ -170,6 +170,9 @@ class AccountDAO(EntityDAO):
         if self.is_posix(account_id):
             self.demote_posix(account_id)
 
+        for aff in account.get_account_types():
+            account.del_account_type(aff['ou_id'], aff['affiliation'])
+
         group_dao = GroupDAO(self.db)
         for group in group_dao.get_groups_for(account_id):
             group_dao.remove_member(group.id, account_id)
