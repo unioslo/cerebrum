49d647107c2b7ff4fdab7db503501374376a245c
Roger Hansen <rogerha@usit.uio.no>
2010-07-21 08:19:38 +0000

    Added the reversed lookup for password change in ChangeLog, such that if password sync fails try again next run. Also only last password change will be synced to ad.
    Also changed indentation level from 8 to 4. and removed all those
    empty lines.
    
    Added usage function, since it was referenced in main()

--- contrib/no/giske/ADquickSync.py
+++ contrib/no/giske/ADquickSync.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 #
-# Copyright 2003 University of Oslo, Norway
+# Copyright 2003-2010 University of Oslo, Norway
 #
 # This file is part of Cerebrum.
 #
@@ -22,12 +22,8 @@
 
 import getopt, sys, pickle
 import cerebrum_path
-import cereconf
 
-from Cerebrum import Errors
 from Cerebrum.Utils import Factory
-from Cerebrum import Entity
-from Cerebrum.modules import MountHost
 from Cerebrum.modules import ADutilMixIn
 from Cerebrum.modules import CLHandler
 
@@ -38,84 +34,92 @@ logger = Factory.get_logger("cronjob")
 
 class ADquiSync(ADutilMixIn.ADuserUtil):
 
-	def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
+        super(ADquiSync, self).__init__(*args, **kwargs)
+        self.cl = CLHandler.CLHandler(db)
 
-		super(ADquiSync, self).__init__(*args, **kwargs)
-		self.cl = CLHandler.CLHandler(db)
+
+    def quick_sync(self, spread, dry_run):
+        # We reverse the set of events, so that if the same account
+        # has multiple password changes, only the last will be updated.
+        # If we didn't reverse, and if the first password update fails,
+        # then this would be retried in the next run, and the next password
+        # update(s) will not be rerun afterwards, leaving the user with
+        # an older password than the last.
+        handled = set()
+        answer = reversed(self.cl.get_events('ad', (self.co.account_password,)))
+        for ans in answer:
+            confirm = True
+            if (ans['change_type_id'] == self.co.account_password and
+                not ans['subject_entity'] in handled):
+                handled.add(ans['subject_entity'])
+                pw = pickle.loads(ans['change_params'])['password']
+                confirm = self.change_pw(ans['subject_entity'],spread, pw, dry_run)
+            else:
+                self.logger.debug("unknown change_type_id %i or user already updated",
+                                  ans['change_type_id'])
+            if confirm:
+                self.cl.confirm_event(ans)
+
+	self.cl.commit_confirmations()
+	self.logger.info("Commited all changes, updated c_l_handler.")
 
 
-	def quick_sync(self, spread, dry_run):
-
-		answer = self.cl.get_events('ad', (self.co.account_password,))
-
-		for ans in answer:
-			if ans['change_type_id'] == self.co.account_password:
-
-			   	pw = pickle.loads(ans['change_params'])['password']
-				self.change_pw(ans['subject_entity'],spread, pw, dry_run)
-
-			else:
-				self.logger.debug("unknown change_type_id %i" % ans['change_type_id'])
-
-		    #We always confirm event. Assume fullsync clean missed events.
-			self.cl.confirm_event(ans)
+    def change_pw(self, account_id, spread, pw, dry_run):
+        self.ac.clear()
+        self.ac.find(account_id)
         
-		self.cl.commit_confirmations()    
+        if self.ac.has_spread(spread):
+            dn = self.server.findObject(self.ac.account_name)
+            ret = self.run_cmd('bindObject', dry_run, dn)
+            pwUnicode = unicode(pw, 'iso-8859-1')
+            ret = self.run_cmd('setPassword', dry_run, pwUnicode)
+            if ret[0]:
+                self.logger.debug('Changed password: %s' % self.ac.account_name)
+                return True
+        else:
+            #Account without ADspread, do nothing and return.
+            return True
+
+        #Something went wrong.
+        self.logger.warn('Failed change password: %s' % self.ac.account_name)
+        return False
 
 
-	def change_pw(self, account_id, spread, pw, dry_run):
-
-		self.ac.clear()
-		self.ac.find(account_id)
-		
-		if self.ac.has_spread(spread):
-
-			dn = self.server.findObject(self.ac.account_name)
-			ret = self.run_cmd('bindObject', dry_run, dn)
-
-			pwUnicode = unicode(pw, 'iso-8859-1')
-			ret = self.run_cmd('setPassword', dry_run, pwUnicode)
-			if ret[0]:
-				self.logger.debug('Changed password: %s' % self.ac.account_name)
-				return True
-		else:
-			#Account without ADspread, do nothing and return.
-			return True
-
-		#Something went wrong.
-		self.logger.warn('Failed change password: %s' % self.ac.account_name)
-		return False
-
-            
+def usage():
+    print """Usage: ADquickSync.py --user_spread spread_ad_account
+                    --user_spread: update onlys for users with spread to AD
+                    --dry_run: don't do any changes in AD
+                    --help: print this text"""
+    
 
 def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],
+                       '', ['user_spread=',
+                        'help',
+                        'dry_run'])
+    except getopt.GetoptError:
+        # usage finnes ikke
+        usage(1)
 
-	try:
-		opts, args = getopt.getopt(sys.argv[1:],
-					   '', ['user_spread=',
-						'help',
-						'dry_run'])
+    delete_objects = False
+    dry_run = False
+    user_spread = None
+    for opt, val in opts:
+        if opt == '--user_spread':
+            user_spread = getattr(co, val)  # TODO: Need support in Util.py
+        elif opt == '--help':
+            usage(1)
+        elif opt == '--dry_run':
+            dry_run = True
 
-	except getopt.GetoptError:
-		usage(1)
+    if not user_spread:
+        user_spread = co.spread_ad_acc
 
-	delete_objects = False
-	dry_run = False
-	user_spread = None
-	
-	for opt, val in opts:
-		if opt == '--user_spread':
-			user_spread = getattr(co, val)  # TODO: Need support in Util.py
-		elif opt == '--help':
-			usage(1)
-		elif opt == '--dry_run':
-			dry_run = True
+    ADquickUser = ADquiSync(db, co, logger) 
+    ADquickUser.quick_sync(user_spread, dry_run)
 
-	if not user_spread:
-		user_spread = co.spread_ad_acc
-
-	ADquickUser = ADquiSync(db, co, logger)	
-	ADquickUser.quick_sync(user_spread, dry_run)
 
 if __name__ == '__main__':
     main()
