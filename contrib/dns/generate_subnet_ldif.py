b8c33bf316b6045ad5d6e141a3983606bbc4b687
Hallvard Breien Furuseth <hallvard@usit.uio.no>
2010-09-15 15:28:49 +0000

    Get subnets from Cerebrum, not cereconf_dns

--- contrib/dns/generate_subnet_ldif.py
+++ contrib/dns/generate_subnet_ldif.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 
-# Copyright 2008 University of Oslo, Norway
+# Copyright 2010 University of Oslo, Norway
 #
 # This file is part of Cerebrum.
 #
@@ -21,53 +21,38 @@
 
 """Usage: generate_subnet_ldif.py [--logger-name=console ...]
 
-Write IP subnet information from /cerebrum/etc/cerebrum/cereconf_dns.py
+Write IP subnet information from Cerebrum
 to an LDIF file, which can then be loaded into LDAP.
 See Cerebrum/default_config.py:LDAP_SUBNETS for configuration.
 """
 
 import cerebrum_path
-import cereconf_dns
-from Cerebrum.modules.dns.IPUtils import IPCalc
+import cereconf
+from Cerebrum.Utils import Factory
 from Cerebrum.modules.LDIFutils import ldapconf, iso2utf, \
      ldif_outfile, end_ldif_outfile, entry_string, container_entry_string
+from Cerebrum.modules.dns.Subnet  import Subnet
+from Cerebrum.modules.dns.IPUtils import IPCalc
+
 netmask_to_ip = IPCalc().netmask_to_ip
 
-# Read the cereconf_dns.py file, looking for Python comments to include
-# in LDAP subnet descriptions.  Yuck.
-# TODO: Change Cerebrum.modules.dns.SubNetDef so we can get rid of this.
-def read_subnet_notes():
-    import re
-    input_file = re.sub(r'\.py\w$', '.py', cereconf_dns.__file__)
-    subnet_re = re.compile(
-        r"^\s*SubNetDef\s*\(\s*'([\d.]+)'[\s,]*(\d+)\s*\)[,\s\)\]]*(?:#(.*))?")
-    subst_longspace = re.compile(r"\t\s*| \s+").sub
-    notes = {}
-    for subnet in filter(None, map(subnet_re.match, file(input_file))):
-        network, mask, note = subnet.groups()
-        cn = "%s/%s" % (network, mask)
-        assert cn not in notes, "Duplicate: " + cn
-        notes[cn] = note and subst_longspace("  ", note.strip())
-    return notes
-
 def write_subnet_ldif():
-    notes = read_subnet_notes()
     DN = ldapconf('SUBNETS', 'dn')
     startAttr, endAttr, objectClasses = ldapconf('SUBNETS', 'rangeSchema')
     objectClasses = ('top', 'ipNetwork') + tuple(objectClasses)
-    f = ldif_outfile('SUBNETS')
+    db = Factory.get('Database')()
+    f  = ldif_outfile('SUBNETS')
     f.write(container_entry_string('SUBNETS'))
-    for subnet in cereconf_dns.all_nets:
-        cn = "%s/%d" % (subnet.net, subnet.mask)
-        note = notes.pop(cn, None)
+    for row in Subnet(db).search():
+        cn   = "%s/%s" % (row['subnet_ip'], row['subnet_mask'])
+        desc = row['description']
         f.write(entry_string("cn=%s,%s" % (cn, DN), {
             'objectClass':     objectClasses,
-            'description':     (note and (iso2utf(note),) or ()),
-            'ipNetworkNumber': (subnet.net,),
-            'ipNetmaskNumber': (netmask_to_ip(subnet.mask),),
-            startAttr:         (str(subnet.start),),
-            endAttr:           (str(subnet.stop ),)}))
-    assert not notes, "Suspicious parse for: " + ", ".join(notes.keys())
+            'description':     (desc and (iso2utf(desc),) or ()),
+            'ipNetworkNumber': (row['subnet_ip'],),
+            'ipNetmaskNumber': (netmask_to_ip(row['subnet_mask']),),
+            startAttr:         (str(int(row['ip_min'])),),
+            endAttr:           (str(int(row['ip_max'])),)}))
     end_ldif_outfile('SUBNETS', f)
 
 if __name__ == '__main__':
