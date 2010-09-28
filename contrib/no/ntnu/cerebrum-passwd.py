c735168f3e209e9448244ca72f110b639d4394fe
Steinar Hamre <stseiham@extern.uio.no>
2010-09-28 11:32:47 +0000

    Easy copying normal passwd to admin passwd. Functionality to check passwd for debugging.

--- contrib/no/ntnu/cerebrum-passwd.py
+++ contrib/no/ntnu/cerebrum-passwd.py
@@ -29,26 +29,63 @@ db=Factory.get("Database")()
 db.cl_init(change_program="set_password")
 ac=Factory.get("Account")(db)
 
-opts,args = getopt.getopt(sys.argv[1:], 'a')
 
-set_admin_password = False
+def set_passwd_interactive(user, admin_password):
+    ac.find_by_name(user)
+
+    pass1 = getpass("Password:")
+    pass2 = getpass("Repeat password:")
+
+    assert pass1 == pass2
+
+    if admin_password:
+        ac.set_admin_password(pass1)
+    else:
+        ac.set_password(pass1)
+
+    ac.write_db()
+
+def copy_to_admin_passwd(user):
+    ac.find_by_name(user)
+    
+    pw = ac.get_account_authentication(ac.const.auth_type_md5_crypt)
+    ac.affect_auth_types(ac.const.auth_type_admin_md5_crypt)
+    ac.populate_authentication_type(ac.const.auth_type_admin_md5_crypt, pw)
+    ac.write_db()
+
+def check_passwd(user, admin_password):
+    ac.find_by_name(user)
+    
+    if admin_password:
+        verify_auth = ac.verify_admin_auth
+    else:
+        verify_auth = ac.verify_auth
+
+    pass1 = getpass("Password:")
+    
+    if verify_auth(pass1):
+        print "OK"
+    else:
+        print "FAILED"
+
+opts,args = getopt.getopt(sys.argv[1:], 'act')
+
+copy_passwd = False
+test_passwd = False
+admin_password = False
 for opt, val in opts:
     if opt=='-a':
-        set_admin_password=True
-
-user = args[0]
-
-ac.find_by_name(user)
-
-pass1 = getpass("Password:")
-pass2 = getpass("Repeat password:")
-
-assert pass1 == pass2
-
-if set_admin_password:
-    ac.set_admin_password(pass1)
+        admin_password = True
+    if opt=='-c':
+        copy_passwd = True
+    if opt=='-t':
+        test_passwd = True
+        
+if copy_passwd:
+    copy_to_admin_passwd(args[0])
+elif test_passwd:
+    check_passwd(args[0], admin_password)
 else:
-    ac.set_password(pass1)
+    set_passwd_interactive(args[0], admin_password)
 
-ac.write_db()
 db.commit()
