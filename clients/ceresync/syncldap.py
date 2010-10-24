8450b5455701d236ffc1e774929680e86a440500
Geir <geirha@ntnu.no>
2010-10-24 13:14:56 +0000

    Make sure accountspread is used for the Person backend

--- clients/ceresync/syncldap.py
+++ clients/ceresync/syncldap.py
@@ -199,7 +199,7 @@ def get_entities(s, system, sync_options):
 
     my_options = sync_options.copy()
 
-    if backend_class in ("OracleCalendar","FeidePerson"):
+    if backend_class in ("OracleCalendar","FeidePerson", "Person"):
         my_options['accountspread'] = get_conf(system, "spread")
 
     if entity == 'account':
