d1f4a527d5212d7b75d466f8d6c5fac49ac90562
Geir <geirha@ntnu.no>
2010-10-25 12:46:10 +0000

    Limit _get_all_account_affiliations to only return affiliations on the account

--- Cerebrum/modules/no/ntnu/Builder.py
+++ Cerebrum/modules/no/ntnu/Builder.py
@@ -315,7 +315,8 @@ class Builder(object):
         for aff in self.account.get_account_types(
               all_persons_types=True,
               owner_id=owner_id):
-            account_affs.add((aff['affiliation'], aff['ou_id']))
+            if aff['account_id'] == self.account.entity_id:
+                account_affs.add((aff['affiliation'], aff['ou_id']))
         return account_affs
 
     def _remove_account_affiliations(self, affs):
