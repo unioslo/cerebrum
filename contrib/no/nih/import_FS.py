87b367bb7b1aea4ee15427a8a259322c51378b3d
Jasmina Hodzic <jasminah@usit.uio.no>
2011-05-26 09:48:12 +0000

    initially import hjemstedsadresse only

--- contrib/no/nih/import_FS.py
+++ contrib/no/nih/import_FS.py
@@ -146,7 +146,8 @@ def _calc_address(person_info):
     # FS.DELTAKER   *_hjem (5) 
     rules = [
         ('fagperson', ('_arbeide', '_hjemsted', '_besok_adr')),
-        ('aktiv', ('_semadr', '_hjemsted', None)),
+        # ('aktiv', ('_semadr', '_hjemsted', None)),
+        ('aktiv', ('_hjemsted', None)),        
         ('evu', ('_job', '_hjem', None)),
         ]
     adr_map = {
