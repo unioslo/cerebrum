63a733b070baf24daa6fd98d3ddf28bcd182b940
Jasmina Hodzic <jasminah@usit.uio.no>
2011-05-27 17:29:46 +0000

    cut-and-paste blues

--- contrib/migrate/import_email_addresses.py
+++ contrib/migrate/import_email_addresses.py
@@ -188,7 +188,7 @@ def process_mail(account, mtype, addr, spread=None, homemdb=None):
                 logger.error("EmailTarget mismatch: ea: %d, et: %d", 
                              ea.email_addr_target_id, et.entity_id)
         if homemdb:
-            account.populate_trait(const.trait_exchange_mdb, strval=homemdb)
+            account.populate_trait(constants.trait_exchange_mdb, strval=homemdb)
             account.write_db()
 
     
