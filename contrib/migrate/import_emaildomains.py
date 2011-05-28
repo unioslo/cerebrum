f929f172c65c44a7d887d664d25f9c045283d75f
Jasmina Hodzic <jasminah@usit.uio.no>
2011-05-28 12:37:42 +0000

    connect multiple ous and domains via -c

--- contrib/migrate/import_emaildomains.py
+++ contrib/migrate/import_emaildomains.py
@@ -96,9 +96,17 @@ def main():
                 logger.error('Only supported with the -c option.')
                 sys.exit(1)
             aff = val
+    
     if not reg_dom and infile == None:
         usage()
 
+    if connect:
+        try:
+            tmp = connect.split(',')
+            connect = tmp
+        except:
+            [connect]
+        
     if reg_dom:
         if infile:
             logger.error('Cannot use both -d and -f options.')
@@ -114,7 +122,6 @@ def main():
 
         for i in all_ous:
             process_domain(dom_name, i['ou_id'])
-            
     db.commit()
     
 def process_line(infile):
@@ -156,12 +163,14 @@ def process_domain(dom, conn, cat, aff):
         edom.populate(dom, 'E-mail domain')
         edom.write_db()
     if conn:
-        try:
-            ou.find(int(conn))
-        except Errors.NotFoundError:
-            logger.error('No such OU %s!', conn)
-            sys.exit(1)
-        connect_domain_ou(ou.entity_id, edom.entity_id, c_aff)
+        for o in conn:
+            try:
+                ou.clear()
+                ou.find(int(o))
+            except Errors.NotFoundError:
+                logger.error('No such OU %s!', o)
+                sys.exit(1)
+            connect_domain_ou(ou.entity_id, edom.entity_id, c_aff)
     if c_cat:
         # Hack to get a hit when doing 'in'
         if (int(c_cat),) in edom.get_categories():
