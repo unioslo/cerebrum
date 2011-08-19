03778df0d2b7b376ac54cd15929eca9ac3325310
Joakim Hovlandsvåg <joakimsh@ulrik.uio.no>
2011-08-19 10:24:48 +0000

    Created a new bug in last bugfix. An entity should not be converted to unicode...

--- Cerebrum/modules/cis/Individuation.py
+++ Cerebrum/modules/cis/Individuation.py
@@ -341,8 +341,6 @@ class Individuation:
         pc = PasswordChecker.PasswordChecker(db)
         if password:
             password = unicode(password).encode('utf8')
-        if account:
-            account = unicode(account).encode('utf8')
         try:
             pc.goodenough(account, password, uname="foobar")
         except PasswordChecker.PasswordGoodEnoughException, m:
