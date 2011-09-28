4f82339578b100ef00f625f320ad7e61da31fd23
Joakim Hovlandsvåg <joakimsh@ulrik.uio.no>
2011-09-28 11:31:57 +0000

    Added initial script for updating persons' publication reservations through traits instead of having large, equal chunks of code in the different exports and the new forgotten password service.
    The script is not done yet - it requires much more testing.
    
    It needs to be in production before the forgotten password service is, since the
    reserved people should not have their usernames publicised through the service.

---
+++ contrib/no/uio/update_publication_reservations.py
@@ -0,0 +1,153 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2011 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""
+A script for updating the reservations from being made public on www.uio.no.
+A person should be considered as reserved when:
+
+    - The person does not have any co.trait_public_reservation
+    - The person has co.trait_public_reservation and numval=1
+
+A person should not be considered as reserved when:
+
+    - The person has co.trait_public_reservation and numval=0
+
+This is to prevent publishing reserved people if the script fails to run for
+some time. TBD: someone should decide if this is the right behaviour.
+
+For now, person traits are used for tagging reserved persons. In the future,
+there should probably exist some reservation table for different kinds of
+reservations.
+"""
+import sys, getopt
+from mx.DateTime import now
+
+import cerebrum_path, cereconf
+from Cerebrum.Utils import Factory
+
+db = Factory.get('Database')()
+db.cl_init(change_program="update_publication_reservations")
+pe = Factory.get('Person')(db)
+ac = Factory.get('Account')(db)
+gr = Factory.get('Group')(db)
+co = Factory.get('Constants')(db)
+logger = Factory.get_logger('cronjob')
+count_resrv_true = count_resrv_false = 0
+
+def usage(exitcode=0):
+    print "Usage: %s [--commit]" % sys.argv[0]
+    print
+    print __doc__
+    sys.exit(exitcode)
+
+def process(with_commit=False):
+    logger.info("Starting update_publication_reservations")
+    logger.info("Harvesting data")
+
+    employees  = get_employees()
+    logger.debug('%d employees found', len(employees))
+    students   = get_students()
+    logger.debug('%d students found', len(students))
+
+    sapmembers = get_members('SAP-elektroniske-reservasjoner')
+    logger.debug('%d members from SAP reservations', len(sapmembers))
+    fsmembers  = get_members('FS-aktivt-samtykke')
+    logger.debug('%d members from FS consent', len(fsmembers))
+
+    # TODO: cache old trait values? Could be quicker, as we then only commit
+    # changes, not everything.
+
+    reservations = set(row['entity_id'] for row in pe.list_traits(
+                                  code = co.trait_public_reservation, numval=1))
+    logger.debug('%d reservations found in db', len(reservations))
+
+    logger.info("Iterate over all persons")
+    for row in pe.search():
+        # note that people can be returned several times by search()
+        person_id = row['person_id']
+        if person_id in employees:
+            set_reservation(person_id, person_id in sapmembers)
+        elif person_id in students:
+            set_reservation(person_id, person_id not in fsmembers)
+        else:
+            set_reservation(person_id, True)
+    logger.info("%d reserved", count_resrv_true)
+    logger.info("%d not reserved", count_resrv_false)
+    if with_commit:
+        db.commit()
+        logger.debug('Changes commited')
+    else:
+        db.rollback()
+        logger.debug('Rolled back changes')
+    logger.info("update_publication_reservations done")
+
+def set_reservation(person_id, value=True):
+    """Set a reservation on or off for a given person."""
+    logger.debug2("Setting reservation to %s for person_id=%s" % (bool(value),
+                                                                  person_id))
+    pe.clear()
+    pe.find(person_id)
+    pe.populate_trait(code=co.trait_public_reservation, date=now(),
+                      numval=int(bool(value)))
+    pe.write_db()
+    global count_resrv_true, count_resrv_false
+    if value:
+        count_resrv_true += 1
+    else:
+        count_resrv_false += 1
+    # TODO: commit here?
+
+def get_employees():
+    """Returns a set with person_id for all that are considered employees."""
+    return set(row['person_id'] for row in
+            pe.list_affiliations(affiliation=(co.affiliation_ansatt,
+                                              co.affiliation_tilknyttet)) 
+            if row['status'] != co.affiliation_tilknyttet_fagperson)
+
+def get_students():
+    """Returns a set with person_id for all that are considered students."""
+    return set(row['person_id'] for row in
+            pe.list_affiliations(status=(co.affiliation_status_student_aktiv,
+                                         co.affiliation_status_student_drgrad)))
+
+def get_members(groupname):
+    """Returns a set with person_id of all person members of a given group."""
+    rows = gr.search(name=groupname)
+    group_id = rows[0]['group_id']
+    return set(row['member_id'] for row in gr.search_members(group_id=group_id,
+                                                member_type=co.entity_person))
+
+if __name__ == '__main__':
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'h',
+                                   ['help', 'commit'])
+    except getopt.GetoptError, e:
+        print e
+        usage(1)
+
+    with_commit = False
+
+    for opt, val in opts:
+        if opt in ('-h', '--help'):
+            usage()
+        elif opt in ('--commit',):
+            with_commit = True
+    process(with_commit = with_commit)
