3b269cc41f8df543d7d6572b99d55c4f9516b9f0
Igor Erokhin <igorer@ulrik.uio.no>
2011-11-01 12:23:24 +0000

    Changed behavior: show quarantines older then given date or an age in months, filter out deleted and non-personal accounts, make a category for unregistered persons, i.e. without affiliation.

--- contrib/statistics/generate_quarantine_report.py
+++ contrib/statistics/generate_quarantine_report.py
@@ -20,65 +20,57 @@
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 """
-Report accounts in quarantine.
-Idea:
-   Events like quarantine_add are logged with a timestamp.
-   Retrieve the events and get account ids,
-   Skip non-personal accounts and accounts with no active quarantines.
-   Get desired information from Account and Person objects.
-   Output:
-   Navn	| Tilknytning | Fødselsdato | Brukernavn | Karantene starter | Karantenens type
-   Sort rows by faculties.
+Report accounts in quarantine. Show accounts that belong to employees.
+Output:
+   Navn	| Tilknytning | Brukernavn | Karantene starter | Karantenens type
+Sort rows by faculties.
 """
 
-import sys, getopt
+import sys
+import getopt
+import time
 
 from mx.DateTime import now, ISO, RelativeDateTime
 
 import cerebrum_path, cereconf
 from Cerebrum.Utils import Factory
 from Cerebrum.Errors import NotFoundError
+from Cerebrum.modules.no.uio import AutoStud
 
 def usage(exitcode=0):
     print """Usage:
-    generate_quarantine_report.py [--from YYYY-MM-DD]\
-    [--to YYYY-MM-DD] [--source_systems SAP,FS] --output FILE
+    generate_quarantine_report.py [Options]
 
-    Generate an html formatted report of accounts with active quaranitnes
-    for the given period.
+    Generate an html formatted report of accounts with active quaranitnes.
 
-    --from            Date to start searching for new quarantines from. Defaults
-                      to 7 days ago.
-    
-    --to              Date to start searching for new quarantines until. Defaults
-                      to now.
-
-    --source_systems  Comma separated list of source systems to search
-                      through. Defaults to 'SAP,FS'.
-
-    --output          The file to print the report to. Defaults to stdout.
+    Options:
+    -s, --start_date      Show quarantines that startet before this ISO date.
+                          Defaults to now.
+    -a, --age <int>       Show quaranintines older than this age.      
+    -o, --output <file>   The file to print the report to. Defaults to stdout.
+        --source_systems  Not implemented. Source systems used by default
+                          are SAP and FS. 
     """ 
     sys.exit(exitcode)
 
-def generate_report(output, source_systems, start_date, end_date):
-    """Generate html formatted report of accounts in quarantine between start_date
-    and end_date, sort by respective persons affiliations.
+
+def generate_report(output, source_systems, start_date):
+    """Generate html formatted report of accounts in quarantine.
+    sort by person affiliations.
     @param output: filename to write output to
     @type output: file object, returned by open or sys.stdout
     @param source_systems: not used for now,
     @type source_systems: string with comma separated systems.
     @param start_date: 
     @type start_date: ISO date, i.e YYYY-MM-DD
-    @param end_date:
-    @type end_date: ISO date, YYY-MM-DD
     """
-    
+    t0 = time.clock()
     logger = Factory.get_logger("cronjob")
-    db = Factory.get('Database')()
-    co = Factory.get('Constants')(db)
-    pe = Factory.get('Person')(db)
-    ac = Factory.get('Account')(db)
-    ou = Factory.get('OU')(db)
+    database = Factory.get('Database')()
+    constants = Factory.get('Constants')(database)
+    person = Factory.get('Person')(database)
+    account = Factory.get('Account')(database)
+    ou = Factory.get('OU')(database)
     
     logger.info('Start Quarantine Report.')
     
@@ -88,54 +80,108 @@ def generate_report(output, source_systems, start_date, end_date):
                                                     for row in ou.get_stedkoder())
 
     sko2name = dict((ou2sko[row['entity_id']], row['name']) for row in
-                    ou.search_name_with_language(name_variant = co.ou_name_display,
-                                                 name_language=co.language_nb))
+                    ou.search_name_with_language(name_variant=
+                                                        constants.ou_name_display,
+                                                        name_language=
+                                                        constants.language_nb))
+    perspective = constants.OUPerspective('FS')
+    autostud = AutoStud.AutoStud(database, logger, debug=0,
+    cfg_file='/cerebrum/uio/etc/cerebrum/studconfig.xml',
+    studieprogs_file='/cerebrum/uio/dumps/FS/studieprogrammer.xml',
+    emne_info_file='/cerebrum/uio/dumps/FS/emner.xml',
+    ou_perspective=int(perspective))
+
+    t1 = time.clock()
+
+    logger.debug('Initialization in %.2g seconds' %(t1-t0))
     
-    events = db.get_log_events_date(int(co.quarantine_add),
-                                    sdate=start_date,
-                                    edate=end_date)
+    quarantine_list = account.list_entity_quarantines(entity_types=constants.entity_account,
+                                                      only_active=True)
+    logger.debug('whole list length is %10d' %len(quarantine_list))
+    
     quarantines_by_sko = {}
     num_quarantines = 0
+    debug_counter = 0
 
-    for row_e in events:
-        ac.clear()
-        try: 
-            ac.find(row_e['subject_entity'])
+    #diagnose if-tests
+    time_if = 0
+    deleted_if = 0
+    owner_type_if = 0
+    disk_if = 0
+
+    t11 = time.clock()
+    logger.debug('time used: %.2f' %(t11-t1))
+    
+    for row_qua in quarantine_list:
+        
+        debug_counter += 1
+        if debug_counter % 10000 == 0:
+            logger.debug('row nr %10d , passed %.2f seconds'
+                    %(debug_counter,time.clock()-t11))
+        
+        if row_qua['start_date'] > start_date:
+            time_if += 1
+            continue
+
+        account.clear()
+        try:
+            account.find(row_qua['entity_id'])
         except NotFoundError, nfe:
             logger.error('%s' %nfe)
             continue
+
         
-        if ac.owner_type <> co.entity_person:
+        if account.owner_type <> constants.entity_person:
+            owner_type_if += 1
             continue # Filter out non-personal accounts.
 
-        account_name = ac.get_account_name()
-        name = ac.get_fullname()
-        
-        pe.clear()
-        pe.find(ac.owner_id)
-        aff = pe.get_affiliations()
-        ac_q = ac.get_entity_quarantine(only_active=True)
+        if account.is_deleted():
+            deleted_if += 1
+            continue # Skip deleted accounts.
 
-        if not ac_q:
-            continue # No active quarantines for that account, skip.
-
-        if pe.birth_date is None:
-            birth = 'ikke satt'
-        else:
-            birth = pe.birth_date.strftime('%Y-%m-%d')
+        try:
+            disk_id = account.get_home(int(constants.spread_uio_nis_user))['disk_id']
+        except NotFoundError:
+            # no homedir exists
+            continue
         
-        num_quarantines += 1
-        for row in aff:
+        if (disk_id and autostud.disk_tool.get_diskdef_by_diskid(disk_id)):
+            disk_if += 1
+            continue # disk_id refers to a student disk, skip.
+        
+        account_name = account.get_account_name()
+        name = account.get_fullname()
+        person.clear()
+        person.find(account.owner_id)
+        affiliations = person.get_affiliations()
+
+        for row in affiliations:
+            status = str(constants.PersonAffStatus(row['status']))
+
             quarantines_by_sko.setdefault(ou2sko[row['ou_id']], []).append({
                 'name': name,
-                'status': str(co.PersonAffStatus(row['status'])),
-                'birth': birth,
+                'status': status,
                 'account': account_name,
-                'quarantines':ac_q
+                'quarantine':row_qua
+                })
+        
+        if not affiliations:
+            quarantines_by_sko.setdefault('Uregistrert', []).append({
+                'name': name,
+                'status': 'ikke satt',
+                'account': account_name,
+                'quarantine':row_qua
                 })
 
-    logger.info('Found %d active quaranines created between %s and %s'
-            %(num_quarantines, start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d')))
+        num_quarantines += 1
+        
+        if num_quarantines % 1000 == 0:
+            logger.debug( 'Found %10d quarantines after %.2f seconds' %(
+                                    num_quarantines,(time.clock()-t11)))
+        
+    t2 = time.clock()
+    logger.info('Retrieved %10d quarantines to show in %.2g seconds.'
+            %(num_quarantines,t2-t11))
 
     #Write html.
     output.write("""<html>
@@ -164,7 +210,7 @@ def generate_report(output, source_systems, start_date, end_date):
                 }
                 table th, table td {
                     padding: .5em 1em;
-                    width: 16%;
+                    width: 20%;
                 }
                 .meta {
                     color: gray;
@@ -175,43 +221,51 @@ def generate_report(output, source_systems, start_date, end_date):
         <body>
     """)
     
-    output.write('<p class="meta">Karantener fra %s til %s</p>' \
-                 %(start_date.strftime('%Y-%m-%d'),
-                   end_date.strftime('%Y-%m-%d')))
+    output.write('<p class="meta">Karantener som startet før %s.</p>'  %(start_date.strftime('%Y-%m-%d')))
     
     output.write("\n<h2>Viser %s karantener</h2>\n" %num_quarantines)
     faculties = []
-    for sko in sorted(quarantines_by_sko):
-        fac = "%02s0000" % sko[:2]
-        if fac not in faculties:
-            output.write("\n<h1>%s</h1>\n" % sko2name[fac])
-            faculties.append(fac)
+    t3 = time.clock()
+    logger.debug('After %4.2f seconds, output loop starts' % (t3-t2))
 
-        output.write("\n<h2>%s - %s</h2>\n" % (sko, sko2name[sko]))
+    logger.debug('Skipped %10d quaranintes started after %s.' %
+                                                            (time_if,start_date))
+    logger.debug('Skipped %10d deleted accounts.' % deleted_if)
+    logger.debug('Skipped %10d non-personal accounts.' % owner_type_if)
+    logger.debug('Skipped %10d accounts on students disks.' % disk_if)
+
+    for sko in sorted(quarantines_by_sko):
+        if sko is 'Uregistrert':
+            faculty_name = sko
+            faculty = sko
+            unit_name = ''
+        else:
+            faculty = "%02s0000" % sko[:2]
+            faculty_name = sko2name[faculty]
+            unit_name = sko2name[sko]
+        if faculty not in faculties:
+            output.write("\n<h1>%s</h1>\n" % faculty_name)
+            faculties.append(faculty)
+
+        output.write("\n<h2>%s - %s</h2>\n" % (sko, unit_name))
         output.write("<table><thead><tr>")
         output.write("<th>Navn</th>")
         output.write("<th>Tilknytning</th>")
-        output.write("<th>Fødselsdato</th>")
         output.write("<th>Brukernavn</th>")
         output.write("<th>Karantene starter</th>")
         output.write("<th>Karantenens type</th>")
         output.write("</tr></thead>\n")
 
-        for qa in quarantines_by_sko[sko]:
+        for report_line in quarantines_by_sko[sko]:
             output.write("\n<tr>\n")
-            output.write("<td>%s</td>\n" % qa['name'])
-            output.write("<td>%s</td>\n" % qa['status'])
-            output.write("<td>%s</td>\n" % qa['birth'])
-            output.write("<td>%s</td>\n" % qa['account'])
-            for entry in qa['quarantines']:
-                output.write("<td>%s</td>\n" % entry['start_date']\
-                             .strftime('%Y-%m-%d kl %H:%M'))
-                output.write("<td>%s</td>\n" % co.Quarantine(entry['quarantine_type']))
-                
-                if entry <> qa['quarantines'][-1]:
-                    output.write("</tr>\n")
-                    output.write("<td></td><td></td><td></td><td></td>\n")
-
+            output.write("<td>%s</td>\n" % report_line['name'])
+            output.write("<td>%s</td>\n" % report_line['status'])
+            output.write("<td>%s</td>\n" % report_line['account'])
+            output.write("<td>%s</td>\n" % report_line['quarantine']['start_date']\
+                         .strftime('%Y-%m-%d kl %H:%M'))
+            output.write("<td>%s</td>\n"
+                         % constants.Quarantine(report_line['quarantine']
+                                                ['quarantine_type']))
             output.write("</tr>\n")
 
         output.write("</table>\n")
@@ -220,35 +274,42 @@ def generate_report(output, source_systems, start_date, end_date):
         <p class="meta">Generert: %s</p>\n</body>
         \n</html>\n""" % now().strftime('%Y-%m-%d kl %H:%M'))
 
-    logger.info('Done. Quarantine Report is generated.')
+    t4 = time.clock()
+    logger.info('Quarantine Report written to output in %4.2f seconds.' %(t4-t3))
+    logger.info('Total CPU time used: %4.2f seconds.' % (t4-t0))
 
 def main():
+    """ 
+    """
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'ho:',
-                ['from=', 'to=', 'source_systems=', 'output='])
+        opts, args = getopt.getopt(sys.argv[1:], 'hs:o:a:',
+                ['start_date=', 'source_systems=', 'output=', 'age='])
     except getopt.GetoptError, e:
         print e
         usage(1)
 
-    end_date = now()
-    start_date = now() + RelativeDateTime(days=-7)
+    start_date = now()
     source_systems = 'SAP,FS'
     output = sys.stdout
 
     for opt, val in opts:
         if opt in ('-h', '--help'):
             usage()
-        elif opt in ('--from', ):
+        elif opt in ('-s', '--start_date', ):
             start_date = ISO.ParseDate(val)
-        elif opt in ('--to', ):
-            end_date = ISO.ParseDate(val)
+        elif opt in ('-a', '--age'):
+            age = int(val)
+            if age < 0:
+                print 'Enter a positive age in months.'
+                usage(2)
+
+            start_date = now() + RelativeDateTime(months=-age)
         elif opt in ('--source_systems', ):
             source_systems = val
         elif opt in ('-o', '--output'):
             output = open(val, 'w')
-
     generate_report(output=output, source_systems=source_systems,
-            start_date=start_date, end_date=end_date)
+                                            start_date=start_date)
     if not output is sys.stdout:
         output.close()
 
