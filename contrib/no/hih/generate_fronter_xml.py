340aa53ca0e2057451c3a9d0ebcc69e506741bb9
Arne Vedø-Hansen <rafiki@usit.uio.no>
2011-11-11 06:05:35 +0000

    It's 'account_name', you doofus

--- contrib/no/hih/generate_fronter_xml.py
+++ contrib/no/hih/generate_fronter_xml.py
@@ -415,7 +415,7 @@ def list_users_for_fronter_export():
                     address = addr[0]
                     alines = address['address_text'].split("\n")+[""]
                 else:
-                    logger.warning("No addr for user '%s'" % account.account_id)
+                    logger.warning("No addr for user '%s'" % account.account_name)
                 if alines:
                     street = alines[0] + '\n' + alines[1]
                 if str(address['city']):
