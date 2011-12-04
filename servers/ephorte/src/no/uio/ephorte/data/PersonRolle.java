c626d5d3c086c09db674f6594f4dc691ed9b2d7f
Jasmina Hodzic <jasminah@usit.uio.no>
2011-12-04 17:38:01 +0000

    reverting again

--- servers/ephorte/src/no/uio/ephorte/data/PersonRolle.java
+++ servers/ephorte/src/no/uio/ephorte/data/PersonRolle.java
@@ -88,9 +88,6 @@ public class PersonRolle {
 	//	" " + arkivDel;
 	//}
 	// trying to make sure that the role title is re-written even
-	// if a title is previously registered, Jazz: 2011-12-03
-	String ouDescr = id2stedAkronym.get(adminDel);                                                            
-	tittel = id2rolleBeskrivelse.get(rolleId) + " " + ouDescr + " " + arkivDel;      
 	this.tittel = tittel;
         this.arkivDel = arkivDel;
         this.journalEnhet = journalEnhet;
