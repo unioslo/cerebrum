f320c1107a861026539206674cfa26f41eaff8d1
Joakim Hovlandsvåg <joakimsh@ulrik.uio.no>
2012-01-09 09:22:14 +0000

    cereconf wants OrgLDIF to use data from PBX, but roomnumbers are imported from SAP.

--- Cerebrum/modules/no/hia/OrgLDIF.py
+++ Cerebrum/modules/no/hia/OrgLDIF.py
@@ -52,7 +52,7 @@ class OrgLDIFHiAMixin(norEduLDIFMixin):
         syntax = self.attr2syntax[attr]
         c = self.get_contact_aliases(
             contact_type  = self.const.contact_office,
-            source_system = source,
+            source_system = self.const.system_sap,
             convert       = syntax[0],
             verify        = syntax[1],
             normalize     = syntax[2])
