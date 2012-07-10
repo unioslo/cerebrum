395b12db2578ad0bb4435f27a3c1d2896a46f6fa
Igor Erokhin <igorer@ulrik.uio.no>
2012-07-10 12:53:13 +0000

    Adjust messages to OFK, the base version concerned UIO

--- Cerebrum/modules/cis/OFKIndividuation.py
+++ Cerebrum/modules/cis/OFKIndividuation.py
@@ -40,20 +40,11 @@ class Individuation(Individuation.Individuation):
     messages = {
         'error_unknown':     {'en': u'An unknown error occured',
                               'no': u'En ukjent feil oppstod'},
-        'person_notfound':   {'en': u'Could not find a person by given data, please try again. Please note that you will ' +
-                                    u'not be able to use this service if you are reserved from being published on the\'s ' +
-                                    u'web pages.',
-                              'no': (u'Kunne ikke finne personen ut fra oppgitte data, vennligst prøv igjen. Merk at du ' +
-                                     u'ikke kan bruke denne tjenesten om du har reservert deg fra å bli publisert på ' +
-                                     u'nettsidene.')},
+        'person_notfound':   {'en': u'Could not find a person by given data, please try again.',
+                              'no': (u'Kunne ikke finne personen ut fra oppgitte data, vennligst prøv igjen.')},
         'person_notfound_usernames':
-            {'en': u'You are either reserved or have given wrong information.'
-                   u' If you are reserved, an SMS have been sent to you, as'
-                   u' long as your cell phone number is registered in our'
-                   u' systems.',
-             'no': u'Du er reservert eller har gitt feil info. Hvis du er'
-                   u' reservert skal du nå ha mottatt en SMS, såfremt ditt'
-                   u' mobilnummer er registrert i våre systemer.'},
+                             {'en': u'Could not find a person by given data, please try again.',
+                              'no': (u'Kunne ikke finne personen ut fra oppgitte data, vennligst prøv igjen.')},
         'person_miss_info':  {'en': u'Not all your information is available. Please contact your HR department or student office.',
                               'no': u'Ikke all din informasjon er tilgjengelig. Vennligst ta kontakt med din personalavdeling eller studentkontor.'},
         'account_blocked':   {'en': u'This account is inactive. Please contact your local IT.',
