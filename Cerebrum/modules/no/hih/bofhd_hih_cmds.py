67c5b38c104157778e7878905ca360c05c651464
Joakim Hovlandsvåg <joakimsh@ulrik.uio.no>
2012-09-24 12:46:03 +0000

    Updated HiH's bofhd to make use of the new superclass with common methods.

--- Cerebrum/modules/no/hih/bofhd_hih_cmds.py
+++ Cerebrum/modules/no/hih/bofhd_hih_cmds.py
@@ -35,7 +35,7 @@ from Cerebrum import Database
 from Cerebrum.modules.bofhd.cmd_param import *
 from Cerebrum.modules import Email
 from Cerebrum.modules.no.hih import bofhd_hih_help
-from Cerebrum.modules.bofhd.bofhd_core import BofhdCommandBase
+from Cerebrum.modules.bofhd.bofhd_core import BofhdCommonMethods
 from Cerebrum.Constants import _CerebrumCode, _SpreadCode
 from Cerebrum.modules.bofhd.auth import BofhdAuth
 from Cerebrum.modules.bofhd.utils import _AuthRoleOpCode
@@ -45,7 +45,7 @@ def format_day(field):
     fmt = "yyyy-MM-dd"                  # 10 characters wide
     return ":".join((field, "date", fmt))
 
-class BofhdExtension(BofhdCommandBase):
+class BofhdExtension(BofhdCommonMethods):
     OU_class = Utils.Factory.get('OU')
     Account_class = Factory.get('Account')
     Group_class = Factory.get('Group')
@@ -81,7 +81,7 @@ class BofhdExtension(BofhdCommandBase):
         # copy relevant group-cmds and util methods
         #
         'group_add', 'group_gadd', '_group_add', '_group_add_entity',
-        '_group_count_memberships', 'group_add_entity', 'group_create',
+        '_group_count_memberships', 'group_add_entity',
         'group_delete', 'group_remove', 'group_gremove', '_group_remove',
         '_group_remove_entity', 'group_remove_entity', 'group_info',
         'group_list', 'group_list_expanded', 'group_search', 'group_set_description',
@@ -170,6 +170,13 @@ class BofhdExtension(BofhdCommandBase):
                                                    size=500,
                                                    timeout=60*60)
 
+        # Copy in all defined commands from the superclass that is not defined
+        # in this class. TODO: This is not an optimal solution: If we are
+        # subclassing this class, we need to run another copy loop there too.
+        # How could we avoid this?
+        for key, cmd in super(BofhdExtension, self).all_commands.iteritems():
+            if not self.all_commands.has_key(key):
+                self.all_commands[key] = cmd
 
     def get_help_strings(self):
         return (bofhd_hih_help.group_help,
