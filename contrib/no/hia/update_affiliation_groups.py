8190ec4c2494218929f900f0ebed5178e2df5c6f
Christopher Culina <cculina@usit.uio.no>
2013-01-16 11:16:56 +0000

    Script for maintaining group memberships based on person affiliations.

---
+++ contrib/no/hia/update_affiliation_groups.py
@@ -0,0 +1,199 @@
+#! /usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+#
+# Copyright 2012 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+import sys
+import getopt
+
+import cerebrum_path
+import cereconf
+from Cerebrum.Utils import Factory
+from Cerebrum import Entity
+from Cerebrum import Errors
+from Cerebrum.modules.bofhd.auth import BofhdAuthOpSet, \
+     AuthConstants, BofhdAuthOpTarget, BofhdAuthRole
+
+# Initialize database connections
+db = Factory.get('Database')()
+ac = Factory.get('Account')(db)
+pe = Factory.get('Person')(db)
+gr = Factory.get('Group')(db)
+baos = BofhdAuthOpSet(db)
+bar = BofhdAuthRole(db)
+baot = BofhdAuthOpTarget(db)
+
+db.cl_init(change_program="update_affiliation_groups.py")
+
+# Initialize logger
+logger = Factory.get_logger('console')
+    
+def usage(exitcode=0):
+    print """Usage:
+    %s [Options]
+
+    Update group memberships for primary accounts based on affiliations.
+
+    Options:
+    -l, --list                      List affiliations.
+    -d, --dryrun                    Don't commit changes to Cerebrum db.
+    -a, --affmap    <aff:group>     Specify mappings between affs and groups.
+                                    "ANSATT:ansattgroup STUDENT:studentgroup"
+    """ % sys.argv[0] 
+    sys.exit(exitcode)
+
+def list_affs():
+    """
+    List person affiliations.
+    """
+    affiliations = pe.list_person_affiliation_codes()
+    for a in affiliations:
+        print str(int(a[0])) + ": " + a[1] + " (" + a[2] + ")"
+
+def update_aff_groups(aff_groups, dryrun):
+    # get all affiliations and aff ids
+    affs = {}
+    for a in pe.list_person_affiliation_codes():
+        affs[a[1]] = int(a[0])
+
+    # for each aff:group...
+    for affiliation,group in aff_groups.items():
+        
+        # valid affiliation?
+        if not affiliation in affs:
+            logger.error("Affiliation not found (" + affiliation + ").")
+            return
+
+        gr.clear()
+        try:
+            # valid group?
+            gr.find_by_name(group)
+        except Errors.NotFoundError:
+            logger.error("Group not found (" + group + ").")
+            return
+
+        # get all persons with this affiliation
+        pe.clear()
+        persons = pe.list_affiliations(affiliation=affs[affiliation])
+
+        # get each person's primary account
+        #####
+        # The current solution is classic brute force. An API change would
+        # be more elegant, e.g. expanding Person or Account (search or 
+        # list_affiliations). Account.list_accounts_by_type may serve as an
+        # example.
+        #####
+        primary_accounts = []
+        for person in persons:
+            ac.clear()
+            account = ac.list_accounts_by_type(person_id=person[0],
+                                               primary_only=True)
+            if account:
+                primary_accounts.append(int(account[0][3]))
+        
+        # get all entity_ids in target group
+        group_members = []
+        for member in list(gr.search_members(group_id=gr.entity_id)):
+            group_members.append(int(member[3]))
+
+        # accounts that should be added to the group
+        to_be_added = list(set(primary_accounts) - set(group_members))
+
+        # accounts that should be removed from the group
+        to_be_removed = list(set(group_members) - set(primary_accounts))
+
+        # remove accounts from group
+        for account in to_be_removed:
+            try:
+                gr.remove_member(account)
+                gr.write_db()
+                logger.debug("Removed " + str(account) + " from " + group + ".")
+            except Errors.DatabaseError, e:
+                logger.error("Failed removing " + str(account) + " from " +
+                             group + ": " + e)
+                db.rollback()
+        if not dryrun:
+            try:
+                gr.commit()
+            except Errors.DatabaseError, e:
+                logger.error("Commit of removed group members failed: " + e)
+                db.rollback()
+        else:
+            db.rollback()
+
+        # add accounts to group
+        for account in to_be_added:
+            try:
+                gr.add_member(account)
+                gr.write_db()
+                logger.debug("Added " + str(account) + " to " + group + ".")
+            except Errors.DatabaseError, e:
+                logger.error("Failed adding " + str(account) + " to " + group + ": "
+                             + e)
+                db.rollback()
+        if not dryrun:
+            try:
+                gr.commit()
+            except Errors.DatabaseError, e:
+                logger.error("Commit of added group members failed: " + e)
+                db.rollback()
+        else:
+            db.rollback()
+
+def main():
+
+    if len(sys.argv) < 2:
+        usage(0)
+
+    dryrun = False
+    aff_groups = None
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'lda:', ['list', 'dryrun', 'affs='])
+    except getopt.GetoptError, e:
+        logger.error(e)
+        usage(1)
+    
+    for opt,val in opts:
+        if opt in ('-l', '--list'):
+            list_affs()
+            sys.exit(0)
+        if opt in ('-d', '--dryrun'):
+            dryrun = True
+        if opt in ('-a', '--affs'):
+            aff_groups = {}
+            tempaffs = val.split(" ")
+            for t in tempaffs:
+                a = t.split(":")
+                aff_groups[a[0]] = a[1]
+            logger.info('Start update of affiliation groups.')
+            update_aff_groups(aff_groups, dryrun)
+            logger.info('End update of affiliation groups.')
+
+    if aff_groups is None:
+        if len(cereconf.AFFILIATION_GROUPS) < 1:
+            logger.error("No affiliations/grops defined.")
+            usage(1)
+        else:
+            logger.info('Start update of affiliation groups.')
+            update_aff_groups(cereconf.AFFILIATION_GROUPS, dryrun)
+            logger.info('End update of affiliation groups.')
+    
+if __name__ == '__main__':
+    sys.exit(main())
