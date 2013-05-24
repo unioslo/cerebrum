05026c4b1cee16f3af5fd0636aba05b0ee13f207
Joakim Hovlandsvåg <jokim@usit.uio.no>
2013-05-24 04:49:45 +0000

    Started working on the periodic sync with the Gateway.
    The script should run periodically to make sure that the Gateway is updated with
    all our changes, as not all changes happens e.g. through a bofh command.

--- Cerebrum/modules/tsd/Gateway.py
+++ Cerebrum/modules/tsd/Gateway.py
@@ -40,10 +40,118 @@ from Cerebrum import Errors
 class GatewayClient(xmlrpclib.Server, object):
     """The client for communicating with TSD's gateway."""
 
-    def __init__(self, uri=cereconf.TSD_GATEWAY_URL, dryrun=False):
+    def __init__(self, logger, uri=cereconf.TSD_GATEWAY_URL, dryrun=False):
         """Sets the proper URL."""
-        super(GatewayClient, self).__init__(uri=uri)
-        # TODO: make use of the dryrun!
+        self.logger = logger
         self.dryrun = dryrun
+        super(GatewayClient, self).__init__(uri=uri, allow_none=True)
 
-    # TODO: Should we add the commands explicitly here?
+    # TBD: Adding explicit commands here, to know what methods we should be
+    # calling, but we still have the option to send them in raw.
+
+    def list_projects(self):
+        """Ask GW for a list of all its projects.
+
+        This call is not affected by the L{dryrun} option as it makes no changes
+        to the GW.
+
+        @rtype: list
+        @return: Each element in the list is a dict from the server, at the time
+            containing the elements:
+
+            - L{name}: The project-ID
+            - L{frozen}: If the project is frozen (quarantined)
+            - L{created}: A DateTime for when the project got created.
+            - L{expires}: A DateTime for when the project expires.
+
+        """
+        self.logger.debug("Gateway: project.list")
+        return self.project.list()
+
+    def list_users(self):
+        """Ask GW for a list of all its users.
+
+        This call is not affected by the L{dryrun} option as it makes no changes
+        to the GW.
+
+        @rtype: list
+        @return: Each element in the list is a dict from the server, at the time
+            containing the elements:
+
+                TODO
+            - L{name}: The username
+            - L{project}: What project the user belongs to.
+            - L{frozen}: If the user is frozen (quarantined)
+            - L{created}: A DateTime for when the user got created.
+            - L{expires}: A DateTime for when the user expires.
+
+        """
+        self.logger.debug("Gateway: user.list")
+        return self.user.list()
+
+    def list_hosts(self):
+        """Ask GW for a list of all its project hosts.
+
+        This call is not affected by the L{dryrun} option as it makes no changes
+        to the GW.
+
+        @rtype: list
+        @return: Each element in the list is a dict from the server, at the time
+            containing the elements:
+
+                TODO
+            - L{name}: The hostname
+            - L{project}: What project the host belongs to.
+            - L{frozen}: If the host is frozen (quarantined)
+            - L{created}: A DateTime for when the host got created.
+            - L{expires}: A DateTime for when the host expires.
+
+        """
+        self.logger.debug("Gateway: host.list")
+        return self.host.list()
+
+    def get_projects(self):
+        """Get all info about all projects from the GW.
+
+        The GW is asked for all information about the projects, hosts, users,
+        subnets, vlans and anything else that is relevant to Cerebrum. The
+        information is then sorted and returned in an easy-to-use format.
+
+        @rtype: dict
+        @return: The keys of the dict are the project-IDs, and each element
+            contains lists with information about the project's users, hosts and
+            other data.
+
+            TODO: Describe the format.
+
+        """
+        ret = dict()
+        # Fetch project info:
+        for proj in self.list_projects():
+            ret[proj['name']] = proj
+        # Fetch user info:
+        for user in self.list_users():
+            # TODO: what if the user belongs to a non-existing project?
+            ret[user['project']].setdefault('users', []).append(user)
+        # Fetch host info:
+        for host in self.list_hosts():
+            ret[host['project']].setdefault('hosts', []).append(host)
+        # Fetch subnet info:
+        # TODO
+        # Fetch vlan info:
+        # TODO
+        return ret
+
+    def delete_project(self, pid):
+        """Delete a given project from the GW.
+
+        TODO: Do we have to delete all its elements explicitly? Find out.
+
+        @type pid: string
+        @param pid: The project-ID that should be deleted.
+
+        """
+        self.logger.info("Gateway: project.delete(%s)", pid)
+        if self.dryrun:
+            return True
+        return self.project.delete({'project': pid})
---
+++ contrib/tsd/gateway_update.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2013 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+"""Job for syncing Cerebrum data with TSD's Gateway.
+
+The Gateway needs information about all projects, accounts, hosts, subnets and
+VLANs, to be able to let users in to the correct project. This information comes
+from Cerebrum. Some of the information is sent to the Gateway e.g through bofh
+commands and the import from Nettskjema, but not all information gets update
+that way. An example is quarantines that gets activated or deactivated.
+
+"""
+
+import sys
+import os
+import getopt
+from mx import DateTime
+
+import cerebrum_path
+import cereconf
+
+from Cerebrum import Errors
+from Cerebrum.Utils import Factory
+from Cerebrum.modules.no import fodselsnr
+from Cerebrum.modules.tsd import Gateway
+
+logger = Factory.get_logger('cronjob')
+db = Factory.get('Database')()
+co = Factory.get('Constants')(db)
+ou = Factory.get('OU')(db)
+pe = Factory.get('Person')(db)
+ac = Factory.get('Account')(db)
+
+
+
+def usage(exitcode=0):
+    print """
+    %(doc)s 
+    
+    Usage: %(file)s TODO
+
+    TODO
+
+    --url URL           The full URL to the Gateway.
+                        Example: https://gw.tsd.uio.no:1234/RPC
+                        Default: cereconf.TSD_GATEWAY_URL
+
+    -d --dryrun         Run the sync in dryrun. Data is retrieved from the
+                        Gateway, but no data is given to it.
+
+    -h --help           Show this and quit.
+
+    """ % {'doc': __doc__,
+           'file': os.path.basename(sys.argv[0])}
+    sys.exit(exitcode)
+
+class Project(object):
+    """Container of a project and its data.
+
+    This is to ease the comparement of projects with the Gateway.
+
+    """
+    def __init__(self, entity_id):
+        self.entity_id = entity_id
+        self.pid = TODO # ?
+        # TODO: add more data
+        pass
+    # TODO
+
+def process_projects(gw, dryrun):
+    """Go through all projects in Cerebrum and compare them with the Gateway.
+
+    If the Gateway contains mismatches, it should be updated.
+
+    """
+    # Cache Cerebrum-projects:
+    pro2name = dict((row['name'], row['entity_id'])
+                    for row in ou.search_tsd_projects())
+    ous = []
+    for row in ou.search(filter_quarantined=True):
+        ous.append(Project(row['ou_id']))
+        print row
+    # TODO: cache project-IDs:
+    #for row in ou.search_name_with_language(TODO)
+    logger.debug("Cached %d projects from Cerebrum", len(pro2name))
+
+    gw_projects = gw.get_projects()
+    logger.debug("Got %d projects from GW", len(gw_projects))
+    for pid, proj in gw_projects.iteritems():
+        if pid not in pro2name:
+            gw.delete_project(pid)
+        print "Project: %s" % (pid,)
+        # TODO: fix the data for each project
+
+
+
+    # TODO: Go throuch list of cached Cerebrum projects, create those that was
+    # not returned from GW:
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hd',
+                                   ['help', 'url=', 'dryrun'])
+    except getopt.GetoptError, e:
+        print e
+        usage(1)
+
+    dryrun = False
+    url = None
+
+    for opt, val in opts:
+        if opt in ('-h', '--help'):
+            usage()
+        elif opt in ('-d', '--dryrun'):
+            dryrun = True
+        elif opt in ('--url',):
+            url = val
+        else:
+            print "Unknown argument: %s" % opt
+            usage(1)
+
+    
+    if url:
+        gw = Gateway.GatewayClient(logger, uri=url)
+    else:
+        gw = Gateway.GatewayClient(logger)
+
+    logger.debug("Gateway: %s", gw)
+    logger.info("Start gw-sync")
+    process_projects(gw, dryrun)
+    logger.info("Finished gw-sync")
+
+if __name__ == '__main__':
+    main()
