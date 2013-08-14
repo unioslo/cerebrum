0d2f1305f79cee06281bb1b6a7c68d62abd5d145
Alexander Rødseth <rodseth@usit.uio.no>
2013-08-14 16:15:27 +0000

    Using bofhd_core_help + PEP8

--- Cerebrum/modules/tsd/bofhd_help.py
+++ Cerebrum/modules/tsd/bofhd_help.py
@@ -1,5 +1,5 @@
 # -*- coding: iso-8859-1 -*-
-# 
+#
 # Copyright 2013 University of Oslo, Norway
 #
 # This file is part of Cerebrum.
@@ -19,9 +19,9 @@
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 """Help text for bofhd in the TSD project."""
 
-from Cerebrum.modules.no.uio.bofhd_uio_help import group_help
-from Cerebrum.modules.no.uio.bofhd_uio_help import command_help
-from Cerebrum.modules.no.uio.bofhd_uio_help import arg_help
+from Cerebrum.modules.bofhd.bofhd_core_help import group_help
+from Cerebrum.modules.bofhd.bofhd_core_help import command_help
+from Cerebrum.modules.bofhd.bofhd_core_help import arg_help
 
 # Add instance specific help text:
 
@@ -30,57 +30,56 @@ group_help['project'] = 'Project related commands'
 # The texts in command_help are automatically line-wrapped, and should
 # not contain \n
 command_help['user'].update({
-    'user_approve': 
-        'Activate a user in the systems, after checking',
+    'user_approve':
+    'Activate a user in the systems, after checking',
     'user_generate_otpkey':
-        'Regenerate a One Time Password (OTP) key for an account',
-    })
+    'Regenerate a One Time Password (OTP) key for an account',
+})
 
 command_help['project'] = {
     'project_approve':
-        'Approve a project with the given name',
+    'Approve a project with the given name',
     'project_create':
-        'Create a new project manually',
+    'Create a new project manually',
     'project_freeze_request':
-        'Add a BofhdRequest for freezing a project',
+    'Add a BofhdRequest for freezing a project',
     'project_info':
-        'Show information about a given project',
+    'Show information about a given project',
     'project_list':
-        'List all projects according to given filter',
+    'List all projects according to given filter',
     'project_set_enddate':
-        'Reset the end date for a project',
+    'Reset the end date for a project',
     'project_terminate':
-        'Terminate a project by removing all data',
-    }
+    'Terminate a project by removing all data',
+}
 
 arg_help.update({
     'project_name':
-        ['projectname', 'Project name',
-         'Short, unique name of the project, around 6 digits'],
+    ['projectname', 'Project name',
+     'Short, unique name of the project, around 6 digits'],
     'project_longname':
-        ['longname', "Project's full name",
-         'The full, long name of the project'],
+    ['longname', "Project's full name",
+     'The full, long name of the project'],
     'project_shortname':
-        ['shortname', "Project's short name",
-         'The short, descriptive name of the project'],
+    ['shortname', "Project's short name",
+     'The short, descriptive name of the project'],
     'project_start_date':
-        ['startdate', "Project's start date",
-         'The day the project should be activated'],
+    ['startdate', "Project's start date",
+     'The day the project should be activated'],
     'project_end_date':
-        ['enddate', "Project's end date",
-         'The day the project should be ended and frozen'],
+    ['enddate', "Project's end date",
+     'The day the project should be ended and frozen'],
     'project_statusfilter':
-        ['filter', 'Filter on project status',
-         'Not implemented yet'],
+    ['filter', 'Filter on project status',
+               'Not implemented yet'],
     'entity_type':
-        ['entity_type', 'Entity type',
-         'Possible values:\n - group\n - account\n - project'],
+    ['entity_type', 'Entity type',
+     'Possible values:\n - group\n - account\n - project'],
     'person_search_type':
-        ['search_type', 'Enter person search type',
-         """Possible values:
+    ['search_type', 'Enter person search type',
+     """Possible values:
   - 'fnr'
   - 'name'
   - 'date' of birth, on format YYYY-MM-DD
   - 'stedkode' - Use project-ID"""],
-    })
-
+})
