3ee8fadd84bd65885bef117a2d991fb8c8ab9115
Joakim Hovlandsvåg <jokim@usit.uio.no>
2013-10-04 12:42:46 +0000

    Added missing help text to TSDs bofhd.

--- Cerebrum/modules/tsd/bofhd_help.py
+++ Cerebrum/modules/tsd/bofhd_help.py
@@ -31,56 +31,73 @@ group_help['project'] = 'Project related commands'
 # not contain \n
 command_help['user'].update({
     'user_approve':
-    'Activate a user in the systems, after checking',
+        'Activate a user in the systems, after checking',
     'user_generate_otpkey':
-    'Regenerate a One Time Password (OTP) key for an account',
+        'Regenerate a One Time Password (OTP) key for an account',
+    'user_set_password':
+        'Set a password for a user',
 })
-
+command_help['group'].update({
+    'group_add_member':
+        'Add a member to a group',
+    'group_remove_member':
+        'Remove a member from a group',
+})
+command_help.setdefault('subnet', {}).update({
+    'subnet_list':
+        'List all subnets',
+})
 command_help['project'] = {
     'project_approve':
-    'Approve a project with the given name',
+        'Approve a project with the given name',
+    'project_reject':
+        'Reject a project with the given name',
     'project_create':
-    'Create a new project manually',
-    'project_freeze_request':
-    'Add a BofhdRequest for freezing a project',
+        'Create a new project manually',
+    'project_freeze':
+        'Add a BofhdRequest for freezing a project',
+    'project_unfreeze':
+        'Thaw a project',
     'project_info':
-    'Show information about a given project',
+        'Show information about a given project',
     'project_list':
-    'List all projects according to given filter',
+        'List all projects according to given filter',
     'project_set_enddate':
-    'Reset the end date for a project',
+        'Reset the end date for a project',
     'project_terminate':
-    'Terminate a project by removing all data',
+        'Terminate a project by removing all data',
+    'project_unapproved':
+        'List all projects that has not been approved or rejected yet',
 }
 
 arg_help.update({
     'project_id':
-    ['projectID', 'Project ID',
-     'The project ID, normally on the form pXX, where XX goes from 01 to 99'],
+        ['projectID', 'Project ID',
+         'The project ID, normally on the form pXX, where XX goes from 01 to 99'],
     'project_name':
-    ['projectname', 'Project name',
-     'Short, unique name of the project, around 6 digits'],
+        ['projectname', 'Project name',
+         'Short, unique name of the project, around 6 digits'],
     'project_longname':
-    ['longname', "Project's full name",
-     'The full, long name of the project'],
+        ['longname', "Project's full name",
+         'The full, long name of the project'],
     'project_shortname':
-    ['shortname', "Project's short name",
-     'The short, descriptive name of the project'],
+        ['shortname', "Project's short name",
+         'The short, descriptive name of the project'],
     'project_start_date':
-    ['startdate', "Project's start date",
-     'The day the project should be activated'],
+        ['startdate', "Project's start date",
+         'The day the project should be activated'],
     'project_end_date':
-    ['enddate', "Project's end date",
-     'The day the project should be ended and frozen'],
+        ['enddate', "Project's end date",
+         'The day the project should be ended and frozen'],
     'project_statusfilter':
-    ['filter', 'Filter on project status',
-               'Not implemented yet'],
+        ['filter', 'Filter on project status',
+                   'Not implemented yet'],
     'entity_type':
-    ['entity_type', 'Entity type',
-     'Possible values:\n - group\n - account\n - project'],
+        ['entity_type', 'Entity type',
+         'Possible values:\n - group\n - account\n - project'],
     'person_search_type':
-    ['search_type', 'Enter person search type',
-     """Possible values:
+        ['search_type', 'Enter person search type',
+         """Possible values:
   - 'fnr'
   - 'name'
   - 'date' of birth, on format YYYY-MM-DD
