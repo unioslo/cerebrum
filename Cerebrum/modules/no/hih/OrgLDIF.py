d0aaa1eda3d19c65aec0585ec70e9d4d5e2d31fa
Joakim Hovlandsvåg <jokim@usit.uio.no>
2013-12-20 10:44:39 +0000

    CRB-174: Added untested OrgLDIF module for HiH, as they need their student's mobile numbers in LDAP for the project Flyt.

---
+++ Cerebrum/modules/no/hih/OrgLDIF.py
@@ -0,0 +1,41 @@
+# -*- coding: iso-8859-1 -*-
+# Copyright 2013 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+"""Mixin for OrgLDIF for HiH."""
+
+from collections import defaultdict
+
+from Cerebrum.Utils import Factory
+from Cerebrum.modules.no.OrgLDIF import *
+
+class hihOrgLDIFMixin(OrgLDIF):
+    def __init__(self, db, logger):
+        self.__super.__init__(db, logger)
+        self.attr2syntax['mobile'] = self.attr2syntax['telephoneNumber']
+
+    def init_attr2id2contacts(self):
+        """Override to include more, local data from contact info."""
+        self.__super.init_attr2id2contacts()
+        sap, fs = self.const.system_sap, self.const.system_fs
+        c = [(a, self.get_contacts(contact_type  = t,
+                                   source_system = s,
+                                   convert       = self.attr2syntax[a][0],
+                                   verify        = self.attr2syntax[a][1],
+                                   normalize     = self.attr2syntax[a][2]))
+             for a,s,t in (('mobile', fs, self.const.contact_mobile_phone),)]
+        self.attr2id2contacts.extend((v for v in c if v[1]))
