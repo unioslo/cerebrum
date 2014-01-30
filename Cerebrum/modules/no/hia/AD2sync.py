e041790f71edea376321e2dd94a7d057b0f85c03
Joakim Hovlandsvåg <jokim@usit.uio.no>
2014-01-30 07:14:31 +0000

    Subclassed the attribute comparement to exclude x500 addresses from ProxyAddresses.
    Have not been tested yet, as we need access to the production environment to do
    this.

--- Cerebrum/modules/no/hia/AD2sync.py
+++ Cerebrum/modules/no/hia/AD2sync.py
@@ -42,7 +42,24 @@ from Cerebrum.modules.ad2.CerebrumData import CerebrumUser, CerebrumGroup
 
 class UiAUserSync(UserSync):
     """UiA specific behaviour for the sync of users."""
-    pass
+    def attribute_mismatch(self, ent, atr, c, a):
+        """Compare an attribute between Cerebrum and AD.
+
+        Overridden to handle special attributes for UiA.
+
+        The ProxyAddresses attribute is also updated by Office365, with
+        addresses starting with x500. We should ignore such attributes when
+        comparing, to avoid having to update 20000 objects at each run. We
+        should only take care of SMTP addresses.
+
+        TODO: We should rather have this configurable and reusable for other
+        instances, as these problems will probably exist for others too.
+
+        """
+        if atr.lower() == 'proxyaddresses':
+            return sorted(c) != sorted(v for v in a if not
+                                                    v.startswith('x500:'))
+        return super(UiAUserSync, self).attribute_mismatch(ent, atr, c, a)
 
 class UiACerebrumUser(CerebrumUser):
     """UiA specific behaviour and attributes for a user object."""
