70aa2c6b95b6193e2698cc9694d99f8e5855d6bc
Fredrik Larsen <fhl@usit.uio.no>
2014-02-11 12:39:43 +0000

    Testing jenkins. There will be a lot of these commits, as Jenkins needs to fetch the files from a repository

--- testsuite/configs/basetest/run.sh
+++ testsuite/configs/basetest/run.sh
@@ -16,14 +16,14 @@
 # Should this stuff be configurable?
 
 # Root directory is either env ${WORKSPACE} or /tmp
+
 root_dir=${WORKSPACE:-/tmp}
-env_name=demo
-test_env=${root_dir}/${env_name}  # Working directory for our env
-crb_src=${root_dir}/src/cerebrum
 
-crb_src=${HOME}/src/cerebrum
-
+# Exit code
 error=0
+
+# Cerebrum source dir should exist in <root>/src/cerebrum
+crb_src=${WORKSPACE}/src/cerebrum
 
 # Source the setup tools
 if ! source ${crb_src}/testsuite/scripts/setup_tools.sh
@@ -32,15 +32,25 @@ then
     exit 1
 fi
 
-# Directory of this script. Useful if we expect local files
-this_dir=$( abs_dirname "${BASH_SOURCE[0]}" )
+
+# 
+# First test
+# 
+
+# Tests should run in <root>/basetests
+env_name=basetests
+env_dir=${WORKSPACE}/${name}
+
+# Config dir
+config=$( abs_dirname "${BASH_SOURCE[0]}" )
 
 ## Setup a new test environmnet in 
-setup_test_env -r"${root_dir}" -c"${crb_src}" -e"${env_name}" -s"${this_dir}"
-error+=$?
+setup_test_env -r"${root_dir}" -c"${crb_src}" -e"${env_name}" -s"${config}"
+error=$(($? + $error))
 
-pypath=$( prepare_pypath ${test_env}/etc/cerebrum ${crb_src}/testsuite/testtools )
+## Setup pythonpath for the tests
 info "pypath ($pypath)"
+export PYTHONPATH=$( prepare_pypath ${env_dir}/etc/cerebrum ${crb_src}/testsuite/testtools )
 error+=$?
 
 if [ $error -ne 0 ]
@@ -49,9 +59,10 @@ then
     exit $error
 fi
 
+# 
+# Setup OK, run tests
 info "Running tests"
-PYTHONPATH=$pypath ${test_env}/bin/nosetests -vv ${crb_src}/testsuite/test_Cerebrum
-error+=$?
+${test_env}/bin/nosetests -c ${config}/noseconfig.cfg  ${crb_src}/testsuite/tests/test_Cerebrum
+error=$(($? + $error))
 
 exit $error
-
