5ee8d926e13384f7289b4a00313997af9f6e7564
Jo Sama <jo.sama@usit.uio.no>
2014-06-03 17:43:17 +0200

    No-force for missing IPv6 subnet cereconfigurable.
    CRB-457: -ipv6-subnett-sjekkes-selv-om-import

--- Cerebrum/modules/dns/bofhd_dns_cmds.py
+++ Cerebrum/modules/dns/bofhd_dns_cmds.py
@@ -510,9 +510,14 @@ class BofhdExtension(BofhdCommandBase):
                                                              force)
         except SubnetError:
             # We don't require forcing if there does not exist a IPv6 subnet
-            # for the address. This is a highly temporary change, that really
-            # should not be implemented at all!
-            if not force and '.' in subnet_or_ip:
+            # for the address, if this is allowed in cereconf.
+            # TODO: This is technical debt and should be fixed!
+            skip = False
+            if (hasattr(cereconf, 'DNS_ACCEPT_MISSING_IPV6_SUBNET') and
+                cereconf.DNS_ACCEPT_MISSING_IPV6_SUBNET is True and
+                ':' in subnet_or_ip):
+                skip = True
+            if not force and not skip:
                 raise SubnetError("Unknown subnet; must force")
             if subnet_or_ip.find('/') > 0:
                 raise SubnetError("Unknown subnet; must use specific ip")
