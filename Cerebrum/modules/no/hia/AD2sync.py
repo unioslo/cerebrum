9b37a0109dc0687e700599cd607d95048341c663
Joakim Hovlandsvåg <jokim@usit.uio.no>
2014-09-04 11:52:30 +0200

    Ignore Exchange 2013 attributes when account has its spread.
    Temporary solution in CRB-545, while waiting for a more generic solution through
    configuration, in CRB-530.

--- Cerebrum/modules/no/hia/AD2sync.py
+++ Cerebrum/modules/no/hia/AD2sync.py
@@ -45,6 +45,8 @@ from Cerebrum.modules.ad2.ConfigUtils import ConfigError
 
 class UiAUserSync(UserSync):
 
+    """ Override of the Usersync, for UiA specific, complex behaviour. """
+
     def configure(self, config_args):
         """Override the configuration for setting specific variables for UiA
         user sync.
@@ -113,6 +115,40 @@ class UiAUserSync(UserSync):
             distgroup_sync.configure(distgroup_conf)
             distgroup_sync.fullsync()
 
+    def attribute_mismatch(self, ent, atr, c, a):
+        """Compare an attribute between Cerebrum and AD, UiA-wize.
+
+        This method ignores certain Exchange attributes if the user has the
+        spread `account@exchange`. For anything else, `super` is doing the rest.
+
+        This is a temporary hack, while waiting for the functionality in
+        CRB-523. Users with the spread "account@exchange" will be provisioned
+        through the new Exchange integration for Exchange 2013. The regular
+        AD-sync updated attributes for Exchange 2010 (spread account@exch_old).
+        The two Exchange versions are using some of the same AD attributes, but
+        they're using them differently, which creates conflicts with the two
+        syncs. The quick solution here is to ignore certain exchange attributes
+        if the user has the new Exchange spread.
+
+        """
+        # List of the attributes that Exchange 2013 needs, and which we
+        # therefore should ignore:
+        exch2013attrs = ('homemdb', 'msexchhomeservername', 'mdbusedefaults',
+                         'deliverandredirect', 'mdboverquotalimit',
+                         'mdboverhardquotalimit', 'mdbstoragequota',
+                         'proxyaddresses', 'targetaddress', 'homemta',
+                         'legacyexchangedn', 'mail', 'msexchmailboxguid',
+                         'msexchpoliciesexcluded', 'msexchpoliciesincluded',
+                         'msexchuserculture',
+                         )
+        # Force not updating certain Exchange attributes when user has spread
+        # for Exchange 2013 (which are updated through event_daemon):
+        if (self.co.spread_exchange_account in ent.spreads and
+                atr.lower() in exch2013attrs):
+            self.logger.debug3('Ignoring Exchange 2013 attribute "%s" for %s',
+                               atr, ent)
+            return (False, None, None)
+        return super(UiAUserSync, self).attribute_mismatch(ent, atr, c, a)
 
 class UiACerebrumUser(CerebrumUser):
     """UiA specific behaviour and attributes for a user object."""
@@ -120,8 +156,6 @@ class UiACerebrumUser(CerebrumUser):
     def calculate_ad_values(self):
         """Adding UiA specific attributes."""
         super(UiACerebrumUser, self).calculate_ad_values()
-        co = Factory.get('Constants')(Factory.get('Database'))
-        has_exchange = co.spread_exchange_acc_old in self.spreads
 
         # Hide all accounts that are not primary accounts:
         self.set_attribute('MsExchHideFromAddressLists',
