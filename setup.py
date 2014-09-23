unioslo/cerebrum

commit 0cec8f70425b80115980605cf5281c66825d4d47

Changes to the documentation and encoding tag
And minor python changes ("<>" to "!=")

* GPL2 has changed since 2002
* Changed from iso-8859-1 to utf-8
* Renamed INSTALL to INSTALL.md and made sure it was valid Markdown.
* Moved the old README.file-layout file to the documentation repo
* Renamed README to README.md and made sure it was valid Markdown

setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: iso-8859-1 -*-
+# -*- coding: utf-8 -*-
 
 # Copyright 2002-2011 University of Oslo, Norway
 #
@@ -28,7 +28,7 @@ NOTE: At least while developing, I recommend using "--prefix
 otherwise noted.
 
 /
-  README:       usr/share/cerebrum/doc/
+  README.md:       usr/share/cerebrum/doc/
   COPYING:      usr/share/cerebrum/doc/
 
 Cerebrum/
@@ -82,7 +82,7 @@ Other directories/files:
   usr/share/cerebrum/data:
     A number of subdirectories for various backends
 
-TODO: 
+TODO:
   comment on template config files (logging.ini, config.dat, cereconf.py)
 
 To install python modules in standard locations, and cerebrum files
@@ -100,7 +100,6 @@ import os
 import sys
 import pwd
 from glob import glob
-from types import StringType
 
 from distutils import sysconfig
 from distutils.command import install_data

COPYING
@@ -1,12 +1,12 @@
-		    GNU GENERAL PUBLIC LICENSE
-		       Version 2, June 1991
+                    GNU GENERAL PUBLIC LICENSE
+                       Version 2, June 1991
 
- Copyright (C) 1989, 1991 Free Software Foundation, Inc.
-                       59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ Copyright (C) 1989, 1991 Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  Everyone is permitted to copy and distribute verbatim copies
  of this license document, but changing it is not allowed.
 
-			    Preamble
+                            Preamble
 
   The licenses for most software are designed to take away your
 freedom to share and change it.  By contrast, the GNU General Public
@@ -15,7 +15,7 @@ software--to make sure the software is free for all its users.  This
 General Public License applies to most of the Free Software
 Foundation's software and to any other program whose authors commit to
 using it.  (Some other Free Software Foundation software is covered by
-the GNU Library General Public License instead.)  You can apply it to
+the GNU Lesser General Public License instead.)  You can apply it to
 your programs, too.
 
   When we speak of free software, we are referring to freedom, not
@@ -55,8 +55,8 @@ patent must be licensed for everyone's free use or not licensed at all.
 
   The precise terms and conditions for copying, distribution and
 modification follow.
-
-		    GNU GENERAL PUBLIC LICENSE
+
+                    GNU GENERAL PUBLIC LICENSE
    TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
 
   0. This License applies to any program or other work which contains
@@ -110,7 +110,7 @@ above, provided that you also meet all of these conditions:
     License.  (Exception: if the Program itself is interactive but
     does not normally print such an announcement, your work based on
     the Program is not required to print an announcement.)
-
+
 These requirements apply to the modified work as a whole.  If
 identifiable sections of that work are not derived from the Program,
 and can be reasonably considered independent and separate works in
@@ -168,7 +168,7 @@ access to copy from a designated place, then offering equivalent
 access to copy the source code from the same place counts as
 distribution of the source code, even though third parties are not
 compelled to copy the source along with the object code.
-
+
   4. You may not copy, modify, sublicense, or distribute the Program
 except as expressly provided under this License.  Any attempt
 otherwise to copy, modify, sublicense or distribute the Program is
@@ -225,7 +225,7 @@ impose that choice.
 
 This section is intended to make thoroughly clear what is believed to
 be a consequence of the rest of this License.
-
+
   8. If the distribution and/or use of the Program is restricted in
 certain countries either by patents or by copyrighted interfaces, the
 original copyright holder who places the Program under this License
@@ -255,7 +255,7 @@ make exceptions for this.  Our decision will be guided by the two goals
 of preserving the free status of all derivatives of our free software and
 of promoting the sharing and reuse of software generally.
 
-			    NO WARRANTY
+                            NO WARRANTY
 
   11. BECAUSE THE PROGRAM IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
 FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW.  EXCEPT WHEN
@@ -277,9 +277,9 @@ YOU OR THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER
 PROGRAMS), EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGES.
 
-		     END OF TERMS AND CONDITIONS
-
-	    How to Apply These Terms to Your New Programs
+                     END OF TERMS AND CONDITIONS
+
+            How to Apply These Terms to Your New Programs
 
   If you develop a new program, and you want it to be of the greatest
 possible use to the public, the best way to achieve this is to make it
@@ -303,10 +303,9 @@ the "copyright" line and a pointer to where the full notice is found.
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 Also add information on how to contact you by electronic and paper mail.
 
@@ -336,7 +335,5 @@ necessary.  Here is a sample; alter the names:
 This General Public License does not permit incorporating your program into
 proprietary programs.  If your program is a subroutine library, you may
 consider it more useful to permit linking proprietary applications with the
-library.  If this is what you want to do, use the GNU Library General
+library.  If this is what you want to do, use the GNU Lesser General
 Public License instead of this License.
-
-arch-tag: a2c3efb4-6641-4401-8317-76ed5598cce2

README
@@ -1,50 +1,0 @@
-Cerebrum
-========
-
-  Cerebrum is a Python/RDBMS framework upon which user administration
-  systems can be built.
-
-Requirements
-------------
-
-  Python 2.2.1
-  SQL Database (PostgreSQL, Oracle)
-
-Mailing lists
--------------
-
-     cerebrum-commits@usit.uio.no    - all commit logs
-     cerebrum-developers@usit.uio.no - the Cerebrum developers
-
-  Report bugs and misfeatures to cerebrum-developers@usit.uio.no.
-
-License
--------
-
-  Cerebrum is licensed using GNU Public License version 2 and later
-
-Fetching the source
--------------------
-
-  The latest source is available from
-  <URL:https://utv.uio.no/stash/projects/CRB/repos/cerebrum/browse>.
-
-Documentation
--------------
-
-  This is a brief summary of the currently existing documentation for
-  Cerebrum:
-
-    README  - this document
-    HACKING - things you should know if you want to develop the core
-       of cerebrum
-    INSTALL - describes the installation procedure
-    design/adminprotocol.html - describes communication between the
-       administration server and its clients
-    design/cerebrum-core.dia - diagram showing the core databasetables
-       in cerebrum.  Can be displayed using http://www.lysator.liu.se/~alla/dia/
-    design/entity_usage.txt - describes how to modify data in the database
-
-  The documentation for the core api can be read using pydoc.
-
-arch-tag: d628597f-d5cd-432a-8c67-62a1e0407c92

README.file-layout
@@ -1,204 +1,0 @@
-As I've promised Andreas, here is my attempt at shedding some light on
-why Cerebrum installs its files in so many different directories.
-
-
-* Layout of the Cerebrum development tree.
-
-  The various files that makes up Cerebrum's CVS tree (and hence any
-  Cerebrum release tarball) can be split into several groups:
-
-** cerebrum/Cerebrum/ -- the 'Cerebrum' python package.
-
-  When some piece of python code does "from Cerebrum import foo", it
-  imports some part of the Cerebrum python package.
-
-  This package contains both the "Core Cerebrum API" (which is made
-  up of all files directly under cerebrum/Cerebrum --
-  e.g. "Entity.py", "Database.py" and "Utils.py") and lots of
-  extensions building on this core API.
-
-  Any such extension is made up of one or more files residing
-  somewhere under cerebrum/Cererum/modules.
-
-  This modules/ directory has a few files placed directly in it --
-  these correspond to extensions that aren't (believed to be)
-  specific to any particular country or institution.
-
-  Modules that *are* considered country- or institution-specific are
-  placed in subdirectories of the modules/ directory; e.g. modules
-  specific to Norway lives in cerebrum/modules/no/, while modules
-  specific to The University of Oslo lives in
-  cerebrum/Cerebrum/modules/no/uio/.
-
-  The reason for splitting the modules/ subpackage in this
-  reverse-domain-name fashion is to allow different institutions to
-  maintain "their own" extensions without too much
-  inter-institutional quarreling over whether or not extension "foo"
-  should be changed (to support new features/clean up the
-  code/whatever).  While code reuse is nice, it isn't necessarily a
-  good idea to make it compulsory for all code to be reusable.
-
-  Example: The folks at uio.no might be perfectly happy with how
-    "Cerebrum.modules.no.foo" is working for them, while the folks at
-    ntnu.no desperately needs to tweak it a bit -- in a way that
-    would make it unusable to uio.no.  Solution: Let the ntnu.no
-    folks use Cerebrum.modules.no.ntnu.foo instead, where they can do
-    all the tweaking they need.  The uio.no folks can then keep using
-    Cerebrum.modules.no.foo.
-
-    Alternatively, the solution might be reversed -- ntnu.no keeps
-    using Cerebrum.modules.no.foo, while uio.no needs to fork the
-    existing module (before ntnu.no started tweaking it) into
-    Cerebrum.modules.no.uio.foo.
-
-    Without the option of forking the module like this, both
-    behaviours (pre- and post-tweak) could be supported in the same
-    module, conditionalized on some configuration setting.  However,
-    if you repeat this practice for N institutions, you'll quickly
-    end up with pretty much impenetrable spaghetti code.
-
-*** cerebrum/Cerebrum/extlib/ -- external code
-
-  Cerebrum makes use of a few Python modules that aren't developed as
-  part of Cerebrum.  These are imported into the development tree
-  under the cerebrum/Cerebrum/extlib/ directory.
-
-** cerebrum/contrib/ -- export and import scripts.
-
-  Scripts for doing various imports to and exports from Cerebrum
-  resides here.
-
-  This directory is also split in a reverse-domain-name fashion.
-  Scripts that are "generic" live directly under cerebrum/contrib/,
-  while scripts that are specific to uio.no lives in
-  cerebrum/contrib/no/uio/.
-
-  The idea is, again, to allow e.g. ntnu.no to implement features they
-  need without getting too tangled up in whatever features e.g. uio.no
-  are already using (and relying on).
-
-  Note that ntnu.no are free to choose filenames under contrib/no/ntnu
-  however they like.  If they make a ntnu.no-specific variant of the
-  existing script contrib/foo.py, they might call it
-  contrib/no/ntnu/foo.py, or they might call it
-  contrib/no/ntnu/foo_ntnu.py, or even contrib/no/ntnu/bar.py -- it's
-  their call.
-
-  Also note that some scripts will depend on specific extensions under
-  cerebrum/Cerebrum being installed and configured -- however, there
-  doesn't have to be any correspondence between the placement of the
-  script under contrib/ and the placement of the extension it uses
-  under Cerebrum.
-
-  Example: A script in contrib/no/uio/ is free to use extensions from
-    Cerebrum/modules/se/su/ and Cerebrum/modules/fi/tut/.
-
-** cerebrum/server/ -- various long-running Cerebrum services.
-** cerebrum/java/ -- Java client code (JBofh).
-** cerebrum/design/ -- SQL definitions and design documents.
-** cerebrum/web/ -- web-based bofh client.
-
-* Installing all this -- what goes where.
-
-** The Cerebrum python package
-
-  The entire cerebrum/Cerebrum/ directory (with all subdirectories) is
-  installed.  This is done partly to ease the installation process:
-
-   - the person doing the install doesn't have to specify which parts
-     to install, and
-
-   - the installation magic doesn't have to figure out if any of the
-     parts it was asked to install happens to rely on some part that
-     wasn't scheduled for installation.
-
-  and partly because we expect many installations to use extensions
-  from multiple (supposedly) institution-specific subpackages.
-  E.g. uio.no may discover that ntnu.no already have forked "foo" to
-  support a feature that uio.no also wants.  uio.no are then free to
-  use the ntnu.no-specific "foo" module (but might at a later date
-  need to fork it into their own directory if ntnu.no makes any
-  changes that uio.no doesn't want).
-
-  -- The Cerebrum python package and its subpackages should be
-     installed as "packages" (in distutils terminology); this means
-     that byte-compiled (".pyc") and, optionally, optimized (".pyo")
-     variants of the files should be installed along with the plain
-     ".py" files.
-
-     The Cerebrum package should be installed in the directory
-     returned by distutils.sysconfig.get_python_lib(prefix=$prefix),
-     e.g. $prefix/lib/python2.2/site-packages/.
-
-  We expect it to be a common case to install Cerebrum with a
-  non-standard --prefix or --root.  This means that python won't be
-  able to find the Cerebrum package without some modification of
-  sys.path.  To solve this, the module "cerebrum_path.py" is installed
-  to a directory in the non-modified sys.path, and Cerebrum scripts
-  should always do "import cerebrum_path" before they try to access
-  anything from the Cerebrum package.
-
-  -- The cerebrum_path.py module should be installed as "py_modules".
-     The module should go in the directory returned by
-     distutils.sysconfig.get_python_lib(),
-     e.g. /usr/lib/python2.2/site-packages/.
-
-  Python code should use fully qualified module names when importing
-  from the Cerebrum package.  Even though module
-  "Cerebrum.modules.no.foo" might be able to (in most circumstances)
-  import module "Cerebrum.modules.no.bar" by saying
-
-    import bar
-
-  it should rather say one of
-
-    from Cerebrum.modules.no import bar
-    import Cerebrum.modules.no.bar
-
-** The plethora of scripts under contrib/.
-
-  It is likely that many Cerebrum installations will want to use a
-  combination of
-
-   1) generic scripts -- the ones that live directly under contrib/,
-
-   2) "their own" scripts -- that live in the domain-specific
-      subdirectory of contrib/ corresponding to that institution, and
-
-   3) (supposedly) institution-specific scripts from one or more other
-      institutions -- which live in those institutions'
-      domain-specific subdirectories of contrib/.
-
-  As it is entirely up to each institution to decide how they want to
-  name the scripts inside their institution-specific subdirectory of
-  contrib, an attempt to install files from both (1), (2) and (3) to
-  the same directory might lead to collisions.
-
-  While it is not likely that any one installation will want to use
-  scripts from *all* the subdirectories of contrib/, the same
-  "simplicity of installation" argument as given above should hold
-  here, too.  Hence, we install the entire contrib/ tree.
-
-  -- All files under contrib/ should be installed as "scripts".
-     However, to avoid collisions, the tree structure of
-     cerebrum/contrib/ should be reflected under the installation
-     target directory, $prefix/share/cerebrum/contrib/.
-
-** Long-running services
-
-  Files under cerebrum/server/ should also be installed as "scripts",
-  and go in $prefix/sbin/.
-
-** SQL definitions etc.
-
-  Files under cerebrum/design/ should be installed as "data_files" to
-  $prefix/share/cerebrum/design/.
-
-
-Hopefully, that should cover most of it.  If there still are areas of
-uncertainty, or if anyone disagrees with any of the above, let me
-know.
--- 
-Harald
-
-arch-tag: 8c9630a4-065f-4c3f-b218-b13c0536249a

cerebrum_path.py.in
@@ -1,4 +1,4 @@
-# -*- coding: iso-8859-1 -*-
+# -*- coding: utf-8 -*-
 
 # Copyright 2002, 2003 University of Oslo, Norway
 #

makedb.py
@@ -221,7 +221,7 @@ def read_country_file(fname):
     for line in f.readlines():
         if line[0] == '#':
             continue
-        dta = [x.strip() for x in line.split("\t") if x.strip() <> ""]
+        dta = [x.strip() for x in line.split("\t") if x.strip() != ""]
         if len(dta) == 4:
             code_str, foo, country, phone_prefix = dta
             code_obj = const.Country(code_str, country, phone_prefix,
@@ -325,7 +325,7 @@ def check_schema_versions(db, strict=False):
                     exit(1)
         elif name[:10] == 'sqlmodule_':
             name = name[10:]
-            if not modules.has_key(name):
+            if name not in modules:
                 # print "WARNING: unknown module %s" % name
                 # if strict: exit(1)
                 continue
@@ -369,6 +369,7 @@ def get_filelist(db, extra_files=[]):
             else:
                 ret.append(f)
     return ret
+
 
 def parsefile(fname):
     """Parse an SQL definition file and return the statements and categories.
@@ -461,6 +462,7 @@ def parsefile(fname):
     f.close()
     return ret
 
+
 def runfile(fname, db, debug, phase):
     """Execute an SQL definition file.
 
@@ -495,7 +497,7 @@ def runfile(fname, db, debug, phase):
     for stmt in statements:
         if state == NO_CATEGORY:
             (type_id, for_phase) = stmt.split(":", 1)
-            if type_id <> 'category':
+            if type_id != 'category':
                 raise ValueError, \
                     "Illegal type_id in file %s: %s" % (fname, type_id)
             for_rdbms = None
@@ -562,7 +564,7 @@ def runfile(fname, db, debug, phase):
             version = metainfo['version']
         meta.set_metainfo(name, version)
         db.commit()
-    if state <> NO_CATEGORY:
+    if state != NO_CATEGORY:
         raise ValueError, \
             "Found more category specs than statements in file %s." % fname
     if output_col is not None:

commit b2a6db45a2b2420961243280781018e10cc09ebf

Bugfix, README renamed to README.md.
Bug introduced in 0cec8f70425b80115980605cf5281c66825d4d47

setup.py
@@ -277,7 +277,7 @@ data_files = [
      [('design/cerebrum-core.dia', 0644),
       ('design/cerebrum-core.html', 0644),
       ('design/adminprotocol.html', 0644),
-      ('README', 0644),
+      ('README.md', 0644),
       ('COPYING', 0644), ]),
     ({'path': sbindir,
       'owner': cerebrum_user,