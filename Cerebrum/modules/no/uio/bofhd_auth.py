f4dfb94587ff332432b16528ce0d283eab4f077b
Jo Sama <jo.sama@usit.uio.no>
2014-12-22 15:32:17 +0100

    Add forward → uname lookup for postmasters.
    CRB-690: -uio-mulighet-for-a-ske-opp-hvilken

--- Cerebrum/modules/no/uio/bofhd_auth.py
+++ Cerebrum/modules/no/uio/bofhd_auth.py
@@ -190,6 +190,19 @@ class BofhdAuth(auth.BofhdAuth):
                 raise PermissionDenied('Unregistered name: %s' % n)
         return True
 
+    def can_email_forward_info(self, operator, query_run_any=False):
+        """Allow access to superusers, postmasters and CERT."""
+        if self.is_superuser(operator):
+            return True
+        if self.is_postmaster(operator):
+            return True
+        if self._has_operation_perm_somewhere(
+            operator, self.const.auth_email_forward_info):
+            return True
+        if query_run_any:
+            return True
+        raise PermissionDenied('Restricted access')
+
     def can_show_history(self, operator, entity=None, query_run_any=False):
         """UiO-specific history-specific authentication rules."""
         if (entity and entity.entity_type == self.const.entity_email_target and
--- Cerebrum/modules/no/uio/bofhd_uio_cmds.py
+++ Cerebrum/modules/no/uio/bofhd_uio_cmds.py
@@ -1418,6 +1418,37 @@ class BofhdExtension(BofhdCommonMethods):
                 return True
         return False
 
+    # email forward_info
+    all_commands['email_forward_info'] = Command(
+        ('email', 'forward_info'),
+        SimpleString(),
+        perm_filter='can_email_forward_info',
+        fs=FormatSuggestion([
+            ('%s', ('id', ))]))
+
+    def email_forward_info(self, operator, forward_to):
+        """List owners of email forwards."""
+        self.ba.can_email_forward_info(operator.get_entity_id())
+        ef = Email.EmailForward(self.db)
+        et = Email.EmailTarget(self.db)
+        ac = Utils.Factory.get('Account')(self.db)
+        ret = []
+
+        # Different output format for different input.
+        rfun = lambda r: (r if '%' not in forward_to else
+                          '%-12s %s' % (r, fwd['forward_to']))
+
+        for fwd in ef.search(forward_to):
+            try:
+                et.clear()
+                et.find(fwd['target_id'])
+                ac.clear()
+                ac.find(et.email_target_entity_id)
+                ret.append({'id': rfun(ac.account_name)})
+            except Errors.NotFoundError:
+                ret.append({'id': rfun('id:%s' % et.entity_id)})
+        return ret
+
     # email info <account>+
     all_commands['email_info'] = Command(
         ("email", "info"),
