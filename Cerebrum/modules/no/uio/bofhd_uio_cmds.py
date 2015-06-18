unioslo/cerebrum

commit 579408bc97a1233c9da6c36e349ad692247bba64

CRB-835: Removes old Name column in group_list, which now has separate colums for Username and Fullname, which is populated according to type of the listed entities.

Cerebrum/modules/no/uio/bofhd_uio_cmds.py
@@ -6230,18 +6230,19 @@ Addresses and settings:
     # group list
     all_commands['group_list'] = Command(
         ("group", "list"), GroupName(),
-        fs=FormatSuggestion("%-10s %-10s %-40s %-10s", ("type",
-                                                        "name",
+        fs=FormatSuggestion("%-10s %-15s %-45s %-10s", ("type",
+                                                        "user_name",
                                                         "full_name",
                                                         "expired"),
-                            hdr="%-10s %-10s %-40s %-10s" % ("Type",
-                                                             "Name",
-                                                             "Full Name",
+                            hdr="%-10s %-15s %-45s %-10s" % ("Type",
+                                                             "Username",
+                                                             "Fullname",
                                                              "Expired")))
     def group_list(self, operator, groupname):
         """List direct members of group"""
         def compare(a, b):
-            return cmp(a['type'], b['type']) or cmp(a['name'], b['name'])
+            return cmp(a['type'], b['type']) or \
+                   cmp(a['user_name'], b['user_name'])
         group = self._get_group(groupname)
         ret = []
         now = DateTime.now()
@@ -6256,14 +6257,19 @@ Addresses and settings:
         for x in self._fetch_member_names(members):
             if x['member_type'] == int(self.const.entity_account):
                 ac.find(x['member_id'])
+                full_name = ac.get_fullname()
+                user_name = x['member_name']
+                ac.clear()
+            else:
+                full_name = x['member_name']
+                user_name = '<non-account>'
             tmp = {'id': x['member_id'],
                    'type': str(self.const.EntityType(x['member_type'])),
-                   'name': x['member_name'],
-                   'full_name': ac.get_fullname(),
+                   'user_name': user_name,
+                   'full_name': full_name,
                    'expired': None}
-            if (x["expire_date"] is not None and x["expire_date"] < now):
+            if x["expire_date"] is not None and x["expire_date"] < now:
                 tmp["expired"] = "expired"
-            ac.clear()
             ret.append(tmp)
 
         ret.sort(compare)

commit 0b897472053cf39f5dca2d77123750c3d15915f6

Fixes group_list incompability with brukerinfo
Adds a 'name' column for group_listing (not visible in clients that use
FormatSuggestion). This adds compatibility for clients that rely on the
structured feedback without using FormatSuggestion.

This was broken in 579408bc
brukerinfo-group-listing

Cerebrum/modules/no/uio/bofhd_uio_cmds.py
@@ -6265,6 +6265,7 @@ Addresses and settings:
                 user_name = '<non-account>'
             tmp = {'id': x['member_id'],
                    'type': str(self.const.EntityType(x['member_type'])),
+                   'name': user_name, # Compability with brukerinfo
                    'user_name': user_name,
                    'full_name': full_name,
                    'expired': None}