2cbec9113d7e12da918d789db38858e6c4f2cc5e
Torkild Gundersen Kjevik <torkild.kjevik@gmail.com>
2015-09-16 16:48:30 +0200

    CRB-966: Adds whitelisting of available quarantines for each opset.

--- Cerebrum/modules/bofhd/auth.py
+++ Cerebrum/modules/bofhd/auth.py
@@ -949,6 +949,21 @@ class BofhdAuth(DatabaseAccessor):
             raise PermissionDenied("No access")
         if not(isinstance(entity, Factory.get('Account'))):
             raise PermissionDenied("No access")
+
+        for row in self._list_target_permissions(
+                operator, self.const.auth_quarantine_disable,
+                self.const.auth_target_type_global_host,
+                None, get_all_op_attrs=True):
+            attr = row.get('operation_attr')
+            # No operation attributes means that all quarantines are allowed
+            # TODO: This can be removed when all opsets for all instances
+            # has a populated qua_disable dict.
+            if not attr:
+                return True
+            if str(qtype) == attr:
+                print 'Found attr: ', attr
+                return True
+
         return self.is_account_owner(
             operator, self.const.auth_quarantine_disable, entity)
 
@@ -977,6 +992,21 @@ class BofhdAuth(DatabaseAccessor):
         else:
             if self._no_account_home(operator, entity):
                 return True
+
+        for row in self._list_target_permissions(
+                operator, self.const.auth_quarantine_remove,
+                self.const.auth_target_type_global_host,
+                None, get_all_op_attrs=True):
+            attr = row.get('operation_attr')
+            # No operation attributes means that all quarantines are allowed
+            # TODO: This can be removed when all opsets for all instances
+            # has a populated qua_remove dict.
+            if not attr:
+                return True
+            if str(qtype) == attr:
+                print 'Found attr: ', attr
+                return True
+
         return self.is_account_owner(
             operator, self.const.auth_quarantine_remove, entity)
 
@@ -991,8 +1021,21 @@ class BofhdAuth(DatabaseAccessor):
             raise PermissionDenied('Not allowed to set automatic quarantine')
         if self.is_superuser(operator):
             return True
-        if str(qtype) in getattr(cereconf, 'QUARANTINE_AUTOMATIC', ()):
-            raise PermissionDenied('Not allowed to set automatic quarantine')
+        #if str(qtype) in getattr(cereconf, 'QUARANTINE_AUTOMATIC', ()):
+        #    raise PermissionDenied('Not allowed to set automatic quarantine')
+        for row in self._list_target_permissions(
+                operator, self.const.auth_quarantine_set,
+                self.const.auth_target_type_global_host,
+                None, get_all_op_attrs=True):
+            attr = row.get('operation_attr')
+            # No operation attributes means that all quarantines are allowed
+            # TODO: This can be removed when all opsets for all instances
+            # has a populated qua_add dict.
+            if not attr:
+                return True
+            if str(qtype) == attr:
+                print 'Found attr: ', attr
+                return True
 
         # TODO 2003-07-04: Bård is going to comment this
         if not(isinstance(entity, Factory.get('Account'))):
