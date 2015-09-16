a42b4a6dec6c6e0d2981157840076b23d010d65f
Torkild Gundersen Kjevik <torkild.kjevik@gmail.com>
2015-09-16 18:02:12 +0200

    CRB-966: Adds the possibility of sending notification emails when removing a quarantine through bofh, which is enabled for specific quarantines in cereconf.

--- Cerebrum/modules/no/uio/bofhd_uio_cmds.py
+++ Cerebrum/modules/no/uio/bofhd_uio_cmds.py
@@ -8661,6 +8661,23 @@ Addresses and settings:
                 self._get_name_from_object(entity), qtype))
 
         entity.delete_entity_quarantine(qconst)
+
+        # Generate notification email if QUARANTINE_NOTIFY is defined,
+        # and contains instructions for removal of this specific quarantine
+        # type.
+        if hasattr(cereconf, 'QUARANTINE_NOTIFY') and \
+           qtype in cereconf.QUARANTINE_NOTIFY and \
+           'remove' in cereconf.QUARANTINE_NOTIFY[qtype]:
+            subject = 'Removal of quarantine %s on %s' % \
+                      (qtype, self._get_name_from_object(entity))
+            body = 'Quarantine %s removed from %s' % \
+                   (qtype, self._get_name_from_object(entity))
+            notify_data = cereconf.QUARANTINE_NOTIFY[qtype]['remove']
+            Utils.sendmail(notify_data['to_addr'],
+                           notify_data['from_addr'],
+                           subject,
+                           body)
+
         return "OK, removed quarantine %s for %s" % (
             qconst, self._get_name_from_object (entity))
 
