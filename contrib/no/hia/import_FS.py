32ddd6b5fbe9d5eedb74673381317ef316301249
Tobias Langhoff <tvl@usit.uio.no>
2015-10-14 14:06:30 +0200

    Import pj in files that now use it

--- contrib/no/hia/import_FS.py
+++ contrib/no/hia/import_FS.py
@@ -19,6 +19,8 @@
 # along with Cerebrum; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
+from os.path import join as pj
+
 import re
 import os
 import sys
