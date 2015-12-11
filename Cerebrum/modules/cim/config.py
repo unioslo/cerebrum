538ed6bf8b078b60bbedbe435e66d8cbdd12b56c
Jonas Braathen <jonas.braathen@usit.uio.no>
2015-12-11 10:41:01 +0100

    CRB-1363: Add some configuration.

--- Cerebrum/modules/cim/config.py
+++ Cerebrum/modules/cim/config.py
@@ -19,3 +19,54 @@
 # along with Cerebrum; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 """ This module defines all neccessary config for the CIM integration. """
+
+from Cerebrum.config.configuration import Configuration
+from Cerebrum.config.configuration import ConfigDescriptor
+from Cerebrum.config.configuration import Namespace
+
+from Cerebrum.config.loader import read, read_config
+
+from Cerebrum.config.settings import Setting
+from Cerebrum.config.settings import String
+from Cerebrum.config.settings import Boolean
+
+
+def mixin_config(attr, cls):
+    return type('_ConfigMixin',
+                (Configuration, ),
+                {attr: ConfigDescriptor(Namespace, config=cls,)})
+
+
+class CimClientConfig(Configuration):
+    """Configuration for the CIM WS client."""
+    api_url = ConfigDescriptor(
+        String,
+        default=None,
+        doc="URL to the JSON API. Will be suffixed with endpoints.")
+
+    dry_run = ConfigDescriptor(
+        Boolean,
+        default=True,
+        doc="Send requests to web service with dry run mode enabled?")
+
+
+class CimEventConfig(Configuration):
+    """Configuration for the CIM event handler."""
+    pass
+
+
+class CimConfig(
+        mixin_config('client', CimClientConfig),
+        mixin_config('event', CimEventConfig),
+        Configuration):
+    pass
+
+
+def load_config(filepath=None):
+    config_cls = CimConfig()
+    if filepath:
+        config_cls.load_dict(read_config(filepath))
+    else:
+        read(config_cls, 'cim')
+    config_cls.validate()
+    return config_cls
