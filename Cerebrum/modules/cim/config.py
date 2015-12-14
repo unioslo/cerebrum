a8694fc671d24aadd118226c4005d3c8eb227c6e
Jonas Braathen <jonas.braathen@usit.uio.no>
2015-12-14 16:14:38 +0100

    CRB-1399: Rewrite configuration for the data source. Needs more work.

--- Cerebrum/modules/cim/config.py
+++ Cerebrum/modules/cim/config.py
@@ -20,36 +20,61 @@
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 """ This module defines all necessary config for the CIM integration. """
 
-from Cerebrum.Utils import Factory
+from Cerebrum.config.configuration import (ConfigDescriptor,
+                                           Configuration,
+                                           Namespace)
 
-__db = Factory.get('Database')()
-__co = Factory.get('Constants')(__db)
+from Cerebrum.config.loader import read, read_config
 
-# Authoritative system settings
-authoritative_system = __co.system_sap
-system_perspective = __co.OUPerspective('SAP')
+from Cerebrum.config.settings import (Boolean,
+                                      Integer,
+                                      Setting,
+                                      String,
+                                      Iterable,
+                                      NotSet)
 
-# Cerebrum -> CIM value mappings
-# Phone data settings
-phone_country = 'NO'
-phone_entry_mappings = {'job_mobile': __co.contact_mobile_phone,
-                        'job_phone': __co.contact_phone,
-                        'private_mobile': __co.contact_private_mobile,
-                        'private_phone': __co.contact_phone_private,
-                        }
+class CIMDataSourceConfig(Configuration):
+    """Configuration for the CIM data source."""
+    authoritative_system = ConfigDescriptor(
+        String,
+        default="SAP",
+        doc=u'Authoritative system to fetch contact information from.')
 
-# Company / Department data settings
-company_name = 'Universitetet i Oslo'
-company_hierarchy = ['company',
-                     'department',
-                     'subdep1',
-                     'subdep2',
-                     'subdep3',
-                     'subdep4',
-                     'subdep5',
-                     'subdep6',
-                     'subdep7',
-                     'subdep8',
-                     'subdep9',
-                     'subdep10',
-                     ]
+    ou_perspective = ConfigDescriptor(
+        String,
+        default="SAP",
+        doc=u'Perspective to use when fetching OU structure.')
+
+    phone_country_default = ConfigDescriptor(
+        String,
+        default="NO",
+        doc=u'Assume this phone region when otherwise unknown.')
+
+    # TODO
+    #phone_mappings = ConfigDescriptor(
+    #    Iterable,
+    #    default=NotSet,
+    #    doc=u'Mapping from field name to contact constant code.')
+
+    company_name = ConfigDescriptor(
+        String,
+        default=NotSet,
+        doc=(u'Company name.'))
+
+    company_hierarchy = ConfigDescriptor(
+        Iterable(template=String),
+        default=['company',
+                 'department',
+                 'subdep1',
+                 'subdep2',
+                 'subdep3',
+                 'subdep4',
+                 'subdep5',
+                 'subdep6',
+                 'subdep7',
+                 'subdep8',
+                 'subdep9',
+                 'subdep10',
+                 ],
+        doc=(u'Hierarchy of field names expected by the CIM web service.'))
+
