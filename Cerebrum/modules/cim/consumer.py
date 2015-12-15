8e246ab57279fb010de9d3cec8cee10ef6b00a0b
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2015-12-15 13:46:19 +0100

    CRB-1397: Fixed CIM EventConsumer boilerplate code

--- Cerebrum/modules/cim/consumer.py
+++ Cerebrum/modules/cim/consumer.py
@@ -31,13 +31,48 @@ from Cerebrum.modules.event.EventExceptions import EntityTypeError
 from Cerebrum.modules.event.EventExceptions import UnrelatedEvent
 from Cerebrum.modules.event.mapping import EventMap
 from Cerebrum.modules.event import evhandlers
-# from Cerebrum.utils.funcwrap import memoize
+from Cerebrum.utils.funcwrap import memoize
+
+from Cerebrum.Utils import Factory
+import pickle
+
+
+class default_config(object):
+    u""" Mock config. """
+
+    client = {
+        'url': 'http://localhost:80',
+        'version': 'v1'
+    }
+
+    event = {
+        'run_interval': 30,
+        'fail_limit': 3,
+        'fail_delay': 60,
+        'abandon_limit': 90
+    }
+
+    def __getitem__(self, name):
+        for prefix in ('client', 'event'):
+            if name == prefix:
+                return getattr(self, name)
+            if '.' in name:
+                pre, key = name.split('.', 1)
+                if pre == prefix:
+                    return getattr(self, prefix)[key]
+        raise KeyError(u'No config {!r}'.format(name))
 
 
 class Listener(evhandlers.EventConsumer):
     u""" Event listener and handler for CIM. """
 
     event_map = EventMap()
+
+    def __init__(self, cim_mock=False, cim_config=default_config(), **kwargs):
+        # TODO Replace with real config
+        self.__config = cim_config
+        self.__mock = cim_mock
+        super(Listener, self).__init__(**kwargs)
 
     def handle_event(self, event):
         u""" Call the appropriate handlers.
@@ -50,100 +85,134 @@ class Listener(evhandlers.EventConsumer):
 
         for callback in self.event_map.get_callbacks(str(key)):
             try:
-                callback(self, event)
+                callback(self, key, event)
             except (EntityTypeError, UnrelatedEvent) as e:
                 self.logger.debug3(
                     u'callback {!r} failed for event {!r} ({!r}): {!s}',
                     callback, key, event, e)
 
-#   @property
-#   @memoize
-#   def client(self):
-#       args = list()
-#       kwargs = dict()
+    @property
+    @memoize
+    def client(self):
+        # TODO: Really use memoize here?
+        # TODO: return real, initialized client?
+        if self.__mock or True:
+            class _mock_cim_client(object):
+                def __init__(s, c):
+                    s.config = c
 
-#       # TODO: return real, initialized client?
-#       if self.mock or True:
-#           return type(
-#               '_mock_cim_client',
-#               (object, ),
-#               {'foo': lambda x: x * 2,
-#                'bar': lambda x: x * 2, })(*args, **kwargs)
+                def __getattribute__(s, n):
+                    def _log(*a, **kw):
+                        self.logger.info('MOCK: {!s}({!r}, {!r})', n, a, kw)
+                    return _log
+            return _mock_cim_client(self.__config['client'])
 
-    @event_map('e_account:create', 'e_account:mod', 'e_account:password')
-    def account_change(self, event):
-        self.logger.info(u'Account change: {!r}', event)
+    @event_map(
+        'e_account:create',
+        'e_account:mod',
+        'e_account:password')
+    def account_change(self, key, event):
+        u""" Account change - update CIM. """
+        pe = Factory.get('Person')(self.db)
+        ac = Factory.get('Account')(self.db)
 
-# Typical `user create` flow
-#
-# e_account:create event
-#   {'event_type': 374L,
-#    'event_id': 4L,
-#    'target_system': 955L,
-#    'subject_entity': 878L,
-#    'dest_entity': None,
-#    'change_params':
-#       "(dp0\nS'owner_type'\np1\nI835\nsS'account_name'\np2\nS'olano'\np3\nsS'creator_id'\np4\nL868L\nsS'owner_id'\np5\nL872L\ns.",
-#    'tstamp': <mx.DateTime.DateTime object for '2015-12-11 18:21:38.00' at 7fb57b0b0078>,
-#    'failed': 0L,
-#    'taken_time': None,
-#   }
-#
-# spread adds..
-#
-# e_account:mod event:
-#   {'event_type': 269L,
-#    'event_id': 8L,
-#    'target_system': 955L,
-#    'subject_entity': 878L,
-#    'dest_entity': None,
-#    'change_params': '(dp0\n.',
-#    'tstamp': <mx.DateTime.DateTime object for '2015-12-11 18:21:38.00' at 7fb57b0b0078>,
-#    'failed': 0L,
-#    'taken_time': None,
-#   }
-#
-# e_account:password event
-#   {'event_type': 283L,
-#    'event_id': 9L,
-#    'target_system': 955L,
-#    'subject_entity': 878L,
-#    'dest_entity': None,
-#    'change_params': "(dp0\nS'password'\np1\nS'kZ%&9)Lr'\np2\ns.",
-#    'tstamp': <mx.DateTime.DateTime object for '2015-12-11 18:21:38.00' at 7fb57b0b0078>,
-#    'failed': 0L,
-#    'taken_time': None,
-#   }
-#
-# ac_type:add event
-#   {'event_type': 342L,
-#    'event_id': 12L,
-#    'target_system': 955L,
-#    'subject_entity': 878L,
-#    'dest_entity': None,
-#    'change_params':
-#       "(dp0\nS'priority'\np1\nI504\nsS'affiliation'\np2\nI726\nsS'ou_id'\np3\nI871\ns.",
-#    'tstamp': <mx.DateTime.DateTime object for '2015-12-11 18:21:38.00' at 7fb57b0b0078>,
-#    'failed': 0L,
-#    'taken_time': None,
-#   }
+        ac.find(event['subject_entity'])
+        if ac.owner_type != self.co.entity_person:
+            self.logger.debug3('Invalid owner type {!r}', ac.owner_type)
+            # TODO: What if account owner has changed from person to group?
+            #       Can that happen?
+            return
 
-    @event_map('ac_type:add', 'ac_type:mod', 'ac_type:del')
-    def account_type_change(self, event):
-        self.logger.info(u'Account type change: {!r}', event)
+        pe.find(ac.owner_id)
+
+        # TODO: Replace with real client call
+        self.client.update_person(pe.entity_id)
+
+    @event_map(
+        'ac_type:add',
+        'ac_type:mod',
+        'ac_type:del')
+    def account_pri_change(self, key, event):
+        u""" Account priority change! """
+        pe = Factory.get('Person')(self.db)
+        ac = Factory.get('Account')(self.db)
+
+        ac.find(event['subject_entity'])
+        if ac.owner_type != self.co.entity_person:
+            self.logger.debug3('Invalid owner type {!r}', ac.owner_type)
+            return
+        pe.find(ac.owner_id)
+
+        # 1. List all pe users.
+        # 2. Check current pri vs params pri?
+        # 2. Del all non-pri
+        # 3. Del or update pri, if eligible
+
+        pri_account = pe.get_primary_account()
+
+        self.logger.info(u'Current primary account: {:d}', pri_account)
+        self.logger.info(u'Current account: {:d}', ac.entity_id)
+
+        params = event.get('change_params')
+        if params:
+            try:
+                params = pickle.loads(params)
+            except Exception as e:
+                params = e
+
+        # TODO: Replace with real client calls
+        self.client.update_person(key, pe.entity_id)
 
     @event_map('e_account:delete', 'e_account:destroy')
-    def delete_account(self, event):
+    def account_delete(self, key, event):
         self.logger.info(u'Account delete (can this happen?): {!r}', event)
 
     @event_map(
         'person:create',
-        'person:update',
+        'person:update')
+    def person_change(self, key, event):
+        u""" Person change - update CIM. """
+        pe = Factory.get('Person')(self.db)
+        ac = Factory.get('Account')(self.db)
+
+        pe.find(ac.owner_id)
+        account_id = pe.get_primary_account()
+        ac.find(account_id)
+
+        # TODO: Replace with real client call
+        self.client.update_person(key, pe.entity_id)
+
+    @event_map(
         'person:name_del',
         'person:name_add',
-        'person:name_mod',
+        'person:name_mod')
+    def person_name_change(self, key, event):
+        u""" Person name change - update CIM. """
+        pe = Factory.get('Person')(self.db)
+        ac = Factory.get('Account')(self.db)
+
+        pe.find(ac.owner_id)
+        account_id = pe.get_primary_account()
+        ac.find(account_id)
+
+        # TODO: Replace with real client call
+        self.client.update_person(key, pe.entity_id)
+
+    @event_map(
         'person:aff_add',
         'person:aff_mod',
         'person:aff_del')
-    def update_person(self, event):
-        self.logger.info(u'Person change: {!r}', event)
+    def person_aff_change(self, key, event):
+        u""" Person aff change - update CIM. """
+        pe = Factory.get('Person')(self.db)
+        ac = Factory.get('Account')(self.db)
+
+        pe.find(ac.owner_id)
+        account_id = pe.get_primary_account()
+        ac.find(account_id)
+
+        # TODO: Decide on delete or update
+        # So much could have happened here!
+
+        # TODO: Replace with real client call
+        self.client.update_person(key, pe.entity_id)
