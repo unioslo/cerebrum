372b5197095f2a41edf86e934fcb3abc90956eb1
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2016-02-11 13:22:03 +0100

    Fixes Subnet.find to not mark entity as changed
    Fixes a bug introduced with CRB-1354. When introducing entity_type lookups in
    the subnet classes, the Entity superclass (mark_update) ends up marking
    `entity_type` as changed. This causes problems when doing write_db.
    
    This commit fixes the real problem – that Subnet.find and IPv6Subnet.find doesn't
    call the superclass `find`-method.
    
    CRB-1508-subnet-bug

--- Cerebrum/modules/dns/IPv6Subnet.py
+++ Cerebrum/modules/dns/IPv6Subnet.py
@@ -388,15 +388,16 @@ class IPv6Subnet(Entity):
             binds['ip'] = IPv6Calc.ip_to_long(identifier)
 
         try:
-            (self.entity_id, self.subnet_ip, self.ip_min, self.ip_max,
+            (eid, self.subnet_ip, self.ip_min, self.ip_max,
              self.description, self.dns_delegated, self.name_prefix,
-             self.vlan_number, self.no_of_reserved_adr,
-             self.entity_type) = self.query_1(
+             self.vlan_number, self.no_of_reserved_adr) = self.query_1(
                  """ SELECT entity_id, subnet_ip, ip_min, ip_max, description,
                             dns_delegated, name_prefix, vlan_number,
-                            no_of_reserved_adr, entity_type
+                            no_of_reserved_adr
                      FROM [:table schema=cerebrum name=dns_ipv6_subnet]
                      WHERE %s""" % where_param, binds)
+
+            self.__super.find(eid)
 
             self.subnet_mask = IPv6Subnet.calculate_subnet_mask(self.ip_min,
                                                                 self.ip_max)
