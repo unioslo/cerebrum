8e76975e1b686ec6ee470f397b6e408a1205e72b
Kenneth Johansen <bas_admin@uit.no>
2016-05-10 15:01:21 +0200

    removed call to set_home

--- process_systemx.py
+++ process_systemx.py
@@ -382,7 +382,7 @@ def _handle_changes(a_id,changes):
         if ccode=='spreads_add':
             for s in cdata:
                 ac.add_spread(s)
-                ac.set_home_dir(s)
+                #ac.set_home_dir(s)
         elif ccode=='quarantine_add':
             ac.add_entity_quarantine(cdata,get_creator_id(),start=TODAY)
             #ac.quarantine_add(cdata)
