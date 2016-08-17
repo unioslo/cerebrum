82ab3d217f349ddfd1c9380d5c8618bb02f6849a
Robert Bauck Hamar <r.b.hamar@usit.uio.no>
2016-08-17 16:22:31 +0200

    PEP-8

--- contrib/no/hia/process_deleted.py
+++ contrib/no/hia/process_deleted.py
@@ -34,32 +34,31 @@
 # email: write file (<uname>:<email_server>), account@imap spread removed
 # other spreads: removed
 #
-# TODO: this script must be more robust and pretty (but first we make it work) :-)
+# TODO: this script must be more robust and pretty (but first we make it work)
 
 
-import getopt
-import sys
 import time
 import os
 import mx
 import string
 
 import cerebrum_path
-import cereconf
 
 from Cerebrum import Errors
 from Cerebrum.modules import Email
-from Cerebrum.modules import PosixGroup
-from Cerebrum import Constants
 from Cerebrum.Utils import Factory
 from Cerebrum.modules.bofhd.utils import BofhdRequests
-from Cerebrum.modules.bofhd.errors import CerebrumError
+
+del cerebrum_path
 
 db = Factory.get('Database')()
 db.cl_init(change_program='process_bofhd_r')
 cl_const = Factory.get('CLConstants')(db)
 const = Factory.get('Constants')(db)
 logger = Factory.get_logger("cronjob")
+max_requests = None
+start_time = None
+
 
 def process_delete_requests():
     br = BofhdRequests(db, const)
@@ -67,7 +66,6 @@ def process_delete_requests():
     del_file = []
     group = Factory.get('Group')(db)
     account = Factory.get('Account')(db)
-    operator = Factory.get('Account')(db)
     spreads = []
     posix_home = ''
     pwd = '*'
@@ -98,49 +96,57 @@ def process_delete_requests():
         logger.info("Trying to delete account %s", account.account_name)
 
         set_operator(r['requestee_id'])
-        ## Set expire_date (do not export data about this account)
+        # Set expire_date (do not export data about this account)
         account.expire_date = br.now
-        logger.debug("expire_date for %s registered as %s", account.account_name, br.now)
+        logger.debug("expire_date for %s registered as %s",
+                     account.account_name, br.now)
         account.write_db()
         # check for posix attrs
-        posix_user = Factory.get('PosixUser')(db)    
+        posix_user = Factory.get('PosixUser')(db)
         posix_user.clear()
         try:
             posix_user.find(r['entity_id'])
         except Errors.NotFoundError:
             posix_user = None
 
-        ## Deal with all the systems that account data is exported to 
+        # Deal with all the systems that account data is exported to
         spreads = account.get_spread()
-        logger.debug("Fetched all spreads for %s, %s", account.account_name, spreads)
+        logger.debug("Fetched all spreads for %s, %s",
+                     account.account_name, spreads)
         for row in spreads:
-            ## Account is valid in AD, remove account@ad spread
+            # Account is valid in AD, remove account@ad spread
             if row['spread'] == const.spread_hia_ad_account:
                 try:
                     home = account.get_home(row['spread'])
                     account.set_homedir(current_id=home['homedir_id'],
                                         status=const.home_status_archived)
-                    account.clear_home(row['spread'])                    
+                    account.clear_home(row['spread'])
                 except Errors.NotFoundError:
-                    logger.debug("No home in AD for %s, will remove spread to AD only.", account.account_name)
+                    logger.debug("No home in AD for %s, will remove spread to "
+                                 "AD only.", account.account_name)
                 account.delete_spread(row['spread'])
-                logger.debug("Deleted account@ad spread for %s", account.account_name)
-            ## student-accounts usually have account@ldap, remove this
+                logger.debug("Deleted account@ad spread for %s",
+                             account.account_name)
+            # student-accounts usually have account@ldap, remove this
             elif row['spread'] == const.spread_ldap_account:
                 account.delete_spread(row['spread'])
-                logger.debug("Deleted account@ldap spread for %s", account.account_name)                
-            ## An email account exists, remove account@imap spread, register email account delete
+                logger.debug("Deleted account@ldap spread for %s",
+                             account.account_name)
+            # An email account exists, remove account@imap spread,
+            # register email account delete
             elif row['spread'] == const.spread_hia_email:
                 et = Email.EmailTarget(db)
                 try:
                     et.find_by_target_entity(account.entity_id)
                 except Errors.NotFoundError:
-                    logger.warn('No email target for %s, removing imap spread only.' % account.account_name)
+                    logger.warn('No email target for %s, removing imap spread '
+                                'only.', account.account_name)
                 logger.debug("Found e-mail target for %s", account.account_name)
                 if et:
                     es = Email.EmailServer(db)
                     es.find(et.email_server_id)
-                    del_file.append('EMAIL:' + account.account_name + ':' + es.name)
+                    del_file.append('EMAIL:' + account.account_name + ':' +
+                                    es.name)
                     et.email_target_type = const.email_target_deleted
                     et.write_db()
                 account.delete_spread(row['spread'])
@@ -150,23 +156,29 @@ def process_delete_requests():
                 try:
                     et.find_by_target_entity(account.entity_id)
                 except Errors.NotFoundError:
-                    logger.warn('No email target for %s, will remove exchange spread.', account.account_name)
+                    logger.warn('No email target for %s, will remove exchange '
+                                'spread.', account.account_name)
                 if et:
                     et.email_target_type = const.email_target_deleted
                     et.write_db()
-                    logger.info("Deleted e-mail target for %s", account.account_name)
+                    logger.info("Deleted e-mail target for %s",
+                                account.account_name)
                 account.delete_spread(row['spread'])
-            ## Account is valid in nis@hia, remove account@nis spread, register nis-home delete
+            # Account is valid in nis@hia, remove account@nis spread,
+            # register nis-home delete
             elif row['spread'] == const.spread_nis_user:
                 if not isinstance(posix_user, Factory.get('PosixUser')):
-                    logger.error("Manual intervention required, no posix account is found for account %s",
+                    logger.error("Manual intervention required, no posix "
+                                 "account is found for account %s",
                                  account.account_name)
                     continue
                 posix_home = posix_user.get_posix_home(row['spread'])
                 uid = posix_user.posix_uid
                 gid = posix_user.gid_id
                 shell = posix_user.shell
-                line = string.join([account.account_name, pwd, str(uid), str(gid), gecos, posix_home, str(shell)], ':')
+                line = string.join([account.account_name, pwd, str(uid),
+                                    str(gid), gecos, posix_home,
+                                    str(shell)], ':')
                 line = 'NIS:' + line
                 del_file.append(line)
                 try:
@@ -175,17 +187,21 @@ def process_delete_requests():
                     continue
                 account.set_homedir(current_id=home['homedir_id'],
                                     status=const.home_status_archived)
-                logger.debug("Set home to archived %s (%s)", home['homedir_id'], row['spread'])
+                logger.debug("Set home to archived %s (%s)", home['homedir_id'],
+                             row['spread'])
                 account.clear_home(row['spread'])
                 logger.debug("clear_home in %s", row['spread'])
                 account.delete_spread(row['spread'])
-            ## Account is valid in nisans@hia, remove account@nisans spread, register nisans-home delete
+            # Account is valid in nisans@hia, remove account@nisans spread,
+            # register nisans-home delete
             elif row['spread'] == const.spread_ans_nis_user:
                 posix_home = posix_user.get_posix_home(row['spread'])
                 uid = posix_user.posix_uid
                 gid = posix_user.gid_id
                 shell = posix_user.shell
-                line = string.join([account.account_name, pwd, str(uid), str(gid), gecos, posix_home, str(shell)], ':')
+                line = string.join([account.account_name, pwd, str(uid),
+                                    str(gid), gecos, posix_home, str(shell)],
+                                   ':')
                 line = 'NISANS:' + line
                 del_file.append(line)
                 try:
@@ -194,27 +210,30 @@ def process_delete_requests():
                     continue
                 account.set_homedir(current_id=home['homedir_id'],
                                     status=const.home_status_archived)
-                logger.debug("Set home to archived %s (%s)", home['homedir_id'], row['spread'])
+                logger.debug("Set home to archived %s (%s)",
+                             home['homedir_id'], row['spread'])
                 account.clear_home(row['spread'])
                 account.delete_spread(row['spread'])
             else:
                 account.delete_spread(row['spread'])
         if posix_user:
             posix_user.delete_posixuser()
-        ## Remove account from all groups
+
+        # Remove account from all groups
         for g in group.search(member_id=account.entity_id,
                               indirect_members=False):
             group.clear()
             group.find(g['group_id'])
             group.remove_member(account.entity_id)
-        ## All done, remove request, commit results
+        # All done, remove request, commit results
         account.write_db()
         if posix_user:
             posix_user.write_db()
         br.delete_request(request_id=r['request_id'])
         db.commit()
     return del_file
-    
+
+
 def keep_running():
     # If we've run for more than half an hour, it's time to go on to
     # the next task.  This check is necessary since job_runner is
@@ -226,6 +245,7 @@ def keep_running():
         return False
     return time.time() - start_time < 15 * 60
 
+
 def is_valid_request(req_id):
     # The request may have been canceled very recently
     br = BofhdRequests(db, const)
@@ -233,11 +253,13 @@ def is_valid_request(req_id):
         return True
     return False
 
+
 def set_operator(entity_id=None):
     if entity_id:
         db.cl_init(change_by=entity_id)
     else:
         db.cl_init(change_program='process_bofhd_r')
+
 
 def main():
     global start_time, max_requests
@@ -249,7 +271,7 @@ def main():
     max_requests = 999999
     start_time = time.time()
 
-    del_list =  process_delete_requests()
+    del_list = process_delete_requests()
 
     if del_list:
         stream = open(outfile, 'w')
@@ -260,5 +282,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
