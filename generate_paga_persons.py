c3209d72969677304240233303f095a87a976743
Kenneth Johansen <bas_admin@uit.no>
2016-09-28 15:16:20 +0200

    AtomicFileWriter has been moved to its own file. updating import lines

--- generate_paga_persons.py
+++ generate_paga_persons.py
@@ -34,7 +34,8 @@ import cerebrum_path
 import cereconf
 from Cerebrum.Utils import Factory
 from Cerebrum import Errors
-from Cerebrum.Utils import Factory, AtomicFileWriter
+from Cerebrum.Utils import Factory
+from Cerebrum.utils.atomicfile import AtomicFileWriter
 from Cerebrum.extlib import xmlprinter
 
 from Cerebrum.modules.no.uit.EntityExpire import EntityExpiredError
