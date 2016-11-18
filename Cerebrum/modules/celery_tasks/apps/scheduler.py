60e2dace62b1079b59b5c1ddd95bcf87ecc883a4
Simeon Simeonov <blackmore@pichove.org>
2016-11-18 12:24:42 +0100

    INT-89: Add schedule_AD_message

--- Cerebrum/modules/celery_tasks/apps/scheduler.py
+++ Cerebrum/modules/celery_tasks/apps/scheduler.py
@@ -3,3 +3,14 @@
 from Cerebrum.modules.celery_tasks import create_celery_app
 
 app = create_celery_app('scheduler')
+
+
+@app.task(bind=True, name='scheduler.schedule_AD_message')
+def schedule_AD_message(self, message):
+    """
+    Keyword Arguments:
+    :param message:
+    :type message:
+
+    """
+    pass
