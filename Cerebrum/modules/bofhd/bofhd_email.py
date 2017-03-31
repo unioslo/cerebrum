unioslo/cerebrum

commit 4c5de203b7293a24b8c18a2f8ea7f38059ce45fe

CRB-1605: Remove various references and functionality related to Mailman

Cerebrum/modules/bofhd/bofhd_email.py
@@ -741,17 +741,7 @@ class BofhdEmailMixin(BofhdEmailMixinBase):
             ("Forwarding:       %s", ("forward_1", )),
             ("                  %s", ("forward", )),
             #
-            # target_type == Mailman
-            #
-            ("Mailing list:     %s", ("mailman_list", )),
-            ("Alias:            %s", ("mailman_alias_1", )),
-            ("                  %s", ("mailman_alias", )),
-            ("Request address:  %s", ("mailman_mailcmd_1", )),
-            ("                  %s", ("mailman_mailcmd", )),
-            ("Owner address:    %s", ("mailman_mailowner_1", )),
-            ("                  %s", ("mailman_mailowner", )),
-            #
-            # target_type == Sympa. Looks like mailman, but has more attributes
+            # target_type == Sympa
             #
             ("Mailing list:     %s", ("sympa_list",)),
             ("Alias:            %s", ("sympa_alias_1",)),
@@ -787,7 +777,7 @@ class BofhdEmailMixin(BofhdEmailMixinBase):
             ("Forwarding:       %s (%s)", ("fw_addr_1", "fw_enable_1")),
             ("                  %s (%s)", ("fw_addr", "fw_enable")),
             #
-            # both account and Mailman/Sympa
+            # both account and Sympa
             #
             ("Spam level:       %s (%s)\n" +
              "Spam action:      %s (%s)", ("spam_level", "spam_level_desc",
@@ -824,8 +814,7 @@ class BofhdEmailMixin(BofhdEmailMixinBase):
                 ret.append({'def_addr': "<none>"})
             # No else?
 
-        if ttype not in (self.const.email_target_Mailman,
-                         self.const.email_target_Sympa):
+        if ttype == self.const.email_target_Sympa:
             # We want to split the valid addresses into multiple
             # parts for MLs, so there is special code for that.
             addrs = self._get_valid_email_addrs(et, special=True, sort=True)
@@ -835,13 +824,9 @@ class BofhdEmailMixin(BofhdEmailMixinBase):
             for addr in addrs[1:]:
                 ret.append({"valid_addr": addr})
 
-        # TODO: The mailman/sympa email_info belongs in
+        # TODO: The sympa email_info belongs in
         #       bofhd_email_list.py
-        if (ttype == self.const.email_target_Mailman and
-                hasattr(self, '_email_info_mailman')):
-            # TODO: What if the constant doesn't exist?
-            ret += getattr(self, '_email_info_mailman')(et, uname)
-        elif (ttype == self.const.email_target_Sympa and
+        if (ttype == self.const.email_target_Sympa and
                 hasattr(self, '_email_info_sympa')):
             # TODO: What if the constant doesn't exist?
             ret += getattr(self, '_email_info_sympa')(operator, et, uname)
@@ -925,8 +910,7 @@ class BofhdEmailMixin(BofhdEmailMixinBase):
         info = {}
         data = [info, ]
         if (et.email_target_alias is not None and
-            et.email_target_type not in (self.const.email_target_Mailman,
-                                         self.const.email_target_Sympa)):
+            et.email_target_type == self.const.email_target_Sympa):
             info['alias_value'] = et.email_target_alias
         info["account"] = acc.account_name
         if et.email_server_id:
@@ -967,7 +951,7 @@ class BofhdEmailMixin(BofhdEmailMixinBase):
             info["filters"] = ", ".join([x for x in filters]),
         else:
             info["filters"] = "<none>"
-        return [info,]
+        return [info, ]
 
     def _email_info_detail(self, acc):
         """ Get email account details.
@@ -1690,8 +1674,7 @@ class BofhdEmailMixin(BofhdEmailMixinBase):
         et, addr = self._get_email_target_and_address(address)
         esf = Email.EmailSpamFilter(self.db)
         all_targets = [et.entity_id]
-        if target_type in (self.const.email_target_Mailman,
-                           self.const.email_target_Sympa):
+        if target_type == self.const.email_target_Sympa:
             # This will fail if we don't have the BofhdEmailListMixin
             all_targets = getattr(self, '_get_all_related_maillist_targets')(
                 addr.get_address())
@@ -1716,8 +1699,7 @@ class BofhdEmailMixin(BofhdEmailMixinBase):
         et, addr = self._get_email_target_and_address(address)
         etf = Email.EmailTargetFilter(self.db)
         all_targets = [et.entity_id]
-        if target_type in (self.const.email_target_Mailman,
-                           self.const.email_target_Sympa):
+        if target_type == self.const.email_target_Sympa:
             # This will fail if we don't have the BofhdEmailListMixin
             all_targets = getattr(self, '_get_all_related_maillist_targets')(
                 addr.get_address())
@@ -1736,7 +1718,6 @@ class BofhdEmailMixin(BofhdEmailMixinBase):
                     etf.populate(filter_code, parent=et)
                     etf.write_db()
 
-    
     def _report_deleted_EA(self, deleted_EA):
         """ Inform postmaster about deleted EmailAddesses.
 
@@ -2173,8 +2154,7 @@ Addresses and settings:
         filter_code = self._get_constant(self.const.EmailTargetFilter, filter)
         et, addr = self._get_email_target_and_address(address)
         target_ids = [et.entity_id]
-        if int(et.email_target_type) in (self.const.email_target_Mailman,
-                                         self.const.email_target_Sympa):
+        if et.email_target_type == self.const.email_target_Sympa:
             # The only way we can get here is if uname is actually an e-mail
             # address on its own.
             # This will fail if we don't have the BofhdEmailListMixin
@@ -2216,8 +2196,7 @@ Addresses and settings:
         filter_code = self._get_constant(self.const.EmailTargetFilter, filter)
         et, addr = self._get_email_target_and_address(address)
         target_ids = [et.entity_id]
-        if int(et.email_target_type) in (self.const.email_target_Mailman,
-                                         self.const.email_target_Sympa):
+        if et.email_target_type == self.const.email_target_Sympa:
             # The only way we can get here is if uname is actually an e-mail
             # address on its own.
             #
@@ -2277,8 +2256,7 @@ Addresses and settings:
         target_ids = [et.entity_id]
         # The only way we can get here is if uname is actually an e-mail
         # address on its own.
-        if int(et.email_target_type) in (self.const.email_target_Mailman,
-                                         self.const.email_target_Sympa):
+        if et.email_target_type == self.const.email_target_Sympa:
             # This will fail if we don't have the BofhdEmailListMixin
             target_ids = getattr(self,
                                  '_get_all_related_maillist_targets')(uname)
@@ -2306,7 +2284,7 @@ Addresses and settings:
 
     #
     # email spam_action <action> <uname>+
-    # 
+    #
     # (This code is cut'n'paste of email_spam_level(), only the call
     # to populate() had to be fixed manually. It's hard to fix this
     # kind of code duplication cleanly.)
@@ -2342,8 +2320,7 @@ Addresses and settings:
         target_ids = [et.entity_id]
         # The only way we can get here is if uname is actually an e-mail
         # address on its own.
-        if int(et.email_target_type) in (self.const.email_target_Mailman,
-                                         self.const.email_target_Sympa):
+        if et.email_target_type == self.const.email_target_Sympa:
             # This will fail if we don't have the BofhdEmailListMixin
             target_ids = getattr(self,
                                  '_get_all_related_maillist_targets')(uname)

Cerebrum/default_config.py
@@ -122,7 +122,6 @@ MVUSER_SCRIPT = '/cerebrum/sbin/mvuser'
 RMUSER_SCRIPT = '/cerebrum/sbin/aruser'
 ARCHIVE_MAIL_SCRIPT = '/cerebrum/sbin/archivemail'
 LEGAL_BATCH_MOVE_TIMES = '20:00-08:00'
-MAILMAN_SCRIPT = None
 SYMPA_SCRIPT = None
 CONVERT_MAILCONFIG_SCRIPT = None
 MVMAIL_SCRIPT = None

Cerebrum/modules/Email.py
@@ -230,13 +230,6 @@ class EmailConstants(Constants.Constants):
         " email_target.using_uid is set, deliveries to this target"
         " will be run as that PosixUser.")
 
-    email_target_Mailman = _EmailTargetCode(
-        'Mailman',
-        "Target is a Mailman mailing list.  The command (and args) to"
-        " pipe mail into is gathered from email_target.alias_value."
-        "  Iff email_target.using_uid is set, deliveries to this target"
-        " will be run as that PosixUser.")
-
     email_target_Sympa = _EmailTargetCode(
         'Sympa',
         "Target is a Sympa mailing list.  The command (and args) to"

Cerebrum/modules/EmailLDAP.py
@@ -104,9 +104,8 @@ class EmailLDAP(DatabaseAccessor):
             et = int(et)
 
         result = {"targetType": self.get_targettype(target_type)}
-        if target_type in (co.email_target_Mailman, co.email_target_Sympa,
-                           co.email_target_pipe, co.email_target_RT,
-                           co.email_target_file):
+        if target_type in (co.email_target_pipe, co.email_target_file,
+                           co.email_target_Sympa, co.email_target_RT):
             result["target"] = alias
         elif target_type in (co.email_target_account, co.email_target_deleted):
             if et == co.entity_account and ei in self.acc2name:

Cerebrum/modules/bofhd/auth.py
@@ -1757,7 +1757,6 @@ class BofhdAuth(DatabaseAccessor):
         if query_run_any or account:
             return self.can_email_forward_toggle(operator, account,
                                                  query_run_any)
-        # typically Mailman lists
         if self.is_superuser(operator):
             return True
         if self.is_postmaster(operator):
@@ -1776,7 +1775,7 @@ class BofhdAuth(DatabaseAccessor):
 
     # only the user may add or remove forward addresses.
     def can_email_forward_edit(self, operator, account=None, domain=None,
-                                query_run_any=False):
+                               query_run_any=False):
         if query_run_any:
             return True
         if account and operator == account.entity_id:
@@ -1794,10 +1793,6 @@ class BofhdAuth(DatabaseAccessor):
         return self.can_email_forward_edit(operator, account,
                                            query_run_any=query_run_any)
 
-    # TODO: when Mailman is better integrated with Cerebrum, we can
-    # allow local postmasters to create lists, but today creating a
-    # list requires shell access to mailman@lister.uio.no, so there's
-    # no point.
     def can_email_list_create(self, operator, domain=None,
                               query_run_any=False):
         if self.is_superuser(operator):
@@ -1821,11 +1816,11 @@ class BofhdAuth(DatabaseAccessor):
         return self.can_email_archive_create(operator, domain, query_run_any)
 
     def can_email_pipe_create(self, operator, domain=None,
-                                 query_run_any=False):
+                              query_run_any=False):
         return self.can_email_list_create(operator, domain, query_run_any)
 
     def can_email_pipe_edit(self, operator, domain=None,
-                                 query_run_any=False):
+                            query_run_any=False):
         return self.can_email_pipe_create(operator, domain, query_run_any)
 
     def can_email_set_failure(self, operator, account=None,
@@ -1839,16 +1834,6 @@ class BofhdAuth(DatabaseAccessor):
         raise PermissionDenied("Currently limited to superusers")
 
     def can_email_domain_create(self, operator, query_run_any=False):
-        if self.is_superuser(operator):
-            return True
-        if self.is_postmaster(operator):
-            return True
-        if query_run_any:
-            return False
-        raise PermissionDenied("Currently limited to superusers")
-
-    def can_email_list_create(self, operator, domain=None,
-                              query_run_any=False):
         if self.is_superuser(operator):
             return True
         if self.is_postmaster(operator):

Cerebrum/modules/bofhd/bofhd_core_help.py
@@ -143,38 +143,22 @@ command_help = {
             "address",
         "email_info":
             "View e-mail information about a user or address",
-        "email_create_archive":
-            "Add address feeding an archive of a list",
-        "email_create_list":
-            "Add addresses needed for a Mailman list",
         "email_create_sympa_list":
             "Add addresses needed for a Sympa list",
         "email_create_sympa_cerebrum_list":
             "Add addresses needed for a Sympa list (Cerebrum only)",
-        "email_create_list_alias":
-            "Add an alias for a Mailman list.  This also adds additional "
-            "-owner and -request addresses.",
         "email_create_sympa_list_alias":
             "Add an alias for a Sympa list.  This also adds additional "
             "addresses (e.g. -owner, -request, etc.)",
-        "email_remove_list_alias":
-            "Remove an alias for a Mailman list. This also removes additional "
-            "administrative addresses (-owner, -request, etc.)",
         "email_remove_sympa_list_alias":
             "Remove an alias for a Sympa list. This also removes additional "
             "administrative addresses (-owner, -request, etc.)",
-        "email_reassign_list_address":
-            "Migrates email targets from Mailman to Sympa",
         "email_create_multi":
             "Make an e-mail target which expands to the members of a group",
         "email_create_pipe":
             "Make an e-mail target which points to a pipe",
         "email_delete_pipe":
             "Delete an e-mail target that points to a pipe",
-        "email_delete_archive":
-            "Remove address for a Mailman archive",
-        "email_delete_list":
-            "Remove a Mailman list's addresses",
         "email_delete_sympa_list":
             "Remove a Sympa list's addresses",
         "email_delete_multi":

Cerebrum/modules/bofhd/bofhd_email_rt.py
@@ -194,15 +194,7 @@ class BofhdEmailRTMixin(BofhdEmailMixinBase):
         except CerebrumError:
             pass
         else:
-            if et.email_target_type == self.const.email_target_Mailman:
-                self.ba.can_rt_replace_mailman(op, domain=rt_dom)
-                if not self._get_boolean(force):
-                    raise CerebrumError(
-                        "Address <%s> exists (mailing list), use force" % addr)
-                replaced_lists = [x['address'] for x in
-                                  self._email_delete_list(op, addr, ea)]
-            else:
-                raise CerebrumError("Address <%s> is in use" % addr)
+            raise CerebrumError("Address <{}> is in use".format(addr))
 
         acc = self._get_account("exim")
         et = Email.EmailTarget(self.db)

commit 93daa2f99702c422deb43e574ba4614297b1b9f0

Fix bugs introduced by 4c5de203b72

Cerebrum/modules/bofhd/bofhd_email.py
@@ -814,7 +814,7 @@ class BofhdEmailMixin(BofhdEmailMixinBase):
                 ret.append({'def_addr': "<none>"})
             # No else?
 
-        if ttype == self.const.email_target_Sympa:
+        if ttype != self.const.email_target_Sympa:
             # We want to split the valid addresses into multiple
             # parts for MLs, so there is special code for that.
             addrs = self._get_valid_email_addrs(et, special=True, sort=True)
@@ -910,7 +910,7 @@ class BofhdEmailMixin(BofhdEmailMixinBase):
         info = {}
         data = [info, ]
         if (et.email_target_alias is not None and
-            et.email_target_type == self.const.email_target_Sympa):
+            et.email_target_type != self.const.email_target_Sympa):
             info['alias_value'] = et.email_target_alias
         info["account"] = acc.account_name
         if et.email_server_id: