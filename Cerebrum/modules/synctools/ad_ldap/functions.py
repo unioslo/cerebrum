d2594b1f9e1f78c5efd6566bb87de3537527993d
Torkild Gundersen Kjevik <torkild.kjevik@gmail.com>
2017-10-24 11:22:43 +0200

    ADMS-24: Add --password-sync option.

--- Cerebrum/modules/synctools/ad_ldap/functions.py
+++ Cerebrum/modules/synctools/ad_ldap/functions.py
@@ -25,12 +25,15 @@ from Cerebrum.modules.synctools.ad_ldap.compare import equal
 from Cerebrum.Utils import Factory
 
 
-def build_event_dict(entity_id, entity_type, event_type):
-    return {
+def build_event_dict(entity_id, entity_type, event_type, attrs=None):
+    event_dict = {
         'entity_id': entity_id,
         'entity_type': entity_type,
         'event_type': event_type
     }
+    if attrs:
+        event_dict['attrs'] = attrs
+    return event_dict
 
 
 def generate_stats(logger, entity_type, desynced, not_in_ad,
@@ -57,13 +60,9 @@ def build_all_account_events(db,
                              group_postfix,
                              path_req_disks,
                              acc_attrs,
-                             show_diff=False):
+                             show_diff,
+                             password_sync=False):
     logger = Factory.get_logger("console")
-    logger.info('Getting account data from AD-LDAP....')
-    ad_ldap_acc_values = mappers.format_ldap_acc_data(
-        df.get_all_ad_ldap_acc_values(client),
-        acc_attrs
-    )
     logger.info('Getting account data from Cerebrum...')
     all_crb_accs_data = df.get_all_crb_accounts_data(db,
                                                      ad_acc_spread,
@@ -77,42 +76,53 @@ def build_all_account_events(db,
     skipped = len(all_crb_accs_data) - len(crb_acc_ad_values)
     desynced_accounts = []
     accounts_not_in_ad = []
-    logger.info('Diffing account data...')
-    for crb_acc in crb_acc_ad_values:
-        if crb_acc['username'] not in ad_ldap_acc_values:
-            accounts_not_in_ad.append(crb_acc['account_id'])
-            continue
-        if not equal(crb_acc,
-                     ad_ldap_acc_values[crb_acc['username']],
-                     acc_attrs,
-                     show_diff=show_diff,
-                     entity_id=crb_acc['account_id'],
-                     entity_type='account'):
-            desynced_accounts.append(crb_acc['account_id'])
-        # Remove from dict to get number of accounts not present in AD,
-        # but not Cerebrum when this for-loop is done.
-        ad_ldap_acc_values.pop(crb_acc['username'])
-
-    generate_stats(logger,
-                   'accounts',
-                   len(desynced_accounts),
-                   len(accounts_not_in_ad),
-                   len(ad_ldap_acc_values),
-                   skipped)
-
-    logger.info('Building account events...')
     events = []
-    for crb_acc in desynced_accounts:
-        events.append(build_event_dict(crb_acc, 'account', 'modify'))
+    if not password_sync:
+        logger.info('Getting account data from AD-LDAP....')
+        ad_ldap_acc_values = mappers.format_ldap_acc_data(
+            df.get_all_ad_ldap_acc_values(client),
+            acc_attrs
+        )
+        logger.info('Diffing account data...')
+        for crb_acc in crb_acc_ad_values:
+            if crb_acc['username'] not in ad_ldap_acc_values:
+                accounts_not_in_ad.append(crb_acc['account_id'])
+                continue
+            if not equal(crb_acc,
+                         ad_ldap_acc_values[crb_acc['username']],
+                         acc_attrs,
+                         show_diff=show_diff,
+                         entity_id=crb_acc['account_id'],
+                         entity_type='account'):
+                desynced_accounts.append(crb_acc['account_id'])
+            # Remove from dict to get number of accounts not present in AD,
+            # but not Cerebrum when this for-loop is done.
+            ad_ldap_acc_values.pop(crb_acc['username'])
+        accs_not_in_crb = [base_df.get_account_id_by_username(db, username)
+                           for username in ad_ldap_acc_values.keys()]
 
-    for crb_acc in accounts_not_in_ad:
-        events.append(build_event_dict(crb_acc, 'account', 'add'))
+        for crb_acc in accs_not_in_crb:
+            events.append(build_event_dict(crb_acc, 'account', 'remove'))
+        generate_stats(logger,
+                       'accounts',
+                       len(desynced_accounts),
+                       len(accounts_not_in_ad),
+                       len(ad_ldap_acc_values),
+                       skipped)
+        logger.info('Building account events...')
+        for crb_acc in desynced_accounts:
+            events.append(build_event_dict(crb_acc, 'account', 'modify'))
 
-    accs_not_in_crb = [base_df.get_account_id_by_username(db, username)
-                       for username in ad_ldap_acc_values.keys()]
-
-    for crb_acc in accs_not_in_crb:
-        events.append(build_event_dict(crb_acc, 'account', 'remove'))
+        for crb_acc in accounts_not_in_ad:
+            events.append(build_event_dict(crb_acc, 'account', 'add'))
+    else:
+        for crb_acc in crb_acc_ad_values:
+            events.append(build_event_dict(crb_acc['account_id'],
+                                           'account',
+                                           'modify',
+                                           attrs=['password']))
+        logger.info('Generated # of password events: {}'
+                    ''.format(len(crb_acc_ad_values)))
     return events
 
 
@@ -122,7 +132,7 @@ def build_all_group_events(db,
                            ad_grp_spread,
                            group_postfix,
                            grp_attrs,
-                           show_diff=False):
+                           show_diff):
     logger = Factory.get_logger("console")
     logger.info('Getting group data from AD-LDAP....')
     all_ad_ldap_grp_values = mappers.format_ldap_grp_data(
@@ -189,7 +199,8 @@ def build_account_events(db,
                          group_postfix,
                          path_req_disks,
                          acc_attrs,
-                         show_diff=False):
+                         show_diff,
+                         password_sync=False):
     crb_accs_data = [
         df.get_crb_account_data(db, acc_id, ad_acc_spread)
         for acc_id in account_ids
@@ -201,37 +212,40 @@ def build_account_events(db,
                                             group_postfix)
         for crb_acc_data in crb_accs_data]
     logger = Factory.get_logger("console")
-    skipped = 0
-    desynced = []
-    not_in_ad = []
-    for crb_acc in crb_acc_ad_values:
-        if crb_acc.get('quarantine_action') == 'skip':
-            skipped += 1
-            continue
-        ad_ldap_acc_values = mappers.format_ldap_acc_data(
-            df.get_ad_ldap_acc_values(client,
-                                      crb_acc['username']),
-            acc_attrs
-        )
-        if crb_acc['username'] not in ad_ldap_acc_values:
-            not_in_ad.append(crb_acc['account_id'])
-            continue
-        if not equal(crb_acc,
-                     ad_ldap_acc_values[crb_acc['username']],
-                     acc_attrs,
-                     show_diff=show_diff,
-                     entity_id=crb_acc['account_id'],
-                     entity_type='account'):
-            desynced.append(crb_acc['account_id'])
-
-    generate_stats(logger, 'accounts', len(desynced), len(not_in_ad),
-                   skipped=skipped)
-
     events = []
-    for acc in desynced:
-        events.append(build_event_dict(acc, 'account', 'modify'))
+    if not password_sync:
+        skipped = 0
+        desynced = []
+        not_in_ad = []
+        for crb_acc in crb_acc_ad_values:
+            if crb_acc.get('quarantine_action') == 'skip':
+                skipped += 1
+                continue
+            ad_ldap_acc_values = mappers.format_ldap_acc_data(
+                df.get_ad_ldap_acc_values(client, crb_acc['username']),
+                acc_attrs
+            )
+            if crb_acc['username'] not in ad_ldap_acc_values:
+                not_in_ad.append(crb_acc['account_id'])
+                continue
+            if not equal(crb_acc,
+                         ad_ldap_acc_values[crb_acc['username']],
+                         acc_attrs,
+                         show_diff=show_diff,
+                         entity_id=crb_acc['account_id'],
+                         entity_type='account'):
+                desynced.append(crb_acc['account_id'])
+        for acc in desynced:
+            events.append(build_event_dict(acc, 'account', 'modify'))
 
-    for acc in not_in_ad:
-        events.append(build_event_dict(acc, 'account', 'add'))
-
+        for acc in not_in_ad:
+            events.append(build_event_dict(acc, 'account', 'add'))
+        generate_stats(logger, 'accounts', len(desynced), len(not_in_ad),
+                       skipped=skipped)
+    else:
+        for crb_acc in crb_acc_ad_values:
+            events.append(build_event_dict(crb_acc['account_id'],
+                                           'account',
+                                           'modify',
+                                           attrs=['password']))
     return events
