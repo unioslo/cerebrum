ad77547b7aaeeaeee91e3207f57d1c39d1fc1618
Torkild Gundersen Kjevik <t.g.kjevik@usit.uio.no>
2017-11-16 15:10:12 +0100

    CRB-2243: Add route for getting person addresses

--- Cerebrum/rest/api/v1/person.py
+++ Cerebrum/rest/api/v1/person.py
@@ -97,6 +97,40 @@ PersonAccountList = api.model('PersonAccountList', {
         description='List of accounts'),
 })
 
+PersonAddress = api.model('PersonAddress', {
+    'id': fields.base.Integer(
+        default=None,
+        description='Person entity ID'),
+    'source_system': fields.Constant(
+        ctype='AuthoritativeSystem',
+        description='Source system'),
+    'address_type': fields.Constant(
+        ctype='Address',
+        description='Address type'
+    ),
+    'address_text': fields.base.String(
+        default=None,
+        description='Address text'
+    ),
+    'p_o_box': fields.base.String(
+        default=None,
+        description='Postal box'
+    ),
+    'postal_number': fields.base.String(
+        default=None,
+        description='Postal number'
+    ),
+    'city': fields.base.String(
+        default=None,
+        description='City'
+    ),
+    'country': fields.base.String(
+        default=None,
+        description='Country'
+    ),
+
+})
+
 
 @api.route('/<int:id>', endpoint='person')
 @api.doc(params={'id': 'Person entity ID'})
@@ -238,3 +272,22 @@ class PersonConsentListResource(Resource):
                 'expires': None,
             })
         return consents
+
+
+@api.route('/<int:id>/addresses', endpoint='person-addresses')
+@api.doc(params={'id': 'Person entity ID'})
+class PersonAddressListResource(Resource):
+    """Resource for person addresses."""
+    @auth.require()
+    @api.marshal_list_with(PersonAddress)
+    def get(self, id):
+        """Get the accounts of a person."""
+        pe = find_person(id)
+        addrs = pe.get_entity_address()
+        data = []
+        for addr in addrs:
+            addr_dict = addr.dict()
+            addr_dict['id'] = addr_dict['entity_id']
+            del addr_dict['entity_id']
+            data.append(addr_dict)
+        return data
