0c26a15173f3d19440d3627f68ec39065df5023b
Jonas Braathen <jonas.braathen@usit.uio.no>
2018-01-17 12:53:51 +0100

    CRB-2353: Add migrations for bofhd_auth 1.0 -> 1.2

--- contrib/migrate_cerebrum_database.py
+++ contrib/migrate_cerebrum_database.py
@@ -45,6 +45,7 @@ targets = {
              'rel_0_9_14', 'rel_0_9_15', 'rel_0_9_16', 'rel_0_9_17',
              'rel_0_9_18', 'rel_0_9_19', ),
     'bofhd': ('bofhd_1_1', 'bofhd_1_2', 'bofhd_1_3',),
+    'bofhd_auth': ('bofhd_auth_1_1', 'bofhd_auth_1_2',),
     'changelog': ('changelog_1_2', 'changelog_1_3'),
     'email': ('email_1_0', 'email_1_1', 'email_1_2', 'email_1_3', 'email_1_4',
               'email_1_5',),
@@ -805,6 +806,26 @@ def migrate_to_bofhd_1_3():
     meta = Metainfo.Metainfo(db)
     meta.set_metainfo("sqlmodule_bofhd", "1.3")
     print "Migration to bofhd 1.3 completed successfully"
+    db.commit()
+
+
+def migrate_to_bofhd_auth_1_1():
+    print "\ndone."
+    assert_db_version("1.0", component='bofhd_auth')
+    makedb('bofhd_auth_1_1', 'pre')
+    meta = Metainfo.Metainfo(db)
+    meta.set_metainfo("sqlmodule_bofhd_auth", "1.1")
+    print "Migration to bofhd_auth 1.1 completed successfully"
+    db.commit()
+
+
+def migrate_to_bofhd_auth_1_2():
+    print "\ndone."
+    assert_db_version("1.1", component='bofhd_auth')
+    makedb('bofhd_auth_1_2', 'pre')
+    meta = Metainfo.Metainfo(db)
+    meta.set_metainfo("sqlmodule_bofhd_auth", "1.2")
+    print "Migration to bofhd_auth 1.2 completed successfully"
     db.commit()
 
 
