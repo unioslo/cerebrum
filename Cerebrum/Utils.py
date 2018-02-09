30e6aea37333505035933296a19f220e9ad022a5
Simeon Simeonov <sgs@usit.uio.no>
2018-02-09 10:59:10 +0100

    Fix several unicode-related and unrelated bugs

--- Cerebrum/Utils.py
+++ Cerebrum/Utils.py
@@ -22,7 +22,6 @@
 tree.
 """
 from __future__ import unicode_literals
-import six
 import cereconf
 import inspect
 import os
@@ -37,6 +36,9 @@ import traceback
 import socket
 import random
 import collections
+
+import six
+
 from string import ascii_lowercase, digits
 from subprocess import Popen, PIPE
 
@@ -955,7 +957,7 @@ def argument_to_sql(argument,
     return "(%s %s :%s)" % (sql_attr_name, compare_scalar, binds_name)
 
 
-def prepare_string(value, transform=str.lower):
+def prepare_string(value, transform=six.text_type.lower):
     """Prepare a string for being used in SQL.
 
     @type value: basestring
@@ -971,11 +973,8 @@ def prepare_string(value, transform=str.lower):
       Send in None or some other callable to override this behaviour.
     """
 
-    if (type(value) == type(unicode())) and (transform == str.lower):
-        transform = unicode.lower
-
-    value = value.replace("*", "%")
-    value = value.replace("?", "_")
+    value = value.replace(u"*", u"%")
+    value = value.replace(u"?", u"_")
 
     if transform:
         return transform(value)
