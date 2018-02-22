eb626d0fc152527846fd936581e389cf445c8ea7
Jonas Braathen <jonas.braathen@usit.uio.no>
2018-02-22 18:31:26 +0100

    CRB-2431: Unicode support for generate_automount_ldif

--- contrib/no/uio/generate_automount_ldif.py
+++ contrib/no/uio/generate_automount_ldif.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright 2002, 2003, 2004 University of Oslo, Norway
+# Copyright 2003-2018 University of Oslo, Norway
 #
 # This file is part of Cerebrum.
 #
@@ -18,23 +18,23 @@
 # along with Cerebrum; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-"""Generate an LDIF-file with automount information for all disks with
-a host and at least one user.
-"""
+"""Generate an LDIF file with automount information for all disks with
+a host and at least one user."""
 
-import sys
-import getopt
+import argparse
+from six import text_type
 
-import cerebrum_path
 from Cerebrum.Utils import Factory
-from Cerebrum.modules.LDIFutils import ldapconf
-from Cerebrum.modules.LDIFutils import entry_string
-from Cerebrum.modules.LDIFutils import ldif_outfile
-from Cerebrum.modules.LDIFutils import container_entry_string
+from Cerebrum.modules.LDIFutils import (ldapconf,
+                                        entry_string,
+                                        ldif_outfile,
+                                        container_entry_string)
+
+logger = Factory.get_logger("cronjob")
+
 
 def generate_automount(f):
     db = Factory.get('Database')()
-    co = Factory.get('Constants')(db)
     d = Factory.get('Disk')(db)
     h = Factory.get('Host')(db)
 
@@ -47,8 +47,6 @@ def generate_automount(f):
         if disk['host_id'] not in hosts:
             hosts.append(disk['host_id'])
     h_id2name = {}
-    # TBD: any point in filtering? does it just consume more resources than
-    # listing all hosts? 
     for host in h.search(host_id=hosts):
         h_id2name[host['host_id']] = host['name']
 
@@ -65,58 +63,49 @@ def generate_automount(f):
 
     for path in paths:
         entry = {}
-        entry['objectClass'] = ['top','automount']
-        dn = "cn=%s,%s" % ("/%s/%s" % (path[0], path[1]), 
-                          ldapconf('AUTOMOUNT_MASTER', 'dn', None))
-        entry['automountInformation'] = "ldap:ou=auto.%s-%s,%s" %(path[1],path[0],
-                                                              ldapconf('AUTOMOUNT', 'dn', None))
+        entry['objectClass'] = ['top', 'automount']
+        dn = "cn={},{}".format(
+            "/{}/{}".format(path[0], path[1]),
+            ldapconf('AUTOMOUNT_MASTER', 'dn', None))
+        entry['automountInformation'] = "ldap:ou=auto.{}-{},{}".format(
+            path[1], path[0], ldapconf('AUTOMOUNT', 'dn', None))
         f.write(entry_string(dn, entry))
 
         entry = {}
-        entry['objectClass'] = ['top','automountMap']
-        dn = "ou=auto.%s-%s,%s" % (path[1], path[0], 
-                                   ldapconf('AUTOMOUNT', 'dn', None))
+        entry['objectClass'] = ['top', 'automountMap']
+        dn = "ou=auto.{}-{},{}".format(
+            path[1], path[0], ldapconf('AUTOMOUNT', 'dn', None))
         f.write(entry_string(dn, entry))
 
         entry = {}
-        entry['objectClass'] = ['top','automount']
-        dn = "cn=/,ou=auto.%s-%s,%s" % (path[1], path[0], 
-                                         ldapconf('AUTOMOUNT', 'dn', None))
+        entry['objectClass'] = ['top', 'automount']
+        dn = "cn=/,ou=auto.{}-{},{}".format(
+            path[1], path[0], ldapconf('AUTOMOUNT', 'dn', None))
         dns = 'uio.no'
         if path[0] == 'ifi':
             dns = 'ifi.uio.no'
-        entry['automountInformation'] = "-fstype=nfs,tcp,vers=3,rw,intr,hard,nodev,nosuid,noacl %s.%s:/%s/%s/&" % (h_id2name[paths[path]], dns, path[0], path[1])
+        automount_opts = ("-fstype=nfs,tcp,vers=3,rw,intr,hard,nodev,"
+                          "nosuid,noacl {}.{}:/{}/{}/&")
+        entry['automountInformation'] = automount_opts.format(
+            h_id2name[paths[path]], dns, path[0], path[1])
         f.write(entry_string(dn, entry))
 
+
 def main():
-    global logger
-    logger = Factory.get_logger("cronjob")
-    ofile = None
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "ho:",("help","outfile="))
-    except getopt.GetoptError, e:
-        usage(str(e))
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        '-o', '--output',
+        type=text_type,
+        dest='output',
+        help='output file')
+    args = parser.parse_args()
 
-    if args:
-        usage("Invalid arguments: " + " ".join(args))
-    for opt, val in opts:
-        if opt in ("-o", "--outfile"):
-            ofile = val
-        else:
-            usage()
-
-    output_encoding = "utf-8"
-    f = ldif_outfile('AUTOMOUNT', ofile)
+    f = ldif_outfile('AUTOMOUNT', args.output)
+    logger.info('Starting automount export to %s', f.name)
     f.write(container_entry_string('AUTOMOUNT'))
     generate_automount(f)
     f.close()
-
-
-def usage(err=0):
-    if err:
-        print >>sys.stderr, err
-    print >>sys.stderr, __doc__
-    sys.exit(bool(err))
+    logger.info('Done')
 
 
 if __name__ == '__main__':
