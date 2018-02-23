fee3604e9998d43339f3d53d9ad9388619eabe3f
Jonas Braathen <jonas.braathen@usit.uio.no>
2018-02-23 19:02:23 +0100

    CRB-2431: Unicode support for virthome generate_ldif

--- Cerebrum/modules/virthome/LDIFHelper.py
+++ Cerebrum/modules/virthome/LDIFHelper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-# Copyright 2007 University of Oslo, Norway
+# Copyright 2007-2018 University of Oslo, Norway
 #
 # This file is part of Cerebrum.
 #
@@ -20,35 +20,16 @@
 """ LDIFHelper contains an object that select users+groups for export, and
 generate structured data for LDIFutils """
 
-import cerebrum_path
-import cereconf
-
 from Cerebrum.Utils import Factory
 from Cerebrum.modules.LDIFutils import ldapconf
 
-def object2utf8(obj):
-    """ Force all string-like values in obj to be utf-8 encoded. """
-    if isinstance(obj, str):
-        return unicode(obj, "latin-1").encode("utf-8")
-    if isinstance(obj, unicode):
-        return obj.encode("utf-8")
-    if isinstance(obj, (tuple, list, set)):
-        return type(obj)(object2utf8(x) for x in obj)
-    if isinstance(obj, dict):
-        return dict((object2utf8(key), object2utf8(obj[key]))
-                    for key in obj)
-    return obj
-# end object2utf8
 
 class LDIFHelper(object):
     """ Utility class for common functionality in LDIF exports. """
 
     def __init__(self, logger):
         """ Fetches all users and groups with the required spreads to qualify
-        for LDAP export 
-        
-        @type logger: Cerebrum.Logger
-        """
+        for LDAP export. """
 
         self.db = Factory.get("Database")()
         self.const = Factory.get("Constants")(self.db)
@@ -58,22 +39,16 @@ class LDIFHelper(object):
         # former due to data precaching.
         self.groups = self._load_groups()
         self.users = self._load_users()
-    # end __init__
-
 
     def _uname2dn(self, uname):
         return ",".join(("uid=" + uname, ldapconf("USER", "dn")))
-    # end _uname2dn
-
 
     def _gname2dn(self, gname):
         return ",".join(("cn=" + gname, ldapconf("GROUP", "dn")))
-    # _gname2dn
-
 
     def _load_groups(self):
         """Cache a dict with group_id -> group_name for all LDAP-exportable
-        groups. 
+        groups.
 
         See L{_load_users} for a related method.
 
@@ -87,48 +62,47 @@ class LDIFHelper(object):
         group = Factory.get("Group")(self.db)
         spreads = tuple(self.const.human2constant(x)
                         for x in ldapconf("GROUP", "spreads", ()))
-        self.logger.debug("Collecting groups for LDAP export. "
-                     "Spreads: %s", ", ".join(str(x) for x in spreads))
+        self.logger.debug(
+            "Collecting groups for LDAP export. "
+            "Spreads: %s", ", ".join(str(x) for x in spreads))
         return dict((x["group_id"], x)
                     for x in group.search(spread=spreads))
-    # end _load_groups
-    
 
     def _load_users(self):
         """Cache enough user information for the export to progress."""
-
-
         account = Factory.get("Account")(self.db)
         spreads = tuple(self.const.human2constant(x)
                         for x in ldapconf("USER", "spreads", ()))
-        self.logger.debug("Collecting users for LDAP export. "
-                     "Spreads: %s", ", ".join(str(x) for x in spreads))
+        self.logger.debug(
+            "Collecting users for LDAP export. "
+            "Spreads: %s", ", ".join(str(x) for x in spreads))
         users = dict()
         account = Factory.get("Account")(self.db)
         for spread in spreads:
             for row in account.search(spread=spread):
-                users[row["account_id"]] = {"uname": row["name"],
-                                            "np_type": row["np_type"],}
+                users[row["account_id"]] = {
+                    "uname": row["name"],
+                    "np_type": row["np_type"],
+                }
 
         users = self._get_contact_info(users)
         users = self._get_password_info(users)
         users = self._get_membership_info(users)
         return users
-    # end _load_users
-
 
     def _get_contact_info(self, users):
         """Update users with name and e-mail data."""
-
         account = Factory.get("Account")(self.db)
-        contact2tag = {self.const.virthome_contact_email: "mail",
-                       self.const.human_first_name: "givenName",
-                       self.const.human_last_name: "sn",}
-                
+        contact2tag = {
+            self.const.virthome_contact_email: "mail",
+            self.const.human_first_name: "givenName",
+            self.const.human_last_name: "sn",
+        }
+
         self.logger.debug("Collecting email/name info for LDAP export")
         for eci in account.list_contact_info(
-                       source_system=self.const.system_virthome,
-                       contact_type=tuple(contact2tag)):
+                source_system=self.const.system_virthome,
+                contact_type=tuple(contact2tag)):
             account_id = eci["entity_id"]
             if account_id not in users:
                 continue
@@ -157,33 +131,24 @@ class LDIFHelper(object):
             vals["givenName"] = first
             vals["sn"] = last
             vals["cn"] = full
-            
         return users
-    # end _get_contact_info
-
 
     def _get_password_info(self, users):
         """Collect md5 hashes for VA-s."""
-
         self.logger.debug("Collecting password information")
         account = Factory.get("Account")(self.db)
         for row in account.list_account_authentication(
-                              auth_type=self.const.auth_type_md5_crypt):
+                auth_type=self.const.auth_type_md5_crypt):
             account_id = row["account_id"]
             if account_id not in users:
                 continue
-
             if users[account_id]["np_type"] != self.const.virtaccount_type:
                 continue
-            
             users[account_id]["userPassword"] = ("{crypt}" + row["auth_data"],)
         return users
-    # end _get_password_info
-
 
     def _get_membership_info(self, users):
         """Collect group memberships information."""
-        
         group = Factory.get("Group")(self.db)
         self.logger.debug("Collecting user membership information")
 
@@ -200,29 +165,23 @@ class LDIFHelper(object):
             gname = self._gname2dn(self.groups[group_id]["name"])
             users[account_id].setdefault("uioMemberOf", list()).append(gname)
         return users
-    # end _get_membership_info
-
 
     def yield_groups(self):
-        """Generate group dicts with all LDAP-relevant information.
-        """
-
+        """Generate group dicts with all LDAP-relevant information."""
         group = Factory.get("Group")(self.db)
         for group_id in self.groups:
             gi = self.groups[group_id]
             group_name = gi["name"]
-            entry = {"dn": (self._gname2dn(group_name),),
-                     "cn": (group_name,),
-                     "objectClass": ldapconf("GROUP", "objectClass"),
-                     "description": (gi["description"],),}
+            entry = {
+                "dn": (self._gname2dn(group_name),),
+                "cn": (group_name,),
+                "objectClass": ldapconf("GROUP", "objectClass"),
+                "description": (gi["description"],),
+            }
             entry.update(self._get_member_info(group_id, group))
             if not entry.get("member"):
                 continue
-
-            entry = object2utf8(entry)
             yield entry
-    # end _extend_group_information
-
 
     def _get_member_info(self, group_id, group):
         """ Retrieve all members of a group.
@@ -233,16 +192,12 @@ class LDIFHelper(object):
           A dict with one entry, 'member' -> sequence of members in the
           respective group.
         """
-
-        # TBD: Maybe this ought to be cached? 
         members = tuple(self._uname2dn(self.users[x["member_id"]]["uname"])
-                                for x in group.search_members(group_id=group_id)
-                                if x["member_id"] in self.users)
+                        for x in group.search_members(group_id=group_id)
+                        if x["member_id"] in self.users)
         if members:
             return {"member": members}
         return {}
-    # end _get_member_info
-
 
     def yield_users(self):
         """ Yield all users qualified for export to LDAP. """
@@ -251,20 +206,18 @@ class LDIFHelper(object):
             if not isinstance(attrs, (list, set, tuple)):
                 return (attrs,)
             return attrs
-        
+
         for user_id in self.users:
             attrs = self.users[user_id]
-            tmp = {"dn": (self._uname2dn(attrs["uname"]),),
-                   "uid": (attrs["uname"],),
-                   "eduPersonPrincipalName": (attrs["uname"],),
-                   "mail": (attrs["mail"],),
-                   "objectClass": ldapconf("USER", "objectClass"),}
-
-            for key in ("cn", "sn", "givenName", "userPassword", "uioMemberOf",):
+            entry = {
+                "dn": (self._uname2dn(attrs["uname"]),),
+                "uid": (attrs["uname"],),
+                "eduPersonPrincipalName": (attrs["uname"],),
+                "mail": (attrs["mail"],),
+                "objectClass": ldapconf("USER", "objectClass"),
+            }
+            for key in ("cn", "sn", "givenName", "userPassword",
+                        "uioMemberOf",):
                 if key in attrs:
-                    tmp[key] = _mangle(attrs[key])
-
-            tmp = object2utf8(tmp)
-            yield tmp
-    # end generate_users
-# end UserLDIF
+                    entry[key] = _mangle(attrs[key])
+            yield entry
