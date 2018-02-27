f3cd206bbd4b4634242dc7260efed909d3fd169d
cerebrum@cleomedes.uit.no <bas_admin@uit.no>
2018-02-27 09:36:43 +0100

    added prefix to phonenumber, see changelog for details

--- import_FS.py
+++ import_FS.py
@@ -321,10 +321,16 @@ def register_cellphone(person, person_info):
     for key in person_info:
         for dct in person_info[key]:
             if phone_selector in dct:
-                if phone_country in dct or phone_region in dct:
-                    logger.debug('Skipping phone for %s', fnr)
-                    break
-                numbers.add(dct[phone_selector].strip())
+                phone = (dct.get(phone_region) or '') + dct[phone_selector]
+                if dct.get(phone_country):
+                    phone = '+' + dct[phone_country] + phone
+                numbers.add(phone.strip().replace(' ', ''))
+                #logger.debug("will add mobile:%s for user:%s" % (phone.strip().replace(' ',''), fnr))
+
+                #if phone_country in dct or phone_region in dct:
+                #    logger.debug('Skipping phone for %s', fnr)
+                #    break
+                #numbers.add(dct[phone_selector].strip())
 
     if len(numbers) < 1:
         return
