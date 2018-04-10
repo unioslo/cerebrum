9d3c039ad1923e390a2d833f13bc742843807311
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2018-04-10 18:26:02 +0200

    Fix bofhd_email email_delete_forward_target
    Fixes a refactoring bug in Cerebrum.bofhd.bofhd_email where a call to a
    renamed helper function is not updated with the new helper function
    name.

--- Cerebrum/modules/bofhd/bofhd_email.py
+++ Cerebrum/modules/bofhd/bofhd_email.py
@@ -1323,7 +1323,7 @@ class BofhdEmailCommands(BofhdEmailBase):
 
         # Allow us to delete an address, even if it is malformed.
         lp, dom = self._split_email_address(address, with_checks=False)
-        ed = self._get_email_domain(dom)
+        ed = self._get_email_domain_from_str(dom)
         et, acc = self._get_email_target_and_account(address)
         self.ba.can_email_forward_create(operator.get_entity_id(), domain=ed)
         epat = Email.EmailPrimaryAddressTarget(self.db)
