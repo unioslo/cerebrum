2d56d198f568ba791d86d7f16f5238658ccbb39d
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2018-05-14 11:29:57 +0200

    Convert constants to unicode for uia, nih, nmh
    Issue: CRB-2483

--- Cerebrum/modules/no/hia/Constants.py
+++ Cerebrum/modules/no/hia/Constants.py
@@ -1,6 +1,6 @@
-# -*- coding: iso-8859-1 -*-
-
-# Copyright 2002-2011 University of Oslo, Norway
+# -*- coding: utf-8 -*-
+#
+# Copyright 2002-2018 University of Oslo, Norway
 #
 # This file is part of Cerebrum.
 #
@@ -17,20 +17,21 @@
 # You should have received a copy of the GNU General Public License
 # along with Cerebrum; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-
 """Access to Cerebrum code values.
 
 The Constants class defines a set of methods that should be used to
 get the actual database code/code_str representing a given Entity,
 Address, Gender etc. type."""
+from __future__ import unicode_literals
 
 from Cerebrum import Constants
-from Cerebrum.Constants import _AuthoritativeSystemCode, \
-                              _SpreadCode, \
-                              _QuarantineCode, \
-                              _ContactInfoCode, \
-                              _PersonAffiliationCode, \
-                              _PersonAffStatusCode
+from Cerebrum.Constants import (
+    _AuthoritativeSystemCode,
+    _SpreadCode,
+    _QuarantineCode,
+    _PersonAffiliationCode,
+    _PersonAffStatusCode,
+)
 from Cerebrum.modules.no.Constants import ConstantsHigherEdu
 from Cerebrum.modules.no.Constants import ConstantsUniversityColleges
 from Cerebrum.modules.PosixUser import _PosixShellCode
@@ -38,43 +39,45 @@ from Cerebrum.modules.EntityTrait import _EntityTraitCode
 from Cerebrum.modules.Email import _EmailSpamLevelCode
 from Cerebrum.modules.Email import _EmailSpamActionCode
 
+
 class Constants(Constants.Constants):
     system_fs_derived = _AuthoritativeSystemCode('FS-auto',
                                                  'Utledet av FS data')
     system_pbx = _AuthoritativeSystemCode('PBX', 'PBX')
 
-## AFFILIATIONS FOR ANSATTE
+    # AFFILIATIONS FOR ANSATTE
     affiliation_ansatt = ConstantsHigherEdu.affiliation_ansatt
     affiliation_status_ansatt_manuell = _PersonAffStatusCode(
         affiliation_ansatt, 'ans_manuell', 'Ansatt, manuell import')
     affiliation_status_ansatt_primaer = _PersonAffStatusCode(
-        affiliation_ansatt, 'primaer', 'Primærtilknytning for SAP ansatte')
+        affiliation_ansatt, 'primaer', 'PrimÃ¦rtilknytning for SAP ansatte')
 
-## AFFILIATIONS FOR STUDENTER
+    # AFFILIATIONS FOR STUDENTER
     affiliation_student = ConstantsHigherEdu.affiliation_student
     affiliation_status_student_manuell = _PersonAffStatusCode(
         affiliation_student, 'stud_manuell', 'Student, manuell import')
     affiliation_status_student_tilbud = _PersonAffStatusCode(
         affiliation_student, 'tilbud', 'Student, tilbud')
 
-## AFFILIATIONS FOR ASSOSIERTE PERSONER
+    # AFFILIATIONS FOR ASSOSIERTE PERSONER
     affiliation_tilknyttet = ConstantsUniversityColleges.affiliation_tilknyttet
     affiliation_status_tilknyttet_feide = _PersonAffStatusCode(
         affiliation_tilknyttet, 'feide',
-        'Personer uten reg. i kildesystem som må ha tilgang til FEIDE-baserte tjenester')
+        'Personer uten reg. i kildesystem som mÃ¥ ha tilgang til '
+        'FEIDE-baserte tjenester')
 
     affiliation_status_tilknyttet_ekstern = _PersonAffStatusCode(
         affiliation_tilknyttet, 'ekstern',
         'Registrert i HR, ekstern tilsatt')
 
-## AFFILIATIONS FOR ANDRE
+    # AFFILIATIONS FOR ANDRE
     affiliation_manuell = _PersonAffiliationCode(
         'MANUELL',
-        ('Tilknyttet UiA uten å være registrert i et av de'
+        ('Tilknyttet UiA uten Ã¥ vÃ¦re registrert i et av de'
          ' autoritative kildesystemene'))
     affiliation_status_manuell_ekstern = _PersonAffStatusCode(
         affiliation_manuell, 'ekstern',
-        'Eksternt tilknyttet person, når spesifikke kategorier ikke passer')
+        'Eksternt tilknyttet person, nÃ¥r spesifikke kategorier ikke passer')
     affiliation_status_manuell_sia = _PersonAffStatusCode(
         affiliation_manuell, 'sia',
         'Person tilknyttet Studentsamskipnaden i Agder')
@@ -100,13 +103,13 @@ class Constants(Constants.Constants):
         'Ansatt som ikke er lagt inn i SAP. En midlertidig status for folk')
     affiliation_status_manuell_gjest_ikke_epost = _PersonAffStatusCode(
         affiliation_manuell, 'gjest_no_epost',
-	'Gjesteopphold som ansatt ved UiA. Skal ikke ha epost')
+        'Gjesteopphold som ansatt ved UiA. Skal ikke ha epost')
     affiliation_status_manuell_gjest_student = _PersonAffStatusCode(
         affiliation_manuell, 'gjest_student',
-	'Gjesteopphold for student ved UiA. Epost skal tildeles')
+        'Gjesteopphold for student ved UiA. Epost skal tildeles')
     affiliation_status_manuell_gjest_student_ikke_epost = _PersonAffStatusCode(
-	affiliation_manuell, 'gj_st_no_epost',
-	'Gjesteopphold for student ved UiA. Epost skal ikke tildeles')
+        affiliation_manuell, 'gj_st_no_epost',
+        'Gjesteopphold for student ved UiA. Epost skal ikke tildeles')
 
     affiliation_upersonlig = _PersonAffiliationCode(
         'UPERSONLIG', 'Fellesbrukere, samt andre brukere uten eier')
@@ -117,10 +120,10 @@ class Constants(Constants.Constants):
     affiliation_upersonlig_pvare = _PersonAffStatusCode(
         affiliation_upersonlig, 'pvare', 'Programvarekonti')
     affiliation_upersonlig_studentforening = _PersonAffStatusCode(
-	affiliation_upersonlig, 'studorg',
-	'Studentforening eller -aktivitet ved UiA')
+        affiliation_upersonlig, 'studorg',
+        'Studentforening eller -aktivitet ved UiA')
 
-## DEFINISJON AV SHELL
+    # DEFINISJON AV SHELL
     # We override the default Cerebrum paths for shells, thus this
     # file should appear before PosixUser in cereconf.CLASS_CONSTANTS
     posix_shell_bash = _PosixShellCode('bash', '/bin/bash')
@@ -216,35 +219,29 @@ class Constants(Constants.Constants):
         'acc@forward', Constants.Constants.entity_account,
         'Account contains only email forward and not a mailbox')
 
-    ## Definisjon av traits
+    # Definisjon av traits
     trait_accept_nondisc = _EntityTraitCode(
         'acc_non_disc',
         Constants.Constants.entity_person,
-        "Trait marking a person who has accepted a non-disclosure agreement with UiA.")
+        "Trait marking a person who has accepted a non-disclosure agreement "
+        "with UiA.")
     trait_reject_nondisc = _EntityTraitCode(
         'rej_non_disc',
         Constants.Constants.entity_person,
-        "Trait marking a person who has rejected a non-disclosure agreement with UiA.")
+        "Trait marking a person who has rejected a non-disclosure agreement "
+        "with UiA.")
     trait_accept_rules = _EntityTraitCode(
         'accept_rules',
         Constants.Constants.entity_person,
-        "Trait marking a person who has accepted terms and rule for use og IT services at UiA.")
+        "Trait marking a person who has accepted terms and rule for use of "
+        "IT services at UiA.")
 
-## Kommenteres ut foreløpig, er usikkert om vi skal ha dem
-
-##     spread_hia_fs = _SpreadCode(
-##         'FS@uia', Constants.Constants.entity_account,
-##         'Account exported to FS')
-##     spread_hia_sap = _SpreadCode(
-##         'SAP@uia', Constants.Constants.entity_account,
-##         'Account exported to SAP')
-
-## KARANTENEGRUPPER
+    # KARANTENEGRUPPER
     quarantine_slutta = _QuarantineCode('slutta', 'Personen har slutta')
     quarantine_permisjon = _QuarantineCode('permisjon',
                                            'Brukeren har permisjon')
     quarantine_autoekstern = _QuarantineCode('autoekstern',
-                                             'Ekstern konto gått ut på dato')
+                                             'Ekstern konto gÃ¥tt ut pÃ¥ dato')
 
     email_spam_level_none = _EmailSpamLevelCode(
         'no_filter', 9999, "No email will be filtered as spam")
@@ -260,4 +257,3 @@ class Constants(Constants.Constants):
         'spamfolder', "Deliver spam to a separate IMAP folder")
     email_spam_action_delete = _EmailSpamActionCode(
         'dropspam', "Reject messages classified as spam")
-
--- Cerebrum/modules/no/nmh/Constants.py
+++ Cerebrum/modules/no/nmh/Constants.py
@@ -1,6 +1,6 @@
-# -*- coding: iso-8859-1 -*-
-
-# Copyright 2006 University of Oslo, Norway
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006-2018 University of Oslo, Norway
 #
 # This file is part of Cerebrum.
 #
@@ -17,38 +17,40 @@
 # You should have received a copy of the GNU General Public License
 # along with Cerebrum; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-
 """Access to Cerebrum code values.
 
 The Constants class defines a set of methods that should be used to
 get the actual database code/code_str representing a given Entity,
 Address, Gender etc. type."""
+from __future__ import unicode_literals
 
 from Cerebrum import Constants
-from Cerebrum.Constants import _SpreadCode, \
-                               _PersonAffiliationCode, \
-                               _ContactInfoCode, \
-                               _PersonAffStatusCode
+from Cerebrum.Constants import (
+    _SpreadCode,
+    _PersonAffiliationCode,
+    _PersonAffStatusCode,
+)
 from Cerebrum.modules.EntityTrait import _EntityTraitCode
 
 
 class Constants(Constants.Constants):
-    ## employees - affiliation definition
-    affiliation_ansatt = _PersonAffiliationCode('ANSATT', 'Ansatt ved NMH')
-    ## affiliations for employees
-    affiliation_status_ansatt_ovlaerer = _PersonAffStatusCode(
-        affiliation_ansatt, 'ans_ovlaerer', 'Ansatt ved NMH, øvingslærer.')
 
-    ## affiliations for others
+    # employees - affiliation definition
+    affiliation_ansatt = _PersonAffiliationCode('ANSATT', 'Ansatt ved NMH')
+    # affiliations for employees
+    affiliation_status_ansatt_ovlaerer = _PersonAffStatusCode(
+        affiliation_ansatt, 'ans_ovlaerer', 'Ansatt ved NMH, Ã¸vingslÃ¦rer.')
+
+    # affiliations for others
     affiliation_manuell = _PersonAffiliationCode(
         'MANUELL',
-        ('Tilknyttet NMH uten å være registrert i et av de'
+        ('Tilknyttet NMH uten Ã¥ vÃ¦re registrert i et av de'
          ' autoritative kildesystemene'))
     affiliation_status_manuell_inaktiv = _PersonAffStatusCode(
         affiliation_manuell, 'inaktiv',
         'Person uten ekte tilknytning til NMH. Bruk med forsiktighet!')
 
-    ## Spread definitions - user related
+    # Spread definitions - user related
     spread_ad_account = _SpreadCode(
         'account@ad', Constants.Constants.entity_account,
         'Brukeren kjent i AD ved NMH')
@@ -56,7 +58,7 @@ class Constants(Constants.Constants):
         'account@exchange', Constants.Constants.entity_account,
         'Brukeren kjent i AD ved NMH')
 
-    ## Spread definitions - group related
+    # Spread definitions - group related
     spread_ad_group = _SpreadCode(
         'group@ad', Constants.Constants.entity_group,
         'Gruppe kjent i AD ved NMH')
@@ -64,23 +66,23 @@ class Constants(Constants.Constants):
         'group@lms', Constants.Constants.entity_group,
         'Gruppe kjent i LMSen til NMH')
 
-    ## Spread definitions - person relates
+    # Spread definitions - person relates
     spread_adgang_person = _SpreadCode(
         'person@adgang', Constants.Constants.entity_person,
         'Person kjent i adgangssystemet til NMH')
 
-    ## Traits for fagområde
+    # Traits for fagomrÃ¥de
     trait_fagomrade_fagfelt = _EntityTraitCode(
             'fagfelt',
             Constants.Constants.entity_person,
-            'Trait registering part of fagområde: fagfelt')
+            'Trait registering part of fagomrÃ¥de: fagfelt')
     trait_fagomrade_instrument = _EntityTraitCode(
             'instrument',
             Constants.Constants.entity_person,
-            'Trait registering part of fagområde: instrument')
+            'Trait registering part of fagomrÃ¥de: instrument')
 
-    ## Trait for fagmiljø
+    # Trait for fagmiljÃ¸
     trait_fagmiljo = _EntityTraitCode(
             'fagmiljo',
             Constants.Constants.entity_person,
-            'Trait registering fagmiljø')
+            'Trait registering fagmiljÃ¸')
