d0f3c09b2f0029cd9d9dbbf6864b1bb10d6cd799
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2018-05-15 18:13:49 +0200

    Rewrites generate_kvernfil to support encodings
    This is a rewrite of the ØFK script generate_kvernfil.py, which adds
    unicode support and configurable encoding, as well as proper CSV
    escaping.
    
    Issue: CRB-2483

--- contrib/no/Indigo/generate_kvernfil.py
+++ contrib/no/Indigo/generate_kvernfil.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-# Copyright 2006 University of Oslo, Norway
+#
+# Copyright 2006-2018 University of Oslo, Norway
 #
 # This file is part of Cerebrum.
 #
@@ -18,143 +18,269 @@
 # You should have received a copy of the GNU General Public License
 # along with Cerebrum; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-
+import argparse
+import csv
+import io
+import logging
+import os
 import sys
-import getopt
 
-import cerebrum_path
-import cereconf
+import six
 
+import Cerebrum.logutils
+import Cerebrum.logutils.options
+import Cerebrum.utils.argutils
 from Cerebrum.Utils import Factory
+from Cerebrum.modules import Email
 from Cerebrum.utils.atomicfile import SimilarSizeWriter
-from Cerebrum.modules import Email, LDIFutils
 
-LDIFutils.needs_base64 = LDIFutils.needs_base64_safe
-LDIFutils.base64_attrs.update({
-    'userpassword': 0, 'ofkpassword': 0, 'userpasswordct': 0})
+logger = logging.getLogger(__name__)
 
 
-def get_account_info():
-    global a_id2email, p_id2name, p_id2fnr, a_id2auth
-    global p_id2a_id, ou_id2name
-    global acc_spreads, ou_spreads
+class CsvDialect(csv.excel):
+    """Specifying the CSV output dialect the script uses.
 
-    acc_spreads = []
-    ou_spreads = []
-    a_id2email = dict()
-    for row in et.list_email_target_primary_addresses():
-        a_id2email[int(row['target_entity_id'])] = "%s@%s" % (row['local_part'],
-                                                              row['domain'])
+    See the module `csv` for a description of the settings.
 
-    p_id2name = p.getdict_persons_names(source_system=co.system_cached,
-                                        name_types=(co.name_first,co.name_last))
-    p_id2fnr = dict()
-    for row in p.list_external_ids(id_type=co.externalid_fodselsnr):
-        p_id2fnr[int(row['entity_id'])] = row['external_id']
-
-    a_id2auth = dict()
-    for row in ac.list_account_authentication(auth_type=(co.auth_type_md4_nt,
-                                                         co.auth_type_plaintext,
-                                                         co.auth_type_ssha)):
-        aid = int(row['account_id'])
-        if row['method'] is None:
-            continue
-        a_id2auth.setdefault(aid, [])
-        if len(a_id2auth[aid]) != 2:
-            a_id2auth[aid].insert(0, row['entity_name'])
-            a_id2auth[aid].insert(1, dict())
-        a_id2auth[aid][1].setdefault(int(row['method']), row['auth_data'])
-
-    p_id2a_id = dict()
-    for row in ac.list_accounts_by_type(primary_only=True):
-        p_id2a_id[int(row['person_id'])] = int(row['account_id'])
-
-    ou_id2name = dict((r["entity_id"], r["name"])
-                      for r in ou.search_name_with_language(entity_type=co.entity_ou,
-                                     name_variant=co.ou_name_acronym,
-                                     name_language=co.language_nb))
-
-    for row in ac.list_all_with_spread(co.spread_oid_acc):
-        acc_spreads.append(row['entity_id'])
-
-    for row in ou.list_all_with_spread(co.spread_oid_ou):
-        ou_spreads.append(row['entity_id'])
-
-def process_txt_file(file):
-    users_ou = dict()
-
-    for row in p.list_affiliations():
-        id = int(row['person_id'])
-        aff = int(row['affiliation'])
-        ou_id = int(row['ou_id'])
-        if not p_id2a_id.has_key(id):
-            # no user
-            logger.info("No user found for '%s'" % id)
-            continue
-        a_id = p_id2a_id[id]
-        if not (a_id2auth.has_key(a_id) and a_id2auth[a_id][0]):
-            # No username
-            logger.warning("ptf: No user found for person '%s'" % id)
-            continue
-        if not p_id2fnr.has_key(id):
-            logger.warning("Fnr not found for '%s'" % id)
-            continue
-        uname = a_id2auth[a_id][0]
-        pwd = ""
-        if a_id2auth[a_id][1] is not None:
-            pwd = a_id2auth[a_id][1][int(co.auth_type_plaintext)]
-        if id not in p_id2name or \
-               (int(co.name_first) not in p_id2name[id] or \
-                int(co.name_last) not in p_id2name[id]):
-            logger.warning("Names not found for person id='%s'" % id)
-            continue
-        if not ou_id2name.has_key(ou_id):
-            logger.warning("OU name not found for '%s'" % ou_id)
-            continue
-        p_names = p_id2name[id]
-        first = p_names[int(co.name_first)]
-        last = p_names[int(co.name_last)]
-        if not a_id2email.has_key(a_id):
-            logger.warning("Mail-addr not found for '%s', '%s'" % (id, uname))
-            continue
-        txt = '$'.join((p_id2fnr[id], uname, pwd,
-                        a_id2email[a_id],
-                        ou_id2name[ou_id], first, last)) + '\n'
-        file.write(txt)
-        if ou_id in ou_spreads and a_id in acc_spreads:
-            users_ou.setdefault(ou_id2name[ou_id], {}).setdefault(aff, []).append(uname)
-    return users_ou
+    """
+    delimiter = '$'
+    lineterminator = '\n'
 
 
-def main():
-    global db, co, ac, p, ou, et, logger
+class CsvUnicodeWriter:
+    """ Unicode-compatible CSV writer.
 
-    logger = Factory.get_logger("cronjob")
+    Adopted from https://docs.python.org/2/library/csv.html
+    """
+
+    def __init__(self, stream, fieldnames, dialect=csv.excel, **kwds):
+        self.queue = io.BytesIO()
+        self.writer = csv.DictWriter(self.queue, fieldnames, dialect=dialect,
+                                     **kwds)
+        self.stream = stream
+
+    def writeheader(self):
+        return self.writer.writeheader()
+
+    def writerow(self, row):
+        # Write utf-8 encoded output to queue
+        self.writer.writerow(dict((k, six.text_type(v).encode("utf-8"))
+                                  for k, v in row.items()))
+        data = self.queue.getvalue()
+
+        # Read formatted CSV data from queue, re-encode and write to stream
+        data = data.decode("utf-8")
+        self.stream.write(data)
+        self.queue.truncate(0)
+        self.queue.seek(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
+
+def format_email(row):
+    return '{local_part}@{domain}'.format(**dict(row))
+
+
+class PersonLookup(object):
+
+    def __init__(self, db):
+        """ Cache data for this iterable object. """
+        co = Factory.get('Constants')(db)
+        ac = Factory.get('Account')(db)
+        pe = Factory.get('Person')(db)
+        ou = Factory.get('OU')(db)
+        et = Email.EmailTarget(db)
+
+        self.db = db
+
+        logger.debug("caching spreads ...")
+        self.account_spreads = set((
+            row['entity_id']
+            for row in ac.list_all_with_spread(co.spread_oid_acc)))
+        self.ou_spreads = set((
+            row['entity_id']
+            for row in ou.list_all_with_spread(co.spread_oid_ou)))
+
+        logger.debug("... email addresses ...")
+        self.account_id_to_email = dict(
+            (int(row['target_entity_id']),
+             format_email(row))
+            for row in et.list_email_target_primary_addresses())
+
+        logger.debug("... names ...")
+        self.person_id_to_name = pe.getdict_persons_names(
+            source_system=co.system_cached,
+            name_types=(co.name_first, co.name_last))
+
+        logger.debug("... external ids ...")
+        self.person_id_to_extid = dict(
+            (int(row['entity_id']), row['external_id'])
+            for row in pe.list_external_ids(id_type=co.externalid_fodselsnr))
+
+        logger.debug("... auth data ...")
+        self.account_id_to_auth = dict()
+        for row in ac.list_account_authentication(
+                auth_type=(co.auth_type_md4_nt,
+                           co.auth_type_plaintext,
+                           co.auth_type_ssha)):
+            account_id = int(row['account_id'])
+            if row['method'] is None:
+                continue
+            if account_id not in self.account_id_to_auth:
+                self.account_id_to_auth[account_id] = [row['entity_name'],
+                                                       dict()]
+            self.account_id_to_auth[account_id][1].setdefault(
+                int(row['method']),
+                row['auth_data'])
+
+        logger.debug("... account data ...")
+        self.person_id_to_account_id = dict(
+            (int(row['person_id']), int(row['account_id']))
+            for row in ac.list_accounts_by_type(primary_only=True))
+
+        logger.debug("... ou data ...")
+        self.ou_id_to_name = dict(
+            (r["entity_id"], r["name"])
+            for r in ou.search_name_with_language(
+                entity_type=co.entity_ou,
+                name_variant=co.ou_name_acronym,
+                name_language=co.language_nb))
+        logger.debug("... done caching data")
+
+    def _person_iterator(self):
+        """ Returns a generator that iterates over exportable persons. """
+        co = Factory.get('Constants')(self.db)
+        pe = Factory.get('Person')(self.db)
+
+        for row in pe.list_affiliations():
+            person_id = int(row['person_id'])
+            ou_id = int(row['ou_id'])
+
+            account_id = self.person_id_to_account_id.get(person_id, None)
+
+            if account_id is None:
+                # no user
+                logger.info("No user found for %r", person_id)
+                continue
+
+            ext_id = self.person_id_to_extid.get(person_id, None)
+            if ext_id is None:
+                logger.warning("Fnr not found for %r", person_id)
+                continue
+
+            uname, auth_data = self.account_id_to_auth.get(account_id,
+                                                           [None, dict()])
+            if uname is None:
+                # No username
+                logger.warning("ptf: No user found for person %r", person_id)
+                continue
+
+            if auth_data:
+                pwd = auth_data[int(co.auth_type_plaintext)]
+            else:
+                pwd = ""
+
+            names = self.person_id_to_name.get(person_id, dict())
+            if not all(int(name_variant) in names
+                       for name_variant in (co.name_first, co.name_last)):
+                logger.warning("Names not found for person id=%r", person_id)
+                continue
+            first = names[int(co.name_first)]
+            last = names[int(co.name_last)]
+
+            ou_name = self.ou_id_to_name.get(ou_id, None)
+            if ou_name is None:
+                logger.warning("OU name not found for %r", ou_id)
+                continue
+
+            email_addr = self.account_id_to_email.get(account_id, None)
+            if email_addr is None:
+                logger.warning("Mail-addr not found for %r, %r",
+                               person_id, uname)
+                continue
+
+            yield {
+                'ext_id': ext_id,
+                'username': uname,
+                'password': pwd,
+                'email': email_addr,
+                'ou': ou_name,
+                'firstname': first,
+                'lastname': last,
+            }
+
+    def __iter__(self):
+        return iter(self._person_iterator())
+
+
+def write_csv_export(stream, iterator):
+    fields = ['ext_id', 'username', 'password',
+              'email', 'ou', 'firstname', 'lastname', ]
+    writer = CsvUnicodeWriter(stream,
+                              dialect=CsvDialect,
+                              fieldnames=fields)
+    count = 0
+    for count, user in enumerate(iterator, 1):
+        writer.writerow(user)
+    logger.info("Wrote %d users", count)
+
+
+DEFAULT_EXPORT_DIR = os.path.join(sys.prefix, 'var/cache/txt')
+DEFAULT_FILENAME = 'ofk.txt'
+DEFAULT_ENCODING = 'latin1'
+
+
+def writable_dir_type(value):
+    value = os.path.abspath(value)
+    if not all((os.path.isdir(value),
+                os.access(value, os.R_OK | os.X_OK))):
+        raise ValueError("not a writable directory")
+    return value
+
+
+def main(inargs=None):
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    # TODO: change to the actual filename...
+    parser.add_argument(
+        '-t', '--txt-path',
+        dest='export_dir',
+        type=writable_dir_type,
+        default=DEFAULT_EXPORT_DIR,
+        metavar='DIR',
+        help='Write export data to %(metavar)s')
+    parser.add_argument(
+        '-e', '--encoding',
+        dest='codec',
+        default=DEFAULT_ENCODING,
+        type=Cerebrum.utils.argutils.codec_type,
+        help="output file encoding, defaults to %(default)s")
+
+    Cerebrum.logutils.options.install_subparser(parser)
+    args = parser.parse_args(inargs)
+    Cerebrum.logutils.autoconf('cronjob', args)
+
+    logger.info('Start of script %s', parser.prog)
+    logger.debug("args: %r", args)
+
+    filename = os.path.join(args.export_dir, DEFAULT_FILENAME)
 
     db = Factory.get('Database')()
-    co = Factory.get('Constants')(db)
-    ac = Factory.get('Account')(db)
-    p = Factory.get('Person')(db)
-    ou = Factory.get('OU')(db)
-    et = Email.EmailTarget(db)
 
-    txt_path = "/cerebrum/var/cache/txt"
-
-    options, rest = getopt.getopt(sys.argv[1:],
-                                  "t:", ["txt-path=",])
-    for option, value in options:
-        if option in ("-t", "--txt-path"):
-            txt_path = value
-
-    # Load dicts with misc info.
-    get_account_info()
+    persons = PersonLookup(db)
 
     # Dump OFK info
-    f = SimilarSizeWriter("%s/ofk.txt" % txt_path, "w")
-    f.max_pct_change = 10
-    users = process_txt_file(f)
-    f.close()
+    with SimilarSizeWriter(filename,
+                           mode="w",
+                           encoding=args.codec.name) as f:
+        f.max_pct_change = 10
+        write_csv_export(f, persons)
+
+    logger.info('Report written to %s', filename)
+    logger.info('Done with script %s', parser.prog)
+
 
 if __name__ == '__main__':
     main()
-
