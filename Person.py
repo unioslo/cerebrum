6c9105b9c55c9761a96442de23ed1b916131c141
cerebrum@cleomedes.uit.no <bas_admin@uit.no>
2018-10-21 14:45:13 +0200

    updared to handle unicode

--- Person.py
+++ Person.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: iso-8859-1 -*-
+# -*- coding: utf-8 -*-
 
 # Copyright 2003, 2004 University of Oslo, Norway
 #
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Cerebrum; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-
+from __future__ import unicode_literals
 
 
 import cerebrum_path
