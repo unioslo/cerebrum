ef86d59c88dc00d94f2d152e39ebadf2439787e3
Andreas Ellewsen <andreas.ellewsen@usit.uio.no>
2019-04-02 16:51:21 +0200

    Fix PEP before doing anything
    Issue: CRB-2739

--- Cerebrum/modules/no/uit/access_SYSX.py
+++ Cerebrum/modules/no/uit/access_SYSX.py
@@ -16,115 +16,110 @@
 # You should have received a copy of the GNU General Public License
 # along with Cerebrum; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+"""
+Uit specific extension for Cerebrum. Read data from SystemX
+"""
 from __future__ import unicode_literals
+
 import io
-import time
 import os
 import sys
 import getopt
-import mx
-from mx import DateTime
-__doc__="""
-Uit specific extension for Cerebrum. Read data from SystemX
-"""
+import mx.DateTime
 
-import cerebrum_path
 import cereconf
-
 from Cerebrum.Utils import Factory
-from Cerebrum.modules.no.uit.EntityExpire import EntityExpiredError
 
 db = Factory.get('Database')()
 
-class SYSX:
 
-    _default_datafile = os.path.join(cereconf.DUMPDIR,'system_x','guest_data')
+class SYSX:
+    _default_datafile = os.path.join(cereconf.DUMPDIR, 'system_x',
+                                     'guest_data')
     _guest_host = cereconf.GUEST_HOST
     _guest_host_dir = cereconf.GUEST_HOST_DIR
     _guest_host_file = cereconf.GUEST_HOST_FILE
     _guest_file = cereconf.GUEST_FILE
-    today=str(mx.DateTime.today())
-    
+    today = str(mx.DateTime.today())
+
     sysxids = {}
     sysxfnrs = {}
-    SPLIT_CHAR=':'
+    SPLIT_CHAR = ':'
 
-    def __init__(self,data_file=None,update=False):
-                
+    def __init__(self, data_file=None, update=False):
+
         if data_file:
             self.sysx_data = data_file
         else:
             self.sysx_data = self._default_datafile
-        
+
         if update:
             self._update()
 
-
     def read_from_sysx(self):
-        url = "http://%s%s%s" % (
-            self._guest_host, 
+        url = "http://{}{}{}".format(
+            self._guest_host,
             self._guest_host_dir,
-            self._guest_host_file) 
+            self._guest_host_file)
         target_file = self.sysx_data
-        try:        
+        try:
             import urllib
-            fname,headers = urllib.urlretrieve(url,target_file)
-        except Exception,m:
-            print "Failed to get data from %s: reason: %s" % (url,m)
+            fname, headers = urllib.urlretrieve(url, target_file)
+        except Exception as m:
+            print("Failed to get data from {0}: reason: {1}".format(url, m))
             return 0
         else:
             return 1
 
-
     def load_sysx_data(self):
         data = []
-        file_handle = io.open(self.sysx_data,"r",encoding="utf-8")
+        file_handle = io.open(self.sysx_data, "r", encoding="utf-8")
         lines = file_handle.readlines()
         file_handle.close()
         for line in lines:
             line = line.rstrip()
-            #line = line.decode("UTF-8")
+            # line = line.decode("UTF-8")
             if not line or line.startswith('#'):
                 continue
-            #line = line.decode("UTF-8")
+            # line = line.decode("UTF-8")
             data.append(line)
         return data
 
-
-    def _prepare_data(self,data_list):
+    def _prepare_data(self, data_list):
         data_list = data_list.rstrip()
         try:
-            id,fodsels_dato,personnr,gender,fornavn,etternavn,ou, \
-            affiliation,affiliation_status,expire_date,spreads,hjemmel, \
-            kontaktinfo,ansvarlig_epost,bruker_epost,national_id, \
-            approved = data_list.split(self.SPLIT_CHAR) 
-        except ValueError,m:
-            self.logger.error("data_list:%s##%s" % (data_list,m))
+            id, fodsels_dato, personnr, gender, fornavn, etternavn, ou, \
+                affiliation, affiliation_status, expire_date, spreads,\
+                hjemmel, kontaktinfo, ansvarlig_epost, bruker_epost, \
+                national_id, approved = data_list.split(self.SPLIT_CHAR)
+        except ValueError as m:
+            self.logger.error("data_list:%s##%s", data_list, m)
             sys.exit(1)
         else:
 
             def fixname(name):
                 # Cerebrum populate expects iso-8859-1.
                 return name.strip()
-            
+
             ##########################
             # collect right stedkode #
             ##########################
             # lets check if the ou referenced as affiliation exists in cerebrum
-            query = "select new_ou_id from ou_history where old_ou_id='%s'" % (ou)
+            query = "select new_ou_id from ou_history where old_ou_id='%s'" % (
+                ou)
             new_ou = db.query(query)
-            if(len(new_ou) != 0):
-                #print ou,"-->",new_ou[0][0]
+            if len(new_ou) != 0:
+                # print ou,"-->",new_ou[0][0]
                 ou = "%s" % new_ou[0][0]
-                
+
             # Fix for people placed on top OU
             if ou == '0':
                 ou = '000000'
-            
+
             if spreads:
-                spreads=spreads.split(',')
+                spreads = spreads.split(',')
             else:
-                spreads=[]
+                spreads = []
             return {'id': id,
                     'fodsels_dato': fodsels_dato,
                     'personnr': personnr,
@@ -132,64 +127,63 @@ class SYSX:
                     'fornavn': fixname(fornavn.strip()),
                     'etternavn': fixname(etternavn.strip()),
                     'ou': ou,
-                    'affiliation':affiliation,
+                    'affiliation': affiliation,
                     'affiliation_status': affiliation_status.lower(),
-                    'expire_date':expire_date,
+                    'expire_date': expire_date,
                     'spreads': spreads,
-                    'ansvarlig_epost':ansvarlig_epost,
-                    'bruker_epost':bruker_epost,
+                    'ansvarlig_epost': ansvarlig_epost,
+                    'bruker_epost': bruker_epost,
                     'national_id': national_id,
-                    'approved':approved,
+                    'approved': approved,
                     'kontaktinfo': kontaktinfo,
-                    'hjemmel' : hjemmel }
-
+                    'hjemmel': hjemmel}
 
     def _update(self):
         return self.read_from_sysx()
 
-
-    def list(self,filter_expired=True,filter_approved=False):
+    def list(self, filter_expired=True, filter_approved=False):
         self._load_data(filter_expired=filter_expired)
 
-
-    def _load_data(self,update=False,filter_expired=True):
-        if update: self._update()
+    def _load_data(self, update=False, filter_expired=True):
+        if update:
+            self._update()
         for item in self.load_sysx_data():
             sysx_data = self._prepare_data(item)
             if filter_expired:
-                if sysx_data['expire_date']<self.today:
+                if sysx_data['expire_date'] < self.today:
                     continue
-            self.sysxids[sysx_data['id']]=sysx_data
+            self.sysxids[sysx_data['id']] = sysx_data
             if sysx_data['personnr']:
-                self.sysxfnrs[sysx_data['personnr']]=sysx_data                
-        
-        
+                self.sysxfnrs[sysx_data['personnr']] = sysx_data
+
+
 def usage():
-    print __doc__
-        
-def main():   
-    do_update=False
-    filter_expired=False
-    try:   
-        opts,args = getopt.getopt(sys.argv[1:],'uhf',
-            ['update','help','filter_expired'])
-    except getopt.GetoptError,m:
-        print "Unknown option: %s" % (m)
+    print(__doc__)
+
+
+def main():
+    do_update = False
+    filter_expired = False
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'uhf',
+                                   ['update', 'help', 'filter_expired'])
+    except getopt.GetoptError as m:
+        print("Unknown option: {}".format(m))
         usage()
-        
-    for opt,val in opts:
-        if opt in ('-h','--help'):
+
+    for opt, val in opts:
+        if opt in ('-h', '--help'):
             usage()
-        elif opt in ('-u','--update'):
+        elif opt in ('-u', '--update'):
             do_update = True
-        elif opt in ('-f','--filter_expired'):
+        elif opt in ('-f', '--filter_expired'):
             filter_expired = True
 
     sysx = SYSX(update=do_update)
     sysx.list(filter_expired=filter_expired)
-    print "SYS_IDs: %d" % len(sysx.sysxids)
-    print "SYS_Fnrs: %d" % len(sysx.sysxfnrs)
-    
+    print("SYS_IDs: {}".format(len(sysx.sysxids)))
+    print("SYS_Fnrs: {}".format(len(sysx.sysxfnrs)))
+
+
 if __name__ == '__main__':
     main()
-
--- contrib/no/uit/import_SYSX.py
+++ contrib/no/uit/import_SYSX.py
@@ -20,228 +20,238 @@
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 from __future__ import unicode_literals
+from __future__ import print_function
+
 import getopt
 import sys
 import os
-import mx
+import mx.DateTime
 
-progname = __file__.split(os.sep)[-1]
-__doc__="""
-    usage:: %s [-s|--source_file <filename>] [-u|--update] [-d|--dryrun]
-    -s file | --source_file file : source file containing information needed to create
-                                   person and user entities in cerebrum
-    -u      | --update_data      : updates the datafile containing guests from the guest database
-    --dryrun : do no commit changes to database
-""" % (progname)
-
-
-import cerebrum_path
 import cereconf
 from Cerebrum import Errors
 from Cerebrum.Utils import Factory
 from Cerebrum.modules.no import fodselsnr
-from Cerebrum.Constants import Constants
-from Cerebrum.modules.no.Stedkode import Stedkode
 from Cerebrum.modules.no.uit.access_SYSX import SYSX
 
 from Cerebrum.modules.no.uit.EntityExpire import EntityExpiredError
 
-SPLIT_CHAR=':'
-logger_name=cereconf.DEFAULT_LOGGER_TARGET
-logger=sysx=old_aff=None
-include_delete=True
-skipped=added=updated=unchanged=deletedaff=0
+progname = __file__.split(os.sep)[-1]
+__doc__ = """
+    usage:: %s [-s|--source_file <filename>] [-u|--update] [-d|--dryrun]
+    -s file | --source_file file : source file containing information needed to
+                                   create person and user entities in cerebrum
+    -u      | --update_data      : updates the datafile containing guests from
+                                   the guest database
+    --dryrun : do no commit changes to database
+""" % progname
 
-db=Factory.get('Database')()
+SPLIT_CHAR = ':'
+logger_name = cereconf.DEFAULT_LOGGER_TARGET
+logger = sysx = old_aff = None
+include_delete = True
+skipped = added = updated = unchanged = deletedaff = 0
+
+db = Factory.get('Database')()
 db.cl_init(change_program='import_SYSX')
-person=Factory.get('Person')(db)
-co=Factory.get('Constants')(db)
+person = Factory.get('Person')(db)
+co = Factory.get('Constants')(db)
+
 
 def _load_cere_aff():
-    cb_aff={}
-    pers=Factory.get("Person")(db)
+    cb_aff = {}
+    pers = Factory.get("Person")(db)
     for row in pers.list_affiliations(source_system=co.system_x):
-        k="%s:%s:%s" % (row['person_id'],row['ou_id'],row['affiliation'])
-        cb_aff[str(k)]=True
-    return(cb_aff)
+        k = "%s:%s:%s" % (row['person_id'], row['ou_id'], row['affiliation'])
+        cb_aff[str(k)] = True
+    return cb_aff
 
 
 def rem_old_aff():
-   global deletedaff
-   person = Factory.get("Person")(db)
-   grace=0
-   for k,v in old_aff.items():
-      if v:
-         ent_id,ou,affi = k.split(':')
-         person.clear()
-         person.find(int(ent_id))
-         for aff in person.list_affiliations(int(ent_id),
-                                         affiliation=int(affi),
-                                         ou_id=int(ou)):
-            last_date = aff['last_date']
-            end_grace_period = last_date + mx.DateTime.DateTimeDelta(grace)
-            if mx.DateTime.today() > end_grace_period:
-               logger.warn("Deleting sysX affiliation for person_id=%s,ou=%s,affi=%s last_date=%s,grace=%s" % (ent_id,ou,affi,last_date,grace))
-               person.delete_affiliation(ou, affi, co.system_x)
-               deletedaff+=1
+    global deletedaff
+    person = Factory.get("Person")(db)
+    grace = 0
+    for k, v in old_aff.items():
+        if v:
+            ent_id, ou, affi = k.split(':')
+            person.clear()
+            person.find(int(ent_id))
+            for aff in person.list_affiliations(int(ent_id),
+                                                affiliation=int(affi),
+                                                ou_id=int(ou)):
+                last_date = aff['last_date']
+                end_grace_period = last_date + mx.DateTime.DateTimeDelta(grace)
+                if mx.DateTime.today() > end_grace_period:
+                    logger.warn(
+                        "Deleting sysX affiliation for person_id=%s,"
+                        "ou=%s,affi=%s last_date=%s,grace=%s",
+                        ent_id, ou, affi, last_date, grace)
+                    person.delete_affiliation(ou, affi, co.system_x)
+                    deletedaff += 1
 
 
-def process_sysx_persons(source_file,update):
+def process_sysx_persons(source_file, update):
     global old_aff
-    sysx = SYSX(source_file,update=update)
+    sysx = SYSX(source_file, update=update)
     sysx.list()
     old_aff = _load_cere_aff()
-    
+
     for p in sysx.sysxids:
         create_sysx_person(sysx.sysxids[p])
     if include_delete:
         rem_old_aff()
-    
-    
+
+
 def check_expired_sourcedata(expire_date):
     expire = mx.DateTime.DateFrom(expire_date)
-    today =  mx.DateTime.today()
-    if ( expire < today):        
+    today = mx.DateTime.today()
+    if expire < today:
         return True
-    else: 
+    else:
         return False
-    
-    
-## sxp =  dict from access_SYSX.prepare_data funtion
+
+
+# sxp =  dict from access_SYSX.prepare_data funtion
 def create_sysx_person(sxp):
-    global skipped,added,updated,unchanged,include_delete,old_aff
+    global skipped, added, updated, unchanged, include_delete, old_aff
     add_nobirthno = add_sysx_id = fnr_found = False
 
-    id=sxp['id']
-    personnr=sxp['personnr']
+    id = sxp['id']
+    personnr = sxp['personnr']
     fornavn = sxp['fornavn']
     etternavn = sxp['etternavn']
-    fullt_navn = "%s %s" % (fornavn,etternavn)
+    fullt_navn = "%s %s" % (fornavn, etternavn)
     approved = sxp['approved']
 
-    #logger.debug("Trying to process %s %s ID=%s,approved=%s" %
+    # logger.debug("Trying to process %s %s ID=%s,approved=%s" %
     #                       (fornavn,etternavn,id,approved))
     if check_expired_sourcedata(sxp['expire_date']):
-       logger.info("Skip %s (SysXId=%s)(pnr=%s), it is expired %s" % (fullt_navn, 
-                                                                      id, 
-                                                                      personnr, 
-                                                                      sxp['expire_date']))
-       skipped+=1
-       return
- 
-    #check if approved. Ditch if not.
-    if (approved != 'Yes'):
-        logger.error("Person: %s (id=%s) not approved, Skip." % (fullt_navn,id))
+        logger.info(
+            "Skip %s (SysXId=%s)(pnr=%s), it is expired %s",
+            fullt_navn, id, personnr, sxp['expire_date'])
+        skipped += 1
         return
-    
+
+    # check if approved. Ditch if not.
+    if approved != 'Yes':
+        logger.error(
+            "Person: %s (id=%s) not approved, Skip." % (fullt_navn, id))
+        return
+
     my_stedkode = Factory.get('OU')(db)
     my_stedkode.clear()
     pers_sysx = Factory.get('Person')(db)
     pers_fnr = Factory.get('Person')(db)
-    
+
     try:
-        pers_sysx.find_by_external_id(co.externalid_sys_x_id,id)
+        pers_sysx.find_by_external_id(co.externalid_sys_x_id, id)
     except Errors.NotFoundError:
         add_sysx_id = True
-        #not found by sysX_id
- 
-    if (personnr != ""):
+        # not found by sysX_id
+
+    if personnr != "":
         try:
-           fnr = fodselsnr.personnr_ok(personnr)
+            fnr = fodselsnr.personnr_ok(personnr)
         except fodselsnr.InvalidFnrError:
-           logger.error("Ugyldig f�dselsnr: %s on sysX id=" % (personnr,id))
-           return
-        (year,mon,day) = fodselsnr.fodt_dato(fnr)
+            logger.error("Ugyldig fødselsnr: %s on sysX id=", personnr, id)
+            return
+        (year, mon, day) = fodselsnr.fodt_dato(fnr)
         gender = co.gender_male
-        if(fodselsnr.er_kvinne(fnr)):
-           gender = co.gender_female
+        if fodselsnr.er_kvinne(fnr):
+            gender = co.gender_female
 
         try:
-            pers_fnr.find_by_external_id(co.externalid_fodselsnr,fnr)
+            pers_fnr.find_by_external_id(co.externalid_fodselsnr, fnr)
         except Errors.NotFoundError:
-            add_nobirthno =  True
+            add_nobirthno = True
         except Errors.TooManyRowsError:
             # This persons fnr has multiple rows in entity_external_id table
             # This is an error, person should not have not more than one entry.
             # Don't know which person object to use, return error message.
-            logger.error("Person with ssn:%s has multiple entries in entity_external_id. "
-                         "Resolve manually" % fnr)
+            logger.error(
+                "Person with ssn:%s has multiple entries in "
+                "entity_external_id. Resolve manually" % fnr)
             return
         else:
             if not add_sysx_id and (pers_fnr.entity_id != pers_sysx.entity_id):
-                logger.error("SysXID=%s with fnr=%s is owned by different personid's! "
-                             "Fnr owned by person_id=%s in db " 
-                             "Resolve manually" % (pers_sysx.entity_id,fnr,pers_fnr.entity_id))
+                logger.error(
+                    "SysXID=%s with fnr=%s is owned by different personid's! "
+                    "Fnr owned by person_id=%s in db "
+                    "Resolve manually",
+                    pers_sysx.entity_id, fnr, pers_fnr.entity_id)
                 return
-            fnr_found=True
-                      
+            fnr_found = True
+
     else:
-        # foreigner without norwegian ssn, 
+        # foreigner without norwegian ssn,
         date_field = sxp['fodsels_dato'].split(".")
         year = int(date_field[2])
-        mon =  int(date_field[1])
-        day =  int(date_field[0])
-        if(sxp['gender']=='M'):
-           gender = co.gender_male
-        elif(sxp['gender']=='F'):
-           gender = co.gender_female
+        mon = int(date_field[1])
+        day = int(date_field[0])
+        if sxp['gender'] == 'M':
+            gender = co.gender_male
+        elif sxp['gender'] == 'F':
+            gender = co.gender_female
         else:
             logger.error("Invalid gender from SysXId=%s" % id)
             return
 
-    logger.info("Processing '%s', SysXId=%s, fnr='%s'" % (fullt_navn,id,personnr))
+    logger.info(
+        "Processing '%s', SysXId=%s, fnr='%s'" % (fullt_navn, id, personnr))
     if fnr_found:
-        person=pers_fnr
+        person = pers_fnr
     else:
-        person=pers_sysx
+        person = pers_sysx
 
     # person object located, populate...
     try:
-        person.populate(mx.DateTime.Date(year, mon, day),gender)
-    except Errors.CerebrumError,m:
-        logger.error("Person %s populate failed: %s" % (personnr or id,m))
+        person.populate(mx.DateTime.Date(year, mon, day), gender)
+    except Errors.CerebrumError as m:
+        logger.error("Person %s populate failed: %s" % (personnr or id, m))
         return
-    
-    person.affect_names(co.system_x,co.name_first,co.name_last,co.name_full)
-    person.populate_name(co.name_first,fornavn)
-    person.populate_name(co.name_last,etternavn) 
-    person.populate_name(co.name_full,fullt_navn)
+
+    person.affect_names(co.system_x, co.name_first, co.name_last, co.name_full)
+    person.populate_name(co.name_first, fornavn)
+    person.populate_name(co.name_last, etternavn)
+    person.populate_name(co.name_full, fullt_navn)
 
     # add external ids
-    if personnr:      
+    if personnr:
         person.affect_external_id(co.system_x,
-                                  co.externalid_fodselsnr, 
+                                  co.externalid_fodselsnr,
                                   co.externalid_sys_x_id)
-        person.populate_external_id(co.system_x,co.externalid_fodselsnr,fnr)
-        logger.debug("Set NO_BIRTHNO for id=%s,fnr=%s" % (id,personnr))
+        person.populate_external_id(co.system_x, co.externalid_fodselsnr, fnr)
+        logger.debug("Set NO_BIRTHNO for id=%s,fnr=%s" % (id, personnr))
     else:
-        person.affect_external_id(co.system_x,co.externalid_sys_x_id)
-    person.populate_external_id(co.system_x,co.externalid_sys_x_id,id)
-    logger.debug("Set syx_id for id=%s,fnr=%s" % (id,personnr))
+        person.affect_external_id(co.system_x, co.externalid_sys_x_id)
+    person.populate_external_id(co.system_x, co.externalid_sys_x_id, id)
+    logger.debug("Set syx_id for id=%s,fnr=%s" % (id, personnr))
 
     # setting affiliation and affiliation_status
     aff = sxp['affiliation']
     aff_stat = sxp['affiliation_status']
     affiliation = int(co.PersonAffiliation(aff))
-    affiliation_status = int(co.PersonAffStatus(aff,aff_stat))
+    affiliation_status = int(co.PersonAffStatus(aff, aff_stat))
 
     # get ou_id of stedkode used
     fak = sxp['ou'][0:2]
     ins = sxp['ou'][2:4]
     avd = sxp['ou'][4:6]
-    
-    #KEB
+
+    # KEB
     if fak == '0':
-        logger.warn("Person with SysX id=%s and fnr=%s, has invalid stedkode: %s%s%s" 
-        % (id, personnr, fak, ins, avd));
+        logger.warn(
+            "Person with SysX id=%s and fnr=%s, has invalid stedkode: %s%s%s",
+            id, personnr, fak, ins, avd)
         ins = fak
         avd = fak
     try:
-        my_stedkode.find_stedkode(fak,ins,avd,cereconf.DEFAULT_INSTITUSJONSNR)
-    except EntityExpiredError, err:
-        logger.error("Person with SysX id %s on expired stedkode %s%s%s" % (id, fak, ins, avd));
+        my_stedkode.find_stedkode(fak, ins, avd,
+                                  cereconf.DEFAULT_INSTITUSJONSNR)
+    except EntityExpiredError as err:
+        logger.error("Person with SysX id %s on expired stedkode %s%s%s",
+                     id, fak, ins, avd)
         return
-    
+
     ou_id = int(my_stedkode.entity_id)
 
     # if this is a new Person, there is no entity_id assigned to it
@@ -253,84 +263,86 @@ def create_sysx_person(sxp):
                                 ou_id,
                                 affiliation,
                                 affiliation_status
-                               )
+                                )
     # make sure we don't delete this aff when processing deleted affs
     if include_delete:
-       key_a = "%s:%s:%s" % (person.entity_id,ou_id,int(affiliation))
-       if old_aff.has_key(key_a):
-          logger.debug("Don't delete affiliation: %s" % (key_a,))
-          old_aff[key_a] = False
+        key_a = "%s:%s:%s" % (person.entity_id, ou_id, int(affiliation))
+        if key_a in old_aff:
+            logger.debug("Don't delete affiliation: %s", key_a)
+            old_aff[key_a] = False
     op2 = person.write_db()
     # Update last-seen date
     try:
-       person.set_affiliation_last_date(co.system_x,
-                                        ou_id,
-                                        affiliation,
-                                        affiliation_status
-                                        )
-    except AttributeError,m:
-       logger.warn("WHOOOO. AttributeErrror: %s"  % m)
-       # in case this is a new person object...
-    except Errors.ProgrammingError,m:
-       logger.warn("WHOOOO. Programming errror: %s"  % m)
-       
- 
-    logger.debug("OP codes: op=%s,op2=%s" % (op,op2))
-    
+        person.set_affiliation_last_date(co.system_x,
+                                         ou_id,
+                                         affiliation,
+                                         affiliation_status
+                                         )
+    except AttributeError as m:
+        logger.warn("WHOOOO. AttributeErrror: %s" % m)
+        # in case this is a new person object...
+    except Errors.ProgrammingError as m:
+        logger.warn("WHOOOO. Programming errror: %s" % m)
+
+    logger.debug("OP codes: op=%s,op2=%s" % (op, op2))
+
     if op is None and op2 is None:
-       logger.info("**** EQUAL ****")
-       unchanged+=1
-    elif op is None and op2 == False:
-       logger.info("**** AFF UPDATE ****")
-       updated+=1
-    elif op == True:
-       logger.info("**** NEW ****")
-       added+=1
-    elif op == False:
-       logger.info("**** UPDATE ****")
-       updated+=1
+        logger.info("**** EQUAL ****")
+        unchanged += 1
+    elif op is None and op2 is False:
+        logger.info("**** AFF UPDATE ****")
+        updated += 1
+    elif op is True:
+        logger.info("**** NEW ****")
+        added += 1
+    elif op is False:
+        logger.info("**** UPDATE ****")
+        updated += 1
     return 0
-    
+
 
 def main():
-    global sysx,logger
-    logger=Factory.get_logger(logger_name)
-    
+    global sysx, logger
+    logger = Factory.get_logger(logger_name)
+
     dryrun = False
 
     try:
-        opts,args = getopt.getopt(sys.argv[1:],'s:ud',['source_file','update','dryrun'])
-    except getopt.GetoptError,m:
-        print "Unknown option: %s" % (m)
+        opts, args = getopt.getopt(sys.argv[1:], 's:ud',
+                                   ['source_file', 'update', 'dryrun'])
+    except getopt.GetoptError as m:
+        print("Unknown option: {}".format(m))
         usage()
 
     ret = 0
     source_file = None
     update = 0
-    for opt,val in opts:
-        if opt in('-s','--source_file'):
+    for opt, val in opts:
+        if opt in ('-s', '--source_file'):
             source_file = val
-        elif opt in ('-u','--update'):
+        elif opt in ('-u', '--update'):
             update = 1
-        elif opt in ('--dryrun'):
+        elif opt in ('--dryrun',):
             dryrun = True
-    process_sysx_persons(source_file,update)
-   
+    process_sysx_persons(source_file, update)
+
     if dryrun:
         logger.info("Dryrun: Rollback all changes")
         db.rollback()
     else:
         logger.info("Committing all changes to database")
         db.commit()
-        
-    fmt_str="Stats: Added: %d, Updated=%d, Skipped=%d, Unchanged=%d, Deleted affs=%d"
-    fmt_var=(added, updated,skipped,unchanged,deletedaff)
-    logger.info( fmt_str % fmt_var)
+
+    fmt_str = ("Stats: Added: %d, Updated=%d, Skipped=%d, "
+               "Unchanged=%d, Deleted affs=%d")
+    fmt_var = (added, updated, skipped, unchanged, deletedaff)
+    logger.info(fmt_str % fmt_var)
+
 
 def usage():
-    print __doc__
+    print(__doc__)
     sys.exit(1)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
