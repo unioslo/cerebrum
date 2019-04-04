1b7b1425839700db36a88a28bbf718f20269dc6d
Stein Elgethun <elg@stein.uio.no>
2019-04-04 14:24:34 +0200

    Make uit.Person follow pep8
    Fix style problems in the code
    
    Issue: CRB-2752

--- Cerebrum/modules/no/uit/Person.py
+++ Cerebrum/modules/no/uit/Person.py
@@ -20,92 +20,81 @@
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 from __future__ import unicode_literals
 
-
-import cerebrum_path
-import cereconf
-
-from Cerebrum import Utils
-from Cerebrum.Utils import argument_to_sql
-from Cerebrum import Person
 from Cerebrum import Errors
+from Cerebrum import Person
+from Cerebrum.Utils import argument_to_sql
 
 
 class UiTPersonMixin(Person.Person):
-  """
-  This class provides an UiT-specific extension to the core Person class.
-  """
+    """
+    This class provides an UiT-specific extension to the core Person class.
+    """
 
-  # check if a person has an electronic listing reservation
-  def has_e_reservation(self):
-    # this method may be applied to any Cerebrum-instances that
-    # use trait_public_reservation
-    r = self.get_trait(self.const.trait_public_reservation)
-    if r and r['numval'] == 0:
-      return False
-    return True
+    # check if a person has an electronic listing reservation
+    def has_e_reservation(self):
+        # this method may be applied to any Cerebrum-instances that
+        # use trait_public_reservation
+        r = self.get_trait(self.const.trait_public_reservation)
+        if r and r['numval'] == 0:
+            return False
+        return True
 
-  # We want to raise an error when person.populate is called on a person with
-  # deceased_date set in the DB.
-  def populate(self, birth_date, gender, description=None, deceased_date=None,
-                 parent=None):
-    try:
-      db_deceased = self.deceased_date
-    except AttributeError:
-      pass
-    else:
-      if db_deceased is not None and deceased_date is None:
-        raise Errors.CerebrumError('Populate called with deceased_date=None,'\
-                                   'but deceased_date already set on person')
-    self.__super.populate(birth_date,gender,description,deceased_date,parent)
-    
+    # We want to raise an error when person.populate is called on a person with
+    # deceased_date set in the DB.
+    def populate(self, birth_date, gender, description=None,
+                 deceased_date=None, parent=None):
+        try:
+            db_deceased = self.deceased_date
+        except AttributeError:
+            pass
+        else:
+            if db_deceased is not None and deceased_date is None:
+                raise Errors.CerebrumError('Populate called with deceased_date'
+                                           '=None, but deceased_date already '
+                                           'set on person')
+        self.__super.populate(birth_date, gender, description, deceased_date,
+                              parent)
 
-  def list_deceased(self):
-      ret = {}
-      for row in self.query("""
-                            SELECT pi.person_id, pi.deceased_date
-                            FROM [:table schema=cerebrum name=person_info] pi
-                            WHERE pi.deceased_date IS NOT NULL """):
-          ret[int(row['person_id'])] = row['deceased_date']
-      return ret
+    def list_deceased(self):
+        ret = {}
+        for row in self.query("""
+                              SELECT pi.person_id, pi.deceased_date
+                              FROM [:table schema=cerebrum name=person_info] pi
+                              WHERE pi.deceased_date IS NOT NULL """):
+            ret[int(row['person_id'])] = row['deceased_date']
+            return ret
 
-
-  def _compare_names(self, type, other):
+    def _compare_names(self, type, other):
         """Returns True if names are equal.
-
         self must be a populated object."""
         try:
             tmp = other.get_name(self._pn_affect_source, type)
-            if tmp ==None:
-            #if len(tmp) == 0:
+            if tmp is None:
                 raise KeyError
         except:
-            raise Person.MissingOtherException 
+            raise Person.MissingOtherException
         try:
             myname = self._name_info[type]
         except:
             raise Person.MissingSelfException
-#        if isinstance(myname, unicode):
-#            return unicode(tmp, 'iso8859-1') == myname
         return tmp == myname
 
-  def list_names(self, source_system=None, variant=None):
-    """Return all names, optionally filtered on source_system or variant"""
-    binds = dict()
-    where = ''
+    def list_names(self, source_system=None, variant=None):
+        """Return all names, optionally filtered on source_system or variant"""
+        binds = dict()
+        where = ''
 
-    if source_system is not None:
-      where += 'WHERE ' + argument_to_sql(source_system, 'source_system',
-                                          binds, int)
-      if variant is not None:
-        where += ' AND ' + argument_to_sql(variant, 'name_variant',
-                                           binds, int)
-    elif variant is not None:
-      where += 'WHERE ' + argument_to_sql(variant, 'name_variant',
-                                          binds, int)
+        if source_system is not None:
+            where += 'WHERE ' + argument_to_sql(source_system, 'source_system',
+                                                binds, int)
+            if variant is not None:
+                where += ' AND ' + argument_to_sql(variant, 'name_variant',
+                                                   binds, int)
+        elif variant is not None:
+            where += 'WHERE ' + argument_to_sql(variant, 'name_variant',
+                                                binds, int)
 
-    return self.query("""
-    SELECT *
-    FROM [:table schema=cerebrum name=person_name]
-    """ + where, binds)
-
-# arch-tag: 07747944-da97-11da-854b-ac67a6778cc2
+            return self.query("""
+            SELECT *
+            FROM [:table schema=cerebrum name=person_name]
+            """ + where, binds)
