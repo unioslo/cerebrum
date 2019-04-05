385d71007c46933afdd8233cdd61b12aca0160e2
Andreas Ellewsen <andreas.ellewsen@usit.uio.no>
2019-04-05 15:37:54 +0200

    Clean up OU implementation of UiT
    Clean up imports and make both files pep8 compliant.
    Deletes sito_utils since it is no longer needed.
    
    Issue: CRB-2756

--- Cerebrum/modules/no/uit/Stedkode.py
+++ Cerebrum/modules/no/uit/Stedkode.py
@@ -1,54 +1,44 @@
-#from Cerebrum import Utils
-#from Cerebrum.OU import OU
-#from Cerebrum.Entity import EntityName
-import random
-import sys
-import re
-import string
-import crypt
-import pprint
+# -*- coding: utf-8 -*-
+# Copyright 2019 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-
-# UIT imports
-import md5
-import base64
-import sha
-import mx
-import traceback
-# UIT end
-import cerebrum_path
 import cereconf
-from Cerebrum import Account
+
 from Cerebrum import Errors
-from Cerebrum.modules.no import Stedkode
-from Cerebrum import Utils
-from Cerebrum.Utils import NotSet
-from Cerebrum.modules import Email
-from Cerebrum.modules.pwcheck.history import PasswordHistory
-#from Cerebrum.modules import PasswordHistory
-from Cerebrum.modules.bofhd.utils import BofhdRequests
-from Cerebrum.Utils import pgp_encrypt, Factory, prepare_string
-from Cerebrum.modules.no.uit.Email import email_address
-logger = Factory.get_logger(cereconf.DEFAULT_LOGGER_TARGET)
-from pprint import pprint
-
-
+from Cerebrum.Utils import Factory
 from Cerebrum.modules.no import Stedkode
 
 db = Factory.get('Database')()
 co = Factory.get('Constants')(db)
 person = Factory.get('Person')(db)
-account = Factory.get('Account')(db)    
-#__version__ = "1.1"
+account = Factory.get('Account')(db)
 
 
 class StedkodeMixin(Stedkode.Stedkode):
-    
-    def get_name(self,domain):
-        self.EntityName.get_name(self,domain)
+    """ UiT specific Stedkode functionality"""
 
-    def find_by_perspective_old(self, ou_id,perspective):
-        """Associate the object with the OU whose identifier is OU_ID and perspective as given.
+    def get_name(self, domain):
+        self.EntityName.get_name(self, domain)
+
+    def find_by_perspective_old(self, ou_id, perspective):
+        """
+        Associate the object with the OU whose identifier is OU_ID and
+        perspective as given.
 
         If OU_ID isn't an existing OU identifier,
         NotFoundError is raised."""
@@ -58,7 +48,10 @@ class StedkodeMixin(Stedkode.Stedkode):
         SELECT oi.ou_id, oi.name, oi.acronym, oi.short_name, oi.display_name
         FROM [:table schema=cerebrum name=ou_info] oi,
              [:table schema=cerebrum name=ou_structure] os
-        WHERE oi.ou_id=:ou_id AND oi.ou_id = os.ou_id AND os.perspective=:perspective""", {'ou_id': ou_id,'perspective': perspective})
+        WHERE oi.ou_id=:ou_id AND oi.ou_id = os.ou_id
+        AND os.perspective=:perspective""",
+                                           {'ou_id': ou_id,
+                                            'perspective': perspective})
         try:
             del self.__in_db
         except AttributeError:
@@ -66,38 +59,65 @@ class StedkodeMixin(Stedkode.Stedkode):
         self.__in_db = True
         self.__updated = []
 
-
-    def find_by_perspective(self, ou_id,perspective):
+    def find_by_perspective(self, ou_id, perspective):
         name_variant = co.ou_name
-        #perspective = co.perspective_fs
+        # perspective = co.perspective_fs
         self.__super.find(ou_id)
-        (self.ou_id, self.name) = self.query_1("""SELECT os.ou_id, eln.name FROM [:table schema=cerebrum name=entity_language_name] eln, [:table schema=cerebrum name=ou_structure] os WHERE eln.name_variant=:name_variant_1 AND eln.entity_id = os.ou_id AND os.ou_id=:ou_id_1 AND os.perspective=:perspective_1""",{'name_variant_1' : name_variant,'ou_id_1' : ou_id, 'perspective_1' : perspective})
+        (self.ou_id, self.name) = self.query_1(
+            """SELECT os.ou_id, eln.name
+            FROM [:table schema=cerebrum name=entity_language_name] eln,
+                 [:table schema=cerebrum name=ou_structure] os
+            WHERE eln.name_variant=:name_variant_1
+            AND eln.entity_id = os.ou_id
+            AND os.ou_id=:ou_id_1
+            AND os.perspective=:perspective_1""",
+            {'name_variant_1': name_variant, 'ou_id_1': ou_id,
+             'perspective_1': perspective})
 
         try:
             name_variant = co.ou_name_acronym
-            self.acronym = self.query_1("""SELECT eln.name FROM [:table schema=cerebrum name=entity_language_name] eln, [:table schema=cerebrum name=ou_structure] os WHERE eln.name_variant=:name_variant_1 AND eln.entity_id = os.ou_id AND os.ou_id=:ou_id_1 AND os.perspective=:perspective_1""",{'name_variant_1' : name_variant,'ou_id_1' : ou_id, 'perspective_1' : perspective})
+            self.acronym = self.query_1(
+                """SELECT eln.name
+                FROM [:table schema=cerebrum name=entity_language_name] eln,
+                     [:table schema=cerebrum name=ou_structure] os
+                WHERE eln.name_variant=:name_variant_1
+                AND eln.entity_id = os.ou_id
+                AND os.ou_id=:ou_id_1
+                AND os.perspective=:perspective_1""",
+                {'name_variant_1': name_variant, 'ou_id_1': ou_id,
+                 'perspective_1': perspective})
         except Errors.NotFoundError:
-            self.acronym=""
-            #print "unable to find acronym for %s" % ou_id
+            self.acronym = ""
+
         try:
             name_variant = co.ou_name_short
-            self.short_name = self.query_1("""SELECT eln.name FROM [:table schema=cerebrum name=entity_language_name] eln, [:table schema=cerebrum name=ou_structure] os WHERE eln.name_variant=:name_variant_1 AND eln.entity_id = os.ou_id AND os.ou_id=:ou_id_1 AND os.perspective=:perspective_1""",{'name_variant_1' : name_variant,'ou_id_1' : ou_id, 'perspective_1' : perspective})
+            self.short_name = self.query_1(
+                """SELECT eln.name
+                FROM [:table schema=cerebrum name=entity_language_name] eln,
+                     [:table schema=cerebrum name=ou_structure] os
+                WHERE eln.name_variant=:name_variant_1
+                AND eln.entity_id = os.ou_id
+                AND os.ou_id=:ou_id_1
+                AND os.perspective=:perspective_1""",
+                {'name_variant_1': name_variant, 'ou_id_1': ou_id,
+                 'perspective_1': perspective})
         except Errors.NotFoundError:
-            self.short_name=""
-            #print "unable to find short name for %s" % ou_id
+            self.short_name = ""
+
         try:
             name_variant = co.ou_name_display
-            self.display_name = self.query_1("""SELECT eln.name FROM [:table schema=cerebrum name=entity_language_name] eln, [:table schema=cerebrum name=ou_structure] os WHERE eln.name_variant=:name_variant_1 AND eln.entity_id = os.ou_id AND os.ou_id=:ou_id_1 AND os.perspective=:perspective_1""",{'name_variant_1' : name_variant,'ou_id_1' : ou_id, 'perspective_1' : perspective})
+            self.display_name = self.query_1(
+                """SELECT eln.name
+                FROM [:table schema=cerebrum name=entity_language_name] eln,
+                     [:table schema=cerebrum name=ou_structure] os
+                WHERE eln.name_variant=:name_variant_1
+                AND eln.entity_id = os.ou_id
+                AND os.ou_id=:ou_id_1
+                AND os.perspective=:perspective_1""",
+                {'name_variant_1': name_variant, 'ou_id_1': ou_id,
+                 'perspective_1': perspective})
         except Errors.NotFoundError:
-            self.display_name=""
-            #print "unable to find display name for %s" % ou_id
-
-        #print "having collected ou names:"
-        #print "ou_name:%s" % self.name
-        #print "ou_acronym:%s" % self.acronym
-        #print "ou_short:%s" % self.short_name
-        #print "ou_display:%s" % self.display_name
-       
+            self.display_name = ""
 
         try:
             del self.__in_db
@@ -105,8 +125,3 @@ class StedkodeMixin(Stedkode.Stedkode):
             pass
         self.__in_db = True
         self.__updated = []
-# 785 = name
-# 784 = acronym
-# 787 = short
-# 783 = display
-# ?   = sort
