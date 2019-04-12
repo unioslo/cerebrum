743675d66184f048fd05028ee6dfbdcb70cb0222
Stein Elgethun <elg@stein.uio.no>
2019-04-12 11:47:06 +0200

    Add comments explaining mod_ad_email
    Issue: CRB-2742

--- design/mod_ad_email.sql
+++ design/mod_ad_email.sql
@@ -17,6 +17,25 @@
 * along with Cerebrum; if not, write to the Free Software Foundation,
 * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 *
+*
+* Tables used by Cerebrum.modules.no.uit.ad_email
+*
+* This module is based on a table from cerebrum @ uit:
+*
+*                   Table "public.ad_email"
+
+*     Column     |            Type       | Modifiers
+*  --------------+-----------------------+-----------
+*   account_name | character varying(20) | not null
+*   local_part   | character varying(64) | not null
+*   domain_part  | character varying(64) | not null
+*   create_date  | date                  |
+*   update_date  | date                  |
+*  Indexes:
+*      "ad_email_pkey" PRIMARY KEY, btree (account_name, local_part, domain_part)
+*
+* The primary key could just be account_name, but for some unknown reason it is not.
+* Might be something that should be fixed when migrating
 */
 
 category:metainfo;
@@ -24,6 +43,23 @@ name=ad_email;
 category:metainfo;
 version=1.0;
 
+
+/**
+* ad_email
+*
+* Table of ad_emails connected to accounts
+*
+* account_name
+*   The account connected to the mail address
+* local_part
+*   The local part of the mail address
+* domain_part
+*   The domain part of the mail address
+* create_date
+*   The date this entry was made
+* update_date
+*   The date this entry was last updated
+**/
 
 category:main;
 CREATE TABLE ad_email
