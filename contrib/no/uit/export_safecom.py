7777b5ae1cbd0f0e7e94c5daab937f676f47a552
Sivert Hatteberg <sivert.hatteberg@usit.uio.no>
2019-04-30 11:05:31 +0200

    Update copyright

--- contrib/no/uit/export_safecom.py
+++ contrib/no/uit/export_safecom.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 #
-# Copyright 2002, 2003 University of Oslo, Norway
+# Copyright 2002-2019 University of Oslo, Norway
 #
 # This file is part of Cerebrum.
 #
