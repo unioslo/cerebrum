cce13e8394efb3ad12d86a3510a59d1e339dba39
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2019-05-08 14:28:14 +0200

    Fix uit PosixLDIF code style
    Issue: CRB-2770

--- uit-modules/PosixLDIF.py
+++ uit-modules/PosixLDIF.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2004-2014 University of Oslo, Norway
+# Copyright 2004-2019 University of Oslo, Norway
 #
 # This file is part of Cerebrum.
 #
@@ -16,20 +16,23 @@
 # You should have received a copy of the GNU General Public License
 # along with Cerebrum; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+"""
+PosixLDIF customization for UiT.
+"""
 from __future__ import unicode_literals
-from Cerebrum.modules.PosixLDIF import PosixLDIF
-from Cerebrum.QuarantineHandler import QuarantineHandler
-from Cerebrum.modules import LDIFutils
-from Cerebrum.Utils import Factory, auto_super, make_timer
+
 from Cerebrum.Utils import Factory
+from Cerebrum.Utils import make_timer
+from Cerebrum.modules import LDIFutils
+from Cerebrum.modules.PosixLDIF import PosixLDIF
 
-class PosixLDIF_UiTMixin(PosixLDIF):
+
+class PosixLDIF_UiTMixin(PosixLDIF):  # noqa: N801
     """PosixLDIF mixin class providing functionality specific to UiT."""
-
 
     def __init__(self, *rest, **kw):
         super(PosixLDIF_UiTMixin, self).__init__(*rest, **kw)
-        
+
         # load person_id -> primary account_id
         account = Factory.get("Account")(self.db)
         self.pid2primary_aid = dict()
@@ -37,18 +40,18 @@ class PosixLDIF_UiTMixin(PosixLDIF):
                                primary_only=True,
                                account_spread=self.spread_d["user"][0]):
             self.pid2primary_aid[row["person_id"]] = row["account_id"]
-    # end __init__
-    
 
     def user_ldif(self, filename=None, auth_meth=None):
         """Generate posix-user."""
         timer = make_timer(self.logger, 'Starting user_ldif...')
         self.init_user(auth_meth)
         f = LDIFutils.ldif_outfile('USER', filename, self.fd)
-        self.generate_system_object(f) # this line is the only reason for having this function here
+        # this line is the only reason for having this function here:
+        self.generate_system_object(f)
         f.write(LDIFutils.container_entry_string('USER'))
         self.logger.debug("filter out quarantined accounts")
-        self.logger.debug("only include accounts with spread:%s" % (self.spread_d['user']))
+        self.logger.debug("only include accounts with spread: %r",
+                          self.spread_d['user'])
         for row in self.posuser.list_extended_posix_users(
                 self.user_auth,
                 spread=self.spread_d['user'],
@@ -56,23 +59,25 @@ class PosixLDIF_UiTMixin(PosixLDIF):
             dn, entry = self.user_object(row)
             if dn:
                 f.write(LDIFutils.entry_string(dn, entry, False))
-                self.logger.warn("writing:%s - %s" % (dn,entry))
+                self.logger.warn("writing:%s - %s" % (dn, entry))
         LDIFutils.end_ldif_outfile('USER', f, self.fd)
         timer('... done user_ldif')
 
-    #UIT: added the system object. This is needed when we add users under it
-    def generate_system_object(self,outfile):
-        entry= {'objectClass':['top','uioUntypedObject']}
+    def generate_system_object(self, outfile):
+        """
+        Add a system object. This is needed when we add users under it
+        """
+        entry = {'objectClass': ['top', 'uioUntypedObject']}
         self.ou_dn = "cn=system,dc=uit,dc=no"
-        outfile.write(LDIFutils.entry_string(self.ou_dn,entry))
-
+        outfile.write(LDIFutils.entry_string(self.ou_dn, entry))
 
     def init_user(self, *args, **kwargs):
         # Prepare to include eduPersonAffiliation, taken from OrgLDIF.
         self.org_ldif = Factory.get('OrgLDIF')(self.db, self.logger)
         self.org_ldif.init_eduPersonAffiliation_lookup()
         self.steder = {}
-        self.__super.init_user(*args, **kwargs)
+
+        super(PosixLDIF_UiTMixin, self).init_user(*args, **kwargs)
 
         self.account_aff = account_aff = {}
         for arow in self.posuser.list_accounts_by_type():
@@ -83,51 +88,37 @@ class PosixLDIF_UiTMixin(PosixLDIF):
             else:
                 account_aff[account_id] = [val]
 
-    def id2stedkode(self, ou_id):
-        try:
-            return self.steder[ou_id]
-        except KeyError:
-            ou = self.org_ldif.ou
-            ou.clear()
-            try:
-                ou.find(ou_id)
-            except Errors.NotFoundError:
-                raise CerebrumError, "Stedkode unknown for ou_id %d" % ou_id
-            ret = self.steder[ou_id] = \
-                  "%02d%02d%02d" % (ou.fakultet, ou.institutt, ou.avdeling)
-            return ret
-
     def user_object(self, row):
-        dn, entry = self.__super.user_object(row)
+        dn, entry = super(PosixLDIF_UiTMixin, self).user_object(row)
 
         if entry:
-
-            # 
-            # skip if sito account.
+            # Skip if sito account.
+            #
             # this should really be done using spreads, but since all accounts
-            # have the same spread as the person object, we are unable to filter 
-            # out any single account (for those persons with multiple accounts)
-            # 
-            #uname = row['entity_name']
-            #if len(uname) == 7:
-            #    if uname[-1] == 's':
-            #        self.logger.debug("filtering out account:%s" %row['entity_name'])
-            #        return None,None
+            # have the same spread as the person object, we are unable to
+            # filter out any single account (for those persons with multiple
+            # accounts)
+            #
+            # uname = row['entity_name']
+            # if len(uname) == 7:
+            #     if uname[-1] == 's':
+            #         self.logger.debug("filtering out account name=%r",
+            #                           row['entity_name'])
+            #         return None, None
 
-
-        # Add displayName, norEduPersonLegalName and objectClass: norEduPerson
-            if entry.has_key('displayName'):
+            # Add displayName, norEduPersonLegalName and
+            # objectClass: norEduPerson
+            if 'displayName' in entry:
                 entry['displayName'].extend(entry['cn'])
             else:
                 entry['displayName'] = entry['cn']
-            
-            if entry.has_key('norEduPersonLegalName'):
+
+            if 'norEduPersonLegalName' in entry:
                 entry['norEduPersonLegalName'].extend(entry['cn'])
             else:
                 entry['norEduPersonLegalName'] = entry['cn']
 
             entry['objectClass'].extend(('norEduPerson',))
-
         return dn, entry
 
     def update_user_entry(self, account_id, entry, row):
@@ -143,9 +134,8 @@ class PosixLDIF_UiTMixin(PosixLDIF):
                 entry['eduPersonAffiliation'] = added
         entry['objectClass'].append('eduPerson')
 
-        return self.__super.update_user_entry(account_id,entry, row)
-
-
+        return super(PosixLDIF_UiTMixin, self).update_user_entry(account_id,
+                                                                 entry, row)
 
     def get_netgrp(self, triples, memgrp):
 
@@ -161,9 +151,11 @@ class PosixLDIF_UiTMixin(PosixLDIF):
 
         # We deal with person members only here, and let the superclass
         # perform its magic on everything else.
-        for row in self.grp.search_members(group_id=self.grp.entity_id,
-                                           member_type=self.const.entity_person):
+        for row in self.grp.search_members(
+                group_id=self.grp.entity_id,
+                member_type=self.const.entity_person):
             person_id = row["member_id"]
+
             # This is a hack. When it fails, ignore it silently.
             if person_id not in self.pid2primary_aid:
                 continue
@@ -178,7 +170,7 @@ class PosixLDIF_UiTMixin(PosixLDIF):
                     self.logger.warn("Cache enabled but user id=%s not found",
                                      uname_id)
                     continue
-        
+
             if uname_id in self._gmemb or "_" in uname:
                 continue
 
@@ -186,5 +178,3 @@ class PosixLDIF_UiTMixin(PosixLDIF):
             self._gmemb[uname_id] = True
 
         return super(PosixLDIF_UiTMixin, self).get_netgrp(triples, memgrp)
-    # end get_netgrp
-# end class PosixLDIF_UiTMixin
