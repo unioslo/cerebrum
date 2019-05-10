3b8118b1a5f18f1372b509f7767aa675ebf06c80
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2019-05-10 11:28:06 +0200

    Generalize paga_adm2020_modify.py
    The script is a glorified 'cat [files ...] > file' script, so we make it
    accept any number of arguments.
    
    Also, logging, error handling. We won't write anything to the output
    file if reading any of the input files fails.
    
    Issue: CRB-2757

--- uit-contrib/paga_adm2020_modify.py
+++ uit-contrib/paga_adm2020_modify.py
@@ -1,47 +1,134 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2018-2019 University of Tromso, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+"""
+"""
+from __future__ import print_function
 
-import sys, getopt
+import argparse
+import io
+import logging
+import os
+import sys
 
-def usage():
-    print("python paga_adm2020_modify.py --input1=test1.txt --input2=test2.txt --output=test3.txt")
+import Cerebrum.logutils
+import Cerebrum.logutils.options
+from Cerebrum.utils.argutils import ParserContext
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:], 'h', ['input1=', 'input2=', 'output=', 'help'])
-except getopt.GetoptError:
-    usage()
-    sys.exit(2)
+logger = logging.getLogger(__name__)
 
-inputname1 = "/cerebrum/var/dumps/paga/uit_paga_last.csv"
-inputname2 = "/home/cerebrum/cerebrum/contrib/no/uit/uit_addons/scripts/adm2020/uit_paga_adminpeople_final.csv"
-outputname = "/cerebrum/var/dumps/paga/uit_paga_last.csv"
 
-for o, a in opts:
-    if o == "--input1":
-        inputname1 = a
-    elif o == "--input2":
-        inputname2 = a
-    elif o == "--output":
-        outputname = a
-    elif o in ("-h", "--help"):
-        usage()
-        sys.exit()
+default_input_1 = os.path.join(sys.prefix, 'var/dumps/paga/uit_paga_last.csv')
+default_input_2 = os.path.join(
+    '/home/cerebrum/cerebrum/contrib/no/uit/'
+    'uit_addons/scripts/adm2020/uit_paga_adminpeople_final.csv')
+default_output = os.path.join(sys.prefix, 'var/dumps/paga/uit_paga_last.csv')
+
+
+def main(inargs=None):
+    parser = argparse.ArgumentParser(
+        description="Concatenate files",
+    )
+    i1_arg = parser.add_argument(  # noqa: F841
+        '--input1',
+        dest='input_1',
+        default=default_input_1,
+        help='First input to write (ignored, default: %(default)s)',
+    )
+    i2_arg = parser.add_argument(
+        '--input2',
+        dest='input_2',
+        default=default_input_2,
+        help='Second input to write (deprecated, default: %(default)s)',
+    )
+    input_arg = parser.add_argument(
+        '--input',
+        action='append',
+        dest='inputs',
+        help='Additional file to write to output',
+        metavar='infile',
+    )
+    parser.add_argument(
+        '--output',
+        dest='output',
+        default=default_output,
+        help='Write input files to %(metavar)s (default: %(default)s)',
+        metavar='outfile'
+    )
+    parser.add_argument(
+        '--output-mode',
+        dest='mode',
+        choices=['w', 'a'],
+        default='a',
+        help='Open output file with mode %(choices)s (default: %(default)s)',
+        metavar='mode',
+    )
+    Cerebrum.logutils.options.install_subparser(parser)
+
+    args = parser.parse_args(inargs)
+    Cerebrum.logutils.autoconf('crontab', args)
+
+    logger.info('Start %r', parser.prog)
+    logger.debug("args: %r", args)
+
+    inputs = []
+
+    def add_file(filename):
+        if os.path.isfile(filename):
+            inputs.append(filename)
+        else:
+            raise ValueError('no file %r' % (filename, ))
+
+    # if args.input_1:
+    #     with ParserContext(parser, i1_arg):
+    #         add_file(args.input_1)
+    if args.input_2:
+        with ParserContext(parser, i2_arg):
+            add_file(args.input_2)
+
+    with ParserContext(parser, input_arg):
+        for filename in (args.inputs or ()):
+            add_file(filename)
+
+    logger.debug("Input files: %r", inputs)
+
+    # TODO: We should use io.open, io.String to ensure all files are in the
+    #       same encoding
+    file_buffer = io.BytesIO()
+    for filename in inputs:
+        with open(filename, 'r') as fin:
+            file_buffer.write(fin.read())
+            logger.debug('Read %r', fin.name)
+
+    def write(fout):
+        fout.write(file_buffer.getvalue())
+        logger.info('Files written to %r', fout.name)
+
+    if args.output == '-':
+        write(sys.stdout)
     else:
-        assert False, "unhandled option"
+        with open(args.output, args.mode) as fout:
+            write(fout)
 
-# Read uit_paga_last.csv
-#fin = open(inputname1, "r")
-#filedata = fin.read()
-#fin.close()
-#fout.write(filedata)
-
-# read uit_paga_adminpeople_final
-fin = open(inputname2, "r")
-filedata = fin.read()
-fin.close()
-
-# append to uit_paga_last.csv
-fout = open(outputname, "a")
-fout.write(filedata)
-fout.close()
+    logger.info('Done %r', parser.prog)
 
 
+if __name__ == '__main__':
+    main()
