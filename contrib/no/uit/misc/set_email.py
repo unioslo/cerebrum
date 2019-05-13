586d9c93c8db73c6b0fee2cf231e72bc019b01bc
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2019-05-13 15:23:49 +0200

    Refactor uit/misc/set_email.py
    - replace getopt with argparse

--- contrib/no/uit/misc/set_email.py
+++ contrib/no/uit/misc/set_email.py
@@ -20,85 +20,66 @@
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 from __future__ import print_function
 
-import getopt
-# import os
+import argparse
+import logging
 import re
-import sys
 
 import cereconf
-# from Cerebrum import Errors
+
+import Cerebrum.logutils
+import Cerebrum.logutils.options
 from Cerebrum.Utils import Factory
 from Cerebrum.modules.no.uit import Email
+from Cerebrum.utils.argutils import add_commit_args
+
+logger = logging.getLogger(__name__)
 
 
-progname = __file__.split("/")[-1]
-__doc__ = """
-This utility creates an exchange email for a user.
-
-usage:: %s [options]
-
-options are
-    -a | --account  : account name to modify
-    -e | --email    : exhange email address to set
-    -n | --noprimary: do not set this to primary
-    -h | --help     : show this
-    -d | --dryrun   : do not change DB
-    --logger-name name   : log name to use
-    --logger-level level : log level to use
-""" % (progname, )
-
-
-logger = Factory.get_logger('cronjob')
-db = Factory.get('Database')()
-ac = Factory.get('Account')(db)
-co = Factory.get('Constants')(db)
-db.cl_init(change_program=progname)
-em = Email.email_address(db, logger=logger)
-
-valid_exchange_domains = [cereconf.NO_MAILBOX_DOMAIN_EMPLOYEES, ]
+# NOTE: Cannot use find_by_domain because we have lots of invalid domains
+#       in our domain table!
+valid_exchange_domains = [
+    cereconf.NO_MAILBOX_DOMAIN_EMPLOYEES,
+    'ad.uit.no',
+    'adm.uit.no',
+    'ak.uit.no',
+    'asp.uit.no',
+    'jusshjelpa.uit.no',
+    'kun.uit.no',
+    'mailbox.uit.no',
+    'post.uit.no',
+    'student.uit.no',
+    'driv.no',
+    'hifm.no',
+    'hih.no',
+    'hin.no',
+    'nordnorsk.vitensenter.no',
+    'norgesuniversitetet.no',
+    'nuv.no',
+    'samskipnaden.no',
+    'sito.no',
+]
 
 validate = re.compile(r'^(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))$')
 
 
-def set_mail(account, localpart, domain, is_primary=True):
+def set_mail(db, account, localpart, domain, is_primary=True):
+
     # Validate localpart
     if not validate.match(localpart) or localpart[len(localpart)-1:] == '-':
         raise ValueError('Invalid localpart (%r)' % (localpart, ))
 
     # Set / validate domain
-    # NOTE: Cannot use find_by_domain because we have lots of invalid domains
-    #       in our domain table!
-    if (
-            domain != 'adm.uit.no' and
-            domain != 'hin.no' and
-            domain != 'hih.no' and
-            domain != 'nordnorsk.vitensenter.no' and
-            domain != 'kun.uit.no' and
-            domain != 'post.uit.no' and
-            domain not in valid_exchange_domains and
-            domain != 'student.uit.no' and
-            domain != 'nuv.no' and
-            domain != 'norgesuniversitetet.no' and
-            domain != 'sito.no' and
-            domain != 'mailbox.uit.no' and
-            domain != 'hifm.no' and
-            domain != 'asp.uit.no' and
-            domain != 'ad.uit.no' and
-            domain != 'ak.uit.no' and
-            domain != 'jusshjelpa.uit.no' and
-            domain != 'driv.no' and
-            domain != 'samskipnaden.no'):
+    if domain not in valid_exchange_domains:
         logger.error('Can only set emails for domains: %r',
                      valid_exchange_domains)
         raise ValueError('Invalid domain (%r)' % (domain, ))
 
     # Find account
-    ac.clear()
+    ac = Factory.get('Account')(db)
     try:
         ac.find_by_name(account)
     except Exception:
-        logger.error('Account name=%r not found', (account))
-        sys.exit(0)
+        raise ValueError('No account (%r)' % (account, ))
 
     # Re-build email address
     email = '%s@%s' % (localpart, domain)
@@ -109,61 +90,68 @@ def set_mail(account, localpart, domain, is_primary=True):
 
     # Update email tables immediately
     logger.info('Running email processing for account name=%r', account)
+    em = Email.email_address(db, logger=logger)
     em.process_mail(ac.entity_id, email, is_primary)
 
 
-def main():
+def email_type(value):
+    lp, _, dom = value.partition('@')
+    if not lp:
+        raise ValueError('Missing local part')
+    if not dom:
+        raise ValueError('Missing domain')
+    return lp, dom
+
+
+def main(inargs=None):
+    parser = argparse.ArgumentParser(
+        description="This utility creates an exchange email for a user",
+    )
+    parser.add_argument(
+        '-n', '--noprimary',
+        action='store_false',
+        dest='primary',
+        default=True,
+        help='Do not set primary address',
+    )
+
+    parser.add_argument(
+        'account',
+        help='Set email address for account name %(metavar)s',
+    )
+    parser.add_argument(
+        'email',
+        type=email_type,
+        help='Set email address to %(metavar)s',
+    )
+    add_commit_args(parser)
+    Cerebrum.logutils.options.install_subparser(parser)
+
+    args = parser.parse_args(inargs)
+    Cerebrum.logutils.autoconf('tee', args)
+
+    logger.info('Start of %s', parser.prog)
+    logger.debug('args: %r', args)
+
+    db = Factory.get('Database')()
+    db.cl_init(change_program=parser.prog)
 
     try:
-        opts, args = getopt.getopt(
-            sys.argv[1:],
-            'a:e:ndh',
-            ['account=', 'email=', 'noprimary', 'dryrun', 'help'])
-    except getopt.GetoptError as m:
-        usage(1, m)
+        set_mail(db, args.account, args.email[0], args.email[1], args.primary)
+    except ValueError as e:
+        logger.error('Unable to set email: %s', e)
+        raise SystemExit(1)
+    except Exception as e:
+        logger.error('Unhandled error', exc_info=True)
+        raise SystemExit(2)
 
-    dryrun = False
-    account = None
-    email = None
-    primary = True
-    for opt, val in opts:
-        if opt in('-d', '--dryrun'):
-            dryrun = True
-        elif opt in ('-e', '--email='):
-            print("email set to:%s" % val)
-            email = val
-        elif opt in ('-n', '--noprimary'):
-            primary = False
-        elif opt in ('-a', '--account='):
-            print("account set to:%s" % val)
-            account = val
-        elif opt in ('-h', '--help'):
-            usage()
-
-    if account is None or email is None:
-        print("you must set account and email \n")
-        usage()
-
-    splitmail = email.split('@')
-    if len(splitmail) != 2:
-        logger.error('You must specify one localpart and one domain. '
-                     'E.g. example@test.com')
-        usage()
-    set_mail(account, splitmail[0], splitmail[1], primary)
-
-    if (dryrun):
-        db.rollback()
-        logger.info("Dryrun, rollback changes")
-    else:
+    if args.commit:
+        logger.info('Commiting changes')
         db.commit()
-        logger.info("Committing all changes to DB")
-
-
-def usage(exitcode=0, msg=None):
-    if msg:
-        print(msg)
-    print(__doc__)
-    sys.exit(exitcode)
+    else:
+        logger.info('Rolling back changes')
+        db.rollback()
+    logger.info('Done %s', parser.prog)
 
 
 if __name__ == '__main__':
