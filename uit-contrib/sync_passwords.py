037391d5a5313bad1a60639662320b0c47050002
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2019-05-20 19:22:50 +0200

    Rewrite uit database password sync
    Issue: CRB-2816

--- uit-contrib/sync_passwords.py
+++ uit-contrib/sync_passwords.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-# -*- coding: iso-8859-1 -*-
-# Copyright 2002, 2003 University of Oslo, Norway
+# -*- coding: utf-8 -*-
+#
+# Copyright 2002-2019 University of Tromso, Norway
 #
 # This file is part of Cerebrum.
 #
@@ -17,78 +18,58 @@
 # You should have received a copy of the GNU General Public License
 # along with Cerebrum; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+"""
+This script checks if password hashes are the same in the old and new
+Cerebrum databases (old on Caesar, new on Clavius).  If they differ
+the hashes on Caesar are copied to Clavius.
+"""
 
-#
-# This script reads data exported from our HR system PAGA.
-# It is a simple CSV file.
-#
-
-#
-# Generic imports
-#
+import argparse
+import logging
 import os
-import getopt
-import sys
 
-#
-# Cerebrum imports
-#
-import cerebrum_path
 import cereconf
+import Cerebrum.logutils
+import Cerebrum.logutils.options
 from Cerebrum import Errors
 from Cerebrum.Utils import Factory
 from Cerebrum.modules.no.uit.account_bridge import AccountBridge
-
-#
-# global variables
-#
-progname =__file__.split(os.sep)[-1]
-logger = Factory.get_logger("cronjob")
-db = Factory.get('Database')()
-db.cl_init(change_program='sync_passwords')
-ac = Factory.get('Account')(db)
-co = Factory.get('Constants')(db)
+from Cerebrum.utils.argutils import add_commit_args
 
 
-#
-# __doc__ (usage)
-#
-__doc__ = """
-This script checks if password hashes are the same in the old and new 
-Cerebrum databases (old on Caesar, new on Clavius). 
-If they differ the hashes on Caesar are copied to Clavius.
+logger = logging.getLogger(__name__)
 
-usage: %s <-a | -n username | -f filename>
 
-options:
-    -h | --help             - this text
-    -a | --all              - sync passwords for all accounts
-    -f | --file filename    - read usernames from file and sync passwords for them
-    -n username | --name username 
-                            - sync passwords for one account, identified with username
-    -d | --dryrun           - do not change DB
-""" % progname
+def sync_one_account(db, bridge, uname):
+    """
+    Sync passwords for a single account.
 
-def sync_one_account(bridge, uname):
+    :param db: A database connection to sync to
+    :param bridge: An AccountBridge object to sync from
+    :param uname: A username to sync
+    """
+    ac = Factory.get('Account')(db)
+    co = Factory.get('Constants')(db)
+
     auth_data = bridge.get_auth_data(uname)
-    if auth_data == None:
-        logger.warning("Couldn't find account with username '%s' in Caesar database, ignoring it." % uname)
+    if auth_data is None:
+        logger.warning("No account with username=%r on Caesar, ignoring",
+                       uname)
         return
 
     # get auth data from new database
     ac.clear()
     ac.find_by_name(uname)
 
-    # logger.debug("############ %s ############" % uname)
-
     first = True
     equal = True
     # compare auth data from the two databases, logg if differences
     for ad in auth_data:
         auth_method, caesar_data = ad
+        auth_method = co.Authentication(auth_method)
 
         # don't compare auth_data for auth methods that aren't in use
-        if str(co.Authentication(auth_method)) not in cereconf.AUTH_CRYPT_METHODS:
+        if str(auth_method) not in cereconf.AUTH_CRYPT_METHODS:
             continue
 
         try:
@@ -98,90 +79,117 @@ def sync_one_account(bridge, uname):
             # ignore it
             continue
 
-        if not caesar_data == clavius_data:
-            if first == True:
-                logger.debug("############ %s has auth_data that differs ############" % uname)
+        if caesar_data != clavius_data:
+            if first:
+                logger.debug("CHANGE: username=%r has auth_data that differs",
+                             uname)
                 first = False
                 equal = False
 
-            logger.debug("type: %s" % auth_method)
-            logger.debug("hash from Caesar: %s" % caesar_data)
-            logger.debug("hash from Clavius: %s" % clavius_data)
-            logger.debug("#####")
+            logger.debug("username=%r, type=%s, caesar=%r, clavius=%r",
+                         uname, auth_method, caesar_data, clavius_data)
     if not equal:
-        logger.debug("######################################################")
+        logger.debug("NO CHANGE: username=%r has no changes")
 
-    # Note: all auth_data from Caesar is written to Clavius, regardless of what type it is.
+    # Note: all auth_data from Caesar is written to Clavius, regardless of what
+    # type it is.
 
     # update auth_data in Clavius database with auth_data from Caesar
     ac.set_auth_data(auth_data)
     ac.write_db()
 
-def sync_from_file(bridge, filename):
-    # read usernames from file
-    with open(filename, "r") as fh:
-        unames = fh.readlines()
 
-    # sync passwords for these usernames
-    for uname in unames:
-        if uname[0] != '\n':
-            # line is not empty
-            sync_one_account(bridge, uname.rstrip())
+def sync_many(db, bridge, usernames):
+    for username in usernames:
+        sync_one_account(db, bridge, username)
 
-def sync_all(bridge):
-    all_accounts = ac.list_all(filter_expired=False)
-    for acc in all_accounts:
-        sync_one_account(bridge, acc['entity_name'])
 
-def main():
-    do_all = False
-    filename = None
-    uname = None
-    dryrun = False
+def read_usernames(filename):
+    """Read usernames from file."""
+    logger.info("Reading usernames from %r", filename)
+    count = 0
+    with open(filename, 'r') as f:
+        for line in f:
+            username = line.strip()
+            if username:
+                count += 1
+                yield username
+    logger.info("Read %d usernames from %r", count, filename)
 
-    try:
-        opts,args=getopt.getopt(sys.argv[1:],'haf:n:d',['help','all','file=','name=','dryrun'])
-    except getopt.GetoptError,m:
-        logger.error("Unknown option: %s" % (m))
-        usage(m)
 
-    for opt,val in opts:
-        if opt in ('-h','--help'):
-            usage()
-        if opt in ('-a','--all'):
-            do_all = True
-        if opt in ('-f','--file'):
-            filename = val
-        if opt in ('-n','--name'):
-            uname = val
-        if opt in ('-d','--dryrun'):
-            dryrun = True
+def fetch_usernames(db):
+    """Fetch usernames from database."""
+    logger.info("Reading usernames from db")
+    ac = Factory.get('Account')(db)
+    count = 0
+    for row in ac.list_all(filter_expired=False):
+        count += 1
+        yield row['entity_name']
+    logger.info("Read %d usernames from db", count)
+
+
+default_log_preset = getattr(cereconf, 'DEFAULT_LOGGER_TARGET', 'console')
+
+
+def existing_file(filename):
+    if not os.path.exists(filename):
+        raise ValueError("File %r seems to be missing" % (filename, ))
+    return filename
+
+
+def main(inargs=None):
+    parser = argparse.ArgumentParser(
+        description="Sync passwords from Caesar to current database",
+    )
+    what = parser.add_mutually_exclusive_group(required=True)
+    what.add_argument(
+        '-a', '--all',
+        action='store_true',
+        default=False,
+        help='Sync all users in current database',
+    )
+    what.add_argument(
+        '-f', '--file',
+        dest='filename',
+        type=existing_file,
+        help='Sync usernames from file %(metavar)s',
+        metavar='FILE',
+    )
+    what.add_argument(
+        '-n', '--name',
+        dest='username',
+        help='Sync a single username %(metavar)s',
+        metavar='USER',
+    )
+    add_commit_args(parser)
+    Cerebrum.logutils.options.install_subparser(parser)
+
+    args = parser.parse_args(inargs)
+    Cerebrum.logutils.autoconf(default_log_preset, args)
+
+    logger.info('Start %s', parser.prog)
+    logger.debug('args: %r', args)
+
+    db = Factory.get('Database')()
+    db.cl_init(change_program='sync_passwords')
 
     with AccountBridge() as bridge:
-        if uname:
-            sync_one_account(bridge, uname)
-        elif filename:
-            if (os.path.exists(filename) != True):
-                msg = "File %s seems to be missing" % filename
-                usage(msg)
-            sync_from_file(bridge, filename)
-        elif do_all:
-            sync_all(bridge)
-        else:
-            usage()
+        if args.username:
+            sync_one_account(bridge, args.username)
+        elif args.filename:
+            sync_many(db, bridge, read_usernames(args.filename))
+        elif args.all:
+            sync_many(db, bridge, fetch_usernames(db))
 
-    if dryrun:
-        db.rollback()
-        logger.info("Dryrun, rollback changes")
-    else:
+    if args.commit:
+        logger.info('Commiting changes')
         db.commit()
-        logger.info("Committing all changes to DB")
+    else:
+        db.rollback()
+        logger.info('Rolling back changes')
 
-def usage(message=None):
-    if message:
-        print message
-    print __doc__
-    sys.exit(1)
+    logger.info('Done %s', parser.prog)
+
 
 if __name__ == '__main__':
     main()
