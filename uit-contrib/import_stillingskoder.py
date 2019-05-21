9e74d1fd09cd413aadb8ca86a3a750634a8709c7
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2019-05-21 16:08:41 +0200

    Fix code style in import_stillingskoder.py
    Issue: CRB-2821

--- uit-contrib/import_stillingskoder.py
+++ uit-contrib/import_stillingskoder.py
@@ -1,6 +1,7 @@
-#!/bin/env python
-# -*- coding: iso-8859-1 -*-
-# Copyright 2002, 2003 University of Oslo, Norway
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2002-2019 University of Oslo, Norway
 #
 # This file is part of Cerebrum.
 #
@@ -18,14 +19,23 @@
 # along with Cerebrum; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
+import csv
+import getopt
+import os
+import sys
+
+import cereconf
+
+from Cerebrum.Utils import Factory
+
 progname = __file__.split("/")[-1]
-__doc__="""
-    
+__doc__ = """
+
     This program reads a csv file containing 3 fields:
     Stillingskode,Stillingsbetegnelse;stillingskategori
     Content of this file is synced with table person_stillingskoder in Cerebrum
-    
-    Usage:    
+
+    Usage:
     %s [options]
     options:
     -h|--help                : Show this message
@@ -36,178 +46,179 @@ __doc__="""
 
     """ % (progname,)
 
-import sys
-import getopt
-import time
-import os
-import csv
-
-import cerebrum_path
-import cereconf
-
-from Cerebrum.Utils import Factory
-
 logger = Factory.get_logger('console')
-db=Factory.get('Database')()
-CHARSEP=';'
-default_file=os.path.join(cereconf.CB_SOURCEDATA_PATH,'stillingskoder.csv')
+db = Factory.get('Database')()
+CHARSEP = ';'
+default_file = os.path.join(cereconf.CB_SOURCEDATA_PATH, 'stillingskoder.csv')
 
 
 def parse_source_file(filename):
-    result=dict()
-    for detail in csv.DictReader(open(filename,'r'),delimiter=CHARSEP):
+    result = dict()
+    for detail in csv.DictReader(open(filename, 'r'), delimiter=CHARSEP):
         stbnv = detail['Stillingsbetegnelse']
         stkat = detail['UiT Stillingskategori']
-        result[int(detail['Stillingskode'])]=(stbnv,stkat)
-    logger.info("Loaded sourcefile %s, found %d kodes" % (filename,len(result)))
+        result[int(detail['Stillingskode'])] = (stbnv, stkat)
+    logger.info("Loaded sourcefile %s, found %d kodes", filename, len(result))
     return result
 
 
-class stillingskoder(object):
-    
-    def __init__(self,db):
-        self.db=db
-    
-    def insert(self,skode,sbeneving,skategori):
-        qry="""
+class Stillingskoder(object):
+
+    def __init__(self, db):
+        self.db = db
+
+    def insert(self, skode, sbeneving, skategori):
+        qry = """
         INSERT INTO
-            [:table schema=cerebrum name=person_stillingskoder]
-            (stillingskode,stillingstittel,stillingstype)
+            [:table schema = cerebrum name = person_stillingskoder]
+            (stillingskode, stillingstittel, stillingstype)
         VALUES
             (:skode, :sbeneving, :skategori)
         """
-        params={'skode':skode, 'skategori':skategori, 'sbeneving':sbeneving}
-        self.db.execute(qry,params)
+        params = {
+            'skode': skode,
+            'skategori': skategori,
+            'sbeneving': sbeneving,
+        }
+        self.db.execute(qry, params)
 
-    def delete(self,skode):        
-        qry="""
-        DELETE 
-        FROM 
-            [:table schema=cerebrum name=person_stillingskoder]
-        WHERE 
-            stillingskode=:skode
+    def delete(self, skode):
+        qry = """
+        DELETE
+        FROM
+            [:table schema = cerebrum name = person_stillingskoder]
+        WHERE
+            stillingskode = :skode
         """
-        self.db.execute(qry,{'skode':skode})
+        self.db.execute(qry, {'skode': skode})
 
-    def update(self,skode,sbeneving,skategori):
-        qry="""
-        UPDATE 
-            [:table schema=cerebrum name=person_stillingskoder]
-        SET 
-            stillingstype=:skategori, stillingstittel=:sbeneving
-        WHERE 
-            stillingskode=:skode
+    def update(self, skode, sbeneving, skategori):
+        qry = """
+        UPDATE
+            [:table schema = cerebrum name = person_stillingskoder]
+        SET
+            stillingstype = :skategori, stillingstittel = :sbeneving
+        WHERE
+            stillingskode = :skode
         """
-        params={'skode':skode, 'skategori':skategori, 'sbeneving':sbeneving}
-        self.db.execute(qry,params)
-    
-    def list_stillingskoder(self,skode=None,skat=None):
-        filter=[]
-        params=dict()
-        
+        params = {
+            'skode': skode,
+            'skategori': skategori,
+            'sbeneving': sbeneving,
+        }
+        self.db.execute(qry, params)
+
+    def list_stillingskoder(self, skode=None, skat=None):
+        filter = []
+        params = dict()
+
         if skode:
-            filter.append('skode=:skode')
-            params['skode']=skode      
+            filter.append('skode = :skode')
+            params['skode'] = skode
 
         if skat:
-            filter.append('stillingstype=:skat')
-            params['skat']=skat
+            filter.append('stillingstype = :skat')
+            params['skat'] = skat
 
-        where=""
+        where = ""
         if filter:
-            where="WHERE %s" % (" AND ".join(filter))
+            where = "WHERE %s" % (" AND ".join(filter))
 
-        qry="""
-        SELECT 
-            stillingskode,stillingstittel,stillingstype 
-        FROM 
-            [:table schema=cerebrum name=person_stillingskoder]
+        qry = """
+        SELECT
+            stillingskode, stillingstittel, stillingstype
+        FROM
+            [:table schema = cerebrum name = person_stillingskoder]
         %s
-        """ % where        
-        return self.db.query(qry,{'skode':skode})    
-    
-        
-def sync_skoder(current,new):
-    
-    current_set=set(current.keys())
-    new_set=set(new.keys())
-    
-    to_add=new_set.difference(current_set)
-    to_delete=current_set.difference(new_set)
-    to_update=new_set.intersection(current_set)
-        
-    #add new
+        """ % where
+        return self.db.query(qry, {'skode': skode})
+
+
+def sync_skoder(current, new):
+
+    current_set = set(current.keys())
+    new_set = set(new.keys())
+
+    to_add = new_set.difference(current_set)
+    to_delete = current_set.difference(new_set)
+    to_update = new_set.intersection(current_set)
+
+    # add new
     for skode in to_add:
-        sben,skat=new.get(skode)
-        logger.info("Insert new stillingskode=%04d, tittel=%s, type=%s" % \
-            (skode,sben,skat))
-        skode_obj.insert(skode,sben,skat)
-        
-    # remove old 
+        sben, skat = new.get(skode)
+        logger.info("Insert new stillingskode=%04d, tittel=%r, type=%r",
+                    skode, sben, skat)
+        skode_obj.insert(skode, sben, skat)
+
+    # remove old
     for skode in to_delete:
-        logger.info("Delete stillingskode=%04d" % (skode,))
+        logger.info("Delete stillingskode=%04d", skode)
         skode_obj.delete(skode)
 
     # update remaining
-    updated=False
+    updated = False
     for skode in to_update:
-        new_sben,new_skat=new.get(skode)
-        old_sben,old_skat=current.get(skode)
-        if new_sben!=old_sben or new_skat!=old_skat:
-            updated+=1
-            logger.info("Update stillingskode=%04d, new:%s, old=%s" % \
-                (skode,(new_sben,new_skat),(old_sben,old_skat)))
-            new_sben=unicode(new_sben,'utf-8').encode('iso-8859-1')
-            new_skat=unicode(new_skat,'utf-8').encode('iso-8859-1')
-            skode_obj.update(skode,new_sben,new_skat)
+        new_sben, new_skat = new.get(skode)
+        old_sben, old_skat = current.get(skode)
+        if new_sben != old_sben or new_skat != old_skat:
+            updated += 1
+            logger.info("Update stillingskode = %04d, new=%r, old=%r",
+                        skode, (new_sben, new_skat), (old_sben, old_skat))
+            new_sben = unicode(new_sben, 'utf-8').encode('iso-8859-1')
+            new_skat = unicode(new_skat, 'utf-8').encode('iso-8859-1')
+            skode_obj.update(skode, new_sben, new_skat)
     if to_add or to_delete or updated:
-        logger.info("Added %d kodes, removed %d kodes and updated %d kodes" % \
-            (len(to_add),len(to_delete),updated))
-    
+        logger.info("Added %d kodes, removed %d kodes and updated %d kodes",
+                    len(to_add), len(to_delete), updated)
 
-def usage(exit_code=0,msg=None):
 
-    if msg: print msg
+def usage(exit_code=0, msg=None):
+    if msg:
+        print msg
     print __doc__
     sys.exit(exit_code)
 
 
 def main():
     global skode_obj
-    
+
     # lets set default file
-    kode_file=default_file 
-    dryrun=False
+    kode_file = default_file
+    dryrun = False
     try:
-        opts,args = getopt.getopt(sys.argv[1:],'k:hd',
-            ['kodefile=','help','dryrun'])
-    except getopt.GetoptError,m:
-        usage(1,m)
+        opts, args = getopt.getopt(
+            sys.argv[1:],
+            'k:hd',
+            ['kodefile = ', 'help', 'dryrun'])
+    except getopt.GetoptError as m:
+        usage(1, m)
 
-    for opt,val in opts:
-        if opt in ('-k','--kodefile'):
+    for opt, val in opts:
+        if opt in ('-k', '--kodefile'):
             kode_file = val
-        elif opt in ('-h','--help'):
+        elif opt in ('-h', '--help'):
             usage()
-        elif opt in ('-d','--dryrun'):
-            dryrun=True
+        elif opt in ('-d', '--dryrun'):
+            dryrun = True
 
-    skode_obj=stillingskoder(db)
-    new_skode=parse_source_file(kode_file)
-    current_skode=dict()
-    for skode,sbeneving,skat in skode_obj.list_stillingskoder():
-        def _utf_name(name, encoding = 'iso-8859-1'):
+    skode_obj = Stillingskoder(db)
+    new_skode = parse_source_file(kode_file)
+    current_skode = dict()
+    for skode, sbeneving, skat in skode_obj.list_stillingskoder():
+        def _utf_name(name, encoding='iso-8859-1'):
             return unicode(name, encoding).encode('utf-8')
-        current_skode[int(skode)]=(_utf_name(sbeneving),_utf_name(skat))
-        
-    sync_skoder(current_skode,new_skode)
-    
-    if (dryrun):
+        current_skode[int(skode)] = (_utf_name(sbeneving),
+                                     _utf_name(skat))
+
+    sync_skoder(current_skode, new_skode)
+
+    if dryrun:
         db.rollback()
         logger.info("Dryrun, rollback changes")
     else:
         db.commit()
         logger.info("Committing all changes to DB")
+
 
 if __name__ == '__main__':
     main()
