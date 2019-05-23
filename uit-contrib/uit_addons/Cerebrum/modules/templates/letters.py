d8236625f7743e3be45c69fd690d63c0ddc9e328
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2019-05-23 15:14:39 +0200

    Fix code style issues in patched letters module

--- uit-contrib/uit_addons/Cerebrum/modules/templates/letters.py
+++ uit-contrib/uit_addons/Cerebrum/modules/templates/letters.py
@@ -1,6 +1,6 @@
-# -*- coding: iso-8859-1 -*-
-
-# Copyright 2003 University of Oslo, Norway
+# -*- coding: utf-8 -*-
+#
+# Copyright 2003-2019 University of Oslo, Norway
 #
 # This file is part of Cerebrum.
 #
@@ -17,15 +17,27 @@
 # You should have received a copy of the GNU General Public License
 # along with Cerebrum; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+"""
+Legacy password letters module for Cerebrum
 
+History
+-------
+This module was removed, and LaTeX templating replaced with HTML/CSS templates
+(the current Cerebrum.modules.templates). It has temporarily been restored in
+order to merge UiT code with the Cerebrum repository.
+"""
 import os
 import re
 import string
+
 import cereconf
+
 from Cerebrum import Utils
 
+
 class TemplateHandler(object):
-    """Handling of templates for letters.
+    """
+    Handling of templates for letters.
 
     Templates are stored in directories like this:
        no/new_password.tex
@@ -47,29 +59,36 @@ class TemplateHandler(object):
           print footer
 
     """
-
     def __init__(self, lang=None, tplname=None, type=None):
         if lang is not None:
             self._type = type
-            (self._hdr, self._body, self._footer) = self.read_templates(lang, tplname)
+            (self._hdr,
+             self._body,
+             self._footer) = self.read_templates(lang, tplname)
 
     def read_templates(self, lang, tplname):
-        pathinfo = (cereconf.TEMPLATE_DIR, lang, tplname, self._type)
-        f = open("%s/%s/%s.%s" % pathinfo, 'rb')
-        hdr = body = footer = ''
-        for t in f.readlines():
-            if t.startswith("<BODY>"):
-                f2 = open("%s/%s/%s_body.%s" % pathinfo, 'rb')
-                for t2 in f2.readlines():
-                    body += t2
-            else:
-                if not body:
-                    hdr += t
+        filename_hdr = os.path.join(
+            cereconf.TEMPLATE_DIR, lang,
+            "{template}.{ext}".format(tplname, self._type))
+        filename_body = os.path.join(
+            cereconf.TEMPLATE_DIR, lang,
+            "{template}_body.{ext}".format(tplname, self._type))
+
+        with open(filename_hdr, 'rb') as f:
+            hdr = body = footer = ''
+            for t in f.readlines():
+                if t.startswith("<BODY>"):
+                    with open(filename_body, 'rb') as f2:
+                        for t2 in f2.readlines():
+                            body += t2
                 else:
-                    footer += t
-        if not body:
-            return (None, hdr, None)
-        return (hdr, body, footer)
+                    if not body:
+                        hdr += t
+                    else:
+                        footer += t
+            if not body:
+                return (None, hdr, None)
+            return (hdr, body, footer)
 
     def apply_template(self, template, mapping, no_quote=()):
         """applies mapping to hdr, body or footer template and returns
@@ -114,40 +133,55 @@ class TemplateHandler(object):
 
     # TODO: Remove use of spool_job, and get rid of:
     def _tail(self, fname, num=-1):
-        f = file(fname)
-        ret = f.readlines()[-num]
-        return "".join(ret).rstrip()
+        with open(fname) as f:
+            ret = f.readlines()[-num]
+            return "".join(ret).rstrip()
 
     def spool_job(self, filename, type, printer, skip_lpr=False, logfile=None,
                   lpr_user='unknown', def_lpr_cmd=None):
-        """Spools the job.  The spool command is executed in the
-        directory where filename resides."""
+        """
+        Spools the job.
+
+        The spool command is executed in the directory where filename
+        resides.
+        """
         if logfile is None:
             logfile = Utils.make_temp_file(only_name=True)
         self.logfile = logfile
         old_dir = os.getcwd()
         if os.path.dirname(filename):
             os.chdir(os.path.dirname(filename))
-        base_filename = filename[:filename.rindex('.')] 
+        base_filename = filename[:filename.rindex('.')]
         try:
             if cereconf.PRINT_DVIPS_CMD:
-                format_sys_cmd = "%s -f < %s.dvi > %s.ps 2>> %s" % (cereconf.PRINT_DVIPS_CMD,
-                                                                    base_filename, base_filename,
-                                                                    logfile)
+                format_sys_cmd = "%s -f < %s.dvi > %s.ps 2>> %s" % (
+                    cereconf.PRINT_DVIPS_CMD,
+                    base_filename,
+                    base_filename,
+                    logfile,
+                )
                 base_filename += ".ps"
             elif cereconf.PRINT_DVIPDF_CMD:
-                format_sys_cmd = "%s %s.dvi %s.pdf 2>> %s" % (cereconf.PRINT_DVIPDF_CMD,
-                                                              base_filename, base_filename,
-                                                              logfile)
+                format_sys_cmd = "%s %s.dvi %s.pdf 2>> %s" % (
+                    cereconf.PRINT_DVIPDF_CMD,
+                    base_filename,
+                    base_filename,
+                    logfile,
+                )
                 base_filename += ".pdf"
             else:
-                raise IOError("Error spooling job, see %s for details" % logfile)
+                raise IOError("Error spooling job, see %s for details" %
+                              (logfile, ))
             if type == 'tex':
-                status = (os.system("%s --interaction nonstopmode %s >> %s 2>&1" % (
-                    cereconf.PRINT_LATEX_CMD, filename, logfile)) or
-                          os.system("%s" % (format_sys_cmd)))
+                tex_cmd = "%s --interaction nonstopmode %s >> %s 2>&1" % (
+                    cereconf.PRINT_LATEX_CMD,
+                    filename,
+                    logfile,
+                )
+                status = os.system(tex_cmd) or os.system(format_sys_cmd)
                 if status:
-                    raise IOError("Error spooling job, see %s for details" % logfile)
+                    raise IOError("Error spooling job, see %s for details" %
+                                  (logfile, ))
             if not skip_lpr:
                 if printer is not None and re.search(r'[^a-z0-9\-_]', printer):
                     raise IOError("Bad printer name")
@@ -159,19 +193,26 @@ class TemplateHandler(object):
 
                 # Assemble parameters that might be of use for further
                 # handling of the job. Contents of def_lpr_cmd/
-                # cereconf.PRINT_LPR_CMD determine what is actually used and for
-                # what purpose
-                lpr_params = {'filename': base_filename,
-                              'uname': lpr_user,
-                              'printer': printer,
-                              'hostname': os.uname()[1]}
+                # cereconf.PRINT_LPR_CMD determine what is actually used and
+                # for what purpose
+                lpr_params = {
+                    'filename': base_filename,
+                    'uname': lpr_user,
+                    'printer': printer,
+                    'hostname': os.uname()[1],
+                }
 
-                status = os.system("%s >> %s 2>&1" % (lpr_cmd.substitute(lpr_params), logfile))
+                status = os.system("%s >> %s 2>&1" % (
+                    lpr_cmd.substitute(lpr_params),
+                    logfile,
+                ))
                 if status:
-                    raise IOError("Error spooling job, see %s for details (tail: %s)"
-                                  % (logfile, self._tail(logfile, num=1)))
+                    raise IOError(
+                        "Error spooling job, see %s for details (tail: %s)"
+                        % (logfile, self._tail(logfile, num=1)))
         finally:
             os.chdir(old_dir)
+
 
 if __name__ == '__main__':
     th = TemplateHandler('no_NO/printer', 'breg', 'ps')
