00a15536a801443ebfb9977d2b4a8e6d43ab9f5c
Marius Jean Bischof Hagalid <marius.hagalid@usit.uio.no>
2019-05-29 13:02:49 +0200

    Do more clean up
    - Replace getopts with argparse
    - Remove globals
    - Use the new logger
    
    Issue: CRB-2774

--- contrib/no/uit/misc/move_emails.py
+++ contrib/no/uit/misc/move_emails.py
@@ -17,27 +17,29 @@
 # You should have received a copy of the GNU General Public License
 # along with Cerebrum; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-"""This script reads data exported from our HR system PAGA.
-   It is a simple CSV file."""
+"""
+This script moves all e-mailaddresses in a given domain from one account to
+another. It reads data exported from our HR system PAGA from a simple CSV file.
+"""
 
-import getopt
+import argparse
 import sys
+import logging
+
 import cereconf
+import Cerebrum.logutils
+from Cerebrum.utils.argutils import add_commit_args
 from Cerebrum.Utils import Factory
 from Cerebrum import Errors
 
-# Init
-db = Factory.get('Database')()
-ac = Factory.get('Account')(db)
-pe = Factory.get('Person')(db)
-co = Factory.get('Constants')(db)
+logger = logging.getLogger(__name__)
 
-logger = Factory.get_logger("console")
-
-default_maildomain = cereconf.NO_MAILBOX_DOMAIN_EMPLOYEES
+DEFAULT_MAIL_DOMAIN = cereconf.NO_MAILBOX_DOMAIN_EMPLOYEES
 
 
-def process_change(from_acc, to_acc, maildomain, primary):
+def process_change(from_acc, to_acc, maildomain, primary, db):
+    # Init
+    ac = Factory.get('Account')(db)
     primary_id = None
 
     # Validate and get acc_id
@@ -183,7 +185,7 @@ def process_change(from_acc, to_acc, maildomain, primary):
         )
 
     # Update ad_email table if current domain is the ad_email domain
-    if primary and default_maildomain == maildomain:
+    if primary and DEFAULT_MAIL_DOMAIN == maildomain:
         try:
             # Only attempt to update ad_emailtable if the from acc has an
             # entry here
@@ -220,7 +222,7 @@ def process_change(from_acc, to_acc, maildomain, primary):
         except Errors.NotFoundError:
             logger.info("Couldn't update ad_email table from %s to %s" % (
                 from_acc, to_acc))
-    elif default_maildomain == maildomain:
+    elif DEFAULT_MAIL_DOMAIN == maildomain:
         logger.info("Deleting ad_email table for %s" % from_acc)
         db.execute(
             """
@@ -232,56 +234,50 @@ def process_change(from_acc, to_acc, maildomain, primary):
 
 
 def main():
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], 'f:t:pm:d',
-                                   ['from=', 'to=', 'primary', 'maildomain=',
-                                    'dryrun'])
-    except getopt.GetoptError:
-        usage(1)
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        '-f', '--from_acc',
+        required=True,
+        help='account name to move e-mailaddresses from'
+    )
+    parser.add_argument(
+        '-t', '--to_acc',
+        required=True,
+        help='account name to move e-mailaddresses to'
+    )
+    parser.add_argument(
+        '-m', '--maildomain',
+        default=DEFAULT_MAIL_DOMAIN,
+        help='maildomain to process. Defaults to {}'.format(
+            DEFAULT_MAIL_DOMAIN)
+    )
+    parser.add_argument(
+        '-p', '--primary',
+        action='store_true',
+        help="set from's primary address as to's primary address"
+    )
+    parser = add_commit_args(parser, default=True)
 
-    from_acc = to_acc = maildomain = None
-    dryrun = primary = False
-    for opt, val in opts:
-        if opt in ('-h', '--help'):
-            usage()
-        elif opt in ('-f', '--from'):
-            from_acc = val
-        elif opt in ('-t', '--to'):
-            to_acc = val
-        elif opt in ('-m', '--maildomain'):
-            maildomain = val
-        elif opt in ('-p', '--primary'):
-            primary = True
-        elif opt in ('-d', '--dryrun'):
-            dryrun = True
+    Cerebrum.logutils.options.install_subparser(parser)
+    args = parser.parse_args()
+    Cerebrum.logutils.autoconf('console', args)
 
-    if maildomain is None:
-        maildomain = default_maildomain
-
-    if from_acc is None or to_acc is None or from_acc == to_acc:
-        usage(1)
+    if args.from_acc == args.to_acc:
+        logger.error('Arguments "from" and "to" are not allowed to be the same'
+                     ' value!')
+        sys.exit()
 
     logger.info("Starting to process")
-    process_change(from_acc, to_acc, maildomain, primary)
+    db = Factory.get('Database')()
+    process_change(args.from_acc, args.to_acc, args.maildomain, args.primary,
+                   db)
 
-    if (dryrun):
-        db.rollback()
-        logger.info("Dryrun, rollback changes")
-    else:
+    if args.commit:
         db.commit()
         logger.info("Committing all changes to DB")
-
-
-def usage(exit_code=0):
-    print """This script moves all e-mailaddresses in a given domain from one account to another
-    -f | --from:       account name to move e-mailaddresses from
-    -t | --to:         account name to move e-mailaddresses to
-    -m | --maildomain: maildomain to process. Defaults to %s
-    -p | --primary:    set from's primary address as to's primary address
-    -d | --dryrun:     roll back changes in the end
-    -h | --help:       this help message""" % (default_maildomain)
-
-    sys.exit(exit_code)
+    else:
+        db.rollback()
+        logger.info("Dryrun, rollback changes")
 
 
 if __name__ == '__main__':
