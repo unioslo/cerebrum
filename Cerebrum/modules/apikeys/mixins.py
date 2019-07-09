24fc7353c315bb8288103a048f51e3b20422d7b6
Fredrik Larsen <fredrik.h.larsen@gmail.com>
2019-07-09 16:09:25 +0200

    Add account mixin to remove apikeys on delete

---
+++ Cerebrum/modules/apikeys/mixins.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+"""
+Mixins related to mod_apikeys.
+"""
+from Cerebrum.Account import Account
+from .dbal import ApiKeys
+
+
+class ApiKeyMixin(Account):
+    """
+    Account mixin class that provides api key cleanup.
+    """
+
+    def delete(self):
+        """Delete any account apikeys."""
+        # Delete any existing API key if account is deleted.
+        keys = ApiKeys(self._db)
+        if keys.exists(self.entity_id):
+            keys.delete(self.entity_id)
+        super(ApiKeyMixin, self).delete()
