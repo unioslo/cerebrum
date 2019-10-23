32f89cf7c66c897ca67969124fe411f503a54049
Axel Sanner <axel@xeneta.com>
2019-10-23 17:09:36 +0200

    Initial commit of new Auth module

---
+++ Cerebrum/auth/Auth.py
@@ -0,0 +1,238 @@
+import base64
+import crypt
+import hashlib
+import passlib
+import passlib.hash
+import string
+import six
+
+from collections import MutableMapping
+
+from Cerebrum import Utils
+
+import cereconf
+
+
+class AuthBaseClass(object):
+
+    def encrypt_password(self):
+        """Returns the plaintext hashed according to the specified
+        method.  A mixin for a new method should not call super for
+        the method it handles.
+
+        This should be fixed for python3
+
+        :type method: Constants.AccountAuthentication
+        :param method: Some auth_type_x constant
+
+        :type plaintext: String (unicode)
+        :param plaintext: The plaintext to hash
+
+        :type salt: String (unicode)
+        :param salt: Salt for hashing
+
+        :type binary: bool
+        :param binary: Treat plaintext as binary data
+        """
+        raise NotImplementedError
+
+    def decrypt_password(self):
+        """Returns the decrypted plaintext according to the specified
+        method.  If decryption is impossible, NotImplementedError is
+        raised.  A mixin for a new method should not call super for
+        the method it handles.
+        """
+        raise NotImplementedError
+
+    def verify_hash(self):
+        """Returns True if the plaintext matches the cryptstring,
+        False if it doesn't.  If the method doesn't support
+        verification, NotImplemented is returned.
+        """
+        raise NotImplementedError
+
+
+class AuthMap(MutableMapping):
+    def __setitem__(self, key, item):
+        self.__dict__[key] = item
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
+
+    def __len__(self):
+        return len(self.__dict__)
+
+    def __delitem__(self, key):
+        del self.__dict__[key]
+
+    def __iter__(self):
+        return iter(self.__dict__)
+
+    def update(self, *args, **kwargs):
+        return self.__dict__.update(*args, **kwargs)
+
+    def __call__(self, method_key):
+        def wrapper(cls):
+            self.__dict__[str(method_key)] = cls
+            return cls
+        return wrapper
+
+
+all_auth_methods = AuthMap()
+
+
+@all_auth_methods('auth_type_ssha')
+class AuthTypeSSHA(AuthBaseClass):
+    def encrypt_password(self, plaintext, salt=None, binary=False):
+        if not binary:
+            assert(isinstance(plaintext, six.text_type))
+            plaintext = plaintext.encode('utf-8')
+        if salt is None:
+            saltchars = string.ascii_letters + string.digits + "./"
+            salt = "$6$" + Utils.random_string(16, saltchars)
+        salt = salt.encode('utf-8')
+        return base64.b64encode(
+            hashlib.sha1(plaintext + salt).digest() + salt).decode()
+
+
+@all_auth_methods('auth_type_sha256')
+class AuthTypeSHA256(AuthBaseClass):
+    def encrypt_password(self, plaintext, salt=None, binary=False):
+        if not binary:
+            assert(isinstance(plaintext, six.text_type))
+            plaintext = plaintext.encode('utf-8')
+        if salt is None:
+            saltchars = string.ascii_letters + string.digits + "./"
+            salt = "$5$" + Utils.random_string(16, saltchars)
+        return crypt.crypt(plaintext, salt.encode('utf-8')).decode()
+
+
+@all_auth_methods('auth_type_sha512')
+class AuthTypeSHA512(AuthBaseClass):
+    def encrypt_password(self, plaintext, salt=None, binary=False):
+        if not binary:
+            assert(isinstance(plaintext, six.text_type))
+            plaintext = plaintext.encode('utf-8')
+        if salt is None:
+            saltchars = string.ascii_letters + string.digits + "./"
+            salt = "$6$" + Utils.random_string(16, saltchars)
+        return crypt.crypt(plaintext, salt.encode('utf-8')).decode()
+
+
+@all_auth_methods('auth_type_md5')
+class AuthTypeMD5(AuthBaseClass):
+    def encrypt_password(self, plaintext, salt=None, binary=False):
+        if not binary:
+            assert(isinstance(plaintext, six.text_type))
+            plaintext = plaintext.encode('utf-8')
+        if salt is None:
+            saltchars = string.ascii_letters + string.digits + "./"
+            salt = "$1$" + Utils.random_string(8, saltchars)
+        return crypt.crypt(plaintext, salt.encode('utf-8')).decode()
+
+
+@all_auth_methods('auth_type_md4_nt')
+class AuthTypeMD4NT(AuthBaseClass):
+    def encrypt_password(self, plaintext, salt=None, binary=False):
+        if not binary:
+            assert(isinstance(plaintext, six.text_type))
+            plaintext = plaintext.encode('utf-8')
+        # Previously the smbpasswd module was used to create nthash, and it
+        # only produced uppercase hashes. The hash is case insensitive, but
+        # be backwards compatible if some comsumers
+        # depend on upper case strings.
+        return passlib.hash.nthash.hash(plaintext).decode().upper()
+
+
+@all_auth_methods('auth_type_plaintext')
+class AuthTypePlaintext(AuthBaseClass):
+    def encrypt_password(self, plaintext, salt=None, binary=False):
+        if not binary:
+            assert(isinstance(plaintext, six.text_type))
+            plaintext = plaintext.encode('utf-8')
+        return plaintext
+
+
+@all_auth_methods('auth_type_md5_unsalt')
+class AuthTypeMD5Unsalt(AuthBaseClass):
+    def encrypt_password(self, plaintext, salt=None, binary=False):
+        if not binary:
+            assert(isinstance(plaintext, six.text_type))
+            plaintext = plaintext.encode('utf-8')
+        return hashlib.md5(plaintext).hexdigest().decode()
+
+
+@all_auth_methods('auth_type_ha1_md5')
+class AuthTypeHA1MD5(AuthBaseClass):
+    def encrypt_password(self, plaintext, salt=None, binary=False):
+        if not binary:
+            assert(isinstance(plaintext, six.text_type))
+            plaintext = plaintext.encode('utf-8')
+        # TODO: This needs some things from Account
+        # FIXME: !
+        s = ":".join([self.account_name, cereconf.AUTH_HA1_REALM, plaintext])
+        return hashlib.md5(s.encode('utf-8')).hexdigest().decode()
+
+
+cereconf.AUTH_CRYPT_METHODS = [
+    'auth_type_ssha',
+    'auth_type_sha256',
+    'auth_type_sha512',
+    'auth_type_md5',
+    'auth_type_md4_nt',
+    'auth_type_plaintext',
+    'auth_type_md5_unsalt',
+    'auth_type_ha1_md5'
+]
+
+
+def get_crypt_methods():
+    auth_crypt_methods = AuthMap()
+    for m in cereconf.AUTH_CRYPT_METHODS:
+        auth_crypt_methods[m] = all_auth_methods[m]
+    return auth_crypt_methods
+
+
+def decrypt_password(self, method, cryptstring):
+    """Returns the decrypted plaintext according to the specified
+    method.  If decryption is impossible, NotImplementedError is
+    raised.  A mixin for a new method should not call super for
+    the method it handles.
+    """
+    if method in (self.const.auth_type_md5_crypt,
+                  self.const.auth_type_ha1_md5,
+                  self.const.auth_type_sha256_crypt,
+                  self.const.auth_type_sha512_crypt,
+                  self.const.auth_type_md4_nt):
+        raise NotImplementedError(
+            "Can't decrypt {method}".format(method=method))
+    elif method == self.const.auth_type_plaintext:
+        return cryptstring
+    raise ValueError('Unknown method {method}'.format(method=method))
+
+
+def verify_password(self, method, plaintext, cryptstring):
+    """Returns True if the plaintext matches the cryptstring,
+    False if it doesn't.  If the method doesn't support
+    verification, NotImplemented is returned.
+    """
+    if method not in (self.const.auth_type_md5_crypt,
+                      self.const.auth_type_md5_unsalt,
+                      self.const.auth_type_ha1_md5,
+                      self.const.auth_type_md4_nt,
+                      self.const.auth_type_ssha,
+                      self.const.auth_type_sha256_crypt,
+                      self.const.auth_type_sha512_crypt,
+                      self.const.auth_type_plaintext):
+        raise ValueError('Unknown method {method}'.format(method=method))
+    salt = cryptstring
+    if method == self.const.auth_type_ssha:
+        salt = base64.decodestring(
+            cryptstring.encode())[20:].decode()
+
+    if method == self.const.auth_type_md4_nt:
+        return passlib.hash.nthash.verify(plaintext, cryptstring)
+    else:
+        return (self.encrypt_password(method,
+                                      plaintext,
+                                      salt=salt) == cryptstring)
---
+++ testsuite/tests/test_core/test_core_Auth.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Basic tests for Cerebrum/Account.py.
+
+Searching (members and groups) has to be thoroughly tested.
+"""
+from __future__ import unicode_literals
+
+import logging
+import unittest
+
+from Cerebrum.Utils import Factory
+from Cerebrum.Account import Account
+from Cerebrum.auth.Auth import get_crypt_methods
+
+from datasource import BasicAccountSource, BasicPersonSource
+from dbtools import DatabaseTools
+
+logger = logging.getLogger(__name__)
+
+# TODO:
+#
+# Not imploemented tests for
+#  - Account/AccountType
+#  - Account/AccountHome
+
+
+class BaseAccountTest(unittest.TestCase):
+    """
+    This is a testcase for Cerebrum.Account class.
+
+    No subclass or mixin should cause this test to fail, so the test is valid
+    for other setups as well.
+    Mixins and subclasses can subclass this test in order to perform additional
+    setup and tests.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up this TestCase module.
+
+        This setup code sets up shared objects between each tests. This is done
+        *once* before running any of the tests within this class.
+        """
+
+        # TODO: We might want this basic class setup in other TestCases. Maybe
+        #       set up a generic TestCase class to inherit common stuff from?
+        cls._db = Factory.get('Database')()
+        cls._db.cl_init(change_program='nosetests')
+        cls._db.commit = cls._db.rollback  # Let's try not to screw up the db
+
+        cls._ac = Factory.get('Account')(cls._db)
+        cls._ac = Account(cls._db)
+        cls._co = Factory.get('Constants')(cls._db)
+
+        # Data sources
+        cls.account_ds = BasicAccountSource()
+        cls.person_ds = BasicPersonSource()
+
+        # Tools for creating and destroying temporary db items
+        cls.db_tools = DatabaseTools(cls._db)
+        cls.db_tools._ac = cls._ac
+
+    @classmethod
+    def tearDownClass(cls):
+        """ Clean up this TestCase class. """
+        cls.db_tools.clear_groups()
+        cls.db_tools.clear_accounts()
+        cls.db_tools.clear_persons()
+        cls.db_tools.clear_constants()
+        cls._db.rollback()
+
+
+class SimpleAuthImplementationTest(BaseAccountTest):
+    """ This is a test case for simple SHA-1 hashing implementation. """
+
+    def test_auth_ssha(self):
+        methods = get_crypt_methods()
+        method = methods["auth_type_ssha"]()
+        _hash = method.encrypt_password("hesterbest", salt="ABCDEFGI")
+        self.assertEqual(
+            _hash, "qBVr/e8BtH7dw2h09V8WL0jxEaxBQkNERUZHSQ==")
+
+    def test_auth_sha256(self):
+        methods = get_crypt_methods()
+        method = methods["auth_type_sha256"]()
+        _hash = method.encrypt_password("hesterbest", salt="$5$ABCDEFGI")
+        logger.info(_hash)
+        self.assertEqual(
+            _hash, "$5$ABCDEFGI$wRL35zTjgAhecyc9CWv5Id.qsz5RZqXvDD3EXmlkUJ4")
+
+    def test_auth_sha512(self):
+        methods = get_crypt_methods()
+        method = methods["auth_type_sha512"]()
+        _hash = method.encrypt_password("hesterbest", salt="$6$ABCDEFGI")
+        logger.info(_hash)
+        self.assertEqual(
+            _hash, "$6$ABCDEFGI$s5rS3hTF2FJrqxToloyKaOcmUwFMVvEft"
+            "Yen3WjaetYz726AFZQkI572G0o/bO9BWC86Sae1QjMUe7TZYBeYg1")
+
+    def test_auth_md5(self):
+        methods = get_crypt_methods()
+        method = methods["auth_type_md5"]()
+        _hash = method.encrypt_password("hesterbest", salt="$1$ABCDEFGI")
+        self.assertEqual(
+            _hash, "$1$ABCDEFGI$iO4CKjwcmvejNZ7j1MEW./")
+
+    def test_auth_md4_nt(self):
+        methods = get_crypt_methods()
+        method = methods["auth_type_md4_nt"]()
+        _hash = method.encrypt_password("hesterbest", salt="ABC")
+        self.assertEqual(
+            _hash, "5DDE3A6B19D3DEB6B63E304A5574A193")
