44cbce05a908e54f7efb43bf400d6d9e9b4c5023
Andreas Ellewsen <andreas.ellewsen@usit.uio.no>
2019-11-25 11:58:45 +0100

    Add a logging handler that rotates based on time
    The new logger combines the CerebrumRotatingHandler with the logging
    library's TimedRotatingHandler to allow for threading and rotating based
    on time.
    
    Issue: CRB-2738

--- Cerebrum/logutils/handlers.py
+++ Cerebrum/logutils/handlers.py
@@ -5,10 +5,13 @@ from __future__ import absolute_import
 import codecs
 import logging
 import os
+import re
+import stat
 import string
 import sys
 import time
 
+from Cerebrum.modules.job_runner.times import to_seconds
 from .config import LoggerConfig
 from .stream import CerelogStreamWriter
 
@@ -16,6 +19,7 @@ from .stream import CerelogStreamWriter
 DEFAULT_DIRECTORY = LoggerConfig.logdir.default
 DEFAULT_DIR_PERMS = 0o770
 DEFAULT_ENCODING = 'utf-8'
+_MIDNIGHT = to_seconds(days=1)
 
 
 def get_script_name():
@@ -211,7 +215,6 @@ class OneRunHandler(DelayedFileHandler):
         super(OneRunHandler, self).__init__(filename, **kwargs)
 
 
-# TODO: Daemons should really use a TimedRotatingFileHandler
 # TODO: Cronjobs should use OneRunHandler
 
 
@@ -327,6 +330,279 @@ class CerebrumRotatingHandler(DelayedFileHandler):
                         os.remove(dfn)
 
                     os.rename(self.baseFilename, dfn)
+
+                # delayed opening. The superclass will take care of everything
+                self.stream = None
+            except:
+                # Something went wrong before we managed to complete
+                # rotations. Let's re-open the stream and hope for the best. In
+                # case of failure, it is preferrable to write to the same file,
+                # rather than barf with some sort of error message. If the
+                # error is transient, all will be well and the file will be
+                # rotated next time around. If not, at least we'd be able to
+                # scribble some messages to the file.
+                if self.stream and self.stream.closed:
+                    self._open()
+                    # Make sure the caller knows that something broke down.
+                    raise
+        finally:
+            self.release()
+
+
+class CerebrumTimedRotatingHandler(CerebrumRotatingHandler):
+    """Timed rotating handler with Cerebrum
+
+    This handler rotates based on time just as the TimedRotatingFileHandler
+    found in the standard logging library, but is thread safe like
+    CerebrumRotatingHandler.
+    """
+    _intervals = {
+        'S': {
+            'interval': to_seconds(seconds=1),
+            'suffix': "%Y-%m-%d_%H-%M-%S",
+            'match': r"^\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}$",
+        },
+        'M': {
+            'interval': to_seconds(minutes=1),
+            'suffix': "%Y-%m-%d_%H-%M",
+            'match': r"^\d{4}-\d{2}-\d{2}_\d{2}-\d{2}$",
+        },
+        'H': {
+            'interval': to_seconds(hours=1),
+            'suffix': "%Y-%m-%d_%H",
+            'match': r"^\d{4}-\d{2}-\d{2}_\d{2}$",
+        },
+        'D': {
+            'interval': to_seconds(days=1),
+            'suffix': "%Y-%m-%d",
+            'match': r"^\d{4}-\d{2}-\d{2}$",
+        },
+        'W': {
+            'interval': to_seconds(weeks=1),
+            'suffix': "%Y-%m-%d",
+            'match': r"^\d{4}-\d{2}-\d{2}$",
+        },
+    }
+
+    def __init__(self, filename, when='H', interval=1, backupCount=0,
+                 utc=False, **kwargs):
+        super(CerebrumRotatingHandler, self).__init__(filename, **kwargs)
+        self.when = when.upper()
+        self.backupCount = backupCount
+        self.utc = utc
+        # Calculate the real rollover interval, which is just the number of
+        # seconds between rollovers.  Also set the filename suffix used when
+        # a rollover occurs.  Current 'when' events supported:
+        # S - Seconds
+        # M - Minutes
+        # H - Hours
+        # D - Days
+        # midnight - roll over at midnight
+        # W{0-6} - roll over on a certain day; 0 - Monday
+        #
+        # Case of the 'when' specifier is not important; lower or upper case
+        # will work.
+
+        # If 'MIDNIGHT' - use 'D' interval params
+        if self.when == 'MIDNIGHT':
+            interval_key = 'D'
+        else:
+            interval_key = self.when[0]
+
+        try:
+            params = self._intervals[interval_key]
+            self.interval = params['interval'] * interval
+            self.suffix = params['suffix']
+            self.extMatch = re.compile(params['match'])
+        except KeyError:
+            raise ValueError("Invalid rollover interval specified: %s" %
+                             self.when)
+
+        # If 'W_' - set day of week
+        if self.when.startswith('W'):
+            if len(self.when) != 2:
+                raise ValueError(
+                    "You must specify a day for weekly rollover from 0 to 6 "
+                    "(0 is Monday): %s" % self.when)
+            if self.when[1] < '0' or self.when[1] > '6':
+                raise ValueError("Invalid day specified for weekly "
+                                 "rollover: %s" % self.when)
+            self.dayOfWeek = int(self.when[1])
+
+        if os.path.exists(filename):
+            t = os.stat(filename)[stat.ST_MTIME]
+        else:
+            t = int(time.time())
+        self.rolloverAt = self.computeRollover(t)
+
+    def computeRollover(self, currentTime):
+        """
+        Work out the rollover time based on the specified time.
+        """
+        result = currentTime + self.interval
+        # If we are rolling over at midnight or weekly, then the interval is
+        # already known.  What we need to figure out is WHEN the next interval
+        # is.  In other words, if you are rolling over at midnight, then your
+        # base interval is 1 day, but you want to start that one day clock at
+        # midnight, not now.  So, we have to fudge the rolloverAt value in
+        # order to trigger the first rollover at the right time.  After that,
+        # the regular interval will take care of the rest.  Note that this code
+        # doesn't care about leap seconds. :)
+        if self.when == 'MIDNIGHT' or self.when.startswith('W'):
+            # This could be done with less code, but I wanted it to be clear
+            if self.utc:
+                t = time.gmtime(currentTime)
+            else:
+                t = time.localtime(currentTime)
+            currenthour = t[3]
+            currentminute = t[4]
+            currentsecond = t[5]
+            # r is the number of seconds left between now and midnight
+            r = _MIDNIGHT - to_seconds(hours=currenthour,
+                                       minutes=currentminute,
+                                       seconds=currentsecond)
+            result = currentTime + r
+            # If we are rolling over on a certain day, add in the number of
+            # days until the next rollover, but offset by 1 since we just
+            # calculated the time until the next day starts.  There are three
+            # cases:
+            # Case 1) The day to rollover is today; in this case, do nothing
+            # Case 2) The day to rollover is further in the interval (i.e.,
+            #         today is day 2 (Wednesday) and rollover is on day 6
+            #         (Sunday).  Days to next rollover is simply 6 - 2 - 1, or
+            #         3.
+            # Case 3) The day to rollover is behind us in the interval (i.e.,
+            #         today is day 5 (Saturday) and rollover is on day 3
+            #         (Thursday).  Days to rollover is 6 - 5 + 3, or 4.  In
+            #         this case, it's the number of days left in the current
+            #         week (1) plus the number of days in the next week until
+            #         the rollover day (3).
+            # The calculations described in 2) and 3) above need to have a day
+            # added.  This is because the above time calculation takes us to
+            # midnight on this day, i.e. the start of the next day.
+            if self.when.startswith('W'):
+                day = t[6]  # 0 is Monday
+                if day != self.dayOfWeek:
+                    if day < self.dayOfWeek:
+                        daystowait = self.dayOfWeek - day
+                    else:
+                        daystowait = 6 - day + self.dayOfWeek + 1
+                    newrolloverat = result + to_seconds(days=daystowait)
+                    if not self.utc:
+                        dstnow = t[-1]
+                        dstatrollover = time.localtime(newrolloverat)[-1]
+                        if dstnow != dstatrollover:
+                            if not dstnow:
+                                # DST kicks in before next rollover, so we need
+                                # to deduct an hour
+                                addend = -3600
+                            else:
+                                # DST bows out before next rollover, so we need
+                                # to add an hour
+                                addend = 3600
+                            newrolloverat += addend
+                    result = newrolloverat
+        return result
+
+    def shouldRollover(self, record):
+        """
+        Determine if rollover should occur.
+
+        record is not used, as we are just comparing times, but it is needed so
+        the method signatures are the same
+        """
+        t = int(time.time())
+        if t >= self.rolloverAt:
+            return True
+        return False
+
+    def getFilesToDelete(self):
+        """
+        Determine the files to delete when rolling over.
+
+        More specific than the earlier method, which just used glob.glob().
+        """
+        dirName, baseName = os.path.split(self.baseFilename)
+        fileNames = os.listdir(dirName)
+        result = []
+        prefix = baseName + "."
+        plen = len(prefix)
+        for fileName in fileNames:
+            if fileName[:plen] == prefix:
+                suffix = fileName[plen:]
+                if self.extMatch.match(suffix):
+                    result.append(os.path.join(dirName, fileName))
+        result.sort()
+        if len(result) < self.backupCount:
+            result = []
+        else:
+            result = result[:len(result) - self.backupCount]
+        return result
+
+    def doRollover(self, record):
+        """
+        This is a slightly modified copy of CerebrumRotatingHandler and
+        logging.handlers.TimedRotatingFileHandler
+
+        In essence we have to rotate based on time, but it also has to be
+        thread safe.
+        """
+        try:
+            self.acquire()
+
+            # Check one more time, if we *really* should roll over. Perhaps a
+            # differnt thread has already done that.
+            if not self.shouldRollover(record):
+                return
+
+            if self.stream and self.stream.closed:
+                self.stream.close()
+
+            try:
+                # get the time that this sequence started at and make it a
+                # TimeTuple
+                currenttime = int(time.time())
+                dstnow = time.localtime(currenttime)[-1]
+                t = self.rolloverAt - self.interval
+                if self.utc:
+                    timetuple = time.gmtime(t)
+                else:
+                    timetuple = time.localtime(t)
+                    dstthen = timetuple[-1]
+                    if dstnow != dstthen:
+                        if dstnow:
+                            addend = 3600
+                        else:
+                            addend = -3600
+                        timetuple = time.localtime(t + addend)
+                dfn = self.baseFilename + "." + time.strftime(self.suffix,
+                                                              timetuple)
+                if os.path.exists(dfn):
+                    os.remove(dfn)
+                if os.path.exists(self.baseFilename):
+                    os.rename(self.baseFilename, dfn)
+                if self.backupCount > 0:
+                    for s in self.getFilesToDelete():
+                        os.remove(s)
+                newrolloverat = self.computeRollover(currenttime)
+                while newrolloverat <= currenttime:
+                    newrolloverat = newrolloverat + self.interval
+                # If DST changes and midnight or weekly rollover, adjust for
+                # this.
+                if (self.when == 'MIDNIGHT' or
+                        self.when.startswith('W')) and not self.utc:
+                    dstatrollover = time.localtime(newrolloverat)[-1]
+                    if dstnow != dstatrollover:
+                        if not dstnow:
+                            # DST kicks in before next rollover, so we need to
+                            # deduct an hour
+                            addend = -3600
+                        else:
+                            # DST bows out before next rollover, so we need to
+                            # add an hour
+                            addend = 3600
+                        newrolloverat += addend
+                self.rolloverAt = newrolloverat
 
                 # delayed opening. The superclass will take care of everything
                 self.stream = None
