471f62292464337e71dc4f75863ba786fd44fbec
Stein Elgethun <stein.elgethun@usit.uio.no>
2019-12-17 15:47:33 +0100

    Add tsd specific auth for admins
    Used to be handled by the opset Group-owner.
    
    Issue: CRB-2936

--- Cerebrum/modules/tsd/bofhd_auth.py
+++ Cerebrum/modules/tsd/bofhd_auth.py
@@ -347,6 +347,40 @@ class TsdBofhdAuth(BofhdAuth):
                         % (src_entity.account_name, proj_name))
         return True
 
+    def can_alter_group(self, operator, group=None, query_run_any=False):
+        """Checks if the operator has permission to add/remove group members
+        for the given group.
+
+        @type operator: int
+        @param operator: The entity_id of the user performing the operation.
+
+        @type group: An entity of EntityType Group
+        @param group: The group to add/remove members to/from.
+
+        @type query_run_any: True or False
+        @param query_run_any: Check if the operator has permission *somewhere*
+
+        @return: True or False
+        """
+        if self.is_superuser(operator):
+            return True
+        if query_run_any and self._is_admin(operator):
+            return True
+        if self._is_admin(operator, group.entity_id):
+            return True
+        return super(TsdBofhdAuth, self).can_alter_group(operator, group,
+                                                         query_run_any)
+
+    def can_show_history(self, operator, entity=None, query_run_any=False):
+        if self.is_superuser(operator):
+            return True
+        if query_run_any and self._is_admin(operator):
+            return True
+        if entity and self._is_admin(operator, entity.entity_id):
+            return True
+        return super(TsdBofhdAuth, self).can_show_history(operator, entity,
+                                                          query_run_any)
+
 
 class ContactAuth(TsdBofhdAuth, bofhd_contact_info.BofhdContactAuth):
 
