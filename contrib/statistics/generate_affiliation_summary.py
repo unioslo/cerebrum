092417783891e9ec56b0deaeeb744a703edff625
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2020-02-06 16:23:48 +0100

    Clean up generate_affiliation_summary
    This commit fixes some logging issues in
    `generate_affiliation_summary.py`, and separates the db lookups from the
    data aggregation.

--- contrib/statistics/generate_affiliation_summary.py
+++ contrib/statistics/generate_affiliation_summary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009-2018 University of Oslo, Norway
+# Copyright 2009-2020 University of Oslo, Norway
 #
 # This file is part of Cerebrum.
 #
@@ -18,49 +18,88 @@
 # You should have received a copy of the GNU General Public License
 # along with Cerebrum; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-""" Dump affiliation count. """
+"""
+Report affiliation counts.
+"""
 from __future__ import print_function
 
-from Cerebrum.Utils import Factory
-from collections import defaultdict
+import argparse
+import collections
+import logging
 
-from six import text_type
+import six
+
+import Cerebrum.logutils
+import Cerebrum.logutils.options
+from Cerebrum.Utils import Factory
+
+logger = logging.getLogger(__name__)
+
+
+def get_aff_types(db):
+    """ Get all valid PersonAffiliation types """
+    co = Factory.get("Constants")(db)
+    return tuple(co.fetch_constants(co.PersonAffiliation))
+
+
+def get_aff_subtypes(db):
+    """ Get all valid PersonAffStatus types """
+    co = Factory.get("Constants")(db)
+    return tuple(co.fetch_constants(co.PersonAffStatus))
+
+
+def get_affiliations(db):
+    """ Get all current person affiliations. """
+    pe = Factory.get("Person")(db)
+
+    types = {int(c): c for c in get_aff_types(db)}
+    subtypes = {int(c): c for c in get_aff_subtypes(db)}
+
+    for row in pe.list_affiliations():
+        person_id = int(row['person_id'])
+        aff = types[row['affiliation']]
+        status = subtypes[row['status']]
+        ou_id = int(row['ou_id'])
+
+        yield person_id, aff, status, ou_id
 
 
 class AffiliationStatistics(object):
-    """ Cache person affiliation by type/ou. """
+    """ Organize persons by affiliation types and ous """
 
-    def __init__(self, db):
-        co = Factory.get("Constants")(db)
-        pe = Factory.get("Person")(db)
+    def __init__(self, affiliations):
+        """
+        :param affiliations:
+            iterable of (person_id, affiliation, status, ou_id) tuples
+        """
 
         # observed ous, affs
         affs = set()
         stat = set()
         ous = set()
 
-        data = defaultdict(list)
+        data = collections.defaultdict(list)
 
-        for row in pe.list_affiliations():
-            affs.add(row['affiliation'])
-            stat.add(row['status'])
-            ous.add(row['ou_id'])
+        for person_id, aff, status, ou_id in affiliations:
+            affs.add(aff)
+            stat.add(status)
+            ous.add(ou_id)
 
             # record person by aff, status, ou
             for key in (
-                (None,               None,          None),
-                (row['affiliation'], None,          None),
-                (None,               row['status'], None),
-                (None,               None,          row['ou_id']),
-                (row['affiliation'], None,          row['ou_id']),
-                (None,               row['status'], row['ou_id']),
+                (None,  None,       None),
+                (aff,   None,       None),
+                (None,  status,     None),
+                (None,  None,       ou_id),
+                (aff,   None,       ou_id),
+                (None,  status,     ou_id),
             ):
-                data[key].append(row['person_id'])
+                data[key].append(person_id)
 
         self._data = dict(data)
         self.ous = ous
-        self.types = tuple((co.PersonAffiliation(a) for a in affs))
-        self.subtypes = tuple((co.PersonAffStatus(s) for s in stat))
+        self.types = tuple(sorted(affs))
+        self.subtypes = tuple(sorted(stat))
 
     def count(self, affiliation=None, status=None, ou=None):
         return len(self._data[affiliation, status, ou])
@@ -71,7 +110,7 @@ class AffiliationStatistics(object):
 
 def print_affiliation_summary(stats):
 
-    status_by_aff = defaultdict(set)
+    status_by_aff = collections.defaultdict(set)
 
     for s in stats.subtypes:
         status_by_aff[s.affiliation].add(s)
@@ -83,18 +122,34 @@ def print_affiliation_summary(stats):
                        persons=stats.count_unique(),
                        affs=stats.count()))
     for a in stats.types:
-        print(entry.format(key=text_type(a),
+        print(entry.format(key=six.text_type(a),
                            persons=stats.count_unique(affiliation=a),
                            affs=stats.count(affiliation=a)))
         for s in status_by_aff.get(a, []):
-            print(entry.format(key=text_type(s),
+            print(entry.format(key=six.text_type(s),
                                persons=stats.count_unique(status=s),
                                affs=stats.count(status=s)))
 
 
-def main():
+default_logger_preset = 'console'
+default_logger_level = logging.WARNING
+
+
+def main(inargs=None):
+    parser = argparse.ArgumentParser(
+        description='Count affiliations by type',
+    )
+
+    Cerebrum.logutils.options.install_subparser(parser)
+    parser.set_defaults(**{
+        Cerebrum.logutils.options.OPTION_LOGGER_LEVEL: default_logger_level,
+    })
+
+    args = parser.parse_args(inargs)
+    Cerebrum.logutils.autoconf(default_logger_preset, args)
+
     db = Factory.get("Database")()
-    stats = AffiliationStatistics(db)
+    stats = AffiliationStatistics(get_affiliations(db))
     print_affiliation_summary(stats)
 
 
