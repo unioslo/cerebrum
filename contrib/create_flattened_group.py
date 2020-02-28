8335a23aa4396420f3b17663d114ae4246923b48
Jon H. Austad <jon.austad@usit.uio.no>
2020-02-28 12:13:27 +0100

    Add and implement new group type for derived groups

--- contrib/create_flattened_group.py
+++ contrib/create_flattened_group.py
@@ -49,7 +49,7 @@ def prepare_empty(db, args):
             visibility=co.group_visibility_all,
             name=args.destination_group,
             description=('Flattened variant of %s' % args.target_group),
-            group_type=co.group_type_internal,
+            group_type=co.group_type_derived,
         )
     gr.write_db()
 
