6a30929552dbfec371998c7fc63ecda71f70e570
Christoffer Klevstad Buen <ckb@usit.uio.no>
2020-05-20 00:44:33 +0200

    Add function to specify affiliation status in deactivate_quarantined_accounts.py
    Added functionality to also be able to specify affiliation status when providing person-affiliations on people which to ignore.
    
    Issue:CRB-3130

--- contrib/no/deactivate_quarantined_accounts.py
+++ contrib/no/deactivate_quarantined_accounts.py
@@ -119,9 +119,28 @@ def fetch_all_relevant_accounts(qua_type, since, ignore_affs,
         return targets
 
     # Ignore those with person affiliations:
-    persons = set(r['person_id'] for r in
-                  person.list_affiliations(include_deleted=False)
-                  if r['affiliation'] not in ignore_affs)
+    person_affiliation_list = person.list_affiliations(include_deleted=False)
+
+    persons = set()
+
+    for r in person_affiliation_list:
+        person_id = r['person_id']
+        person_affiliation = r['affiliation']
+        person_affiliation_status = r['status']  # None, if no status
+        aff_status = (person_affiliation, person_affiliation_status)
+        if (person_affiliation, None) not in ignore_affs and aff_status not in ignore_affs:
+            """
+            Check if affiliation without status is specified, 
+            then ignore all persons with affiliation no matter what status they've got.
+            Also check if the combo of affiliation and status is in ignore_affs
+            If this persons affiliation or affiliation+status combo isn't specified to be ignored,
+            then add the person to the list of persons.
+            """
+            persons.add(person_id)
+
+    #persons = set(r['person_id'] for r in person_affiliation_list
+    #                       if r['affiliation'] not in ignore_affs)
+
     logger.debug2("Found %d persons with affiliations", len(persons))
     accounts_to_ignore = set(int(r['account_id']) for r in
                              account.search(owner_type=constants.entity_person)
@@ -259,6 +278,9 @@ def main():
     system_accounts = False
     affiliations = set()
 
+    import pydevd_pycharm
+    pydevd_pycharm.settrace('localhost', port=35789, stdoutToServer=True, stderrToServer=True)
+
     for option, value in options:
         if option in ("-q", "--quarantines"):
             quarantines = []
@@ -280,14 +302,16 @@ def main():
             logger.debug('Set to delete accounts and not just deactivating!')
             delete = True
         elif option in ('-a', '--affiliations'):
-            for af in value.split(','):
-                aff = constants.PersonAffiliation(af)
+            for af in value.strip().split(','):
                 try:
-                    int(aff)
+                    aff = constants.get_affiliation(af)
                 except Errors.NotFoundError:
                     print("Unknown affiliation: %s" % af)
                     sys.exit(2)
-                affiliations.add(aff)
+                aff_status = ((int(aff[0]), None)  # to prevent int(None)
+                                    if aff[1] is None   # if status=None
+                                    else (int(aff[0]), int(aff[1])))  # (aff, status)
+                affiliations.add(aff_status)
         elif option in ("-h", "--help"):
             usage()
         else:
@@ -299,7 +323,9 @@ def main():
                 [str(constants.human2constant(q)) for q in quarantines],
                 since, delete, bofhdreq, limit)
     logger.debug("Ignoring those with person-affilations: %s",
-                 ', '.join(str(a) for a in affiliations))
+                 ', '.join(
+                     "\naffiliation:"+str(a[0])+" - status:"+str(a[1])
+                           for a in affiliations))
     logger.info("Fetching relevant accounts")
     rel_accounts = fetch_all_relevant_accounts(quarantines, since,
                                                ignore_affs=affiliations,
