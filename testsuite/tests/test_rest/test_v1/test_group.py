134e86c09675eea190a862c90536f7d709e46ee6
Andreas Tolfsen <andretol@usit.uio.no>
2020-08-26 16:00:16 +0200

    rest: test GroupMember content and data schema
    test_group_members_list and test_group_members_crud have been very
    lax about checking data coherence, to the extent that certain fields
    on the GroupMember data object would be lost during serialisation
    into JSON, which we fixed in 6b75b892d.
    
    This patch adds tests to avoid regressions in serialising the return
    values for the endpoints {GET,PUT} /<group>/members/<member>.
    Additionally it tests that the returned JSON objects are valid
    according to the JSON-Schema generated by flask-restplus.
    
    Fixes: CRB-3254

--- testsuite/tests/test_rest/test_v1/test_group.py
+++ testsuite/tests/test_rest/test_v1/test_group.py
@@ -5,7 +5,13 @@
 
 from __future__ import unicode_literals
 
+import jsonschema
+import six
+
 from flask import url_for
+
+from Cerebrum.rest.api import db, utils
+from Cerebrum.rest.api.v1.group import GroupMember
 
 
 def test_group_crud(client, auth_header, group_bar):
@@ -36,14 +42,14 @@ def test_group_members_list(client, auth_header, group_bar, account_foo):
         headers=auth_header,
     )
     assert res.status_code == 200
-    assert res.json.get("members") == []
+    assert_group_member_list(res.json, [])
     group_bar.add_member(account_foo.entity_id)
     res = client.get(
         url_for("api_v1.group-members-list", name=group_bar.group_name),
         headers=auth_header,
     )
     assert res.status_code == 200
-    assert res.json.get("members")[0].get("name") == account_foo.account_name
+    assert_group_member_list(res.json, [account_foo])
 
 
 def test_group_members_crud(client, auth_header, group_bar, account_foo):
@@ -55,10 +61,35 @@ def test_group_members_crud(client, auth_header, group_bar, account_foo):
     not_a_member = client.get(url, headers=auth_header)
     assert not_a_member.status_code == 404
     put = client.put(url, headers=auth_header)
-    assert put.json.get("name") == account_foo.account_name
+    assert put.status_code == 200
+    assert_group_member(put.json, account_foo)
     get = client.get(url, headers=auth_header)
-    assert get.json.get("name") == account_foo.account_name
+    assert get.status_code == 200
+    assert_group_member(get.json, account_foo)
     delete = client.delete(url, headers=auth_header)
     assert delete.status_code == 204  # no content
     deleted = client.get(url, headers=auth_header)
     assert deleted.status_code == 404
+
+
+def assert_group_member_list(json, members):
+    for member, account in six.moves.zip(json.get("members"), members):
+        assert_group_member(member, account)
+
+
+def assert_group_member(json, account):
+    jsonschema.validate(json, GroupMember.__schema__)
+    assert json == {
+        "href": href(account),
+        "type": str(getattr(db.const, "EntityType")(account.entity_type)),
+        "id": account.entity_id,
+        "name": account.account_name,
+    }
+
+
+def href(account):
+    return utils.href_from_entity_type(
+        entity_type=account.entity_type,
+        entity_id=account.entity_id,
+        entity_name=utils.get_entity_name(account),
+    )
