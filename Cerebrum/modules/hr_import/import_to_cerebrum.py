1c9dbf7f136eec827d9080f65f23fdb5b44964d1
Stein Elgethun <elg@ulrik.uio.no>
2020-08-28 12:40:04 +0200

    Add logging and constant as class attribute
    Had to make constant in many places, unnecessary when it could just be as a
    class attribute
    
    Issue: CRB-3193

--- Cerebrum/modules/hr_import/import_to_cerebrum.py
+++ Cerebrum/modules/hr_import/import_to_cerebrum.py
@@ -18,10 +18,14 @@
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 from __future__ import unicode_literals
 
+import logging
+
 from Cerebrum import Errors
 from Cerebrum.modules.hr_import import models
 from Cerebrum.modules.automatic_group.structure import update_memberships
 from Cerebrum.Utils import Factory
+
+logger = logging.getLogger(__name__)
 
 LEADER_GROUP_PREFIX = 'adm-leder-'
 
@@ -33,10 +37,11 @@ class HRDataImport(object):
         self.cerebrum_person = cerebrum_person
         self.database = database
         self.source_system = hr_person.source_system
-        self.logger = logger
+        self.co = Factory.get('Constants')(self.database)
 
     def import_to_cerebrum(self):
         """Call all the update functions, creating or updating a person"""
+        logger.info('Starting import_to_cerebrum for %r', self.hr_person.hr_id)
         self.update_person()
         self.update_external_ids()
         self.update_names()
@@ -47,6 +52,8 @@ class HRDataImport(object):
         self.update_leader_groups()
         self.update_contact_info()
         self.update_reservation()
+        logger.info('Done with import_to_cereburm for %r',
+                    self.cerebrum_person.entity_id)
 
     def update_person(self):
         """Update person with birth date and gender"""
@@ -58,6 +65,10 @@ class HRDataImport(object):
                 self.hr_person.birth_date,
                 self.hr_person.gender)
             self.cerebrum_person.write_db()
+            logger.info('Added birth date %r and gender %r for %r',
+                        self.hr_person.birth_date,
+                        self.hr_person.gender,
+                        self.cerebrum_person.entity_id)
 
     def update_external_ids(self):
         """Update person in Cerebrum with appropriate external ids"""
@@ -74,9 +85,18 @@ class HRDataImport(object):
         self.cerebrum_person.affect_external_id(
             self.source_system,
             *(ext_id.id_type for ext_id in to_remove | to_add))
+        if to_remove:
+            logger.info(
+                'Purging externalids of types %r for id: %r',
+                (unicode(ext_type)
+                 for ext_type in to_remove),
+                self.cerebrum_person.entity_id)
         for (id_type, ext_id) in to_add:
             self.cerebrum_person.populate_external_id(
                 self.source_system, id_type, ext_id)
+            logger.info('Adding externalid %r for id: %r',
+                        (unicode(id_type),
+                         ext_id), self.cerebrum_person.entity_id)
         self.cerebrum_person.write_db()
 
     def update_names(self):
@@ -90,21 +110,24 @@ class HRDataImport(object):
             except Errors.NotFoundError:
                 return None
 
-        co = Factory.get('Constants')(self.database)
-        crb_first_name = _get_name_type(co.name_first)
-        crb_last_name = _get_name_type(co.name_last)
+        crb_first_name = _get_name_type(self.co.name_first)
+        crb_last_name = _get_name_type(self.co.name_last)
 
         if crb_first_name != self.hr_person.first_name:
             self.cerebrum_person.affect_names(
-                self.source_system, co.name_first)
+                self.source_system, self.co.name_first)
             self.cerebrum_person.populate_name(
-                co.name_first, self.hr_person.first_name)
+                self.co.name_first, self.hr_person.first_name)
+            logger.info('Changing first name from %r to %r',
+                        crb_first_name, self.hr_person.first_name)
 
         if crb_last_name != self.hr_person.last_name:
             self.cerebrum_person.affect_names(
-                self.source_system, co.name_last)
+                self.source_system, self.co.name_last)
             self.cerebrum_person.populate_name(
-                co.name_last, self.hr_person.last_name)
+                self.co.name_last, self.hr_person.last_name)
+            logger.info('Changing last name from %r to %r',
+                        crb_last_name, self.hr_person.last_name)
         self.cerebrum_person.write_db()
 
     def update_affiliations(self):
@@ -122,9 +145,12 @@ class HRDataImport(object):
             )
         for aff in cerebrum_affiliations - self.hr_person.affiliations:
             self.cerebrum_person.delete_affiliation(
-                ou_id=aff['ou_id'],
-                affiliation=aff['affiliation'],
+                ou_id=aff.ou_id,
+                affiliation=aff.affiliation,
                 source=self.source_system)
+            logger.info('Removing affiliation %r for id: %r',
+                        unicode(aff.affiliation),
+                        self.cerebrum_person.entity_id)
         for aff in self.hr_person.affilations:
             self.cerebrum_person.populate_affiliation(
                 source_system=self.source_system,
@@ -133,6 +159,9 @@ class HRDataImport(object):
                 status=aff['status'],
                 precedence=aff['precedence']
             )
+            logger.info('Adding affiliation %r for id: %r',
+                        unicode(aff.affiliation),
+                        self.cerebrum_person.entity_id)
         self.cerebrum_person.write_db()
 
     def update_account_types(self):
@@ -175,10 +204,9 @@ class HRDataImport(object):
             ac.set_account_type(ac, acc_type.ou_id, acc_type.affiliation)
 
     def _check_account_type(self, account_type):
-        co = Factory.get('Constants')(self.database)
-        if not int(co.affiliation_ansatt) == account_type['affiliation']:
+        if not int(self.co.affiliation_ansatt) == account_type['affiliation']:
             self.logger.info('account has affiliation(s) besides '
-                             '%r', co.affiliation_ansatt)
+                             '%r', self.co.affiliation_ansatt)
             return False
         aff_info = self.cerebrum_person.list_affiliations(
             person_id=account_type['person_id'],
@@ -186,9 +214,9 @@ class HRDataImport(object):
             affiliation=account_type['affiliation'],
         )
         if aff_info:
-            if not int(co.system_sap) == aff_info[0]['source_system']:
+            if not int(self.co.system_sap) == aff_info[0]['source_system']:
                 self.logger.info('Account has affiliation from source(s) '
-                                 'other than %r', co.system_sap)
+                                 'other than %r', self.co.system_sap)
                 return False
         return True
 
@@ -210,6 +238,10 @@ class HRDataImport(object):
                 source_type=self.source_system,
                 a_type=add.address_type
             )
+            logger.info('Removing address %r for id: %r',
+                        (unicode(add.address_type),
+                         add.city, add.postal_number, add.address_text),
+                        self.cerebrum_person.entity_id)
         for add in self.hr_person.addresses - cerebrum_addresses:
             self.cerebrum_person.add_entity_address(
                 source=self.source_system,
@@ -218,14 +250,17 @@ class HRDataImport(object):
                 postal_number=add.postal_code,
                 city=add.city
             )
+            logger.info('Adding address %r for id: %r',
+                        (unicode(add.address_type),
+                         add.city, add.postal_number, add.address_text),
+                        self.cerebrum_person.entity_id)
 
     def update_titles(self):
         """Update person in Cerebrum with work and personal titles"""
-        co = Factory.get('Constants')(self.database)
         cerebrum_titles = set()
         for title in self.cerebrum_person.search_name_with_language(
                 entity_id=self.cerebrum_person.entity_id,
-                name_variant=[co.work_title, co.personal_title]):
+                name_variant=[self.co.work_title, self.co.personal_title]):
             cerebrum_titles.add(
                 models.HRTitle(
                     title['name_variant'],
@@ -239,22 +274,25 @@ class HRDataImport(object):
                 name_language=title.name_language,
                 name=title.name,
             )
+            logger.info('Adding title %r for id: %r',
+                        title.name, self.cerebrum_person.entity_id)
         for title in cerebrum_titles - self.hr_person.titles:
             self.cerebrum_person.delete_name_with_language(
                 name_variant=title.name_variant,
                 name_language=title.name_language,
                 name=title.name,
             )
+            logger.info('Removing title %r for id: %r',
+                        title.name, self.cerebrum_person.entity_id)
 
     def update_leader_groups(self):
         """Update leader group memberships for person in Cerebrum"""
         gr = Factory.get('Group')(self.database)
-        co = Factory.get('Constants')(self.database)
         cerebrum_memberships = set(
             group['group_id'] for group in gr.search(
                 member_id=self.cerebrum_person.entity_id,
                 name=LEADER_GROUP_PREFIX + '*',
-                group_type=co.group_type_affiliation,
+                group_type=self.co.group_type_affiliation,
                 filter_expired=True,
                 fetchall=False)
         )
@@ -263,6 +301,9 @@ class HRDataImport(object):
             self.cerebrum_person.entity_id,
             cerebrum_memberships,
             self.hr_person.leader_groups)
+        logger.info('Assert (person: %r) is member of (leader_groups: %r)',
+                    self.cerebrum_person.entity_id,
+                    self.hr_person.leader_groups)
 
     def update_contact_info(self):
         """Update person in Cerebrum with contact information"""
@@ -282,6 +323,12 @@ class HRDataImport(object):
                 contact_type_type=contact.contact_type,
                 pref=contact.contact_pref,
             )
+            logger.info('Removing contact %r of type %r with preference %r '
+                        'for id: %r',
+                        contact.contact_value,
+                        contact.contact_type,
+                        contact.contact_pref,
+                        self.cerebrum_person.entity_id)
         for contact in self.hr_person.contact_infos - cerebrum_contacts:
             self.cerebrum_person.add_contact_info(
                 source=self.source_system,
@@ -289,6 +336,12 @@ class HRDataImport(object):
                 value=contact.contact_value,
                 pref=contact.contact_pref,
             )
+            logger.info('Adding contact %r of type %r with preference %r '
+                        'for id: %r',
+                        contact.contact_value,
+                        contact.contact_type,
+                        contact.contact_pref,
+                        self.cerebrum_person.entity_id)
 
     def update_reservation(self):
         """Manage reservation from public display for person in Cerebrum"""
@@ -297,5 +350,9 @@ class HRDataImport(object):
         in_reserved_group = gr.has_member(self.cerebrum_person.entity_id)
         if self.hr_person.reserved and not in_reserved_group:
             gr.add_member(self.cerebrum_person.entity_id)
+            logger.info('Adding id: %r to reservation group',
+                        self.cerebrum_person.entity_id)
         elif not self.hr_person.reserved and in_reserved_group:
             gr.remove_member(self.cerebrum_person.entity_id)
+            logger.info('Removing id: %r from reservation group',
+                        self.cerebrum_person.entity_id)
