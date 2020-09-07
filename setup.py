2830781c2b20d909e6e5562e852f1bcde8f9f922
Marius Jean Bischof Hagalid <marius.hagalid@usit.uio.no>
2020-09-07 10:21:50 +0200

    Move modules and add them to setup.py
    Issue: CRB-3194

--- setup.py
+++ setup.py
@@ -369,6 +369,7 @@ setup(
         'Cerebrum/modules/ad',
         'Cerebrum/modules/ad2',
         'Cerebrum/modules/amqp',
+        'Cerebrum/modules/amqp/message_mapping',
         'Cerebrum/modules/apikeys',
         'Cerebrum/modules/audit',
         'Cerebrum/modules/automatic_group',
