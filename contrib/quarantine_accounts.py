a7308a587753ba00941d1a53a93b54bbc60f8dab
Jon H. Austad <jon.austad@usit.uio.no>
2021-02-11 13:51:50 +0100

    Ensure correct formatting of datetime string

--- contrib/quarantine_accounts.py
+++ contrib/quarantine_accounts.py
@@ -213,7 +213,7 @@ def notify_user(ac, quar_start_in_days):
     body = body.replace('${DAYS_TO_START}', str(quar_start_in_days))
     body = body.replace(
         '${QUARANTINE_DATE}',
-        (now() + quar_start_in_days).Format("%F"))
+        (now() + quar_start_in_days).strftime("%F"))
 
     try:
         first_name = (pe.search_person_names(
