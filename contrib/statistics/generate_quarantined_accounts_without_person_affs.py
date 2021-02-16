95d41b258745a5e17e4d93cfcb1e43098e83e104
Jon H. Austad <jon.austad@usit.uio.no>
2021-02-16 15:14:06 +0100

    Remove dependency on mx.DateTime from script

--- contrib/statistics/generate_quarantined_accounts_without_person_affs.py
+++ contrib/statistics/generate_quarantined_accounts_without_person_affs.py
@@ -30,17 +30,17 @@ import argparse
 import logging
 import sys
 from collections import defaultdict
+from datetime import timedelta
 
 from jinja2 import Environment
 from six import text_type
-
-from mx.DateTime import now, DateTimeDelta
 
 import Cerebrum.logutils
 import Cerebrum.logutils.options
 from Cerebrum.Utils import Factory
 from Cerebrum.utils.argutils import codec_type
-
+from Cerebrum.utils.date import now
+from Cerebrum.utils.date_compat import get_datetime_tz
 
 logger = logging.getLogger(__name__)
 
@@ -170,7 +170,8 @@ def get_matching_accs(db):
         return {
             'q_type': text_type(co.Quarantine(row['quarantine_type'])),
             'q_desc': _u(row['description']),
-            'q_date': text_type(row['start_date'].strftime('%Y-%m-%d')),
+            'q_date': text_type(
+                get_datetime_tz(row['start_date']).strftime('%Y-%m-%d')),
         }
 
     logger.debug('caching personal accounts ...')
@@ -209,7 +210,7 @@ def get_matching_accs(db):
             continue
 
         for quar in acc2quar[acc['account_id']]:
-            if (quar['start_date'] + DateTimeDelta(365)) < now():
+            if (quar['start_date'] + timedelta(365)) < now():
                 break
         else:
             # loop terminated wihtout finding a 'quar' -- i.e. no active
