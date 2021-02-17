5934bea4fca272a9087b6dd7b553b1e4759a4139
Hans Jenvild <hj@hj-data.uio.no>
2021-02-17 11:53:48 +0100

    Fix spelling error in comment

--- contrib/remove_old_spreads.py
+++ contrib/remove_old_spreads.py
@@ -20,7 +20,7 @@
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 """
-his script removes unwanted spreads from entities.
+This script removes unwanted spreads from entities.
 """
 
 import cereconf
