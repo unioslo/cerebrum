da63eda49b6098830181779cfb37e505a2f87ee1
Fredrik Larsen <fredrik.h.larsen@gmail.com>
2021-08-12 16:30:23 +0200

    Add an extlib.records module
    This commit adds relevant part of the *records* module as
    ``Cerebrum.extlib.records``.  This repeats work previously done by
    ~kolbu, but without:
    
    - changing how `value in database_row` tests work
    - actually changing from `db_row.row` to `records.Record`
    
    Also documents our existing extlib modules.
    
    Issue: CRB-487

--- Cerebrum/extlib/__init__.py
+++ Cerebrum/extlib/__init__.py
@@ -1,10 +1,56 @@
 # -*- coding: utf-8 -*-
-# This directory contains various modules that Cerebrum makes use of,
-# but that have been developed independently of Cerebrum.
 #
-# db_row.py
-#    Database query result objects that ease attribute access
-#    Current version: 1.5  (requires Python 2.2)
-#    Source: http://opensource.theopalgroup.com/files/db_row.py
+# Copyright 2002-2021 University of Oslo, Norway
 #
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+"""
+Bundled and/or modified third party modules.
 
+``db_row``
+    Heavily modified version of *db_row v0.8*, a comprehensive module with
+    various row object implementations.  Deprecated, as *db_row* only supports
+    Python 2.  Will be replaced with `records` in time.
+
+``doc_exception``
+    A copy of the *doc_exception 0.1.0* exception message formatting module,
+    written by a Cerebrum contributor.  Similar functionality is already
+    implemented in ``Cerebrum.Errors``.  This module is currently *only* used
+    in:
+
+    - ``Cerebrum.modules.abcenterprise.Object2Cerebrum``
+    - ``Cerebrum.modules.abcenterprise.ABCUtils``
+
+``Plex``
+    Somewhat modified copy of the generic lexer framework *Plex v1.1.5*, used
+    for parsing, modifying and validating SQL queries.  Used in
+    ``Cerebrum.database.translate`` and ``Cerebrum.database.lexer_plex``.  This
+    module is deprecated, and has already been replaced with *sqlparse*.
+
+``records``
+    This module contains the row objects from *records v0.5.3*.  Introduced to
+    replace `db_row`.
+
+``xmlprinter``
+    Simplified xml formatting util, based on *xmlprinter v0.1.0*.  Widely used
+    in contrib scripts for formatting XML output.  Also used in:
+
+    - ``Cerebrum.modules.abcenterprise.ABCXmlWriter.ABCXMLWriter``
+    - ``Cerebrum.modules.fs.import_from_FS.ImportFromFs``
+    - ``Cerebrum.modules.xmlutils.GeneralXMLWriter.XMLWriter``
+    - ``Cerebrum.modules.no.fronter_lib.XMLWriter``
+    - ``Cerebrum.modules.no.hia.fronter_lib.XMLWriter``
+"""
---
+++ Cerebrum/extlib/records.py
@@ -0,0 +1,250 @@
+# -*- coding: utf-8 -*-
+#
+# ISC License
+#
+# Copyright (c) 2016, Kenneth Reitz <me@kennethreitz.org>
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+"""
+This module contains a modified copy of *records v0.5.3* by Kenneth Reitz
+
+Source: `<https://github.com/kennethreitz/records>`_.
+
+The *records* module is originally a database wrapper module, similar to
+Cerebrum.Database, but utilizing *sqlalchemy* for the DBAL.  We're only
+interested in the database row models, for use with our own row factory.  To
+avoid secondary dependencies, we include a copy of the relevant parts of
+*records*.
+
+- Removed:
+
+    - ``Database``
+    - ``Connection``
+    - ``Record.dataset``
+    - ``Record.export``
+    - ``RecordCollection.dataset``
+    - ``RecordCollection.export``
+    - ``_reduce_datetimes``
+    - ``cli``
+    - ``print_bytes``
+
+- Modified:
+
+    - ``Record.__repr__``
+
+- Added:
+
+    - ``Record.__len__``
+"""
+from collections import OrderedDict
+from inspect import isclass
+
+
+def isexception(obj):
+    """Given an object, return a boolean indicating whether it is an instance
+    or subclass of :py:class:`Exception`.
+    """
+    if isinstance(obj, Exception):
+        return True
+    if isclass(obj) and issubclass(obj, Exception):
+        return True
+    return False
+
+
+class Record(object):
+    """A row, from a query, from a database."""
+    __slots__ = ('_keys', '_values')
+
+    def __init__(self, keys, values):
+        self._keys = keys
+        self._values = values
+
+        # Ensure that lengths match properly.
+        assert len(self._keys) == len(self._values)
+
+    def keys(self):
+        """Returns the list of column names from the query."""
+        return self._keys
+
+    def values(self):
+        """Returns the list of values from the query."""
+        return self._values
+
+    def __repr__(self):
+        return '<Record {}>'.format(self.as_dict())
+
+    def __getitem__(self, key):
+        # Support for index-based lookup.
+        if isinstance(key, int):
+            return self.values()[key]
+
+        # Support for string-based lookup.
+        if key in self.keys():
+            i = self.keys().index(key)
+            if self.keys().count(key) > 1:
+                raise KeyError(
+                    "Record contains multiple '{}' fields.".format(key))
+            return self.values()[i]
+
+        raise KeyError("Record contains no '{}' field.".format(key))
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError as e:
+            raise AttributeError(e)
+
+    def __dir__(self):
+        standard = dir(super(Record, self))
+        # Merge standard attrs with generated ones (from column names).
+        return sorted(standard + [str(k) for k in self.keys()])
+
+    def __len__(self):
+        return len(self.keys())
+
+    def get(self, key, default=None):
+        """Returns the value for a given key, or default."""
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def as_dict(self, ordered=False):
+        """Returns the row as a dictionary, as ordered."""
+        items = zip(self.keys(), self.values())
+
+        return OrderedDict(items) if ordered else dict(items)
+
+
+class RecordCollection(object):
+    """A set of excellent Records from a query."""
+    def __init__(self, rows):
+        self._rows = rows
+        self._all_rows = []
+        self.pending = True
+
+    def __repr__(self):
+        return '<RecordCollection size={} pending={}>'.format(len(self),
+                                                              self.pending)
+
+    def __iter__(self):
+        """Iterate over all rows, consuming the underlying generator
+        only when necessary."""
+        i = 0
+        while True:
+            # Other code may have iterated between yields,
+            # so always check the cache.
+            if i < len(self):
+                yield self[i]
+            else:
+                # Throws StopIteration when done.
+                # Prevent StopIteration bubbling from generator, following
+                # https://www.python.org/dev/peps/pep-0479/
+                try:
+                    yield next(self)
+                except StopIteration:
+                    return
+            i += 1
+
+    def next(self):
+        return self.__next__()
+
+    def __next__(self):
+        try:
+            nextrow = next(self._rows)
+            self._all_rows.append(nextrow)
+            return nextrow
+        except StopIteration:
+            self.pending = False
+            raise StopIteration('RecordCollection contains no more rows.')
+
+    def __getitem__(self, key):
+        is_int = isinstance(key, int)
+
+        # Convert RecordCollection[1] into slice.
+        if is_int:
+            key = slice(key, key + 1)
+
+        while len(self) < key.stop or key.stop is None:
+            try:
+                next(self)
+            except StopIteration:
+                break
+
+        rows = self._all_rows[key]
+        if is_int:
+            return rows[0]
+        else:
+            return RecordCollection(iter(rows))
+
+    def __len__(self):
+        return len(self._all_rows)
+
+    def all(self, as_dict=False, as_ordereddict=False):
+        """Returns a list of all rows for the RecordCollection. If they haven't
+        been fetched yet, consume the iterator and cache the results."""
+
+        # By calling list it calls the __iter__ method
+        rows = list(self)
+
+        if as_dict:
+            return [r.as_dict() for r in rows]
+        elif as_ordereddict:
+            return [r.as_dict(ordered=True) for r in rows]
+
+        return rows
+
+    def as_dict(self, ordered=False):
+        return self.all(as_dict=not(ordered), as_ordereddict=ordered)
+
+    def first(self, default=None, as_dict=False, as_ordereddict=False):
+        """Returns a single record for the RecordCollection, or `default`. If
+        `default` is an instance or subclass of Exception, then raise it
+        instead of returning it."""
+
+        # Try to get a record, or return/raise default.
+        try:
+            record = self[0]
+        except IndexError:
+            if isexception(default):
+                raise default
+            return default
+
+        # Cast and return.
+        if as_dict:
+            return record.as_dict()
+        elif as_ordereddict:
+            return record.as_dict(ordered=True)
+        else:
+            return record
+
+    def one(self, default=None, as_dict=False, as_ordereddict=False):
+        """Returns a single record for the RecordCollection, ensuring that it
+        is the only record, or returns `default`. If `default` is an instance
+        or subclass of Exception, then raise it instead of returning it."""
+
+        # Ensure that we don't have more than one row.
+        try:
+            self[1]
+        except IndexError:
+            return self.first(default=default, as_dict=as_dict,
+                              as_ordereddict=as_ordereddict)
+        else:
+            raise ValueError('RecordCollection contained more than one row. '
+                             'Expects only one row when using '
+                             'RecordCollection.one')
+
+    def scalar(self, default=None):
+        """Returns the first column of the first row, or `default`."""
+        row = self.one()
+        return row[0] if row else default
