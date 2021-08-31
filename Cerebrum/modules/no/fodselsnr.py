84b06c5f2f5993816782375282722b31458598a1
Simen Hellesund <sim@usit.uio.no>
2021-08-31 12:52:23 +0200

    Simplify SO-number year check
    personnr_ok were returning future birth years for some person objects.
    Deleting some lines in the SO-number year check seems to do away with the issue.
    
    CRB-3726

--- Cerebrum/modules/no/fodselsnr.py
+++ Cerebrum/modules/no/fodselsnr.py
@@ -93,14 +93,11 @@ def personnr_ok(nr, _ret_date=0, accept_00x00=True):
 
     # The rest of the hack for FS/SO numbers
     if SO_NUMBER:
-        if 120 <= pnr <= 199:
-            year += 2000
-        else:
-            import time
-            if year in range(int(time.strftime("%y")) + 1, 99 + 1):
-                year += 1900            # If year in [now + 1, ... 99] => year
-            else:                       # probably be previous century.
-                year += 2000            # Will potentially be a problem in 2050
+        import time
+        if year in range(int(time.strftime("%y")) + 1, 99):
+            year += 1900            # If year in [now + 1, ... 99] => year
+        else:                       # probably be previous century.
+            year += 2000            # Will potentially be a problem in 2050
 
     else:
         # Så var det det å kjenne igjen hvilket hundreår som er det riktige.
