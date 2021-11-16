3120a9cd341e5916bcbd186db6bd6fcd2fc1f8f0
Fredrik Larsen <fredrik.h.larsen@gmail.com>
2021-11-16 17:29:05 +0100

    Fix issue with finding forward_to addr for sysadm
    This commit fixes an issue with finding the appropriate forward_to email
    address for a given sysadm target account.
    
    Also adds util function for fetching primary email addresses for a given
    target type.  This will be useful when figuring out which email address
    to use for sysadm accounts.
    
    Issue: CRB-3274

--- Cerebrum/modules/no/uio/sysadm_ldif.py
+++ Cerebrum/modules/no/uio/sysadm_ldif.py
@@ -20,44 +20,17 @@
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 """
 OrgLDIF module for generating a sysadm ldif.
-
-TODOs
------
-Non-arbitrary account priority
-    We should fix the account priority lookups to avoid 'fallback' to a less
-    prioritized account when adding filters (expire_date, spreads).
-
-    The whole account priority bit should probably be separated from
-    account_type.
-
-Missing mandatory attrs
-    Missing eduPersonPrimaryOrgUnitDN, eduPersonOrgUnitDN.  Not sure how to
-    handle:
-
-    1. Dummy OU and set OrgUnit to that?
-    2. Refer to OUs at cn=organization,dc=uio,dc=no?  We'd need to somehow
-       build the ou-mapping (ou2DN) like OrgLDIF, but without outputting any
-       OUs.
-    3. These attrs are semi-mandatory, and low availability in higher edu -
-       should we just ignore them?
 """
 from __future__ import print_function, unicode_literals
 
 import logging
 
 from Cerebrum.Utils import make_timer
-from Cerebrum.modules.Email import EmailTarget
 from Cerebrum.modules.no.OrgLDIF import norEduLDIFMixin
 
 from . import sysadm_utils
 
 logger = logging.getLogger(__name__)
-
-
-def _get_feide_sysadm_accounts(db):
-    return sysadm_utils.get_sysadm_accounts(
-        db,
-        suffix=sysadm_utils.SYSADM_SUFFIX_DRIFT)
 
 
 class SysAdmOrgLdif(norEduLDIFMixin):
@@ -74,32 +47,19 @@ class SysAdmOrgLdif(norEduLDIFMixin):
 
         We override it with one that returns sysadm users.
         """
-        self._account_to_primary = pri = {}
-        for account in _get_feide_sysadm_accounts(self.db):
-            pri[account['account_id']] = account.pop('primary_account_id')
-            yield account
+        return sysadm_utils.get_sysadm_accounts(
+            self.db,
+            suffix=sysadm_utils.SYSADM_SUFFIX_DRIFT)
 
     def init_account_mail(self, use_mail_module):
         if use_mail_module:
             timer = make_timer(logger,
-                               "Fetching primary account e-mail addresses...")
-            # cache all <uname>@uio.no email addresses
-            targets = EmailTarget(self.db).list_email_target_addresses
-            mail = {}
-            for row in targets(target_type=self.const.email_target_account,
-                               domain='uio.no', uname_local=True):
-                # Can only return username@uio.no so no need for any checks
-                mail[int(row['target_entity_id'])] = "@".join(
-                    (row['local_part'], row['domain']))
+                               "Fetching e-mail forward addresses...")
 
-            # Pick an appropriate email address for each account
-            self.account_mail = {}
-            for account_id, pri_id in self._account_to_primary.items():
-                if pri_id in mail:
-                    self.account_mail[account_id] = mail[pri_id]
-                else:
-                    logger.warning('No email address for account_id=%d, '
-                                   'primary_account_id=%d', account_id, pri_id)
+            # cache all sysadm <uname>@uio.no primary addresses (only forwards)
+            self.account_mail = sysadm_utils.cache_primary_addresses(
+                self.db, self.const.email_target_forward)
+
             logger.info('found e-mail address for %d accounts',
                         len(self.account_mail))
 
