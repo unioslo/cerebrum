6dbac050d037aec68c14d7a6cabe07391be125d4
Sverre Stafsengen Broen <sbr@cere-utv04.uio.no>
2021-11-24 14:18:16 +0100

    added error handling, moved parts of get_task to EmployeeTask, added config as args for script
    fixup! added error handling, moved parts of get_task to EmployeeTask, added config as args for script
    
    Removed function in dfo/tasks for creating specific task with sub queue, using create_manual_task insted in script for manually syncing. Using same db_context for all task-queue transactions. Not merging old and new task, overwriting old-task with new instead
    
    removed unused function
    
    replaced person_id with dfo affiliation, to dfo pid from search_external_ids
    
     removed unused import, dummy parameter in create_tasks, fixed some comments and renamed functions and variables

--- contrib/no/dfo/manually_sync_dfo_sap_accounts.py
+++ contrib/no/dfo/manually_sync_dfo_sap_accounts.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright 2009, 2010, 2012 University of Oslo, Norway
+# Copyright 2021 University of Oslo, Norway
 # This file is part of Cerebrum.
 # Cerebrum is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
@@ -16,7 +16,7 @@
 """
 Script for filling up task queue with id for persons from source system SAPDFO
 and accounts from DFØ to force synchronization.
-For each person there is added 5 minutes penalty to nbf based on previous
+For each person there is added 2 minutes penalty to nbf based on previous
 person's nbf.
 """
 
@@ -25,9 +25,9 @@ import argparse
 
 import Cerebrum.logutils
 from Cerebrum.Utils import Factory
-from Cerebrum.modules.tasks import task_models
 from Cerebrum.modules.tasks.task_queue import TaskQueue
 from Cerebrum.modules.no.dfo.client import get_client, SapClientConfig
+from Cerebrum.modules.no.dfo.tasks import EmployeeTasks
 from Cerebrum.config.loader import read_config
 from Cerebrum.utils.date import now
 from Cerebrum.utils.argutils import add_commit_args
@@ -37,82 +37,62 @@ from datetime import timedelta
 logger = logging.getLogger(__name__)
 
 
-def create_tasks(db, dryrun):
-    account_ids = get_dfo_accounts()
-    person_ids = get_persons(db)
-    all_persons = merge_lists(account_ids, person_ids)
+# creating a task and pushes it to the task queue for dfo and dfo_sap person id
+def create_tasks(db_ctx, dfo_accounts):
     nbf = now()
-    test = all_persons[:25]
-    for person_id in test:
-        nbf = nbf + timedelta(seconds=60*5)
-        task = get_task(person_id, nbf, db)
-        with db_context(Factory.get('Database')(), dryrun) as db:
-            result = TaskQueue(db).push_task(task)
+    for account_id in dfo_accounts:
+        try:
+            nbf = nbf + timedelta(seconds=60*2)
+            task = EmployeeTasks.create_manual_task(account_id,
+                                                    sub='sap-dfo-fullsync',
+                                                    nbf=nbf)
+            result = TaskQueue(db_ctx).push_task(task)
             logger.info(result)
+        except Exception as e:
+            logger.warning('Could not create task and push to queue ' +
+                           'for person %s', account_id)
+            logger.error(e)
 
 
-def get_dfo_accounts():
-    config = SapClientConfig()
-    config.load_dict(read_config('../../../../crb-config-uio/etc/' +
-                                 'cerebrum/config/hr-import-client.yml'))
+# fetching all dfo accounts and generate list with person id's
+def get_dfo_accounts(config):
     client = get_client(config)
-    accounts = client.get_employee("")
-    account_ids = []
+    dfo_acc = client.get_employee("")
+    account_ids = set()
 
-    for account in accounts:
+    for account in dfo_acc:
         # Converting to int to remove 00 prefix
         acc_id = int(account['id'])
-        account_ids.append(acc_id)
+        account_ids.add(acc_id)
     return account_ids
 
 
-def get_persons(db):
+# fetching all dfo ids in cerebrum using persons with dfo id type as external id
+def get_external_ids(db):
     pe = Factory.get('Person')(db)
     co = Factory.get('Constants')(db)
-    persons = pe.list_affiliations(source_system=co.system_dfo_sap)
-    person_ids = []
+    persons = pe.search_external_ids(id_type=co.externalid_dfo_pid)
+    external_ids = set()
     for person in persons:
-        person_ids.append(person['person_id'])
-    return person_ids
+        external_ids.add(person['external_id'])
+    return external_ids
 
 
-def merge_lists(account_ids, person_ids):
-    for account_id in account_ids:
-        if account_id not in person_ids:
-            person_ids.append(account_id)
-    return person_ids
-
-
-def get_task(person_id, nbf, db):
-    queue = 'dfo-employee'
-    sub = 'forced-sapdfo-sync'
-
-    old_task = TaskQueue.get_task(TaskQueue(db), queue, sub, person_id)
-
-    payload = task_models.Payload(
-        fmt='dfo_event',
-        version=1,
-        data={'id': person_id, 'uri': 'dfo:ansatte'})
-    new_task = task_models.Task(
-        queue=queue,
-        sub=sub,
-        key=person_id,
-        nbf=nbf,
-        attempts=0,
-        reason='forced-sync: on={when}'.format(when=now()),
-        payload=payload,
-    )
-
-    task = task_models.merge_tasks(new_task, old_task)
-
-    return task
+# setting up config with path from args for dfo api
+def get_config(path):
+    config = SapClientConfig()
+    config.load_dict(read_config(path))
+    return config
 
 
 def main(args=None):
-    db = Factory.get('Database')()
-
     parser = argparse.ArgumentParser(
         description='Create manual tasks for DFO-SAP accounts to update'
+    )
+    parser.add_argument(
+        '-c', '--config',
+        required=True,
+        help='config for dfo api'
     )
     add_commit_args(parser)
     log_subparser = Cerebrum.logutils.options.install_subparser(parser)
@@ -120,10 +100,20 @@ def main(args=None):
         Cerebrum.logutils.options.OPTION_LOGGER_LEVEL: 'INFO',
     })
     args = parser.parse_args(args)
+
     Cerebrum.logutils.autoconf('tee', args)
+
+    config = get_config(args.config)
     dryrun = not args.commit
 
-    create_tasks(db, dryrun)
+    db = Factory.get('Database')()
+
+    dfo_acc = get_dfo_accounts(config)
+    external_ids = get_external_ids(db)
+    all_ids = set.union(dfo_acc, external_ids)
+
+    with db_context(Factory.get('Database')(), dryrun) as db_ctx:
+        create_tasks(db_ctx, all_ids)
 
 
 if __name__ == '__main__':
