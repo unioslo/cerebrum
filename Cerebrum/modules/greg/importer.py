724c55b8b912709f94c7d4f8edc863f3f93491ad
Fredrik Larsen <fredrik.h.larsen@gmail.com>
2021-12-07 18:51:01 +0100

    Add mapping/importing business logic for Greg
    Issue: CRB-3783

---
+++ Cerebrum/modules/greg/importer.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2021 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+""" Greg person import/update.  """
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
+import logging
+
+from Cerebrum.Utils import Factory
+from Cerebrum.modules.import_utils.matcher import (
+    OuMatcher,
+    PersonMatcher,
+)
+from Cerebrum.modules.import_utils.syncs import (
+    AffiliationSync,
+    ContactInfoSync,
+    ExternalIdSync,
+    PersonNameSync,
+)
+
+from .mapper import GregMapper
+from .datasource import GregDatasource
+
+logger = logging.getLogger(__name__)
+
+
+class GregImporter(object):
+
+    REQUIRED_PERSON_ID = (
+        'NO_BIRTHNO',
+        'PASSNR',
+    )
+
+    MATCH_ID_TYPES = (
+        'GREG_PID',
+    )
+
+    mapper = GregMapper()
+
+    def __init__(self, db, client):
+        self.db = db
+        self.const = co = Factory.get('Constants')(db)
+        self.datasource = GregDatasource(client)
+
+        source_system = co.system_greg
+        self._sync_affs = AffiliationSync(db, source_system)
+        self._sync_cinfo = ContactInfoSync(db, source_system)
+        self._sync_ids = ExternalIdSync(db, source_system)
+        self._sync_name = PersonNameSync(db, source_system, (co.name_first,
+                                                             co.name_last))
+
+    def get_person(self, greg_person):
+        """ Find matching person from a Greg person dict. """
+        search = PersonMatcher(self.MATCH_ID_TYPES)
+        criterias = tuple(self.mapper.get_person_ids(greg_person))
+        if not criterias:
+            raise ValueError('invalid person: no external_ids')
+        return search(self.db, criterias, required=False)
+
+    def get_ou(self, greg_orgunit):
+        """ Find matching ou from a Greg orgunit dict. """
+        search = OuMatcher()
+        criterias = tuple(self.mapper.get_orgunit_ids(greg_orgunit))
+        if not criterias:
+            raise ValueError('invalid orgunit: no external_ids')
+        return search(self.db, criterias, required=True)
+
+    def handle_reference(self, reference):
+        """
+        Initiate hr import from reference.
+
+        This is the entrypoint for use with e.g. scripts.
+        Fetches object data from the datasource and calls handle_object.
+        """
+        greg_person = self.datasource.get_object(reference)
+        db_object = self.get_person(greg_person)
+        return self.handle_object(greg_person, db_object)
+
+    def handle_object(self, greg_person, person_obj):
+        """
+        Process info from Greg and update Cerebrum (i.e. initiate import).
+
+        This method inspects and compares source data and cerebrum data, and
+        calls the relevant create/update/remove method.
+
+        :type greg_person: dict
+        :type person_obj: Cerebrum.Person.Person, NoneType
+        """
+        greg_id = greg_person['id']
+        if self.mapper.is_active(greg_person):
+            if person_obj:
+                logger.info('handle_object: update greg_id=%s, person_id=%s',
+                            greg_id, person_obj.entity_id)
+                self.update(greg_person, person_obj)
+            else:
+                logger.info('handle_object: creating greg_id=%s', greg_id)
+                self.create(greg_person)
+        elif person_obj:
+            logger.info('handle_object: remove greg_id=%s, person_id=%s',
+                        greg_id, person_obj.entity_id)
+            self.remove(greg_person, person_obj)
+        else:
+            logger.info('handle_object: ignoring greg_id=%s', greg_id)
+
+        # Greg sends new messages for future events, no retry dates needed
+        return tuple()
+
+    def create(self, greg_person):
+        """ Create a new Person object using greg person data. """
+        if not greg_person:
+            raise ValueError('create() called without greg data!')
+        greg_id = greg_person['id']
+
+        id_types = set(id_type for id_type, _
+                       in self.mapper.get_person_ids(greg_person))
+
+        if (self.REQUIRED_PERSON_ID
+                and not any(id_type in self.REQUIRED_PERSON_ID
+                            for id_type in id_types)):
+            raise ValueError('Missing required identifier, need one of '
+                             + repr(self.REQUIRED_PERSON_ID))
+
+        person_obj = Factory.get('Person')(self.db)
+        gender = self.const.gender_unknown
+        dob = greg_person['date_of_birth']
+        if not dob:
+            raise ValueError('No birth date, unable to create!')
+        person_obj.populate(dob, gender)
+        person_obj.write_db()
+
+        if not person_obj.entity_id:
+            raise RuntimeError('Write failed, unable to create!')
+
+        logger.info('created new person, greg_id=%s, person_id=%s',
+                    greg_id, person_obj.entity_id)
+        self.update(greg_person, person_obj)
+
+    def remove(self, greg_person, person_obj):
+        """ Clear HR data from a Person object. """
+        if person_obj is None or not person_obj.entity_id:
+            raise ValueError('remove() called without cerebrum person!')
+
+        # TODO/TBD: Are there any steps needed besides clearing everything
+        # (except the GREG_PID) that update() sets?
+        blank_person = {'id': greg_person['id']}
+        self.update(blank_person, person_obj)
+
+    def update(self, greg_person, person_obj):
+        """ Update the Person object using employee_data. """
+        if not greg_person:
+            raise ValueError('update() called without greg person data!')
+        if person_obj is None or not person_obj.entity_id:
+            raise ValueError('update() called without cerebrum person!')
+
+        self._sync_name(person_obj, self.mapper.get_names(greg_person))
+        self._sync_ids(person_obj, self.mapper.get_person_ids(greg_person))
+        self._sync_cinfo(person_obj, self.mapper.get_contact_info(greg_person))
+        affs = (
+            (aff_status, self.get_ou(ou_data).entity_id)
+            for aff_status, ou_data
+            in self.mapper.get_affiliations(greg_person)
+        )
+        self._sync_affs(person_obj, affs)
---
+++ Cerebrum/modules/greg/mapper.py
@@ -0,0 +1,352 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2021 University of Oslo, Norway
+#
+# This file is part of Cerebrum.
+#
+# Cerebrum is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Cerebrum is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Cerebrum; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+"""
+Parsing, mapping and filtering of Greg values
+
+This module contains utils for extracting Cerebrum data from Greg objects.
+
+Import/update utils should use a py:class:`.GregMapper` to extract relevant
+Cerebrum data from a (sanitized) Greg object.
+
+Future changes
+--------------
+The Greg mappers are not configurable.  If diverging business logic is required
+in the future, we may opt to either:
+
+- Subclass GregMapper
+- Add a mapper config, which we feed to the GregMapper on init.
+"""
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
+import datetime
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class GregOrgunitIds(object):
+    """
+    Extract external ids from greg org unit data.
+
+    >>> orgunit = {
+    ...     'id': '1',
+    ...     'identifiers': [{
+    ...        'source': 'orgreg',
+    ...        'name': 'orgreg_id',
+    ...        'value': '1234',
+    ...     }]}
+    >>> get_org_ids = GregOrgunitIds()
+    >>> list(get_org_ids(orgunit))
+    [('ORGREG_OU_ID', '1234')]
+    """
+
+    # (identifiers.source, identifiers.name) -> cerebrum external_id
+    type_map = {
+        ('orgreg', 'orgreg_id'): 'ORGREG_OU_ID',
+    }
+
+    # id -> cerebrum external_id
+    greg_id_type = None
+
+    def __call__(self, greg_data):
+        """
+        :param dict greg_data:
+            Sanitized org unit data (e.g. from `datasource.parse_orgunit`)
+
+        :returns generator:
+            Valid Cerebrum (id_type, id_value) pairs
+        """
+        greg_id = greg_data['id']
+
+        if self.greg_id_type:
+            yield self.greg_id_type, greg_id
+
+        for id_obj in greg_data.get('identifiers', ()):
+            (source, name, value) = (id_obj['source'], id_obj['name'],
+                                     id_obj['value'])
+            if (source, name) in self.type_map:
+                yield self.type_map[source, name], value
+            else:
+                logger.debug('ignoring unknown org unit id (%s, %s) '
+                             'for greg_id=%s', source, name, greg_id)
+
+
+def get_names(greg_data):
+    """
+    Get names for a given person.
+
+    :param dict greg_data:
+        Sanitized greg person data (e.g. from `datasource.parse_person`)
+
+    :returns generator:
+        Valid Cerebrum (name_type, name_value) pairs
+    """
+    fn = greg_data['first_name']
+    ln = greg_data['last_name']
+    if fn:
+        yield ('FIRST', fn)
+    if ln:
+        yield ('LAST', ln)
+
+
+class GregPersonIds(object):
+    """ Extract external ids from greg person data.
+
+    >>> person = {
+    ...     'id': '1',
+    ...     'identities': [{
+    ...         'type': 'passport_number',
+    ...         'verified': 'automatic',
+    ...         'value': 'NO-123',
+    ...     }]}
+    >>> get_ids = GregPersonIds()
+    >>> list(get_ids(person))
+    [('GREG_PID', '1'), ('PASSNR', 'NO-123')]
+    """
+
+    # identities.type value -> cerebrum external_id
+    type_map = {
+        'norwegian_national_id_number': 'NO_BIRTHNO',
+        'passport_number': 'PASSNR',
+    }
+
+    # known, irrelevant identities.type
+    ignore_types = set((
+        'feide_id',
+        'private_email',
+        'private_mobile',
+    ))
+
+    # values of identities.verified to accept
+    verified_values = set((
+        'automatic',
+        'manual',
+    ))
+
+    # id -> cerebrum external_id
+    greg_id_type = 'GREG_PID'
+
+    def __call__(self, greg_data):
+        """
+        :param dict greg_data:
+            Sanitized greg person data (e.g. from `datasource.parse_person`)
+
+        :returns generator:
+            Valid Cerebrum (id_type, id_value) pairs
+        """
+        greg_id = greg_data['id']
+
+        if self.greg_id_type:
+            yield self.greg_id_type, greg_id
+
+        for id_obj in greg_data.get('identities', ()):
+            if id_obj['type'] in self.ignore_types:
+                continue
+            if id_obj['type'] not in self.type_map:
+                logger.debug('ignoring unknown id_type=%r for greg_id=%s',
+                             id_obj['type'], greg_id)
+                continue
+            crb_type = self.type_map[id_obj['type']]
+            if id_obj['verified'] not in self.verified_values:
+                logger.debug('ignoring unverified id_type=%r for greg_id=%s',
+                             id_obj['type'], greg_id)
+                continue
+
+            value = id_obj['value']
+            # TODO/TBD: Validate values (e.g. valid fnr), or should this be
+            # done by the import?
+            yield crb_type, value
+
+
+class GregContactInfo(object):
+    """ Extract contanct info from greg person data.
+
+    >>> person = {
+    ...     'id': '1',
+    ...     'identities': [{
+    ...         'type': 'private_mobile',
+    ...         'verified': 'automatic',
+    ...         'value': '20123456',
+    ...     }]}
+    >>> get_contacts = GregContactInfo()
+    >>> list(get_contacts(person))
+    [('PRIVATE_MOBILE', '20123456')]
+    """
+
+    # identities.type value -> cerebrum contact_info_type
+    type_map = {
+        'private_email': 'EMAIL',
+        'private_mobile': 'PRIVATE_MOBILE',
+    }
+
+    # known, irrelevant identities.type
+    ignore_types = set((
+        'feide_id',
+        'norwegian_national_id_number',
+        'passport_number',
+    ))
+
+    # values of identities.verified to accept
+    verified_values = set((
+        'automatic',
+        'manual',
+    ))
+
+    def __call__(self, greg_data):
+        """
+        :param dict greg_data:
+            Sanitized greg person data (e.g. from `datasource.parse_person`)
+
+        :returns generator:
+            Valid Cerebrum (contact_info_type, contact_info_value) pairs
+        """
+        greg_id = int(greg_data['id'])
+        for id_obj in greg_data.get('identities', ()):
+            if id_obj['type'] in self.ignore_types:
+                continue
+            if id_obj['type'] not in self.type_map:
+                logger.debug('ignoring unknown id_type=%r for greg_id=%s',
+                             id_obj['type'], greg_id)
+                continue
+            crb_type = self.type_map[id_obj['type']]
+            if id_obj['verified'] not in self.verified_values:
+                logger.debug('ignoring unverified id_type=%r for greg_id=%s',
+                             id_obj['type'], greg_id)
+                continue
+
+            value = id_obj['value']
+            # TODO/TBD: Validate values (e.g. valid email, phone), or should
+            # this be done by the import?
+            yield crb_type, value
+
+
+class GregConsents(object):
+    """ Extract consent data from greg person data. """
+
+    def __call__(self, greg_data):
+        """
+        :param dict greg_data:
+            Sanitized greg person data (e.g. from `datasource.parse_person`)
+
+        :returns generator:
+            TODO/TBD: Return value depends on how consents are actually
+            represented in Greg, and how they will be represented in Cerebrum.
+        """
+        greg_id = int(greg_data['id'])
+        for c_obj in greg_data.get('consents', ()):
+            consent_type = c_obj['consent_type']
+            given_at = c_obj['consent_given_at']
+            # TODO/TBD: what constitutes a consent?  Is it enough that it
+            # exists in the consents tuple, or does consent_given_at need to be
+            # set as well?
+            if False:
+                # TODO: never reached 'yield' to make this a generator
+                yield (greg_id, consent_type, given_at)
+
+
+class GregRoles(object):
+    """ Extract affiliations from greg person roles. """
+
+    type_map = {
+        'emeritus': 'TILKNYTTET/emeritus',
+        'external-consultant': 'TILKNYTTET/ekst_partner',
+        'external-partner': 'TILKNYTTET/ekst_partner',
+        'guest-researcher': 'TILKNYTTET/gjesteforsker',
+    }
+
+    def __call__(self, greg_data, _today=None):
+        """
+        :param dict greg_data:
+            Sanitized greg person data (e.g. from `datasource.parse_person`)
+
+        :returns generator:
+            yields (affiliation, orgunit) tuples.
+
+            - Affiliation is an AFFILIATION/status string
+            - orgunit is the role orgunit value.
+        """
+        greg_id = int(greg_data['id'])
+
+        # TODO: We may want to allow expired roles here, and filter them out in
+        # the is_active check, and ignore them when updating person affs.
+        today = _today or datetime.date.today()
+
+        for role_obj in greg_data.get('roles', ()):
+            if role_obj['type'] not in self.type_map:
+                logger.debug(
+                    'ignoring unknown role type=%r, id=%r for greg_id=%s',
+                    role_obj['type'], role_obj['id'], greg_id)
+                continue
+            if role_obj['start_date'] > today:
+                logger.debug(
+                    'ignoring future role type=%r, id=%r for greg_id=%s',
+                    role_obj['type'], role_obj['id'], greg_id)
+                continue
+            if role_obj['end_date'] < today:
+                logger.debug(
+                    'ignoring expired role type=%r id=%r for greg_id=%s',
+                    role_obj['type'], role_obj['id'], greg_id)
+                continue
+
+            crb_type = self.type_map[role_obj['type']]
+            orgunit = role_obj['orgunit']
+            yield crb_type, orgunit
+
+
+class GregMapper(object):
+    """ Mapper object for greg person objects. """
+
+    get_contact_info = GregContactInfo()
+    get_person_ids = GregPersonIds()
+    get_affiliations = GregRoles()
+    get_consents = GregConsents()
+    get_orgunit_ids = GregOrgunitIds()
+
+    @classmethod
+    def get_names(cls, greg_data):
+        return tuple(get_names(greg_data))
+
+    def is_active(self, greg_data, _today=None):
+        """ Check if a guest is active from the provided greg data. """
+        greg_id = int(greg_data['id'])
+        today = _today or datetime.date.today()
+
+        if len(greg_data) == 1:
+            # We only have {'id': ...} - which means that the person does
+            # not exist in greg
+            logger.info('no greg-data for greg_id=%s', greg_id)
+            return False
+
+        if (not greg_data['registration_completed_date']
+                or greg_data['registration_completed_date'] > today):
+            logger.info('incomplete registration for greg_id=%s', greg_id)
+            return False
+
+        if not list(self.get_affiliations(greg_data, _today=today)):
+            logger.info('no active roles for greg_id=%s', greg_id)
+            return False
+
+        # TODO: should we check for certain required consents?
+        return True
