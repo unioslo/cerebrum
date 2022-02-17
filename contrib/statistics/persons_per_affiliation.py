3ba2167cb3c3b5708944bca0e3b4056802cdbbb4
Mikael Nielsen R|ykenes <mnr@cere-utv04.uio.no>
2022-02-17 16:12:58 +0100

    Now it checks for affiliations of emplyees, students, and connected users, and outputs the csb print to the file, currently called output.txt

--- contrib/statistics/persons_per_affiliation.py
+++ contrib/statistics/persons_per_affiliation.py
@@ -1,4 +1,5 @@
 import sys
+import codecs
 
 import Cerebrum.utils.csvutils as _csvutils
 from Cerebrum.Errors import NotFoundError
@@ -9,7 +10,6 @@ from Cerebrum.Utils import Factory
 def generate_amounts_affiliations(db, affiliation_type):
     pe = Factory.get('Person')(db)
     ou = Factory.get('OU')(db)
-    ou_cache = OuCache(db)
 
     amount_dict = {}
     affiliations = pe.list_affiliations(affiliation=affiliation_type)
@@ -36,22 +36,71 @@ def generate_amounts_affiliations(db, affiliation_type):
                 else:
                     amount_dict[parent] = amount_dict[parent]+amount_dict.pop(place)
             ou.clear()
+    return amount_dict
+
+def combine_numbers(db, ansatt_dict, student_dict, tilknyttet_dict):
+    ou_cache = OuCache(db)
+
     print_info = []
-    for place in amount_dict:
+    for place in list(ansatt_dict):
+        ansatte = ansatt_dict.pop(place)
+        try:
+            studenter = student_dict.pop(place)
+        except KeyError:
+            studenter = 0
+        try:
+            tilknyttede = tilknyttet_dict.pop(place)
+        except KeyError:
+            tilknyttede = 0
         print_info.append({
         'ou_id': place,
-        'seksjon': ou_cache.get_name(place).encode('utf-8'),
-        'antall brukere': amount_dict[place],
+        'seksjon': ou_cache.get_name(place),
+        'antall ansatt brukere': ansatte,
+        'antall student brukere': studenter,
+        'antall tilknyttet brukere': tilknyttede,
         })
+    if len(student_dict) != 0:
+        for place in list(student_dict):
+            ansatte = 0
+            studenter = student_dict.pop(place)
+            try:
+                tilknyttede = tilknyttet_dict.pop(place)
+            except KeyError:
+                tilknyttede = 0
+            print_info.append({
+            'ou_id': place,
+            'seksjon': ou_cache.get_name(place),
+            'antall ansatt brukere': ansatte,
+            'antall student brukere': studenter,
+            'antall tilknyttet brukere': tilknyttede,
+            })
+    if len(tilknyttet_dict) != 0:
+        for place in list(tilknyttet_dict):
+            ansatte = 0
+            studenter = 0
+            tilknyttede = tilknyttet_dict.pop(place)
+            print_info.append({
+            'ou_id': place,
+            'seksjon': ou_cache.get_name(place),
+            'antall ansatt brukere': ansatte,
+            'antall student brukere': studenter,
+            'antall tilknyttet brukere': tilknyttede,
+            })
+
     return print_info
 
 def main():
     db = Factory.get('Database')()
     co = Factory.get('Constants')(db)
-    print_info = generate_amounts_affiliations(db, co.affiliation_ansatt)
-    fields = ['ou_id', 'seksjon', 'antall brukere']
+    ansatt = generate_amounts_affiliations(db, co.affiliation_ansatt)
+    student = generate_amounts_affiliations(db, co.affiliation_student)
+    tilknyttet = generate_amounts_affiliations(db, co.affiliation_tilknyttet)
+    print_info = combine_numbers(db, ansatt, student, tilknyttet)
+    fields = ['ou_id', 'seksjon', 'antall ansatt brukere', 'antall student brukere', 'antall tilknyttet brukere']
+    codec = codecs.lookup('utf-8')
     output_file = open('output.txt', 'w')
-    writer = _csvutils.UnicodeDictWriter(output_file, fields)
+    output = codec.streamwriter(output_file)
+    writer = _csvutils.UnicodeDictWriter(output, fields)
     writer.writeheader()
     writer.writerows(print_info)
     output_file.close()
