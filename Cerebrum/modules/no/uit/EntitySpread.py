616bf3322b0c82a0c70c8ba20a62fb3248ce4b18
Christina Skåre Nordtømme <csn@sylvain.uio.no>
2022-05-05 15:01:41 +0200

    Removed superfluous code from EntitySpread.py for uit

--- Cerebrum/modules/no/uit/EntitySpread.py
+++ Cerebrum/modules/no/uit/EntitySpread.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2003-2019 University of Oslo, Norway
+# Copyright 2003-2022 University of Oslo, Norway
 #
 # This file is part of Cerebrum.
 #
@@ -33,20 +33,6 @@ from Cerebrum.utils import date_compat
 logger = logging.getLogger(__name__)
 
 
-def to_date(obj):
-    if obj is None:
-        return None
-    else:
-        return date_compat.get_date(obj, allow_none=False )
-
-
-def to_delta(obj):
-    if obj is None:
-        return None
-    else:
-        return date_compat.get_timedelta(obj, allow_none=False)
-
-
 def get_expire_policy(spread):
     """
     Get spread policy for a given spread.
@@ -56,7 +42,7 @@ def get_expire_policy(spread):
     """
     policy_db = getattr(cereconf, 'SPREAD_EXPIRE_POLICY', {})
     policies = [
-        (to_delta(days), template)
+        (date_compat.get_timedelta(days, allow_none=False), template)
         for days, template in policy_db.get(spread, ())]
     if not policies:
         logger.debug('No spread expire policy for spread=%s', repr(spread))
@@ -89,7 +75,7 @@ class UitEntitySpreadMixin(spread_expire.EntitySpreadMixin):
         if entity_id is None:
             entity_id = self.entity_id
         if expire_date is None:
-            expire_date = datetime.datetime.now().date()
+            expire_date = datetime.date.today()
 
         needs_reset = self._spread_expire_db.exists(entity_id, spread)
         super(UitEntitySpreadMixin, self).set_spread_expire(spread,
