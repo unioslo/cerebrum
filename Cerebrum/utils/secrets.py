eef2a56b01a70914439fa9551675459514301ceb
Fredrik Larsen <fredrik.h.larsen@gmail.com>
2022-09-01 16:42:04 +0200

    Refactor secrets config + utils
    This commit moves some useful functionality from the
    ``Cerebrum.config.secrets`` module to the ``Cerebrum.utils.secrets``
    module, and documents these modules a bit more.
    
    Issue: CRB-1312

--- Cerebrum/utils/secrets.py
+++ Cerebrum/utils/secrets.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2020 University of Oslo, Norway
+# Copyright 2020-2022 University of Oslo, Norway
 #
 # This file is part of Cerebrum.
 #
@@ -21,45 +21,65 @@
 """
 Utilities for fetching passwords and other secrets.
 
+This module contains *handlers* for various sources of secret/protected
+information, like passwords or keys.  Each *handler* takes a single argument,
+but some sources may require many arguments to be serialized into a single
+argument string (e.g. legacy-file).
+
+
 Sources
 -------
 file
     Specify the full path to a file with secret data. Trailing newlines are
-    removed.  Example: ``/path/to/file``
+    removed.  Example argument: ``/path/to/file``
 
 auth-file
     Specify a filename in the ``cereconf.DB_AUTH_DIR`` directory.  Trailing
-    newlines are removed.  Example: ``passwd-example-file``
+    newlines are removed.  Example argument: ``passwd-example-file``
 
 legacy-file
     Specify a password to retrieve from ``cereconf.DB_AUTH_DIR`` using the same
     parameters as the legacy ``read_password`` function.
-    Examples:
-        - ``user@system`` (equivalent to ``auth-file``/``passwd-user@system``)
-        - ``user@system@host`` (equivalent to
-        ``auth-file``/``passwd-user@system@host``)
+
+    Note that legacy files contain both a username (for sanity check) and a
+    password, separated by a tab character.  This means that passord files
+    formatted for legacy-file cannot be fetched using e.g. ``auth-file``
+    without further processing.
+
+    Example arguments:
+
+    - ``user@system`` (looks up ``<DB_AUTH_DIR>/passwd-user@system``)
+    - ``user@system@host`` (looks up ``<DB_AUTH_DIR>/passwd-user@system@host``)
 
 plaintext
     Provide a plaintext secret, as is.  Useful in configuration files where
-    providing the plaintext password is ok.  Example: ``hunter2``.
+    providing the plaintext secret is OK (e.g. mock values for tests).  Example
+    argument: ``hunter2``.
 
 
-Usage
------
-To look up a given secret using a given method:
+To look up a given secret using a given source:
 
 >>> get_secret('plaintext', 'hunter2')
 'hunter2'
 
-To allow multiple sources in a config field:
 
-::
+Secret strings
+--------------
+Secret strings makes it possible to encode *source* and a *source-argument* as
+a single string value, with format ``<source>:<source-argument>``.
 
-    >>> def get_password(value):
-    ...     source, _, arg = value.partition(':')
-    ...     return get_secret(source, arg)
-    >>> get_password('plaintext:hunter2')
-    'hunter2'
+This is typically used in config files to allow multiple types of lookup of
+secrets.  Example:
+
+>>> config = {"user": "AzureDiamond", "pass": "plaintext:hunter2"}
+>>> get_secret_from_string(config["pass"])
+'hunter2'
+
+Note that the *source-argument* may contain ``:`` characters:
+
+>>> get_secret_from_string("plaintext::foo:bar:)
+':foo:bar:'
+
 
 TODO
 ----
@@ -131,6 +151,21 @@ def _read_legacy_password_file(value):
 sources.register('plaintext')(lambda s: s)
 
 
+def get_handler(source):
+    """
+    Fetch a given source handler.
+
+    :param source:
+        One of the methods for fetching a secret (as provided by ``sources``).
+
+    :rtype: callable
+    """
+    if source not in sources:
+        raise ValueError("Invalid source %s, must be one of %s"
+                         % (repr(source), repr(tuple(sources))))
+    return sources[source]
+
+
 def get_secret(source, value):
     """
     Fetch a secret from a given source.
@@ -144,7 +179,35 @@ def get_secret(source, value):
     :rtype: str
     :returns: Returns a matching secret
     """
-    return sources[source](value)
+    handler = get_handler(source)
+    return handler(value)
+
+
+def split_secret_string(raw_value):
+    """
+    Split raw secrets string value into (source, args) tuple.
+
+    :param raw_value: A text string with format "<source>:<value>".
+
+    :rtype: tuple
+    :returns: Returns a pair with "<source>" and "<value>"
+    """
+    source, sep, source_arg = raw_value.partition(':')
+    if not sep:
+        # Missing mandatory ':' separator
+        raise ValueError("Invalid format, must be '<source>:<secret>'")
+    return source, source_arg
+
+
+def get_secret_from_string(raw_value):
+    """
+    Lookup secret from "<source>:<args>" string.
+
+    :rtype: str
+    :returns: The secret from the given source
+    """
+    source, arg = split_secret_string(raw_value)
+    return get_secret(source, arg)
 
 
 def _main():
