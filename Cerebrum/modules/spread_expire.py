bf1f56871597fe233a9f40b7d4ce07867cedd22e
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2023-02-03 11:58:32 +0100

    Move sync_spread_expire to spread_expire module

--- Cerebrum/modules/spread_expire.py
+++ Cerebrum/modules/spread_expire.py
@@ -26,12 +26,12 @@ inform users that gets a spread revoked.
 import logging
 
 import mx.DateTime
-import six
 
 from Cerebrum import Errors
 from Cerebrum.DatabaseAccessor import DatabaseAccessor
 from Cerebrum.Entity import EntitySpread
 from Cerebrum.Utils import argument_to_sql
+from Cerebrum.utils import date_compat
 
 logger = logging.getLogger(__name__)
 
@@ -272,3 +272,92 @@ class EntitySpreadMixin(EntitySpread):
                 spread=spread,
                 after_date=expire_date,
             ))
+
+
+def sync_spread_expire(entity, spread_expire_map):
+    """
+    Update spread expire-date for a selection of spreads.
+
+    :param EntitySpreadMixin entity: the object to sync spread-expire to
+    :param dict spread_expire_map: spread -> expire-date mapping
+    """
+    # TODO: We should probably use this function everywhere spread-expire is
+    # updated.  I.e.: Rather than doing:
+    #
+    #   for spread in need_spreads:
+    #       account.set_spread_expire(spread, expire_date=expire_date)
+    #
+    # We should do:
+    #
+    #   spread_expire = {spread: expire_date for spread in need_spreads}
+    #   sync_spread_expire(account, spread_expire)
+    #
+    expire_db = SpreadExpire(entity._db)
+    entity_id = int(entity.entity_id)
+    const = entity.const
+
+    # Fetch current spread expire dates (and register duplicate or obsolete
+    # entries)
+    current = {}
+    obsolete = set()
+    duplicate = set()
+    for row in expire_db.search(entity_id=entity_id):
+        spread_int = row['spread']
+        expire_date = date_compat.get_date(row['expire_date'])
+        try:
+            spread = const.get_constant(const.Spread, spread_int)
+        except LookupError as e:
+            # spread_expire has no foreign key to spread_code.code
+            logger.debug("invalid spread in spread-expire (%r, %r, %r): %s",
+                         row['entity_id'], row['spread'], row['expire_date'],
+                         e)
+            obsolete.add(spread_int)
+            continue
+
+        # no (entity_id, spread) primary key - multiple expire date entries
+        # can exist for the same spread!
+        if spread in current:
+            if expire_date > current[spread]:
+                current[spread] = expire_date
+            duplicate.add(spread)
+        else:
+            current[spread] = expire_date
+
+    # We don't need to keep around expire date values for spreads that no
+    # longer exist:
+    # - if the same spread gets re-added it'll likely get another code!
+    # - if a spread gets re-added using this code, it'll probably not be the
+    #   same spread!
+    for spread_int in obsolete:
+        logger.info("Removing spread-expire for entity id=%r: "
+                    "obsolete spread=%r",
+                    entity_id, spread_int)
+        expire_db.delete(entity_id, spread_int)
+
+    # Identify expire dates that actually needs update (i.e. prolongs the
+    # current lifetime of the spread)
+    update = {}
+    for spread, expire_date in spread_expire_map.items():
+        if spread in current and expire_date > current[spread]:
+            # prolong existing expire date
+            update[spread] = expire_date
+        elif spread not in current:
+            # no existing expire date -- apply
+            update[spread] = expire_date
+
+    # Remove all duplicates by doing a delete() + set() cycle
+    for spread in duplicate:
+        expire_date = current[spread]
+        expire_db.delete(entity_id, spread)
+        if spread not in update:
+            logger.info("Resetting spread-expire for entity id=%r: "
+                        "duplicate spread=%s (%s)",
+                        entity_id, spread, expire_date)
+            expire_db.set(entity_id, spread, expire_date)
+
+    # Update spreads that needs a new expire-date
+    for spread, expire_date in update.items():
+        logger.info("Setting spread-expire for entity id=%r: "
+                    "spread=%s (%s -> %s)",
+                    entity_id, spread, current.get(spread), expire_date)
+        expire_db.set(entity_id, spread, expire_date)
