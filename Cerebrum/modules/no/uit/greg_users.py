ab0172d53b8725021d6915f96fcfbec9086c573a
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2023-02-03 10:55:51 +0100

    Update greg user spreads for UiT
    This commit fixes the `greg_user` user account maintenance to set the
    correct spreads and spread-expire for all user acocunts.
    
    Issue: CRB-3976

--- Cerebrum/modules/no/uit/greg_users.py
+++ Cerebrum/modules/no/uit/greg_users.py
@@ -32,8 +32,6 @@ The user maintenance is mostly a re-implementation of functions from
 import datetime
 import logging
 
-import six
-
 import cereconf
 
 from Cerebrum import Errors
@@ -41,6 +39,7 @@ from Cerebrum.Utils import Factory
 from Cerebrum.utils import backoff
 from Cerebrum.utils import date_compat
 from Cerebrum.modules.entity_expire.entity_expire import EntityExpiredError
+from Cerebrum.modules.spread_expire import SpreadExpire
 from Cerebrum.modules.tasks import task_models
 from Cerebrum.modules.tasks import queue_handler
 from Cerebrum.modules.no.uit import POSIX_GROUP_NAME
@@ -316,32 +315,6 @@ def create_account(owner, default_expire_date):
     return account
 
 
-#
-# TODO: Update spread map
-#
-# This assumes that the Greg roles maps one-to-one with affiliations - if two
-# or more roles map to the *same* affiliation in Cerebrum, but needs
-# *different* spreads, then we'll need to re-work this mapping and
-# `calculate_greg_spreads`.
-#
-# If this is the case, we'll also need additional information from Greg on
-# which roles the owner has.
-#
-SPREAD_MAP = {
-    'TILKNYTTET/emeritus': ('people@ldap', 'system@ldap'),
-    'MANUELL/gjest': ('people@ldap', 'system@ldap', 'cristin@uit'),
-    'TILKNYTTET/fagperson': ('AD_account',),
-}
-# spread_list = [
-#     co.spread_uit_ldap_people,
-#     co.spread_uit_fronter_account,
-#     co.spread_uit_ldap_system,
-#     co.spread_uit_ad_account,
-#     co.spread_uit_cristin,
-#     co.spread_uit_exchange,
-# ]
-
-
 def calculate_greg_spreads(const, affiliations):
     """
     Get selection of spreads to add from affiliations.
@@ -349,18 +322,113 @@ def calculate_greg_spreads(const, affiliations):
     :type const: Cerebrum.Constants.ConstantsBase
     :param affiliations: sequence of aff tuples (aff-status, ou-id)
     """
-    spreads = set((const.spread_uit_ldap_system,))
+    # Currently, all active user accounts @ UiT should have the same spreads.
+    spreads = set((
+        const.spread_uit_ad_account,
+        const.spread_uit_exchange,
+        const.spread_uit_ldap_people,
+        const.spread_uit_ldap_system,
+    ))
 
-    for aff_status, _ in affiliations:
-        aff_string = six.text_type(aff_status)
-        for spread in SPREAD_MAP.get(aff_string, ()):
-            spreads.add(const.get_constant(const.Spread, spread))
+    # for aff_status, _ in affiliations:
+    #     aff_string = six.text_type(aff_status)
+    #     for spread in SPREAD_MAP.get(aff_string, ()):
+    #         spreads.add(const.get_constant(const.Spread, spread))
 
-    # Exchange-spread follows AD-spread.
-    if const.spread_uit_ad_account in spreads:
-        spreads.add(const.spread_uit_exchange)
+    # # Exchange-spread follows AD-spread.
+    # if const.spread_uit_ad_account in spreads:
+    #     spreads.add(const.spread_uit_exchange)
 
     return spreads
+
+
+def sync_spread_expire(entity, spread_expire_map):
+    """
+    Update spread expire-date for a selection of spreads.
+
+    :param Entity entity: the object to sync spread-expire to
+    :param dict spread_expire_map: spread -> expire-date mapping
+    """
+    # TODO: We should probably use this function everywhere spread-expire is
+    # updated.  I.e.: Rather than doing:
+    #
+    #   for spread in need_spreads:
+    #       account.set_spread_expire(spread, expire_date=expire_date)
+    #
+    # We should do:
+    #
+    #   spread_expire = {spread: expire_date for spread in need_spreads}
+    #   sync_spread_expire(account, spread_expire)
+    #
+    expire_db = SpreadExpire(entity._db)
+    entity_id = int(entity.entity_id)
+    const = entity.const
+
+    # Fetch current spread expire dates (and register duplicate or obsolete
+    # entries)
+    current = {}
+    obsolete = set()
+    duplicate = set()
+    for row in expire_db.search(entity_id=entity_id):
+        spread_int = row['spread']
+        expire_date = date_compat.get_date(row['expire_date'])
+        try:
+            spread = const.get_constant(const.Spread, spread_int)
+        except LookupError as e:
+            # spread_expire has no foreign key to spread_code.code
+            logger.debug("invalid spread in spread-expire (%r, %r, %r): %s",
+                         row['entity_id'], row['spread'], row['expire_date'],
+                         e)
+            obsolete.add(spread_int)
+            continue
+
+        # no (entity_id, spread) primary key - multiple expire date entries
+        # can exist for the same spread!
+        if spread in current:
+            if expire_date > current[spread]:
+                current[spread] = expire_date
+            duplicate.add(spread)
+        else:
+            current[spread] = expire_date
+
+    # We don't need to keep around expire date values for spreads that no
+    # longer exist:
+    # - if the same spread gets re-added it'll likely get another code!
+    # - if a spread gets re-added using this code, it'll probably not be the
+    #   same spread!
+    for spread_int in obsolete:
+        logger.info("Removing spread-expire for entity id=%r: "
+                    "obsolete spread=%r",
+                    entity_id, spread_int)
+        expire_db.delete(entity_id, spread_int)
+
+    # Identify expire dates that actually needs update (i.e. prolongs the
+    # current lifetime of the spread)
+    update = {}
+    for spread, expire_date in spread_expire_map.items():
+        if spread in current and expire_date > current[spread]:
+            # prolong existing expire date
+            update[spread] = expire_date
+        elif spread not in current:
+            # no existing expire date -- apply
+            update[spread] = expire_date
+
+    # Remove all duplicates by doing a delete() + set() cycle
+    for spread in duplicate:
+        expire_date = current[spread]
+        expire_db.delete(entity_id, spread)
+        if spread not in update:
+            logger.info("Resetting spread-expire for entity id=%r: "
+                        "duplicate spread=%s (%s)",
+                        entity_id, spread, expire_date)
+            expire_db.set(entity_id, spread, expire_date)
+
+    # Update spreads that needs a new expire-date
+    for spread, expire_date in update.items():
+        logger.info("Setting spread-expire for entity id=%r: "
+                    "spread=%s (%s -> %s)",
+                    entity_id, spread, current.get(spread), expire_date)
+        expire_db.set(entity_id, spread, expire_date)
 
 
 def update_greg_person(db, person_id, new_expire_date, _today=None):
@@ -495,22 +563,14 @@ def update_greg_person(db, person_id, new_expire_date, _today=None):
         need_spreads = set()
 
     # Ensure spreads added by this logic has an expire date
-    #
-    # Note: We set expire date here for spreads that may already be in
-    # place from other systems, not only the spreads that we end up adding!
-    # This has two consequences - we may *prolong* spread expire for
-    # existing spreads, but we may also end up *shortening* spread expire!
-    #
-    # TODO: Maybe only set expire date if it *adds* an expire date, or
-    # *prolongs* a current expire date?
-    for spread in need_spreads:
-        account.set_spread_expire(entity_id=int(account.entity_id),
-                                  spread=spread,
-                                  expire_date=new_expire_date)
+    sync_spread_expire(account,
+                       {spread: new_expire_date for spread in need_spreads})
 
     current_spreads = set(const.get_constant(const.Spread, row['spread'])
                           for row in account.get_spread())
     spreads_to_add = need_spreads - current_spreads
     for spread in spreads_to_add:
+        logger.info("Adding spread to account %s (id=%r): %s",
+                    account.account_name, account.entity_id, spread)
         account.add_spread(spread)
         account.set_home_dir(spread)
