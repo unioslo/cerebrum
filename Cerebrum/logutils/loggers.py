8a8a2d5f397ee4da1e053b4c54b849523f686604
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2023-07-14 14:05:21 +0200

    Fix API issue with Cerebrum.logutils in Python 3
    This commit fixes some issues with `Cerebrum.logutils` in Python 3,
    where logging warnings won't work correctly.  This is fixed by removing
    our custom stack frame inspector (`findCaller`) which is no longer
    needed.  It seems the caller is identified correctly in both Python 2
    and Python 3 without this override.
    
    Issue: CRB-4083

--- Cerebrum/logutils/loggers.py
+++ Cerebrum/logutils/loggers.py
@@ -1,38 +1,32 @@
 # encoding: utf-8
-""" Cerebrum custom loggers.
+"""
+Cerebrum custom loggers.
 
 This module contains the CerebrumLogger, which adds additional log levels and
 associated API-methods to the python logger.
 
-In order to use the additional API methods in a module level logger, you have to
-install the logger class. This can be achieved by doing:
+In order to use the additional API methods in a module level logger, you have
+to install the logger class. This can be achieved by doing:
+::
 
-
+    CerebrumLogger.install()
 
 """
-from __future__ import absolute_import, unicode_literals
-
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
 import logging
-import os
-from inspect import currentframe
 
 
 # Additional debug levels. They will be added to the logging framework
 DEBUG1 = logging.DEBUG - 1
-DEBUG2 = DEBUG1-1
-DEBUG3 = DEBUG2-1
-DEBUG4 = DEBUG3-1
-DEBUG5 = DEBUG4-1
-
-
-try:
-    if any(__file__.lower().endswith(ext) for ext in ['.pyc', '.pyo']):
-        _srcfile = __file__[:-4] + '.py'
-    else:
-        _srcfile = __file__
-    _srcfile = os.path.normcase(_srcfile)
-except:
-    _srcfile = None
+DEBUG2 = DEBUG1 - 1
+DEBUG3 = DEBUG2 - 1
+DEBUG4 = DEBUG3 - 1
+DEBUG5 = DEBUG4 - 1
 
 
 class CerebrumLogger(logging.Logger, object):
@@ -42,29 +36,6 @@ class CerebrumLogger(logging.Logger, object):
 
     def __init__(self, name, level=logging.NOTSET):
         super(CerebrumLogger, self).__init__(name, level)
-
-    def findCaller(self):
-        """Find the stack frame of the caller.
-
-        This function overloads the default implementation. This is so that we
-        can ignore this module when looking through the call stack.
-
-        """
-        f = currentframe().f_back
-        rv = "(unknown file)", 0, "(unknown function)"
-        while hasattr(f, "f_code"):
-            co = f.f_code
-            filename = os.path.normcase(co.co_filename)
-            if (filename == _srcfile or
-                    filename.find("logging/__init__.py") >= 0):
-                f = f.f_back
-                continue
-            rv = (filename, f.f_lineno, co.co_name)
-            break
-        return rv
-
-    def callHandlers(self, record):
-        super(CerebrumLogger, self).callHandlers(record)
 
     def debug1(self, msg, *args, **kwargs):
         if self.isEnabledFor(DEBUG1):
@@ -91,7 +62,7 @@ class CerebrumLogger(logging.Logger, object):
         # TODO: Deprecate this insanity
         self._indent = max(0, int(indent))
 
-    def makeRecord(self, *args, **kwargs):
+    def makeRecord(self, *args, **kwargs):  # noqa: N802
         r = super(CerebrumLogger, self).makeRecord(*args, **kwargs)
         setattr(r, 'indent', getattr(self, '_indent', 0))
         return r
