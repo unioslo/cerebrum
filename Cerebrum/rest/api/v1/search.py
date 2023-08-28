c32fb355f99e02d356cc4c2221ba720515d56208
Andreas Ellewsen <andreas@ellewsen.no>
2023-08-28 14:06:39 +0200

    Remove unused import

--- Cerebrum/rest/api/v1/search.py
+++ Cerebrum/rest/api/v1/search.py
@@ -25,7 +25,6 @@ from __future__ import unicode_literals
 from flask_restx import Namespace, Resource, abort
 from flask_restx import fields as base_fields
 
-from Cerebrum.Constants import ConstantsBase
 from Cerebrum.Entity import EntityExternalId
 from Cerebrum.Utils import Factory
 from Cerebrum import Errors
