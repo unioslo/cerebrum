d7cab44310085fea9b3e28a011e2dd3fe984c77d
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2024-02-23 13:21:35 +0100

    Replace constants wiht legacy_constants in tests
    This commit cleans up and update some old `cereconf` modules for tests.
    These were horribly out of date, and should probably be removed...
    
    Issue: CRB-3166

--- testsuite/docker/test-config/uio/cereconf.py
+++ testsuite/docker/test-config/uio/cereconf.py
@@ -21,37 +21,38 @@
 
 from cereconf_test import *
 
-CLASS_CHANGELOG = [
-    'Cerebrum.modules.EventLog/EventLog',
-    'Cerebrum.modules.ChangeLog/ChangeLog', ]
-
-CLASS_CONSTANTS = [
-    'Cerebrum.modules.no.Constants/ConstantsCommon',
-    'Cerebrum.modules.no.Constants/ConstantsHigherEdu',
+CLASS_CONSTANTS = (
     'Cerebrum.modules.no.uio.Constants/Constants',
+    'Cerebrum.modules.no.uio.legacy_constants/LegacyConstants',
+    'Cerebrum.modules.no.Constants/ConstantsCommon',
     'Cerebrum.modules.exchange.Constants/Constants',
     'Cerebrum.modules.exchange.TargetSystemConstants/TargetSystemConstants',
     'Cerebrum.modules.PosixConstants/Constants',
     'Cerebrum.modules.bofhd.bofhd_constants/Constants',
+    'Cerebrum.modules.bofhd_requests.constants/Constants',
     'Cerebrum.modules.EmailConstants/EmailConstants',
     'Cerebrum.modules.no.uio.EphorteConstants/EphorteConstants',
+    'Cerebrum.modules.no.dfo.constants/DfoConstants',
+    'Cerebrum.modules.orgreg.constants/OrgregConstants',
+    'Cerebrum.modules.otp.constants/OtpConstants',
     'Cerebrum.modules.password_notifier.constants/Constants',
     'Cerebrum.modules.guest.Constants/GuestConstants',
     'Cerebrum.modules.consent.ConsentConstants/Constants',
     'Cerebrum.modules.cim.constants/ConstantsMixin',
     'Cerebrum.modules.disk_quota.constants/Constants',
     'Cerebrum.modules.feide.Constants/Constants',
+    'Cerebrum.modules.greg.constants/GregConstants',
     'Cerebrum.modules.no.uio.voip.Constants/VoipConstants',
     'Cerebrum.modules.no.uio.voip.Constants/VoipAuthConstants',
-    'Cerebrum.modules.dns.DnsConstants/Constants',
     'Cerebrum.modules.EntityTraitConstants/Constants',
-    'Cerebrum.modules.hostpolicy.HostPolicyConstants/Constants'
-]
+)
 
-CLASS_CL_CONSTANTS = [
-    'Cerebrum.modules.hostpolicy.HostPolicyConstants/CLConstants',
+CLASS_CL_CONSTANTS = (
+    'Cerebrum.modules.no.uio.legacy_constants/LegacyChangelogConstants',
+    'Cerebrum.modules.otp.constants/OtpChangeTypeConstants',
+    'Cerebrum.modules.ou_disk_mapping.constants/CLConstants',
+    'Cerebrum.modules.apikeys.constants/CLConstants',
     'Cerebrum.modules.disk_quota.constants/CLConstants',
-    'Cerebrum.modules.dns.DnsConstants/CLConstants',
     'Cerebrum.modules.feide.Constants/CLConstants',
     'Cerebrum.modules.consent.ConsentConstants/CLConstants',
     'Cerebrum.modules.no.uio.EphorteConstants/CLConstants',
@@ -60,18 +61,30 @@ CLASS_CL_CONSTANTS = [
     'Cerebrum.modules.exchange.CLConstants/CLConstants',
     'Cerebrum.modules.ad2.CLConstants/CLConstants',
     'Cerebrum.Constants/CLConstants',
-]
+)
 
-CLASS_ENTITY = ['Cerebrum.modules.EntityTrait/EntityTrait',]
+CLASS_CHANGELOG = (
+    'Cerebrum.modules.event_publisher.eventlog/EventLog',
+    'Cerebrum.modules.EventLog/EventLog',
+    'Cerebrum.modules.ChangeLog/ChangeLog',
+    'Cerebrum.modules.audit.auditlog/AuditLog',
+)
 
-# TODO: The commented mixins are not testable
+CLASS_ENTITY = (
+    'Cerebrum.modules.EntityTrait/EntityTrait',
+    'Cerebrum.modules.bofhd.bofhd_mixins/BofhdAuthEntityMixin',
+)
+
 CLASS_ACCOUNT = (
-    # 'Cerebrum.modules.no.uio.Account/AccountUiOMixin',
+    'Cerebrum.modules.no.uio.voip.voipAuthAccountMixin/VoipAuthAccountMixin',
+    'Cerebrum.modules.no.uio.Account/AccountUiOMixin',
+    'Cerebrum.modules.apikeys.mixins/ApiMappingAccountMixin',
     'Cerebrum.modules.pwcheck.history/PasswordHistoryMixin',
     'Cerebrum.modules.AccountExtras/AutoPriorityAccountMixin',
     'Cerebrum.modules.Email/AccountEmailMixin',
     'Cerebrum.modules.Email/AccountEmailQuotaMixin',
-    # 'Cerebrum.modules.gpg.password/AccountPasswordEncrypterMixin',
+    'Cerebrum.modules.gpg.password/AccountPasswordEncrypterMixin',
+    'Cerebrum.modules.no.uio.changed_password_notifier/NotifyChangePasswordMixin',  # noqa: E501
 )
 
 CLASS_GROUP = (
@@ -79,19 +92,43 @@ CLASS_GROUP = (
     'Cerebrum.modules.posix.mixins/PosixGroupMixin',
     'Cerebrum.modules.exchange.mixins/SecurityGroupMixin',
     'Cerebrum.modules.exchange.mixins/DistributionGroupMixin',
+    'Cerebrum.modules.feide.service/FeideServiceAuthnLevelMixin',
     'Cerebrum.Group/Group',
 )
 
-CLASS_OU = ('Cerebrum.modules.no.Stedkode/Stedkode',)
-CLASS_DISK = ('Cerebrum.modules.disk_quota.mixins/DiskQuotaMixin',)
 CLASS_PERSON = (
+    'Cerebrum.modules.otp.mixins/OtpPersonMixin',
     'Cerebrum.modules.Email/PersonEmailMixin',
     'Cerebrum.modules.no.Person/PersonFnrMixin',
     'Cerebrum.modules.consent.Consent/EntityConsentMixin',
-    'Cerebrum.modules.no.uio.Person/PersonUiOMixin'
+    'Cerebrum.modules.feide.service/FeideServiceAuthnLevelMixin',
+    'Cerebrum.modules.no.uio.Person/PersonUiOMixin',
 )
-CLASS_POSIX_USER = ('Cerebrum.modules.no.uio.PosixUser/PosixUserUiOMixin',
-                    'Cerebrum.modules.PosixUser/PosixUser')
-CLASS_POSIXLDIF = ('Cerebrum.modules.no.uio.PosixLDIF/PosixLDIF_UiOMixin',
-                   'Cerebrum.modules.PosixLDIF/PosixLDIFRadius',
-                   'Cerebrum.modules.PosixLDIF/PosixLDIF')
+
+CLASS_OU = (
+    'Cerebrum.modules.ou_disk_mapping.mixins/OUMixin',
+    'Cerebrum.modules.no.Stedkode/Stedkode',
+)
+
+CLASS_DISK = (
+    'Cerebrum.modules.ou_disk_mapping.mixins/DiskMixin',
+    'Cerebrum.modules.disk_quota.mixins/DiskQuotaMixin',
+)
+
+CLASS_POSIX_USER = (
+    'Cerebrum.modules.no.uio.PosixUser/PosixUserUiOMixin',
+    'Cerebrum.modules.PosixUser/PosixUser',
+)
+
+CLASS_POSIX_GROUP = (
+    'Cerebrum.modules.no.uio.PosixGroup/PosixGroupUiOMixin',
+    'Cerebrum.modules.PosixGroup/PosixGroup',
+)
+
+CLASS_POSIXLDIF = (
+    'Cerebrum.modules.otp.otp_ldif_utils/RadiusOtpMixin',
+    'Cerebrum.modules.no.uio.PosixLDIF/PosixLDIF_UiOMixin',
+    'Cerebrum.modules.PosixLDIF/PosixLDIFRadius',
+    'Cerebrum.modules.PosixLDIF/PosixLDIFMail',
+    'Cerebrum.modules.PosixLDIF/PosixLDIF',
+)
