83c5286add2c968b1eb5d3a453d39461f6c6d5e7
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2024-03-07 13:31:22 +0100

    Change attr and description of legacy constants.
    This commit updates the attribute name and constant description of
    legacy dns constants to indicate that these should not be used.
    
    Also improves legacy constant module docstrings.
    
    Issue: CRB-3165

--- Cerebrum/modules/no/uio/legacy_constants.py
+++ Cerebrum/modules/no/uio/legacy_constants.py
@@ -20,10 +20,25 @@
 """
 Deprecated constants.
 
-This module contains collections of CerebrumCode types that are either:
+This module contains collections of CerebrumCode types that *should* be
+removed, but can't because of various constraints.  Note that the module is
+specific for UiO - other deployments should have their own legacy constant
+collections, if needed.
 
-1. Scheduled from removal
-2. *Should* be removed, but can't be because of various constraints
+DNS and hostpolicy
+-------------------
+These constants should not occur in their referenced tables (e.g.
+entity_info, entity_trait, etc...), but they may still be referenced as
+change params in change_log and audit_log records.  We *mainly* keep these
+around because:
+
+- Change/audit records may refer to the constants by their numeric/code value,
+  which makes no sense if we don't have a code to human readable mapping.
+
+- Certain change types may require these values to exist in CLASS_CONSTANTS for
+  formatting.  Showing history for entities with these changes may break if the
+  constant disappears.
+
 """
 from __future__ import (
     absolute_import,
@@ -39,99 +54,97 @@ _cereconst = Cerebrum.Constants
 
 
 class _LegacyDnsConstants(Cerebrum.Constants.Constants):
-    """ Common DNS module constants. """
+    """
+    Deprecated constants from the deleted Cerebrum.modules.dns module.
+    """
 
     #
     # DNS Entities
     #
-    entity_dns_cname = _cereconst._EntityTypeCode(
-        'cname',
-        'cname - see table "cerebrum.cname_info" and friends.',
+    _legacy_entity_dns_cname = _cereconst._EntityTypeCode(
+        "cname",
+        "Deprecated entity type - Cerebrum.modules.dns CNAME record",
     )
-    entity_dns_host = _cereconst._EntityTypeCode(
+    _legacy_entity_dns_host = _cereconst._EntityTypeCode(
         # name-clash with existing entity_type
-        'dns_host',
-        'dns_host - see table "cerebrum.dns_host_info" and friends.',
+        "dns_host",
+        "Deprecated entity type - Cerebrum.modules.dns HINFO record",
     )
-    entity_dns_a_record = _cereconst._EntityTypeCode(
-        'a_record',
-        'a_record - see table "cerebrum.a_record_info" and friends.',
+    _legacy_entity_dns_a_record = _cereconst._EntityTypeCode(
+        "a_record",
+        "Deprecated entity type - Cerebrum.modules.dns A record",
     )
-    entity_dns_aaaa_record = _cereconst._EntityTypeCode(
-        'aaaa_record',
-        'aaaa_record - see table "cerebrum.aaaa_record_info" and friends.',
+    _legacy_entity_dns_aaaa_record = _cereconst._EntityTypeCode(
+        "aaaa_record",
+        "Deprecated entity type - Cerebrum.modules.dns AAAA record",
     )
-    entity_dns_owner = _cereconst._EntityTypeCode(
-        'dns_owner',
-        'dns_owner - see table "cerebrum.dns_owner" and friends.',
+    _legacy_entity_dns_owner = _cereconst._EntityTypeCode(
+        "dns_owner",
+        "Deprecated entity type - Cerebrum.modules.dns DNS label",
     )
-    entity_dns_ip_number = _cereconst._EntityTypeCode(
-        'dns_ip_number',
-        'dns_ip_number - see table "cerebrum.dns_ip_number" and friends.',
+    _legacy_entity_dns_ip_number = _cereconst._EntityTypeCode(
+        "dns_ip_number",
+        "Deprecated entity type - Cerebrum.modules.dns reserved IPv4 address"
     )
-    entity_dns_ipv6_number = _cereconst._EntityTypeCode(
-        'dns_ipv6_number',
-        'dns_ipv6_number - see table "cerebrum.dns_ipv6_number" and friends.',
+    _legacy_entity_dns_ipv6_number = _cereconst._EntityTypeCode(
+        "dns_ipv6_number",
+        "Deprecated entity type - Cerebrum.modules.dns reserved IPv6 address"
     )
-    entity_dns_subnet = _cereconst._EntityTypeCode(
-        'dns_subnet',
-        'dns_subnet - see table "cerebrum.dns_subnet" and friends.',
+    _legacy_entity_dns_subnet = _cereconst._EntityTypeCode(
+        "dns_subnet",
+        "Deprecated entity type - Cerebrum.modules.dns IPv4 subnet"
     )
-    entity_dns_ipv6_subnet = _cereconst._EntityTypeCode(
-        'dns_ipv6_subnet',
-        'dns_ipv6_subnet - see table "cerebrum.dns_ipv6_subnet" and friends.',
+    _legacy_entity_dns_ipv6_subnet = _cereconst._EntityTypeCode(
+        "dns_ipv6_subnet",
+        "Deprecated entity type - Cerebrum.modules.dns IPv6 subnet"
     )
 
     #
     # Namespace for DNS names
     #
-    dns_owner_namespace = _cereconst._ValueDomainCode(
-        'dns_owner_ns',
-        'Domain for dns_owners',
+    _legacy_dns_owner_namespace = _cereconst._ValueDomainCode(
+        "dns_owner_ns",
+        "Deprecated namespace - Cerebrum.modules.dns label names",
     )
 
     #
-    # NIS host group?
+    # NIS host group
     #
-    spread_uio_machine_netgroup = _cereconst._SpreadCode(
-        'NIS_mng@uio',
+    _legacy_spread_uio_machine_netgroup = _cereconst._SpreadCode(
+        "NIS_mng@uio",
         _cereconst.Constants.entity_group,
-        'Machine netgroup in NIS domain "uio"',
+        "Deprecated spread - Cerebrum.modules.dns spread for host groups",
     )
 
     #
     # Traits
     #
-    trait_dns_contact = _EntityTraitCode(
-        'dns_contact',
-        entity_dns_owner,
-        """Contact information (e-mail address) for the host.""",
+    _legacy_trait_dns_contact = _EntityTraitCode(
+        "dns_contact",
+        _legacy_entity_dns_owner,
+        "Deprecated trait - Cerebrum.modules.dns owner contact info",
     )
-    trait_dns_comment = _EntityTraitCode(
-        'dns_comment',
-        entity_dns_owner,
-        """A freeform comment about the host.""",
+    _legacy_trait_dns_comment = _EntityTraitCode(
+        "dns_comment",
+        _legacy_entity_dns_owner,
+        "Deprecated trait - Cerebrum.modules.dns owner description",
     )
 
 
 class _LegacyHostpolicyConstants(_cereconst.Constants):
     """
-    Legacy entity-related constants for Cerebrum.modules.hostpolicy.
-
-    They should not be used, or referred to from anywhere.  These constants
-    need to be kept around, as the base entities and entity-related audit log
-    records are still present in the database.
+    Deprecated constants from the deleted Cerebrum.modules.hostpolicy module.
     """
     _legacy_entity_hostpolicy_atom = _cereconst._EntityTypeCode(
-        'hostpolicy_atom',
+        "hostpolicy_atom",
         "Deprecated entity type - was part of Cerebrum.modules.hostpolicy",
     )
     _legacy_entity_hostpolicy_role = _cereconst._EntityTypeCode(
-        'hostpolicy_role',
+        "hostpolicy_role",
         "Deprecated entity type - was part of Cerebrum.modules.hostpolicy",
     )
     _legacy_hostpolicy_component_namespace = _cereconst._ValueDomainCode(
-        'hostpol_comp_ns',
+        "hostpol_comp_ns",
         "Deprecated namespace - was part of Cerebrum.modules.hostpolicy",
     )
 
@@ -141,12 +154,16 @@ class LegacyConstants(
     _LegacyHostpolicyConstants,
     Cerebrum.Constants.Constants,
 ):
-    """ Legacy constants to keep around for now. """
+    """
+    Legacy constants to keep.
+    """
     pass
 
 
 class LegacyChangelogConstants(
     Cerebrum.Constants.CLConstants,
 ):
-    """ Legacy constants to keep around for now. """
+    """
+    Legacy changelog constants to keep.
+    """
     pass
