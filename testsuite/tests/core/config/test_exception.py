7dc7dd4fc643595fa48296502123edf8d7c7124b
Fredrik Larsen <fredrik.larsen@usit.uio.no>
2024-05-21 16:50:32 +0200

    Fix PY3 issues in `Cerebrum.config.errors`
    This commit fixes some Python 3 compatibility issues in
    `Cerebrum.config.errors`, and improves test coverage.
    
    Issue: CRB-4223

--- testsuite/tests/core/config/test_exception.py
+++ testsuite/tests/core/config/test_exception.py
@@ -1,24 +1,36 @@
 # -*- coding: utf-8 -*-
+""" Unit tests for :mod:`Cerebrum.config.errors`. """
 import pytest
+import six
+
+from Cerebrum.config.errors import ConfigurationError
+
+
+def test_error_init_empty():
+    err = ConfigurationError()
+    assert not err.errors
+
+
+def test_error_init():
+    error_map = {
+        'foo': ValueError("invalid foo"),
+        'bar': TypeError("invalid bar"),
+    }
+    err = ConfigurationError(error_map)
+    assert len(err.errors) == 2
+    assert err.errors['foo'] == error_map['foo']
+    assert err.errors['bar'] == error_map['bar']
 
 
 @pytest.fixture
-def error_cls():
-    from Cerebrum.config.errors import ConfigurationError
-    return ConfigurationError
-
-
-@pytest.fixture
-def empty_error(error_cls):
-    err = error_cls()
+def empty_error():
+    err = ConfigurationError()
     return err
 
 
 @pytest.fixture
-def error(error_cls):
-    err = error_cls()
-    err.set_error('test', Exception("Test"))
-    return err
+def error():
+    return ConfigurationError({'test': Exception("Test")})
 
 
 def test_set_error(empty_error):
@@ -41,9 +53,18 @@ def test_bool_config_error(empty_error, error):
     assert error
 
 
-def test_add_error_from_error(error_cls, error):
-    error2 = error_cls()
-    error2.set_error('test', Exception("Test"))
+def test_add_error_from_error(error):
+    error2 = ConfigurationError({'test': Exception("Test")})
     error.set_error('group', error2)
     assert 'test' in error.errors
     assert 'group.test' in error.errors
+
+
+def test_str_config_error(error):
+    strval = six.text_type(error)
+    assert strval.startswith("Errors in ")
+
+
+def test_repr_config_error(error):
+    strval = repr(error)
+    assert strval.startswith("ConfigurationError(")
