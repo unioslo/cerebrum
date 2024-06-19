c8bc46d071134fb1e1413687352481a40df94df1
Tobias V. Langhoff <tvl@usit.uio.no>
2024-06-19 12:47:31 +0200

    Fix remove_email_forwards log order

--- contrib/no/uio/remove_email_forwards.py
+++ contrib/no/uio/remove_email_forwards.py
@@ -165,6 +165,8 @@ def main(args=None):
     prog_name = parser.prog.rsplit('.', 1)[0]
     logger.info(args.send_notification)
 
+    logger.info('Starting {}'.format(prog_name))
+
     db = Factory.get('Database')()
     db.cl_init(change_program=prog_name)
 
@@ -175,7 +177,7 @@ def main(args=None):
 
     iterate_persons(db, args, config)
 
-    logger.info('Starting {}'.format(prog_name))
+    logger.info('Done {}'.format(prog_name))
 
 
 if __name__ == "__main__":
