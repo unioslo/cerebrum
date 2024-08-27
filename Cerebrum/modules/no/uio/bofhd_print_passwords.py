d149449ab5fd17922cee2acf06153895d6655ca4
Tobias V. Langhoff <tvl@usit.uio.no>
2024-08-27 14:09:17 +0200

    Remove Pullprint

--- Cerebrum/modules/no/uio/bofhd_print_passwords.py
+++ Cerebrum/modules/no/uio/bofhd_print_passwords.py
@@ -34,17 +34,15 @@ class BofhdExtension(base.BofhdExtension):
     def _get_default_printer(self, session):
         """ Get a default printer for the prompt. """
         previous = super(BofhdExtension, self)._get_default_printer(session)
-        return previous or 'pullprint'
+        return previous or cereconf.PRINT_PRINTER
 
     def _get_printer(self, session, template):
         """ Get printer preset. """
-        if template.get('type', '') == cereconf.PRINT_PAPERCUT:
-            return cereconf.PRINT_PAPERCUT
         return cereconf.PRINT_PRINTER
 
     def _can_set_spool_user(self, session, template):
         """ Can spool user be set? """
-        return self._get_printer(session, template) in (cereconf.PRINT_PRINTER, cereconf.PRINT_PAPERCUT)
+        return self._get_printer(session, template) == cereconf.PRINT_PRINTER
 
     def _get_group_account_mappings(self, account):
         group = self._get_group(account.owner_id, idtype='id')
